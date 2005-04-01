a07f66e13cc03e96c1d69a005824c1878d6af6e3
Bahadır Kandemir <kandemir@gmail.com>
2005-04-01 22:48:16 +0000

    Gettext araç araçları yüklendi

--- pardil/sys.gettext.php
+++ pardil/sys.gettext.php
@@ -1,5 +1,5 @@
 <?php
-  setlocale(LC_ALL, CONF_LOCALE . '.utf8');
+  setlocale(LC_ALL, CONF_LOCALE . '.utf-8');
   bindtextdomain(CONF_LOCALE, './l10n');
   bind_textdomain_codeset(CONF_LOCALE, 'utf-8');
   textdomain(CONF_LOCALE);
