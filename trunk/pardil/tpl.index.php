dbfa19d25eaeba5077f46c60924ef2017ed77b6e
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2005-04-09 18:37:58 +0000

    Ana sayfaya öneri görüntüleme ve öneri ekleme sayfaları bağlantısı eklendi.

--- trunk/pardil/tpl.index.php
+++ trunk/pardil/tpl.index.php
@@ -10,6 +10,10 @@
         <div class="proposal">
           <p>
             <b>&raquo;</b> <a href="login.php"><?php echo __('User Login'); ?></a>
+            <br/>
+            <b>&raquo;</b> <a href="newproposal.php"><?php echo __('New Proposal'); ?></a>
+            <br/>
+            <b>&raquo;</b> <a href="proposal.php"><?php echo __('Proposals'); ?></a>
           </p>
           <p>
             <b><?php echo __('Session Information:'); ?></b>
