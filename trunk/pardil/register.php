953153f80506f788d60dbc169184c0523576e163
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2005-04-10 11:34:15 +0000

    URL'lerde fazladan "/" karakteri kullanma hatası giderildi

--- trunk/pardil/register.php
+++ trunk/pardil/register.php
@@ -50,7 +50,7 @@
     if ($int_status == 0) {
       // E-posta gönderimi
       $str_subject = sprintf(__('%s Account Activation'), getop('site_name'));
-      $str_url = $_PCONF['site_url'] . '/activate.php?code=' . $str_code . '&user=' . $int_user;
+      $str_url = $_PCONF['site_url'] . 'activate.php?code=' . $str_code . '&user=' . $int_user;
       $str_body = sprintf(__("Hello,\n\nTo complete your registration at %1\$s, please visit the address below:\n\n%2\$s\n\nThanks,\n%3\$s Team"), getop('site_url'), $str_url, getop('site_name'));
       $bln_mail = mail($_POST['register_email'], $str_subject, $str_body);
       
