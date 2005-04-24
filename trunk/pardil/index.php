c6bc252608d128967533485e58bd46cdbb8c8070
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2005-04-24 13:23:25 +0000

    Ana sayfaya önerilerin listesi eklendi. (Öneri Listesi sayfası eklendiğinde ana sayfadan kaldırılacak.)

--- trunk/pardil/index.php
+++ trunk/pardil/index.php
@@ -8,8 +8,16 @@
   
   require('class/class.template.php');
 
+  $str_sql = sprintf('SELECT pardil_main.id, pardil_main.title FROM pardil_main');
+  $res_sql = mysql_query($str_sql);
+  $arr_list = array();
+  while ($arr_fetch = mysql_fetch_array($res_sql, MYSQL_ASSOC)) {
+    $arr_list[] = $arr_fetch;
+  }
+
   $_PCONF['title'] = $_PCONF['site_title'];
   $obj_page = new template('tpl/tpl.index.php');
   $obj_page->setvar('_PSESSION', $_PSESSION);
+  $obj_page->setvar('arr_list', $arr_list);
   $obj_page->flush();
 ?>
