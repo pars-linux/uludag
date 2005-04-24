d5cbfddbb3417337d22a4b4fb0ed6e5dddba42e2
Bahadır Kandemir <kandemir@gmail.com>
2005-04-24 11:56:13 +0000

    Prosedürler gibi, sorguların da ayrı dosyalara taşınmasına başlandı. Öneri görüntüleme ve değiştirme sayfalarındaki öneri numarası ve sürümü denetimindeki hatalar giderildi.

--- pardil/sys/sys.procedures.php
+++ pardil/sys/sys.procedures.php
@@ -9,12 +9,25 @@
         include('procedures/' . $arr_files[$i]);
       }
     }
+    $arr_files = scandir('queries');
+    for ($i = 0; $i < count($arr_files); $i++) {
+      if (substr($arr_files[$i], 0, 2) == 'q.') {
+        include('queries/' . $arr_files[$i]);
+      }
+    }
   }
   else {
-    $str_dir = "procedures";
+    $str_dir = 'procedures';
     $res_dir  = opendir($str_dir);
     while (false !== ($str_fname = readdir($res_dir))) {
       if (substr($str_fname, 0, 5) == 'proc.') {
+        include('procedures/' . $str_fname);
+      }
+    }
+    $str_dir = 'queries';
+    $res_dir  = opendir($str_dir);
+    while (false !== ($str_fname = readdir($res_dir))) {
+      if (substr($str_fname, 0, 2) == 'q.') {
         include('procedures/' . $str_fname);
       }
     }
