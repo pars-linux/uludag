9ecb669838db1703156ba8cdfd5a24e7571dda9b
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2005-04-29 13:34:00 +0000

    Dosyalara yorumlar eklendi.

--- trunk/pardil/sys/sys.database.php
+++ trunk/pardil/sys/sys.database.php
@@ -1,12 +1,17 @@
 <?php
+  
   if (defined('CONF_DATABASE_HOST')) {
-    mysql_connect(CONF_DATABASE_HOST, CONF_DATABASE_USER, CONF_DATABASE_PASS) or die(MSG_DATABASE_CONNECT_ERROR);
-    mysql_select_db(CONF_DATABASE_NAME) or die(MSG_DATABASE_SELECT_ERROR);
+    mysql_connect(CONF_DATABASE_HOST, CONF_DATABASE_USER, CONF_DATABASE_PASS) or die(__('Could not connect to database server.'));
+    mysql_select_db(CONF_DATABASE_NAME) or die(__('Could not select database.'));
   }
   else {
-    die('Configuration error.');
+    die(__e('Eksik konfigürasyon.'));
   }
 
+  /*
+    Yanıtı bir skaler veri olan sorgularda kullanılabilecek fonksiyon.
+    Değer dönmezse "false" değerini verir.
+  */
   function database_query_scalar($str_sql) {
     $res_sql = mysql_query($str_sql);
     if (mysql_num_rows($res_sql) == 1) {
@@ -18,6 +23,11 @@
     }
   }
   
+  /*
+    array('sütun' => 'yeni değer', ...) şeklindeki diziyi
+    UPDATE sorgusunda kullanılabilen "sütun='yeni değer', ..." formatına
+    çeviren fonksiyon
+  */
   function database_updateStr($arr_update) {
     $arr_tmp = array();
     foreach ($arr_update as $str_column => $mix_value) {
--- trunk/pardil/sys/sys.procedures.php
+++ trunk/pardil/sys/sys.procedures.php
@@ -2,35 +2,18 @@
 
   // 'procedures' dizini altındaki tüm prosedür tanımlarını yükle
 
-  if (function_exists('scandir')) {
-    $arr_files = scandir(dirname(__FILE__) . '/../procedures');
-    for ($i = 0; $i < count($arr_files); $i++) {
-      if (substr($arr_files[$i], 0, 5) == 'proc.') {
-        include(dirname(__FILE__) . '/../procedures/' . $arr_files[$i]);
-      }
-    }
-    $arr_files = scandir(dirname(__FILE__) . '/../queries');
-    for ($i = 0; $i < count($arr_files); $i++) {
-      if (substr($arr_files[$i], 0, 2) == 'q.') {
-        include(dirname(__FILE__) . '/../queries/' . $arr_files[$i]);
-      }
+  $arr_files = scandir(dirname(__FILE__) . '/../procedures');
+  for ($i = 0; $i < count($arr_files); $i++) {
+    if (substr($arr_files[$i], 0, 5) == 'proc.') {
+      include(dirname(__FILE__) . '/../procedures/' . $arr_files[$i]);
     }
   }
-  else {
-    $str_dir = dirname(__FILE__) . '/../procedures';
-    $res_dir  = opendir($str_dir);
-    while (false !== ($str_fname = readdir($res_dir))) {
-      if (substr($str_fname, 0, 5) == 'proc.') {
-        include($str_dir . '/' . $str_fname);
-      }
-    }
-    $str_dir = dirname(__FILE__) . '/../queries';
-    $res_dir  = opendir($str_dir);
-    while (false !== ($str_fname = readdir($res_dir))) {
-      if (substr($str_fname, 0, 2) == 'q.') {
-        include($str_dir . '/' . $str_fname);
-      }
+  
+  // 'queries' dizini altındaki tüm prosedür tanımlarını yükle
+  $arr_files = scandir(dirname(__FILE__) . '/../queries');
+  for ($i = 0; $i < count($arr_files); $i++) {
+    if (substr($arr_files[$i], 0, 2) == 'q.') {
+      include(dirname(__FILE__) . '/../queries/' . $arr_files[$i]);
     }
   }
-
 ?>
