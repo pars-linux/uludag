7b99be815add92cfdde9e7c1ebeffbe5c5be9408
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2005-04-29 16:28:09 +0000

    Sayfaların SQL sorgularından arındırılmasıne devam edildi.

--- trunk/pardil/denied.php
+++ trunk/pardil/denied.php
@@ -4,7 +4,7 @@
   
   require(dirname(__FILE__) . '/class/class.template.php');
 
-  $_PCONF['title'] = $_PCONF['site_name'] . ' - ' . __('Access Denied');
+  $_PCONF['title'] = getop('site_name') . ' - ' . __('Access Denied');
   $obj_page = new template(dirname(__FILE__) . '/tpl/tpl.denied.php');
   $obj_page->flush();
 ?>
--- trunk/pardil/editproposal_ok.php
+++ trunk/pardil/editproposal_ok.php
@@ -4,7 +4,7 @@
 
   require(dirname(__FILE__) . '/class/class.template.php');
 
-  $_PCONF['title'] = $_PCONF['site_name'] . ' - ' . __('Proposal Updated');
+  $_PCONF['title'] = getop('site_name') . ' - ' . __('Proposal Updated');
   $obj_page = new template(dirname(__FILE__) . '/tpl/tpl.editproposal_ok.php');
   $obj_page->setvar('int_proposal', $_GET['proposal']);
   $obj_page->setvar('dbl_revision', $_GET['revision']);
