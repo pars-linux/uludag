a942727c5153d3d7f7553cd4fc177dfff95d5273
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2005-05-10 09:11:00 +0000

    .htaccess dosyasının okunmamasından kaynaklanan hatalar giderildi.

--- trunk/pardil/sys.common.php
+++ trunk/pardil/sys.common.php
@@ -2,7 +2,7 @@
   // Min. konfigürasyonu yükle.
   require(dirname(__FILE__) . '/cfg/sys.define.php');
   
-  // 
+  // Özel fonksiyonları yükle.
   require(dirname(__FILE__) . '/sys/sys.lib.php');
   
   // Yerelleştirme dosyasını yükle.
@@ -17,8 +17,13 @@
   // Veritabanına kaydedilen konfigürasyonu yükle.
   require(dirname(__FILE__) . '/sys/sys.pconf.php');
 
-  // Aksi belirtilmedikçe oturum yönetim dosyasını yükle.
+  // Aksi belirtilmediyse oturum yönetim dosyasını yükle.
   if (!isset($_NOSESSION)) {
     require(dirname(__FILE__) . '/sys/sys.session.php');
+  }
+
+  // PHP konfigürasyonunu kontrol et.
+  if (get_magic_quotes_gpc() == 1) {
+    die(__('PHP\'s Magic Quotes option is set to ON, this is bad. It seems that this web server does not read .htaccess file. Please contact your server administrator.'));
   }
 ?>
