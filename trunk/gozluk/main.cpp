09cb904f6368e22d0913c1d397dfeb1fedbe26c9
kaya.oguz <kaya.oguz@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2005-05-16 14:46:34 +0000

    ulubot veritabani (sozluk.xml) icin bir grafik arayuz

--- trunk/gozluk/main.cpp
+++ trunk/gozluk/main.cpp
@@ -1,32 +1,14 @@
-/*
-  Copyright (c) 2004, TUBITAK/UEKAE
-
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
-
-  Please read the COPYING file.
-*/
-
 #include <qapplication.h>
 
-#include "gozlukwin.h"
+#include "anapencere.h"
 
-int main ( int argc,  char **argv )
+
+int main(int argc, char **argv)
 {
-    QApplication app ( argc,  argv );
-
-    GozlukWin win;
-    win.setFixedSize( 400, 400 );
-    win.show();
-
-    QObject::connect( &win, SIGNAL( signalQuit() ),
-                      &app, SLOT( quit() ) );
-
-    app.setMainWidget( &win );
-
-    return app.exec();
+	QApplication uyg(argc, argv);
+	anaPencere k;
+	k.show();
+	
+	uyg.setMainWidget(&k);
+	return uyg.exec();
 }
-
-
