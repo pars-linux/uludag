25e393718eab51f0b5bdb14ae253841bc80780f5
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2005-05-28 20:52:38 +0000

    Pardil'i taşıma işlemini tamamlıyorum.

--- trunk/pardil/tools/makepo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-. ./gettext.conf
-
-./getpo
-./createpo tr_TR.utf8
-vi ../locales/tr_TR.utf8/LC_MESSAGES/$APP_NAME.po
-./compilepo tr_TR.utf8
--- trunk/pardil/tpl/tpl.password_ok.php
+++
@@ -1,9 +0,0 @@
-<?php
-  include(dirname(__FILE__) . '/tpl.header.php');
-?>
-      <div id="content">
-        <h2><?php echo __('Temporary Password Sent'); ?></h2>
-      </div>
-<?php
-  include(dirname(__FILE__) . '/tpl.footer.php');
-?>
