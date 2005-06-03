ea79f0ec49c3a003393953d809d7df3d1d1ac345
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2005-06-03 23:50:46 +0000

    İskeletle oynamaya devam ediyorum.

--- trunk/pardil/ajax_xhr/init.php
+++ trunk/pardil/ajax_xhr/init.php
@@ -1,6 +1,6 @@
 <?php
 
-  function session_control($mix_data) {
+  function ajax_init() {
     if (isset($_COOKIE['ajax_session'])) {
       return array('session' => $str_session, 'user' => 'Pardus');
     }
@@ -9,6 +9,6 @@
 
   require '../ajax_base/xhr.php';
   $obj_xhr = new xhr();
-  $obj_xhr->register('session_control');
+  $obj_xhr->register('ajax_init');
   $obj_xhr->handle();
 ?>
