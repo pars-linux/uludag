b0885477668cab729cb04255dec56e682fa4a946
cartman <cartman@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2005-07-28 10:29:03 +0000

    Fixes

--- trunk/pardus_packager/src/packager_ui.cpp
+++ trunk/pardus_packager/src/packager_ui.cpp
@@ -3,6 +3,7 @@
   Licensed under GNU GPLv2 or later at your option
 */
 
+#include <klocale.h>
 #include <khtmlview.h>
 #include <khtml_part.h>
 
@@ -12,19 +13,24 @@
 Packager_UI::Packager_UI(QWidget* parent, const char* name)
   : Package_Manager(parent,name)
 {
-  KHTMLPart *khtmlpart =  new KHTMLPart (m_displayFrame);
-  HtmlWriter *htmlWriter = new HtmlWriter();
+  setCaption(i18n("Package Manager"));
+
+  khtmlPart =  new KHTMLPart (m_displayFrame);
+  htmlWriter = new HtmlWriter();
 
   QString html = htmlWriter->createHtml("Foo");
   html += htmlWriter->createHtml("Foo");
 
-  khtmlpart->begin();
-  khtmlpart->write(html);
-  khtmlpart->end();
-  khtmlpart->view()->resize(400, 300); 
+  khtmlPart->begin();
+  khtmlPart->write(html);
+  khtmlPart->end();
+  khtmlPart->view()->resize(400, 300); 
 }
 
 Packager_UI::~Packager_UI()
-{}
+{
+  delete khtmlPart;
+  delete htmlWriter;
+}
 
 #include "packager_ui.moc"
