136678a3645e5dac456c36a49fb8d0a3b223a431
cartman <cartman@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2005-07-29 13:04:36 +0000

    Temiz vede pak

--- trunk/pardus_packager/src/packagerui.cpp
+++ trunk/pardus_packager/src/packagerui.cpp
@@ -18,6 +18,8 @@ PackagerUI::PackagerUI(QWidget* parent, const char* name)
   khtmlPart =  new KHTMLPart (m_displayFrame);
   htmlWriter = new HtmlWriter();
 
+  khtmlPart->setJScriptEnabled(true);
+
   createUI();
 }
 
@@ -32,59 +34,16 @@ void PackagerUI::createUI()
   QStringList apps;
   apps << "Foo" << "Bar" << "Baz";
   QString html = htmlWriter->createHtml(apps);
+  // HACK for now
+  QString path = QString(getenv("PWD"))+QString("/functions.js");
+  kdDebug() << "Got " << path << endl;
 
-  QString startHtml = "<html>"
-       "<head>"
-        "   <script language=\"javascript\">"
-    "function linkClicked(link, text1, text2) {"
-    " var s = text1;"
-    " if (link.state) { link.state = false; s = text2; }"
-    "else link.state = true;"
-    " while (link.firstChild) "
-    "link.removeChild(link.firstChild); link.appendChild(document.createTextNode(s)); "
-    "}"
-    "function getItem(id)"
-    "{"
-      "var itm = false;"
-      "if(document.getElementById)"
-	"itm = document.getElementById(id);"
-      "else if(document.all)"
-	"itm = document.all[id];"
-      "else if(document.layers)"
-	"itm = document.layers[id];"
-      "return itm;"
-    "}"
-  "function toggleItem(id)"
-  "{"
-   " itm = getItem(id);"
-    "if(!itm)"
-     " return false;"
-    "if(itm.style.display == 'none')"
-     " itm.style.display = '';"
-  "else"
-   "   itm.style.display = 'none';"
-    "return false;"
-  "}"
-  "function toggleAll(dowhat)"
-  "{"
-   " var tags = document.getElementsByTagName('tbody');"
-    "if(!tags)"
-     " return false;"
-    "for(var i = 0; i < tags.length; i++)"
-      "{"
-	"if(tags[i].className == 'collapse_obj')"
-	  "{"
-	   " if(dowhat == 'collapse')"
-	      "tags[i].style.display = 'none';"
-  "else"
-	"      tags[i].style.display = '';"
-	  "}"
-   "}"
-  "return false;"
-  "}"
-  "</script>"
-  "</head>"
-  "<body onload=\"toggleAll('collapse')\">";
+  QString startHtml = 
+    "<html>"
+    "<head>"
+    "<script language=\"JavaScript\" src=\""+path+"\"></script>"
+    "</head>"
+    "<body onload=\"toggleAll('collapse')\">";
 
   khtmlPart->begin();
   khtmlPart->write(startHtml);
