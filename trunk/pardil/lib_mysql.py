288a5116783cdea35a2eb415ec40a4dcc475ace6
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2005-08-03 21:26:17 +0000

    Bir takım değişiklikler...

---
+++ trunk/pardil/lib_mysql.py
@@ -0,0 +1,38 @@
+import MySQLdb
+
+class mysql_db:
+
+  def __init__(self, db_host, db_name, db_user, db_pw):
+    self.conn = MySQLdb.connect(host=db_host, user=db_user, passwd=db_pw, db=db_name)
+
+  # 
+  def query(self, str):
+    c = self.conn.cursor()
+    c.execute(str)
+    return c.fetchall()
+   
+  # Satır/hücre sayısı bilinmiyorsa kullanılması önerilen fonksiyon...
+  def scalar_query(self, str):
+    c = self.conn.cursor()
+    c.execute(str)
+    return c.fetchone()[0]
+    
+  # Sorgu sonunda tek satır veri dönecekse, kullanılması önerilen fonksiyon...
+  def row_query(self, str):
+    c = self.conn.cursor()
+    c.execute(str)
+    return c.fetchall()[0]
+
+  # SQL komutuna fesat karıştırılmasını önleyen fonksiyon
+  def escape(self, s):
+    return MySQLdb.escape_string(s)
+  
+  # Dict. tipindeki veriyi INSERT komutuna dönüştüren fonksiyon
+  def insert(self, table, data):
+    columns = []
+    values = []
+    for k, v in data.items():
+      columns.append(k)
+      values.append(""" "%s" """ % (self.escape(v)))
+    queryStr = "INSERT INTO %s (%s) VALUES (%s)" % (table, ','.join(columns), ','.join(values))
+    return queryStr
