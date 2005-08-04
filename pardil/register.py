11e77e061e448ee1f40f4d82840d62b23b247955
Bahadır Kandemir <kandemir@gmail.com>
2005-08-04 23:13:52 +0000

    Oturum yönetimindeki eksiklikler giderildi.

--- pardil/register.py
+++ pardil/register.py
@@ -12,11 +12,6 @@ def index(req):
   # Veritabanı bağlantısı kur.
   db = mysql_db(site_config['db_host'], site_config['db_name'], site_config['db_user'], site_config['db_pass'])
 
-  # Oturum yoksa yarat, varsa bilgileri yükle.
-  sess = Session.Session(req)
-  if not sess.is_new():
-    sess.load()
-
   # Tema motoruna gönderilecek değişken sözlüğünü oluştur.
   data = {}
   data['site_title'] = site_config['title']
@@ -25,6 +20,14 @@ def index(req):
   data['errors'] = {}
   data['status'] = ''
   data['posted_values'] = {}
+  data['session'] = {}
+  
+  # Oturum yoksa yarat, varsa bilgileri yükle.
+  sess = Session.Session(req)
+  if not sess.is_new():
+    sess.load()
+    data['session']['uid'] = sess['uid']
+    data['session']['username'] = sess['username']
 
   # Form gönderildiyse...
   if req.form.has_key('register'):
