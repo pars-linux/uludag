da7c992ba859f32890056385f7f16e19d12e73b4
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2005-08-09 17:27:02 +0000

    1-2 düzeltme

--- trunk/pardil/new_proposal.py
+++ trunk/pardil/new_proposal.py
@@ -29,31 +29,42 @@ def index(req):
       util.redirect(req, 'error.py?tag=not_maintainer')
     else:
       data['revision'] = 1
+      data['pid'] = int(req.form['edit'])
 
-      row = db.row_query('SELECT version, title, content FROM proposals_versions WHERE pid=%d' % (int(req.form['edit'])))
+      if req.form.has_key('version'):
+        data['version'] = req.form['version']
+      else:
+        data['version'] = db.scalar_query('SELECT version FROM proposals_versions WHERE pid=%d ORDER BY vid DESC LIMIT 1' % (data['pid']))
+
+      row = db.row_query('SELECT version, title, content FROM proposals_versions WHERE pid=%d AND version="%s"' % (data['pid'], db.escape(data['version'])))
+
       data['posted_values']['p_version'] = row[0]
       data['posted_values']['p_title'] = row[1]
       data['posted_values']['p_content'] = row[2]
 
-  """
   # Form gönderildiyse...
-  if req.form.has_key('register'):
+  if req.form.has_key('new_proposal'):
 
     # Gönderilen verileri data['posted_values'] içine aktar.
     for i in req.form.keys():
       data['posted_values'][i] = req.form[i]
 
-      
+    # Hata denetimi
+
+    if not len(req.form['p_title']):
+      data['errors']['p_title'] = 'Başlık boş bırakılamaz.'
+    
+    if data['revision']:
+      pass
     # Hiç hata yoksa...
     if not len(data['errors']):
 
       # Veritabanına kayıt yap...
-      insert_list = {}
-      db.query_com(db.insert('users', insert_list))
+      #insert_list = {}
+      #db.query_com(db.insert('users', insert_list))
 
       # İşlem durumunu "bitti" olarak belirle
       data['status'] = 'done'
-  """
       
   # Sayfayı derle.
   return build_page(site_config['path'] + 'templates/new_proposal.tpl', data)
