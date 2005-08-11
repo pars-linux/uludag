f96d50cc9a2f4af4b2d760339378ea10b334bd70
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2005-08-11 15:01:07 +0000

    MySQL 4.0'da çalışmayan sorgu, 57345. defa değiştirildi.

--- trunk/pardil/proposals.py
+++ trunk/pardil/proposals.py
@@ -2,6 +2,7 @@
 from cfg_main import site_config
 from lib_cheetah import build_page
 from lib_std import page_init
+import sys
 
 def index():
   # Veritabanı bağlantısı kur, oturum aç, template bilgilerini yükle
@@ -11,7 +12,11 @@ def index():
 
   # Önerilerin en yüksek sürüm numarasına sahip olanlarını listele
 
-  list = db.query('SELECT proposals.pid AS _pid, proposals_versions.version, proposals_versions.title FROM proposals INNER JOIN proposals_versions ON proposals.pid=proposals_versions.pid WHERE proposals_versions.vid IN (SELECT max(vid) FROM proposals_versions GROUP BY pid) ORDER BY proposals.pid ASC')
+  versions = []
+  for row in db.query('SELECT max(vid) FROM proposals_versions GROUP BY pid'):
+    versions.append(str(row[0]))
+
+  list = db.query('SELECT proposals.pid AS _pid, proposals_versions.version, proposals_versions.title FROM proposals INNER JOIN proposals_versions ON proposals.pid=proposals_versions.pid WHERE proposals_versions.vid IN (%s) ORDER BY proposals.pid ASC' % (','.join(versions)))
   for i in list:
     data['proposals'].append({'pid': i[0], 'version': i[1], 'title': i[2]})
 
