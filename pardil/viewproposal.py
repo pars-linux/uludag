e1dde1360cf9822240fd3b9a8a6647c4a785b8fc
Bahadır Kandemir <kandemir@gmail.com>
2005-08-12 15:40:51 +0000

    Görsel tasarımda değişiklikler.

--- pardil/viewproposal.py
+++ pardil/viewproposal.py
@@ -11,19 +11,17 @@ def index():
 
   form = cgi.FieldStorage()
 
-  if form.has_key('pid') and form.has_key('version'):
+  #if form.has_key('pid') and form.has_key('version'):
+  try:
     pid = int(form.getvalue('pid'))
     version = form.getvalue('version')
-  else:
+  except:
     data['status'] = 'error'
 
-  # FIXME:
-  # Öneriyi gönderen kişinin adı yazılacak.
-  row = db.row_query('SELECT proposals.pid, proposals_versions.version, proposals_versions.title, proposals_versions.content FROM proposals INNER JOIN proposals_versions ON proposals.pid=proposals_versions.pid WHERE proposals.pid=%d AND proposals_versions.version="%s"' % (pid, version))
+  if pid and version:
+    row = db.row_query('SELECT proposals.pid, proposals_versions.version, proposals_versions.title, proposals_versions.content FROM proposals INNER JOIN proposals_versions ON proposals.pid=proposals_versions.pid WHERE proposals.pid=%d AND proposals_versions.version="%s"' % (pid, version))
 
-  if not row:
-    data['status'] = 'error'
-  else:
+  if row:
     data['proposal'] = {}
     data['proposal']['pid'] = row[0]
     data['proposal']['version'] = row[1]
@@ -35,6 +33,12 @@ def index():
 
     # Sürüm geçmişi
     data['versions'] = db.query('SELECT proposals_versions.version FROM proposals_versions WHERE pid=%d ORDER BY vid DESC' % (pid))
+    
+    # Yorumlar
+    data['comments'] = db.query('SELECT proposals_comments.cid, users.username, proposals_comments.title, proposals_comments.content FROM proposals_comments INNER JOIN users ON users.uid=proposals_comments.uid INNER JOIN proposals_versions ON proposals_versions.vid=proposals_comments.vid WHERE proposals_versions.pid=%d AND proposals_versions.version="%s" ' % (pid, version))
+
+  else:
+    data['status'] = 'error'
 
   # Sayfayı derle.
   build_page(site_config['path'] + 'templates/viewproposal.tpl', data)
