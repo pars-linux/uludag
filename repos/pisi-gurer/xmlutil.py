93d4a9d9d41258472cde41a170358688348b166e
gurer <gurer@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2005-08-19 11:47:27 +0000

    Evet, artık hatalar güzelce collect ediliyor. SpecFile okuma için tamamen çalışıyor, repostats ile test ettim. LOCALTEXT diye bişi ekledim, dil parametresi verilirse doğru xml:lang'lı stringi alıyor.

--- repos/pisi-gurer/xmlutil.py
+++ repos/pisi-gurer/xmlutil.py
@@ -20,7 +20,8 @@ class XmlError(Exception):
 ROOT = 1
 CLASS = 2
 TEXT = 3
-ATTRIBUTE = 4
+LOCALTEXT =4
+ATTRIBUTE = 5
 
 SINGLE = 1
 MULTIPLE = 2
@@ -34,7 +35,7 @@ def getText(node):
     except:
         return None
     # strip whitespaces around
-    return c.split()[0]
+    return c.strip()
 
 def getByName(parent, childName):
     return [x for x in parent.childNodes if x.nodeType == x.ELEMENT_NODE if x.tagName == childName]
@@ -48,9 +49,10 @@ def getNodes(parent, path):
     tags = path.split('/')
     
     for tag in tags[:-1]:
-        parent = getByName(parent, tag)[0]
+        parent = getByName(parent, tag)
         if not parent:
             return None
+        parent = parent[0]
     
     return getByName(parent, tags[-1])
 
@@ -63,89 +65,137 @@ def getAttribute(node, attrname):
 
 # xml reader
 
-def readXmlFile(inst, fileName):
+def readXmlFile(inst, fileName, language='en'):
     try:
         doc = mdom.parse(fileName)
     except ExpatError, inst:
-        raise XmlError("File '%s' has invalid XML: %s" % (fileName, str(inst)))
-    readXml(inst, fileName, doc.documentElement)
+        raise XmlError("File '%s' has invalid XML: %s\n" % (fileName, str(inst)))
+    errs = []
+    readXml(errs, inst, doc.documentElement, language)
+    if errs != []:
+        e = ""
+        for err in errs:
+            e += err
+        raise XmlError("File '%s' has errors:\n%s" % (fileName, e))
+    doc.unlink()
 
-
-def readXml(inst, fileName, parent):
+def readXml(errs, inst, parent, language='en'):
     for d in inst.xmldefs:
         if d[0] == ROOT:
             if parent.tagName != d[1]:
-                raise XmlError("File '%s' root tag is '%s' instead of '%s'", fileName,
-                                        parent.tagName, d[1])
+                errs.append("Root tag is '%s' instead of '%s'\n" % (parent.tagName, d[1]))
+                continue
         
         elif d[0] == CLASS:
             # flags, tag name, class name, instance attribute
             nodes = getNodes(parent, d[2])
-            if len(nodes) > 1 and d[1] & SINGLE:
-                raise XmlError("File '%s' tag '%s' must not have more than one '%s' tags" % (fileName, parent.tagName, d[2]))
+            if nodes and len(nodes) > 1 and d[1] & SINGLE:
+                errs.append("Tag '%s' must not have more than one '%s' tags\n" % (parent.tagName, d[2]))
+                continue
             if d[1] & SINGLE:
                 # single case
                 if not nodes:
                     if not d[1] & OPTIONAL:
-                        raise XmlError("File '%s' tag '%s' must have a '%s' tag" % (fileName, parent.tagName, d[2]))
+                        errs.append("Tag '%s' must have a '%s' tag\n" % (parent.tagName, d[2]))
+                        continue
+                    inst.__dict__[d[4]] = None
                 else:
                     inst.__dict__[d[4]] = d[3](nodes[0])
-                    readXml(inst.__dict__[d[4]], fileName, nodes[0])
+                    readXml(errs, inst.__dict__[d[4]], nodes[0], language)
             else:
                 # multiple case
                 if not nodes:
                     if not d[1] & OPTIONAL:
-                        raise XmlError("File '%s' tag '%s' must have at least one '%s' tags" % (fileName, parent.tagName, d[2]))
+                        errs.append("Tag '%s' must have at least one '%s' tags\n" % (parent.tagName, d[2]))
+                        continue
+                    inst.__dict__[d[4]] = []
                 else:
                     classes = []
                     for node in nodes:
                         c = d[3](node)
-                        readXml(c, fileName, node)
+                        readXml(errs, c, node, language)
                         classes.append(c)
                     inst.__dict__[d[4]] = classes
         
         elif d[0] == TEXT:
             # flags, tag name, instance attribute
+            inst.__dict__[d[3]] = None
             nodes = getNodes(parent, d[2])
-            if len(nodes) > 1 and d[1] & SINGLE:
-                raise XmlError("File '%s' tag '%s' must not have more than one '%s' tags" % (fileName, parent.tagName, d[2]))
+            if nodes and len(nodes) > 1 and d[1] & SINGLE:
+                errs.append("Tag '%s' must not have more than one '%s' tags\n" % (parent.tagName, d[2]))
+                continue
             if d[1] & SINGLE:
                 # single case
                 if not nodes:
                     if not d[1] & OPTIONAL:
-                        raise XmlError("File '%s' tag '%s' must have a '%s' tag" % (fileName, parent.tagName, d[2]))
+                        errs.append("Tag '%s' must have a '%s' tag\n" % (parent.tagName, d[2]))
+                        continue
                 else:
                     c = getText(nodes[0])
                     if not c:
-                        raise XmlError("File '%s' tag '%s' should have some text data" % (fileName, d[2]))
+                        errs.append("Tag '%s' should have some text data\n" % nodes[0].tagName)
+                        continue
                     inst.__dict__[d[3]] = c
             else:
                 # multiple case
                 if not nodes:
                     if not d[1] & OPTIONAL:
-                        raise XmlError("File '%s' tag '%s' must have at least one '%s' tags" % (fileName, parent.tagName, d[2]))
+                        errs.append("Tag '%s' must have at least one '%s' tags\n" % (parent.tagName, d[2]))
+                        continue
                 else:
                     strs = []
                     for node in nodes:
                         c = getText(node)
                         if not c:
-                            raise XmlError("File '%s' tag '%s' should have some text data" % (fileName, d[2]))
+                            errs.append("Tag '%s' should have some text data\n" % node.tagName)
+                            break
                         strs.append(c)
                     inst.__dict__[d[3]] = strs
+        
+        elif d[0] == LOCALTEXT:
+            # flags, tag name, instance attribute
+            inst.__dict__[d[3]] = None
+            nodes = getNodes(parent, d[2])
+            if not nodes:
+                if not d[1] & OPTIONAL:
+                    errs.append("Tag '%s' should have at least one '%s' tag\n" % (parent.tagName, d[2]))
+                continue
+            locs = {}
+            for node in nodes:
+                lang = getAttribute(node, "xml:lang")
+                c = getText(node)
+                if not c:
+                    errs.append("Tag '%s' should have some text data\n" % node.tagName)
+                    break
+                # FIXME: check for dups and 'en'
+                if not lang:
+                    lang = 'en'
+                locs[lang] = c
+            # FIXME: return full list too
+            L = language
+            if not locs.has_key(L):
+                L = 'en'
+            if not locs.has_key(L):
+                errs.append("Tag '%s' should have an English version\n" % d[2])
+                continue
+            inst.__dict__[d[3]] = locs[L]
         
         elif d[0] == ATTRIBUTE:
             # flags, tag name, attribute name, instance attribute
             if d[2] != "":
                 t = getNodes(parent, d[2])
                 if not t:
-                    raise XmlError("File '%s' tag '%s' must have a '%s' tag" % (fileName, parent.tagName, d[2]))
+                    errs.append("Tag '%s' must have a '%s' tag\n" % (parent.tagName, d[2]))
+                    continue
                 if len(t) > 1:
-                    raise XmlError("File '%s' tag '%s' must not have more than one '%s' tags" % (fileName, parent.tagName, d[2]))
+                    errs.append("Tag '%s' must not have more than one '%s' tags\n" % (parent.tagName, d[2]))
+                    continue
                 t = t[0]
             else:
                 t = parent
             attr = getAttribute(t, d[3])
             if not attr:
                 if not d[1] & OPTIONAL:
-                    raise XmlError("File '%s' tag '%s' must have a '%s' attribute" % (fileName, t[0].tagName, d[3]))
+                    errs.append("Tag '%s' must have a '%s' attribute\n" % (t.tagName, d[3]))
+                    continue
             inst.__dict__[d[4]] = attr
