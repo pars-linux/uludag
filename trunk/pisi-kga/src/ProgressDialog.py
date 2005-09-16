f100acfe0e75d12e4f5f40292568b27fed784333
cartman <cartman@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2005-09-16 12:51:42 +0000

    CopyLeft

--- trunk/pisi-kga/src/ProgressDialog.py
+++ trunk/pisi-kga/src/ProgressDialog.py
@@ -1,3 +1,20 @@
+# -*- coding: utf-8 -*-
+###########################################################################
+# PiSi KGA - Progress Dialog                                              #
+# ------------------------------                                          #
+# begin     : Cum Eyl 16 15:57:56 EEST 2005                               #
+# copyright : (C) 2005 by UEKAE/TÜBİTAK                                   #
+# email     : ismail@uludag.org.tr                                        #
+#                                                                         #
+###########################################################################
+#                                                                         #
+#   This program is free software; you can redistribute it and/or modify  #
+#   it under the terms of the GNU General Public License as published by  #
+#   the Free Software Foundation; either version 2 of the License, or     #
+#   (at your option) any later version.                                   #
+#                                                                         #
+###########################################################################
+
 from Progress import *
 
 class ProgressDialog(Progress):
