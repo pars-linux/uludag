797c13ac04ddf2dd8c3b5b3a4c80a4db560cf679
İsmail Dönmez <ismail@users.noreply.github.com>
2005-09-28 08:40:08 +0000

    They call me eye candy man for a reason :P

--- network-kga/src/FirstPage.py
+++ network-kga/src/FirstPage.py
@@ -7,3 +7,41 @@ class FirstPage(FirstWidget.FirstWidget):
         self.directConnectionIcon.setPixmap(loadIcon("network"))
         self.wirelessConnectionIcon.setPixmap(loadIcon("wireless"))
         self.dialupConnectionIcon.setPixmap(loadIcon("dialup"))
+        self.currentWidget = ''
+
+    def resetWidgetColors(self):
+        if self.currentWidget.startswith('direct'):
+            self.directConnectionLabelTop.setBackgroundMode(Qt.PaletteBackground)
+            self.directConnectionLabel.setBackgroundMode(Qt.PaletteBackground)
+        elif self.currentWidget.startswith('wireless'):
+            self.wirelessConnectionLabelTop.setBackgroundMode(Qt.PaletteBackground)
+            self.wirelessConnectionLabel.setBackgroundMode(Qt.PaletteBackground)
+        elif self.currentWidget.startswith('dialup'):
+            self.dialupConnectionLabelTop.setBackgroundMode(Qt.PaletteBackground)
+            self.dialupConnectionLabel.setBackgroundMode(Qt.PaletteBackground)
+        
+    def mousePressEvent(self,event):
+        widget = self.childAt(event.pos())
+
+        if not widget:
+            pass
+        else:
+            name = widget.name()
+            if self.currentWidget.startswith(name):
+                pass
+            elif name and event.button() & Qt.LeftButton:
+                self.resetWidgetColors()
+                if name.startswith('direct'):
+                    self.currentWidget = 'directConnectionLabelTop'
+                    self.directConnectionLabelTop.setBackgroundMode(Qt.PaletteHighlight)
+                    self.directConnectionLabel.setBackgroundMode(Qt.PaletteLight)
+                elif name.startswith('wireless'):
+                    self.currentWidget = 'wirelessConnectionLabelTop'
+                    self.wirelessConnectionLabelTop.setBackgroundMode(Qt.PaletteHighlight)
+                    self.wirelessConnectionLabel.setBackgroundMode(Qt.PaletteLight)
+                elif name.startswith('dialup'):
+                    self.currentWidget = 'dialupConnectionLabelTop'
+                    self.dialupConnectionLabelTop.setBackgroundMode(Qt.PaletteHighlight)
+                    self.dialupConnectionLabel.setBackgroundMode(Qt.PaletteLight)
+
+        FirstWidget.FirstWidget.mousePressEvent(self,event)
