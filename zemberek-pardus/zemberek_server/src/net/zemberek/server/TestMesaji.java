69e609609c21aa2a842ea9aa3476b1a6375cf42a
mdakin <mdakin@gmail.com>
2005-10-13 20:27:21 +0000

    zemberek_server crash landing

---
+++ zemberek-pardus/zemberek_server/src/net/zemberek/server/TestMesaji.java
@@ -0,0 +1,15 @@
+/*
+ * Created on 20.Şub.2005
+ *
+ */
+package net.zemberek.server;
+
+public class TestMesaji extends ZemberekMesaji {
+
+    private String mesaj;
+
+    public TestMesaji(String mesaj) {
+        super(1, mesaj);
+    }
+
+}
