31e7b4c188fdebc0103fcfcee10cf41abc50d4dd
S.Çağlar Onur <caglar10ur@users.noreply.github.com>
2005-10-14 19:47:14 +0000

    tab/space :(, kod sahipleri indentationlarda hata var mı diye kontrol edebilir mi?

--- web_araclari/bulten_entry.py
+++ web_araclari/bulten_entry.py
@@ -7,28 +7,27 @@ from os.path import basename
 class Entry(object):
     """An entry, that will be shown"""
     def __init__(self, src):
-	self.src = src
-	self.file = open(src, "r")
-	super(Entry, self).__init__()
+        self.src = src
+        self.file = open(src, "r")
+        super(Entry, self).__init__()
 
     def __str__(self):
-	return self.src 
+        return self.src 
 
     def content(self):
-	self.file.seek(0)
-	return self.file.read()
+        self.file.seek(0)
+        return self.file.read()
 
     def title(self):
-	self.file.seek(0)
-	return self.file.readline()
+        self.file.seek(0)
+        return self.file.readline()
 
     def file_name(self):
-	return basename(self.file.name)
+        return basename(self.file.name)
 
     def html_name(self):
-	fname = self.file_name()
-	return fname[:-3] + "html"
-
+        fname = self.file_name()
+        return fname[:-3] + "html"
 
 if __name__ == "__main__":
     pass
--- web_araclari/bulten_yap.py
+++ web_araclari/bulten_yap.py
@@ -24,12 +24,12 @@ def check_logs(index):
     logs=glob(LOGS+"/*"+log_prefix)
     # check if all are in index. If not add it.
     for log in logs:
-	if not index.check(log):
-	    index.add(log)
+    if not index.check(log):
+        index.add(log)
 
-	    mtime =  index.get_mtime(log)
-	    # date <- (year, month)
-	    date = time.localtime(mtime)[:2]
+        mtime =  index.get_mtime(log)
+        # date <- (year, month)
+        date = time.localtime(mtime)[:2]
 
     # sort the list using mtimes in the .index
     logs = index.sort_filelist(logs)
@@ -52,29 +52,28 @@ def gen_html(index, logs):
 
     # Print the entries
     if not entry_count: 
-	entry_count = len(logs)
+        entry_count = len(logs)
     else:
-	entry_count = int(entry_count)
-	if entry_count > len(logs):
-	    entry_count = len(logs)
+        entry_count = int(entry_count)
+    if entry_count > len(logs):
+        entry_count = len(logs)
 
     index_html = open("index.html", "w")
     index_html.write(header_text)
     index_html.write("<center> <p><b>Bülten Arşivi</b></p>")
 
     for i in range(entry_count):
-	hname = Entry(logs[i]).html_name()
-	title = Entry(logs[i]).title()
-	# first add to index.html
-	index_html.write("<a href=\"%s/%s\">%s</a><br>" 
-			 % (ARCHIVE, hname, title))
+        hname = Entry(logs[i]).html_name()
+        title = Entry(logs[i]).title()
+        # first add to index.html
+        index_html.write("<a href=\"%s/%s\">%s</a><br>" % (ARCHIVE, hname, title))
 		
-	# then write to archive
-	hfile = open(ARCHIVE+"/"+hname, "w")
-	hfile.write(archive_header)
-	hfile.write(Entry(logs[i]).content())
-	hfile.write(footer_text)
-	hfile.close()
+    # then write to archive
+    hfile = open(ARCHIVE+"/"+hname, "w")
+    hfile.write(archive_header)
+    hfile.write(Entry(logs[i]).content())
+    hfile.write(footer_text)
+    hfile.close()
 
     index_html.write("</center>")
     index_html.write(footer_text)
@@ -86,17 +85,16 @@ def gen_rss(index, logs):
     rss_items = []
 
     if not entry_count: 
-	entry_count = len(logs)
+        entry_count = len(logs)
     else:
-	entry_count = int(entry_count)
-	if entry_count > len(logs):
-	    entry_count = len(logs)
-
+        entry_count = int(entry_count)
+    if entry_count > len(logs):
+        entry_count = len(logs)
 
     for i in range(entry_count):
-	file = codecs.open(logs[i], encoding="utf-8")
+        file = codecs.open(logs[i], encoding="utf-8")
 
-	# first line is title
+        # first line is title
         entry_title = file.readline().strip('\n')
         entry_link = "http://www.uludag.org.tr/bulten/arsiv/"+basename(logs[i])[:-3]+"html"
         entry_desc = file.read()
