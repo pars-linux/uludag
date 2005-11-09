74355e66bde4663e18e2ef1d127533a714a527a4
gurer <gurer@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2005-11-09 17:50:21 +0000

    iterasyon bug fix. toPrettyString() kendisine tag node verilmediğinde NotTag raise etsin. testleri genişlettim.

--- trunk/piksemel/test.py
+++ trunk/piksemel/test.py
@@ -3,6 +3,7 @@
 import sys
 import piksemel as iks
 
+# test accessors
 doc = iks.parseString("<a><c>lala<b>merhaba</b>bibi</c><d/></a>")
 c = doc.getTag("c")
 x = c.firstChild()
@@ -15,11 +16,27 @@ while x:
         print x.name()
     x = x.next()
 
+# test attributes
+doc = iks.parseString("<a><blah hede='lala'/></a>")
+blah = doc.getTag("blah")
+print blah.getAttribute("hede"), blah.getAttribute("hodo")
+
+# test iteration
+doc = iks.parse(sys.argv[1])
+for a in doc:
+    a.toString()
+    if a.type() == iks.TAG:
+        a.toPrettyString()
+doc.toString()
+doc.toPrettyString()
+
+# test tag iteration
 doc = iks.parse(sys.argv[1])
 for tag in doc.tags():
     if tag.getTagData("Size") == "42":
         print tag.toString()
 
+# test builders
 doc2 = iks.newDocument("Test")
 t = doc2.appendTag("Deneme1")
 t.appendTag("isim").appendData("meduketto")
