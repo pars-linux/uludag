b30d281f156412e4ade69fecaffd90a5a61cce19
cartman <cartman@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2005-12-08 18:13:26 +0000

    i18n!

--- trunk/pisi-kga/src/Success.py
+++ trunk/pisi-kga/src/Success.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from qt import *
+from kdecore import i18n
 import PisiKga
 import SuccessDialog
 
@@ -13,9 +14,9 @@ class Success(SuccessDialog.SuccessDialog):
 
     def showHideBrowser(self):
         if self.infoBrowser.isShown():
-            self.showButton.setText(u"Daha fazla bilgi göster >>")
+            self.showButton.setText(i18n("Show more information >>"))
             self.infoBrowser.hide()
         else:
-            self.showButton.setText(u"Daha az bilgi <<")
+            self.showButton.setText(i18n("Show less information <<"))
             self.infoBrowser.show()
         self.adjustSize()
