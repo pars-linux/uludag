fc18a797cf6444f1c6e7511ba3043016d5d83a29
Bahadır Kandemir <kandemir@gmail.com>
2005-12-15 12:43:02 +0000

    UI'ler param parçaaa

--- feedback/src/feedback.py
+++ feedback/src/feedback.py
@@ -11,8 +11,19 @@ from kdecore import *
 from kdeui import *
 import kdedesigner
 
-from feedbackdlg import FeedbackForm
+# Wizard Pages
+from welcomedlg import WelcomeDlg
+from experiencedlg import ExperienceDlg
+from purposedlg import PurposeDlg
+from usagedlg import UsageDlg
+from questiondlg import QuestionDlg
+from opiniondlg import OpinionDlg
+from bilgitemini import PersonalInfoDlg
+from hardwareinfodlg import HardwareInfoDlg
+from upload import UploadDlg
+from goodbyedlg import GoodbyeDlg
 
+# Gettext
 import gettext
 t = gettext.translation("feedback", fallback=True)
 _ = t.ugettext
@@ -40,42 +51,53 @@ def loadIcon(name, group=KIcon.Desktop):
 def loadIconSet(name, group=KIcon.Desktop):
         return KGlobal.iconLoader().loadIconSet(name, group)
 
-class Form(FeedbackForm):
-    def __init__(self,parent = None,name = None,modal = 0,fl = 0):
-        FeedbackForm.__init__(self, parent, name, modal, fl)
+class Form(KWizard):
+    def __init__(self, parent = None, name = None, modal = 0, fl = 0):
+        KWizard.__init__(self, parent, name, modal, fl)
 
+        self.resize(QSize(600,373).expandedTo(self.minimumSizeHint()))
+        
         # Images
         self.image_feedback = QPixmap("feedback.png")
 
-        # Pixmaps
-        self.welcomePixmap.setPixmap(self.image_feedback)
-        self.experiencePixmap.setPixmap(self.image_feedback)
-        self.purposePixmap.setPixmap(self.image_feedback)
-        self.usagePixmap.setPixmap(self.image_feedback)
-        self.questionPixmap.setPixmap(self.image_feedback)
-        self.opinionPixmap.setPixmap(self.image_feedback)
-        self.hardwareInfoPixmap.setPixmap(self.image_feedback)
-        self.uploadPixmap.setPixmap(self.image_feedback)
-        self.goodbyePixmap.setPixmap(self.image_feedback)
+        self.pageWelcomeDlg = WelcomeDlg()
+        self.addPage(self.pageWelcomeDlg, self.__tr("Welcome"))
+
+        self.pageExperienceDlg = ExperienceDlg()
+        self.addPage(self.pageExperienceDlg, self.__tr("Experience"))
+
+        self.pagePurposeDlg = PurposeDlg()
+        self.addPage(self.pagePurposeDlg, self.__tr("Purpose"))
+
+        self.pageUsageDlg = UsageDlg()
+        self.addPage(self.pageUsageDlg, self.__tr("Usage"))
+
+        self.pageQuestionDlg = QuestionDlg()
+        self.addPage(self.pageQuestionDlg, self.__tr("Questions"))
+
+        self.pageOpinionDlg = OpinionDlg()
+        self.addPage(self.pageOpinionDlg, self.__tr("Opinions"))
+
+        self.pagePersonalInfoDlg = PersonalInfoDlg()
+        self.addPage(self.pagePersonalInfoDlg, self.__tr("Personal Info"))
+
+        self.pageHardwareInfoDlg = HardwareInfoDlg()
+        self.addPage(self.pageHardwareInfoDlg, self.__tr("Hardware Info"))
+
+        self.pageUploadDlg = UploadDlg()
+        self.addPage(self.pageUploadDlg, self.__tr("Uploading"))
+
+        self.pageGoodbyeDlg  = GoodbyeDlg()
+        self.addPage(self.pageGoodbyeDlg, self.__tr("Goodbye!"))
 
         # Buttons
         n = self.nextButton()
-        n.setIconSet(loadIconSet("forward", KIcon.Small))
-        n.setText(_("Next"))
         QObject.connect(n, SIGNAL("clicked()"), self.next_clicked)
         #
         b = self.backButton()
-        b.setIconSet(loadIconSet("back", KIcon.Small))
-        b.setText(_("Back"))
         QObject.connect(b, SIGNAL("clicked()"), self.back_clicked)
         #
-        c = self.cancelButton()
-        c.setIconSet(loadIconSet("cancel", KIcon.Small))
-        c.setText(_("Cancel"))
-        #
         f = self.finishButton()
-        f.setIconSet(loadIconSet("ok", KIcon.Small))
-        f.setText(_("Finish"))
         QObject.connect(f, SIGNAL("clicked()"), self.finish_clicked)
 
         # Common elements & options
@@ -83,20 +105,23 @@ class Form(FeedbackForm):
             # No help button
             self.setHelpEnabled(self.page(i), 0)
         
-        self.buttonRetry.hide()
-        QObject.connect(self.buttonRetry, SIGNAL("clicked()"), self.button_retry_clicked)
+        self.pageUploadDlg.buttonRetry.hide()
+        QObject.connect(self.pageUploadDlg.buttonRetry, SIGNAL("clicked()"), self.button_retry_clicked)
         
-        # Buttons on pages M and N
-        self.setBackEnabled(self.PageUpload, 0)
-        self.setNextEnabled(self.PageUpload, 0)
-        self.setBackEnabled(self.PageThank, 0)
-        self.setFinishEnabled(self.PageThank, 1)
+        # Buttons on last 2 pages
+        self.setBackEnabled(self.pageUploadDlg, 0)
+        self.setNextEnabled(self.pageUploadDlg, 0)
+        self.setBackEnabled(self.pageGoodbyeDlg, 0)
+        self.setFinishEnabled(self.pageGoodbyeDlg, 1)
 
         # Create upload thread
         self.thread_1 = thread_upload()
 
+    def __tr(self,s,c = None):
+        return qApp.translate("Feedback",s,c)
+
     def next_clicked(self):
-        if self.currentPage() == self.PageUpload:
+        if self.currentPage() == self.pageUploadDlg:
             self.thread_1.start()
         
     def back_clicked(self):
@@ -106,8 +131,8 @@ class Form(FeedbackForm):
         pass
 
     def button_retry_clicked(self):
-        self.buttonRetry.hide()
-        self.labelStatus.setText("")
+        self.pageUploadDlg.buttonRetry.hide()
+        self.pageUploadDlg.labelStatus.setText("")
         self.thread_1.start()
         
 
@@ -117,34 +142,34 @@ class thread_upload(QThread):
         done = _("<font color=\"#008800\">Done</font><br>\n")
         failed = _("<font color=\"#ff0000\">Failed</font><br>\n")
         # Collect hardware information
-        if not w.hardwareInfoBox.isChecked():
+        if not w.pageHardwareInfoDlg.hardwareInfoBox.isChecked():
             text += _("Collecting hardware information...")
-            w.labelStatus.setText(text)
+            w.pageUploadDlg.labelStatus.setText(text)
             stdin, stdout, stderr = os.popen3("uhinv -f text")
             if "".join(stderr):
                 text += failed
-                w.labelStatus.setText(text)
-                w.buttonRetry.show()
+                w.pageUploadDlg.labelStatus.setText(text)
+                w.pageUploadDlg.buttonRetry.show()
                 return
             else:
                 text += done
-                w.labelStatus.setText(text)
+                w.pageUploadDlg.labelStatus.setText(text)
         # Upload data to dev. center
         text += _("Uploading data...")
-        w.labelStatus.setText(text)
+        w.pageUploadDlg.labelStatus.setText(text)
         try:
             # FIXME
             pass
         except:
             text += failed
-            w.labelStatus.setText(text)
-            w.buttonRetry.show()
+            w.pageUploadDlg.labelStatus.setText(text)
+            w.pageUploadDlg.buttonRetry.show()
             return
         else:
             text += done
-            w.labelStatus.setText(text)
+            w.pageUploadDlg.labelStatus.setText(text)
         #
-        w.setNextEnabled(w.PageUpload, 1)
+        w.setNextEnabled(w.pageUploadDlg, 1)
 
 def main():
     global w, url
---
+++ feedback/src/purposedlg.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'purposedlg.ui'
+#
+# Created: Prş Ara 15 14:17:39 2005
+#      by: The PyQt User Interface Compiler (pyuic) snapshot-20051013
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from qt import *
+from kdecore import *
+from kdeui import *
+
+
+
+class PurposeDlg(QDialog):
+    def __init__(self,parent = None,name = None,modal = 0,fl = 0):
+        QDialog.__init__(self,parent,name,modal,fl)
+
+        if not name:
+            self.setName("PurposeDlg")
+
+
+
+        self.buttonGroup = QButtonGroup(self,"buttonGroup")
+        self.buttonGroup.setGeometry(QRect(170,130,400,120))
+
+        LayoutWidget = QWidget(self.buttonGroup,"layout6")
+        LayoutWidget.setGeometry(QRect(16,11,370,100))
+        layout6 = QHBoxLayout(LayoutWidget,11,6,"layout6")
+
+        layout18 = QVBoxLayout(None,0,6,"layout18")
+
+        self.checkBox2_3 = QCheckBox(LayoutWidget,"checkBox2_3")
+        layout18.addWidget(self.checkBox2_3)
+
+        self.checkBox2 = QCheckBox(LayoutWidget,"checkBox2")
+        layout18.addWidget(self.checkBox2)
+
+        self.checkBox2_2 = QCheckBox(LayoutWidget,"checkBox2_2")
+        layout18.addWidget(self.checkBox2_2)
+        layout6.addLayout(layout18)
+
+        layout18_2 = QVBoxLayout(None,0,6,"layout18_2")
+
+        self.checkBox2_3_2 = QCheckBox(LayoutWidget,"checkBox2_3_2")
+        layout18_2.addWidget(self.checkBox2_3_2)
+
+        self.checkBox2_4 = QCheckBox(LayoutWidget,"checkBox2_4")
+        layout18_2.addWidget(self.checkBox2_4)
+
+        self.checkBox2_2_2 = QCheckBox(LayoutWidget,"checkBox2_2_2")
+        layout18_2.addWidget(self.checkBox2_2_2)
+        layout6.addLayout(layout18_2)
+
+        self.purposeMiniLabel = QLabel(self,"purposeMiniLabel")
+        self.purposeMiniLabel.setGeometry(QRect(310,100,260,20))
+
+        self.purposeLabel = QLabel(self,"purposeLabel")
+        self.purposeLabel.setGeometry(QRect(170,60,420,40))
+        purposeLabel_font = QFont(self.purposeLabel.font())
+        self.purposeLabel.setFont(purposeLabel_font)
+        self.purposeLabel.setTextFormat(QLabel.RichText)
+        self.purposeLabel.setAlignment(QLabel.WordBreak | QLabel.AlignTop)
+
+        self.stepLabel = QLabel(self,"stepLabel")
+        self.stepLabel.setGeometry(QRect(160,20,100,21))
+        self.stepLabel.setPaletteForegroundColor(QColor(77,77,77))
+        self.stepLabel.setFrameShape(QLabel.NoFrame)
+        self.stepLabel.setFrameShadow(QLabel.Plain)
+
+        self.purposePixmap = QLabel(self,"purposePixmap")
+        self.purposePixmap.setGeometry(QRect(-5,0,142,290))
+        self.purposePixmap.setScaledContents(1)
+
+        self.languageChange()
+
+        self.resize(QSize(620,286).expandedTo(self.minimumSizeHint()))
+        self.clearWState(Qt.WState_Polished)
+
+
+    def languageChange(self):
+        self.setCaption(i18n("Feedback wizard"))
+        self.buttonGroup.setTitle(QString.null)
+        self.checkBox2_3.setText(i18n("Daily use"))
+        self.checkBox2.setText(i18n("It's my hobby"))
+        self.checkBox2_2.setText(i18n("For internet access"))
+        self.checkBox2_3_2.setText(i18n("For business"))
+        self.checkBox2_4.setText(i18n("Entertainment purposes"))
+        self.checkBox2_2_2.setText(i18n("Educational purposes"))
+        self.purposeMiniLabel.setText(i18n("<p align=\"right\">(You can click on multiple items)</p>"))
+        self.purposeLabel.setText(i18n("<h2>Why do you use Pardus?</h2>"))
+        self.stepLabel.setText(i18n("<b>Step 2 of 7</b>"))
+
