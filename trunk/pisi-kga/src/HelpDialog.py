3f4088c39471cf5fc864a6be735cfb9a7bd9e261
cartman <cartman@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2005-12-21 18:22:04 +0000

    fix

--- trunk/pisi-kga/src/HelpDialog.py
+++ trunk/pisi-kga/src/HelpDialog.py
@@ -20,7 +20,7 @@ from khtml import *
 class HelpDialog(QDialog):
     def __init__(self, parent=None):
         QDialog.__init__(self)
-        self.setCaption("PiSi KGA Help")
+        self.setCaption(i18n("PiSi KGA Help"))
         self.layout = QGridLayout(self)
         self.htmlPart = KHTMLPart(self)
         self.resize(500,600)
