352623f025f528ec30cc6bf234dc5f5730016505
A. Murat Eren <a.murat.eren@gmail.com>
2005-12-26 06:37:26 +0000

    more fix

--- user-kga/SetupUsers.py
+++ user-kga/SetupUsers.py
@@ -8,6 +8,7 @@
 # any later version.
 #
 # Please read the COPYING file.
+# Copyright (C) 2005, İsmail Dönmez (architect and designer)
 # Copyright (C) 2005, Barış Metin
 # Copyright (C) 2005, Gürer Özen
 # 
