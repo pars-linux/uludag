1fad19522387bd0cc69bcea6d8a309bfd40ac7a1
ahmet.aygun <ahmet.aygun@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2006-01-22 00:52:30 +0000

    Gökmen kaymak haklı, üye olmadan da önizleme yapılır. download.php'de Gökmen'in kodları var bir ara çeki düzen veririz.

--- trunk/arto/download.php
+++ trunk/arto/download.php
@@ -1,3 +1,13 @@
 <?php
-echo "Geliyor";
+include("globals.php");
+
+            $file=rtag($_GET["file"]);
+            if (count_download($file)) header ("location: ".$config['core']['url']."files/".$_GET["file"]);
+            else {
+                $message["title"] = ERROR;
+                $message["message"] = FILE_NOT_FOUND;
+		set_smarty_vars("message",$message);
+		$smarty->display("message.html");
+            }
+            die();
 ?>
