dfea73351702a335527f10f53d37a3681ebb0a87
Ahmet AYGÜN <me@ahmetaygun.net>
2006-01-22 00:42:30 +0000

    eksik dosya tipleri eklendi, typo

--- arto/functions.php
+++ arto/functions.php
@@ -259,7 +259,7 @@
     function get_file_type($file){
         $file = pathinfo ($file);
         $extension = $file["extension"];
-        $package = array("zip","rar","gz","bz2","sh","tar.gz","tar.bz2","bin","pisi","skz");
+        $package = array("zip","rar","gz","bz2","sh","tar.gz","tar.bz2","bin","pisi","skz","aiz","wav");
         $image   = array("gif","jpeg","jpg","png","bmp","xpm");
         foreach ($package as $key) { if ($key==$extension) return "package"; }
         foreach ($image as $key) { if ($key==$extension) return "image"; }
