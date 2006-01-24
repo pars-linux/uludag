f5cad7a65f73ed8908365294dce22fd1ce5079a2
Görkem Çetin <gorkem-cetin@users.noreply.github.com>
2006-01-24 17:56:10 +0000

    güncelleme.

--- feedback/src/purposedlg.py
+++ feedback/src/purposedlg.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'purposedlg.ui'
 #
-# Created: Cum Ara 23 15:04:58 2005
+# Created: Sal Oca 24 19:45:13 2006
 #      by: The PyQt User Interface Compiler (pyuic) snapshot-20051013
 #
 # WARNING! All changes made in this file will be lost!
--- feedback/src/upload.py
+++ feedback/src/upload.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'upload.ui'
 #
-# Created: Pzt Ara 19 23:31:30 2005
+# Created: Sal Oca 24 19:45:13 2006
 #      by: The PyQt User Interface Compiler (pyuic) snapshot-20051013
 #
 # WARNING! All changes made in this file will be lost!
