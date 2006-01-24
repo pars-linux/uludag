5bdba4153986a5653005ea956367335e32a8bad0
ahmet.aygun <ahmet.aygun@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2006-01-24 01:16:02 +0000

    Çağlar, çok masraflısın.. ;)

--- trunk/arto/download.php
+++ trunk/arto/download.php
@@ -1,15 +1,28 @@
 <?php
 include("globals.php");
 
-            $file = get_filepaths($_GET["id"]);
-            if($file[0]['path2']){$download = $file[0]['path2'];}
-            elseif($file[0]['path2'] == "" && isset($file[0]['path'])){$download = $file[0]['path'];}
-            if (count_download($download)) header ("location: ".$config['core']['url']."files/".$download);
-            else {
-                $message["title"] = ERROR;
-                $message["message"] = FILE_NOT_FOUND;
-		set_smarty_vars("message",$message);
-		$smarty->display("message.html");
-            }
-            die();
+    if(!$_GET['file']){$_GET['file'] = 1;}
+
+    $file = get_filepaths($_GET["id"]);
+    if($_GET['file'] == 2 && $file[0]['path2'] != ""){$download = $file[0]['path2'];}
+    elseif($_GET['file'] == 1 && $file[0]['path'] != ""){$download = $file[0]['path'];}
+    elseif($_GET['file'] == 1 && $file[0]['path'] == ""){$hata = TRUE;}
+    elseif($_GET['file'] == 2 && $file[0]['path2'] == ""){$hata = TRUE;}
+
+    if(!$hata){
+    if(count_download($download)){Header("location: ".$config['core']['url']."files/".$download);}
+    else{
+        $message["title"] = ERROR;
+        $message["message"] = FILE_NOT_FOUND;
+        set_smarty_vars("message",$message);
+        $smarty->display("message.html");
+    }
+    }
+    elseif($hata){
+        $message["title"] = ERROR;
+        $message["message"] = FILE_NOT_FOUND;
+        set_smarty_vars("message",$message);
+        $smarty->display("message.html");
+    }
+    die();
 ?>
