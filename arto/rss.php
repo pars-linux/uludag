186d40237e3b8096c2fb4f6d712d9ad2b60def5f
Ahmet AYGÜN <me@ahmetaygun.net>
2006-02-08 00:19:31 +0000

    #1983, artoya rss desteği, biraz deneme lazım

---
+++ arto/rss.php
@@ -0,0 +1,43 @@
+<?php
+ob_start();
+
+echo "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?>";
+include_once("globals.php");
+
+$posts = get_something("","","","release","4","db2rss");
+
+// set content-type to xml
+Header("Content-type: text/xml; charset=utf-8");
+?>
+<!-- generator="Arto Feed Engine/Arto v<?=$config['arto']['version']?>" -->
+<rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:wfw="http://wellformedweb.org/CommentAPI/" xmlns:dc="http://purl.org/dc/elements/1.1/">
+<channel>
+	<title><?=$config['core']['title']?></title>
+	<link><?=$config['core']['url']?></link>
+	<description><?=$config['core']['desc']?></description>
+	<pubDate><?php echo date("r"); ?></pubDate>
+	<generator>http://arto.fasafiso.org/?version=<?=$config['arto']['version']?></generator>
+	<?php for($i = 0; $i < count($posts); $i++){ ?>
+	<item>
+		<title><?=$posts[$i]['name']?></title>
+		<link><?=$config['core']['url']?>?id=<?=$posts[$i]['id']?></link>
+		<comments><?=$config['core']['url']?>?id=<?=$posts[$i]['id']?>#comments</comments>
+		<pubDate><?=$posts[$i]['release']?></pubDate>
+		<dc:creator><?=$posts[$i]['author']?></dc:creator>
+		<guid isPermaLink="true"><?=$config['core']['url']?>?id=<?=$posts[$i]['id']?></guid>
+		<description><?=htmlspecialchars(strip_tags($posts[$i]['description']), ENT_QUOTES)?></description>
+		<content:encoded><?=htmlspecialchars($posts[$i]['description'], ENT_QUOTES)?></content:encoded>
+	</item>
+	<?php } ?>
+</channel>
+</rss>
+<?
+
+/*
+echo "<pre>";
+print_r($posts);
+echo "</pre>";
+*/
+
+ob_end_flush();
+?>
