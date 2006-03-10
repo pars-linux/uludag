9028bb790e32d11b14615426ada13dfeb94222aa
Gokmen Goksel <gokmen@goksel.me>
2006-03-10 18:25:25 +0000

    pardul base gelsin..

---
+++ pardul/lib/utils.php
@@ -0,0 +1,173 @@
+<?php
+
+    /*
+        TUBITAK UEKAE 2005-2006
+        Gökmen GÖKSEL gokmen_at_pardus.org.tr
+    */
+
+    require_once ("functions.php");
+
+    /*
+        make_*($parameters) it adds or updates a field with given paramaeters
+        return Boolean;
+    */
+    function make_category($categoryid="x",$categoryname,$parentid=0){
+        global $config;
+        $categoryname = rtag($categoryname);
+	$categoryid = "x" ? $sql_word = "INSERT INTO {$config['db']['tableprefix']}Categories VALUES ('','{$categoryname}', '{$parentid}')" :
+        $sql_word = "UPDATE {$config['db']['tableprefix']}Category SET CategoryName='{$categoryname}', ParentID='{$parentid}' WHERE ID='$categoryid'";
+        return @mysql_query($sql_word);
+    }
+
+    function make_distribution($distid="x",$distversion,$distname="Pardus"){
+        global $config;
+        $distversion = rtag($distversion);
+        $distname = rtag($distname);
+	$distid = "x" ? $sql_word = "INSERT INTO {$config['db']['tableprefix']}Distribution VALUES ('','{$distversion}', '{$distname}')" :
+        $sql_word = "UPDATE {$config['db']['tableprefix']}Distribution SET DistVersion='{$distversion}', DistName='{$distname}' WHERE ID='$distid'";
+        return @mysql_query($sql_word);
+    }
+
+    function make_user($uid="x",$realname,$web,$email,$passwordc,$uname=""){
+        global $config;
+        $realname = rtag ($realname);
+        $email = rtag ($email);
+        $web = rtag ($web);
+        $password = md5(rtag($passwordc));
+        if ($passwordc<>"") $attach_sql=", UserPass='{$password}'";
+        $uid = "x" ? $sql_word = "INSERT INTO {$config['db']['tableprefix']}Users VALUES ('', '{$uname}', '{$password}','{$realname}', '{$email}', '{$web}', 'G', '')" :
+        $sql_word = "UPDATE {$config['db']['tableprefix']}Users SET UserName='{$realname}', UserWeb='{$web}', UserEmail='{$email}'".$attach_sql." WHERE ID='$uid'";
+        return @mysql_query($sql_word);
+    }
+
+    function make_vendor($vendorid="x",$vendorname,$vendorurl,$vendorvid=''){
+        global $config;
+        $vendorname = rtag ($vendorname);
+        $vendorurl = rtag ($vendorurl);
+        $vendorvid = rtag ($vendorvid);
+        $vendorid = "x" ? $sql_word = "INSERT INTO {$config['db']['tableprefix']}Vendors VALUES ('', '{$vendorname}', '{$vendorurl}','{$vendorvid}')" :
+        $sql_word = "UPDATE {$config['db']['tableprefix']}Vendors SET VendorName'{$vendorname}', VendorURL='{$vendorurl}', VendorID='{$vendorvid}' WHERE ID='$vendorid'";
+        return @mysql_query($sql_word);
+    }
+
+    /*
+        get_categories() it returns types with parents and childs like ['parent_type']['sub']['child_type'] sub is CONST
+        return Array;
+    */
+    function get_categories($parent=0){
+        global $config;
+        $sql_word = "SELECT * FROM {$config['db']['tableprefix']}Categories WHERE parentID='{$parent}'";
+        $sql_query = @mysql_query($sql_word);
+        for($i = 0; $i < @mysql_num_rows($sql_query); $i++){
+            $assoc_arr = mysql_fetch_assoc($sql_query);
+            $return_array[$i] = $assoc_arr;
+            $j=0;
+            if ($parent==0){
+                if ($subs=get_types($return_array[$i]['id'])) {
+                    foreach ( $subs as $sub_type ) {
+                        $return_array[$i]['sub'][$j]=$sub_type;
+                        $j++;
+                    }
+                }
+            }
+        }
+        return $return_array;
+    }
+
+        /*
+        get_($id) it returns a field from $table which has $id
+        $table can be Categories,Distribution,Hardwares,Users,Platform,Vendors,Comments or ActionCompatibility
+        return Array;
+    */
+    function get_($id,$table){
+        global $config;
+        $sql_word = "SELECT * FROM {$config['db']['tableprefix']}{$table} WHERE ID='{$id}'";
+        return perform_sql($sql_word);
+    }
+
+    /*
+        del_($id,$table) it removes a field from $table which has $id
+        $table can be Categories,Distribution,Hardwares,Users,Platform,Vendors,Comments or ActionCompatibility
+        return Boolean;
+    */
+    function del_($id,$table){
+        global $config;
+	$sql_word = "DELETE FROM {$config['db']['tableprefix']}{$table} WHERE ID='{$id}'";
+        return @mysql_query($sql_word);
+    }
+
+
+    /*
+        user_exist($uname)
+        it checks the user $uname and returns users info
+        return Array;
+    */
+    function user_exist($uname){
+        global $config;
+        $sql_word = "SELECT ID FROM {$config['db']['tableprefix']}Users WHERE UserName='$uname'";
+        return perform_sql($sql_word);
+    }
+
+/**/
+    function get_entry($id){
+        global $config;
+	$sql_word = "SELECT * FROM {$config['db']['tableprefix']}entry WHERE id = '$id'";
+        return perform_sql($sql_word);
+    }
+
+    function get_entries($userid,$date=""){
+        global $config;
+
+        $date <> "" ? $sql_add=" date LIKE '{$date}%' AND " : $sql_add="";
+        $sql_word = "SELECT * FROM {$config['db']['tableprefix']}entry WHERE ".$sql_add." author = '$userid'";
+        return perform_sql($sql_word);
+    }
+
+    function get_entries_by_month($userid){
+        global $config;
+        $sql_word = "SELECT * FROM {$config['db']['tableprefix']}entry WHERE ".$sql_add." author = '$userid'";
+        return perform_sql($sql_word);
+    }
+    
+    function add_entry($userid,$title,$entry,$date,$categories,$state){
+        global $config;
+        $sql_word = "INSERT INTO {$config['db']['tableprefix']}entry VALUES ('','{$title}', '{$entry}', '{$date}', '{$userid}', '{$state}')";
+        if (!(@mysql_query($sql_word))) return FALSE;
+        else link_category_entry($categories,mysql_insert_id());
+        return mysql_insert_id();
+    }
+
+    function del_entry($userid,$id){
+        global $config;
+
+    }
+
+    function update_entry($id,$title,$entry,$date,$categories,$state){
+        global $config;
+	$sql_word = "UPDATE {$config['db']['tableprefix']}entry SET title='{$title}', entry='{$entry}', date='{$date}', status='{$state}' WHERE id='$id'";
+        if (!(@mysql_query($sql_word))) return FALSE;
+        else link_category_entry($categories,$id);
+        return $id;
+    }
+
+    function link_category_entry($cat_id,$entry_id){
+	global $config;
+        $sql_word = "DELETE FROM {$config['db']['tableprefix']}category_act WHERE entry_id='{$entry_id}'";
+        $sql_query = @mysql_query($sql_word);
+        if ($cat_id):
+	foreach ($cat_id as $cat) {
+            $sql_word = "INSERT INTO {$config['db']['tableprefix']}category_act VALUES ('', '{$cat}', '{$entry_id}')";
+	    $sql_query = @mysql_query($sql_word);
+	}
+        endif;
+    }
+
+    function get_entry_categories($id){
+        global $config;
+	$sql_word = "SELECT * FROM {$config['db']['tableprefix']}category_act WHERE entry_id = '$id'";
+        return perform_sql($sql_word);
+    }
+
+    /* ENTRIES - END */
+
+?>
