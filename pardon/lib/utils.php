339fb2684c5b3d368d5f9d8b83ef06f699158b9b
Gokmen Goksel <gokmen@goksel.me>
2006-03-20 20:48:56 +0000

    bitti bitti :) yarın işallah son bulacak kendilerii..

--- pardon/lib/utils.php
+++ pardon/lib/utils.php
@@ -26,7 +26,7 @@
         $suserid        = rtag($suserid);
         $todo           = rtag($todo);
 
-        $hwid = "x" ? $sql_word = "INSERT INTO {$config['db']['tableprefix']}Hardwares VALUES ('','{$productname}','{$vendor}','{$deviceid}','{$bustype}','{$categoryid}','{$date}','','{$status}','{$userid}','{$suserid}','{$todo}')" :
+        if ($hwid == "x") $sql_word = "INSERT INTO {$config['db']['tableprefix']}Hardwares VALUES ('','{$productname}','{$vendor}','{$deviceid}','{$bustype}','{$categoryid}','{$date}','','{$status}','{$userid}','{$suserid}','{$todo}')"; else 
         $sql_word = "UPDATE {$config['db']['tableprefix']}Hardwares SET HWProductName='{$productname}', HWVendor='{$vendor}',HWDeviceID='{$deviceid}',HWBusType='{$bustype}',HWCategoryID='{$categoryid}',HWUpdateDate='{$date}',Status='{$status}',UserID='{$userid}',SuperUserID='{$suserid}', ToDo = '{$todo}' WHERE ID='$hwid'";
         if (mysql_query($sql_word)) if (make_hardware_status(mysql_insert_id(),$state)) return TRUE; else return FALSE;
     }
@@ -45,8 +45,8 @@
     function make_category($categoryid="x",$categoryname,$parentid=0){
         global $config;
         $categoryname   = rtag($categoryname);
-        $categoryid = "x" ? $sql_word = "INSERT INTO {$config['db']['tableprefix']}Categories VALUES ('','{$categoryname}', '{$parentid}')" :
-        $sql_word = "UPDATE {$config['db']['tableprefix']}Categories SET CategoryName='{$categoryname}', ParentID='{$parentid}' WHERE ID='$categoryid'";
+        if ($categoryid == "x") $sql_word = "INSERT INTO {$config['db']['tableprefix']}Categories VALUES ('','{$categoryname}', '{$parentid}')";
+        else $sql_word = "UPDATE {$config['db']['tableprefix']}Categories SET CategoryName='{$categoryname}', ParentID='{$parentid}' WHERE ID='$categoryid'";
         return mysql_query($sql_word);
     }
 
@@ -54,16 +54,16 @@
         global $config;
         $distversion    = rtag($distversion);
         $distname       = rtag($distname);
-        $distid = "x" ? $sql_word = "INSERT INTO {$config['db']['tableprefix']}Distribution VALUES ('','{$distversion}', '{$distname}')" :
-        $sql_word = "UPDATE {$config['db']['tableprefix']}Distribution SET DistVersion='{$distversion}', DistName='{$distname}' WHERE ID='$distid'";
+        if ($distid == "x") $sql_word = "INSERT INTO {$config['db']['tableprefix']}Distribution VALUES ('','{$distversion}', '{$distname}')";
+        else $sql_word = "UPDATE {$config['db']['tableprefix']}Distribution SET DistVersion='{$distversion}', DistName='{$distname}' WHERE ID='$distid'";
         return @mysql_query($sql_word);
     }
 
     function make_platform($platformid="x",$platform){
         global $config;
         $platform       = rtag($platform);
-        $platformid = "x" ? $sql_word = "INSERT INTO {$config['db']['tableprefix']}Platform VALUES ('','{$platform}')" :
-        $sql_word = "UPDATE {$config['db']['tableprefix']}Platform SET Platform='{$platform}' WHERE ID='$platformid'";
+        if ($platformid == "x")$sql_word = "INSERT INTO {$config['db']['tableprefix']}Platform VALUES ('','{$platform}')";
+        else $sql_word = "UPDATE {$config['db']['tableprefix']}Platform SET Platform='{$platform}' WHERE ID='$platformid'";
         return @mysql_query($sql_word);
     }
 
@@ -75,16 +75,16 @@
         $password       = md5(rtag($passwordc));
         if ($uname<>"") if (user_exist($uname)) return 0;
         if ($passwordc<>"") $attach_sql=", UserPass='{$password}'";
-        $uid = "x" ? $sql_word = "INSERT INTO {$config['db']['tableprefix']}Users VALUES ('', '{$uname}', '{$password}','{$realname}', '{$email}', '{$web}', 'N')" :
-        $sql_word = "UPDATE {$config['db']['tableprefix']}Users SET UserRealName='{$realname}', UserWeb='{$web}', UserEmail='{$email}'".$attach_sql." WHERE ID='$uid'";
+        if ($uid == "x") $sql_word = "INSERT INTO {$config['db']['tableprefix']}Users VALUES ('', '{$uname}', '{$password}','{$realname}', '{$email}', '{$web}', 'N')";
+        else $sql_word = "UPDATE {$config['db']['tableprefix']}Users SET UserRealName='{$realname}', UserWeb='{$web}',UserEmail='{$email}'".$attach_sql." WHERE ID='$uid'";
         return @mysql_query($sql_word);
     }
 
     function make_vendor($act="x",$vendorname){
         global $config;
         $vendorname     = rtag ($vendorname);
-        $act = "x" ? $sql_word = "INSERT INTO {$config['db']['tableprefix']}Vendors VALUES ('{$vendorname}')" :
-        $sql_word = "UPDATE {$config['db']['tableprefix']}Vendors SET VendorName='{$vendorname}' WHERE VendorName='{$vendorname}'";
+        if ($act == "x") $sql_word = "INSERT INTO {$config['db']['tableprefix']}Vendors VALUES ('{$vendorname}')";
+        else $sql_word = "UPDATE {$config['db']['tableprefix']}Vendors SET VendorName='{$vendorname}' WHERE VendorName='{$vendorname}'";
         return @mysql_query($sql_word);
     }
 
@@ -93,8 +93,8 @@
         $hwid           = rtag ($hwid);
         $uid            = rtag ($uid);
         $comment        = rtag ($comment);
-        $commentid = "x" ? $sql_word = "INSERT INTO {$config['db']['tableprefix']}Comments VALUES ('', '{$hwid}','{$uid}','{$comment}','{$adddate}')" :
-        $sql_word = "UPDATE {$config['db']['tableprefix']}Comments SET HWID'{$hwid}', UID='{$uid}', Comment='{$comment}', AddDate='{$adddate}' WHERE ID='$commentid'";
+        if ($commentid == "x") $sql_word = "INSERT INTO {$config['db']['tableprefix']}Comments VALUES ('', '{$hwid}','{$uid}','{$comment}','{$adddate}')";
+        else $sql_word = "UPDATE {$config['db']['tableprefix']}Comments SET HWID'{$hwid}', UID='{$uid}', Comment='{$comment}', AddDate='{$adddate}' WHERE ID='$commentid'";
         return @mysql_query($sql_word);
     }
 
@@ -129,7 +129,7 @@
     */
     function get_($id="x",$table){
         global $config;
-        $id = "x" ? $sql_word = "SELECT * FROM {$config['db']['tableprefix']}{$table}" : $sql_word = "SELECT * FROM {$config['db']['tableprefix']}{$table} WHERE ID='{$id}'";
+        if ($id == "x") $sql_word = "SELECT * FROM {$config['db']['tableprefix']}{$table}"; else $sql_word = "SELECT * FROM {$config['db']['tableprefix']}{$table} WHERE ID='{$id}'";
         return perform_sql($sql_word);
     }
 
@@ -144,6 +144,17 @@
         return @mysql_query($sql_word);
     }
 
+
+    function activate_($id){
+        global $config;
+        $tmp = get_($id,"Hardwares");
+        $vendor = $tmp[0]["HWVendor"];
+        $sql_word = "SELECT * FROM {$config['db']['tableprefix']}Vendors WHERE VendorName='$vendor'";
+        echo $sql_word;
+        if (mysql_num_rows(mysql_query($sql_word))==0) mysql_query("INSERT INTO {$config['db']['tableprefix']}Vendors VALUES ('{$vendor}')");
+	$sql_word = "UPDATE {$config['db']['tableprefix']}Hardwares SET Status=1 WHERE ID='$id'";
+        return @mysql_query($sql_word);
+    }
     /*
         user_exist($uname)
         it checks the user $uname and returns users info
@@ -194,10 +205,20 @@
         return perform_sql($sql_word);
     }
 
-    function get_products($field,$userid){
+    function get_products($field,$value){
         global $config;
-        $sql_word = "SELECT * FROM {$config['db']['tableprefix']}Hardwares WHERE $field = '$userid'";
-        return perform_sql($sql_word);
+        $sql_word = "SELECT * FROM {$config['db']['tableprefix']}Hardwares WHERE $field = '$value'";
+        $single = perform_sql($sql_word);
+        foreach ($single as $key => $node) {
+            $value = $node["UserID"];
+            $sql_word = "SELECT UserRealName FROM {$config['db']['tableprefix']}Users WHERE ID = '$value'";
+            $tmp = mysql_fetch_row(mysql_query($sql_word));
+            $single[$key]["UserName"] = $tmp[0];
+            $value = $node["ID"];
+            $sql_word = "SELECT HWState FROM {$config['db']['tableprefix']}ActionCompatibility WHERE HWID = '$value' AND (HWState = 'F' OR HWState = 'S')";
+            $single[$key]["HWState"] = mysql_num_rows(mysql_query($sql_word));
+        }
+        return $single;
     }
 
 ?>
