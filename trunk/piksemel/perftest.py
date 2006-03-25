52e21ba4446815417623a6f0a6ea37488e962355
gurer <gurer@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2006-03-25 22:05:31 +0000

    böyle takarlar cdom'a :p

--- trunk/piksemel/perftest.py
+++ trunk/piksemel/perftest.py
@@ -3,28 +3,32 @@
 import sys
 import piksemel as iks
 import xml.dom.minidom as mini
+from Ft.Xml.Domlette import NoExtDtdReader
+import Ft.Lib
 import timeit
 
 def piksor(name):
     a = iks.parse(name)
-    print "Processing %s" % a.name()
     total = 0
     for b in a.tags():
         size = b.getTagData("Size")
         if size:
             size = size.strip()
             total += int(size)
-    print "Total size is %d" % total
+
+def cdom(name):
+    doc = NoExtDtdReader.parseUri(Ft.Lib.Uri.OsPathToUri(name))
 
 def suxor(name):
     a = mini.parse(name)
-    print a.documentElement.tagName
 
 if __name__ == "__main__":
     name = sys.argv[1]
     a = timeit.Timer('piksor("%s")' % name, "from __main__ import piksor")
-    b = timeit.Timer('suxor("%s")' % name, "from __main__ import suxor")
-    print a.timeit(1)
-    print b.timeit(1)
+    b = timeit.Timer('cdom("%s")' % name, "from __main__ import cdom")
+    c = timeit.Timer('suxor("%s")' % name, "from __main__ import suxor")
+    print "piksemel", a.timeit(1)
+    print "cdom", b.timeit(1)
+    print "minidom", c.timeit(1)
 
 
