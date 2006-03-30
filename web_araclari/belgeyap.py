fb1a50aa57e47aa99c5e3e176c025e452976bfc2
Gurer Ozen <meduketto@users.noreply.github.com>
2006-03-30 11:40:02 +0000

    çalışıyor, biraz daha fix gelecek

--- web_araclari/belgeyap.py
+++ web_araclari/belgeyap.py
@@ -36,15 +36,15 @@ def run(*cmd):
 # SVN
 #
 
-def svn_fetch(path, filename):
-    print "'%s' çekiliyor..." % path
+def svn_fetch(repo_uri, filename):
+    print "'%s' çekiliyor..." % repo_uri
     # fetch last revision
-    data = capture("/usr/bin/svn", "cat", path)
+    data = capture("/usr/bin/svn", "cat", repo_uri)
     f = file(filename, "w")
     f.write(data[0])
     f.close()
     # get last changed date
-    data = capture("/usr/bin/svn", "info", path)
+    data = capture("/usr/bin/svn", "info", repo_uri)
     date = None
     for tmp in data[0].split("\n"):
         if tmp.startswith("Last Changed Date: "):
@@ -57,16 +57,16 @@ def svn_fetch(path, filename):
 
 def retouch_lyx(lyxname):
     # FIXME: fix regexps, handle hyperref package, and other minor probs
-    print "'%s' düzeltiliyor..." % self.lyxname
+    print "'%s' düzeltiliyor..." % lyxname
     # lyx dosyasini okuyalim
-    f = file(self.lyxfile, "r")
+    f = file(lyxname, "r")
     lyx = f.read()
     f.close()
     # paragraf aralari bosluk olmali
     #re.sub("\\paragraph_separation .*?\n", "\\paragraph_separation skip\n", lyx)
     # kaliteli pdf cikti icin font secimi
     #re.sub("\\fontscheme .*?\n", "\\fontscheme pslatex\n", lyx)
-    f = file(self.lyxfile, "w")
+    f = file(lyxname, "w")
     f.write(lyx)
     f.close()
 
@@ -77,7 +77,7 @@ def retouch_lyx(lyxname):
 def export_pdf(lyxname, pdfname):
     print "'%s' oluşturuluyor..." % pdfname
     run("/usr/bin/lyx", "-e", "pdf2", lyxname)
-    shutil.move(lyxname + ".pdf", pdfname)
+    shutil.move(lyxname[:-4] + ".pdf", pdfname)
     return str(os.stat(pdfname)[ST_SIZE] / 1024)
 
 #
@@ -126,40 +126,65 @@ def fix_html(htmlname):
     f.write(html_tmpl % doc)
     f.close()
 
-def export_html(lyxname, texname, htmlname):
+def export_html(lyxname, htmlname):
     f = file("duzeltmeler.hva", "w")
     f.write(hevea_fixes)
     f.close()
+    texname = lyxname[:-4] + ".tex"
     print "'%s' oluşturuluyor..." % texname
     run("/usr/bin/lyx", "-e", "latex", lyxname)
     print "'%s' oluşturuluyor..." % htmlname
     run("/usr/bin/hevea", "-fix", "duzeltmeler.hva", texname, "-o", htmlname)
+    os.unlink("duzeltmeler.hva")
+    os.unlink(texname)
     fix_html(htmlname)
 
 #
-# Main
+# Document converter
 #
 
 entry_tmpl = """
 <tr>
 <td align="left"><b>%(NAME)s</b> (%(DATE)s)</td>
-<td><a href="./%(DIR)s/index.html">HTML</a></td>
-<td><a href="./%(DIR)s/%(DIR)s.html">HTML (tek sayfa)</a></td>
-<td><a href="./%(DIR)s/%(DIR)s.pdf">PDF (%(PDFSIZE)s KB)</a></td>
+<td><a href="./%(HTMLNAME)s">HTML</a></td>
+<td><a href="./%(PDFNAME)s">PDF (%(PDFSIZE)s KB)</a></td>
 </tr>
 """
 
-def make_document(repo_uri, name, do_fetch=True):
-    path = os.path.dirname(repo_uri)
+def make_document(repo_uri, do_fetch=True):
     filename = os.path.basename(repo_uri)
     basename = filename[:]
     if basename.endswith(".lyx"):
         basename = basename[:-4]
     pdfname = basename + ".pdf"
     htmlname = basename + ".html"
+    
+    if do_fetch:
+        last_edit = svn_fetch(repo_uri, filename)
+        # FIXME: gereken diğer dosyaları da çek
+    else:
+        last_edit = "depodan çekmeden bilemem"
+    
+    retouch_lyx(filename)
+    pdf_size = export_pdf(filename, pdfname)
+    export_html(filename, htmlname)
+    
+    # FIXME: belge adını dosyadan çekmeye çalış
+    dict = {
+        "NAME": basename,
+        "HTMLNAME": htmlname,
+        "PDFNAME": pdfname,
+        "DATE": last_edit,
+        "PDFSIZE": pdf_size,
+    }
+    print entry_tmpl % dict
+
+#
+# Command line driver
+#
 
 def usage():
-    print "Kullanım: belgeyap.py [seçenekler] <svn_repo_adresi> <belge_adı>"
+    print "Kullanım: belgeyap.py [seçenekler] <svn_belge_adresi>..."
     print " -h, --help        Yardım"
     print " -f, --no-fetch   Dosyaları yeniden çekme"
 
@@ -187,56 +212,12 @@ def main(args):
         if o in ("-f", "--no-fetch"):
             do_fetch = False
     
-    if len(args) != 2:
+    if len(args) < 1:
         usage()
         return
     
-    make_document(args[0], args[1], do_fetch)
-
-#
-#
-#
-
-class Exporter:
-    def __init__(self, lyxfile, basename, img_path):
-
-def generate_doc(docfile, do_fetch=True):
-    # defaults
-    ikonlar = "../../images"
-    dosyalar = []
-    # open document template and get information
-    f = file(docfile)
-    exec(f)
-    f.close()
-    # cd into document directory
-    enter_dir(dizin)
-    # fetch files
-    date = ""
-    if do_fetch:
-        if do_fetch:
-            date = svn_fetch(depo, belge)
-        # extra files, images, etc
-        for t in dosyalar:
-            ensure_path(t)
-            svn_fetch(depo, t)
-    # do the export
-    a = Exporter(belge, dizin, ikonlar)
-    a.export()
-    # add to svn
-    # FIXME: handle svn
-    # output information
-    dict = {
-        "NAME": isim,
-        "DATE": date,
-        "DIR": dizin,
-        "PDFSIZE": str(os.stat(dizin + ".pdf")[ST_SIZE] / 1024)
-    }
-    print entry_tmpl % dict
-    # leave directory
-    os.chdir("..")
-
-
-
+    for arg in args:
+        make_document(arg, do_fetch)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
