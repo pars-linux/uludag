a8648ed86ef3de3a3e0def6696ba3dee225b203c
gokmen.goksel <gokmen.goksel@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2006-04-04 21:34:30 +0000

    ve gökmen uyur, bu iş yarın biter, saygılar zevgiler..

---
+++ trunk/mawe/commonlib/php/maWeFunctions.php
@@ -0,0 +1,168 @@
+<?php
+
+    /**
+    *    TUBITAK UEKAE 2005-2006
+    *    Gökmen GÖKSEL gokmen_at_pardus.org.tr
+    *    Mawe - version 0.3
+    */
+
+    require_once 'Smarty/Smarty.class.php';
+    require_once 'FCKeditor/fckeditor.php';
+
+    /** DB FUNCTIONS BEGIN **/
+
+    /**
+    *    maWedbConnect();
+    */
+    function maWeDBConnect($db){
+        global $db_connection;
+        $db_connection = @mysql_pconnect($db['Server'], $db['UserName'], $db['Password']);
+        show_mysql_errors();
+        @mysql_select_db($db['DbName']);
+        show_mysql_errors();
+    }
+
+    /**
+    * show_mysql_errors()
+    * It shows nice mysql errors
+    * return NULL;
+    */
+    function show_mysql_errors() {
+        if (mysql_error()) {
+            echo "<pre>";
+            echo "Database Connection Error !<br>";
+            echo "Error Message : <b>".mysql_error()."</b><br>";
+            echo "Error Number  : <b>".mysql_errno()."</b><br>";
+            echo "</pre>";
+            die();
+        }
+    }
+
+    /**
+    * perform_sql()
+    * it performs given sql query
+    * return BOOLEAN;
+    */
+    function perform_sql($sql_word){
+        $sql_query = mysql_query($sql_word);
+        for($i = 0; $i < mysql_num_rows($sql_query); $i++){
+            $assoc_arr = mysql_fetch_assoc($sql_query);
+            $return[$i] = $assoc_arr;
+        }
+        if (empty($sql_query)) return 0;
+        else return $return;
+    }
+
+    /** DB Functions END **/
+
+    /** SMARTY Functions BEGIN **/
+
+    /**
+    * maWeSetSmartyVar($varname, $var)
+    * It makes variable for using in smarty (in theme files)
+    * return NULL;
+    */
+    function maWeSetSmartyVar($varname, $var){
+        global $maWeSmarty;
+        $maWeSmarty->assign($varname,$var);
+    }
+
+	function maWeShowSmarty($page){
+		global $maWeSmarty;
+		$maWeSmarty->display($page);
+	}
+	
+    function maWeSetSmarty($sm){
+        global $maWeSmarty;
+        $maWeSmarty = new Smarty;
+        $maWeSmarty->template_dir    = $cc['Path'].'themes/'.$sm['Theme'];
+        $maWeSmarty->plugins_dir     = $cc['Path'].'commonlib/php/Smarty/plugins';
+        $maWeSmarty->cache_dir       = $cc['Path'].'temp';
+        $maWeSmarty->caching         = $sm['Cache'];
+        $maWeSmarty->compile_dir     = $cc['Path'].'temp';
+        $maWeSmarty->force_compile   = $sm['Force'];
+        $maWeSmarty->clear_all_cache();
+       
+    }
+    /** SMARTY Functions END **/
+
+    /** FCK Functions BEGIN **/
+
+    /**
+    * maWeShowFCK();
+    * it shows FCK Editor with given config
+    */
+    function maWeShowFCK($Value=""){
+        global $maWeFCK;
+
+        $maWeFCK = new FCKeditor('maWeFCKeditor');
+        $maWeFCK->BasePath = $cc['Path'].'commonlib/php/FCKeditor/';
+        $maWeFCK->Height = '500px';
+        $maWeFCK->Width  = '100%';
+        $maWeFCK->ToolbarCanCollapse = true;
+        
+        if ($Value<>"") 
+            $maWeFCK->Value = $Value;
+        
+        return $maWeFCK->CreateHtml();
+    }
+    
+    /** FCK Functions END **/
+
+    /** Misc Functions BEGIN **/
+
+    /**
+    * rtag($foo)
+    * it returns removed known html tags version of $foo
+    */
+    function rtag($foo){
+        return htmlspecialchars($foo,ENT_QUOTES);
+    }
+
+    /**
+    * maWeCheckLogin($Login,$User,$Pass,$Quit)
+    * huge login function, if ok return true else shows login.mt end die.
+    * FIXME write Light version of it also check security
+    */
+	function maWeCheckLogin($Login,$User,$Pass,$Quit){
+        session_start();
+        if (isset($Quit)) {
+            session_unregister($cc['Session']);
+            header ("location: login.php");
+            return;
+        }
+        if (session_is_registered($cc['Session'])) { 
+            return true;
+        }
+        elseif($Login) { 
+            if (maWeCheckUser($User,$Pass)){
+                maWeSetSession($info,$cc['Session']);
+                header ("location: login.php");
+            }
+            else {
+                maWeSetSmartyVar("error",true);
+            }
+        }
+        maWeShowSmarty('login.mt'); 
+        die(); 
+	}
+	
+    /**
+    - maWeCheckUser($User,$Pass)
+    * Checks user info from Users table
+    * if ok returns user info else return false
+    */
+    function maWeCheckUser($User, $Pass){
+        global $db;
+        $Pass  = md5($Pass);
+        $sql = "SELECT * FROM {$db['DbPrefix']}Users WHERE UserName = '$User' AND UserPass = '$Pass'";
+        return perform_sql($sql);
+    }
+
+    function maWeSetSession($info,$key){
+        @session_register($key);
+        return true;
+    }
+    /** Misc Functions END **/
+
+?>
---
+++ trunk/mawe/themes/mawe/login.mt
@@ -0,0 +1,30 @@
+{include file="head.mt"}
+    {if $error}{assign var="errorstyle" value="style='border: 1px solid #FF0000';"}{/if}
+    <form action="login.php" method="post">
+    <fieldset id="loginFieldSet" style="width: 300px;">
+        <legend>Login</legend>
+        <div class="bebox">
+            <span class="label"><label for="maweUsername">Username:&nbsp;</label></span>
+            <span class="input"><input {$errorstyle} type="text" name="maweUserName" id="maweUserName" title="Username" /></span>
+        </div>
+        <br />
+        <div class="bebox">
+            <span class="label"><label for="mawePassword">Password:&nbsp;</label></span>
+            <span class="input"><input {$errorstyle} type="password" name="mawePassword" id="mawePassword" title="Password" /></span>
+        </div>
+        <br />
+        <div class="bebox">
+            <button type="submit" name="DoLogin" id="DoLogin" value="Login" style="float: right;">Login</button>
+        </div>
+        <br />
+    </fieldset>
+    </form>
+    {if $error}
+    <code>
+    <b>An error occured : </b><span style='color: red'>Username or Password Wrong !</span>
+    </code>
+    {/if}
+</div>
+</center>
+</body>
+</html>
