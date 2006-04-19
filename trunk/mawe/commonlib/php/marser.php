bb1bafcdfb9e6dd2e08ba6605bf43af2c333d8e3
gokmen.goksel <gokmen.goksel@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2006-04-19 06:44:38 +0000

    be a good boy, forget it and imagine your watch hadn't been scratched

--- trunk/mawe/commonlib/php/marser.php
+++ trunk/mawe/commonlib/php/marser.php
@@ -2,23 +2,41 @@
 
     // marser
 
+
     function coded($string)
     {
+        $rules = Array();
 
-        $string = preg_replace("#\[b\](.+?)\[/b\]#is", "<b>\\1</b>", $string);
-        $string = preg_replace("#\[i\](.+?)\[/i\]#is", "<i>\\1</i>", $string);
-        $string = preg_replace("#\[u\](.+?)\[/u\]#is", "<u>\\1</u>", $string);
+        $rules[0]["replace"] = "#\[b\](.+?)\[/b\]#is";
+        $rules[0]["to"]      = "<b>\\1</b>";
 
-        $string = preg_replace("#\[br\]#is", "<br>", $string);
+        $rules[1]["replace"] = "#\[i\](.+?)\[/i\]#is";
+        $rules[1]["to"]      = "<i>\\1</i>";
 
+        $rules[2]["replace"] = "#\[u\](.+?)\[/u\]#is";
+        $rules[2]["to"]      = "<u>\\1</u>";
 
-        $string = preg_replace("#\[link\]www\.(.+?)\[/link\]#is", "<a href=\"http://www.\\1\">www.\\1</a>", $string);
-        $string = preg_replace("#\[link\](.+?)\[/link\]#is", "<a href=\"\\1\">\\1</a>", $string);
-        $string = preg_replace("#\[link=(.+?)\](.+?)\[/link\]#is", "<a href=\"\\1\">\\2</a>", $string);
+        $rules[3]["replace"] = "#\[br\]#is";
+        $rules[3]["to"]      = "<br>";
 
-        $string = preg_replace("#\[img\](.+?)\[/img\]#is", "<img src=\"\\1\" alt=\"[image]\" style=\"margin: 5px 0px 5px 0px\" />", $string);
-        $string = preg_replace("#\[img-l\](.+?)\[/img\]#is", "<img src=\"\\1\" alt=\"[image]\" style=\"float: left; margin: 0px 5px 5px 0px\" />", $string);
-        $string = preg_replace("#\[img-r\](.+?)\[/img\]#is", "<img src=\"\\1\" alt=\"[image]\" style=\"float: right; margin: 0px 0px 5px 5px\" />", $string);
+        $rules[4]["replace"] = "#\[link\]www\.(.+?)\[/link\]#is";
+        $rules[4]["to"]      = "<a href=\"http://www.\\1\">www.\\1</a>";
+
+        $rules[5]["replace"] = "#\[link\](.+?)\[/link\]#is";
+        $rules[5]["to"]      = "<a href=\"\\1\">\\1</a>";
+
+        $rules[6]["replace"] = "#\[link=(.+?)\](.+?)\[/link\]#is";
+        $rules[6]["to"]      = "<a href=\"\\1\">\\2</a>";
+
+        $rules[7]["replace"] = "#\[img\](.+?)\[/img\]#is";
+        $rules[7]["to"]      = "<img src=\"\\1\" alt=\"[image]\" style=\"margin: 5px 0px 5px 0px\" />";
+
+        $rules[8]["replace"] = "#\[img-(.+?)\](.+?)\[/img\]#is";
+        $rules[8]["to"]      = "<img src=\"\\2\" alt=\"[image]\" style=\"float: \\1; margin: 0px 5px 5px 0px\" />";
+
+        foreach($rules as $node) {
+            $string = preg_replace($node["replace"],$node["to"],$string);
+        }
 
         return $string;
     }
