5d06e55da1653320aeb8a309fa148646645e1109
gokmen.goksel <gokmen.goksel@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2006-04-20 22:12:16 +0000

    fixes, lang, etc.

--- trunk/arto/index.php
+++ trunk/arto/index.php
@@ -51,7 +51,7 @@ include_once("globals.php");
                 elseif (update_user("x",$_POST["realname"],$_POST["web"],$_POST["email"],$_POST["password"],$_POST["username"])){
                             $temporary = get_user_id($_POST["username"]);
                             $activationcode = md5($temporary[0]['id'].$config["core"]["secretkey"]);
-                            $mail_message = "Merhaba\n\n    Siz ya da bir başkası bu e-posta adresini kullanarak {$config['core']['title']} ({$config['core']['url']}) sitesine kayıt yaptırdı.\n    Eğer kaydı siz yaptırdıysanız onaylamak için aşağıdaki bağlantıyı tıklayın.\n\n Onaylamak için tıklayın: {$config['core']['url']}?activateuser&username={$_POST["username"]}&code={$activationcode}\n İptal etmek için tıklayın: {$config['core']['url']}?activateuser&action=delete&username={$_POST["username"]}&code={$activationcode}\n\n İlginiz için teşekkürler.\n Uludağ Projesi";
+                            $mail_message = "Merhaba\n\n    Siz ya da bir başkası bu e-posta adresini kullanarak {$config['core']['title']} ({$config['core']['url']}) sitesine kayıt yaptırdı.\n    Eğer kaydı siz yaptırdıysanız onaylamak için aşağıdaki bağlantıyı tıklayın.\n\n {$config['core']['url']}?activateuser&username={$_POST["username"]}&code={$activationcode}\n İlginiz için teşekkürler.\n Pardus Projesi";
                             sendmail($config['core']['email'],$_POST["email"],REGISTER_EMAIL_SUBJECT,$mail_message,"3");
                             set_smarty_vars("info",REGISTER_OK);
                 }
