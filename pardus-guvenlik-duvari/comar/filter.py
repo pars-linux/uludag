26b2ead3e3828cfca8db395b7d5af0556bac6b65
Bahadır Kandemir <kandemir@gmail.com>
2006-04-25 18:22:35 +0000

    Heta varsa söyle...

--- pardus-guvenlik-duvari/comar/filter.py
+++ pardus-guvenlik-duvari/comar/filter.py
@@ -11,6 +11,21 @@ def run(*cmd):
         return subprocess.call(cmd)
 
 
+def atoi(s):
+    """String to integer"""
+    t = ""
+    for c in s.lstrip():
+        if c in "0123456789":
+            t += c
+        else:
+            break
+    try:
+        ret = int(t)
+    except:
+        ret = 0
+    return ret
+
+
 def buildRule(action='A', rules={}):
     """Generate IPTables command from given rule"""
     args = []
@@ -19,7 +34,7 @@ def buildRule(action='A', rules={}):
         args.append("-A %s" % rules.get("chain", "INPUT"))
     else:
         args.append("-D %s" % rules.get("chain", "INPUT"))
-        
+
     args.append("--protocol %s" % rules.get("protocol", "tcp"))
 
     if "src" in rules:
@@ -30,34 +45,46 @@ def buildRule(action='A', rules={}):
 
     # FIXME: not all protocols allow these parameters
     if "sport" in rules:
-        args.append("--source-port %s" % rules["sport"])
+        if 1 < atoi(rules["sport"]) < 65535:
+            args.append("--source-port %s" % rules["sport"])
+        else:
+            fail("Invalid source port")
 
     if "dport" in rules:
-        args.append("--destination-port %s" % rules["dport"])
+        if 1 < atoi(rules["dport"]) < 65535:
+            args.append("--destination-port %s" % rules["dport"])
+        else:
+            fail("Invalid destination port")
+
+    jump = rules.get("jump", "REJECT")
+    if jump not in ["REJECT", "ALLOW", "DROP"]:
+        fail("Invalid action")
 
     cmds = []
     cmds.append('/sbin/iptables -t filter %s -j LOG --log-tcp-options --log-level 3' % ' '.join(args))
 
-    if rules.get("jump", "REJECT") == 'REJECT':
+    if jump == 'REJECT':
         cmds.append('/sbin/iptables -t filter %s -j REJECT --reject-with tcp-reset' % ' '.join(args))
     else:
-        cmds.append('/sbin/iptables -t filter tcp %s -j %s' % (' '.join(args), rules.get("jump", "REJECT")))
+        cmds.append('/sbin/iptables -t filter %s -j %s' % (' '.join(args), jump))
 
     return cmds
 
 
-def setRule(**rules):
+def setRule(**rule):
     """Append new firewall rule"""
-    for cmd in buildRule('A', rules):
-        ret = run(cmd)
-
+    cmds = buildRule('A', rule)
+    ret_log, ret_jmp = map(run, cmds)
+    if ret_log != 0 or ret_jmp != 0:
+        fail("Invalid command")
 
 def unsetRule(no):
     """Remove given firewall rule"""
-    d = get_instance("no", no)
-    
-    for cmd in buildRule('D', d):
-        run(cmd)
+    rule = get_instance("no", no)
+    cmds = buildRule('D', rule)
+    ret_log, ret_jmp = map(run, cmds)
+    if ret_log != 0 or ret_jmp != 0:
+        fail("Invalid command")
 
 def getRule(no):
     """Get details of given rule number"""
