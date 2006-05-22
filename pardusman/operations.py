64bbd768f7d99ea8c8e0b26e4c7f188fd49c8f26
Gurer Ozen <meduketto@users.noreply.github.com>
2006-05-22 11:41:47 +0000

    iso oluşturma işlemleri

---
+++ pardusman/operations.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2006, TUBITAK/UEKAE
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version. Please read the COPYING file.
+#
+
+import os
+import shutil
+
+
+class ISO:
+    def __init__(self, tmpdir):
+        self.tmpdir = tmpdir
+        self.workdir = os.path.join(tmpdir, "pardusman_cd_work_dir")
+        if os.path.exists(self.workdir):
+            os.system("rm -rf %s" % self.workdir)
+        try:
+            os.makedirs(self.tmpdir)
+        except:
+            pass
+    
+    def setup_contents(self, contentdir):
+        shutil.copytree(contentdir, self.workdir)
+    
+    def setup_cdroot(self, cdroot):
+        os.link(cdroot, os.path.join(self.workdir, "pardus"))
+    
+    def setup_packages(self, packagelist):
+        repodir = os.path.join(self.workdir, "repo") 
+        os.mkdir(repodir)
+        for path in packagelist:
+            os.link(path, os.path.join(repodir, os.path.basename(path)))
+    
+    def setup_boot(self, ):
+        pass
+    
+    def make(self, name):
+        os.system('mkisofs -J -joliet-long -R -l -V "%s" -o "%s" -b boot/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -boot-info-table %s' %
+            (name, os.path.join(self.tmpdir, "%s.iso" % name), self.workdir))
