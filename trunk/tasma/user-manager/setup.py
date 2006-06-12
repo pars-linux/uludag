f35e553ffc70e462e7644188746a6a618a31a485
gurer <gurer@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2006-06-12 07:37:37 +0000

    değişim sürüyor!

--- trunk/tasma/user-manager/setup.py
+++ trunk/tasma/user-manager/setup.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2005, TUBITAK/UEKAE
+# Copyright (C) 2005-2006, TUBITAK/UEKAE
 #
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free
 # Software Foundation; either version 2 of the License, or (at your option)
 # any later version.
 #
-# Please read the COPYING file.
-#
-#
-# Authors:  Gürer Özen <gurer@uludag.org.tr>
 
 import kdedistutils
 
