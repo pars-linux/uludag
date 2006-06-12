0197640292f21c849b543700d2741819a22a509c
gurer <gurer@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2006-06-12 19:46:37 +0000

    hafif düzen

--- trunk/tasma/user-manager/user-manager.py
+++ trunk/tasma/user-manager/user-manager.py
@@ -18,7 +18,7 @@ from kdeui import *
 import mainview
 from utility import *
 
-version = "0.1"
+version = "0.5"
 
 def AboutData():
     return KAboutData(
@@ -34,88 +34,74 @@ def AboutData():
     )
 
 
-# Are we running as a separate standalone application or in KControl?
-standalone = __name__=='__main__'
-
-if standalone:
-    programbase = QDialog
-else:
-    programbase = KCModule
-
-
-class MainApplication(programbase):
-    def __init__(self, parent=None, name=None):
-        global standalone
-
-        if standalone:
-            QDialog.__init__(self, parent, name)
-            self.setCaption(i18n("User Management"))
-            self.setMinimumSize(620, 380)
-            self.resize(520, 420)
-        else:
-            KCModule.__init__(self, parent, name)
-            KGlobal.locale().insertCatalogue("user-manager")
-            # Create a configuration object.
-            self.config = KConfig("user-manager")
-            self.setButtons(self.Help)
-            self.aboutdata = AboutData()
-
-        # The appdir needs to be explicitly otherwise we won't be able to
-        # load our icons and images.
+class Module(KCModule):
+    def __init__(self, parent, name):
+        KCModule.__init__(self, parent, name)
+        KGlobal.locale().insertCatalogue("user-manager")
+        self.config = KConfig("user-manager")
+        self.setButtons(self.Help)
+        self.aboutdata = AboutData()
         KGlobal.iconLoader().addAppDir("user-manager")
-        
-        
         self.mainwidget = mainview.UserManager(self, self)
-
         toplayout = QVBoxLayout(self, 0, KDialog.spacingHint())
         toplayout.addWidget(self.mainwidget)
-
-        self.aboutus = KAboutApplication(self)
-
-    def __del__(self):
-        pass
-
-    # KControl virtual void methods
+    
     def load(self):
         pass
+    
     def save(self):
         self.mainwidget.execute()
+    
     def defaults(self):
         pass        
+    
     def sysdefaults(self):
         pass
     
     def aboutData(self):
-        # Return the KAboutData object which we created during initialisation.
         return self.aboutdata
     
     def buttons(self):
-        # Only supply a Help button. Other choices are Default and Apply.
         return KCModule.Help
+
+
+class App(QDialog):
+    def __init__(self):
+        QDialog.__init__(self)
+        self.setCaption(i18n("User Manager"))
+        self.setMinimumSize(620, 380)
+        self.resize(520, 420)
+        KGlobal.iconLoader().addAppDir("user-manager")
+        self.mainwidget = mainview.UserManager(self, self)
+        toplayout = QVBoxLayout(self, 0, KDialog.spacingHint())
+        toplayout.addWidget(self.mainwidget)
+
+#        self.aboutus = KAboutApplication(self)
 
 
 # This is the entry point used when running this module outside of kcontrol.
 def main():
     global kapp
-
+    
     about_data = AboutData()
     KCmdLineArgs.init(sys.argv, about_data)
-
+    
     if not KUniqueApplication.start():
         print i18n("User manager module is already started!")
         return
-
+    
     kapp = KUniqueApplication(True, True, True)
-    myapp = MainApplication()
+    myapp = App()
     kapp.setMainWidget(myapp)
     sys.exit(myapp.exec_loop())
-    
+
 # Factory function for KControl
 def create_user_manager(parent,name):
     global kapp
     
     kapp = KApplication.kApplication()
-    return MainApplication(parent, name)
+    return Module(parent, name)
 
-if standalone:
+
+if __name__ == "__main__":
     main()
