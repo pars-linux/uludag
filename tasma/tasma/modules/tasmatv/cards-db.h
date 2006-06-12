027d9347d845b741dbe397afdb6ab59fa71c8e75
Faik Uygur <faikuygur@gmail.com>
2006-06-12 09:18:28 +0000

    some more work for additional chipset support.

--- tasma/tasma/modules/tasmatv/cards-db.h
+++ tasma/tasma/modules/tasmatv/cards-db.h
@@ -18,8 +18,17 @@
 #include <qvaluevector.h>
 #include <qstringlist.h>
 
+enum ChipSet { BTTV, SAA7134, CX88 };
+
+struct card_info {
+  unsigned int vendor_id;
+  unsigned int card_id;
+  const char *card_name;
+};
+
 struct Card
 {
+    ChipSet chipset;
     unsigned int card_id;
     const char *card_name;
 };
@@ -32,15 +41,16 @@ class CardsDB : public QObject
 public:
     CardsDB();
     ~CardsDB();
+    void addCards(struct card_info *card_infos, ChipSet chipset);
+    
     void getCards(QString vendor, QStringList *cards);
     void getVendors(QStringList *vendors);
 
     int getCard(QString card_name);
-    int getCard(unsigned int card_id, QString &vendor_name, QString &card_name);
+    int getCard(unsigned int card_id, ChipSet chipset, QString &vendor_name, QString &card_name);
 
 private:
     CardVendors m_vendors;
-    void initVendors();
 };
 
 #endif
