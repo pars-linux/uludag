3dcb62448f85a4361c8ec97d674484a9a1179c4f
gurer <gurer@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2006-07-02 11:20:06 +0000

    versiyon

--- trunk/tasma/firewall-config/setup.py
+++ trunk/tasma/firewall-config/setup.py
@@ -17,7 +17,7 @@ import kdedistutils
 
 kdedistutils.setup(
     name="firewall-config",
-    version="1.4",
+    version="1.5",
     author="Bahadır Kandemir",
     author_email="bahadir@pardus.org.tr",
     min_kde_version = "3.5.0",
