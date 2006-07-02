8dcbb3d0d64c74be2c7e9a9d04ce3d36a4ad7beb
gokmen.goksel <gokmen.goksel@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2006-07-02 18:52:31 +0000

    fix for enable/disable

--- trunk/web/yeni/Modules/RSS.php
+++ trunk/web/yeni/Modules/RSS.php
@@ -7,18 +7,21 @@
 
     public $RssContent;
 
-        function RssRead($file){
+        function RssRead($file=""){
             # FIXME incase of File Not Found
+            if (!$file) return 0;
             $trans_xml = @fopen($file,"rb");
             $xml_content = stream_get_contents($trans_xml);
             $this->RssContent=simplexml_load_string($xml_content);
         }
 
         function ShowList($Count,$CharLen){
-            $i=0;
-            foreach($this->RssContent->channel->item as $blog) {
-                echo "<li><a href='".$blog->link."'>".substr($blog->title,0,$CharLen)."..</a><br>";
-                if ($i>=$Count) break; else $i++;
+            if ($this->RssContent) {
+                $i=0;
+                foreach($this->RssContent->channel->item as $blog) {
+                    echo "<li><a href='".$blog->link."'>".substr($blog->title,0,$CharLen)."..</a><br>";
+                    if ($i>=$Count) break; else $i++;
+                }
             }
         }
     }
