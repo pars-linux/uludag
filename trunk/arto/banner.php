1da444e0ea7879c5671bdcb282d1f9d7421a06b8
ahmet <ahmet@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2006-07-04 08:29:47 +0000

    typo

--- trunk/arto/banner.php
+++ trunk/arto/banner.php
@@ -1,3 +1,3 @@
 <?php
-echo "<a href=\"http://www.uludag.org.tr/\" target=\"_blank\"><img src=\"images/banners/banner03.png\" border=\"0\"></a>";
+echo "<a href=\"http://www.pardus.org.tr/\" target=\"_blank\"><img src=\"images/banners/banner03.png\" border=\"0\"></a>";
 ?>
