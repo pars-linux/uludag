9a147c6ce0b38d0c933d9cca749d0ddff7d31210
gokmen.goksel <gokmen.goksel@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2006-07-05 11:33:59 +0000

    svn hooku ile yapıyoruz

--- trunk/web/yeni/Modules/SVN.php
+++ trunk/web/yeni/Modules/SVN.php
@@ -4,8 +4,8 @@
     $SvnServer = 'http://svn.pardus.org.tr/';
     $Limit = 5;
     function getLogs($Repo) {
-        global $SvnServer,$Limit;
-        exec ('svn log '.$SvnServer.$Repo.' --xml --limit '.$Limit.' > /tmp/svnlog-'.$Repo);
+#        global $SvnServer,$Limit;
+#        exec ('svn log '.$SvnServer.$Repo.' --xml --limit '.$Limit.' > /tmp/svnlog-'.$Repo);
         $logFile = '/tmp/svnlog-'.$Repo;
         $filePointer = @fopen($logFile,'r');
         echo fread($filePointer,filesize($logFile));
