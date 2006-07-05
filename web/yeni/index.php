e9c92f5da302e3a3f65597b796313cc8df910f35
Gokmen Goksel <gokmen@goksel.me>
2006-07-05 09:22:58 +0000

    new design..

--- web/yeni/index.php
+++ web/yeni/index.php
@@ -1,19 +1,25 @@
 <html>
 <head>
-<?php
 
-        @require_once('utils.php');
-        if (!($Pardus = new Pardus($DbHost,$DbUser,$DbPass,$DbData)))
-            die();
+<?php
+    
+    $ActivePage = "R";
+    require_once('utils.php');
+    require_once('Modules/Main.php');
+    require_once('Modules/RSS.php');
+
+    $Blogs = new RssRead($BlogRssLink);
 
 ?>
-    <title>PardusOrgTr</title>
+
+    <title><?=$PageTitle?></title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <link href="stil.css" rel="stylesheet" type="text/css">
     <script src="scripts/prototype.js"></script>
     <script src="scripts/scriptaculous.js"></script>
     <script src="scripts/effects.js"></script>
     <script>
+
         function get_news(nid) {
             var url ='utils.php';
             var linke = 'NewsID='+nid;
@@ -32,59 +38,112 @@
 
 <body>
 <center>
-
-<!-- Content -->
-
 <table>
+
+<?php if($Page==="Main") { ?>
+
     <tr>
         <td id="header" colspan=2>
             <div id="menu"><span id="searchbar"><input type="text" value="ara"></span> </div>
         </td>
     </tr>
     <tr>
-            <td id="pardus-11">
-                <div id="boxdetay1">
+        <td id="pardus-11">
+            <div id="boxdetay1">
                 Pardus 1.1 Muhteşem :)
-                </div>
-            </td>
-            <td id="kutular">
-                    <div id="pardus-nedir">
-                    Pardus, TUBITAK UEKAE tarafından yürütülen, kodları ve geliştirme süreçleri açık, herkesin katılabileceği, özgür yazılım modeliyle geliştirilen bir işletim sistemidir. Kolay kullanılır ve bir bilişim okuryazarının tüm ihtiyaçlarını doğrudan karşılar.
-                    <br>
-                    Pardus ile ilgili ayrıntılı bilgi için <a href="/Bireysel/PardusTanitim">tıklayınız</a>.
-                    </div>
-                    <div id="pardus-indir"></div>
-            </td>
+            </div>
+        </td>
+        <td id="kutular">
+            <div id="pardus-nedir">
+                Pardus, TUBITAK UEKAE tarafından yürütülen, kodları ve geliştirme süreçleri açık, herkesin katılabileceği, özgür yazılım modeliyle geliştirilen bir işletim sistemidir. Kolay kullanılır ve bir bilişim okuryazarının tüm ihtiyaçlarını doğrudan karşılar.<br>Pardus ile ilgili ayrıntılı bilgi için <a href="?Bireysel/PardusTanitim">tıklayınız</a>.
+            </div>
+            <div id="pardus-indir"></div>
+        </td>
     </tr>
 
     <tr>
-            <td id="navi" colspan=2>
+        <td id="navi" colspan=2>
             <div id="ana-butonlar">
                 <a href="Bireysel"><img src="images/newdesign/button-bireysel-kullanici.png" border="0" alt="" /></a>
                 <a href="Kurumsal"><img src="images/newdesign/button-kurumsal-kullanici.png" border="0" alt="" /></a>
                 <a href="Gelistirici"><img src="images/newdesign/button-gelistirici.png" border="0" alt="" /></a>
             </div>
-            </td>
+        </td>
     </tr>
 
     <tr>
-            <td id="icerik">
+        <td id="icerik">
             <div id="ayrintilar">
-            <div id="haber">
-            <?php
+                <div id="haber">
+                <?php
                     $Pardus->GetNews();
-            ?>
+                ?>
+                </div>
             </div>
-            </div>
-            </td>
-            <td id="kutular">
+        </td>
+        
+        <td id="kutular">
             <div id="haberler">
                 <?php
                     $Pardus->GetNewsList();
                 ?>
             </div>
-            </td>
+        </td>
     </tr>
+
+<?php } else { ?>
+
+    <tr>
+        <td id="header">
+            <div id="menu">
+                <b>
+                <a href="?">Ana Sayfa</a> »
+            <?php
+                if($Page<>"Main"){
+                    if ($Parent)
+                        echo "<a href='?".$ParentContent["NiceTitle"]."'>".$ParentContent["Title"]."</a> » ";
+                     echo $PageContent["Title"];
+               }
+            ?>
+                </b>
+            </div>
+        </td>
+        <td>
+            <!-- <img src="../images/newdesign/head-pardus.png" style="float:right;padding-right:5px;" /> -->
+        </td>
+    </tr>
+    
+    <tr>
+        <td id="mainBody" <?php if ($PageContent["PType"]==="D") { ?> style="width:770px" colspan=2 <?php } ?> >
+            <div id="hede">
+                <?=$PageContent["Content"]?>
+            </div>
+        </td>
+        <?php if ($PageContent["PType"]<>"D") { ?>
+        <td id="kucular">
+            <div id="hodo">
+                <img src="images/newdesign/head-nasil-katkida-bulunabilirim.png">
+                <div id="RssList">
+                    <li>Wiki'de deneyimlerinizi paylaşabileceğiniz belgelerle yeni kullanıcılara yardım edebilir;
+                    <li>Yaşadığınız sorunları ya da önerilerinizi Hata Takip Sistemine girerek daha iyi bir sistem yaratmamıza yardımcı olabilir;
+                    <li>İmece çalışmalarına katılarak, katkılarınızı doğrudan Pardus'a ekleyebilir;
+                    <li>Bunlar dışında fikirleriniz varsa Pardus Gönüllülerine başvurabilirsiniz...
+                </div>
+                <img src="images/newdesign/head-bugzilla-degisiklik.png">
+                <img src="images/newdesign/head-gelistirici-gunlukler.png">
+                <div id="RssList">
+                <?php
+                    $Blogs->ShowList(4,30);
+                ?>
+                </div>
+                <img src="images/newdesign/dock-gelistirici.png">
+            </div>
+        </td>
+        <?php } ?>
+    </tr>
+
+<?php } ?>
+    
     <tr>
         <td colspan=2>
             <div id="footer-forpw">Pardus TUBITAK/UEKAE 'nin Tescilli Markasıdır.</div>
