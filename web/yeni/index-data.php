6c7a64536aebf220ae81e7456e8dcdf57ebe33ba
Gokmen Goksel <gokmen@goksel.me>
2006-07-05 13:35:49 +0000

    sepearte pages..

---
+++ web/yeni/index-data.php
@@ -0,0 +1,49 @@
+    <tr>
+        <td id="header" colspan=2>
+            <div id="menu"><span id="searchbar"><input type="text" value="ara"></span> </div>
+        </td>
+    </tr>
+    <tr>
+        <td id="pardus-11">
+            <div id="boxdetay1">
+                Pardus 1.1 Muhteşem :)
+            </div>
+        </td>
+        <td id="kutular">
+            <div id="pardus-nedir">
+                Pardus, TUBITAK UEKAE tarafından yürütülen, kodları ve geliştirme süreçleri açık, herkesin katılabileceği, özgür yazılım modeliyle geliştirilen bir işletim sistemidir. Kolay kullanılır ve bir bilişim okuryazarının tüm ihtiyaçlarını doğrudan karşılar.<br>Pardus ile ilgili ayrıntılı bilgi için <a href="?PardusTanitim">tıklayınız</a>.
+            </div>
+            <div id="pardus-indir"></div>
+        </td>
+    </tr>
+
+    <tr>
+        <td id="navi" colspan=2>
+            <div id="ana-butonlar">
+                <a href="Bireysel"><img src="images/newdesign/button-bireysel-kullanici.png" border="0" alt="" /></a>
+                <a href="Kurumsal"><img src="images/newdesign/button-kurumsal-kullanici.png" border="0" alt="" /></a>
+                <a href="Gelistirici"><img src="images/newdesign/button-gelistirici.png" border="0" alt="" /></a>
+            </div>
+        </td>
+    </tr>
+
+    <tr>
+        <td id="icerik">
+            <div id="ayrintilar">
+                <div id="haber">
+                <?php
+                    $Pardus->GetNews();
+                ?>
+                </div>
+            </div>
+        </td>
+        
+        <td id="kutular">
+            <div id="haberler">
+                <?php
+                    $Pardus->GetNewsList();
+                ?>
+            </div>
+        </td>
+    </tr>
+
