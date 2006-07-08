83436c47a35891b515275d25e408eb0b88aa9cf5
Bahadır Kandemir <kandemir@gmail.com>
2006-07-08 19:50:34 +0000

    * map, hide, unhack support * toString function for generating grub.conf

--- tasma/boot-manager/comar/grub.py
+++ tasma/boot-manager/comar/grub.py
@@ -21,19 +21,18 @@ def parseGrubConfig(filename):
             data = i[i.find(" ") + 1:-1].lstrip(" =")
             if label == "title":
                 entry["title"] = data
-            elif label == "root":
-                entry["root"] = data[data.find("(") + 1:-1]
-            elif label == "rootnoverify":
-                entry["root"] = data[data.find("(") + 1:-1]
-                entry["foreign"] = entry.get("foreign", []) + ["noverify"]
-            elif label == "chainloader":
-                entry["foreign"] = entry.get("foreign", []) + ["chainloader"]
-            elif label == "makeactive":
-                entry["foreign"] = entry.get("foreign", []) + ["makeactive"]
+            elif label in ["root", "rootnoverify"]:
+                entry[label] = data[data.find("(") + 1:-1]
+            elif label in ["chainloader", "makeactive"]:
+                entry[label] = True
+            elif label in ["map", "hide", "unhide"]:
+                entry[label] = entry.get(label, []) + [(data)]
             elif label == "kernel":
                 kernelopts = data[data.find("(") + 1:].split(" ")
                 entry["kernel"] = kernelopts[0][kernelopts[0].find(")") + 1:] 
                 entry["opts"] = kernelopts[1:]
+            elif label == "initrd":
+                entry["initrd"] = data[data.find(")") + 1:]
             elif not entry and i.strip():
                 options[i[0:i.find(" ")]] = i[i.find(" ") + 1:-1].lstrip(" =")
         elif i == "\n" and entry:
@@ -43,3 +42,33 @@ def parseGrubConfig(filename):
     entries.append(entry)
 
     return options, entries
+
+
+def grubToString(opts, entries):
+    s = ""
+
+    for label, data in opts.iteritems():
+        s += "%s %s\n" % (label, data)
+
+    s += "\n"
+
+    for entry in entries:
+        if "rootnoverify" in entry:
+            s += "title %s\n" % entry["title"]
+            for op in ["map", "hide", "unhide"]:
+                if op in entry:
+                    for i in entry[op]:
+                        s += "%s %s\n" % (op, i)
+            s += "rootnoverify (%s)\n" % entry["rootnoverify"]
+            if "chainloader" in entry:
+                s += "chainloader +1\n"
+            if "makeactive" in entry:
+                s += "makeactive\n"
+        else:
+            s += "title %s\n" % entry["title"]
+            s += "root (%s)\n" % entry["root"]
+            s += "kernel (%s)%s %s\n" % (entry["root"], entry["kernel"], " ".join(entry["opts"]))
+            if "initrd" in entry:
+                s += "initrd (%s)%s\n" % (entry["root"], entry["initrd"])
+        s += "\n"
+    return s
