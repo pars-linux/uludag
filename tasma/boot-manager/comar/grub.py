020a27fabbcbfb11e449f21ecb44b661a3a61a9d
Bahadır Kandemir <kandemir@gmail.com>
2006-07-10 19:12:48 +0000

    * Support for all grub-0.* commands

--- tasma/boot-manager/comar/grub.py
+++ tasma/boot-manager/comar/grub.py
@@ -2,9 +2,10 @@ def dev2grub(dev):
     type = dev[0:2]
     disc = ord(dev[2:3]) - 97
     part = int(dev[3:]) - 1
-    return "%s%s,%s" % (type, disc, part)
+    return "(%s%s,%s)" % (type, disc, part)
 
 def grub2dev(gr):
+    gr = gr[1:-1]
     type = gr[0:2]
     disc, part = gr[2:].split(",")
     disc = chr(int(disc) + 97)
@@ -12,33 +13,40 @@ def grub2dev(gr):
     return "%s%s%s" % (type, disc, part)
 
 def parseGrubConfig(filename):
+    lines = [i.strip() for i in file(filename) if not i.startswith("#") and i.strip()]
+
     options = {}
     entries = []
     entry = {}
-    for i in file(filename):
-        if i != "\n":
+
+    for i in lines:
+        if i.find(" ") == -1:
+            label = i
+            data = ""
+        else:
             label = i[0:i.find(" ")].rstrip(" =")
-            data = i[i.find(" ") + 1:-1].lstrip(" =")
-            if label == "title":
-                entry["title"] = data
-            elif label in ["root", "rootnoverify"]:
-                entry[label] = data[data.find("(") + 1:-1]
-            elif label in ["chainloader", "makeactive"]:
-                entry[label] = True
-            elif label in ["map", "hide", "unhide"]:
-                entry[label] = entry.get(label, []) + [(data)]
-            elif label == "kernel":
-                kernelopts = data[data.find("(") + 1:].split(" ")
-                entry["kernel"] = kernelopts[0][kernelopts[0].find(")") + 1:] 
-                entry["opts"] = kernelopts[1:]
-            elif label == "initrd":
-                entry["initrd"] = data[data.find(")") + 1:]
-            elif not entry and i.strip():
-                options[i[0:i.find(" ")]] = i[i.find(" ") + 1:-1].lstrip(" =")
-        elif i == "\n" and entry:
-            entries.append(entry)
-            entry = {}
-    
+            data = i[i.find(" ") + 1:].lstrip(" =")
+
+        if label in ["default", "fallback", "hiddenmenu", "timeout",
+                     "bootp", "color", "dhcp", "ifconfig", "pager", "passwd",
+                     "rarp", "terminal", "terminfo", "tftpserver", "hide",
+                     "partnew", "parttype", "serial", "setkey", "unhide"]:
+            options[label] = options.get(label, []) + [data]
+        elif label == "title":
+            if entry:
+                entries.append(entry)
+                entry = {}
+            entry["title"] = data
+        # FIXME: Remove shell commands
+        elif label in ["blocklist", "boot", "cat", "chainloader", "cmp",
+                       "configfile", "debug", "displayapm", "displaymem",
+                       "embed", "find", "fstest", "geometry", "halt",
+                       "help", "impsprobe", "initrd", "install", "ioprobe",
+                       "kernel", "lock", "makeactive", "map", "md5crypt",
+                       "module", "modulenounzip", "pause", "quit", "reboot",
+                       "read", "root", "rootnoverify", "savedefault", "setup",
+                       "testload", "testvbe", "uppermem", "vbeprobe"]:
+            entry[label] = entry.get(label, []) + [data]
     entries.append(entry)
 
     return options, entries
@@ -48,27 +56,19 @@ def grubToString(opts, entries):
     s = ""
 
     for label, data in opts.iteritems():
-        s += "%s %s\n" % (label, data)
+        for i in data:
+            s += "%s %s\n" % (label, i)
 
     s += "\n"
 
     for entry in entries:
-        if "rootnoverify" in entry:
-            s += "title %s\n" % entry["title"]
-            for op in ["map", "hide", "unhide"]:
-                if op in entry:
-                    for i in entry[op]:
-                        s += "%s %s\n" % (op, i)
-            s += "rootnoverify (%s)\n" % entry["rootnoverify"]
-            if "chainloader" in entry:
-                s += "chainloader +1\n"
-            if "makeactive" in entry:
-                s += "makeactive\n"
-        else:
-            s += "title %s\n" % entry["title"]
-            s += "root (%s)\n" % entry["root"]
-            s += "kernel (%s)%s %s\n" % (entry["root"], entry["kernel"], " ".join(entry["opts"]))
-            if "initrd" in entry:
-                s += "initrd (%s)%s\n" % (entry["root"], entry["initrd"])
+        s += "title %s\n" % entry["title"]
+        if "map" in entry:
+            for i in entry["map"]:
+                s += "map %s\n" % i
+        for label, data in entry.iteritems():
+            if label not in ["title", "map"]:
+                for i in data:
+                    s += "%s %s\n" % (label,i)
         s += "\n"
     return s
