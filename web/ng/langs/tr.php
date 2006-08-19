c48ead9d30a1267bae19bd842e92f36fe2ef8f53
Gokmen Goksel <gokmen@goksel.me>
2006-08-19 14:41:47 +0000

    türkçe destekli konserve kurufasulye..

---
+++ web/ng/langs/tr.php
@@ -0,0 +1,19 @@
+<?php
+
+    // TUBITAK/UEAKE - 2006-07 - Pardus
+    // Gökmen GÖKSEL <gokmen@pardus.org.tr>
+    // TR Language File for Pardus Web
+
+    define ("ERROR_SEARCH_WORD_AT_LEAST_FOUR_CHARACTER","Hata : En az dört karakter girmelisiniz !.");
+    define ("PAGE_TITLE","pardus.org.tr");
+    define ("HIDEHIGHLIGHT","Renklendirmeleri Gizle");
+    define ("SHOWHIGHLIGHT","Renklendirmeleri Göster");
+    define ("MAIN_PAGE","Ana Sayfa");
+    define ("SEARCHED","için arama yapıldı.");
+    define ("RESULTS","için sonuçlar");
+    define ("TOTAL","Toplam ");
+    define ("RECORD_FOUND_ALSO_YOU_CAN_LOOK_AT_GOOGLE_RESULTS"," kayıt bulundu.");
+    define ("NO_RECORD_FOUND_BUT_YOU_CAN_LOOK_AT_GOOGLE_RESULTS","Hiç kayıt bulunamadı.");
+    define ("PARDUS_REGISTER","Pardus TUBITAK/UEKAE 'nin tescilli markasıdır.");
+
+?>
