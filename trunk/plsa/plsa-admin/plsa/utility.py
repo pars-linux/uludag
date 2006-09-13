b2288307a0d2cc1059ecdb18bb2dd98d37fcc0dd
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2006-09-13 13:56:47 +0000

    * setup script * gettext support

---
+++ trunk/plsa/plsa-admin/plsa/utility.py
@@ -0,0 +1,37 @@
+def localized_node(parent, name, lang="en"):
+    """Returns tag data with selected xml:lang attribute"""
+    node = [x for x in parent.tags() if x.name() == name and "xml:lang" in x.attributes() and x.getAttribute("xml:lang") == lang][0]
+    if node.firstChild():
+        return unicode(node.firstChild().data())
+    else:
+        return ""
+
+def justify(text, delim=" ", width=72):
+    words = text.split(delim)
+    space = width - len(text)
+    while space > 0:
+        p = space % (len(words) - 1) + 1
+        words[p * -1 + 1] += " "
+        space -= 1
+    return delim.join(words)
+
+def wwrap(text, width=72, lpad=0, rpad=0, just=True):
+    width -= rpad
+    words = text.split()
+    sel = [" " * lpad]
+    for w in words:
+        if len(sel[-1]) + len(w) <= width:
+            sel[-1] += "%s " % w
+        else:
+            if just:
+                sel[-1] = " " * lpad + justify(sel[-1][lpad:-1], " ", width - lpad)
+            sel.append("%s%s " % (" " * lpad, w))
+    return "\n".join(sel)
+
+def calign(lst, width=72):
+    t = []
+    m = max(map(lambda x: len(x[0]), lst))
+    for h, c in lst:
+        h = h.rjust(m + 2)
+        t.append("%s: %s" % (h, wwrap(c, width, len(h) + 2).strip()))
+    return t
