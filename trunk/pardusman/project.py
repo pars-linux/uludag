b956f0b1d2a52df4c794eda201b15edd55afc078
gurer <gurer@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2006-09-22 15:03:00 +0000

    work work

--- trunk/pardusman/project.py
+++ trunk/pardusman/project.py
@@ -15,26 +15,65 @@ import piksemel
 def _(x):
     return x
 
-INSTALL, LIVE = range(2)
-
 
 class Project:
     def __init__(self):
-        self.filename = None
+        self.reset()
+    
+    def reset(self):
         self.work_dir = None
         self.release_files = None
         self.repo_uri = None
-        self.media_type = INSTALL
+        self.media_type = "install"
         self.media_size = 700 * 1024 * 1024
         self.selected_components = []
         self.selected_packages = []
         self.all_packages = []
     
-    def open(self, name):
-        pass
+    def open(self, filename):
+        try:
+            doc = piksemel.parse(filename)
+        except piksemel.ParseError:
+            return _("Not a Pardusman project file, invalid xml!")
+        if doc.name() != "PardusmanProject":
+            return _("Not a Pardusman project file")
+        
+        self.reset()
+        
+        self.media_type = doc.getAttribute("type")
+        self.media_size = doc.getAttribute("size")
+        self.work_dir = doc.getTagData("WorkDir")
+        self.release_files = doc.getTagData("ReleaseFiles")
+        
+        paksel = doc.getTag("PackageSelection")
+        for tag in paksel.tags("SelectedComponent"):
+            self.selected_components.append(tag.firstChild().data())
+        for tag in paksel.tags("SelectedPackage"):
+            self.selected_packages.append(tag.firstChild().data())
+        for tag in paksel.tags("Package"):
+            self.all_packages.append(tag.firstChild().data())
     
-    def save(self, name=None):
-        pass
+    def save(self, filename):
+        doc = piksemel.newDocument("PardusmanProject")
+        doc.setAttribute("type", self.media_type)
+        doc.setAttribute("size", str(self.media_size))
+        if self.work_dir:
+            doc.insertTag("WorkDir").insertData(self.work_dir)
+        if self.release_files:
+            doc.insertTag("ReleaseFiles").insertData(self.release_files)
+        if self.repo_uri:
+            paks = doc.insertTag("PackageSelection")
+            paks.setAttribute("repo_uri", self.repo_uri)
+            for item in self.selected_components:
+                paks.insertTag("SelectedComponent").insertData(item)
+            for item in self.selected_packages:
+                paks.insertTag("SelectedPackage").insertData(item)
+            for item in self.all_packages:
+                paks.insertTag("Package").insertData(item)
+        data = doc.toPrettyString()
+        f = file(filename, "w")
+        f.write(data)
+        f.close()
     
     def make(self, console):
         pass
@@ -42,86 +81,6 @@ class Project:
 
 
 """
-class Project(QMainWindow):
-    def __init__(self, parent):
-        QMainWindow.__init__(self, parent)
-        self.setMinimumSize(520, 360)
-        
-        self.pak_selection = None
-        self.pak_size = 0
-        self.pak_inst_size = 0
-        self.project_name = None
-        
-        vb = QVBox(self)
-        vb.setSpacing(6)
-        vb.setMargin(6)
-        self.setCentralWidget(vb)
-        
-        tab = QTabWidget(vb)
-        self.tab = tab
-        tab.setMargin(6)
-        
-        w = QWidget(tab)
-        tab.addTab(w, _("Details"))
-        
-        grid = QGridLayout(w, 8, 2, 6, 6)
-        
-        lab = QLabel(_("Project name:"), w)
-        grid.addWidget(lab, 0, 0, Qt.AlignRight)
-        self.name = QLineEdit(w)
-        grid.addWidget(self.name, 0, 1)
-        
-        line = HLine(_("Media Content:"), w)
-        grid.addMultiCellWidget(line, 1, 1, 0, 1)
-        self.contentdir = makePathEntry(
-            _("Release files:"),
-            _("Select release files..."),
-            grid, 2, w
-        )
-        self.cdroot = makePathEntry(
-            _("Boot image:"),
-            _("Select boot image..."),
-            grid, 3, w, is_dir=False
-        )
-        
-        line = HLine(_("Package selection:"), w)
-        grid.addMultiCellWidget(line, 4, 4, 0, 1)
-        self.sourcedir = makePathEntry(
-            _("SVN checkout (source packages):"),
-            _("Select SVN checkout folder..."),
-            grid, 5, w
-        )
-        self.packagedir = makePathEntry(
-            _("Binary packages:"),
-            _("Select binary package folder..."),
-            grid, 6, w
-        )
-        
-        but = QPushButton(_("Select packages"), w)
-        self.packagebut = but
-        self.connect(but, SIGNAL("clicked()"), self.selectPackages)
-        grid.addMultiCellWidget(but, 7, 7, 0, 1)
-        
-        self.paklabel= QLabel(w)
-        grid.addMultiCellWidget(self.paklabel, 8, 8, 0, 1)
-        
-        self.console = Console(self)
-        tab.addTab(self.console, _("Log"))
-        
-        hb = QHBox(vb)
-        hb.setSpacing(12)
-        
-        but = QPushButton(_("Save"), hb)
-        self.connect(but, SIGNAL("clicked()"), self.save)
-        but = QPushButton(_("Save as..."), hb)
-        self.connect(but, SIGNAL("clicked()"), self.save_as)
-        but = QPushButton(_("Prepare Media"), hb)
-        self.connect(but, SIGNAL("clicked()"), self.prepareMedia)
-        
-        self.updateStatus()
-        
-        self.show()
-    
     def prepareMedia(self):
         con = self.console
         self.tab.setCurrentPage(1)
@@ -135,49 +94,6 @@ class Project(QMainWindow):
             con.state("--- media prepared succesfully ---\n\n")
         else:
             con.state("--- operation failed ---")
-    
-    def save_project(self, filename):
-        doc = piksemel.newDocument("pardusman-project")
-        doc.setAttribute("type", "media")
-        doc.insertTag("name").insertData(unicode(self.name.text()))
-        doc.insertTag("release_files").insertData(unicode(self.contentdir.text()))
-        doc.insertTag("boot_image").insertData(unicode(self.cdroot.text()))
-        paks = doc.insertTag("packages")
-        paks.setAttribute("path", unicode(self.packagedir.text()))
-        paks.setAttribute("source", unicode(self.sourcedir.text()))
-        if self.pak_selection:
-            for item in self.pak_selection[0]:
-                paks.insertTag("component").insertData(unicode(item))
-            for item in self.pak_selection[1]:
-                paks.insertTag("selected-package").insertData(unicode(item))
-            for item in self.pak_selection[2]:
-                paks.insertTag("package").insertData(unicode(item))
-            paks.insertTag("zip-size").insertData(str(self.pak_size))
-            paks.insertTag("installed-size").insertData(str(self.pak_inst_size))
-        data = doc.toPrettyString()
-        f = file(filename, "w")
-        f.write(data)
-        f.close()
-    
-    def load_project(self, filename, doc):
-        self.project_name = filename
-        self.name.setText(doc.getTagData("name"))
-        self.contentdir.setText(doc.getTagData("release_files"))
-        self.cdroot.setText(doc.getTagData("boot_image"))
-        paks = doc.getTag("packages")
-        self.packagedir.setText(paks.getAttribute("path"))
-        self.sourcedir.setText(paks.getAttribute("source"))
-        self.pak_selection = ([], [], [])
-        L = self.pak_selection[0]
-        for item in paks.tags("component"):
-            L.append(item.firstChild().data())
-        L = self.pak_selection[1]
-        for item in paks.tags("selected-package"):
-            L.append(item.firstChild().data())
-        L = self.pak_selection[2]
-        for item in paks.tags("package"):
-            L.append(item.firstChild().data())
-        self.updateStatus()
     
     def save(self):
         if self.project_name:
