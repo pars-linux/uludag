d0002c907629e7465b3d01ae179b2e1ee8bad419
gokmen.goksel <gokmen.goksel@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2006-09-26 20:06:35 +0000

    aşt sayfalarda adam olsun. -inanmiyorum ..

---
+++ trunk/web/ng/index__.php
@@ -0,0 +1,16 @@
+    <div id="head_grey">
+        <div id="blue2"></div>
+        <div id="quick_download">
+            <img src="images/box-hizli-indir.png" alt="Pardus 1.1Beta"/>
+            <form action="search/">
+                <input type="text" name="q" id="search" value="<?=SEARCH?>" onclick="Javascript:if (this.value=='<?=SEARCH?>') this.value='';" onblur="Javascript:if (this.value=='') this.value='<?=SEARCH?>';"/>
+            </form>
+        </div>
+    </div>
+
+    <div id="navi">
+        <a href="?"><?=MAIN_PAGE?></a> » 
+        <?=$Navi?>
+    </div>
+    <div id="fullpage">
+
