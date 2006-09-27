f23aee0052165b0b8e058321a8a865c1868f8779
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2006-09-27 10:42:15 +0000

    paketler.pardus.org.tr yolda...
    web'de hızlı sorgulama için tüm paket bilgilerini mysql'de tutuyoruz.

---
+++ trunk/repo-stats/repo2mysql/config.py
@@ -0,0 +1,11 @@
+db_host = "localhost"
+db_name = "repostats"
+db_user = "user"
+db_pass = "pw"
+
+tables = ["sources", "components", "packagers", "licenses",
+          "sources_licenses", "sources_components", "patches",
+          "sources_patches", "sources_deps", "sources_summaries",
+          "sources_descriptions", "packages", "packages_deps",
+          "types", "components_localnames", "components_summaries",
+          "components_descriptions", "history"]
