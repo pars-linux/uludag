73b7e5cbdd422af92d822a650c76ae0e0a092fc5
gokmen.goksel <gokmen.goksel@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2006-10-05 10:16:45 +0000

    bidi

--- trunk/web/ng/index__.php
+++ trunk/web/ng/index__.php
@@ -2,7 +2,7 @@
         <div id="blue2"></div>
         <div id="quick_download">
             <img src="/images/box-hizli-indir.png" alt="Pardus 1.1Beta"/>
-            <form action="/search/">
+            <form action="q.php">
                 <input type="text" name="q" id="search" value="<?=SEARCH?>" onclick="Javascript:if (this.value=='<?=SEARCH?>') this.value='';" onblur="Javascript:if (this.value=='') this.value='<?=SEARCH?>';"/>
             </form>
         </div>
