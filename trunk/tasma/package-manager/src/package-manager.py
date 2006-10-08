7505842add87abd9863dd6d4fa556843734814b3
faik <faik@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2006-10-08 21:04:30 +0000

    * dont be shy

--- trunk/tasma/package-manager/src/package-manager.py
+++ trunk/tasma/package-manager/src/package-manager.py
@@ -417,7 +417,7 @@ class MainApplicationWidget(QWidget):
             self.progressDialog.updateProgressText()
         elif operation in ["installed","removed","upgraded"]:
             self.progressDialog.currentAppIndex += 1
-        else:
+        elif operation in ["removing"]:
             self.packagesOrder = data
             self.progressDialog.totalAppCount = len(self.packagesOrder)
 
