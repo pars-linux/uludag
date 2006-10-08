8a06ad23fa75c717b84dde17ded2359b2992a9e7
Faik Uygur <faikuygur@gmail.com>
2006-10-08 09:11:22 +0000

    * move more UpdateDialog related stuff

--- tasma/package-manager/src/UpdateDialog.py
+++ tasma/package-manager/src/UpdateDialog.py
@@ -2,9 +2,10 @@ from qt import *
 from khtml import *
 from kdecore import i18n
 import pisi.api
+import CustomEventListener
 
 class UpdateDialog(QDialog):
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, upgradables=None):
         QDialog.__init__(self,parent)
         self.setCaption(i18n("Currently Available Updates"))
         self.parent = parent
@@ -21,7 +22,17 @@ class UpdateDialog(QDialog):
         layout1.addWidget(layout3,2,1)
 
         self.htmlPart = KHTMLPart(layout2)
+        self.parent.createHTML(upgradables, self.htmlPart)
+
+        self.connect(self.htmlPart,SIGNAL("completed()"),self.registerEventListener)
+        self.connect(self.updateButton,SIGNAL("clicked()"),self.parent.updatePackages)
+
         self.resize(self.width()-50,self.height()-50)
+
+    def registerEventListener(self):
+        self.eventListener = CustomEventListener.CustomEventListener(self, self.updatesToProcess)
+        node = self.htmlPart.document().getElementsByTagName(DOM.DOMString("body")).item(0)
+        node.addEventListener(DOM.DOMString("click"),self.eventListener,True)
 
     def updateButtons(self):
         if self.updatesToProcess:
@@ -40,4 +51,3 @@ class UpdateDialog(QDialog):
             print e
 
         self.updateButtons()
-
