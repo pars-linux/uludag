b50d8d6f11dbee38389c6ac792e1c209ca78f8f4
Faik Uygur <faikuygur@gmail.com>
2006-10-14 12:58:14 +0000

    * add i18n for "installing" text * remove possibleError thingy. If any error occurs, for safety we will refresh the view. (todo)

--- tasma/package-manager/src/CommonText.py
+++ tasma/package-manager/src/CommonText.py
@@ -1,3 +1,4 @@
 from kdecore import i18n
 
-commonText = [i18n("downloading"),i18n("extracting"),i18n("configuring"),i18n("removing")]
+commonText = [i18n("downloading"),i18n("extracting"),i18n("configuring"),i18n("removing"),i18n("installing")]
+
