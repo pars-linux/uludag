66c64f7b6ebaaeb6bc60cb59b4c68cc466fdfc2a
faik <faik@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2006-10-19 10:16:12 +0000

    * trans update..
    * let this be 1.1.0_b6... latest package was beta5

--- trunk/tasma/package-manager/setup.py
+++ trunk/tasma/package-manager/setup.py
@@ -14,7 +14,7 @@ import kdedistutils
 
 kdedistutils.setup(
     name="package-manager",
-    version="1.1.0_b4",
+    version="1.1.0_b6",
     author="İsmail Dönmez",
     author_email="ismail@pardus.org.tr",
     url="http://www.pardus.org.tr/projeler/pisi/index.html",
