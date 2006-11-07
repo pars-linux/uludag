4dc4b31c2af689ab460e3e3acf16c2aa457a20e6
cartman <cartman@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2006-11-07 20:10:19 +0000

    cosmetics , remove i18n calls this program doesn't need to be localized

--- trunk/kio-to-local/main.cpp
+++ trunk/kio-to-local/main.cpp
@@ -3,20 +3,17 @@
 #include <kapplication.h>
 #include <kaboutdata.h>
 #include <kcmdlineargs.h>
-#include <klocale.h>
 #include <kurl.h>
 #include <kio/netaccess.h>
 #include <kmessagebox.h>
 #include <kprocess.h>
 
-static const char description[] = I18N_NOOP("External KIO support for non-KDE programs");
-
+static const char description[] = "External KIO support for non-KDE programs";
 static const char version[] = "0.5";
-
 static KCmdLineOptions options[] =
 {
-    { "+program", I18N_NOOP( "Program to run" ), 0 },
-    { "+url", I18N_NOOP( "URL to process" ), 0 },
+    { "+program", "Program to run", 0 },
+    { "+url", "URL to process", 0 },
     KCmdLineLastOption
 };
 
@@ -30,8 +27,10 @@ void runProgramWithURL(const QString& program, const QString& url)
 
 int main(int argc, char **argv)
 {
-    KAboutData about("kio-to-local","kio-to-local" , version, description, KAboutData::License_GPL, "", 0, 0, "ismail@pardus.org.tr");
-    about.addAuthor("İsmail Dönmez",I18N_NOOP("Author"),"ismail@pardus.org.tr","http://www.pardus.org.tr");
+    KAboutData about("kio-to-local","kio-to-local" , version, description,
+                     KAboutData::License_GPL, "", 0, 0, "ismail@pardus.org.tr");
+    about.addAuthor("İsmail Dönmez","Author","ismail@pardus.org.tr",
+                    "http://www.pardus.org.tr");
     KCmdLineArgs::init(argc, argv, &about);
     KCmdLineArgs::addCmdLineOptions(options);
     KApplication app;
@@ -40,10 +39,10 @@ int main(int argc, char **argv)
     switch(args->count())
       {
       case 0:
-        KCmdLineArgs::usage(i18n("Command and URL expected.\n"));
+        KCmdLineArgs::usage("Command and URL expected.\n");
         break;
       case 1:
-        KCmdLineArgs::usage(i18n("URL expected.\n"));
+        KCmdLineArgs::usage("URL expected.\n");
         break;
       case 2:
         {
@@ -57,14 +56,18 @@ int main(int argc, char **argv)
             }
           else // A remote URL or kioslave
             {
-              const QString original = QString("/tmp/%1").arg(target.fileName());
-              QString destination = original;
+              QString original, destination;
 
-              unsigned int i=1;
-              while(QFileInfo(destination).isSymLink()) // Protect against symlink attacks
+              original = QString("/tmp/%1").arg(target.fileName());
+              destination = original;
+
+              unsigned int i = 1;
+              // Protect against symlink attacks
+              while(QFileInfo(destination).isSymLink())
                 {
 
-                  destination = original.section(".",-2,0) + "_" + QString::number(i) + "." + original.section(".",-1);
+                  destination = original.section(".",-2,0) + "_"
+                    + QString::number(i) + "." + original.section(".",-1);
                   ++i;
                 }
 
@@ -85,7 +88,7 @@ int main(int argc, char **argv)
           break;
         }
       default:
-        KCmdLineArgs::usage(i18n("Only _one_ command and _one_ URL expected.\n"));
+        KCmdLineArgs::usage("Only one command and one URL expected.\n");
         break;
       }
 
