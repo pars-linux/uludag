0e1c04f88223baca8096d80d09a2c89d3ab79304
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2006-11-08 14:01:00 +0000

    New release

--- trunk/tasma/service-manager/setup.py
+++ trunk/tasma/service-manager/setup.py
@@ -10,11 +10,54 @@
 #
 # Please read the COPYING file.
 
+import os
+import sys
+import glob
+import shutil
+
 import kdedistutils
+
+version = "1.1.2"
+
+distfiles = """
+    AUTHORS
+    README
+    COPYING
+    *.py
+    src/*.py
+    src/*.ui
+    src/*.desktop
+    src/*.png
+    po/*.po
+    po/*.pot
+"""
+
+def make_dist():
+    distdir = "service-manager-%s" % version
+    list = []
+    for t in distfiles.split():
+        list.extend(glob.glob(t))
+    if os.path.exists(distdir):
+        shutil.rmtree(distdir)
+    os.mkdir(distdir)
+    for file_ in list:
+        cum = distdir[:]
+        for d in os.path.dirname(file_).split('/'):
+            dn = os.path.join(cum, d)
+            cum = dn[:]
+            if not os.path.exists(dn):
+                os.mkdir(dn)
+        shutil.copy(file_, os.path.join(distdir, file_))
+    os.popen("tar -cjf %s %s" % ("service-manager-" + version + ".tar.bz2", distdir))
+    shutil.rmtree(distdir)
+
+if "dist" in sys.argv:
+    make_dist()
+    sys.exit(0)
 
 kdedistutils.setup(
     name="service-manager",
-    version="1.1.1",
+    version="1.1.2",
     author="Bahadır Kandemir",
     author_email="bahadir@pardus.org.tr",
     min_kde_version = "3.5.0",
