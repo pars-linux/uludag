f2f6d7c9322f57d15d3f4a57ff7e1a5bf8ea6148
cartman <cartman@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2006-11-20 17:30:06 +0000

    Oscar goes to Markus, finally everything works

--- trunk/pardus-support/test.cpp
+++ trunk/pardus-support/test.cpp
@@ -8,8 +8,8 @@ int main()
 {
   QString test1, test2;
 
-  test1 = "türkiye";
-  test2 = "TURKIYE";
+  test1 = QString::fromLocal8Bit("türkiye");
+  test2 = QString::fromLocal8Bit("TURKIYE");
 
   cout << "TÜRKİYE == " << (Pardus::upper(test1)).local8Bit() << endl;
   cout << "turkıye == " << (Pardus::lower(test2)).local8Bit() << endl;
