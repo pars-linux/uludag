f40f611bde4cc9324b039feb3d4382dc2b6d53f9
Gurer Ozen <meduketto@users.noreply.github.com>
2006-11-22 17:53:57 +0000

    layout macerası bitti

--- tasma/network-manager/browser.py
+++ tasma/network-manager/browser.py
@@ -33,21 +33,6 @@ class IconButton(QPushButton):
         self.resize(self.myWidth, self.myHeight)
 
 
-class StateButton(QPushButton):
-    def __init__(self, parent):
-        QPushButton.__init__(self, parent)
-        self.setToggleButton(True)
-        self.setFlat(True)
-        self.myset = QIconSet()
-        self.myset.setPixmap(KGlobal.iconLoader().loadIcon("fileclose.png", KIcon.Small), QIconSet.Small, QIconSet.Normal, QIconSet.Off)
-        self.myset.setPixmap(KGlobal.iconLoader().loadIcon("apply.png", KIcon.Small), QIconSet.Small, QIconSet.Normal, QIconSet.On)
-        self.setIconSet(self.myset)
-        size = self.myset.iconSize(QIconSet.Small)
-        self.myWidth = size.width() + 4
-        self.myHeight = size.height() + 4
-        self.resize(self.myWidth, self.myHeight)
-
-
 class Connection(QWidget):
     def __init__(self, view, conn):
         self.is_odd = 0
@@ -62,16 +47,17 @@ class Connection(QWidget):
         self.view = view
         self.conn = conn
         
-        fm = self.fontMetrics()
-        self.myBase = fm.ascent()
-        self.myHeight = fm.height()
         self.mypix = icons.get_state(comlink.links[conn.script].type, conn.state)
-        #self.check = QCheckBox(self)
-        #self.check.setGeometry(3, 3, self.check.width(), self.check.height())
-        #self.connect(self.check, SIGNAL("toggled(bool)"), self.slotToggle)
-        #self.check.setAutoMask(True)
-        self.buttok = StateButton(self)
-        self.buttok.setGeometry(3, 3, self.buttok.width(), self.buttok.height())
+        self.check = QCheckBox(self)
+        QToolTip.add(self.check, i18n("Turn on/off connection"))
+        self.check.setGeometry(6, 3, 16, 16)
+        self.connect(self.check, SIGNAL("toggled(bool)"), self.slotToggle)
+        self.check.setAutoMask(True)
+        
+        w = self.check.width()
+        self.pix_start = 6 + w + 3
+        w = self.mypix.width()
+        self.text_start = self.pix_start + w + 6
         
         view.connections[conn.hash] = self
         
@@ -132,30 +118,40 @@ class Connection(QWidget):
         self.edit_but.setPaletteBackgroundColor(col)
         self.diksi.setPaletteBackgroundColor(col)
         paint.fillRect(event.rect(), QBrush(col))
-        paint.drawPixmap(36, 3, self.mypix)
+        dip = (self.height() - self.mypix.height()) / 2
+        paint.drawPixmap(self.pix_start, dip, self.mypix)
         paint.save()
         font = paint.font()
         font.setPointSize(font.pointSize() + 2)
-        paint.drawText(64, self.myBase + 5, unicode(self.conn.name))
+        font.setBold(True)
+        fm = QFontMetrics(font)
+        paint.drawText(self.text_start, fm.ascent() + 5, unicode(self.conn.name))
+        fark = fm.height()
         paint.restore()
-        paint.drawText(64, self.myHeight + self.myBase + 7, self.addressText())
+        fm = self.fontMetrics()
+        paint.drawText(self.text_start, 5 + fark + 3 + fm.ascent(), self.addressText())
     
     def resizeEvent(self, event):
         w = event.size().width()
         h = event.size().height()
+        dip = (h - self.check.height()) / 2
+        self.check.move(6, dip)
         dip = (h - self.diksi.myHeight) / 2
         self.diksi.setGeometry(w - self.diksi.myWidth - 6 - 6, dip, self.diksi.myWidth, self.diksi.myHeight)
         self.edit_but.setGeometry(w - self.diksi.myWidth - 6 - 6 - self.edit_but.myWidth - 3, dip, self.edit_but.myWidth, self.edit_but.myHeight)
         return QWidget.resizeEvent(self, event)
     
     def sizeHint(self):
-        fm = self.fontMetrics()
+        f = QFont(self.font())
+        f.setPointSize(f.pointSize() + 2)
+        fm = QFontMetrics(f)
+        fm2 = self.fontMetrics()
         rect = fm.boundingRect(unicode(self.conn.name))
-        rect2 = fm.boundingRect(self.addressText())
-        w = max(rect.width(), 80) + 8 + 32 + 16 + self.diksi.myWidth + 3 + self.edit_but.myWidth + 8 + 8
-        w2 = max(rect2.width(), 80) + 8 + 32 + 16 + self.diksi.myWidth + 3 + self.edit_but.myWidth + 8 + 8
+        rect2 = fm2.boundingRect(self.addressText())
+        w = self.text_start + min(rect.width(), 240) + 6 + self.edit_but.myWidth + 3 + self.diksi.myWidth + 6
+        w2 = self.text_start + min(rect2.width(), 240) + 6 + self.edit_but.myWidth + 3 + self.diksi.myWidth + 6
         w = max(w, w2)
-        h = max(rect.height() + rect2.height(), 32) + 6
+        h = max(fm.height() + 3 + fm2.height(), 32) + 10
         return QSize(w, h)
 
 
