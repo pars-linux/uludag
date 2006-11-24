709333f987645986f8a4ad2c4a7249f2cbd2ed13
Faik Uygur <faikuygur@gmail.com>
2006-11-24 12:36:04 +0000

    package-manager-1.1_rc4 is coming

--- tasma/package-manager/src/package-manager.py
+++ tasma/package-manager/src/package-manager.py
@@ -42,7 +42,7 @@ def I18N_NOOP(str):
     return str
 
 description = I18N_NOOP("GUI for PiSi package manager")
-version = "1.1_rc3"
+version = "1.1_rc4"
 unremovable_packages = set(['qt','kdelibs','kdebase','sip','PyQt','PyKDE','pisi'])
 (install_state, remove_state, upgrade_state) = range(3)
 
