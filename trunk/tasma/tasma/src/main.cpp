ad9735fd2d52191b86fb9161c687b9ef4fd72800
cartman <cartman@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2006-11-26 19:20:12 +0000

    its 2006

--- trunk/tasma/tasma/src/main.cpp
+++ trunk/tasma/tasma/src/main.cpp
@@ -61,7 +61,7 @@ int main( int argc, char *argv[] )
   KLocale::setMainCatalogue( "tasma" );
   KAboutData aboutData( "tasma", I18N_NOOP( "TASMA - Pardus Configuration Center" ), "1.0.12",
                         I18N_NOOP( "Pardus Configuration Center" ), KAboutData::License_GPL,
-                        I18N_NOOP( "(c) 2005, TUBITAK - UEKAE" ) );
+                        I18N_NOOP( "(c) 2005-2006, TUBITAK - UEKAE" ) );
   aboutData.addAuthor( "İsmail Dönmez", I18N_NOOP( "Current Maintainer" ), "ismail@uludag.org.tr" );
   aboutData.addAuthor( "Barış Metin", I18N_NOOP( "Old Maintainer & Original Author" ), "baris@uludag.org.tr" );
   aboutData.addAuthor( I18N_NOOP( "KDE Developers" ), I18N_NOOP( "Module Developers" ) );
