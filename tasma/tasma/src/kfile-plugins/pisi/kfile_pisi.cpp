8b0ecac685f3e307a7fd129879a56108847ccf81
İsmail Dönmez <ismail@users.noreply.github.com>
2006-11-27 19:00:46 +0000

    80 characters ought to be enough for everyone (TM)

--- tasma/tasma/src/kfile-plugins/pisi/kfile_pisi.cpp
+++ tasma/tasma/src/kfile-plugins/pisi/kfile_pisi.cpp
@@ -10,6 +10,7 @@
 */
 
 #include <kgenericfactory.h>
+#include <kstringhandler.h>
 
 #include <qfile.h>
 #include <qstringlist.h>
@@ -65,7 +66,7 @@ bool PisiPlugin::readInfo(KFileMetaInfo& info, uint)
 
     appendItem(group, "Name", iks_cdata(iks_child(iks_find(iks_find(x, "Package"), "Name"))));
     appendItem(group, "Size", QString(iks_cdata(iks_child(iks_find(iks_find(x, "Package"), "InstalledSize")))).toInt()/1024);
-    appendItem(group, "Description", iks_cdata(iks_child(iks_find(iks_find(x, "Package"), "Description"))));
+    appendItem(group, "Description", KStringHandler::rsqueeze(iks_cdata(iks_child(iks_find(iks_find(x, "Package"), "Description"))), 80));
 
     zip_close(pisi);
     iks_delete(x);
