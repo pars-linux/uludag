9efb97d24377ea36e7b7e3227e4bb5ef09851943
faik <faik@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2006-11-27 09:24:22 +0000

    Copyrights.

--- trunk/tasma/package-manager/src/BasketDialog.py
+++ trunk/tasma/package-manager/src/BasketDialog.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2006 TUBITAK/UEKAE
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file
+
 import string
 
 from sets import Set as set
--- trunk/tasma/package-manager/src/ComarIface.py
+++ trunk/tasma/package-manager/src/ComarIface.py
@@ -1,5 +1,14 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Comar Interface
+#
+# Copyright (C) 2005, 2006 TUBITAK/UEKAE
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file
 
 from qt import QSocketNotifier, QMutex, SIGNAL
 import comar
