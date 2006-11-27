d731590422fad3f771dd76262c5996e3c34c0a2d
cartman <cartman@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2006-11-27 15:50:54 +0000

    handle errors

--- trunk/tasma/tasma/src/kfile-plugins/pisi/kfile_pisi.cpp
+++ trunk/tasma/tasma/src/kfile-plugins/pisi/kfile_pisi.cpp
@@ -51,7 +51,17 @@ bool PisiPlugin::readInfo(KFileMetaInfo& info, uint)
     int err;
 
     zip *pisi = zip_open(info.path().data(), &err);
+
+    if (err)
+      return false;
+
     iks *x = zip_load_xml(pisi, "metadata.xml", &err);
+
+    if (err)
+      {
+        iks_delete(x);
+        return false;
+      }
 
     appendItem(group, "Name", iks_cdata(iks_child(iks_find(iks_find(x, "Package"), "Name"))));
     appendItem(group, "Size", QString(iks_cdata(iks_child(iks_find(iks_find(x, "Package"), "InstalledSize")))).toInt()/1024);
