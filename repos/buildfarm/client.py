906b1f9e3a8b2e3244455999e63e0d107b6695be
caglar <caglar@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2007-01-14 17:54:51 +0000

    ignore

--- repos/buildfarm/client.py
+++ repos/buildfarm/client.py
@@ -4,7 +4,7 @@ import xmlrpclib
 
 server = xmlrpclib.Server('http://localhost:8000')
 
-print "Provided Methotds: %s" % server.system.listMethods()
+print "Provided Methods: %s\n" % server.system.listMethods()
 
 print "Update repository: %s" % server.updateRepository()
 
@@ -12,7 +12,7 @@ print "WorkQueue: %s" % server.getWorkQueue()
 print "WaitQueue: %s" % server.getWaitQueue()
 
 print "Remove A from WorkQueue: %s" % server.removeFromWorkQueue("a/pspec.xml")
-print "Append b to WaitQueue: %s" % server.appendToWaitQueue("b/pspec.xml")
+print "Append B to WaitQueue: %s" % server.appendToWaitQueue("b/pspec.xml")
 print "Append A to WorkQueue: %s" % server.appendToWorkQueue("a/pspec.xml")
 
 print "WorkQueue: %s" % server.getWorkQueue()
