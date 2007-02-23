2cd9a666bf4ce1ba8f810a4b69f66fed74323977
Bahadır Kandemir <kandemir@gmail.com>
2007-02-23 12:50:38 +0000

    Allow user to interrupt ISO making process

--- pardusman/src/maker.py
+++ pardusman/src/maker.py
@@ -145,19 +145,23 @@ def squash_image(project):
 def make_repos(project):
     print "Preparing image repo..."
     
-    repo = project.get_repo()
-    repo_dir = project.image_repo_dir(clean=True)
-    if project.type == "install":
-        imagedeps = repo.full_deps("yali")
-    else:
-        imagedeps = project.all_packages
-    repo.make_local_repo(repo_dir, imagedeps)
-    
-    if project.type == "install":
-        print "Preparing installation repository..."
+    try:
+        repo = project.get_repo()
+        repo_dir = project.image_repo_dir(clean=True)
+        if project.type == "install":
+            imagedeps = repo.full_deps("yali")
+        else:
+            imagedeps = project.all_packages
+        repo.make_local_repo(repo_dir, imagedeps)
         
-        repo_dir = project.install_repo_dir(clean=True)
-        repo.make_local_repo(repo_dir, project.all_packages)
+        if project.type == "install":
+            print "Preparing installation repository..."
+            
+            repo_dir = project.install_repo_dir(clean=True)
+            repo.make_local_repo(repo_dir, project.all_packages)
+    except KeyboardInterrupt:
+        print "Keyboard Interrupt: make_repo() cancelled."
+        sys.exit(1)
 
 def check_file(repo_dir, name, hash):
     path = os.path.join(repo_dir, name)
@@ -171,114 +175,127 @@ def check_file(repo_dir, name, hash):
 
 def check_repo_files(project):
     print "Checking image repo..."
-    repo = project.get_repo()
-    repo_dir = project.image_repo_dir()
-    if project.type == "install":
-        imagedeps = repo.full_deps("yali")
-    else:
-        imagedeps = project.all_packages
-    i = 0
-    for name in imagedeps:
-        i += 1
-        sys.stdout.write("\r%-70.70s" % "Checking %d of %s packages" % (i, len(imagedeps)))
-        sys.stdout.flush()
-        pak = repo.packages[name]
-        check_file(repo_dir, pak.uri, pak.sha1sum)
-    sys.stdout.write("\n")
-    
-    if project.type == "install":
-        repo_dir = project.install_repo_dir()
+    try:
+        repo = project.get_repo()
+        repo_dir = project.image_repo_dir()
+        if project.type == "install":
+            imagedeps = repo.full_deps("yali")
+        else:
+            imagedeps = project.all_packages
         i = 0
-        for name in project.all_packages:
+        for name in imagedeps:
             i += 1
-            sys.stdout.write("\r%-70.70s" % "Checking %d of %s packages" % (i, len(project.all_packages)))
+            sys.stdout.write("\r%-70.70s" % "Checking %d of %s packages" % (i, len(imagedeps)))
             sys.stdout.flush()
             pak = repo.packages[name]
             check_file(repo_dir, pak.uri, pak.sha1sum)
-    sys.stdout.write("\n")
+        sys.stdout.write("\n")
+        
+        if project.type == "install":
+            repo_dir = project.install_repo_dir()
+            i = 0
+            for name in project.all_packages:
+                i += 1
+                sys.stdout.write("\r%-70.70s" % "Checking %d of %s packages" % (i, len(project.all_packages)))
+                sys.stdout.flush()
+                pak = repo.packages[name]
+                check_file(repo_dir, pak.uri, pak.sha1sum)
+        sys.stdout.write("\n")
+    except KeyboardInterrupt:
+        print "Keyboard Interrupt: check_repo() cancelled."
+        sys.exit(1)
 
 def make_image(project):
     print "Preparing install image..."
-    
-    repo = project.get_repo()
-    repo_dir = project.image_repo_dir()
-    image_file = project.image_file()
-    
-    image_dir = project.image_dir()
-    run('umount %s/proc' % image_dir)
-    run('umount %s/sys' % image_dir)
-    image_dir = project.image_dir(clean=True)
-    
-    run('pisi --yes-all -D"%s" ar pardus-install %s' % (image_dir, repo_dir + "/pisi-index.xml.bz2"))
-    if project.type == "install":
-        run('pisi --yes-all --ignore-comar -D"%s" it yali' % image_dir)
-    else:
-        install_packages(project)
-    
-    def chrun(cmd):
-        run('chroot "%s" %s' % (image_dir, cmd))
-    
-    os.mknod("%s/dev/null" % image_dir, 0666 | stat.S_IFCHR, os.makedev(1, 3))
-    os.mknod("%s/dev/console" % image_dir, 0666 | stat.S_IFCHR, os.makedev(5, 1))
-    
-    path = "%s/usr/share/baselayout/" % image_dir
-    path2 = "%s/etc" % image_dir
-    for name in os.listdir(path):
-        run('cp -p "%s" "%s"' % (os.path.join(path, name), os.path.join(path2, name)))
-    run('/bin/mount --bind /proc %s/proc' % image_dir)
-    run('/bin/mount --bind /sys %s/sys' % image_dir)
-    
-    chrun("/sbin/ldconfig")
-    chrun("/sbin/update-environment")
-    chroot_comar(image_dir)
-    chrun("/usr/bin/hav call-package System.Package.postInstall baselayout")
-    chrun("/usr/bin/pisi configure-pending")
-    
-    chrun("hav call User.Manager.setUser uid 0 password pardus")
-    if project.type != "install":
-        chrun("hav call User.Manager.addUser uid 1000 name pars realname Pardus groups users,wheel,disk,removable,power,pnp,pnpadmin,video,audio password pardus")
-    chrun("/usr/bin/comar --stop")
-    
-    path1 = os.path.join(image_dir, "usr/share/baselayout/inittab.live")
-    path2 = os.path.join(image_dir, "etc/inittab")
-    os.unlink(path2)
-    run('mv "%s" "%s"' % (path1, path2))
-    
-    file(os.path.join(image_dir, "etc/pardus-release"), "w").write("%s\n" % project.title)
-    
-    if project.type != "install":
-        setup_live_kdm(project)
-    
-    run('umount %s/proc' % image_dir)
-    run('umount %s/sys' % image_dir)
+   
+    try:
+        repo = project.get_repo()
+        repo_dir = project.image_repo_dir()
+        image_file = project.image_file()
+        
+        image_dir = project.image_dir()
+        run('umount %s/proc' % image_dir)
+        run('umount %s/sys' % image_dir)
+        image_dir = project.image_dir(clean=True)
+        
+        run('pisi --yes-all -D"%s" ar pardus-install %s' % (image_dir, repo_dir + "/pisi-index.xml.bz2"))
+        if project.type == "install":
+            run('pisi --yes-all --ignore-comar -D"%s" it yali' % image_dir)
+        else:
+            install_packages(project)
+        
+        def chrun(cmd):
+            run('chroot "%s" %s' % (image_dir, cmd))
+        
+        os.mknod("%s/dev/null" % image_dir, 0666 | stat.S_IFCHR, os.makedev(1, 3))
+        os.mknod("%s/dev/console" % image_dir, 0666 | stat.S_IFCHR, os.makedev(5, 1))
+        
+        path = "%s/usr/share/baselayout/" % image_dir
+        path2 = "%s/etc" % image_dir
+        for name in os.listdir(path):
+            run('cp -p "%s" "%s"' % (os.path.join(path, name), os.path.join(path2, name)))
+        run('/bin/mount --bind /proc %s/proc' % image_dir)
+        run('/bin/mount --bind /sys %s/sys' % image_dir)
+        
+        chrun("/sbin/ldconfig")
+        chrun("/sbin/update-environment")
+        chroot_comar(image_dir)
+        chrun("/usr/bin/hav call-package System.Package.postInstall baselayout")
+        chrun("/usr/bin/pisi configure-pending")
+        
+        chrun("hav call User.Manager.setUser uid 0 password pardus")
+        if project.type != "install":
+            chrun("hav call User.Manager.addUser uid 1000 name pars realname Pardus groups users,wheel,disk,removable,power,pnp,pnpadmin,video,audio password pardus")
+        chrun("/usr/bin/comar --stop")
+        
+        path1 = os.path.join(image_dir, "usr/share/baselayout/inittab.live")
+        path2 = os.path.join(image_dir, "etc/inittab")
+        os.unlink(path2)
+        run('mv "%s" "%s"' % (path1, path2))
+        
+        file(os.path.join(image_dir, "etc/pardus-release"), "w").write("%s\n" % project.title)
+        
+        if project.type != "install":
+            setup_live_kdm(project)
+        
+        run('umount %s/proc' % image_dir)
+        run('umount %s/sys' % image_dir)
+    except KeyboardInterrupt:
+        print "Keyboard Interrupt: make_image() cancelled."
+        sys.exit(1)
 
 def make_iso(project):
     print "Preparing ISO..."
     
-    iso_dir = project.iso_dir(clean=True)
-    iso_file = project.iso_file(clean=True)
-    image_dir = project.image_dir()
-    image_file = project.image_file()
-    
-    os.link(image_file, os.path.join(iso_dir, "pardus.img"))
-    
-    def copy(src, dest):
-        run('cp "%s" "%s"' % (src, os.path.join(iso_dir, dest)))
-    
-    path = project.release_files
-    for name in os.listdir(path):
-        if name != ".svn":
-            copy(os.path.join(path, name), name)
-    
-    setup_grub(project)
-    
-    if project.type == "install":
-        run('ln -s "%s" "%s"' % (project.install_repo_dir(), os.path.join(iso_dir, "repo")))
-    
-    run('mkisofs -f -J -joliet-long -R -l -V "Pardus" -o "%s" -b boot/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -boot-info-table "%s"' % (
-        iso_file,
-        iso_dir,
-    ))
+    try:
+        iso_dir = project.iso_dir(clean=True)
+        iso_file = project.iso_file(clean=True)
+        image_dir = project.image_dir()
+        image_file = project.image_file()
+        
+        os.link(image_file, os.path.join(iso_dir, "pardus.img"))
+        
+        def copy(src, dest):
+            run('cp "%s" "%s"' % (src, os.path.join(iso_dir, dest)))
+        
+        path = project.release_files
+        for name in os.listdir(path):
+            if name != ".svn":
+                copy(os.path.join(path, name), name)
+        
+        setup_grub(project)
+        
+        if project.type == "install":
+            run('ln -s "%s" "%s"' % (project.install_repo_dir(), os.path.join(iso_dir, "repo")))
+        
+        run('mkisofs -f -J -joliet-long -R -l -V "Pardus" -o "%s" -b boot/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -boot-info-table "%s"' % (
+            iso_file,
+            iso_dir,
+        ))
+    except KeyboardInterrupt:
+        print "Keyboard Interrupt: make_iso() cancelled."
+        sys.exit(1)
+
 
 def make(project):
     make_image(project)
