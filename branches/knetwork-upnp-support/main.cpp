26665193efb2b65aad052ab7642e7298ce889cc5
cartman <cartman@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2007-03-02 14:02:16 +0000

    Operation "Bebe's Boobs Destroys the MSN" started

---
+++ branches/knetwork-upnp-support/main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <qapplication.h>
+#include <qthread.h>
+
+#include "upnp.h"
+
+using namespace std;
+
+//Simple example usage....
+
+class MainThread : public QThread
+{
+public:
+    virtual void run();
+};
+
+void MainThread::run()
+{
+    /*
+     * UPnP controller instance. When this goes, all the services will be destroyed.
+     */
+    UPnP upnp;
+
+    /*
+     * isReady() has the controller got any services
+     */
+    while (!upnp.isReady())
+    {
+        cout <<"Not ready" << endl;
+        sleep(1);
+    }
+
+    /*
+     * Helper function to locate a suitable NAT service to use
+     */
+    UPnPWANService *srv;
+    while ( !(srv = upnp.GetService("NAT")))
+    {
+        cout << "Waiting" << endl;
+        sleep(1);
+    }
+
+    if (srv)
+    {
+        /*
+         * Get our IP address
+         */
+        QString ip;
+        if (srv->GetExternalIPAddress(ip))
+        {
+            cout << ip << endl;
+        }
+
+/*        if (srv->AddPortMapping( "", 8010,
+                                 "TCP", 8010,
+                                 "192.168.0.32",
+                                 "test tunnel", 0 ))
+        {
+            cout << "New port mapping created." << endl;
+        }
+
+        if (srv->DeletePortMapping( "", 8010, "TCP"))
+        {
+            cout << "Port mapping deleted." << endl;
+            }*/
+    }
+
+}
+
+int main(int argc, char **argv)
+{
+    QApplication a( argc, argv );
+    qInitNetworkProtocols();
+
+
+    MainThread t;
+    t.start();
+
+
+    return a.exec();
+}
---
+++ branches/knetwork-upnp-support/upnp.h
@@ -0,0 +1,102 @@
+#include <qobject.h>
+#include <qnetwork.h>
+#include <qhttp.h>
+#include <qdom.h>
+#include <qurl.h>
+
+class UPnP;
+class Device;
+class Service;
+
+class ServiceFactory
+{
+public:
+    static Service *Create(const Device* dev, const QDomElement& el);
+};
+
+
+class Device : public QObject
+{
+Q_OBJECT
+
+public:
+    Device( const UPnP *upnp, const QUrl &url );
+    virtual ~Device();
+
+    QPtrList<Service> m_services;
+
+    bool Command( const QString &service, const QString &command, const QString &url_path, const QString &content, QString &response ) const;
+
+private slots:
+    void requestFinished( int, bool );
+
+private:
+    const UPnP *m_upnp;
+
+    QUrl m_url;
+    unsigned int m_port;
+
+    QHttp http;
+};
+
+class Service
+{
+public:
+    Service( const Device *dev, const QString &name, const QString &type, const QString &url ) : m_dev(dev),m_serviceName(name), m_serviceType(type), m_controlURL(url) {}
+    virtual ~Service();
+
+    const QString &serviceName() { return m_serviceName;}
+    const QString &controlURL() { return m_controlURL;}
+
+    const Device* m_dev;;
+
+protected:
+    QString m_serviceName;
+    QString m_serviceType;
+    QString m_controlURL;
+
+ private:
+    friend class ServiceFactory;
+};
+
+
+
+class UPnPWANService : public Service
+{
+public:
+    UPnPWANService( const Device *dev, const QString &name, const QString &type, const QString &url ) : Service(dev, name, type, url) {}
+    ~UPnPWANService();
+
+    bool GetExternalIPAddress( QString &ip );
+
+    bool AddPortMapping( const QString& remotehost, unsigned int external_port,
+                         const QString& protocol, unsigned int internal_port,
+                         const QString& localclient,
+                         const QString& comment, int duration );
+
+    bool DeletePortMapping( const QString& remotehost, int external_port, const QString &protocol );
+
+    bool GetStatusInfo( QString &status ) {status="Connected"; return true;}
+    bool GetNATStatus( QString &status ) {status="Enabled"; return true;}
+
+private:
+};
+
+
+
+class UPnP
+{
+public:
+    UPnP();
+    ~UPnP();
+
+    bool isReady() { return m_ready;}
+    UPnPWANService* getNATService();
+    void findDevices();
+
+private:
+    QPtrList<Device> m_devices;
+
+    bool m_ready;
+};
+
