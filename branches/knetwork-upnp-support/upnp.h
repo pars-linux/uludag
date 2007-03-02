9dcf08f51d186ea6a0a76c6bd261e148a7e62759
cartman <cartman@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2007-03-02 14:32:20 +0000

    indent

--- branches/knetwork-upnp-support/upnp.h
+++ branches/knetwork-upnp-support/upnp.h
@@ -12,87 +12,87 @@ class Service;
 
 class ServiceFactory
 {
-public:
-    static Service *create(const Device* dev, const QDomElement& el);
+ public:
+  static Service *create(const Device* dev, const QDomElement& el);
 };
 
 class Device : public QObject
 {
-Q_OBJECT
+ Q_OBJECT
 
-public:
-    Device( const UPnP *upnp, const QUrl &url );
-    virtual ~Device();
+   public:
+  Device( const UPnP *upnp, const QUrl &url );
+  virtual ~Device();
 
-    QPtrList<Service> m_services;
+  QPtrList<Service> m_services;
 
-    bool command( const QString &service, const QString &command, const QString &url_path, const QString &content, QString &response ) const;
+  bool command( const QString &service, const QString &command, const QString &url_path, const QString &content, QString &response ) const;
 
-private slots:
-    void requestFinished( int, bool );
+  private slots:
+  void requestFinished( int, bool );
 
-private:
-    const UPnP *m_upnp;
+ private:
+  const UPnP *m_upnp;
 
-    QUrl m_url;
-    unsigned int m_port;
+  QUrl m_url;
+  unsigned int m_port;
 
-    QHttp http;
+  QHttp http;
 };
 
 class Service
 {
-public:
-    Service( const Device *dev, const QString &name, const QString &type, const QString &url ) : m_dev(dev),m_serviceName(name), m_serviceType(type), m_controlURL(url) {}
-    virtual ~Service();
+ public:
+ Service( const Device *dev, const QString &name, const QString &type, const QString &url ) : m_dev(dev),m_serviceName(name), m_serviceType(type), m_controlURL(url) {}
+  virtual ~Service();
 
-    const QString& serviceName() { return m_serviceName;}
-    const QString& controlURL() { return m_controlURL;}
+  const QString& serviceName() { return m_serviceName;}
+  const QString& controlURL() { return m_controlURL;}
 
-    const Device* m_dev;;
+  const Device* m_dev;;
 
-protected:
-    QString m_serviceName;
-    QString m_serviceType;
-    QString m_controlURL;
+ protected:
+  QString m_serviceName;
+  QString m_serviceType;
+  QString m_controlURL;
 
  private:
-    friend class ServiceFactory;
+  friend class ServiceFactory;
 };
 
 class UPnPWANService : public Service
 {
-public:
-    UPnPWANService( const Device *dev, const QString &name, const QString &type, const QString &url ) : Service(dev, name, type, url) {}
-    ~UPnPWANService();
+ public:
+ UPnPWANService( const Device *dev, const QString &name, const QString &type, const QString &url ) : Service(dev, name, type, url) {}
+  ~UPnPWANService();
 
-    bool getExternalIPAddress( QString &ip );
+  bool getExternalIPAddress( QString &ip );
 
-    bool addPortMapping( const QString& remotehost, unsigned int external_port,
-                         const QString& protocol, unsigned int internal_port,
-                         const QString& localclient,
-                         const QString& comment, int duration );
+  bool addPortMapping( const QString& remotehost, unsigned int external_port,
+                       const QString& protocol, unsigned int internal_port,
+                       const QString& localclient,
+                       const QString& comment, int duration );
 
-    bool deletePortMapping( const QString& remotehost, int external_port, const QString &protocol );
+  bool deletePortMapping( const QString& remotehost, int external_port, const QString &protocol );
 
-    bool getStatusInfo( QString &status ) {status="Connected"; return true;}
-    bool getNATStatus( QString &status ) {status="Enabled"; return true;}
+  bool getStatusInfo( QString &status ) {status="Connected"; return true;}
+  bool getNATStatus( QString &status ) {status="Enabled"; return true;}
 
-private:
+ private:
 };
 
 class UPnP
 {
-public:
-    UPnP();
-    ~UPnP();
+ public:
+  UPnP();
+  ~UPnP();
 
-    bool isReady() { return m_ready;}
-    UPnPWANService* getNATService();
-    void findDevices();
+  bool isReady() { return m_ready;}
+  UPnPWANService* getNATService();
+  void findDevices();
 
-private:
-    QPtrList<Device> m_devices;
+ private:
+  QPtrList<Device> m_devices;
 
-    bool m_ready;
+  bool m_ready;
 };
