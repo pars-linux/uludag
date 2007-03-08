f006979066eae48bb8766e4e7362db84a587597c
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2007-03-08 11:55:23 +0000

    Don't folow symbolic links while copying grub files

--- trunk/pardusman/src/gui.py
+++ trunk/pardusman/src/gui.py
@@ -265,7 +265,7 @@ class ProjectWindow(KMainWindow):
     def make(self):
         if not self.checkSettings():
             return
-        repo = self.project.get_repo(self.progress)
+        repo = self.project.get_repo()
         f = tempfile.NamedTemporaryFile()
         self.project.save(f.name)
         ppath = sys.argv[0]
