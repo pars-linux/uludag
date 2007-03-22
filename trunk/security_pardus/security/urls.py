3969f42fe3c8ad0f25b0737db856ee8aa5df920b
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2007-03-22 14:25:14 +0000

    Better variable / template naming

--- trunk/security_pardus/security/urls.py
+++ trunk/security_pardus/security/urls.py
@@ -1,18 +1,13 @@
 from django.conf.urls.defaults import *
-from security.plsa.models import PLSA
 from security.settings import WEB_ROOT
 
 urlpatterns = patterns('',
     (r'^mudur/', include('django.contrib.admin.urls')),
     (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '%s/media' % WEB_ROOT, 'show_indexes': True}),
-
-    (r'^(?P<lang>\w{2,})/(?P<plsa_id>\d{4}-\d{1,})/text/$', 'security.plsa.views.details_text'),
-    (r'^(?P<lang>\w{2,})/(?P<plsa_id>\d{4}-\d{1,})/$', 'security.plsa.views.details'),
-
-    (r'^(?P<lang>\w{2,})/(?P<year>\d{4})/$', 'security.plsa.views.list_year'),
-    (r'^(?P<lang>\w{2,})/$', 'security.plsa.views.page_lang'),
-
-    (r'^(?P<lang>\w{2,})/rss/$', 'security.plsa.views.feed'),
-
-    (r'^$', 'security.plsa.views.page_index'),
+    (r'^(?P<lang_code>\w{2,})/(?P<plsa_id>\d{4}-\d{1,})/text/$', 'security.advisory.views.advisory_text'),
+    (r'^(?P<lang_code>\w{2,})/(?P<plsa_id>\d{4}-\d{1,})/$', 'security.advisory.views.advisory_details'),
+    (r'^(?P<lang_code>\w{2,})/(?P<year>\d{4})/$', 'security.advisory.views.archive_year'),
+    (r'^(?P<lang_code>\w{2,})/$', 'security.advisory.views.index_language'),
+    (r'^$', 'security.advisory.views.index'),
+    (r'^(?P<lang_code>\w{2,})/rss/$', 'security.advisory.views.feed'),
 )
