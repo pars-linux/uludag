93bac7ddae657246865636c7af1a8b67c3bb98e3
gurer <gurer@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2007-04-04 08:39:18 +0000

    those need dont_follow too

--- trunk/python-modules/catbox/tests/follow-test.py
+++ trunk/python-modules/catbox/tests/follow-test.py
@@ -4,19 +4,27 @@ import os
 import sys
 import catbox
 
+good_path = "catboxtest.link"
+bad_path = "catboxtest2.link"
+
 def good_op():
-    os.unlink("catboxtest.link")
+    os.lchown(good_path, os.getuid(), os.getgid())
+    os.unlink(good_path)
 
 def bad_op():
-    file("catboxtest2.link", "w").write("hello world\n")
+    try:
+        file(bad_path, "w").write("hello world\n")
+    except:
+        pass
+    os.chown(bad_path, os.getuid(), os.getgid())
 
 def mklink(dest, source):
     if os.path.exists(source):
         os.unlink(source)
     os.symlink(dest, source)
 
-mklink("/var", "catboxtest.link")
-mklink("/tmp/hede", "catboxtest2.link")
+mklink("/var", good_path)
+mklink("/tmp/hede", bad_path)
 
 ret = catbox.run(good_op, writable_paths=[os.getcwd()])
 assert(ret.code == 0)
@@ -24,4 +32,4 @@ assert(ret.violations == [])
 
 ret = catbox.run(bad_op, writable_paths=[os.getcwd()])
 assert(ret.code == 1)
-assert(len(ret.violations) == 1)
+assert(map(lambda x: x[0], ret.violations) == ["open", "chown32"])
