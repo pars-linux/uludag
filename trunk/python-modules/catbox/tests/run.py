8cec1d162904095709f9248a6573b697348fa66e
gurer <gurer@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2007-04-06 06:44:07 +0000

    run tests automatically

--- trunk/python-modules/catbox/tests/run.py
+++ trunk/python-modules/catbox/tests/run.py
@@ -3,17 +3,8 @@
 import os
 import sys
 
-tests = """
-    basic-test.py
-    operations-test.py
-    follow-test.py
-    fork-test.py
-    class-test.py
-    retcode-test.py
-    maxpath-test.py
-"""
-
 base = os.path.dirname(sys.argv[0])
-for name in tests.split():
-    os.system(os.path.join(base, name))
-
+for name in os.listdir(base):
+    if name.endswith("-test.py"):
+        print name
+        os.system(os.path.join(base, name))
