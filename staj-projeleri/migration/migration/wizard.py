110d062aed9294431daaad5fd42cf285d0d3f3b9
muratongan <muratongan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2007-07-12 17:07:03 +0000

    close :$

--- staj-projeleri/migration/migration/wizard.py
+++ staj-projeleri/migration/migration/wizard.py
@@ -121,7 +121,7 @@ class MigrationWizard(KWizard):
         if len(self.users) == 0:
             message = i18n("Migration tool couldn't find any old users in your computer. You can't use this aplication.")
             QMessageBox.critical(self, i18n("No User"), message, QMessageBox.Ok, QMessageBox.NoButton)
-            self.exit()
+            self.close()
         for user in self.users:
             part, parttype, username, userdir = user
             self.userpage.usersBox.insertItem("%s - %s (%s)" % (username, parttype, part))
