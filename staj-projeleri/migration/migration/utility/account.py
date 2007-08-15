1a34eb879ef3adb6d5ec9089bc23bf6fd2b82323
muratongan <muratongan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2007-08-15 11:38:32 +0000

    Hesapları Thunderbird'den oku, MSN'den, GTalk'tan oku, Kopete'ye, KMail'e yaz. (Hala alpha, testler makbule geçer)

--- staj-projeleri/migration/migration/utility/account.py
+++ staj-projeleri/migration/migration/utility/account.py
@@ -17,6 +17,7 @@ from kdecore import KConfig
 class Account:
     def __init__(self):
         self.accounts = []
+        self.folders = []
     
     def getTBAccounts(self, path):
         "Imports Thunderbird accounts using prefs.js file in TB profile directory"
@@ -38,17 +39,52 @@ class Account:
             # Define account types:
             if servertype == "pop3":
                 accountdict["type"] = "POP3"
-                fields = {"name":"name", "host":"hostname", "port":"port", "user":"userName", "SSL":"isSecure", "dir":"directory-rel"}
+                fields = {"name":"name", "host":"hostname", "port":"port", "user":"userName", "SSL":"isSecure"}
+                # Get folder name and path:
+                field = "mail.server." + server + ".defer_get_new_mail"
+                if prefs.get(field, False):     # Using Local Folders
+                    field = "mail.server." + server + ".deferred_to_account"
+                    realaccount = prefs.get(field, account)
+                    realserver = prefs["mail.account." + realaccount + ".server"]
+                else:       # Using its own folder
+                    realserver = server
+                foldername = prefs["mail.server." + realserver + ".name"]
+                folderpath = os.path.join(path, prefs["mail.server." + realserver + ".directory-rel"].replace("[ProfD]", ""))
+                # Add message boxes in this account:
+                def addFolders(name, path):
+                    for itemname in os.listdir(path):
+                        itempath = os.path.join(path, itemname)
+                        if os.path.splitext(itemname)[1] == ".msf" and os.path.isfile(itempath):
+                            mboxpath = os.path.splitext(itempath)[0]
+                            mboxname = os.path.join(name, os.path.basename(mboxpath))
+                            if (mboxname, mboxpath) not in self.folders:
+                                self.folders.append((mboxname, mboxpath))
+                        elif os.path.splitext(itemname)[1] == ".sbd" and os.path.isdir(itempath):
+                            addFolders(os.path.join(name, os.path.splitext(itemname)[0]), itempath)
+                self.folders.append((foldername, ""))
+                addFolders(foldername, folderpath)
+                accountdict["inbox"] = os.path.join(foldername, "Inbox")
             elif servertype == "nntp":
                 accountdict["type"] = "NNTP"
                 fields = {"name":"name", "host":"hostname", "port":"port", "SSL":"isSecure", "dir":"directory-rel", "file":"newsrc.file-rel"}
             elif servertype == "imap":
                 accountdict["type"] = "IMAP"
                 fields = {"name":"name", "host":"hostname", "port":"port", "user":"userName", "SSL":"isSecure", "dir":"directory-rel"}
+            elif servertype == "rss":
+                accountdict["type"] = "RSS"
+                fields = {"name":"name", "dir":"directory-rel"}
             # Get account information from TB prefs:
             for key in fields.keys():
                 field = "mail.server." + server + "." + fields[key]
                 accountdict[key] = prefs.get(field, None)
+            # Correct values:
+            if accountdict.has_key("dir"):
+                accountdict["dir"] = accountdict["dir"].replace("[ProfD]", path)
+            if accountdict.has_key("file"):
+                accountdict["file"] = accountdict["file"].replace("[ProfD]", path)
+            if accountdict.has_key("SSL"):
+                if accountdict["SSL"]:
+                    accountdict["SSL"] = True
             # Add new account:
             if accountdict:
                 self.accounts.append(accountdict)
@@ -63,11 +99,19 @@ class Account:
             accountdict = {}
             accountdict["type"] = "SMTP"
             account = account.strip()
-            fields = {"host":"hostname", "port":"port", "user":"username", "Try SSL":"try_ssl"}
+            fields = {"host":"hostname", "port":"port", "user":"username", "secure":"try_ssl"}
             # Get account information from TB prefs:
             for key in fields.keys():
                 field = "mail.smtpserver." + account + "." + fields[key]
                 accountdict[key] = prefs.get(field, None)
+            # Correct values:
+            if accountdict.has_key("secure"):
+                accountdict["SSL"] = False
+                accountdict["TLS"] = False
+                if accountdict["secure"] in ["1", "2"]:
+                    accountdict["TLS"] = True
+                elif accountdict["secure"] == "3":
+                    accountdict["SSL"] = True
             # Add new accounts:
             if accountdict:
                 self.accounts.append(accountdict)
@@ -135,11 +179,14 @@ class Account:
                             self.accounts.append(accountdict)
     
     def setKopeteAccounts(self):
+        "Add imported accounts into Kopete"
         config = KConfig("kopeterc")
         for account in self.accounts:
             if account["type"] == "Jabber":
                 groupname = "Account_JabberProtocol_" + account["user"]
                 if not config.hasGroup(groupname):
+                    config.setGroup("Plugins")
+                    config.writeEntry("kopete_jabberEnabled", "true")
                     config.setGroup(groupname)
                     config.writeEntry("AccountId", account["user"])
                     config.writeEntry("Protocol", "JabberProtocol")
@@ -149,22 +196,115 @@ class Account:
                     if account["SSL"]:
                         config.writeEntry("UseSSL", "true")
             elif account["type"] == "MSN":
-                groupname = "Account_MSNProtocol_" + account["user"]
+                groupname = "Account_MSNProtocol_" + account["mail"]
                 if not config.hasGroup(groupname):
+                    config.setGroup("Plugins")
+                    config.writeEntry("kopete_msnEnabled", "true")
                     config.setGroup(groupname)
-                    config.writeEntry("AccountId", account["user"])
+                    config.writeEntry("AccountId", account["mail"])
                     config.writeEntry("Protocol", "MSNProtocol")
-                    config.writeEntry("serverName", account["host"])
-                    config.writeEntry("serverPort", account["port"])
+                    config.writeEntry("serverName", "messenger.hotmail.com")
+                    config.writeEntry("serverPort", 1863)
         config.sync()
+    
+    def setKMailAccounts(self):
+        "Add imported accounts into Kopete"
+        config = KConfig("kmailrc")
+        config.setGroup("General")
+        accountno = config.readNumEntry("accounts") + 1
+        config.setGroup("General")
+        transportno = config.readNumEntry("transports") + 1
+        for account in self.accounts:
+            if account["type"] == "POP3":
+                if KMailAccountExists(config, "pop", account["host"], account["user"]):
+                    continue
+                config.setGroup("General")
+                config.writeEntry("accounts", accountno)
+                config.setGroup("Account " + str(accountno))
+                accountno += 1
+                config.writeEntry("trash", "trash")
+                config.writeEntry("Type", "pop")
+                config.writeEntry("Name", account["name"])
+                config.writeEntry("auth", "USER")
+                config.writeEntry("host", account["host"])
+                config.writeEntry("login", account["user"])
+                inbox = account.get("inbox", "inbox")
+                inbox = KMailFolderName(inbox)
+                config.writeEntry("Folder", inbox)
+                if account.has_key("SSL") and account["SSL"]:
+                    config.writeEntry("use-ssl", "true")
+                    config.writeEntry("port", 995)
+                else:
+                    config.writeEntry("use-ssl", "false")
+                    config.writeEntry("port", 110)
+                if account.has_key("port") and account["port"]:
+                    config.writeEntry("port", account["port"])
+                config.writeEntry("use-tls", "false")
+            elif account["type"] == "IMAP":
+                if KMailAccountExists(config, "imap", account["host"], account["user"]):
+                    continue
+                config.setGroup("General")
+                config.writeEntry("accounts", accountno)
+                config.setGroup("Account " + str(accountno))
+                accountno += 1
+                config.writeEntry("Folder", "")
+                config.writeEntry("trash", "trash")
+                config.writeEntry("Type", "imap")
+                config.writeEntry("Name", account["name"])
+                config.writeEntry("auth", "*")
+                config.writeEntry("host", account["host"])
+                config.writeEntry("login", account["user"])
+                if account.has_key("SSL") and account["SSL"]:
+                    config.writeEntry("use-ssl", "true")
+                    config.writeEntry("port", 993)
+                else:
+                    config.writeEntry("use-ssl", "false")
+                    config.writeEntry("port", 143)
+                if account.has_key("port") and account["port"]:
+                    config.writeEntry("port", account["port"])
+                config.writeEntry("use-tls", "false")
+            elif account["type"] == "SMTP":
+                if KMailTransportExists(config, account["host"], account["user"]):
+                    continue
+                config.setGroup("General")
+                config.writeEntry("transports", transportno)
+                config.setGroup("Transport " + str(transportno))
+                transportno += 1
+                config.writeEntry("auth", "true")
+                config.writeEntry("authtype", "PLAIN")
+                config.writeEntry("name", account["host"])
+                config.writeEntry("host", account["host"])
+                config.writeEntry("user", account["user"])
+                if account.has_key("SSL") and account["SSL"]:
+                    config.writeEntry("encryption", "SSL")
+                    config.writeEntry("port", 465)
+                else:
+                    config.writeEntry("port", 25)
+                    if account.has_key("TLS") and account["TLS"]:
+                        config.writeEntry("encryption", "TLS")
+                if account.has_key("port") and account["port"]:
+                    config.writeEntry("port", account["port"])
+        config.sync()
+        # Add missing directories and mbox files:
+        for folder in self.folders:
+            name = folder[0]
+            path = KMailFolderName(name)
+            path = os.path.join(os.path.expanduser("~/.kde/share/apps/kmail/mail"), path)
+            if not os.path.exists(path):
+                dirpath = os.path.dirname(path)
+                if not os.path.isdir(dirpath):
+                    os.makedirs(dirpath)
+                open(path, "w").close()
     
     def yaz(self):
         "Prints accounts"
         for account in self.accounts:
             print account["type"]
             for key in account.keys():
-                if key != "type":
-                    print "%10s : %s" % (key, account[key])
+                if key not in ["type", "folders"]:
+                    print "%15s : %s" % (key, account[key])
+        for folder in self.folders:
+            print "%30s : %s" % folder
 
 def parsePrefs(filepath):
     "Parses Thunderbird's prefs.js file and returns it as a dictionary"
@@ -216,4 +356,43 @@ def getData(dom, tagname):
     if element.getAttribute("type") == "DWORD":
         data = int(data, 16)
     return data
+
+def KMailFolderName(folder):
+    "Returns KMail folder name of given folder. (GMail/Inbox -> .GMail.directory/Inbox)"
+    path = folder.split("/")
+    for i in xrange(0, len(path) - 1):
+        path[i] = "." + path[i] + ".directory"
+    folder = "/".join(path)
+    return folder
+
+def KMailAccountExists(config, type1, host1, user1):
+    found = False
+    oldgroup = config.group()
+    config.setGroup("General")
+    accounts = config.readNumEntry("accounts")
+    for account in xrange(1, accounts + 1):
+        config.setGroup("Account " + str(account))
+        type2 = config.readEntry("Type")
+        host2 = config.readEntry("host")
+        user2 = config.readEntry("login")
+        if type1 == type2 and host1 == host2 and user1 == user2:
+            found = True
+            break
+    config.setGroup(oldgroup)
+    return found
+
+def KMailTransportExists(config, host1, user1):
+    found = False
+    oldgroup = config.group()
+    config.setGroup("General")
+    accounts = config.readNumEntry("transports")
+    for account in xrange(1, accounts + 1):
+        config.setGroup("Transport " + str(account))
+        host2 = config.readEntry("host")
+        user2 = config.readEntry("user")
+        if host1 == host2 and user1 == user2:
+            found = True
+            break
+    config.setGroup(oldgroup)
+    return found
 
