11c0dcbcc05f84458a410a8a78dfd9424e522393
gokcen.eraslan <gokcen.eraslan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2007-08-15 09:21:02 +0000

    paket yapım aracı - ilk sürüm

---
+++ staj-projeleri/Packager/src/actions-template.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-·
+#
+# Copyright 2005 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+#
+# Name <email@address>
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    pass
+
+def build():
+    pass
+
+def install():
+    pass
