1928f3fb13e2cecda6c3fd6d1ef54f9495ada165
ozan.caglayan <ozan.caglayan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2007-08-17 06:32:08 +0000

    initial commit

---
+++ trunk/staj-projeleri/buildfarm/helpers/qmanager.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2006, TUBITAK/UEKAE
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Please read the COPYING file.
+
+""" Standart Python Modules """
+import os
+
+""" BuildFarm Modules """
+import config
+import dependency
+
+class QueueManager:
+    def __init__(self):
+        self.workQueue = []
+        self.waitQueue = []
+
+        self.__deserialize__(self.workQueue, "workQueue")
+        self.__deserialize__(self.waitQueue, "waitQueue")
+
+        if len(self.waitQueue):
+            self.workQueue += self.waitQueue
+            self.waitQueue = []
+        else:
+            self.waitQueue = dependency.DependencyResolver(self.waitQueue).resolvDependencies()
+
+        self.workQueue = dependency.DependencyResolver(self.workQueue).resolvDependencies()
+
+    def __del__(self):
+        self.__serialize__(self.waitQueue, "waitQueue")
+        self.__serialize__(self.workQueue, "workQueue")
+
+    def __serialize__(self, queueName, fileName):
+        try:
+            queue = open(os.path.join(config.workDir, fileName), "w")
+        except IOError:
+            return
+
+        for pspec in queueName:
+            queue.write("%s\n" % pspec)
+        queue.close()
+
+    def __deserialize__(self, queueName, fileName):
+        try:
+            queue = open(os.path.join(config.workDir, fileName), "r")
+        except IOError:
+            return
+
+        for line in queue.readlines():
+            if not line.startswith("#"):
+                queueName.append(line.strip("\n"))
+        queue.close()
+
+    def getWorkQueue(self):
+        return self.workQueue
+
+    def getWaitQueue(self):
+        return self.waitQueue
+
+    def removeFromWaitQueue(self):
+        if self.waitQueue.__contains__(pspec):
+            self.waitQueue.remove(pspec)
+            self.__serialize__(self.waitQueue, "waitQueue")
+            return True
+        return False
+
+    def removeFromWorkQueue(self, pspec):
+        if self.workQueue.__contains__(pspec):
+            self.workQueue.remove(pspec)
+            self.__serialize__(self.workQueue, "workQueue")
+            return True
+        return False
+
+    def appendToWorkQueue(self, pspec):
+         if not self.workQueue.__contains__(pspec):
+            self.workQueue.append(pspec)
+            self.__serialize__(self.workQueue, "workQueue")
+            return True
+         return False
+
+    def appendToWaitQueue(self, pspec):
+         if not self.waitQueue.__contains__(pspec):
+            self.waitQueue.append(pspec)
+            self.__serialize__(self.waitQueue, "waitQueue")
+            return True
+         return False
+
+    def transferToWorkQueue(self, pspec):
+        if self.appendToWorkQueue(pspec):
+            self.removeFromWaitQueue(pspec)
+            return True
+        return False
+
+    def transferToWaitQueue(self, pspec):
+        if self.appendToWaitQueue(pspec):
+            self.removeFromWorkQueue(pspec)
+            return True
+        return False
---
+++ trunk/staj-projeleri/buildfarm/server.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2006, TUBITAK/UEKAE
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Please read the COPYING file.
+#
+# Based on http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/496786
+
+LISTEN_HOST="localhost"
+LISTEN_PORT=443
+KEYFILE="certs/new.cert.key"
+CERTFILE="certs/new.cert.cert"
+
+import socket
+import SocketServer
+import BaseHTTPServer
+import SimpleXMLRPCServer
+
+""" pyOpenSSL """
+from OpenSSL import SSL
+
+""" helpers """
+from helpers import qmanager
+from helpers import repomanager
+
+import main
+
+class SecureXMLRPCServer(BaseHTTPServer.HTTPServer,SimpleXMLRPCServer.SimpleXMLRPCDispatcher):
+    def __init__(self, server_address, HandlerClass, logRequests=True):
+        self.logRequests = logRequests
+
+        SimpleXMLRPCServer.SimpleXMLRPCDispatcher.__init__(self)
+        SocketServer.BaseServer.__init__(self, server_address, HandlerClass)
+        ctx = SSL.Context(SSL.SSLv23_METHOD)
+        ctx.use_privatekey_file (KEYFILE)
+        ctx.use_certificate_file(CERTFILE)
+        self.socket = SSL.Connection(ctx, socket.socket(self.address_family, self.socket_type))
+        self.server_bind()
+        self.server_activate()
+
+class SecureXMLRpcRequestHandler(SimpleXMLRPCServer.SimpleXMLRPCRequestHandler):
+    def setup(self):
+        self.connection = self.request
+        self.rfile = socket._fileobject(self.request, "rb", self.rbufsize)
+        self.wfile = socket._fileobject(self.request, "wb", self.wbufsize)
+
+    def do_POST(self):
+        try:
+            # get arguments
+            data = self.rfile.read(int(self.headers["content-length"]))
+            # In previous versions of SimpleXMLRPCServer, _dispatch
+            # could be overridden in this class, instead of in
+            # SimpleXMLRPCDispatcher. To maintain backwards compatibility,
+            # check to see if a subclass implements _dispatch and dispatch
+            # using that method if present.
+            response = self.server._marshaled_dispatch(data, getattr(self, "_dispatch", None))
+        except: 
+            # This should only happen if the module is buggy
+            # internal error, report as HTTP server error
+            self.send_response(500)
+            self.end_headers()
+        else:
+            # got a valid XML RPC response
+            self.send_response(200)
+            self.send_header("Content-type", "text/xml")
+            self.send_header("Content-length", str(len(response)))
+            self.end_headers()
+            self.wfile.write(response)
+
+            # shut down the connection
+            self.wfile.flush()
+            self.connection.shutdown()
+
+class CombinedServerClass(qmanager.QueueManager, repomanager.RepositoryManager):
+    pass
+
+server = SecureXMLRPCServer((LISTEN_HOST, LISTEN_PORT), SecureXMLRpcRequestHandler)
+
+# let server.system.listMethods
+server.register_introspection_functions()
+
+# export CombinedServerClass
+server.register_instance(CombinedServerClass())
+
+# export buildPackages, buildIndex
+# FIXME: run these on another thread and return to client ASAP
+server.register_function(main.buildPackages)
+server.register_function(main.buildIndex)
+
+# enter main loop
+server.serve_forever()
