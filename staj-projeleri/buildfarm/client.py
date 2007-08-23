31fb589fada1dc057dd404c7c94d9ba0c7d9f7a0
ozan.caglayan <ozan.caglayan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2007-08-23 13:22:33 +0000

    argument parsing improvement

--- staj-projeleri/buildfarm/client.py
+++ staj-projeleri/buildfarm/client.py
@@ -32,6 +32,7 @@ def usage():
   status         Displays the buildfarm's active job(s)
   sync           Synchronizes the binary repositories
   update         Updates local pspec repository
+  add p          Adds the package 'p' to the work queue
   list
     wait         Dumps the wait queue
     work         Dumps the work queue
@@ -41,9 +42,6 @@ def usage():
   transfer
     wait p       Transfers the package 'p' from the work queue to the wait queue
     work p       Transfers the package 'p' from the wait queue to the work queue
-  append
-    wait p       Appends the package 'p' to the wait queue
-    work p       Appends the package 'p' to the work queue
   remove
     wait p       Removes the package 'p' from the wait queue
     work p       Removes the package 'p' from the work queue
@@ -64,24 +62,41 @@ def client(op, cmd=None, pspec=None):
     remoteURI = "https://" + REMOTE_HOST + ":" + str(REMOTE_PORT)
     server = xmlrpclib.ServerProxy(remoteURI)
     
-    # 1 Parameter : op
+    # 1 Parameter
     if op == "update":
-        print _("These packages are added to the work queue")
-        print '-' * 45
-        print "\n".join(server.updateRepository())
+        result = server.updateRepository()
+        if result:
+            print _("These packages are added to the work queue\n%s\n" % ('-'*42))
+            print "\n".join(result)
+        else:
+            print _("Local pspec repository is up-to-date")
     elif op == "sync":
-        print _("'%s' doesn't contain these packages: " % "pardus-2007")
-        print '-' * 45
-        print "\n".join(server.sync())
+        result = server.sync()
+        if result:
+            print _("'%s' doesn't contain these packages:\n%s" % ("pardus-2007", ('-'*45)))
+            print "\n".join(result)
+        else:
+            print_("The repositories are already synchronized.")
     elif op == "status":
         print op
     
-    # 2 Parameters : op,cmd
+    # 2 Parameters
+    elif op == "add":
+        retval = server.appendToWorkQueue(pspec)
+        
+        if retval:
+            print _("%s successfully added to the work queue!" % pspec)
+        else:
+            print _("The package '%s' is already in the work queue!" % pspec)
+        
     elif op == "list":
         funcString = "get" + cmd.capitalize() + "Queue"
-        print _("Current %s queue" % cmd)
-        print '-' * 20
-        print "\n".join(server.__getattr__(funcString)())
+        result = server.__getattr__(funcString)()
+        if result:    
+            print _("Current %s queue\n%s" % (cmd, ('-'*19)))
+            print "\n".join(result)
+        else:
+            print _("%s queue is empty!" % cmd.capitalize())
         
     elif op == "build":
         funcString = "build" + cmd.capitalize()
@@ -92,7 +107,7 @@ def client(op, cmd=None, pspec=None):
         else:
             print _("Errors during %s." % funcString)
     
-    # 3 Parameters : op,cmd,pspec
+    # 3 Parameters
     elif op == "remove":
         funcString = "removeFrom" + cmd.capitalize() + "Queue"
         print _("Removing '%s' from %s queue.." % (pspec, cmd))
@@ -101,15 +116,6 @@ def client(op, cmd=None, pspec=None):
             print _("Removed!")
         else:
             print _("Make sure that the packages are already in the %s queue!" % cmd)
-            
-    elif op == "append":
-        funcString = "appendTo" + cmd.capitalize() + "Queue"
-        retval = server.__getattr__(funcString)(pspec)
-        
-        if retval:
-            print _("%s successfully appended to the %s queue!" % (pspec, cmd))
-        else:
-            print _("The package '%s' is already in the %s queue!" % (pspec, cmd))
             
     elif op == "transfer":
         funcString = "transferTo" + cmd.capitalize() + "Queue"
@@ -136,14 +142,16 @@ if __name__ == "__main__":
             usage()
     
     elif len(args) == 2:
-        if args[0] == "list" and args[1] in ("work","wait") or \
+        if args[0] == "add":
+            client(args[0],pspec=args[1])
+        elif args[0] == "list" and args[1] in ("work","wait") or \
             args[0] == "build" and args[1] in ("index","packages"):
             client(args[0],args[1])
         else:
             usage()
             
     elif len(args) == 3:
-        if args[0] in ("append","remove","transfer") and args[1] in ("work","wait"):
+        if args[0] in ("remove","transfer") and args[1] in ("work","wait"):
             client(args[0],args[1],args[2])
         else:
             usage()
