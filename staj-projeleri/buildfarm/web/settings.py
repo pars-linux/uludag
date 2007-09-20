84d5253c6c90f5378e379c518465a06d6b9a566b
ozan.caglayan <ozan.caglayan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2007-09-20 08:55:52 +0000

    updated settings.py

--- staj-projeleri/buildfarm/web/settings.py
+++ staj-projeleri/buildfarm/web/settings.py
@@ -1,32 +1,47 @@
-# Django settings for web project.
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2007 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+# Default django settings for buildfarm web interface
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
+    ('S.Çağlar Onur', 'caglar@pardus.org.tr'),
+    ('Ozan Çağlayan', 'ozancag@gmail.com'),
 )
+
+# Don't forget to define these!
+DOCUMENT_ROOT = ''
+WEB_URL = ''
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = ''           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
-DATABASE_NAME = ''             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASE_ENGINE = 'sqlite3'         # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
+DATABASE_NAME = 'db/buildfarm.db'   # Or path to database file if using sqlite3.
+DATABASE_USER = ''                  # Not used with sqlite3.
+DATABASE_PASSWORD = ''              # Not used with sqlite3.
+DATABASE_HOST = ''                  # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''                  # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
 # http://www.postgresql.org/docs/8.1/static/datetime-keywords.html#DATETIME-TIMEZONE-SET-TABLE
 # although not all variations may be possible on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europe/Istanbul'
 
 # Language code for this installation. All choices can be found here:
 # http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
 # http://blogs.law.harvard.edu/tech/stories/storyReader$15
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'tr'
+
+LANGUAGES = (
+    ('en', ('English')),
+    ('tr', ('Turkish')),
+)
 
 SITE_ID = 1
 
@@ -36,11 +51,11 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '%s/static/' % DOCUMENT_ROOT
 
 # URL that handles the media served from MEDIA_ROOT.
 # Example: "http://media.lawrence.com"
-MEDIA_URL = ''
+MEDIA_URL = '%s/static/' % WEB_URL
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
@@ -67,9 +82,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'web.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    '%s/templates' % DOCUMENT_ROOT,
 )
 
 INSTALLED_APPS = (
@@ -77,4 +90,5 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'web.ciftci',
 )
