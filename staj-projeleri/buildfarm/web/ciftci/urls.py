97da8338dec72dbbbdd97bc62ab1d73a699bed9b
ozan.caglayan <ozan.caglayan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2007-09-28 09:19:56 +0000

    added another urlpattern

--- staj-projeleri/buildfarm/web/ciftci/urls.py
+++ staj-projeleri/buildfarm/web/ciftci/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^list/$', 'web.ciftci.views.list_repository'),
+    (r'^list/(?P<repo_name>.*)/$', 'web.ciftci.views.list_repository')
 )
