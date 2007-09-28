2fe703bf484d1b1b44866efe22d3e8fc73fb9bfc
faik <faik@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2007-09-28 11:27:42 +0000

    Implement get_packages, get_union_packages, get_component, get_union_component
    These functions may be combined in some way in the future.
    With this we also increase the memory usage. But optimizations will come later.

--- branches/pisi-db/pisi/db/componentdb.py
+++ branches/pisi-db/pisi/db/componentdb.py
@@ -26,20 +26,43 @@ class ComponentDB(object):
     def __init__(self):
 
         component_nodes = {}
+        component_packages = {}
+        component_sources = {}
+
         self.repodb = pisi.db.repodb.RepoDB()
 
         for repo in self.repodb.list_repos():
             doc = self.repodb.get_repo_doc(repo)
             component_nodes[repo] = self.__generate_components(doc)
+            component_packages[repo] = self.__generate_packages(doc)
+            component_sources[repo] = self.__generate_sources(doc)
 
         self.cdb = pisi.db.itembyrepo.ItemByRepo(component_nodes)
+        self.cpdb = pisi.db.itembyrepo.ItemByRepo(component_packages)
+        self.csdb = pisi.db.itembyrepo.ItemByRepo(component_sources)
 
+    def __generate_packages(self, doc):
+        components = {}
+        for pkg in doc.tags("Package"):
+            components.setdefault(pkg.getTagData("PartOf"), []).append(pkg.getTagData("Name"))
+        return components
+
+    def __generate_sources(self, doc):
+        components = {}
+        for pkg in doc.tags("Source"):
+            components.setdefault(pkg.getTagData("PartOf"), []).append(pkg.getTagData("Name"))
+        return components
+ 
     def __generate_components(self, doc):
         return dict(map(lambda x: (x.getTagData("Name"), x.toString()), doc.tags("Component")))
 
     def has_component(self, name, repo = None):
         return self.cdb.has_item(name, repo)
 
+    def list_components(self, repo=None):
+        return self.cdb.get_item_keys(repo)
+
+    # Returns the component in given repo or first found component in repo order if repo is None
     def get_component(self, component_name, repo = None):
 
         if not self.has_component(component_name, repo):
@@ -47,43 +70,67 @@ class ComponentDB(object):
 
         component = pisi.component.Component()
         component.parse(self.cdb.get_item(component_name, repo))
+
+        try:
+            component.packages = self.cpdb.get_item(component_name, repo)
+            component.sources = self.csdb.get_item(component_name, repo)
+        except Exception: #FIXME: what exception could we catch here, replace with that.
+            pass
+
         return component
 
-    def get_packages(self, component_name, repo, walk=False):
+    # Returns the component with combined packages and sources from all repos that contain this component
+    def get_union_component(self, component_name):
+
+        component = pisi.component.Component()
+        component.parse(self.cdb.get_item(component_name))
+        
+        for repo in self.repodb.list_repos():
+            try:
+                component.packages.extend(self.cpdb.get_item(component_name, repo))
+                component.sources.extend(self.csdb.get_item(component_name, repo))
+            except Exception: #FIXME: what exception could we catch here, replace with that.
+                pass
+            
+        return component
+
+    # Returns packages of given component from given repo or first found component's packages in repo
+    # order if repo is None. 
+    # If walk is True than also the sub components' packages are returned
+    def get_packages(self, component_name, repo=None, walk=False):
+
+        component = self.get_component(component_name, repo)
+        if not walk:
+            return component.packages
 
         packages = []
+        packages.extend(component.packages)
 
-        if not self.has_component(component_name, repo):
-            raise Exception(_('Component %s not found') % component_name)
-        
-        if walk:
-            components = filter(lambda x:x.startswith(component_name), self.cdb.get_item_keys())
-        else:
-            components = self.cdb.get_item_keys()
-
-        doc = self.repodb.get_repo_doc(repo)
-        for pkg in doc.tags("Package"):
-            if pkg.getTagData("PartOf") in components:
-                packages.append(pkg.getTagData("Name"))
+        sub_components = filter(lambda x:x.startswith(component_name+"."), self.list_components(repo))
+        for sub in sub_components:
+            try:
+                packages.extend(self.get_component(sub, repo).packages)
+            except Exception: #FIXME: what exception could we catch here, replace with that.
+                pass
 
         return packages
 
-    def list_components(self, repo=None):
-        return self.cdb.get_item_keys(repo)
+    # Returns the component with combined packages and sources from all repos that contain this component
+    # If walk is True than also the sub components' packages from all repos are returned
+    def get_union_packages(self, component_name, walk=False):
 
-    def get_union_comp(self, component_name):
+        component = self.get_union_component(component_name)
+        if not walk:
+            return component.packages
 
-        component = self.get_component(component_name)
+        packages = []
+        packages.extend(component.packages)
 
-        for repo in self.repodb.list_repos():
-            doc = self.repodb.get_repo_doc(repo)
-            for pkg in doc.tags("Package"):
-                if pkg.getTagData("PartOf") == component_name:
-                    component.packages.append(pkg.getTagData("Name"))
+        sub_components = filter(lambda x:x.startswith(component_name+"."), self.list_components())
+        for sub in sub_components:
+            try:
+                packages.extend(self.get_union_component(sub).packages)
+            except Exception: #FIXME: what exception could we catch here, replace with that.
+                pass
 
-            for pkg in doc.tags("Source"):
-                if pkg.getTagData("PartOf") == component_name:
-                    component.sources.append(pkg.getTagData("Name"))
-
-        return component
-    
+        return packages
