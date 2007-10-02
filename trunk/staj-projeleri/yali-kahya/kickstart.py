43d150d5ef6975756763c924a6781bdba024917e
zey.sagar <zey.sagar@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2007-10-02 12:47:57 +0000

    yali-kickstart is now yali-kahya

--- trunk/staj-projeleri/yali-kahya/kickstart.py
+++ trunk/staj-projeleri/yali-kahya/kickstart.py
@@ -3,10 +3,9 @@
 
 import re
 import sys
-from yalireadpiks import *
-sys.path.append("/home/zeynep/svn/uludag/yali/yali")
-from localedata import *
-correctData=yaliKickstartData()
+import yali.yalireadpiks as yaliReadPiks
+from yali.localedata import *
+from yali.users import *
 
 class userErrors:
     def __init__(self):
@@ -14,6 +13,8 @@ class userErrors:
         self.Uname=False
         self.Rname=False
         self.Password=False
+        self.Groups=False
+
 class partitionErrors:
     def __init__(self):
         self.PartitionType=False
@@ -21,6 +22,7 @@ class partitionErrors:
         self.Disk=False
         self.FsType=False
         self.MountPoint=False
+
 class errors:
     def __init__(self):
         self.Keymap=False
@@ -31,14 +33,17 @@ class errors:
         self.Disk=False
         self.Root=False
         self.TotalRatio=False
+
 class userFunctions:
-    def __init__(self,username):
+    def __init__(self,username,groups,correctdata):
         self.username=username
+        self.groups=groups
+        self.correctData = correctdata
+
     def checkAutologin(self):
-        for currentUser in correctData.users:
-            if(currentUser.autologin=="yes"):
-                return True
-        return False
+        if(self.correctData.autoLoginUser):
+            return False
+        return True
 
     def checkValidity(self):
         if self.username and re.search("[0-9a-zA-Z.?!_-]",self.username):
@@ -46,20 +51,37 @@ class userFunctions:
         return False
 
     def checkName(self):
-        for usr in correctData.users:
+        for usr in self.correctData.users:
             if(usr.username==self.username):
                 return True
         return False
+
+    def checkGroups(self):
+        for element in self.groups:
+            for group in yaliKickStart().defaultGroups:
+                if (group==element or element=="wheel"):
+                    break
+            else:
+                return element
+        else:
+            return False
+
 class otherFunctions:
-    def __init__(self,key):
-        self.key=key
-    
-    def checkKeymap(self):
-        for element in yaliKickStart().KeymapXList:
-            if element==self.key:
+    def __init__(self,keyX):
+        self.keyX=keyX
+
+    def checkKeymapX(self):
+        for element in getKeymaps():
+            if element.X==self.keyX:
                 return True
-        return False       
-        
+        return False 
+
+    def findKeymap(self):
+        for element in getKeymaps():
+            if element.X==self.keyX:
+                return element.console
+        return False
+
 class partitionFunctions:
     def __init__(self,fs,disk):
         self.fs=fs
@@ -75,20 +97,35 @@ class partitionFunctions:
                 return True
         return False
     def checkDiskSyntax(self):
-        return re.match("[h,s]d(\D{1})[1-9]$",self.disk) 
+        return re.match("disk[0-9]p[1-9]$",self.disk) 
 
-class yaliKickStart():    
+    def checkDiskSyntax2(self):
+        return re.match("disk[0-9]$",self.disk) 
+
+
+    def convertDisk(self):
+        list={'a':'p0','b':'p1','c':'s0','d':'s1'}
+        self.letter=self.disk[2]
+        device=str(list[self.letter]+'p'+str(int(self.disk[3])-1))
+        print device
+        return device
+
+class yaliKickStart:
     def __init__(self):
-        self.KeymapXList=["us","ara","be","bg","es","hr","cz","dk","ee","fi","fr","de","gr","hu","is","it","jp","mkd","no","pl","pt","br","ru","srp","sk","si","se","trq","trf","ua","vn","gb"]
         self.fileSystems=["swap","ext3","ntfs","reiserf","xfs"]
         self.fileSystems2=["ext3","xfs"]
         self.defaultGroups=["audio","dialout","disk","pnp","pnpadmin","power","removable","users","video"]
         self.errorList=[]
         self.RatioList=[]
-        self.data=main("/home/zeynep/Desktop/yali3.xml")
+        self.correctData=yaliReadPiks.yaliKickstartData()
         self.total=0
-    
+
+    def readData(self,kickstartFile):
+        self.filePath = kickstartFile
+        self.data = yaliReadPiks.read(kickstartFile)
+
     def checkRatio(self):
+        """ It checks partition ratios """
         for eachRatio in self.data.partitioning:
             self.RatioList.append(eachRatio.ratio)
         for i in self.RatioList:
@@ -96,80 +133,66 @@ class yaliKickStart():
         if self.total==100:
             return True
         return False
-    def readData(self,kickstartFile):
-        self.filePath=kickstartFile
-        self.data=main(kickstartFile)
+
     def checkAllOptions(self):
         error=errors()
-        otherFunct=otherFunctions(self.data.keyData)
-        
+        otherFunct=otherFunctions(self.data.keyData.X)
+
         ###language selection###
-        
-        
         if(locales.has_key(self.data.language)):
-            correctData.language=self.data.language
+            self.correctData.language=self.data.language
         else:
             error.Lang=True
             self.errorList.append("Language Error: %s does not exist"%self.data.language)
-            
-            
-         ###keymap selection###
-        
-        
-        if(self.data.keyData):
-            if (otherFunct.checkKeymap()):
-                correctData.keyData=self.data.keyData
+
+        ###keymap selection###
+        if(self.data.keyData.X):
+            if (otherFunct.checkKeymapX()):
+                self.correctData.keyData.X=self.data.keyData.X
+                self.correctData.keyData.console=otherFunct.findKeymap()
             else:
                 error.Keymap=True
-                self.errorList.append("Keymap Error: %s not valid "%self.data.language)
+                self.errorList.append("Keymap Error: %s not valid "%self.data.keyData.X)
         else:
             if error.Lang!=True:
                 for lang in getLangsWithKeymaps():
-                    if(lang[0]==correctData.language):
-                        if(correctData.language=="tr"):
-                            correctData.keyData=lang[1][0].X
+                    if(lang[0]==self.correctData.language):
+                        if(self.correctData.language=="tr"):
+                            self.correctData.keyData.X=lang[1][0].X
+                            self.correctData.keyData.console=lang[1][0].console
                         else:
-                            correctData.keyData=lang[1].X
+                            self.correctData.keyData.X=lang[1].X
+                            self.correctData.keyData.console=lang[1].console
                         break
                 else:
                     error.Keymap=True
                     self.errorList.append("Keymap Error: Cannot associate Keymap for %s"%self.data.language)
-                        
-                        
+
         ###root password selection###
-        
-        
         if(len(self.data.rootPassword)<4):
             error.Root=True
             self.errorList.append("Root Password Error : Password is too short")
         else:
-            correctData.rootPassword=self.data.rootPassword
-            
-            
-        ###hostname selection###
-        
-        
-        if(self.data.hostname):
-            correctData.hostname=self.data.hostname
-        else:
-            correctData.hostname="pardus"
+            self.correctData.rootPassword=self.data.rootPassword
 
+        ###hostname selection###
+        if(self.data.hostname):
+            self.correctData.hostname=self.data.hostname
+        else:
+            self.correctData.hostname="pardus"
 
         ###users selections###
-
-    
         if(len(self.data.users)==0):
             error.Users=True
             self.errorList.append("User Error: No user entry")
         else:
-            correctData.users=[]
+            self.correctData.users=[]
             for user in self.data.users:
                 userError=userErrors()
-                checkFunctions=userFunctions(user.username)
-                if(user.autologin=="yes" and checkFunctions.checkAutologin()==0):
-                    user.autologin="yes"
-                else:
-                    user.autologin="no"
+                correctUser=User()
+                checkFunctions=userFunctions(user.username,user.groups,self.correctData)
+                if(user.autologin=="yes" and checkFunctions.checkAutologin()):
+                    self.correctData.autoLoginUser=user.username
                 if (user.username=="root" or checkFunctions.checkValidity()!=True or checkFunctions.checkName()==True):
                     userError.Uname=True
                     if (user.username and userError.Uname==True):
@@ -185,27 +208,31 @@ class yaliKickStart():
                     if userError.Uname!=True:
                         self.errorList.append("Password Error for %s "%user.username)
                 if len(user.groups)==0:
-                    user.groups=self.defaultGroups            
-                if(userError.Uname!=True and userError.Rname!=True and userError.Password!=True):
-                    correctData.users.append(user)
+                    user.groups=self.defaultGroups
+                elif (checkFunctions.checkGroups()):
+                    self.errorList.append("Groups Error for %s : %s group not valid"%(user.username,checkFunctions.checkGroups()))
+                    userError.Groups=True
+                if(userError.Uname!=True and userError.Rname!=True and userError.Password!=True and userError.Groups!=True):
+                    correctUser.username=user.username
+                    correctUser.realname=user.realname
+                    correctUser.passwd=user.password
+                    correctUser.groups=user.groups
+                    self.correctData.users.append(correctUser)
 
-        if (len(correctData.users)==0):
+        if (len(self.correctData.users)==0):
             error.Users=True
             self.errorList.append("User Error: No user added")
-        
-        
+
         ###partitioning selection###
-        
-        correctPart=yaliPartition()
+        correctPart=yaliReadPiks.yaliPartition()
         if(self.data.partitioningType=="auto" or self.data.partitioningType!="manuel"):
-            correctData.partitioningType="auto"
+            self.correctData.partitioningType="auto"
             if(len(self.data.partitioning)!=1):
                 error.Empty=True
                 self.errorList.append("Auto Partitioning Error : No partition entry  or too many partition")
             else:
                 PartiFunction=partitionFunctions(self.data.partitioning[0].fsType,self.data.partitioning[0].disk)
-                print self.data.partitioning[0].disk
-                if not PartiFunction.checkDiskSyntax():
+                if not PartiFunction.checkDiskSyntax2():
                     error.Disk=True
                     self.errorList.append("Auto Partitioning Error : Wrong Disk Syntax")
                 else:
@@ -213,12 +240,11 @@ class yaliKickStart():
                     correctPart.format="true"
                     correctPart.ratio="100"
                     correctPart.disk=self.data.partitioning[0].disk
-                    correctData.partitioning.append(correctPart)
-                    
-                             
+                    self.correctData.partitioning.append(correctPart)
+
         elif(self.data.partitioningType=="manuel"):
-            correctData.partitioningType="manuel"
-    
+            self.correctData.partitioningType="manuel"
+
             if(len(self.data.partitioning)==0):
                 error.Empty=True
                 self.errorList.append("Manuel Partitioning Error : No partition entry ")
@@ -257,19 +283,20 @@ class yaliKickStart():
                                 errorPartition.MountPoint=True
                                 self.errorList.append("Mountpoint Error for %s : %s not valid"%(partition.partitionType,partition.mountPoint))
                             if(errorPartition.PartitionType!=True and errorPartition.Disk!=True and errorPartition.FsType!=True and errorPartition.MountPoint!=True):
-                                correctData.partitioning.append(partition)
+                                partition.disk=functPart.convertDisk()
+                                self.correctData.partitioning.append(partition)
+
         return self.errorList
-    
+
     def checkFileValidity(self):
-        correctData=None
+        self.correctData=yaliReadPiks.yaliKickstartData()
         self.errorList=self.checkAllOptions()
         if(len(self.errorList)==0):
             return True
         return self.errorList
+
     def getValues(self):
         if self.checkFileValidity() == True:
-            print self.errorList
-            return correctData
+            return self.correctData
         return self.errorList
-    
 
