e30a5dfcf0075bb80f2d91c71860a9066fbc9d4f
ozan.caglayan <ozan.caglayan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2007-10-15 08:24:30 +0000

    added a new view

--- staj-projeleri/buildfarm/web/ciftci/views.py
+++ staj-projeleri/buildfarm/web/ciftci/views.py
@@ -1,9 +1,28 @@
-from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404, get_list_or_404
 from web.ciftci.models import Repository, Package
 
 # Helper module for repo operations
 import operations as op
+
+def sync_repositories(request):
+    # First we must get the POST data
+    try:
+        selectRepoSource = request.POST['selectRepoSource']
+        selectDestSource = request.POST['selectDestSource']
+
+    except KeyError:
+        # Redisplay the form.
+        pass
+
+    else:
+        source_path = get_object_or_404(Repository, repo_name=selectRepoSource).repo_path
+        dest_path = get_object_or_404(Repository, repo_name=selectDestSource).repo_path
+
+        # Returns a list of packages(with deps) which are in source_path but not in dest_path
+        pisi_list = op.getDifferences(source_path, dest_path)
+
+        return render_to_response('ciftci/ciftci_repodifferences.html',
+                                  {'pisi_list' : pisi_list})
 
 def list_repository(request, repo_name):
 
@@ -22,9 +41,7 @@ def list_repository(request, repo_name):
 
 def choose_repository(request):
 
-    repo_list = [x.repo_name for x in get_list_or_404(Repository)]
-
     return render_to_response('ciftci/ciftci_chooserepo.html',
-                              {'repo_list' : repo_list})
+                              {'repo_list' : [p.repo_name for p in get_list_or_404(Repository)]})
 
 
