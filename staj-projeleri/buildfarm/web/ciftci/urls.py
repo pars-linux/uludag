49526995c77e9f8b5b340b29b4e574e32fda53e7
ozan.caglayan <ozan.caglayan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2007-10-15 08:24:13 +0000

    new urlconf

--- staj-projeleri/buildfarm/web/ciftci/urls.py
+++ staj-projeleri/buildfarm/web/ciftci/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^choose/$', 'web.ciftci.views.choose_repository'),
+    (r'^sync/$', 'web.ciftci.views.sync_repositories'),
     (r'^list/$', 'web.ciftci.views.list_repository'),
     (r'^list/(?P<repo_name>.*)/$', 'web.ciftci.views.list_repository')
 )
