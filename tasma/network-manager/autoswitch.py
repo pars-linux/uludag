55a6f6fdb8590521223f6f5ded9310ff90ef3cd2
Gokmen Goksel <gokmen@goksel.me>
2007-10-17 09:39:35 +0000

    Ready for experimental tests with basic futures..

--- tasma/network-manager/autoswitch.py
+++ tasma/network-manager/autoswitch.py
@@ -22,31 +22,59 @@ def scanAndConnect():
         return
 
     # Get current APs
+    justEssIds = []
+    temp = None
     for dev in devices:
         # Some times we need to scan twice to get all access points
         for x in range(2):
             link.Net.Link['wireless-tools'].scanRemote(device=dev)
-        scanResults = map(lambda x: parseReply(x.split('\t')),link.read_cmd().data.split('\n'))
-
+        temp = link.read_cmd()
+        if temp.data:
+            scanResults = map(lambda x: parseReply(x.split('\t')),temp.data.split('\n'))
+            map(lambda x: justEssIds.append(x['remote']),scanResults)
+        else:
+            print 'No scan result'
+            return
+    
     # Clear the queue
     link.read_cmd()
 
     # Get profiles
     profiles = []
+
     link.Net.Link['wireless-tools'].connections()
     _profiles = link.read_cmd().data.split('\n')
     for profile in _profiles:
+
         # Get profile details
         link.Net.Link['wireless-tools'].connectionInfo(name=profile)
-        profiles.append(parseReply(link.read_cmd().data.split('\n')))
-        #print link.read_cmd().data.split('\n')
+        temp = parseReply(link.read_cmd().data.split('\n'))
 
-    # Clear the queue
-    #link.read_cmd()
+        # Add to list if in scanResults
+        if temp['remote'] in justEssIds:
+            profiles.append(temp)
+
+    print "Possible Profiles :"
+    for profile in profiles:
+        print profile
+
+    print
+
+    print "Scan Results :"
+    for res in scanResults:
+        print res
+
+    print
     
-    print profiles
-    print "-----------"
-    print scanResults
-    print "-----------"
+    # If there is one result let switch to it
+    if len(profiles)==1:
+        profile = profiles[0]['name']
+        print "Profile '%s' mathced." % profile
+        # And also if it's not connected yet.
+        if profiles[0]['state']=='down':
+            print "Connecting to '%s'..." % profile
+            link.Net.Link['wireless-tools'].setState(name=profile,state='up')
+        else:
+            print "Already connected."
 
 scanAndConnect()
