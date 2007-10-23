5645577b6b006a1bc00f0eddc41596a2a802a38b
faik <faik@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2007-10-23 06:40:54 +0000

    Add filesdb tests

--- branches/pisi-db/tests/database/filesdbtest.py
+++ branches/pisi-db/tests/database/filesdbtest.py
@@ -10,28 +10,50 @@
 # Please read the COPYING file.
 #
 
-import unittest
+import testcase
+import pisi
 
-class FilesDBTestCase(unittest.TestCase):
+class FilesDBTestCase(testcase.TestCase):
+
+    filesdb = pisi.db.filesdb.FilesDB()
 
     def testHasFile(self):
-        assert False
+        assert not self.filesdb.has_file("usr/bin/ethtool")
+        pisi.api.install(["ethtool"])
+        assert self.filesdb.has_file("usr/bin/ethtool")
+        pisi.api.remove(["ethtool"])
 
     def testGetFile(self):
-        assert False
+        pisi.api.install(["ethtool"])
+        pkg, path = self.filesdb.get_file("usr/bin/ethtool")
+        assert pkg == "ethtool"
+        assert path == "usr/bin/ethtool"
+        pisi.api.remove(["ethtool"]) 
 
-    def testGetNonExistingFile(self):
-        assert False
+    def testAddRemoveFiles(self):
+        fileinfo1 = pisi.files.FileInfo()
+        fileinfo1.path = "etc/pisi/pisi.conf"
+        fileinfo2 = pisi.files.FileInfo()
+        fileinfo2.path = "etc/pisi/mirrors.conf"
+        
+        files = pisi.files.Files()
+        files.list.append(fileinfo1)
+        files.list.append(fileinfo2)
 
-    def testAddFiles(self):
-        assert False
+        assert not self.filesdb.has_file("etc/pisi/pisi.conf")
+        assert not self.filesdb.has_file("etc/pisi/mirrors.conf")
 
-    def testAddExistingFiles(self):
-        assert False
+        self.filesdb.add_files("pisi", files)
 
-    def testRemoveFiles(self):
-        assert False
+        assert self.filesdb.has_file("etc/pisi/pisi.conf")
+        assert self.filesdb.has_file("etc/pisi/mirrors.conf")
 
-    def testRemoveNonExistingFiles(self):
-        assert False
+        pkg, path = self.filesdb.get_file("etc/pisi/pisi.conf")
+        assert pkg == "pisi"
 
+        # FIXME: inconsistency in filesdb.py add_remove and remove_remove parameters
+        self.filesdb.remove_files(files.list)
+
+        assert not self.filesdb.has_file("etc/pisi/pisi.conf")
+        assert not self.filesdb.has_file("etc/pisi/mirrors.conf")
+        
