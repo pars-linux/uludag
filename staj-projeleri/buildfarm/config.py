65ecc0c9b859d44441377278b1776b4e23700952
ozan.caglayan <ozan.caglayan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2007-10-24 11:17:13 +0000

    added deltaPath constant

--- staj-projeleri/buildfarm/config.py
+++ staj-projeleri/buildfarm/config.py
@@ -18,6 +18,7 @@ PORT = 443
 workDir          = "/var/pisi/"
 outputDir        = "/var/pisi/buildlogs/"
 binaryPath       = "/var/cache/pisi/packages/"
+deltaPath        = "/var/cache/pisi/delta/"
 localPspecRepo   = "./exampleRepo"      # must be an absolute path!
 logFile          = "%s/buildfarm.log" % workDir
 
