2e27848cef04cd79c36abede508a7494763c4764
gokmen.goksel <gokmen.goksel@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2007-12-11 10:49:27 +0000

    We have a problem Houston..

--- branches/yali-qt4/yali4.py
+++ branches/yali-qt4/yali4.py
@@ -13,24 +13,16 @@
 import sys
 from PyQt4 import QtCore, QtGui
 from uis.main import Ui_YaliMain
-from uis.welcomewidget import Ui_WelcomeWidget
 
-class yali4MainUi(QtGui.QMainWindow):
+class yali4MainUi(QtGui.QWidget):
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
-
-        # WTF ??
-        self.center = QtGui.QWidget(self)
-        layout = QtGui.QGridLayout(self.center)
-        self.setCentralWidget(self.center)
-
         self.ui = Ui_YaliMain()
-        self.ui.setupUi(self.center)
-        #self.mainWidget = Ui_WelcomeWidget()
+        self.ui.setupUi(self)
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
-    Yali = yali4MainUi()
-    Yali.show()
+    YaliMain = yali4MainUi()
+    YaliMain.show()
     sys.exit(app.exec_())
 
