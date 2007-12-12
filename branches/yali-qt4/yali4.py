4967847e08b39b897e6c16b96744b816f43811bd
gokmen.goksel <gokmen.goksel@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2007-12-12 21:37:29 +0000

    use StackedWidget now can navigate ;)

--- branches/yali-qt4/yali4.py
+++ branches/yali-qt4/yali4.py
@@ -14,15 +14,45 @@ import sys
 from PyQt4 import QtGui
 from PyQt4.QtCore import *
 from uis.main import Ui_YaliMain
+from uis.welcomewidget import *
+from uis.checkcdwidget import *
+from uis.keyboardwidget import *
+from uis.autopartwidget import *
+
+screens = [Ui_WelcomeWidget,
+           Ui_CheckCDWidget,
+           Ui_KeyboardWidget,
+           Ui_AutoPartWidget]
 
 class yali4MainUi(Ui_YaliMain):
     def __init__(self):
         self.ui = QtGui.QWidget()
         self.setupUi(self.ui)
         QObject.connect(self.buttonNext,SIGNAL("clicked()"),self.slotNext)
+        QObject.connect(self.buttonBack,SIGNAL("clicked()"),self.slotBack)
+        self.createWidgets()
+        self.mainStack.setCurrentIndex(0)
 
     def slotNext(self):
-        print "Yattaa !!"
+        self.stackMove(+1)
+
+    def slotBack(self):
+        self.stackMove(-1)
+
+    def stackMove(self,d):
+        new   = self.mainStack.currentIndex() + d
+        total = self.mainStack.count()
+        if new < 0: new = 0
+        if new > total: new = total
+        self.mainStack.setCurrentIndex(new)
+
+    def createWidgets(self):
+        self.mainStack.removeWidget(self.page)
+        for screen in screens:
+            _q = QtGui.QWidget()
+            _w = screen()
+            _w.setupUi(_q)
+            self.mainStack.addWidget(_q)
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
