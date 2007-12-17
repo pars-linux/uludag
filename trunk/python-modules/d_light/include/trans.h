3134c1039f796b3350e62351b6b7207ec60e9fc8
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2007-12-17 07:55:07 +0000

    move under Comar-dbus branch

--- trunk/python-modules/d_light/include/trans.h
+++
@@ -1,7 +0,0 @@
-#include <dbus/dbus.h>
-#include <Python.h>
-
-PyObject *dbus_py_get_list(DBusMessageIter *iter);
-PyObject *dbus_py_get_dict(DBusMessageIter *iter);
-PyObject *dbus_py_get_item(DBusMessageIter* iter);
-char *dbus_py_get_object_signature(PyObject *obj);
