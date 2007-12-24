eba69950812d9f01d28ce4705a3fe33c2815534a
gokcen.eraslan <gokcen.eraslan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2007-12-24 09:32:02 +0000

    sync from new snapshot of PyQt4

--- trunk/dbus-pyqt3-mainloop/src/mainloop.h
+++ trunk/dbus-pyqt3-mainloop/src/mainloop.h
@@ -1,24 +1,20 @@
 #ifndef QDBUSCONNECTION_P_H
 #define QDBUSCONNECTION_P_H
 
-#include <qguardedptr.h>
 #include <qmap.h>
+#include <qptrlist.h>
 #include <qobject.h>
 #include <qvaluelist.h>
 
 #include <dbus/dbus.h>
-
 #include "qdbusatomic.h"
-//#include "dbus/qdbuserror.h"
-//#include "dbus/qdbusobject.h"
 
-//class QDBusMessage;
 class QSocketNotifier;
 class QTimer;
 class QTimerEvent;
 
-typedef struct DBusConnection;
-typedef struct DBusServer;
+//typedef struct DBusConnection;
+//typedef struct DBusServer;
 
 class QDBusConnectionPrivate: public QObject
 {
@@ -26,29 +22,11 @@ class QDBusConnectionPrivate: public QObject
 public:
     QDBusConnectionPrivate(QObject *parent = 0);
     ~QDBusConnectionPrivate();
-
-//    void bindToApplication();
-
-//    void setConnection(DBusConnection *connection);
-//    void setServer(DBusServer *server);
-//    void closeConnection();
     void timerEvent(QTimerEvent *e);
-
-//    bool handleSignal(DBusMessage *msg);
-//    bool handleObjectCall(DBusMessage *message);
-//    bool handleError();
-
-//    void emitPendingCallReply(const QDBusMessage& message);
-
-//signals:
-//    void dbusSignal(const QDBusMessage& message);
-//    void dbusPendingCallReply(const QDBusMessage& message);
 
 public slots:
     void socketRead(int);
     void socketWrite(int);
-
-//    void objectDestroyed(QObject* object);
 
     void purgeRemovedWatches();
 
@@ -56,22 +34,12 @@ public slots:
     void dispatch();
 
 public:
-//    DBusError error;
-//    QDBusError lastError;
-
     enum ConnectionMode { InvalidMode, ServerMode, ClientMode };
 
-    // FIXME QAtomic ref;
     Atomic ref;
-    //ConnectionMode mode;
     DBusConnection *connection;
-//    DBusServer *server;
 
     QTimer* dispatcher;
-
-//    static int messageMetaType;
-//    static int registerMessageMetaType();
-//    int sendWithReplyAsync(const QDBusMessage &message, QObject *receiver, const char *method);
     void flush();
 
     struct Watcher
@@ -86,25 +54,14 @@ public:
     WatcherList removedWatches;
     typedef QMap<int, WatcherList> WatcherHash;
     WatcherHash watchers;
+    typedef QPtrList<DBusConnection>Connections;
+    Connections connections;
 
     typedef QMap<int, DBusTimeout*> TimeoutHash;
     TimeoutHash timeouts;
 
-//    typedef QMap<QString, QDBusObjectBase*> ObjectMap;
-//    ObjectMap registeredObjects;
-
     QValueList<DBusTimeout *> pendingTimeouts;
 
-/*    struct QDBusPendingCall
-    {
-        QGuardedPtr<QObject> receiver;
-        QCString method;
-        DBusPendingCall *pending;
-    };
-
-    typedef QMap<DBusPendingCall*, QDBusPendingCall*> PendingCallMap;
-    PendingCallMap pendingCalls;
-*/
 };
 
 #endif
