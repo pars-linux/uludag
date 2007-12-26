7eac3152ff00bfbd4127752b7d8f3b9da062e086
gokmen.goksel <gokmen.goksel@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2007-12-26 08:23:34 +0000

    a little bit.

--- branches/yali-qt4/partEdit.py
+++ branches/yali-qt4/partEdit.py
@@ -4,10 +4,14 @@
 from PyQt4 import QtCore, QtGui
 
 class DiskList(QtGui.QWidget):
-     def __init__(self, name):
+    def __init__(self, *args):
         QtGui.QWidget.__init__(self,None)
-        self.resize(QtCore.QSize(QtCore.QRect(0,0,778,80).size()).expandedTo(self.minimumSizeHint()))
+        self.resize(QtCore.QSize(QtCore.QRect(0,0,778,100).size()).expandedTo(self.minimumSizeHint()))
         self.setStyleSheet("""
+            QToolBox::tab { background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,
+                                                        stop: 0 #E1E1E1, stop: 0.4 #DDDDDD,
+                                                        stop: 0.5 #D8D8D8, stop: 1.0 #D3D3D3);
+                            border-radius: 5px; }
             QRadioButton::indicator { width:1px;height:1px;border-color:white; }
             QRadioButton:checked { border:3px solid #777 }
             QSplitter::handle { background-color:white; }
@@ -17,7 +21,7 @@ class DiskList(QtGui.QWidget):
         self.gridlayout.addWidget(self.toolBox,0,0,1,1)
 
     def addDisk(self,dw):
-        pass
+        self.toolBox.addItem(dw,dw.name)
 
 class DiskWidget(QtGui.QWidget):
     def __init__(self, name):
@@ -25,7 +29,7 @@ class DiskWidget(QtGui.QWidget):
         self.resize(QtCore.QSize(QtCore.QRect(0,0,778,80).size()).expandedTo(self.minimumSizeHint()))
         self.layout = QtGui.QGridLayout(self)
         self.diskGroup = QtGui.QGroupBox(self)
-        self.diskGroup.setMinimumSize(QtCore.QSize(0,80))
+        self.diskGroup.setMinimumSize(QtCore.QSize(0,50))
         self.gridlayout = QtGui.QGridLayout(self.diskGroup)
         self.gridlayout.setMargin(0)
         self.gridlayout.setSpacing(0)
@@ -45,9 +49,19 @@ class DiskWidget(QtGui.QWidget):
 if __name__ == "__main__":
     import sys
     app = QtGui.QApplication(sys.argv)
-    partList = DiskWidget("/dev/sda")
-    partList.addPartition("/dev/sda1",{"size":"50GB"})
-    partList.addPartition("/dev/sda1",{"size":"50GB"})
-    partList.show()
+    diskList = DiskList()
+
+    disk1 = DiskWidget("/dev/sda")
+    disk1.addPartition("/dev/sda1",{"size":"50GB"})
+    disk1.addPartition("/dev/sda2",{"size":"50GB"})
+    diskList.addDisk(disk1)
+
+    disk2 = DiskWidget("/dev/sdb")
+    disk2.addPartition("/dev/sdb1",{"size":"10GB"})
+    disk2.addPartition("/dev/sdb2",{"size":"50GB"})
+    disk2.addPartition("/dev/sdb3",{"size":"10GB"})
+    diskList.addDisk(disk2)
+
+    diskList.show()
     sys.exit(app.exec_())
 
