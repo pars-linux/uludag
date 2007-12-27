2c14cafe464cc7cd201625707a81617b2b13321d
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2007-12-27 08:26:42 +0000

    _() method not implemented yet

--- branches/comar-dbus/tests/scripts/Boot_Modules.py
+++ branches/comar-dbus/tests/scripts/Boot_Modules.py
@@ -15,6 +15,8 @@ TIMEOUT = 5.0
 
 # l10n
 
+_ = lambda x: x["tr"]
+
 FAIL_TIMEOUT = _({
     "en": "Request timed out. Try again later.",
     "tr": "Talep zaman aşımına uğradı. Daha sonra tekrar deneyin.",
