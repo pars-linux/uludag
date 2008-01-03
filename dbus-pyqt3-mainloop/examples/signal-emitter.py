78169d468c4076128e476e22b72f9172f09b4717
gokcen.eraslan <gokcen.eraslan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-01-03 07:22:35 +0000

    update service script to use qt mainloop

--- dbus-pyqt3-mainloop/examples/signal-emitter.py
+++ dbus-pyqt3-mainloop/examples/signal-emitter.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
-import gobject
-
 import dbus
 import dbus.service
-import dbus.mainloop.glib
+import dbus.mainloop.qt3
+
+from qt import *
+
+import sys
 
 class TestObject(dbus.service.Object):
     def __init__(self, conn, object_path='/com/example/TestService/object'):
@@ -22,19 +24,17 @@ class TestObject(dbus.service.Object):
         self.HelloSignal('Hello')
         return 'Signal emitted'
 
-    @dbus.service.method("com.example.TestService",
-                         in_signature='', out_signature='')
+    @dbus.service.method("com.example.TestService", in_signature='', out_signature='')
     def Exit(self):
         loop.quit()
 
 if __name__ == '__main__':
-    dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+    dbus.mainloop.qt3.DBusQtMainLoop(set_as_default=True)
 
     session_bus = dbus.SessionBus()
     name = dbus.service.BusName('com.example.TestService', session_bus)
     object = TestObject(session_bus)
 
-    loop = gobject.MainLoop()
+    loop = QApplication(sys.argv)
     print "Running example signal emitter service."
-    #print usage
-    loop.run()
+    sys.exit(loop.exec_loop())
