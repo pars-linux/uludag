c5fb1c7812c10bcf4582ef176400b3689585e1b4
caglar <caglar@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-01-04 19:52:28 +0000

    listen gürersans :)

--- branches/comar-dbus/comar/src/data.c
+++ branches/comar-dbus/comar/src/data.c
@@ -56,14 +56,12 @@ db_init(void)
     if (stat(code_dir, &fs) != 0) {
         if (0 != mkdir(code_dir, S_IRWXU)) {
             log_error("Cannot create code dir '%s'\n", code_dir);
-            free(code_dir);
             return -1;
         }
     }
     else {
         if (0 != access(code_dir, W_OK)) {
             log_error("Cannot write code dir '%s'\n", code_dir);
-            free(code_dir);
             return -1;
         }
     }
