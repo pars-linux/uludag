110a8a5d58060a191b1f323fc4711e5a8c3d3812
caglar <caglar@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-02-08 20:39:44 +0000

    create a devel branch for -devel pisi

---
+++ branches/buildfarm-devel/config.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2006, TUBITAK/UEKAE
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Please read the COPYING file.
+import os
+
+#Some configuration info for other modules..
+workDir          = "/var/pisi/"
+outputDir        = "/var/cache/pisi/buildlogs/"
+binaryPath       = "/var/cache/pisi/packages/"
+localPspecRepo   = "%s/exampleRepo" % os.getcwd()
+logFile          = "/var/cache/pisi/buildfarm.log"
+
+#information for mailer module.
+mailFrom         = "buildfarm@pardus.org.tr"
+announceAddr     = "gelistirici@pardus.org.tr"
+ccList           = []
+smtpServer       = "mail.pardus.org.tr"
+useSmtpAuth      = False
+smtpUser         = ""
+smtpPassword     = ""
