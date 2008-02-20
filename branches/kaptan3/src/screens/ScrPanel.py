f1daa9a6f90c687c7a583b10c0eb242db7643a67
pinar <pinar@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-02-20 08:14:37 +0000

    kicker update

--- branches/kaptan3/src/screens/ScrPanel.py
+++ branches/kaptan3/src/screens/ScrPanel.py
@@ -15,6 +15,8 @@ import qtxml
 from kdecore import *
 from kdeui import *
 import kdedesigner
+import dcop
+import kdecore
 
 from screens.Screen import ScreenWidget
 from screens.paneldlg import PanelWidget
@@ -25,6 +27,7 @@ class Widget(PanelWidget, ScreenWidget):
     title = "Configure your panel !"
     desc = "Select the one you like..."
     selectedStyle= QString()
+    testedStyle = 0
     def __init__(self, *args):
         apply(PanelWidget.__init__, (self,) + args)
 
@@ -57,9 +60,25 @@ class Widget(PanelWidget, ScreenWidget):
         dom.setContent(file.readAll())
         file.close()
 
-        client = DCOPClient(kapp.dcopClient())
-        if not client.isAttached():
-            client.attach()
+        client = kdecore.KApplication.dcopClient()
+        #if not client.isAttached():
+        client.attach()
+
+        kickerConf = KConfig("kickerrc")
+        kickerConf.setGroup("General")
+
+        Kicker = qtxml.QDomElement
+        Kicker = dom.elementsByTagName("kicker").item(0).toElement()
+
+        # kickerConf.writeEntry("LegacyKMenu", checkKickoff.isChecked())
+        kickerConf.writeEntry("Transparent", self.getProperty(Kicker, "Transparent", "value"))
+        kickerConf.writeEntry("SizePercentage", self.getProperty(Kicker, "SizePercentage", "value"))
+        kickerConf.writeEntry("CustomSize", self.getProperty(Kicker, "CustomSize", "value"))
+        kickerConf.writeEntry("Position", self.getProperty(Kicker, "Position", "value"))
+        kickerConf.writeEntry("Alignment", self.getProperty(Kicker, "Alignment", "value"))
+        kickerConf.sync()
+
+        client.send("kicker", "kicker", "restart()", "")
 
         kwinConf = KConfig("kwinrc")
         kwinConf.setGroup("Style")
@@ -67,12 +86,33 @@ class Widget(PanelWidget, ScreenWidget):
         KWin =  qtxml.QDomElement
         KWin = dom.elementsByTagName("kwin").item(0).toElement()
 
-        kwinConf.writeEntry("PluginLib", getProperty(KWin, "PluginLib", "value"))
+        kwinConf.writeEntry("PluginLib", self.getProperty(KWin, "PluginLib", "value"))
         kwinConf.sync()
         
         client.send("kwin", "KWinInterface", "reconfigure()", "")
+    
+        globalConf = KConfig("kdeglobals")
 
+        globalConf.setGroup("General")
 
+        Widget = qtxml.QDomElement
+        Widget = dom.elementsByTagName("widget").item(0).toElement()
+
+        globalConf.writeEntry("widgetStyle", self.getProperty(Widget, "widgetStyle", "value"))
+        globalConf.sync()
+    
+        KIPC.sendMessageAll(KIPC.StyleChanged)
+        
+
+    def getProperty(self, parent,tag, attr):
+        pList = qtxml.QDomNodeList
+        pList = parent.elementsByTagName(tag)
+        if pList.count():
+            return  pList.item(0).toElement().attribute(attr)
+        else:
+            return
+
+    
     def kickoffSelected(self):
         pass
 
@@ -82,8 +122,6 @@ class Widget(PanelWidget, ScreenWidget):
         previewPath = QString
         
         previewPath = KGlobal.dirs().findResourceDir("themes", "/" ) + name + "/" + name + ".preview.png"
-        print previewPath
-
 
         xmlFile = QString
         xmlFile = KGlobal.dirs().findResourceDir("themes", "/" ) + name + "/" + name + ".xml"
