d6c783ee7d60661a013c4fc83c6a354fd9f560f0
pinar <pinar@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-02-22 00:15:57 +0000

    mouse maps

--- branches/kaptan3/src/screens/ScrMouse.py
+++ branches/kaptan3/src/screens/ScrMouse.py
@@ -14,6 +14,8 @@ from qt import *
 from kdecore import *
 from kdeui import *
 import kdedesigner
+from Xlib import display
+
 
 from screens.Screen import ScreenWidget
 from screens.mousedlg import MouseWidget
@@ -26,7 +28,12 @@ class Widget(MouseWidget, ScreenWidget):
 
     def __init__(self, *args):
         apply(MouseWidget.__init__, (self,) + args)
+        self.pix_mouse.setPixmap(QPixmap(locate("data", "kaptan/pics/mouse_rh.png")))
+        #self.pix_mouse.scaled(190,100)
+        #self.pix_mouse.resize(190,100)
+        #self.setMinimumSize( QSize( 190, 100 ) )
         self.connect(self.singleClick, SIGNAL("toggled(bool)"),self.setClickBehaviour)
+        self.connect(self.rightHanded, SIGNAL("toggled(bool)"), self.setHandedness)
 
     def shown(self):
         pass
@@ -40,3 +47,44 @@ class Widget(MouseWidget, ScreenWidget):
         config.writeEntry("SingleClick", self.singleClick.isChecked())
         config.sync()
         KIPC.sendMessageAll(KIPC.SettingsChanged, KApplication.SETTINGS_MOUSE)
+
+    def setHandedness(self):
+        if self.rightHanded.isChecked():
+            map = {}
+            handed = "RIGHT_HANDED"
+            map= display.Display().get_pointer_mapping()
+            num_buttons = len(map)
+
+            if num_buttons == 1:
+                map[0] = 1
+            elif num_buttons == 2:
+                if handed == "RIGHT_HANDED":
+                    map[0], map[1] = 1,3
+                else:
+                    map[0],map[1]= 3,1
+            else:
+                if handed == "RIGHT_HANDED":
+                    map[0],map[2] = 1,3
+                else:
+                    map[0],map[2] = 3,1
+            if num_buttons >=5:
+                print type(num_buttons)
+                pos = 0
+                for pos in range(num_buttons):
+                    if map[pos] == 4 or map[pos] == 5:
+                        break
+
+                #check reverse
+
+            #while(retVal = len(display.Display().get_pointer_mapping()))==MappingBusy):
+            self.pix_mouse.setPixmap(QPixmap(locate("data", "kaptan/pics/mouse_rh.png")))
+            config = KConfig("kcminputrc")
+            config.setGroup("Mouse")
+            config.writeEntry("MouseButtonMapping", QString("RightHanded"))
+            config.sync()
+            KIPC.sendMessageAll(KIPC.SettingsChanged, KApplication.SETTINGS_MOUSE)
+
+        else:
+            self.pix_mouse.setPixmap(QPixmap(locate("data", "kaptan/pics/mouse_lh.png")))
+
+
