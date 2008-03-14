c58ca2d52e306e939dbe4995066acd1f03c2082f
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-03-14 07:12:35 +0000

    BUG:FIXED:6866

--- trunk/tasma/network-manager/connection.py
+++ trunk/tasma/network-manager/connection.py
@@ -37,7 +37,9 @@ class ScanItem(QListViewItem):
         self.info = {}
         
         if not data:
-            self.setText(0, i18n("No remotes found"))
+            self.setPixmap(0, getIconSet("remove", KIcon.Small).pixmap(QIconSet.Automatic, QIconSet.Normal))
+            self.setText(1, "")
+            self.setText(2, i18n("No remotes found"))
             return
         
         for param in data.split("\t"):
