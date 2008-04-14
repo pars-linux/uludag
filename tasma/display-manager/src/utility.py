cdf22aafafdd11479c881b4ff744ec8201cddf97
Fatih Aşıcı <fatih-asici@users.noreply.github.com>
2008-04-14 12:06:59 +0000

    initial commit

---
+++ tasma/display-manager/src/utility.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2007, TUBITAK/UEKAE
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+
+from qt import *
+from kdecore import *
+from kdeui import *
+
+def I18N_NOOP(str):
+    return str
+
+def getIconSet(name, group=KIcon.Toolbar):
+    return KGlobal.iconLoader().loadIconSet(name, group)
+
