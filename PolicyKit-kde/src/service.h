f2deff65157f2cb13a98e95c0861bcab0b799a4f
gokcen.eraslan <gokcen.eraslan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-04-14 12:18:04 +0000

    add polkit-grant's watch functions

--- PolicyKit-kde/src/service.h
+++ PolicyKit-kde/src/service.h
@@ -2,6 +2,8 @@
 #define SERVICE_H
 
 #include <polkit/polkit.h>
+#include <polkit-grant/polkit-grant.h>
+
 #include <qobject.h>
 #include <qmap.h>
 #include <qsocketnotifier.h>
@@ -26,22 +28,30 @@ protected:
 
 protected slots:
     void slotBusNameOwnerChanged(const QDBusMessage& msg);
-    void watchActivated(int fd);
+    void contextWatchActivated(int fd);
+    void grantWatchActivated(int fd);
 
 private:
     QDBusConnection m_sessionBus;
     QDBusConnection m_systemBus;
 
     PolKitContext *m_context;
+    PolKitGrant *m_grant;
     PolKitError *m_error;
 
     bool m_authInProgress;
     static PolicyService* m_self;
 
-    QMap<int, QSocketNotifier*> m_watches;
+    QMap<int, QSocketNotifier*> m_contextwatches;
+    QMap<int, QSocketNotifier*> m_grantwatches;
 
-    static int polkit_add_watch(PolKitContext *context, int fd);
-    static void polkit_remove_watch(PolKitContext *context, int fd);
+    static int polkit_context_add_watch(PolKitContext *context, int fd);
+    static void polkit_context_remove_watch(PolKitContext *context, int fd);
+
+    // functions required by polkit_grant_set_functions
+    static int polkit_grant_add_watch(PolKitGrant *grant, int fd);
+    static int polkit_grant_add_child_watch(PolKitGrant *grant, pid_t pid);
+    static void polkit_grant_remove_watch(PolKitGrant *grant, int fd);
 };
 
 #endif
