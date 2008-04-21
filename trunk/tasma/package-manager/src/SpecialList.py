a8ed4e597336eb8a0a6d45aa223f012357d6a6c6
faik <faik@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-04-21 08:51:25 +0000

    QString -> str

--- trunk/tasma/package-manager/src/SpecialList.py
+++ trunk/tasma/package-manager/src/SpecialList.py
@@ -27,9 +27,9 @@ class SpecialList(QObject):
 
         # Read javascript
         js = file(str(locate("data","package-manager/animation.js"))).read()
-        js = re.sub("#3cBB39", KGlobalSettings.alternateBackgroundColor().name(), js)
-        js = re.sub("#3c8839", KGlobalSettings.baseColor().name(), js)
-        self.javascript = re.sub("#533359",KGlobalSettings.highlightColor().name(), js)
+        js = re.sub("#3cBB39", str(KGlobalSettings.alternateBackgroundColor().name()), js)
+        js = re.sub("#3c8839", str(KGlobalSettings.baseColor().name()), js)
+        self.javascript = re.sub("#533359",str(KGlobalSettings.highlightColor().name()), js)
 
         # Read Css
         cssFile = file(str(locate("data","package-manager/layout.css"))).read()
