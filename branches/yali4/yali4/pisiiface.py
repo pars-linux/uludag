5a01874b62c820b904b0de0027c0865ffe6b8d3f
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-04-21 17:58:23 +0000

    Set dbus timeout to 1200
    pisi-devel r17764 required.

--- branches/yali4/yali4/pisiiface.py
+++ branches/yali4/yali4/pisiiface.py
@@ -36,6 +36,7 @@ def initialize(ui, with_comar = False, nodestDir = False):
         except dbus.DBusException:
             time.sleep(1)
     pisi.api.set_dbus_sockname("%s/var/run/dbus/system_bus_socket" % options.destdir)
+    pisi.api.set_dbus_timeout(1200)
     pisi.api.set_userinterface(ui)
     pisi.api.set_options(options)
     pisi.api.set_comar(with_comar)
