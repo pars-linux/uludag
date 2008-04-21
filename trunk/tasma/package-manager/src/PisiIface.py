96bcb8794383ea7d72b28f50f1403823b24bce32
faik <faik@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-04-21 07:49:09 +0000

    use PisiIface

--- trunk/tasma/package-manager/src/PisiIface.py
+++ trunk/tasma/package-manager/src/PisiIface.py
@@ -23,6 +23,15 @@ def get_upgrade_order(packages):
     base = pisi.api.get_base_upgrade_order(packages)
     return pisi.api.get_upgrade_order(set(base+packages))
 
+def get_union_component_packages(name, walk=True):
+    return pisi.db.componentdb.ComponentDB().get_union_packages(name, walk)
+
+def get_union_component(name):
+    return pisi.db.componentdb.ComponentDB().get_union_component(name)
+
+def get_components():
+    return pisi.db.componentdb.ComponentDB().list_components()
+
 def get_installed_package(package):
     return pisi.db.installdb.InstallDB().get_package(package)
 
@@ -43,3 +52,15 @@ def parse_package_name(name):
 
 def read_config(name):
     return pisi.configfile.ConfigurationFile(name)
+
+def is_component_visible(name):
+    cdb = pisi.db.componentdb.ComponentDB()
+    return cdb.get_component(name).visibleTo == 'user'
+
+def reloadPisi(self):
+    for module in sys.modules.keys():
+        if module.startswith("pisi."):
+            """removal from sys.modules forces reload via import"""
+            del sys.modules[module]
+
+    reload(pisi)
