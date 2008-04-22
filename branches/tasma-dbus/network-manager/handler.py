afdfc086fa3eb2007f67b64b1e951bffa03d84ae
gokcen.eraslan <gokcen.eraslan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-04-22 20:41:04 +0000

    policykit-kde varken gnome'u neden çağırıyoruz demi Bahadır?

--- branches/tasma-dbus/network-manager/handler.py
+++ branches/tasma-dbus/network-manager/handler.py
@@ -121,7 +121,7 @@ class CallHandler:
     
     def __getAuthIface(self):
         try:
-            obj = self.busSes.get_object("org.gnome.PolicyKit", "/")
+            obj = self.busSes.get_object("org.freedesktop.PolicyKit.AuthenticationAgent", "/")
             return dbus.Interface(obj, "org.freedesktop.PolicyKit.AuthenticationAgent")
         except dbus.DBusException, e:
             for func, args in self.handleDBusError.iteritems():
