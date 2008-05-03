e6fbaeb0617b0ba2d3dd35f96ca60264c601d9c3
fatih <fatih@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-05-03 23:17:51 +0000

    support fglrx

--- trunk/tasma/display-manager/src/displayconfig.py
+++ trunk/tasma/display-manager/src/displayconfig.py
@@ -19,6 +19,23 @@ class ComarLink:
 
 link = ComarLink()
 
+def fglrxOutputInfo():
+    connected_outputs = []
+    enabled_outputs = []
+
+    out, err = capture("aticonfig", "--query-monitor")
+
+    lines = out.splitlines()
+    for line in lines:
+        if "Connected monitors" in line:
+            outputs = line.split(": ")[1]
+            connected_outputs = outputs.split(", ")
+        elif "Enabled monitors" in line:
+            outputs = line.split(": ")[1]
+            enabled_outputs = outputs.split(", ")
+
+    return connected_outputs, enabled_outputs
+
 class DisplayConfig:
     def __init__(self):
         self._rriface = randriface.RandRIface()
@@ -26,12 +43,13 @@ class DisplayConfig:
         self._bus = link.activeDeviceID()
         self._info = zorg.config.getDeviceInfo(self._bus)
 
-        self._flags = self._info.probe_result.get("flags", "").split(",")
-        self._randr12 = "randr12" in self._flags
-
         self.outputs = self._info.probe_result["outputs"].split(",")
         self.modes = {}
         self.current_modes = {}
+
+        self._flags = self._info.probe_result.get("flags", "").split(",")
+        #self._randr12 = "randr12" in self._flags
+        self._randr12 = len(self._rriface.outputs) > 1
 
         if self._randr12:
             for output in self.outputs:
@@ -42,8 +60,21 @@ class DisplayConfig:
                 self.current_modes[output] = current if current else self.modes[output][0]
 
         else:
+            if self._info.driver == "fglrx":
+                connected_outputs, enabled_outputs = fglrxOutputInfo()
+
+                for out in connected_outputs:
+                    if out not in self.outputs:
+                        self.outputs.append(out)
+
             for output in self.outputs:
-                pass
+                if self._info.probe_result.has_key("%s-modes" % output):
+                    modes = self._info.probe_result["%s-modes" % output].split(",")
+                else:
+                    modes = ["1024x768", "800x600", "640x480"]
+
+                self.modes[output] = modes
+                self.current_modes[output] = self._info.modes.get(output, "800x600")
 
         self.primaryScr = self._info.active_outputs[0]
         self.secondaryScr = None
