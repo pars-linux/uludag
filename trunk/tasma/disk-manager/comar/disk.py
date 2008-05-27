c0c3f5a5ce591ced3e0309ed7c47ad0e090d0f75
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-05-27 20:56:02 +0000

    getDevices() method

--- trunk/tasma/disk-manager/comar/disk.py
+++ trunk/tasma/disk-manager/comar/disk.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
 
+import fnmatch
 import glob
 import os
 
 def getDevices():
     if not os.path.exists("/sys/block"):
         return fail("sysfs not found!")
-    return glob.glob("/sys/block/*")
+
+    devices = []
+    for sysfs_dev in os.listdir("/sys/block"):
+        if filter(lambda x: fnmatch.fnmatch(sysfs_dev, x), ["fd*", "loop*", "ram*", "sr*"]):
+            continue
+        dev_name = os.path.basename(sysfs_dev)
+        dev_name = dev_name.replace("!", "/")
+        devices.append("/dev/" + dev_name)
+    devices.sort()
+    return devices
 
 def getDeviceByLabel(label):
     return
