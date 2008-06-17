6c5b1561cf87addfa6b13486a99cc1e4fe669ff9
ozan <ozan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-06-17 14:11:56 +0000

    Fixed a schroedinbug about threading.

--- trunk/gsoc/notifier/notman.py
+++ trunk/gsoc/notifier/notman.py
@@ -13,7 +13,6 @@ import dbus
 import dbus.service
 # Import Qt main loop stuff:
 from dbus.mainloop.qt import DBusQtMainLoop
-DBusQtMainLoop(set_as_default = True)
 import PyQt4.QtCore
 from PyQt4.QtGui import QApplication
 
@@ -46,26 +45,20 @@ class Timer(QtCore.QThread):
 				break
     
 class NotificationManager(QApplication):
-    def __init__(self,  object_path):
+    def __init__(self):
         QApplication.__init__(self,  sys.argv)
         print _("Initializing notification manager...")
         # We don't want the program to get closed when there is no visible notificaition window:
         self.setQuitOnLastWindowClosed(False)
-        self.object_path = object_path
-        self.session_bus = dbus.SessionBus()
-        self.bus_name = dbus.service.BusName("org.pardus.notificationmanager", self.session_bus)
         # Initialize the pending message queue:
         self.message_queue = []
         self.lifespan = 15.0
         self.last_notification_time = time.time()
         # Initialize the timer:
         self.timer = Timer(self)
-        # Initialize the notification displayer (spawned in a new thread):
+        # Initialize the notification displayer:
         self.notification_displayer = NotificationDisplayer()
-        # Signal handlers:
-        self.connect(self, QtCore.SIGNAL("handleEvent()"), self.HandleNotification)
-        self.connect(self, QtCore.SIGNAL("exitProgram()"), self.Die)
-    
+        
     def Die(self):
         QtCore.QTimer().singleShot(1000,  quit)
     
@@ -82,12 +75,18 @@ class NotificationManager(QApplication):
             self.notification_displayer.DisplayNotification(self.message_queue[0])
             self.message_queue = self.message_queue[1:]
             print _("Queue state: %s") % self.message_queue
+    
+    def SetSignalHandlers(self, signal_source):
+		# Signal handlers:
+		self.connect(signal_source, QtCore.SIGNAL("handleEvent()"), self.HandleNotification)
+		self.connect(signal_source, QtCore.SIGNAL("exitProgram()"), self.Die)
 
 class NotXFace(dbus.service.Object):
-    def __init__(self, notification_manager):
+    def __init__(self, notification_manager, notxfacethread):
+        dbus.service.Object.__init__(self, notxfacethread.bus_name, notxfacethread.object_path)
         print _("Initializing / exporting NotXFace object...")
-        dbus.service.Object.__init__(self, notification_manager.bus_name, notification_manager.object_path)
         self.notification_manager = notification_manager
+        self.notxfacethread = notxfacethread
         
     @dbus.service.method(dbus_interface = "org.pardus.notmanxface", in_signature = "s", out_signature = "s", sender_keyword = "sender")
     def AddNotification(self, serialized_notification, sender = None):
@@ -109,7 +108,7 @@ class NotXFace(dbus.service.Object):
 			print _("Received message text: %s") % notification.notification_text
 			print _("Added notification to queue.")
 			# Handle the notification in the other thread (in case handling the notification takes too much time, we dont want to stall the requestor):
-			self.notification_manager.emit(QtCore.SIGNAL("handleEvent()"))
+			self.notxfacethread.emit(QtCore.SIGNAL("handleEvent()"))
 			# Update the last notification timestamp of the notification manager:
 			self.notification_manager.last_notification_time = time.time()
 			# Return the notification to the requestor, acknowledging that the notification is received:
@@ -126,22 +125,32 @@ class NotXFace(dbus.service.Object):
     def Exit(self, sender = None):
         print _("Sender's bus name: %s") % sender
         print _("Exit method called, exiting service...")
-        self.notification_manager.emit(QtCore.SIGNAL("exitProgram()"))
+        self.notxfacethread.emit(QtCore.SIGNAL("exitProgram()"))
  
 class NotXFaceThread(QtCore.QThread):
-    def __init__(self,  notification_manager):
+    def __init__(self, notification_manager, object_path):
         QtCore.QThread.__init__(self)
         self.notification_manager = notification_manager
+        self.object_path = object_path
     
     def run(self):
-        self.exported_obj = NotXFace(self.notification_manager)
+    	DBusQtMainLoop(set_as_default = True)
+        self.session_bus = dbus.SessionBus()
+        self.bus_name = dbus.service.BusName("org.pardus.notificationmanager", self.session_bus)
+        self.exported_obj = NotXFace(self.notification_manager, self)
         self.exec_()
 
 # If executed as the main program:
 if __name__ == "__main__":
-	notification_manager = NotificationManager("/NotificationManager")
-	notxfacethread = NotXFaceThread(notification_manager)
+	# Create the notification manager object (which is a QApplication that manages the GUI thread):
+	notification_manager = NotificationManager()
+	# Create the thread containing the notification manager DBus interface object:
+	notxfacethread = NotXFaceThread(notification_manager, "/NotificationManager")
+	# Set the signal handlers of the main thread so that it can receive signals from the DBus interface thread:
+	notification_manager.SetSignalHandlers(notxfacethread)
+	# Launch up the DBus interface thread:
 	notxfacethread.start()
+	# Launch up the main (GUI) thread:
 	notification_manager.Go()
 	print _("Exited successfully.")
 else:
