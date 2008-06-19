5e1530d839defbf81566a27186072c6c3e95b77f
ozan <ozan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-06-19 23:21:07 +0000

    Major commit involving the following changes: 	* The notification sender can now place buttons on the notification window. 	  These buttons are laid out in a grid manner. The sender is informed of the 	  clicked button. 	* This feedback to the sender can be synchronous or asynchronous as chosen 	  by the sender. In other words, the SendNotification() call can be chosen 	  to be blocking or nonblocking by the sender. 	* For nonblocking calls callbacks need to be supplied to the Notifier client 	  module. Note that a main loop is required for callbacks to work. Both GLib 	  and Qt main loops are supported.
    What to do next?
    	* Add a configuration tool to configure the PNM.

--- trunk/gsoc/notifier/notification.py
+++ trunk/gsoc/notifier/notification.py
@@ -9,16 +9,42 @@ class Notification:
 		self.notification_title = notification_title
 		self.notification_text = notification_text
 		self.notification_icon_path = notification_icon_path
-		self.isReceived = False		
+		# Notifications do not have default buttons:
+		self.buttons = []
+		self.isReceived = False
+		# The chosen button field will reflect the index of the chosen button (when the notification is sent back after getting processed by the notification manager). -1 will mean that the user has not pressed any.
+		self.chosen_button = -1
+		# The fields below this line are not intended for use by the users of this software.
+		# They are used by the Notifier, NotificationManager and NotificationDisplayer implementations.
+		self.reporter = None
+		self.notification_response = None
+
+	def SetNotificationIcon(self, new_icon_path):
+		self.notification_icon_path = new_icon_path
+	
+	def AddButton(self, button):
+		self.buttons.append(button)
+
+	# The following methods are not intended for use by the users of this software.
+	# They are used by the Notifier, NotificationManager and NotificationDisplayer implementations.
 
 	def Pack(self):
 		# For multi-platform compatibility send the text with utf-8 no matter what:
 		self.notification_title = self.notification_title.encode("utf-8")
 		self.notification_text = self.notification_text.encode("utf-8")
 		self.notification_icon_path = self.notification_icon_path.encode("utf-8")
-		
+		for i in range(self.buttons.__len__()):
+			self.buttons[i] = self.buttons[i].encode("utf-8")
+
 	def Unpack(self):
 		# Convert the text back to its natural encoding:
 		self.notification_title = self.notification_title.decode("utf-8")
 		self.notification_text = self.notification_text.decode("utf-8")
 		self.notification_icon_path = self.notification_icon_path.decode("utf-8")
+		for i in range(self.buttons.__len__()):
+			self.buttons[i] = self.buttons[i].decode("utf-8")
+
+	def SetReporterCallable(self, callable, notification_response):
+		self.reporter = callable
+		self.notification_response = notification_response
+
--- trunk/gsoc/notifier/notman.py
+++ trunk/gsoc/notifier/notman.py
@@ -24,8 +24,8 @@ from notdisplayer import *
 
 class Timer(QtCore.QThread):
 	def __init__(self, notification_manager):
-	    QtCore.QThread.__init__(self)
-	    self.notification_manager = notification_manager
+		QtCore.QThread.__init__(self)
+		self.notification_manager = notification_manager
 	
 	def run(self):
 		# If the notification queue is not empty or the manager's lifespan is not over contrinue running. Else, exit gracefully:
@@ -45,100 +45,113 @@ class Timer(QtCore.QThread):
 				break
     
 class NotificationManager(QApplication):
-    def __init__(self):
-        QApplication.__init__(self,  sys.argv)
-        print _("Initializing notification manager...")
-        # We don't want the program to get closed when there is no visible notificaition window:
-        self.setQuitOnLastWindowClosed(False)
-        # Initialize the pending message queue:
-        self.message_queue = []
-        self.lifespan = 15.0
-        self.last_notification_time = time.time()
-        # Initialize the timer:
-        self.timer = Timer(self)
-        # Initialize the notification displayer:
-        self.notification_displayer = NotificationDisplayer()
-        
-    def Die(self):
-        QtCore.QTimer().singleShot(1000,  quit)
-    
-    def Go(self):        
-        # Start the timer (spawned in its own thread):
-        self.timer.start()
-        # Start the Qt4 main loop:
-        self.exec_()
-        
-    def HandleNotification(self):
-        # If the notification queue is not empty, handle the first notification in the queue:
-        if self.message_queue != []:
-            print _("Handling notification with text: %s") % self.message_queue[0].notification_text
-            self.notification_displayer.DisplayNotification(self.message_queue[0])
-            self.message_queue = self.message_queue[1:]
-            print _("Queue state: %s") % self.message_queue
-    
-    def SetSignalHandlers(self, signal_source):
-		# Signal handlers:
-		self.connect(signal_source, QtCore.SIGNAL("handleEvent()"), self.HandleNotification)
+	def __init__(self):
+		QApplication.__init__(self,  sys.argv)
+		print _("Initializing notification manager...")
+		# We don't want the program to get closed when there is no visible notificaition window:
+		self.setQuitOnLastWindowClosed(False)
+		# Initialize the pending message queue:
+		self.message_queue = []
+		self.lifespan = 15.0
+		self.last_notification_time = time.time()
+		# Initialize the timer:
+		self.timer = Timer(self)
+		# Initialize the notification displayer:
+		self.notification_displayer = NotificationDisplayer()    
+
+	def Die(self):
+		QtCore.QTimer().singleShot(1000,  quit)
+		
+	def Go(self):        
+		# Start the timer (spawned in its own thread):
+		self.timer.start()
+		# Start the Qt4 main loop:
+		self.exec_()
+
+	def HandleNotification(self):
+		# If the notification queue is not empty, handle the first notification in the queue:
+		if self.message_queue != []:
+			# Debug line:
+			# print _("Handling notification with text: %s") % self.message_queue[0].notification_text
+			self.notification_displayer.DisplayNotification(self.message_queue[0])
+			self.message_queue = self.message_queue[1:]
+			print _("Queue state: %s") % self.message_queue
+
+	def SetNotificationSignalHandler(self, signal_source):
+		self.connect(signal_source, QtCore.SIGNAL("handleNotification()"), self.HandleNotification)
+
+	def SetExitSignalHandler(self, signal_source):
 		self.connect(signal_source, QtCore.SIGNAL("exitProgram()"), self.Die)
 
 class NotXFace(dbus.service.Object):
-    def __init__(self, notification_manager, notxfacethread):
-        dbus.service.Object.__init__(self, notxfacethread.bus_name, notxfacethread.object_path)
-        print _("Initializing / exporting NotXFace object...")
-        self.notification_manager = notification_manager
-        self.notxfacethread = notxfacethread
-        
-    @dbus.service.method(dbus_interface = "org.pardus.notmanxface", in_signature = "s", out_signature = "s", sender_keyword = "sender")
-    def AddNotification(self, serialized_notification, sender = None):
-        try:
-            # Try unpickling the serialized notification:
+	def __init__(self, notification_manager, notxfacethread):
+		dbus.service.Object.__init__(self, notxfacethread.bus_name, notxfacethread.object_path)
+		print _("Initializing / exporting NotXFace object...")
+		self.notification_manager = notification_manager
+		self.notxfacethread = notxfacethread
+	
+	# This method checks if the incoming notification is bundled with any buttons or not.
+	# If it is, the method does not return immediately but waits until the user presses a button (async mode).
+	# When the user presses a button, the index of the pressed button is recorded in the chosen_button field of the incoming notification and the notification object is sent back to the notification sender.
+
+	# If the notification is not bundled with any buttons, the method return immediately (sync mode) with chosen_button field set to -1.
+	@dbus.service.method(dbus_interface = "org.pardus.notmanxface", async_callbacks = ("report", "report_error"), in_signature = "s", out_signature = "s", sender_keyword = "sender")
+	def AddNotification(self, serialized_notification, report, report_error, sender = None):
+		try:
+			# Try unpickling the serialized notification:
 			notification = pickle.loads(serialized_notification.__str__())
-        except:
-            # If an error occurs while unpickling serialized notification, notify the requestor of it:
-            notification_response = Notification(_("Couldn't unpickle sent notification"))
-            return pickle.dumps(notification_reponse)
-        else:
+		except:
+			# If an error occurs while unpickling serialized notification, notify the requestor of it:
+			notification_response = Notification(_("Couldn't unpickle sent notification"))
+			return pickle.dumps(notification_reponse)
+		else:
 			# Make a copy of the received notification to send as the response:
 			notification_response = copy.deepcopy(notification)
+			# Acknowledge that the notification is received:
+			notification_response.isReceived = True
 			# Unpack the received notification:
 			notification.Unpack()
-        	# Append the notification to the queue of the notification manager:			
+			# Set the reporting callable (= async return callable) of the notification:
+			if notification.buttons != []:
+				notification.SetReporterCallable(report, notification_response)
+			# Append the notification to the queue of the notification manager:			
 			self.notification_manager.message_queue.append(notification)
 			print _("Notification successfully received. Sender's bus name: %s") % sender
 			print _("Received message text: %s") % notification.notification_text
 			print _("Added notification to queue.")
-			# Handle the notification in the other thread (in case handling the notification takes too much time, we dont want to stall the requestor):
-			self.notxfacethread.emit(QtCore.SIGNAL("handleEvent()"))
+			# Handle the notification in the other thread (in case handling the notification takes too much time, we dont want to stall the whole dbus listener):
+			self.notxfacethread.emit(QtCore.SIGNAL("handleNotification()"))
 			# Update the last notification timestamp of the notification manager:
 			self.notification_manager.last_notification_time = time.time()
-			# Return the notification to the requestor, acknowledging that the notification is received:
-			notification_response.isReceived = True
-			return pickle.dumps(notification_response)        	
+			# If the notification is not bundled with any buttons, return immediately:
+			if notification.buttons == []:
+				report(pickle.dumps(notification_response))
+			# If it is, the displayer will call the reporting callable.
+
+	@dbus.service.method(dbus_interface = "org.pardus.notmanxface", in_signature = "s", out_signature = "s", sender_keyword = "sender")
+	def EchoSender(self, message_string, sender = None):
+		print _("Echo request received. Sender's bus name: %s") % sender
+		print _("Text to be echoed: %s") % message_string
+		return _("Echoing: %s") % message_string
         
-    @dbus.service.method(dbus_interface = "org.pardus.notmanxface", in_signature = "s", out_signature = "s", sender_keyword = "sender")
-    def EchoSender(self, message_string, sender = None):
-        print _("Echo request received. Sender's bus name: %s") % sender
-        print _("Text to be echoed: %s") % message_string
-        return _("Echoing: %s") % message_string
-        
-    @dbus.service.method(dbus_interface = "org.pardus.notmanxface", in_signature = "", out_signature = "", sender_keyword = "sender")
-    def Exit(self, sender = None):
-        print _("Sender's bus name: %s") % sender
-        print _("Exit method called, exiting service...")
-        self.notxfacethread.emit(QtCore.SIGNAL("exitProgram()"))
- 
+	@dbus.service.method(dbus_interface = "org.pardus.notmanxface", in_signature = "", out_signature = "", sender_keyword = "sender")
+	def Exit(self, sender = None):
+		print _("Sender's bus name: %s") % sender
+		print _("Exit method called, exiting service...")
+		self.notxfacethread.emit(QtCore.SIGNAL("exitProgram()"))
+
 class NotXFaceThread(QtCore.QThread):
-    def __init__(self, notification_manager, object_path):
-        QtCore.QThread.__init__(self)
-        self.notification_manager = notification_manager
-        self.object_path = object_path
-    
-    def run(self):
-    	DBusQtMainLoop(set_as_default = True)
-        self.session_bus = dbus.SessionBus()
-        self.bus_name = dbus.service.BusName("org.pardus.notificationmanager", self.session_bus)
-        self.exported_obj = NotXFace(self.notification_manager, self)
-        self.exec_()
+	def __init__(self, notification_manager, object_path):
+		QtCore.QThread.__init__(self)
+		self.notification_manager = notification_manager
+		self.object_path = object_path
+
+	def run(self):
+		DBusQtMainLoop(set_as_default = True)
+		self.session_bus = dbus.SessionBus()
+		self.bus_name = dbus.service.BusName("org.pardus.notificationmanager", self.session_bus)
+		self.exported_obj = NotXFace(self.notification_manager, self)
+		self.exec_()
 
 # If executed as the main program:
 if __name__ == "__main__":
@@ -147,12 +160,13 @@ if __name__ == "__main__":
 	# Create the thread containing the notification manager DBus interface object:
 	notxfacethread = NotXFaceThread(notification_manager, "/NotificationManager")
 	# Set the signal handlers of the main thread so that it can receive signals from the DBus interface thread:
-	notification_manager.SetSignalHandlers(notxfacethread)
+	notification_manager.SetNotificationSignalHandler(notxfacethread)
+	notification_manager.SetExitSignalHandler(notxfacethread)
 	# Launch up the DBus interface thread:
 	notxfacethread.start()
 	# Launch up the main (GUI) thread:
 	notification_manager.Go()
 	print _("Exited successfully.")
 else:
-    print _("This program is not meant to be loaded as a module.")
+	print _("This program is not meant to be loaded as a module.")
     
