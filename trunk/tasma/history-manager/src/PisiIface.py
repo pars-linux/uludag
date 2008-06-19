869a8db213dc7ac54bd5d6bcbcf0bbe1115aeb5f
isbaran <isbaran@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-06-19 00:54:37 +0000

    daha paket yöneticimsi ve ferah bir gui operasyonlar yarın devam etsin ..

---
+++ trunk/tasma/history-manager/src/PisiIface.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2008 TUBITAK/UEKAE
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file
+
+import os
+import pisi
+
+def get_history_dir():
+    return pisi.ctx.config.history_dir()
+
+def get_config_files(op):
+    pass
+
+def getPlan(op):
+    return pisi.api.get_takeback_plan(op)
+
