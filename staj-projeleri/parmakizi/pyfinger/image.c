bf321ebcf4c601c941fdf188cef5a0504bdbef29
makmanalp <makmanalp@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-06-24 06:45:05 +0000

    Bu sefer dogru ...

---
+++ staj-projeleri/parmakizi/pyfinger/image.c
@@ -0,0 +1,44 @@
+#include "core.h"
+
+// ------------ HELPERS -----------------
+
+/**Helper: Read an image into a variable*/
+struct fp_img* image_get(){
+    //imaging supported?
+    if(!fp_dev_supports_imaging(device)){
+        pyferror(ERR_LFP_NOIMAGING);
+    }
+
+    //capture image
+    struct fp_img* image = NULL;
+    if (fp_dev_img_capture(device, 0, &image)){
+        pyferror(ERR_LFP_IMAGINGFAIL);
+    }
+
+    return image;
+}
+
+/**Helper: Write an image to a file*/
+int writefile(struct fp_img* image, char* filename){
+    return fp_img_save_to_file(image, filename); // 0 on sucess
+}
+
+// --------------------------------------
+
+/**Capture an image, standardize it, and write it to a file*/
+void image_write(char* filename){
+    struct fp_img* img = image_get();
+    fp_img_standardize(img);
+    writefile(img, filename);
+    fp_img_free(img); //free it
+}
+
+/**Capture an image, standardize and binarize it, and write it to a file*/
+void image_write_binarized(char* filename){
+    struct fp_img* img = image_get();
+    fp_img_standardize(img);
+    struct fp_img* img_binarized = fp_img_binarize(img);
+    writefile(img_binarized, filename);
+    fp_img_free(img); //free it
+    fp_img_free(img_binarized);
+}
