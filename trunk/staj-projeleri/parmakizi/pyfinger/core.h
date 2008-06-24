e13a5fe3df621b06bd489616958e15750823a4f3
makmanalp <makmanalp@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-06-24 06:45:05 +0000

    Bu sefer dogru ...

---
+++ trunk/staj-projeleri/parmakizi/pyfinger/core.h
@@ -0,0 +1,57 @@
+#ifndef _CORE_H_
+#define _CORE_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <libfprint/fprint.h>
+//#include <python2.5/Python.h>
+
+
+//-------------- ERROR CODES ---------------
+
+/**\brief Basic error codes
+ *
+ * A list of all the errors that might be returned by the library.
+ */
+enum errortype{
+    ERR_LFP_LIBRARYFAIL,    ///library load failed
+    ERR_LFP_DISCOVERYFAIL,  ///device discovery failed
+    ERR_LFP_NODEVICE,       ///no device found
+    ERR_LFP_DEVICEINITFAIL, ///device failed to start
+    ERR_LFP_NOIMAGING,      ///imaging not supported
+    ERR_LFP_IMAGINGFAIL     ///imaging attempt failed
+};
+
+#define MAXERROR 5                  ///Largest error code for error bounds check
+extern char* errormsg[MAXERROR+1];    ///Error messages
+
+
+// ------------- GLOBALS --------------
+
+struct fp_dscv_dev** ddevice_list; ///Discovered devices
+struct fp_dscv_dev* ddevice_curr;  ///Selected discovered-device
+
+struct fp_dev* device; ///Device that we're using
+
+// ------------ PROTOTYPES ------------
+
+//debug
+void pyferror(int id);
+
+//core
+void load();
+void unload();
+void device_discover();
+void device_open();
+void device_close();
+
+//imaging
+void image_write(char* filename);
+void image_write_binarized(char* filename);
+
+//enrolling
+void enroll();
+
+//verifying
+
+#endif
