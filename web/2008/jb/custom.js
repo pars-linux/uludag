dd5563a286393a6410a491289f3d957d1dee11a4
Ahmet ERDOĞAN <tuxposta@gmail.com>
2008-06-24 14:52:33 +0000

    Java betikleri ve sağdaki resimler.

---
+++ web/2008/jb/custom.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $("#bireysel").click(function () {
+        $("#bireysel_kapali").fadeOut("slow");
+        $("#bireysel_acik").fadeIn("slow");
+//         $("#bireysel_kapali").attr({ src: "media/img/bireysel_turuncu.png" });
+//         $("#bireysel_kapali").animate({
+//             src: "media/img/bireysel_turuncu.png"
+//         }, 1500 );
+//         $("#bireysel").animate({
+//             height: 60
+//         }, 1500 );
+    });
+
+//     $("#bireysel").click(function(){
+//         $("#bireysel").animate({
+//             backgroundSrc: "media/img/menu/bireysel_turuncu.png";
+//         }, 1500 );
+//     });
+
+    $("#bireysel").(function () {
+        $("#bireysel_kapali").fadeOut("slow");
+        $("#bireysel_acik").fadeIn("slow");
+//         $("#bireysel_kapali").attr({ src: "media/img/bireysel_turuncu.png" });
+//         $("#bireysel_kapali").animate({
+//             src: "media/img/bireysel_turuncu.png"
+//         }, 1500 );
+//         $("#bireysel").animate({
+//             height: 60
+//         }, 1500 );
+    });
+});
