44f2356ca710d37699d9ebf6377f3f68756c1ab4
ahmet <ahmet@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-06-24 15:30:39 +0000

    bitti gibi

--- trunk/web/2008/jb/custom.js
+++ trunk/web/2008/jb/custom.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
-    $("#bireysel").click(function () {
-        $("#bireysel_kapali").fadeOut("slow");
-        $("#bireysel_acik").fadeIn("slow");
+// $(document).ready(function(){
+//     $("#bireysel").click(function () {
+//         $("#bireysel_kapali").fadeOut("slow");
+//         $("#bireysel_acik").fadeIn("slow");
 //         $("#bireysel_kapali").attr({ src: "media/img/bireysel_turuncu.png" });
 //         $("#bireysel_kapali").animate({
 //             src: "media/img/bireysel_turuncu.png"
@@ -9,17 +9,17 @@ $(document).ready(function(){
 //         $("#bireysel").animate({
 //             height: 60
 //         }, 1500 );
-    });
-
+//     });
+//
 //     $("#bireysel").click(function(){
 //         $("#bireysel").animate({
 //             backgroundSrc: "media/img/menu/bireysel_turuncu.png";
 //         }, 1500 );
 //     });
-
-    $("#bireysel").(function () {
-        $("#bireysel_kapali").fadeOut("slow");
-        $("#bireysel_acik").fadeIn("slow");
+//
+//     $("#bireysel").(function () {
+//         $("#bireysel_kapali").fadeOut("slow");
+//         $("#bireysel_acik").fadeIn("slow");
 //         $("#bireysel_kapali").attr({ src: "media/img/bireysel_turuncu.png" });
 //         $("#bireysel_kapali").animate({
 //             src: "media/img/bireysel_turuncu.png"
@@ -27,5 +27,5 @@ $(document).ready(function(){
 //         $("#bireysel").animate({
 //             height: 60
 //         }, 1500 );
-    });
-});
+//     });
+// });
