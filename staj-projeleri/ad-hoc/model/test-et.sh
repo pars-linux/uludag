4efb436d738340b460068b92e6429920aa9b66b7
ozirus <ozirus@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-06-27 20:13:41 +0000

    some comar script editing and todo doc

--- staj-projeleri/ad-hoc/model/test-et.sh
+++ staj-projeleri/ad-hoc/model/test-et.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-hav call wireless_tools Net.Link setConnection "pardus-2" "usb:148f_2573_wlan0"
+hav call wireless_tools Net.Link setConnection "pardus-2" "pci:8086_4220_eth0"
 hav call wireless_tools Net.Link setConnectionMode "pardus-2" "ad-hoc"
 hav call wireless_tools Net.Link setAddress "pardus-2" "manual" "192.168.3.1" "255.255.255.0" ""
-hav call wireless_tools Net.Link setRemote "pardus-2" "staj2008" ""
+hav call wireless_tools Net.Link setRemote "pardus-2" "staj-adhoc-test" ""
 hav call wireless_tools Net.Link setAuthentication "pardus-2" "wep" "" "1234567898"
 hav call wireless_tools Net.Link setState "pardus-2" "up"
