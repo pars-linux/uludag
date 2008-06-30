1d22d64ed62e3d9401e60f7e14204d5c3aecd2e9
makmanalp <makmanalp@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-06-30 12:26:23 +0000

    unstable? wtf?

--- trunk/staj-projeleri/parmakizi/fingerui/finger-manager.py
+++ trunk/staj-projeleri/parmakizi/fingerui/finger-manager.py
@@ -1,20 +1,70 @@
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 import fingerform
+from libfprint import *
 
 class fmDialog(QDialog, fingerform.Ui_dialogFinger):
-    def __init__(self, parent=None):
+    def __init__(self, uid, parent=None):
+        
+        #uid
+        if uid == None:
+            raise ValueError
+        self.__uid = uid
+
+        #devices
+        self.__devices = None
+
+        #device
+        self.__device = None
+
+
         super(fmDialog, self).__init__(parent)
         self.setupUi(self)
         self.UpdateUi()
+        self.initDevice()
+        self.getImage()
+
+    #--------ui functions-------
+
+    @pyqtSignature("")
+    def on_pushEnroll_clicked(self):
+        print "Enrolled for uid " + self.__uid.__str__()
+
+    @pyqtSignature("")
+    def on_pushErase_clicked(self):
+        print "FP for uid " + self.__uid.__str__() + " erased"
+
+    @pyqtSignature("")
+    def on_pushVerify_clicked(self):
+        print "FP matches!"
 
     def UpdateUi(self):
         pass
 
+    #------helper functions------
+    def initFprint(self):
+        libfprint.fp_init()
+
+    def initDevice(self):
+        self.__devices = pyfprint.discover_devices()
+        print [x.get_driver().get_full_name() for x in self.__devices]
+        if self.__devices == None:
+            raise "No devices found"
+        self.__device = self.__devices[0]
+    
+    def getImage(self):
+        self.__device.open()
+        img = self.__device.capture_image(True)
+        img.standardize()
+        img = img.binarize()
+        img.save_to_file("parmak.ppm")
+
+
+
 if __name__ == "__main__":
     import sys
     app = QApplication(sys.argv)
-    form = fmDialog()
+    form = fmDialog(1)
     form.show()
     app.exec_()
 
--- trunk/staj-projeleri/parmakizi/fingerui/fingerform.py
+++ trunk/staj-projeleri/parmakizi/fingerui/fingerform.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'fingerform.ui'
 #
-# Created: Fri Jun 27 14:47:53 2008
+# Created: Mon Jun 30 09:22:40 2008
 #      by: PyQt4 UI code generator 4.4.2
 #
 # WARNING! All changes made in this file will be lost!
@@ -12,33 +12,17 @@ from PyQt4 import QtCore, QtGui
 class Ui_dialogFinger(object):
     def setupUi(self, dialogFinger):
         dialogFinger.setObjectName("dialogFinger")
-        dialogFinger.resize(355,215)
+        dialogFinger.resize(303,164)
         dialogFinger.setModal(True)
-        self.horizontalLayout_2 = QtGui.QHBoxLayout(dialogFinger)
-        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.horizontalLayout = QtGui.QHBoxLayout(dialogFinger)
+        self.horizontalLayout.setObjectName("horizontalLayout")
         self.viewFinger = QtGui.QGraphicsView(dialogFinger)
         self.viewFinger.setFocusPolicy(QtCore.Qt.NoFocus)
         self.viewFinger.setInteractive(False)
         self.viewFinger.setObjectName("viewFinger")
-        self.horizontalLayout_2.addWidget(self.viewFinger)
-        self.verticalLayout_2 = QtGui.QVBoxLayout()
-        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.horizontalLayout.addWidget(self.viewFinger)
         self.verticalLayout = QtGui.QVBoxLayout()
         self.verticalLayout.setObjectName("verticalLayout")
-        self.horizontalLayout = QtGui.QHBoxLayout()
-        self.horizontalLayout.setObjectName("horizontalLayout")
-        self.labelFinger = QtGui.QLabel(dialogFinger)
-        self.labelFinger.setObjectName("labelFinger")
-        self.horizontalLayout.addWidget(self.labelFinger)
-        self.comboFinger = QtGui.QComboBox(dialogFinger)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.MinimumExpanding,QtGui.QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.comboFinger.sizePolicy().hasHeightForWidth())
-        self.comboFinger.setSizePolicy(sizePolicy)
-        self.comboFinger.setObjectName("comboFinger")
-        self.horizontalLayout.addWidget(self.comboFinger)
-        self.verticalLayout.addLayout(self.horizontalLayout)
         self.pushEnroll = QtGui.QPushButton(dialogFinger)
         self.pushEnroll.setObjectName("pushEnroll")
         self.verticalLayout.addWidget(self.pushEnroll)
@@ -48,29 +32,23 @@ class Ui_dialogFinger(object):
         self.pushVerify = QtGui.QPushButton(dialogFinger)
         self.pushVerify.setObjectName("pushVerify")
         self.verticalLayout.addWidget(self.pushVerify)
-        self.verticalLayout_2.addLayout(self.verticalLayout)
         self.buttonBox = QtGui.QDialogButtonBox(dialogFinger)
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
         self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
         self.buttonBox.setObjectName("buttonBox")
-        self.verticalLayout_2.addWidget(self.buttonBox)
-        self.horizontalLayout_2.addLayout(self.verticalLayout_2)
-        self.labelFinger.setBuddy(self.comboFinger)
+        self.verticalLayout.addWidget(self.buttonBox)
+        self.horizontalLayout.addLayout(self.verticalLayout)
 
         self.retranslateUi(dialogFinger)
         QtCore.QObject.connect(self.buttonBox,QtCore.SIGNAL("accepted()"),dialogFinger.accept)
         QtCore.QObject.connect(self.buttonBox,QtCore.SIGNAL("rejected()"),dialogFinger.reject)
         QtCore.QMetaObject.connectSlotsByName(dialogFinger)
-        dialogFinger.setTabOrder(self.comboFinger,self.pushEnroll)
         dialogFinger.setTabOrder(self.pushEnroll,self.pushErase)
         dialogFinger.setTabOrder(self.pushErase,self.pushVerify)
         dialogFinger.setTabOrder(self.pushVerify,self.buttonBox)
 
     def retranslateUi(self, dialogFinger):
         dialogFinger.setWindowTitle(QtGui.QApplication.translate("dialogFinger", "Parmak İzi Düzenleyici", None, QtGui.QApplication.UnicodeUTF8))
-        self.labelFinger.setText(QtGui.QApplication.translate("dialogFinger", "Parmak:", None, QtGui.QApplication.UnicodeUTF8))
-        self.comboFinger.addItem(QtGui.QApplication.translate("dialogFinger", "Sağ Orta Parmak", None, QtGui.QApplication.UnicodeUTF8))
-        self.comboFinger.addItem(QtGui.QApplication.translate("dialogFinger", "Sağ İşaret Parmağı", None, QtGui.QApplication.UnicodeUTF8))
         self.pushEnroll.setText(QtGui.QApplication.translate("dialogFinger", "Parmak İzi Tanıt", None, QtGui.QApplication.UnicodeUTF8))
         self.pushErase.setText(QtGui.QApplication.translate("dialogFinger", "Parmak İzini Sil", None, QtGui.QApplication.UnicodeUTF8))
         self.pushVerify.setText(QtGui.QApplication.translate("dialogFinger", "Parmak İzini Dene", None, QtGui.QApplication.UnicodeUTF8))
