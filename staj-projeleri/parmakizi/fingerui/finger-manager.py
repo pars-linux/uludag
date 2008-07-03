86b2b0a108ff1f1d98b0f80c5b05e76b1cd7caf5
makmanalp <makmanalp@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-07-03 09:02:03 +0000

    Code quality improved drastically. Need to implement UpdateUi better, and then do COMAR stuff.

--- staj-projeleri/parmakizi/fingerui/finger-manager.py
+++ staj-projeleri/parmakizi/fingerui/finger-manager.py
@@ -1,11 +1,16 @@
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
+"""finger-manager gui."""
+from PyQt4.QtCore import pyqtSignature
+from PyQt4.QtGui import QDialog, QPixmap, QApplication
 import fingerform
-from libfprint import *
+import libfprint
 
 class fmDialog(QDialog, fingerform.Ui_dialogFinger):
+    """Dialog for finger-manager.
+    Supports 3 basic functions: Enroll, Verify and Erase.
+    Enroll asks the user for fingerprint data and saves it.
+    Verify verifies the fingerprint data with the currently saved data.
+    Erase erases the currently saved data."""
     def __init__(self, uid, parent=None):
-        
         #uid
         if uid == None:
             raise ValueError
@@ -21,110 +26,128 @@ class fmDialog(QDialog, fingerform.Ui_dialogFinger):
         super(fmDialog, self).__init__(parent)
         self.setupUi(self)
         self.UpdateUi()
-        self.initFprint()
-        self.initDevice()
-        self.enroll()
+        self._initFprint()
 
     def __del__(self):
         #super(self.__class__, self).__del__()
-        if self.__device:
-            self.closeDevice()
-        self.exitFprint()
-        
+        self._exitFprint()
 
     #--------ui functions-------
 
     @pyqtSignature("")
     def on_pushEnroll_clicked(self):
-        self.enroll(self.__uid.__str__())
+        """Enroll button event handler."""
+        self.enroll()
 
     @pyqtSignature("")
     def on_pushErase_clicked(self):
+        """Erase button event handler."""
         print "FP for uid " + self.__uid.__str__()
 
     @pyqtSignature("")
     def on_pushVerify_clicked(self):
-        self.verify(self.__uid.__str__())
+        """Verify button event handler."""
+        self.verify()
 
     def UpdateUi(self):
+        """Updates the UI to set disabled buttons where appropriate.
+        Example: When there is no existing fprint, then the user should
+        not be able to press erase."""
         pass
 
     #------helper functions------
-    def initFprint(self):
-        fp_init()
-
-    def initDevice(self):
-        self.__devices = pyfprint.discover_devices()
+    def _initFprint(self):
+        """Start the fprint class and discover devices."""
+        libfprint.fp_init()
+        self.__devices = libfprint.discover_devices()
         print [x.get_driver().get_full_name() for x in self.__devices]
         if self.__devices == []:
-            raise "No devices found"
+            sys.exit("No devices found")
         self.__device = self.__devices[0]
-    
-    def exitFprint(self):
-        fp_exit()
 
-    def openDevice(self):
-        self.__device.open()
+    def _exitFprint(self):
+        """Exit the fprint class."""
+        self._closeDevice()
+        libfprint.fp_exit()
 
-    def closeDevice(self):
-        self.__device.close()
+    def _openDevice(self):
+        """Open the current device, if not already open."""
+        if not self.__device.dev:
+            self.__device.open()
+
+    def _closeDevice(self):
+        """Close the current device, if not already closed."""
+        if self.__device.dev:
+            self.__device.close()
 
     #QImage.Format_RGB32 works.
     def getImage(self):
-        self.openDevice()
+        """Get the fingerprint image and then display it. Blocking."""
+        self._openDevice()
         img = self.__device.capture_image(True)
         img = img.binarize()
         pixmap = self._pixmapize(img, "parmak.pgm")
         self.viewFinger.setPixmap(pixmap)
-        self.closeDevice()
+        self._closeDevice()
 
-    def _pixmapize(self, img, savepath=".tmpimg"):
-        img.save_to_file(filename) #(TODO: comarize)
+    @staticmethod
+    def _pixmapize(img, filename=".tmpimg"):
+        """Convert image into pixmap."""
+        img.save_to_file(filename) #TODO: comarize OR Fix workaround.
         return QPixmap(filename)
 
-    def enroll(self, uid):
-        self.openDevice()
+    def enroll(self):
+        """Get fingerprint data, store it, and show image. Blocking."""
+        self._openDevice()
         while 1:
             (fprnt, img) = self.__device.enroll_finger()
-            if fprnt == "xxx": #TODO: Fix binding return data. Also, memory leak?
+            if fprnt == "xxx": #TODO: Fix binding return. Also, memory leak?
                 print "Please retry" 
             else:
                 print "Enrolled"
                 break
         pixmap = self._pixmapize(img.binarize())
         self.viewFinger.setPixmap(pixmap)
-        self._savePrint(fprnt)
-        self.closeDevice()
+        self._savePrint(fprnt) #TODO: save with uid
+        self._closeDevice()
 
-    def _savePrint(self, fprint, path=".printdata"):
-        file = open(path, "w")
-        file.write(fprint.get_data())
-        file.close()
+    @staticmethod
+    def _savePrint(fprint, path=".printdata"): #TODO: Comarize.
+        """Save serialized print data."""
+        printfile = open(path, "w")
+        printfile.write(fprint.get_data())
+        printfile.close()
 
-    def _loadPrint(self, fprint, path=".printdata"):
-        file=open(path, "r")
-        printdata = file.read()
-        file.close()
-        return Fprint(printdata)
+    @staticmethod
+    def _loadPrint(path=".printdata"): #TODO: Comarize.
+        """Load serialized print data."""
+        printfile = open(path, "r")
+        printdata = printfile.read()
+        printfile.close()
+        return libfprint.Fprint(printdata)
 
-    def verify(self, uid):
-        self.openDevice()
+    def verify(self):
+        """Get fingerprint data and verify against previously stored data."""
+        compareprint = self._loadPrint() #TODO: comarize w/ uid, check existance
+        self._openDevice()
         while 1:
-            (ret , img) = self.__device.verify_finger()
+            (ret , img) = self.__device.verify_finger(compareprint)
             if ret == True:
+                print "FP matched"
                 break
-            elif ret == FALSE:
+            elif ret == False:
                 print "Match failed"
+                break
             else:
                 print "please retry"
         pixmap = self._pixmapize(img.binarize())
         self.viewFinger.setPixmap(pixmap)
-        self._savePrint(fprint)
-        self.closeDevice()
+        self._closeDevice()
 
 if __name__ == "__main__":
     import sys
     app = QApplication(sys.argv)
     form = fmDialog(1)
     form.show()
+    form.enroll()
     app.exec_()
