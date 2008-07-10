d3c8defcae068e90d607d559b7a5055200786552
makmanalp <makmanalp@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-07-10 09:09:23 +0000

    Changed function names to better suit user-manager integration. Changed COMAR script name.

--- staj-projeleri/parmakizi/fingerui/comar/test.sh
+++ staj-projeleri/parmakizi/fingerui/comar/test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
-hav call fingermanager User.Manager getStatus 1337
-hav call fingermanager User.Manager saveData 1337 fingerprintttt imageeeeee
-hav call fingermanager User.Manager loadData 1337
-hav call fingermanager User.Manager eraseData 1337
-hav call fingermanager User.Manager getStatus 1337
+hav call baselayout User.Manager getFPStatus 1337
+hav call baselayout User.Manager saveFPData 1337 fingerprintttt imageeeeee
+hav call baselayout User.Manager loadFPData 1337
+hav call baselayout User.Manager eraseFPData 1337
+hav call baselayout User.Manager getFPStatus 1337
