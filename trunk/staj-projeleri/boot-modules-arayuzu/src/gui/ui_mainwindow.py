2000c59ce50c7a23c47b32faeeace7a1e4e67af6
ozirus <ozirus@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-07-10 13:39:48 +0000

    some gui modifications

--- trunk/staj-projeleri/boot-modules-arayuzu/src/gui/ui_mainwindow.py
+++ trunk/staj-projeleri/boot-modules-arayuzu/src/gui/ui_mainwindow.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'module-manager.ui'
+# Form implementation generated from reading ui file 'ui_mainwindow.ui'
 #
-# Created: Thu Jul 10 14:19:30 2008
+# Created: Thu Jul 10 16:37:01 2008
 #      by: PyQt4 UI code generator 4.4.2
 #
 # WARNING! All changes made in this file will be lost!
@@ -12,23 +12,32 @@ from PyQt4 import QtCore, QtGui
 class Ui_moduleManagerDlg(object):
     def setupUi(self, moduleManagerDlg):
         moduleManagerDlg.setObjectName("moduleManagerDlg")
-        moduleManagerDlg.resize(400,395)
-        self.gridLayoutWidget = QtGui.QWidget(moduleManagerDlg)
-        self.gridLayoutWidget.setGeometry(QtCore.QRect(30,30,341,311))
-        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
-        self.gridLayout_2 = QtGui.QGridLayout(self.gridLayoutWidget)
-        self.gridLayout_2.setObjectName("gridLayout_2")
-        self.listModule = QtGui.QListWidget(self.gridLayoutWidget)
-        self.listModule.setObjectName("listModule")
-        self.gridLayout_2.addWidget(self.listModule,0,0,1,1)
-        self.btnListModules = QtGui.QPushButton(moduleManagerDlg)
-        self.btnListModules.setGeometry(QtCore.QRect(30,354,86,30))
-        self.btnListModules.setObjectName("btnListModules")
+        moduleManagerDlg.resize(316,456)
+        self.splitter = QtGui.QSplitter(moduleManagerDlg)
+        self.splitter.setGeometry(QtCore.QRect(10,20,291,431))
+        self.splitter.setOrientation(QtCore.Qt.Vertical)
+        self.splitter.setObjectName("splitter")
+        self.verticalLayoutWidget = QtGui.QWidget(self.splitter)
+        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
+        self.verticalLayout = QtGui.QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.listModules = QtGui.QListWidget(self.verticalLayoutWidget)
+        self.listModules.setObjectName("listModules")
+        self.verticalLayout.addWidget(self.listModules)
+        self.horizontalLayoutWidget = QtGui.QWidget(self.splitter)
+        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
+        self.horizontalLayout = QtGui.QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.btnList = QtGui.QPushButton(self.horizontalLayoutWidget)
+        self.btnList.setObjectName("btnList")
+        self.horizontalLayout.addWidget(self.btnList)
+        spacerItem = QtGui.QSpacerItem(40,20,QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem)
 
         self.retranslateUi(moduleManagerDlg)
         QtCore.QMetaObject.connectSlotsByName(moduleManagerDlg)
 
     def retranslateUi(self, moduleManagerDlg):
         moduleManagerDlg.setWindowTitle(QtGui.QApplication.translate("moduleManagerDlg", "Module Manager", None, QtGui.QApplication.UnicodeUTF8))
-        self.btnListModules.setText(QtGui.QApplication.translate("moduleManagerDlg", "Listele", None, QtGui.QApplication.UnicodeUTF8))
+        self.btnList.setText(QtGui.QApplication.translate("moduleManagerDlg", "Listele", None, QtGui.QApplication.UnicodeUTF8))
 
