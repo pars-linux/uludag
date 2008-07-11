9dcae9ec9dec792fd3b8e63629f54205c53dac3d
ozirus <ozirus@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-07-11 13:44:20 +0000

    some gui modification and dbus integration

--- staj-projeleri/boot-modules-arayuzu/src/gui/ui_mainwindow.py
+++ staj-projeleri/boot-modules-arayuzu/src/gui/ui_mainwindow.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'ui_mainwindow.ui'
+# Form implementation generated from reading ui file 'uis/ui_mainwindow.ui'
 #
-# Created: Thu Jul 10 16:37:01 2008
+# Created: Fri Jul 11 09:36:40 2008
 #      by: PyQt4 UI code generator 4.4.2
 #
 # WARNING! All changes made in this file will be lost!
@@ -12,32 +12,41 @@ from PyQt4 import QtCore, QtGui
 class Ui_moduleManagerDlg(object):
     def setupUi(self, moduleManagerDlg):
         moduleManagerDlg.setObjectName("moduleManagerDlg")
-        moduleManagerDlg.resize(316,456)
-        self.splitter = QtGui.QSplitter(moduleManagerDlg)
-        self.splitter.setGeometry(QtCore.QRect(10,20,291,431))
-        self.splitter.setOrientation(QtCore.Qt.Vertical)
-        self.splitter.setObjectName("splitter")
-        self.verticalLayoutWidget = QtGui.QWidget(self.splitter)
-        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
-        self.verticalLayout = QtGui.QVBoxLayout(self.verticalLayoutWidget)
+        moduleManagerDlg.resize(311,443)
+        self.layoutWidget = QtGui.QWidget(moduleManagerDlg)
+        self.layoutWidget.setGeometry(QtCore.QRect(10,10,291,431))
+        self.layoutWidget.setObjectName("layoutWidget")
+        self.verticalLayout = QtGui.QVBoxLayout(self.layoutWidget)
         self.verticalLayout.setObjectName("verticalLayout")
-        self.listModules = QtGui.QListWidget(self.verticalLayoutWidget)
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.lblSearch = QtGui.QLabel(self.layoutWidget)
+        self.lblSearch.setScaledContents(True)
+        self.lblSearch.setWordWrap(True)
+        self.lblSearch.setObjectName("lblSearch")
+        self.horizontalLayout.addWidget(self.lblSearch)
+        self.editSearch = QtGui.QLineEdit(self.layoutWidget)
+        self.editSearch.setMaximumSize(QtCore.QSize(16777215,28))
+        self.editSearch.setObjectName("editSearch")
+        self.horizontalLayout.addWidget(self.editSearch)
+        self.btnSearch = QtGui.QPushButton(self.layoutWidget)
+        self.btnSearch.setMaximumSize(QtCore.QSize(30,28))
+        self.btnSearch.setObjectName("btnSearch")
+        self.horizontalLayout.addWidget(self.btnSearch)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.listModules = QtGui.QListWidget(self.layoutWidget)
         self.listModules.setObjectName("listModules")
         self.verticalLayout.addWidget(self.listModules)
-        self.horizontalLayoutWidget = QtGui.QWidget(self.splitter)
-        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
-        self.horizontalLayout = QtGui.QHBoxLayout(self.horizontalLayoutWidget)
-        self.horizontalLayout.setObjectName("horizontalLayout")
-        self.btnList = QtGui.QPushButton(self.horizontalLayoutWidget)
-        self.btnList.setObjectName("btnList")
-        self.horizontalLayout.addWidget(self.btnList)
-        spacerItem = QtGui.QSpacerItem(40,20,QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Minimum)
-        self.horizontalLayout.addItem(spacerItem)
+        self.chkLoaded = QtGui.QCheckBox(self.layoutWidget)
+        self.chkLoaded.setObjectName("chkLoaded")
+        self.verticalLayout.addWidget(self.chkLoaded)
 
         self.retranslateUi(moduleManagerDlg)
         QtCore.QMetaObject.connectSlotsByName(moduleManagerDlg)
 
     def retranslateUi(self, moduleManagerDlg):
         moduleManagerDlg.setWindowTitle(QtGui.QApplication.translate("moduleManagerDlg", "Module Manager", None, QtGui.QApplication.UnicodeUTF8))
-        self.btnList.setText(QtGui.QApplication.translate("moduleManagerDlg", "Listele", None, QtGui.QApplication.UnicodeUTF8))
+        self.lblSearch.setText(QtGui.QApplication.translate("moduleManagerDlg", "Ara  ", None, QtGui.QApplication.UnicodeUTF8))
+        self.btnSearch.setText(QtGui.QApplication.translate("moduleManagerDlg", ">>", None, QtGui.QApplication.UnicodeUTF8))
+        self.chkLoaded.setText(QtGui.QApplication.translate("moduleManagerDlg", "Sadece yüklü olanları listele", None, QtGui.QApplication.UnicodeUTF8))
 
