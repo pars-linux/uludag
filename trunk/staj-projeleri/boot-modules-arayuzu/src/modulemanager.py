1932b452ff9949775b9232637ace7768dd6c302a
ozirus <ozirus@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-07-15 10:42:39 +0000

    some more pyqt4 and dbus code editings and finally it can list kernel modules

--- trunk/staj-projeleri/boot-modules-arayuzu/src/modulemanager.py
+++ trunk/staj-projeleri/boot-modules-arayuzu/src/modulemanager.py
@@ -6,88 +6,86 @@ from PyQt4 import QtCore
 from PyQt4 import QtGui
 from gui.ui_mainwindow import Ui_moduleManagerDlg
 
-
 import dbus
-from handler import CallHandler
+import dbus.mainloop.qt
+
+from handler import * 
 
 class ModuleManagerDlg(QtGui.QDialog, Ui_moduleManagerDlg):
 
-    def __init__(self,parent=None):
+    def __init__(self, parent=None):
 
-        if not self.setupBusses():
-            sys.exit(1)
-
-        QtGui.QDialog.__init__(self,parent) 
+        QtGui.QDialog.__init__(self, parent) 
         self.setupUi(self)
 
-        # Populate list
-        self.populateList()
+        if not dbus.get_default_main_loop():
+            dbus.mainloop.qt.DBusQtMainLoop(set_as_default=True)
 
-    def setupBusses(self):
-        try:
-            self.busSys = dbus.SystemBus()
-            self.busSes = dbus.SessionBus()
-        except dbus.DBusException:
-            KMessageBox.error(self, i18n("Unable to connect to DBus."), i18n("DBus Error"))
-            return False
-        return True
+        if not self.openBus():
+            sys.exit(1)
 
-    def callHandler(self, script, model, method, action):
-        ch = CallHandler(script, model, method, action, 0, self.busSys, self.busSes)
+        self.loadedModules=[]
+        self.availableModules={}
+
+        self.connect(self.btnSearch, QtCore.SIGNAL("clicked()"), self.populateList)
+
+    
+    def callMethod(self, method, action):
+        ch = CallHandler("module_init_tools", "Boot.Modules", method,
+                         action,
+                         self.winId(),
+                         self.busSys, self.busSes)
         ch.registerError(self.comarError)
+        ch.registerAuthError(self.comarError)
         ch.registerDBusError(self.busError)
-        ch.registerAuthError(self.busError)
         ch.registerCancel(self.cancelError)
         return ch
+    
+    def callHandler(self, script, model, method, action):
+        ch = CallHandler(script, model, method, action, self.winID, self.busSys, self.busSes)
+        ch.registerError(self.error)
+        ch.registerDBusError(self.errorDBus)
+        ch.registerAuthError(self.errorDBus)
+        return ch
 
-    def cancelError(self):
-        message = i18n("You are not authorized for this operation.")
-        KMessageBox.sorry(None, message, i18n("Error"))
-
-    def call(self, script, model, method, *args):
-        try:
-            obj = self.busSys.get_object("tr.org.pardus.comar", "/package/%s" % script)
-            iface = dbus.Interface(obj, dbus_interface="tr.org.pardus.comar.%s" % model)
-        except dbus.DBusException, exception:
-            self.errorDBus(exception)
-        try:
-            func = getattr(iface, method)
-            return func(*args)
-        except dbus.DBusException, exception:
-            self.error(exception)
-
-    def callSys(self, method, *args):
-        try:
-            obj = self.busSys.get_object("tr.org.pardus.comar", "/")
-            iface = dbus.Interface(obj, dbus_interface="tr.org.pardus.comar")
-        except dbus.DBusException, exception:
-            self.errorDBus(exception)
-            return
-        try:
-            func = getattr(iface, method)
-            return func(*args)
-        except dbus.DBusException, exception:
-            self.error(exception)
-
-    def busError(self, exception):
-        KMessageBox.error(self, str(exception), i18n("D-Bus Error"))
-        self.setupBusses()
 
     def comarError(self, exception):
         if "Access denied" in exception.message:
             message = i18n("You are not authorized for this operation.")
-            KMessageBox.sorry(self, message, i18n("Error"))
+            QtGui.QMessageBox.warning(self, i18n("Error"), message)
         else:
-            KMessageBox.error(self, str(exception), i18n("COMAR Error"))
+            QtGui.QMessageBox.warning(self, i18n("COMAR Error"), str(exception))
+
+    def cancelError(self):
+        message = i18n("You are not authorized for this operation.")
+        QtGui.QMessageBox.warning(self, i18n("Error"), message)
+
+
+    def busError(self, exception):
+        QtGui.QMessageBox.warning(self, i18n("Comar Error"), i18n("Cannot connect to the DBus! If it is not running you should start it with the 'service dbus start' command in a root console."))
+        sys.exit()
+
+    def openBus(self):
+        try:
+            self.busSys = dbus.SystemBus()
+            self.busSes = dbus.SessionBus()
+        except dbus.DBusException:
+            QtGui.QMessageBox.warning(self, i18n("Unable to connect to DBus."), i18n("DBus Error"))
+            return False
+        return True
 
     def populateList(self):
-        """loadedModules = self.callSys("listLoaded", "Boot.Modules")
-        if loadedModules:
-            for i,j in loadedModules.iteritems():
-                dosya = open("/home/ozirus/Desktop/test.txt","w")
-                dosya.write(i+ "-->" + j)
-                dosya.close()
-        """    
+        def handler(modules):
+            for key in modules:
+                self.loadedModules.append(str(key))
+
+            self.listModules.addItems(self.loadedModules)
+
+        ch = self.callMethod("listLoaded", "tr.org.pardus.comar.boot.modules.get")
+        ch.registerDone(handler)
+        ch.call()
+
+
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
     form = ModuleManagerDlg()
