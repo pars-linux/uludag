c0b1a3d90882aac0463b5d9d0c69f05b701c5fd3
ozirus <ozirus@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-07-18 22:50:49 +0000

    - widget'lara bazı grafik eklemeleri - modul ayar penceresine seçime göre blacklisted, autoloading ve available modülleri listeleme yetisi - modül ayar penceresi listwidget'ına sağ tık ile açılan pop-up menü. (modül düzenlemeleri bununla yapılacak) - başka bazı küçük gui düzenlemeleri

--- trunk/staj-projeleri/boot-modules-arayuzu/src/gui/ui_availablemodules.py
+++ trunk/staj-projeleri/boot-modules-arayuzu/src/gui/ui_availablemodules.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'gui/uis/ui_availablemodules.ui'
 #
-# Created: Fri Jul 18 01:47:45 2008
+# Created: Fri Jul 18 15:40:54 2008
 #      by: PyQt4 UI code generator 4.4.2
 #
 # WARNING! All changes made in this file will be lost!
@@ -13,17 +13,47 @@ class Ui_availableModulesDlg(object):
     def setupUi(self, availableModulesDlg):
         availableModulesDlg.setObjectName("availableModulesDlg")
         availableModulesDlg.setWindowModality(QtCore.Qt.NonModal)
-        availableModulesDlg.resize(291,403)
-        self.listAllModules = QtGui.QListWidget(availableModulesDlg)
-        self.listAllModules.setGeometry(QtCore.QRect(14,12,261,371))
+        availableModulesDlg.resize(291,450)
+
         font = QtGui.QFont()
         font.setPointSize(8)
+
+
+        self.cmbListType = QtGui.QComboBox(availableModulesDlg)
+        self.cmbListType.setObjectName("cmbListType")
+        self.cmbListType.setGeometry(QtCore.QRect(14,10,261,23))
+        self.cmbListType.setFont(font)
+        self.cmbListType.addItem("Select listing filter")
+        self.cmbListType.addItem("All available")
+        self.cmbListType.addItem("Blacklisted")
+        self.cmbListType.addItem("Autoloading")
+
+        self.lblListType = QtGui.QLabel(availableModulesDlg)
+        self.lblListType.setObjectName("lblListType")
+        self.lblListType.setFont(font)
+        self.lblListType.setText("Listing all available modules")
+        self.lblListType.setGeometry(QtCore.QRect(14,37,261,16))
+        
+        
+        self.listAllModules = QtGui.QListWidget(availableModulesDlg)
+        self.listAllModules.setGeometry(QtCore.QRect(14,55,261,375))
         self.listAllModules.setFont(font)
-        self.listAllModules.addAction(QtGui.QAction("Selam",self))
-        self.listAllModules.addAction(QtGui.QAction("Selam",self))
-        self.listAllModules.addAction(QtGui.QAction("Selam",self))
         self.listAllModules.setContextMenuPolicy(QtCore.Qt.ActionsContextMenu)
         self.listAllModules.setObjectName("listAllModules")
+
+        loadAction = QtGui.QAction(QtGui.QIcon(":/load.png"),"&Load", self)
+        self.listAllModules.addAction(loadAction)
+        loadAction.setFont(font)
+
+        blacklistAction = QtGui.QAction(QtGui.QIcon(":/blacklist.png"),"&Blacklist", self)
+        self.listAllModules.addAction(blacklistAction)
+        blacklistAction.setFont(font)
+
+        addAutoloadAction = QtGui.QAction(QtGui.QIcon(":/autoload.png"),"&Add to autoload", self)
+        self.listAllModules.addAction(addAutoloadAction)
+        addAutoloadAction.setFont(font)
+
+        #removeAutoloadAction
 
         self.retranslateUi(availableModulesDlg)
         QtCore.QMetaObject.connectSlotsByName(availableModulesDlg)
