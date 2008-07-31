772b5b9dfed7abd6cf482fbfaa1db5d7bec55a29
makmanalp <makmanalp@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-07-31 11:47:56 +0000

    Fixed setup script to remove before adding new COMAR scripts.

--- staj-projeleri/parmakizi/fingerui/setup.sh
+++ staj-projeleri/parmakizi/fingerui/setup.sh
@@ -1,7 +1,10 @@
+read -p "This is going to remove the baselayout scripts. You might need to reinstall baselayout afterwards."
+
 sudo cp ./action/tr.org.pardus.comar.user.manager.policy /usr/share/PolicyKit/policy/
 sudo cp ./comar/model.xml /etc/comar/
 
 cd ./comar
+sudo hav remove baselayout
 sudo hav register baselayout User.Manager ./User_Manager_Baselayout.py
 
 cd ../ui
