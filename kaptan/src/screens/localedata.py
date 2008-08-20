694fc2df03b9ca708dfaf8963e5bc8a58f76451f
pinar <pinar@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-08-20 12:52:00 +0000

    add locale datas for keyboards

---
+++ kaptan/src/screens/localedata.py
@@ -0,0 +1,234 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2008, TUBITAK/UEKAE
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file.
+#
+
+import gettext
+__trans = gettext.translation('kaptan', fallback=True)
+_ = __trans.ugettext
+
+locales = {
+
+"tr" : {
+        "name" : _("Turkish"),
+        "xkblayout" : "tr",
+        "xkbvariant" : [["f",_("Turkish F")],["",_("Turkish Q")]],
+        },
+
+"en" : {
+        "name" : _("English"),
+        "xkblayout" : "us",
+        "xkbvariant" : None,
+        },
+
+"gb" : {
+        "name" : _("English GB"),
+        "xkblayout" : "gb",
+        "xkbvariant" : None,
+        },
+
+"gr" : {
+        "name" : _("Greek"),
+        "xkblayout" : "gr",
+        "xkbvariant" : None,
+        },
+
+"af" : {
+        "name" : _("Afrikaans"),
+        "xkblayout" : "us",
+        "xkbvariant" : None,
+        },
+
+"vi" : {
+        "name" : _("Vietnamese"),
+        "xkblayout" : "vn",
+        "xkbvariant" : None,
+        },
+
+"ca" : {
+        "name" : _("Catalan"),
+        "xkblayout" : "es",
+        "xkbvariant" : None,
+        },
+
+"it" : {
+        "name" : _("Italian"),
+        "xkblayout" : "it",
+        "xkbvariant" : None,
+        },
+
+"cz" : {
+        "name" : _("Czech"),
+        "xkblayout" : "cz",
+        "xkbvariant" : None,
+        },
+
+"cy" : {
+        "name" : _("Welsh"),
+        "xkblayout" : "gb",
+        "xkbvariant" : None,
+        },
+
+"ar" : {
+        "name" : _("Arabic"),
+        "xkblayout" : "ara",
+        "xkbvariant" : None,
+        },
+
+"et" : {
+        "name" : _("Estonian"),
+        "xkblayout" : "ee",
+        "xkbvariant" : None,
+        },
+
+"es" : {
+        "name" : _("Spanish"),
+        "xkblayout" : "es",
+        "xkbvariant" : None,
+        },
+
+"ru" : {
+        "name" : _("Russian"),
+        "xkblayout" : "ru",
+        "xkbvariant" : None,
+        },
+
+"nl" : {
+        "name" : _("Dutch"),
+        "xkblayout" : "us",
+        "xkbvariant" : None,
+        },
+
+"pt" : {
+        "name" : _("Portuguese"),
+        "xkblayout" : "pt",
+        "xkbvariant" : None,
+        },
+
+"nb" : {
+        "name" : _("Norwegian"),
+        "xkblayout" : "no",
+        "xkbvariant" : None,
+        },
+
+"is" : {
+        "name" : _("Icelandic"),
+        "xkblayout" : "is",
+        "xkbvariant" : None,
+        },
+
+"pl" : {
+        "name" : _("Polish"),
+        "xkblayout" : "pl",
+        "xkbvariant" : None,
+        },
+
+"be" : {
+        "name" : _("Belgium"),
+        "xkblayout" : "be",
+        "xkbvariant" : None,
+        },
+
+"fr" : {
+        "name" : _("French"),
+        "xkblayout" : "fr",
+        "xkbvariant" : None,
+        },
+
+"bg" : {
+        "name" : _("Bulgarian"),
+        "xkblayout" : "bg",
+        "xkbvariant" : None,
+        },
+
+"sl" : {
+        "name" : _("Slovenian"),
+        "xkblayout" : "si",
+        "xkbvariant" : None,
+        },
+
+"hr" : {
+        "name" : _("Croatian"),
+        "xkblayout" : "hr",
+        "xkbvariant" : None,
+        },
+
+"de" : {
+        "name" : _("German"),
+        "xkblayout" : "de",
+        "xkbvariant" : None,
+        },
+
+"da" : {
+        "name" : _("Danish"),
+        "xkblayout" : "dk",
+        "xkbvariant" : None,
+        },
+
+"br" : {
+        "name" : _("Brazilian"),
+        "xkblayout" : "br",
+        "xkbvariant" : None,
+        },
+
+"fi" : {
+        "name" : _("Finnish"),
+        "xkblayout" : "fi",
+        "xkbvariant" : None,
+        },
+
+"hu" : {
+        "name" : _("Hungarian"),
+        "xkblayout" : "hu",
+        "xkbvariant" : None,
+        },
+
+"ja" : {
+        "name" : _("Japanese"),
+        "xkblayout" : "jp",
+        "xkbvariant" : None,
+        },
+
+"ml" : {
+        "name" : _("Malayalam"),
+        "xkblayout" : "us",
+        "xkbvariant" : None,
+        },
+
+"sv" : {
+        "name" : _("Swedish"),
+        "xkblayout" : "se",
+        "xkbvariant" : None,
+        },
+
+"mk" : {
+        "name" : _("Macedonian"),
+        "xkblayout" : "mk",
+        "xkbvariant" : None,
+        },
+
+"sk" : {
+        "name" : _("Slovak"),
+        "xkblayout" : "sk",
+        "xkbvariant" : None,
+        },
+
+"uk" : {
+        "name" : _("Ukrainian"),
+        "xkblayout" : "ua",
+        "xkbvariant" : None,
+        },
+
+"sr" : {
+        "name" : _("Serbian"),
+        "xkblayout" : "rs",
+        "xkbvariant" : None,
+        },
+}
