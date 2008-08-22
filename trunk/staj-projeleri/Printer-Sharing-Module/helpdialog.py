d28cc1fe76a5e33f9b1594488ef8b6dbd5cb35b6
ender.demirkaya <ender.demirkaya@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-08-22 13:49:24 +0000

    modification -scroll bar added, gui bug fixed

--- trunk/staj-projeleri/Printer-Sharing-Module/helpdialog.py
+++ trunk/staj-projeleri/Printer-Sharing-Module/helpdialog.py
@@ -45,23 +45,7 @@ class HelpDialog(QDialog):
 			self.setCaption(self.__tr("MyDialog"))
 			self.ok.setText(self.__tr("OK"))
 			self.label.setText(self.__tr("<b><u>HELP</u></b>"))
-			self.textBrowser.setText(self.__tr("<blockquote><b><u>PRINTER SHARING MODULE</u></b>\n"
-		"<p>Printer Sharing Module uses Common UNIX Printing System(CUPS) and Samba file sharing system together.\n"
-		"This module provides an interface to simplify printer sharing over a local network.</p>\n"
-		"<p><b>Utilities:</b><br>\n"
-		"This module automatically collects the printers added in your system and shows as buttons on the big white part.\n"
-		"This buttons show the name of printer, whether that printer shared or not (top-right of each button) and connection type of printer (top-left of each button)\n"
-		"By clicking these buttons you can see the related properties (name, status, location, URI) of that printer at below the interface.</p>\n"
-		"<p><b>Sharing:</b><br>\n"
-		"After clicking a printer you can share it on local network just by clicking the share button. \n"
-		"Using only this share button proviedes you to share printer to linux machines. \n"
-		"If you want to share that printer also to windows machines you should choose \"Allow Windows Clients\" button.\n"
-		"At \"Windows Clients\" part, you can decide the sharing policy over windows clients. \n"
-		"Related to your network connection; you can choose a local network and only share that network.\n"
-		"Also, you may allow only one ip for using your printer. Then you should choose \"Custom\" below the \"Allowed Addresses\"\n"
-		"Otherwise, you can allow everyone, who can reach your computer, to attain your printer by selecting \"All\" option.</p> \n"
-		"<p><b>Advance Settings:</b>\n"
-		"If you are well informed about CUPS. You can modify the cups server settings by clicking \"Advance\" button.</p></blockquote>"))
+			self.textBrowser.setText(open("help_eng").read())
 		
 		
 	def __tr(self,s,c = None):
