fe43889b3a4357530f36966568228d80f4c08338
fethican.coşkuner <fethican.cokuner@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-08-28 10:04:28 +0000

    added all dependencies of smolt

--- staj-projeleri/paketler/fethican/turbojson/actions.py
+++ staj-projeleri/paketler/fethican/turbojson/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2007 TUBITAK/UEKAE
+# Copyright 2006-2008 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -9,13 +9,11 @@ from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "TurboCheetah-%s" % get.srcVERSION()
+WorkDir = "TurboJson-1.1.3"
 
 def install():
     pythonmodules.install()
 
-    #pisitools.removeDir("/usr/lib/python2.5/site-packages/TurboCheetah-1.0-py2.5.egg-info")
-    pisitools.dosed("%s/usr/bin/TurboCheetah" % get.installDIR(),
+    pisitools.dosed("%s/usr/bin/turbojson" % get.installDIR(),
                     get.installDIR(),
                     "/")
-    pisitools.dodoc("README.txt")
