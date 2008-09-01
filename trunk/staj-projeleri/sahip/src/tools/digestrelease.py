0b235079609fb90c5ac8ad15da58b66f8a8bc16c
emre.aladag <emre.aladag@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-09-01 08:03:29 +0000

    * Prepared a release digester (cleans up useless files, creates a copy) * Renamed sahip.py to sahip, updated setup.py to copy sahip script to /usr/bin * Updated module imports accordingly (sahip.modulename) * Now it's pisi package prepareable

---
+++ trunk/staj-projeleri/sahip/src/tools/digestrelease.py
@@ -0,0 +1,27 @@
+import os 
+po_dir = "po"
+prog_dir = "sahip"
+setup_file = "setup.py"
+digest_dir = "/home/emre/Desktop/sahip-0.1"
+
+
+
+os.system("mkdir %s" % digest_dir)
+os.system("cp -rf %s %s" % (po_dir, digest_dir))
+os.system("cp -rf %s %s" % (prog_dir, digest_dir))
+os.system("cp %s %s" % (setup_file, digest_dir))
+
+os.system("rm -rf %s/%s/.svn *.pot *.mo" % (digest_dir, po_dir))
+os.system("rm -rf %s/%s/.svn" % (digest_dir, prog_dir))
+
+files_to_delete = []
+files = os.listdir(digest_dir+"/"+prog_dir)
+for file in files:
+	if file[-3:]=='.ui':
+		name = file[:-3]
+		pyfile = name+".py"
+		if pyfile in files:
+			files_to_delete.append(digest_dir+"/"+prog_dir+"/"+pyfile)
+			
+if files_to_delete:
+	os.system("rm "+" ".join(files_to_delete))
