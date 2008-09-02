a63c941774154e4bee990c4de45f869a049e4bc4
fethican <fethican@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-09-02 06:35:54 +0000

    added srcVERSION()

--- staj-projeleri/paketler/fethican/decoratortools/actions.py
+++ staj-projeleri/paketler/fethican/decoratortools/actions.py
@@ -9,7 +9,7 @@ from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "DecoratorTools-1.7"
+WorkDir = "DecoratorTools-%s" % get.srcVERSION()
 
 def install():
     pythonmodules.install()
