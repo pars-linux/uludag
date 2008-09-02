492fd45ab60eff6b0599d215dd57330df7f87634
emre.aladag <emre.aladag@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-09-02 13:36:36 +0000

    * Turkish&Spanish translation update * added file sahip (non-py extension) into the update script parameters

--- trunk/staj-projeleri/sahip/src/tools/update-po.sh
+++ trunk/staj-projeleri/sahip/src/tools/update-po.sh
@@ -4,7 +4,7 @@ LANGUAGES=`ls po/*.po`
 
 set -x
 
-xgettext -L "python" -k__tr -k_ sahip/*.py -o po/sahip.pot
+xgettext -L "python" -k__tr -k_ sahip/sahip sahip/*.py -o po/sahip.pot
 for lang in $LANGUAGES
 do
     msgmerge -U $lang po/sahip.pot
