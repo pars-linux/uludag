75b97f51c3103360ec9a831a35bfb2b8621e92e5
fatih.arslan <fatih.arslan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-09-04 06:24:58 +0000

    Changes the checkBoxes to radioButtons

--- staj-projeleri/sinerji/ui_sinerjigui.py
+++ staj-projeleri/sinerji/ui_sinerjigui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file './sinerjigui.ui'
 #
-# Created: Sun Aug 31 12:54:07 2008
+# Created: Thu Sep  4 09:26:53 2008
 #      by: PyQt4 UI code generator 4.4.3
 #
 # WARNING! All changes made in this file will be lost!
@@ -12,9 +12,12 @@ from PyQt4 import QtCore, QtGui
 class Ui_SinerjiGui(object):
     def setupUi(self, SinerjiGui):
         SinerjiGui.setObjectName("SinerjiGui")
-        SinerjiGui.resize(406, 329)
+        SinerjiGui.resize(389, 365)
         self.gridLayout_2 = QtGui.QGridLayout(SinerjiGui)
         self.gridLayout_2.setObjectName("gridLayout_2")
+        self.serverButton = QtGui.QRadioButton(SinerjiGui)
+        self.serverButton.setObjectName("serverButton")
+        self.gridLayout_2.addWidget(self.serverButton, 0, 0, 1, 1)
         self.frame = QtGui.QFrame(SinerjiGui)
         self.frame.setFrameShape(QtGui.QFrame.StyledPanel)
         self.frame.setFrameShadow(QtGui.QFrame.Raised)
@@ -38,30 +41,26 @@ class Ui_SinerjiGui(object):
         self.icobLabel.setAlignment(QtCore.Qt.AlignCenter)
         self.icobLabel.setObjectName("icobLabel")
         self.gridLayout.addWidget(self.icobLabel, 1, 1, 1, 1)
-        self.gridLayout_2.addWidget(self.frame, 1, 0, 1, 3)
+        self.gridLayout_2.addWidget(self.frame, 1, 0, 1, 4)
+        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.gridLayout_2.addItem(spacerItem, 2, 0, 1, 2)
         self.savequitButton = QtGui.QPushButton(SinerjiGui)
         self.savequitButton.setObjectName("savequitButton")
-        self.gridLayout_2.addWidget(self.savequitButton, 2, 1, 1, 1)
+        self.gridLayout_2.addWidget(self.savequitButton, 2, 2, 1, 1)
         self.cancelButton = QtGui.QPushButton(SinerjiGui)
         self.cancelButton.setObjectName("cancelButton")
-        self.gridLayout_2.addWidget(self.cancelButton, 2, 2, 1, 1)
-        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
-        self.gridLayout_2.addItem(spacerItem, 2, 0, 1, 1)
-        self.serverBox = QtGui.QCheckBox(SinerjiGui)
-        self.serverBox.setObjectName("serverBox")
-        self.gridLayout_2.addWidget(self.serverBox, 0, 0, 1, 1)
-        self.clientBox = QtGui.QCheckBox(SinerjiGui)
-        self.clientBox.setChecked(True)
-        self.clientBox.setObjectName("clientBox")
-        self.gridLayout_2.addWidget(self.clientBox, 0, 1, 1, 1)
+        self.gridLayout_2.addWidget(self.cancelButton, 2, 3, 1, 1)
+        self.clientButton = QtGui.QRadioButton(SinerjiGui)
+        self.clientButton.setObjectName("clientButton")
+        self.gridLayout_2.addWidget(self.clientButton, 0, 2, 1, 1)
 
         self.retranslateUi(SinerjiGui)
         QtCore.QMetaObject.connectSlotsByName(SinerjiGui)
 
     def retranslateUi(self, SinerjiGui):
         SinerjiGui.setWindowTitle(QtGui.QApplication.translate("SinerjiGui", "Sinerji", None, QtGui.QApplication.UnicodeUTF8))
+        self.serverButton.setText(QtGui.QApplication.translate("SinerjiGui", "Server Mode", None, QtGui.QApplication.UnicodeUTF8))
         self.savequitButton.setText(QtGui.QApplication.translate("SinerjiGui", "Save and Quit", None, QtGui.QApplication.UnicodeUTF8))
         self.cancelButton.setText(QtGui.QApplication.translate("SinerjiGui", "Cancel", None, QtGui.QApplication.UnicodeUTF8))
-        self.serverBox.setText(QtGui.QApplication.translate("SinerjiGui", "I want to share", None, QtGui.QApplication.UnicodeUTF8))
-        self.clientBox.setText(QtGui.QApplication.translate("SinerjiGui", "I want to be shared", None, QtGui.QApplication.UnicodeUTF8))
+        self.clientButton.setText(QtGui.QApplication.translate("SinerjiGui", "Client Mode", None, QtGui.QApplication.UnicodeUTF8))
 
