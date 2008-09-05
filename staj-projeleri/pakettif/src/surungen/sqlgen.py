e837e84a81599ceae2bb9741b26da7a0f8e58a71
emre.aladag <emre.aladag@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-09-05 13:11:41 +0000

    - Autoincrement id field is added to sqlgen. - sqlgen now drops and re-creates files table. - listing the contents of a package is now supported: /search/package/package_name/list - searching for a term inside a package in 'term in:pkg' format is now supported but needs improvements.
     --This line, and those below, will be ignored-- 
    M pakettif/pathsearch/views.py 
    M pakettif/pathsearch/urls.py 
    M pakettif/src/surungen/sqlgen.py 
    M pakettif/templates/pathsearch/packages.html 
    M pakettif/templates/pathsearch/results.html

--- staj-projeleri/pakettif/src/surungen/sqlgen.py
+++ staj-projeleri/pakettif/src/surungen/sqlgen.py
@@ -8,31 +8,49 @@ import time
 import sys
 import os
 
-if sys.argv[1] in ['--debug', '-d', '-v']:
-    debug = True
-else:
-    debug = False
+debug = False
 
-if sys.argv[1] in ['--help', '-h']:
-    print """Usage: python sqlgen.py [option]
-    Options:
-    -h        Help
-    --help
+if len(sys.argv) > 1:
+    if sys.argv[1] in ['--debug', '-d', '-v']:
+        debug = True
+    else:
+        debug = False
     
-    -d        Debugging
-    --debug
-    -v"""
-    sys.exit()
-
-if os.path.exists('./output.sql'):
-    os.rename('./output.sql','./old-output.sql') 
+    if sys.argv[1] in ['--help', '-h']:
+        print """Usage: python sqlgen.py [option]
+        Options:
+        -h        Help
+        --help
+        
+        -d        Debugging
+        --debug
+        -v"""
+        sys.exit()
 
 start = time.time()
+
+if os.path.exists('./output.sql'):
+    os.rename('./output.sql','./old-output.sql')
+
+f = open("output.sql", "w")
+f.write("""BEGIN;
+DROP TABLE files;
+CREATE TABLE `files` (
+    `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    `package` varchar(60) NOT NULL,
+    `path` varchar(200) NOT NULL
+)
+;
+COMMIT;
+""")
+f.close()
+
 
 pi     = pisi.db.installdb.InstallDB()
 statements = ""
 installed_packages = pi.list_installed()
 counter = 0
+index = 1
 
 for package in installed_packages:
     if debug: print "Package: %s" % package 
@@ -41,8 +59,8 @@ for package in installed_packages:
     
     # For each file, generate an INSERT INTO statement and append it
     for file in files:
-        statements += "INSERT INTO files VALUES('%s', '/%s');\n" % (package, file)
-        
+        statements += "INSERT INTO files VALUES('%d', '%s', '/%s');\n" % (index, package, file)
+        index += 1
     counter+=1
     if counter == 50:
         f = open("output.sql", "a")
@@ -50,7 +68,7 @@ for package in installed_packages:
         f.close()
         statements = ""
         counter = 0
-        print "Appended..."
+        if debug: print "Appended..."
 
 finish = time.time()
 diff = finish - start
