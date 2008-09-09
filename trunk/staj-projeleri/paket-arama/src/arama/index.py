1c4dbdd322d8f79420dc5acdbf96aacbbaa792da
emre.aladag <emre.aladag@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-09-09 10:04:06 +0000

    - Migrated to MySQL. - Added hand-written escaping function but needs to be tested (for SQL Injection) - i18n support - Link to package contents in results.
    TODO:
    - Needs setup file
    - Listing large packages is a painful headache (e.g. boost). Should we use pagination?

--- trunk/staj-projeleri/paket-arama/src/arama/index.py
+++ trunk/staj-projeleri/paket-arama/src/arama/index.py
@@ -1,4 +1,17 @@
 from lib import *
+import gettext
+__trans = gettext.translation('paketarama', fallback=True)
+_ = __trans.ugettext
 
 def index():
-    return header % "" + footer
+    help = TableGenerator((_('Usage'), _('Meaning')),
+                          (
+                            ('term in:pkg  ' , 'term in pkg'),
+                            ('in:pkg'      , 'all paths in pkg'),
+                            ('p:pkg'       , 'packages like pkg'),
+                            ('p:'          , 'all packages'), 
+                            ('term'        , 'path in all packages'),  
+                           )
+                          ).table.code
+ 
+    return header % 'Usage' + help +footer
