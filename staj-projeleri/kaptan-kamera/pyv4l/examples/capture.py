f0daac63c2302ead8b73d430729e1cf4e8078a13
caglar.kilimci <caglar.kilimci@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-09-09 20:00:44 +0000

    simple capture example but there is an error like: *** glibc detected *** python: free(): invalid pointer: 0xb7d38840 ***

---
+++ staj-projeleri/kaptan-kamera/pyv4l/examples/capture.py
@@ -0,0 +1,36 @@
+import v4l
+import Image
+import ImageChops
+WIDTH = 320
+HEIGHT = 240
+vid = v4l.video('/dev/video')
+cap = vid.getCapabilities()
+print "Device Name: %s" % cap[0]
+print "Type: %d" % cap[1]
+print "Channels: %d" % cap[2]
+print "Audios: %d" % cap[3]
+print "Maximum Width: %d" % cap[4]
+print "Maximum Height: %d" % cap[5]
+print "Minimum Width: %d" % cap[6]
+print "Minimum Height: %d" % cap[7]
+#vid.setupImage(WIDTH, HEIGHT, v4l.VIDEO_PALETTE_YUYV)
+vid.setupImage(WIDTH, HEIGHT, v4l.VIDEO_PALETTE_RGB24)
+
+vid.preQueueFrames()
+nextFrame = 0;
+
+try:
+    #while 1:
+	output = vid.getImage(nextFrame)
+	im = Image.fromstring("RGB", (WIDTH, HEIGHT), output)
+
+	# save with PIL as jpeg
+	im.save("xyz.jpg","JPEG")
+	# Display image in xv
+	im.show()
+
+	nextFrame = vid.queueFrame()
+
+except Tkinter.TclError:
+    print "Cant take picture"
+    pass
