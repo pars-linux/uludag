a765495a6604eaba9a6391ab89c358eadd41ccda
emre.aladag <emre.aladag@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-09-11 09:34:02 +0000

    Generated this english dir

---
+++ trunk/staj-projeleri/paket-arama/src/search/settings.py
@@ -0,0 +1,14 @@
+dbhost = 'localhost'
+dbuser = 'root'
+dbpass = ''
+dbname = 'arama'
+root = '/arama'
+
+#dbhost = 'localhost'
+#dbuser = 'arama'
+#dbpass = 'test'
+#dbname = 'arama'
+#root	= '/arama'
+
+
+limit = 100
