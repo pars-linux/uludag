020de5d18cbda53be1c6304b1384acaaac3b3395
emre.aladag <emre.aladag@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-09-11 09:31:29 +0000

    English version generator etc.

--- staj-projeleri/paket-arama/src/tools/gettrans.py
+++ staj-projeleri/paket-arama/src/tools/gettrans.py
@@ -16,32 +16,33 @@ def get_between(line, keyword):
 	return statement
 
 
-messages = {}	
-os.system('sh tools/update-po.sh')
-f = open('po/tr.po')
-lines = ['#/usr/bin/python',
-	 	'# -*- coding: utf-8 -*-',
-	 	'messages = {',]
-
-matched = True
-for line in f:
-	if not matched:
-		print "not matched: ", line
-		# If not matched yet, msgid seen, now looking for msgstr
-		statement = get_between(line, 'msgstr ')
-		translation = statement
-		new = "'%s' : '%s'," % (original, translation)
-		lines.append(new)
-		matched = True
-	else:
-		# If matched, looking for a new msgid
-		statement = get_between(line, 'msgid ')
-		print statement
-		if statement:
-			original = statement
-			matched = False
-
-lines.append('}')
-f = open('arama/msg.py','w')
-f.write("\n".join(lines))
-f.close()
+def get_trans():
+	messages = {}
+	f = open('po/tr.po')
+	lines = ['#/usr/bin/python',
+		 	'# -*- coding: utf-8 -*-',
+		 	'messages = {',]
+	
+	matched = True
+	for line in f:
+		if not matched:
+			# If not matched yet, msgid seen, now looking for msgstr
+			statement = get_between(line, 'msgstr ')
+			translation = statement
+			new = "'%s' : '%s'," % (original, translation)
+			lines.append(new)
+			matched = True
+		else:
+			# If matched, looking for a new msgid
+			statement = get_between(line, 'msgid ')
+			if statement:
+				original = statement
+				matched = False
+	
+	lines.append('}')
+	f = open('arama/msg.py','w')
+	f.write("\n".join(lines))
+	f.close()
+	
+if __name__ == '__main__':
+	get_trans()
