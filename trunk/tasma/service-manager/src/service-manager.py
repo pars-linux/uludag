a26c39b06e0baa8a3726ac84627681a7fa8fb5ac
pinar <pinar@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-09-12 13:29:52 +0000

    minor fixes

--- trunk/tasma/service-manager/src/service-manager.py
+++ trunk/tasma/service-manager/src/service-manager.py
@@ -23,7 +23,7 @@ from dbus.mainloop.qt3 import DBusQtMainLoop
 
 mod_name = 'Service Manager'
 mod_app = 'service-manager'
-mod_version = '2.0.2'
+mod_version = '2.0.3'
 
 def AboutData():
     return KAboutData(
