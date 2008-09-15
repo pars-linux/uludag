ffeae18276426fb8169b8c147ef429cbd8fe5b2e
fatih <fatih@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-09-15 22:59:50 +0000

    if binary package has its own summary, show it

--- trunk/repokit/src/repostats.py
+++ trunk/repokit/src/repostats.py
@@ -126,19 +126,19 @@ def find_pisi_specs(folder):
 def make_table(elements, titles=None):
     def make_row(element):
         return "<tr><td> %s </td></tr>" % ("</td><td>".join(map(str, element)).replace('\n', '<br/>'))
-    
+
     title_html = ""
     if titles:
         title_html = """
             <thead><tr><th>%s</tr></thead>
         """ % "<th>".join(titles)
-    
+
     html = """
         <table>%s<tbody>
         %s
         </tbody></table>
     """ % (title_html, "\n".join(map(make_row, elements)))
-    
+
     return html
 
 def make_url(name, path="./"):
@@ -155,22 +155,22 @@ class HTML:
         self.dest = destdir
         name = self.path("stil.css")
         codecs.open(name, "w", "utf-8").write(css_template)
-    
+
     def path(self, filename):
         name = os.path.join(self.dest, filename)
         path = os.path.dirname(name)
         if not os.path.exists(path):
             os.makedirs(path)
         return name
-    
+
     def write(self, filename, title, content):
         name = self.path(filename)
-        
+
         # Find path to top level index.html
         root = "./"
         if "/" in filename:
             root = "../" * (len(filename.split("/")) - 1)
-        
+
         dict = {
             "title": title,
             "content": content,
@@ -180,7 +180,7 @@ class HTML:
             "menu3": _("Binary Packages"),
             "menu4": _("Packagers")
         }
-        
+
         f = codecs.open(name, "w", "utf-8")
         f.write(html_header % dict)
         f.close()
@@ -189,17 +189,17 @@ class HTML:
 class Histogram:
     def __init__(self):
         self.list = {}
-    
+
     def add(self, name, value=None):
         if value:
             self.list[name] = value
         else:
             self.list[name] = self.list.get(name, 0) + 1
-    
+
     def note(self, name):
         if not self.list.has_key(name):
             self.list[name] = 0
-    
+
     def get_list(self, max=0):
         items = self.list.items()
         items.sort(key=lambda x: x[1], reverse=True)
@@ -248,7 +248,7 @@ class Package:
         self.revRuntimeDeps = []
         self.installedSize = 0
         self.size = 0
-    
+
     def markDeps(self):
         # mark reverse build dependencies
         for d in self.source.spec.source.buildDependencies:
@@ -268,7 +268,7 @@ class Package:
                 if not missing.has_key(p):
                     Missing(p)
                 missing[p].revRuntimeDeps.append(self.name)
-    
+
     def report_html(self, html):
         source = self.source.spec.source
         bDeps = map(lambda x: "<a href='./%s.html'>%s</a>" % (x, x),
@@ -277,30 +277,30 @@ class Package:
             (map(lambda x: x.package, self.pakspec.packageDependencies)))
         rbDeps = map(lambda x: "<a href='./%s.html'>%s</a>" % (x, x), self.revBuildDeps)
         rrDeps = map(lambda x: "<a href='./%s.html'>%s</a>" % (x, x), self.revRuntimeDeps)
-        
+
         sizehtml = ""
         if self.size > 0:
             sizehtml = "<p>%s: %s<br>%s: %s</p>" % (
                 _("Package size"), self.size,
                 _("Installed size"), self.installedSize,
             )
-        
+
         data = """
             <h3>%s <em>%s</em></h3>
-            
+
             %s
-            
+
             <p>%s: %s</p>
-            
+
             <h3>%s</h3>
             <p>%s</p>
-            
+
             <h3>%s</h3>
             <p>%s</p>
-            
+
             <h3>%s</h3>
             <p>%s</p>
-            
+
             <h3>%s</h3>
             <p>%s</p>
         """ % (
@@ -318,7 +318,7 @@ class Package:
             _("Required by (for runtime)"),
             ", ".join(rrDeps),
         )
-        
+
         html.write("binary/%s.html" % self.name, self.name, data)
 
 
@@ -336,7 +336,7 @@ class Source:
         self.uri = svn_uri(path)
         for p in spec.packages:
             Package(self, p)
-    
+
     def report_html(self, html):
         source = self.spec.source
         paks = map(lambda x: "<a href='../binary/%s.html'>%s</a>" % (x, x),
@@ -344,10 +344,10 @@ class Source:
         histdata = map(lambda x: (x.release, x.date, x.version, make_url(x.name, "../packager/"), x.comment), self.spec.history)
         ptch = map(lambda x: "<a href='%s/files/%s'>%s</a>" % (self.uri,
             x.filename, x.filename), source.patches)
-        
+
         titles = _("Release"), _("Release date"), _("Version"), _("Updater"), _("Comment")
         hist = make_table(histdata, titles)
-        
+
         data = """
             <h3>%s <em>%s</em></h3>
             <p>%s</p>
@@ -356,13 +356,13 @@ class Source:
             <h3>%s</h3>
             <p><a href="%s">%s</a></p>
             <p><a href="http://bugs.pardus.org.tr/buglist.cgi?product=Paketler&component=%s&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED">%s</a></p>
-            
+
             <h3>%s</h3>
             <p>%s</p>
-            
+
             <h3>%s</h3>
             %s
-            
+
             <h3>%s</h3>
             %s
         """ % (
@@ -385,7 +385,7 @@ class Source:
             _("Patches"),
             "<br>".join(ptch),
         )
-        
+
         html.write("source/%s.html" % self.name, self.name, data)
 
 
@@ -397,45 +397,45 @@ class Packager:
         else:
             name = spec.source.packager.name
             email = spec.source.packager.email
-        
+
         if not packagers.has_key(name):
             self.sources = []
             self.updates = []
             self.name = name
             self.email = email
             packagers[name] = self
-        
+
         if update:
             packagers[name].updates.append((spec.source.name, update.release, update.comment))
         else:
             packagers[name].sources.append(spec.source.name)
             for update in spec.history:
                 Packager(spec, update)
-    
+
     def report_html(self, html):
         srcs = map(lambda x: ("<a href='../source/%s.html'>%s</a>" % (x, x), ), self.sources)
         srcs.sort()
         upds = map(lambda x: (u"<b><a href='../source/%s.html'>%s</a> (%s)</b><br>%s<br>" % (
             x[0], x[0], x[1], x[2]), ), self.updates)
-        
+
         data = """
             <p>%s: %s (%s)</p>
         """ % (_("Packager"), self.name, mangle_email(self.email))
-        
+
         data += """
             <div class='statstat'>
             <h3>%s</h3><p>
             %s
             </p></div>
         """ % (_("Maintained packages:"), make_table(srcs))
-        
+
         data += """
             <div class='statstat'>
             <h3>%s</h3><p>
             %s
             </p></div>
         """ % (_("Package updates:"), make_table(upds))
-        
+
         html.write("packager/%s.html" % self.name, self.name, data)
 
 
@@ -453,7 +453,7 @@ class Repository:
         self.total_installed_size = 0
         self.total_size = 0
         self.installed_sizes = {}
-    
+
     def processPspec(self, path, spec):
         # new classes
         Packager(spec)
@@ -479,7 +479,7 @@ class Repository:
             f.close()
         except:
             pass
-    
+
     def scan(self):
         for pak in find_pisi_specs(self.path):
             spec = pisi.specfile.SpecFile()
@@ -491,19 +491,19 @@ class Repository:
             self.processPspec(pak, spec)
         for p in packages.values():
             p.markDeps()
-    
+
     def processPisi(self, path):
         zip = zipfile.ZipFile(path, "r")
-        
+
         size = os.stat(path).st_size
         self.total_size += size
-        
+
         doc = piksemel.parseString(zip.read("metadata.xml"))
         p = doc.getTag("Package")
         name = p.getTagData("Name")
         inst_size = int(p.getTagData("InstalledSize"))
         self.total_installed_size += inst_size
-        
+
         doc = piksemel.parseString(zip.read("files.xml"))
         for item in doc.tags("File"):
             tname = item.getTagData("Type")
@@ -516,19 +516,19 @@ class Repository:
                 self.installed_sizes[tname] += sz
             else:
                 self.installed_sizes[tname] = sz
-        
+
         pak = packages.get(name, None)
         if not pak:
             return
-        
+
         pak.size = size
         pak.installedSize = inst_size
-    
+
     def scan_bins(self, binpath):
         binpaks = filter_latest_packages(glob.glob(binpath + "/*.pisi"))
         for pak in binpaks:
             self.processPisi(pak)
-    
+
     def report_html(self, html):
         table = (
             (_("Number of source packages"), self.nr_sources),
@@ -541,9 +541,9 @@ class Repository:
                 (_("Archived size of packages"), self.total_size),
                 (_("Installed size of packages"), self.total_installed_size),
             )
-        
+
         data = make_table(table)
-        
+
         data += """
             <div class='statstat'>
             <h3>%s</h3><p>
@@ -553,7 +553,7 @@ class Repository:
             _("Most patched:"),
             make_table(map(lambda x: (make_url(x[0], "./source/"), x[1]), self.mostpatched.get_list(5)))
         )
-        
+
         data += """
             <div class='statstat'>
             <h3>%s</h3><p>
@@ -563,7 +563,7 @@ class Repository:
             _("Longest build scripts:"),
             make_table(map(lambda x: (make_url(x[0], "./source/"), x[1]), self.longpy.get_list(5)))
         )
-        
+
         if self.total_installed_size > 0:
             data += """
                 <h3>%s</h3><p>
@@ -573,29 +573,29 @@ class Repository:
                 _("Total file sizes by file type:"),
                 make_table(self.installed_sizes.items())
             )
-        
+
         html.write("index.html", _("Information"), data)
-        
+
         titles = (
             "<a href='packagers_by_name.html'>%s</a>" % _("Packager"),
             "<a href='packagers.html'>%s</a>" % _("Package count")
         )
-        
+
         people = self.people.get_list()
         people = map(lambda x: ("<a href='./packager/%s.html'>%s</a>" % (x[0], x[0]), x[1]), people)
         html.write("packagers.html", _("Packagers (by package count)"), make_table(people, titles))
-        
+
         people.sort(key=lambda x: x[0])
         html.write("packagers_by_name.html", _("Packagers (by name)"), make_table(people, titles))
-        
+
         titles = _("Package name"), _("Version"), _("Summary")
-        
+
         srclist = map(lambda x: (make_url(x.name, "source/"), x.spec.getSourceVersion(), x.spec.source.summary), sources.values())
         srclist.sort(key=lambda x: x[0])
         data = make_table(srclist, titles)
         html.write("sources.html", _("Source Packages"), data)
-        
-        binlist = map(lambda x: (make_url(x.name, "binary/"), x.source.spec.getSourceVersion(), x.source.spec.source.summary), packages.values())
+
+        binlist = map(lambda x: (make_url(x.name, "binary/"), x.source.spec.getSourceVersion(), x.pakspec.summary or x.source.spec.source.summary), packages.values())
         binlist.sort(key=lambda x: x[0])
         data = make_table(binlist, titles)
         html.write("binaries.html", _("Binary Packages"), data)
@@ -607,13 +607,13 @@ def make_report(destdir, source_repo, binary_repo=None):
     repo = Repository(source_repo)
     printu(_("Scanning source repository...\n"))
     repo.scan()
-    
+
     if binary_repo:
         printu(_("Scanning binary packages...\n"))
         repo.scan_bins(binary_repo)
-    
+
     html = HTML(destdir)
-    
+
     repo.report_html(html)
     for p in packagers.values():
         p.report_html(html)
@@ -632,17 +632,17 @@ def main(args):
         opts, args = getopt.gnu_getopt(args, "ho:", ["help", "output="])
     except:
         usage()
-    
+
     if args == []:
         usage()
-    
+
     destdir = "paksite"
     for o, v in opts:
         if o in ("-h", "--help"):
             usage()
         if o in ("-o", "--output"):
             destdir = v
-    
+
     if len(args) > 1:
         make_report(destdir, args[0], args[1])
     else:
