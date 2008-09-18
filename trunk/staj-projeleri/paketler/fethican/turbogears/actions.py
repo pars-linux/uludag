2c1e44bf6d6559ad61a7654c1eedf57e519b9169
fethican <fethican@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-09-18 13:35:34 +0000

    file types and make-up

--- trunk/staj-projeleri/paketler/fethican/turbogears/actions.py
+++ trunk/staj-projeleri/paketler/fethican/turbogears/actions.py
@@ -9,8 +9,6 @@ from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "TurboGears-%s" % get.srcVERSION()
-
 def install():
     pythonmodules.install()
 
