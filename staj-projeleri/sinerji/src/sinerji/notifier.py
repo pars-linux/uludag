ec68156a76525c87f93cd8a3c24c73d074179a51
fatih.arslan <fatih.arslan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-09-18 11:57:37 +0000

    Fixes problem if there are more than one client that want to connect to the same server

--- staj-projeleri/sinerji/src/sinerji/notifier.py
+++ staj-projeleri/sinerji/src/sinerji/notifier.py
@@ -12,6 +12,7 @@ class Notifier(QObject):
         QObject.__init__(self)
         bus = dbus.SessionBus()
         self.notifyid = 0
+        self.notifyData = {}
         try:
             object  = bus.get_object("org.freedesktop.Notifications", "/org/freedesktop/Notifications")
             self.iface = dbus.Interface(object, dbus_interface='org.freedesktop.Notifications')
@@ -22,11 +23,12 @@ class Notifier(QObject):
             traceback.print_exc()
 
     def click_handler(self, id, button):
-        if id == self.notifyid:
+        if id in self.notifyData:
             if button == "reject":
                 self.emit(SIGNAL("rejectServer"), ())
             elif button == "accept":
-                self.emit(SIGNAL("acceptServer"), self.hostname)
+                print "*************** Notify" , self.notifyData[id]
+                self.emit(SIGNAL("acceptServer"), self.notifyData[id])
             else:
                 pass
 
@@ -34,20 +36,16 @@ class Notifier(QObject):
         #If notification popup is closed
         self.emit(SIGNAL("rejectServer"), ())
 
-    def stopNotifier(self):
-        self.iface.CloseNotification(self.notifyid)
+    def stopNotifier(self, hostname):
+        for id in self.notifyData:
+            if not self.notifyData[id] == hostname:
+                self.iface.CloseNotification(id)
 
     def show(self, icon, header, msg, time=3000, button=[]):
-        """ ## If getPos do work than use it
-        
-        if not pos or pos[0] < 0 or pos[1] < 0:
-            hints = {}
-        else:
-            hints= {"x": pos[0], "y": pos[1]}
-        """
         # close previous notification window
         self.hostname = header
         #self.buttonList = ["accept", unicode("Accept"), "reject", unicode("Reject")]
+
         self.notifyid = self.iface.Notify("sinerji",
                          0,
                          icon,
@@ -56,3 +54,5 @@ class Notifier(QObject):
                          button,
                          {},
                          time)
+        self.notifyData[self.notifyid] = header
+
