8736978f96f62fe2fb579d8a391860a14272dfb3
emre.aladag <emre.aladag@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-09-19 11:56:32 +0000

    Unfortunately, this project is going to be developed in Django, not in pure mod_python. Thanks for the translation contributors but we have to put this into BLACKHOLE to avoid confusion.
    The new project will be trunk/staj-projeleri/paket-arama or pakettif (might rename it)

--- staj-projeleri/paket-arama/src/arama/default-settings.py
+++
@@ -1,9 +0,0 @@
-dbhost = 'localhost'
-dbuser = 'root'
-dbpass = ''
-dbname = 'arama'
-root = '/arama'
-
-versions = ['2007', '2008',]
-
-limit = 500
--- staj-projeleri/paket-arama/src/tools/clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -f ../arama/*.pyc
-rm -f ../arama/*.pyo
-rm -f ../arama/*.py~
-
