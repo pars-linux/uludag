c234057473950437d4710269cd31998232a6247a
emre.aladag <emre.aladag@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-10-05 12:39:19 +0000

    Some URL fixes... It needs to learn it is not in the root of the world (I mean apache...) but in search sub-dir.

--- staj-projeleri/paket-arama/search/urls.py
+++ staj-projeleri/paket-arama/search/urls.py
@@ -3,8 +3,10 @@ from search.settings import DOCUMENT_ROOT
 
 
 urlpatterns = patterns('',
-    (r'^$', 'search.pathsearch.views.index'),
-    (r'^search/', include('search.pathsearch.urls')),
     (r'^media/(.*)$', 'django.views.static.serve', {'document_root': '%s/media' % DOCUMENT_ROOT, 'show_indexes': True}),
+    #(r'^search/', include('search.pathsearch.urls')),
+    (r'^', include('search.pathsearch.urls')),
 
 )
+
+
