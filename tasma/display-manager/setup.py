e7abcf90b742f2e246b6c08faccc94bfc675ca2c
Fatih Aşıcı <fatih-asici@users.noreply.github.com>
2008-11-01 17:30:41 +0000

    bump

--- tasma/display-manager/setup.py
+++ tasma/display-manager/setup.py
@@ -16,7 +16,7 @@ import shutil
 from distutils.core import Extension
 import kdedistutils
 
-version = '0.3.4'
+version = '0.3.5'
 
 distfiles = """
     README
--- tasma/display-manager/src/display-manager.py
+++ tasma/display-manager/src/display-manager.py
@@ -30,7 +30,7 @@ from utility import *
 
 mod_name = 'Display Manager'
 mod_app = 'display-manager'
-mod_version = '0.3.4'
+mod_version = '0.3.5'
 
 def AboutData():
     return KAboutData(
