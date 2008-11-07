c270a8069e6e883f520c3f8eeb69e14a093c129f
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-11-07 09:02:10 +0000

    Network.Link script for net-tools

--- branches/net-link-2.0/scripts/net-tools.py
+++ branches/net-link-2.0/scripts/net-tools.py
@@ -1,13 +1,53 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+MSG_ETHERNET = {
+    "en": "Ethernet",
+    "tr": "Ethernet Ağları",
+}
+
+MSG_DHCP_FAILED = {
+    "en": "Unable to get address.",
+    "tr": "Adres alınamadı.",
+}
+
+import os
+from pardus import netutils, iniutils
+
+def listProfiles():
+    db = iniutils.iniDB(os.path.join("/etc/network-link", script()))
+    return db.listDB()
+
+class Profile:
+    def __init__(self, name):
+        self.name = name
+        self.db = iniutils.iniDB(os.path.join("/etc/network-link", script()))
+        self.info = self.db.getDB(self.name)
+
+    def delete(self):
+        self.db.remDB(self.name)
+
+    def save(self):
+        self.db.setDB(self.name, self.info)
+
+def stopSameDevice(name):
+    profile = Profile(name)
+    device = profile.info["device"]
+    for pn in listProfiles():
+        if pn == name:
+            continue
+        pro = Profile(pn)
+        if pro.info["device"] == device:
+            setState(pn, "down")
+
+# Network.Link methods
+
 def linkInfo():
-    d = {
+    return {
         "type": "net",
-        "name": "Ethernet", # TODO: Localize value
+        "name": _(MSG_ETHERNET),
         "modes": "device,net,auto",
     }
-    return d
 
 def authMethods():
     # TODO: Raise an exception here. "auth" mode not supported
@@ -26,28 +66,38 @@ def deviceModes():
     return []
 
 def deviceList():
-    # TODO: Return device dictionary
-    return {}
+    iflist = {}
+    for ifc in netutils.interfaces():
+        if ifc.isEthernet() and not ifc.isWireless():
+            uid = ifc.deviceUID()
+            info = netutils.deviceName(uid)
+            iflist[uid] = info
+    return iflist
 
 def scanRemote(device):
     # TODO: Raise an exception here. "remote_scan" mode not supported
     return []
 
 def setDevice(name, device):
-    # TODO: Add/update device
-    pass
+    profile = Profile(name)
+    profile.info["device"] = device
+    profile.save()
 
 def setDeviceMode(name, mode):
-    # TODO: Add/update device mode
+    # TODO: Raise an exception here. "device_mode" mode not supported
     pass
 
 def deleteConnection(name):
-    # TODO: Delete profile
-    pass
+    profile = Profile(name)
+    profile.delete()
 
 def setAddress(name, mode, address, mask, gateway):
-    # TODO:
-    pass
+    profile = Profile(name)
+    profile.info["net_mode"] = mode
+    profile.info["net_address"] = address
+    profile.info["net_mask"] = mask
+    profile.info["net_gateway"] = gateway
+    profile.save()
 
 def setRemote(name, remote):
     # TODO: Raise an exception here. "remote" mode not supported
@@ -74,20 +124,80 @@ def getAuthParameters(name):
     return {}
 
 def getState(name):
-    # TODO: Return state
-    return "down"
+    profile = Profile(name)
+    return profile.info.get("state", "down")
 
 def setState(name, state):
-    # TODO: Change state
-    pass
+    profile = Profile(name)
+    iface = netutils.findInterface(profile.info["device"])
+    if state == "up":
+        if profile.info.get("net_mode", "auto") == "auto":
+            # Stop other profiles on same device
+            stopSameDevice(name)
+            # Notify clients
+            notify("Net.Link", "stateChanged", (name, "connecting", ""))
+            # Start DHCP client
+            ret = iface.startAuto()
+            if ret == 0 and iface.isUp():
+                address = iface.getAddress()
+                if address:
+                    # Save state to profile database
+                    profile.info["state"] = "up " + address[0]
+                    profile.save()
+                    # Notify clients
+                    notify("Net.Link", "stateChanged", (name, "up", address[0]))
+                else:
+                    iface.down()
+                    # Save state to profile database
+                    profile.info["state"] = "down"
+                    profile.save()
+                    # Notify clients
+                    notify("Net.Link", "stateChanged", (name, "inaccesible", _(MSG_DHCP_FAILED)))
+            else:
+                iface.down()
+                # Save state to profile database
+                profile.info["state"] = "down"
+                profile.save()
+                # Notify clients
+                notify("Net.Link", "stateChanged", (name, "inaccesible", _(MSG_DHCP_FAILED)))
+        else:
+            try:
+                net_address = profile.info["net_address"]
+                net_mask = profile.info["net_mask"]
+                net_gateway = profile.info["net_gateway"]
+            except KeyError:
+                return
+            # Set address
+            iface.setAddress(net_address, net_mask)
+            iface.up()
+            # Set default gateway
+            route = netutils.Route()
+            route.setDefault(net_gateway)
+            # Save state to profile database
+            profile.info["state"] = "up " + net_address
+            profile.save()
+            # Notify clients
+            notify("Net.Link", "stateChanged", (name, "up", net_address))
+    elif state == "down":
+        if profile.info.get("net_mode", "auto") == "auto":
+            iface.stopAuto()
+        iface.down()
+        # Save state to profile database
+        profile.info["state"] = "down"
+        profile.save()
+        # Notify clients
+        notify("Net.Link", "stateChanged", (name, "down", ""))
 
 def connections():
-    # TODO: Return list of profile names
-    return []
+    return listProfiles()
 
-def connectionInfo(name=None):
-    # TODO: Return profile info
-    return {}
+def connectionInfo(name):
+    # TODO: Expand
+    profile = Profile(name)
+    return {
+        "name": name,
+        "state": profile.info.get("state", "down"),
+    }
 
 def kernelEvent(data):
     # TODO: Handle UDEV event
