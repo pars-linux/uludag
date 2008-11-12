2043b624ba02f4e13ee0aac6d588aa8f77052508
Gokmen Goksel <gokmen@goksel.me>
2008-11-12 15:19:15 +0000

    * Cleanup, they are moved to projeler ..

--- arto/kde/index.php
+++
@@ -1,4 +0,0 @@
-<?
-echo "This directory is using for kde integration.\n<br>\nFor more information please visit our homepage: <a 
-href=\"http://arto.fasafiso.org\" target=\"_blank\">http://arto.fasafiso.org</a>";
-?>
--- arto/preview.php
+++
@@ -1,7 +0,0 @@
-<?php
-include("globals.php");
-                $preview = get_filepaths($_GET["id"]);
-                set_smarty_vars("preview",$preview);
-                $smarty->display("preview.html");
-                die();
-?>
