f7f526cdd495d281bc321ac072efeffee30d5791
gokmen.goksel <gokmen.goksel@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-11-12 18:30:49 +0000

    * Created tasma-2009 branch  * Created service-manager for KDE4 with PyKDE4 as KApplication    It is just a template for now.

---
+++ branches/tasma-2009/service-manager/src/about.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2008, TUBITAK/UEKAE
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file.
+#
+
+# PyKDE4 Stuff
+from PyKDE4.kdecore import *
+
+# Application Data
+appName     = "services"
+catalog     = ""
+programName = ki18n("Service manager")
+version     = "0.1"
+description = ki18n("Service Manager")
+license     = KAboutData.License_GPL
+copyright   = ki18n("(c) 2008 TUBITAK/UEKAE")
+text        = ki18n(None)
+homePage    = "http://www.pardus.org.tr/eng/projects"
+bugEmail    = "bugs@pardus.org.tr"
+aboutData   = KAboutData(appName, catalog, programName, version, description, license, copyright, text, homePage, bugEmail)
+
+# Authors
+aboutData.addAuthor (ki18n("Gökmen GÖKSEL"),    ki18n("Current Maintainer"))
+aboutData.addAuthor (ki18n("Bahadır Kandemir"), ki18n("COMAR Author"))
+
---
+++ branches/tasma-2009/service-manager/src/main.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2008, TUBITAK/UEKAE
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file.
+#
+
+# System
+import sys
+import comar
+
+# Qt Stuff
+from PyQt4 import QtGui
+from PyQt4.QtCore import *
+
+# PyKDE4 Stuff
+from PyKDE4.kdeui import *
+from PyKDE4.kdecore import *
+
+# Application Stuff
+from about import aboutData
+from ui import Ui_mainManager
+
+# DBUS-QT
+from dbus.mainloop.qt import DBusQtMainLoop
+
+class MainManager(QtGui.QWidget):
+    def __init__(self, *args):
+        QtGui.QWidget.__init__(self, None)
+
+        # Create the ui
+        self.ui = Ui_mainManager()
+        self.ui.setupUi(self)
+
+        # Call Comar
+        self.link = comar.Link()
+
+        # Fill service list
+        self.getServices()
+
+    def handleServices(self, package, exception, results):
+
+        # Handle request and fill the listServices in the ui
+        if not exception:
+            serviceName, serviceDesc, serviceState = results
+            self.ui.listServices.addItem("%s - %s - %s - %s" % (package, serviceName, serviceDesc, serviceState))
+
+    def getServices(self):
+
+        # Get service list from comar link
+        self.link.System.Service.info(async=self.handleServices)
+
+class Manager(KMainWindow):
+    def __init__ (self, *args):
+        KMainWindow.__init__(self)
+        self.resize (640, 480)
+        self.setCentralWidget(MainManager(self))
+
+if __name__ == '__main__':
+
+    # Set Command-line arguments
+    KCmdLineArgs.init(sys.argv, aboutData)
+
+    # Create a Kapplitcation instance
+    app = KApplication()
+
+    # DBUS MainLoop
+    DBusQtMainLoop(set_as_default = True)
+
+    # Create Main Widget
+    mainWindow = Manager(None, "service-manager")
+    mainWindow.show()
+
+    # Create connection for lastWindowClosed signal to quit app
+    app.connect(app, SIGNAL("lastWindowClosed()"), app.quit)
+
+    # Run the application
+    app.exec_()
+
