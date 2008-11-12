15c74289e63293301a45c1da3e9570eaa9f44102
gokmen.goksel <gokmen.goksel@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-11-12 15:19:15 +0000

    * Cleanup, they are moved to projeler ..

--- trunk/arto/banner.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "<a href=\"http://www.pardus.org.tr/\" target=\"_blank\"><img src=\"images/banners/banner03.png\" border=\"0\"></a>";
-?>
--- trunk/pardon/clean.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    require_once ("etc/config.php");
-    require_once ("lib/functions.php");
-    $Sql = "DELETE FROM {$config['db']['users_table']} WHERE UserState='N'";
-    db_connection('connect', $config['db']['host'].':'.$config['db']['port'], $config['db']['user'], $config['db']['pass'], $config['db']['dbname'], $config['db']['ctype']);
-    mysql_query($Sql);
-
-?>
