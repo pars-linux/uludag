406f0762e4e704ce1ab4e8b9e96a5dcee8ce3a90
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2008-12-02 06:49:31 +0000

    Auto connect for mudur

--- branches/net-link-2.0/examples/test-mudur.py
+++ branches/net-link-2.0/examples/test-mudur.py
@@ -17,11 +17,34 @@ import comar
 link = comar.Link()
 
 for package in link.Network.Link:
-    for profile in link.Network.Link[package].connections():
-        info = link.Network.Link[package].connectionInfo(profile)
-        if info.get("state", "down").startswith("up"):
-            print "Bringing up %s" % info["device_id"]
-            link.Network.Link[package].setState(profile, "up", quiet=True)
+    info = link.Network.Link[package].linkInfo()
+    if info["type"] == "net":
+        for profileName in link.Network.Link[package].connections():
+            profileInfo = link.Network.Link[package].connectionInfo(profileName)
+            if profileInfo.get("state", "down").startswith("up"):
+                print "Bringing up %s" % profileInfo["device_id"]
+                link.Network.Link[package].setState(profileName, "up", quiet=True)
+    elif info["type"] == "wifi":
+        devices = {}
+        for deviceId in link.Network.Link[package].deviceList():
+            devices[deviceId] = []
+            for point in link.Network.Link[package].scanRemote(deviceId):
+                devices[deviceId].append(point["remote"])
+        #
+        for profileName in link.Network.Link[package].connections():
+            profileInfo = link.Network.Link[package].connectionInfo(profileName)
+            if profileInfo.get("state", "down").startswith("up") and profileInfo.get("device_id", None) in devices:
+                print "Bringing up %s" % profileInfo["device_id"]
+                link.Network.Link[package].setState(profileName, "up", quiet=True)
+                break
+        #
+        for profileName in link.Network.Link[package].connections():
+            profileInfo = link.Network.Link[package].connectionInfo(profileName)
+            if profileInfo.get("device_id", None) in devices and profileInfo["remote"] in devices[profileInfo["device_id"]]:
+                print "Bringing up %s" % profileInfo["device_id"]
+                link.Network.Link[package].setState(profileName, "up", quiet=True)
+                break
+
 
 # Give COMAR some time to get process ID
 time.sleep(5)
