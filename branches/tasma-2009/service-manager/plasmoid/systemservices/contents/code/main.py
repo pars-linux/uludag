1bd1b843e63f67d98a2904b3cb4486afb13120ca
gokmen.goksel <gokmen.goksel@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-01-13 14:40:26 +0000

    * Plasma is crashing if we use DBUSQtMainLoop in our plasmoid :(    - I added another main.py which doesnt use DBUSQtMainLoop but there are some problems still..    - I am still happy with this shit..

--- branches/tasma-2009/service-manager/plasmoid/systemservices/contents/code/main.py
+++ branches/tasma-2009/service-manager/plasmoid/systemservices/contents/code/main.py
@@ -23,28 +23,37 @@ from config import SystemServicesConfig
 from dbus.mainloop.qt import DBusQtMainLoop
 
 class WidgetSystemServices(QGraphicsWidget):
-    def __init__(self, parent, data):
+    def __init__(self, parent, name, state=None):
         QGraphicsWidget.__init__(self, parent)
-        self._data = data
-
+        self.name = name
         self.layout = QGraphicsLinearLayout(Qt.Horizontal, self)
 
         self.label = Plasma.Label(self)
-        self.label.setText(data)
+        self.label.setText(name)
         self.layout.addItem(self.label)
 
         self.layout.addStretch()
 
-        self.start = Plasma.PushButton(self)
-        self.start.setText("Start")
-        self.layout.addItem(self.start)
+        self.switcher = Plasma.TabBar(self)
+        self.switcher.addTab("Start")
+        self.switcher.addTab("Stop")
+        self.layout.addItem(self.switcher)
 
-        self.stop = Plasma.PushButton(self)
-        self.stop.setText("Stop")
-        self.layout.addItem(self.stop)
+class WidgetStack(QGraphicsWidget):
+    def __init__(self, parent):
+        QGraphicsWidget.__init__(self, parent)
+        self.layout = QGraphicsLinearLayout(Qt.Vertical, self)
+        self._widgets = {}
 
-    def getData(self):
-        return self._data
+        # Our animator instance to animate adding/deleting widgets
+        self.animator = Plasma.Animator.self()
+
+    def addItem(self, widget):
+        # Animate creation of widget
+        self.animator.animateItem(widget, 0)
+        self.layout.addItem(widget)
+
+        self._widgets[widget.name] = widget
 
 class SystemServicesApplet(plasmascript.Applet):
     """ Our main applet derived from plasmascript.Applet """
@@ -55,11 +64,11 @@ class SystemServicesApplet(plasmascript.Applet):
         # Our Comar Link
         self.link = comar.Link()
 
+        # Available services
+        self._services = list(self.link.System.Service)
+
     def init(self):
         """ Const method for initializing the applet """
-
-        # Our widget stack
-        self._widgets = {}
 
         # Configuration interface support comes with plasma
         self.setHasConfigurationInterface(True)
@@ -72,103 +81,16 @@ class SystemServicesApplet(plasmascript.Applet):
 
         # It gets default plasma theme's background
         self.theme.setImagePath("widgets/background")
-        self.theme.resize(self.size())
 
         # Resize current theme as applet size
         self.theme.resize(self.size())
 
+        self.mainWidget = None
+        self.layout = None
+
         # Create config dialog
-        self.prepareConfigDialog()
-
-        # Main layout
-        self.createLayout()
-
-        # Our animator instance to animate adding/deleting widgets
-        self.animator = Plasma.Animator.self()
-
-        # Call comar to get all services infos
-        self._just_update = False
-        self.getServices()
-
-    def createLayout(self):
-        """ Creates main layout for ServiceItemWidgets """
-        self.layout = QGraphicsLinearLayout(Qt.Vertical, self.applet)
-        self.layout.setContentsMargins(0,0,0,0)
-        self.layout.setSizePolicy(QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding))
-        self.setLayout(self.layout)
-
-        self.resize(200,200)
-        self.constraintsEvent(Plasma.SizeConstraint)
-
-    def handleServices(self, package, exception, results):
-        """ Handles comar feedbacks and creates the widgets in applet."""
-        if not exception:
-            package = str(package)
-
-            # If service is enabled create a proper widget and add it to the plasmoid
-            if package in self.config_ui.enabledServices:
-                # Create widget which describes and handles service state
-                widget = WidgetSystemServices(self.applet, package)
-
-                # Add widget to layout
-                self.layout.addItem(widget)
-
-                # Animate creation of widget
-                self.animator.animateItem(widget, 0)
-
-                # Add widget to our widget stack
-                self._widgets[package] = widget
-
-                # Update the size of Plasmoid
-                self.constraintsEvent(Plasma.SizeConstraint)
-
-            # This method handles all service info requests
-            # but in configuration list we don't need to add them again..
-            if not self._just_update:
-                self.config_ui.addItemToList(package)
-
-    def constraintsEvent(self, constraints):
-        if constraints & Plasma.SizeConstraint:
-            if len(self._widgets) > 0:
-                # FIXME Find a better solution
-                height = max(self._widgets.values()[0].size().height() * (len(self._widgets) + 1) + 30, self.size().height())
-                width = max(self._widgets.values()[0].size().width(), self.size().width())
-                self.resize(width, height)
-
-            self.layout.updateGeometry()
-            self.theme.resize(self.size())
-
-    def handler(self, package, signal, args):
-        pass
-
-    def getServices(self):
-        """ Makes comar call to get all services information """
-
-        # It listens System.Service signals and route them to handler method
-        self.link.listenSignals("System.Service", self.handler)
-
-        # Get service list from comar link
-        self.link.System.Service.info(async=self.handleServices)
-
-    def updateList(self):
-
-        # call hide for each widget
-        for wi in self._widgets.values():
-            wi.hide()
-
-        # remove them from layout
-        for i in range(self.layout.count()):
-            self.layout.removeAt(i)
-
-        # and reset the widgets stack
-        self._widgets = {}
-
-        # and create a new layout
-        self.createLayout()
-
-        # Get service list from comar link again and again..
-        self._just_update = True
-        self.link.System.Service.info(async=self.handleServices)
+        if self.prepareConfigDialog():
+            self.initPlasmoid()
 
     def prepareConfigDialog(self):
         windowTitle = str(self.applet.name()) + " Settings"
@@ -179,6 +101,9 @@ class SystemServicesApplet(plasmascript.Applet):
         self.config_ui = SystemServicesConfig(self.dialog, self.config())
         self.dialog.setMainWidget(self.config_ui)
 
+        for package in self._services:
+            self.config_ui.addItemToList(package)
+
         self.dialog.setButtons(KDialog.ButtonCodes(KDialog.ButtonCode(KDialog.Ok | KDialog.Cancel | KDialog.Apply)))
         self.dialog.showButton(KDialog.Apply, False)
 
@@ -186,8 +111,49 @@ class SystemServicesApplet(plasmascript.Applet):
         self.connect(self.dialog, SIGNAL("okClicked()"), self, SLOT("configAccepted()"))
 
         if self.config_ui.enabledServices[0] == '':
-            # we need new kdebindings for that
             self.setConfigurationRequired(True, "Click configure to select services")
+            self.resize(200,200)
+            return False
+        return True
+
+    def initPlasmoid(self):
+        # Layout
+        if not self.layout:
+            self.layout = QGraphicsLinearLayout(Qt.Vertical, self.applet)
+            self.layout.setContentsMargins(0,0,0,0)
+            self.layout.setSizePolicy(QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding))
+
+            self.setLayout(self.layout)
+            self.constraintsEvent(Plasma.SizeConstraint)
+
+        if self.mainWidget:
+            self.mainWidget.hide()
+            self.layout.removeAt(0)
+            del self.mainWidget
+
+        self.mainWidget = WidgetStack(self.applet)
+        self.layout.addItem(self.mainWidget)
+
+        for package in self._services:
+            # If service is enabled create a proper widget and add it to the plasmoid
+            if package in self.config_ui.enabledServices:
+                # Create widget which describes and handles service state
+                widget = WidgetSystemServices(self.applet, package)
+
+                # Add widget to mainWidget
+                self.mainWidget.addItem(widget)
+
+                # Update the size of Plasmoid
+                self.constraintsEvent(Plasma.SizeConstraint)
+
+    def constraintsEvent(self, constraints):
+        if constraints & Plasma.SizeConstraint:
+            self.layout.updateGeometry()
+            self.theme.resize(self.size())
+
+    def handler(self, package, signal, args):
+        import os
+        os.system("notify bibi bibi ...")
 
     def showConfigurationInterface(self):
         self.dialog.show()
@@ -223,10 +189,9 @@ class SystemServicesApplet(plasmascript.Applet):
             self.setConfigurationRequired(False, '')
 
         # and update the widget
-        self.updateList()
+        self.initPlasmoid()
 
 def CreateApplet(parent):
-    # DBUS MainLoop
-    DBusQtMainLoop(set_as_default = True)
+    applet = SystemServicesApplet(parent)
+    return applet
 
-    return SystemServicesApplet(parent)
