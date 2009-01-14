41f40e41c6d51fe73ce66d6e10772b8896e02e8f
Gokmen Goksel <gokmen@goksel.me>
2009-01-14 10:25:58 +0000

    * Po updates
    BUG:FIXED:8937

--- yali4/tools/update-po.sh
+++ yali4/tools/update-po.sh
@@ -7,8 +7,8 @@ set -x
 xgettext -L "python" -k__tr -k_ yali4/gui/Ui/*.py yali4/gui/*.py yali4/*.py -o po/yali4.pot
 for lang in $LANGUAGES
 do
-    msgcat --use-first -o $TEMP $lang po/yali4.pot
-    msgmerge -U $lang $TEMP
-    #sed -e '/^#~/d' $TEMP > $lang
+    #msgcat --use-first -o $TEMP $lang po/yali4.pot
+    msgmerge -q -o $TEMP $lang po/yali4.pot
+    cat $TEMP > $lang
 done
 rm -f $TEMP
