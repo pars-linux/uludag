4aa7c7a1ee54ea855dec48ee088353376b9914b3
ozirus <ozirus@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-01-24 13:19:13 +0000

    kimi widgetlar adama döndü

--- staj-projeleri/boot-modules-arayuzu/src/gui/ui_availablemodules.py
+++ staj-projeleri/boot-modules-arayuzu/src/gui/ui_availablemodules.py
@@ -17,7 +17,7 @@ class Ui_availableModulesDlg(object):
         availableModulesDlg.setWindowIcon(QtGui.QIcon("tt:/program-icon.png"))
 
         font = QtGui.QFont()
-        font.setPointSize(9)
+        font.setPointSize(10)
 
 
         self.cmbListType = QtGui.QComboBox(availableModulesDlg)
