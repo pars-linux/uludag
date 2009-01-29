2701b0c6cf8bded998b466cb6524192e9a7dfe0f
pinar <pinar@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-01-29 14:47:43 +0000

    improve main stack, thanx to gokmen :)

--- trunk/kaptan4/src/kaptan.py
+++ trunk/kaptan4/src/kaptan.py
@@ -6,21 +6,68 @@ from PyQt4 import QtCore, QtGui
 import gui
 
 from gui.kaptanMain import Ui_kaptanUI
+import gui.ScrWelcome as welcomeWidget
 
 class Kaptan(QtGui.QMainWindow):
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
         self.ui = Ui_kaptanUI()
+
         self.ui.setupUi(self)
+        self.screens = [welcomeWidget,welcomeWidget]
+        self.screenData = None
+        self.moveInc = 1
+
+        self.createWidgets(self.screens)
 
         QtCore.QObject.connect(self.ui.buttonNext, QtCore.SIGNAL("clicked()"), self.slotNext)
         QtCore.QObject.connect(self.ui.buttonBack, QtCore.SIGNAL("clicked()"), self.slotBack)
 
-    def slotNext(self):
-        pass
+    # returns the id of current stack
+    def getCur(self, d):
+        new   = self.ui.mainStack.currentIndex() + d
+        total = self.ui.mainStack.count()
+        if new < 0: new = 0
+        if new > total: new = total
+        return new
 
+    # move to id numbered step
+    def setCurrent(self, id=None):
+        if id:
+            self.stackMove(id)
+
+    # execute next step
+    def slotNext(self,dryRun=False):
+        _w = self.ui.mainStack.currentWidget()
+        ret = _w.execute()
+        if ret:
+            self.stackMove(self.getCur(self.moveInc))
+            self.moveInc = 1
+
+    # execute previous step
     def slotBack(self):
-        pass
+        _w = self.ui.mainStack.currentWidget()
+        _w.backCheck()
+        self.stackMove(self.getCur(self.moveInc * -1))
+        self.moveInc = 1
+
+    # move to id numbered stack
+    def stackMove(self, id):
+        if not id == self.ui.mainStack.currentIndex() or id==0:
+            self.ui.mainStack.setCurrentIndex(id)
+            _w = self.ui.mainStack.currentWidget()
+            _w.update()
+            _w.shown()
+
+    # create all widgets and add inside stack
+    def createWidgets(self, screens=[]):
+        self.ui.mainStack.removeWidget(self.ui.page)
+        for screen in screens:
+            _scr = screen.Widget()
+            print _scr.title," added."
+            self.ui.mainStack.addWidget(_scr)
+
+        self.stackMove(0)
 
     def disableNext(self):
         self.buttonNext.setEnabled(False)
