35bed5e8559bc4efa13d7e693eb3dee851c2a46e
pinar <pinar@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-02-05 14:53:19 +0000

    package configuration code

--- kaptan4/src/gui/ScrPackage.py
+++ kaptan4/src/gui/ScrPackage.py
@@ -17,14 +17,66 @@ from PyKDE4.kdecore import ki18n
 from gui.ScreenWidget import ScreenWidget
 from gui.packageWidget import Ui_packageWidget
 
+import contribrepo
+import pisi
+
+isUpdateOn = False
+
 class Widget(QtGui.QWidget, ScreenWidget):
     title = ki18n("Package Manager")
     desc = ki18n("Configure package manager settings")
+
+    # min update time
+    updateTime = 12
 
     def __init__(self, *args):
         QtGui.QWidget.__init__(self,None)
         self.ui = Ui_packageWidget()
         self.ui.setupUi(self)
+
+        self.flagRepo = 0
+
+        # set updateTime
+        self.ui.updateInterval.setValue(self.updateTime)
+
+        # set repo name and address
+        self.repoName = "contrib"
+        self.repoAddress = "http://paketler.pardus.org.tr/contrib-2008/pisi-index.xml.bz2"
+
+        # add signals here
+
+        # create a db object
+        self.repodb = pisi.db.repodb.RepoDB()
+        n = 1 # temporary index variable for repo names
+
+        # control if we already have contrib repo
+        # if so, hide configuration box
+        if self.repodb.has_repo_url(self.repoAddress):
+            self.ui.groupBoxRepo.hide()
+        else:
+            # control if we already have the same repo name
+            if self.repodb.has_repo(self.repoName):
+                tmpRepoName = self.repoName
+                # if so, try to give a name like "contribn"
+                for r in self.repodb.list_repos():
+                    if self.repodb.has_repo(tmpRepoName):
+                        tmpRepoName = self.repoName + str(n)
+                        n = n +1
+                    else:
+                        break
+                self.repoName = tmpRepoName
+
+    def slotContribRepo(self):
+        if self.checkBoxContrib.isChecked():
+            if self.addRepo(self.repoName, self.repoAddress) == False:
+                self.flagRepo = 1
+                self.ui.checkBoxContrib.setChecked(0)
+
+                message = i18n("You are not authorized for this operation.")
+                KMessageBox.error(self, message, ki18n("Authentication Error!"))
+        else:
+            if self.flagRepo != 1:
+                self.removeRepo(self.repoName)
 
     def shown(self):
         pass
