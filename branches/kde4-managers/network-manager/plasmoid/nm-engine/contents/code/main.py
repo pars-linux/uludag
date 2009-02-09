0c22ad682b7ae0ce4c8871e4f909ce044a46bf24
gokmen.goksel <gokmen.goksel@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-02-09 14:51:31 +0000

    * Network Manager Data engine for querying network profiles ..

---
+++ branches/kde4-managers/network-manager/plasmoid/nm-engine/contents/code/main.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2009, TUBITAK/UEKAE
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file.
+#
+
+import comar
+
+from PyQt4.QtCore import *
+from PyKDE4.kdecore import *
+from PyKDE4 import plasmascript
+
+# DBUS MainLoop
+from dbus.mainloop.qt import DBusQtMainLoop
+DBusQtMainLoop(set_as_default = True)
+
+net_packages = ["net_tools", "wireless_tools"]
+link = comar.Link()
+
+def asString(lst, pkg):
+    return map(lambda x:'%s.%s' % (pkg, str(x)), lst)
+
+class NMEngine(plasmascript.DataEngine):
+    def __init__(self, parent, args=None):
+        plasmascript.DataEngine.__init__(self, parent)
+
+    def init(self):
+        self.setMinimumPollingInterval(333)
+        link.listenSignals("Net.Link", self.handler)
+
+    def handler(self, package, signal, args):
+        self.updateSourceEvent("%s.%s" % (package, args[0]))
+
+    def sources(self):
+        sources = []
+        for package in net_packages:
+            profiles = asString(list(link.Net.Link[package].connections()), package)
+            for profile in profiles:
+                sources.append(profile)
+        return sources
+
+    def sourceRequestEvent(self, name):
+        return self.updateSourceEvent(name)
+
+    def updateSourceEvent(self, _name):
+        pkg, name = _name.split('.')
+        _st = link.Net.Link[str(pkg)].connectionInfo(str(name))
+        self.setData(_name, "State", QVariant(str(_st['state'])))
+        self.setData(_name, "Device", QVariant(str(_st['device_id'])))
+        return True
+
+def CreateDataEngine(parent):
+    return NMEngine(parent)
