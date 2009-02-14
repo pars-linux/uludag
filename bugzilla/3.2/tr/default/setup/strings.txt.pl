45bc12a09576184b3e55ef6237734a7813a9dcda
necmettin <necmettin@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-02-14 22:51:30 +0000

    Bugzilla translation continued

--- bugzilla/3.2/tr/default/setup/strings.txt.pl
+++ bugzilla/3.2/tr/default/setup/strings.txt.pl
@@ -15,6 +15,7 @@
 # The Original Code is the Bugzilla Bug Tracking System.
 #
 # Contributor(s): Max Kanat-Alexander <mkanat@bugzilla.org>
+# Translator(s):  Necmettin Begiter <necmettin@pardus.org.tr> (Turkish)
 
 # This file contains a single hash named %strings, which is used by the
 # installation code to display strings before Template-Toolkit can safely
@@ -27,37 +28,33 @@
 # Please keep the strings in alphabetical order by their name.
 
 %strings = (
-    any  => 'any',
-    blacklisted => '(blacklisted)',
-    checking_for => 'Checking for',
-    checking_dbd      => 'Checking available perl DBD modules...',
-    checking_optional => 'The following Perl modules are optional:',
-    checking_modules  => 'Checking perl modules...',
-    done => 'done.',
-    header => "* This is Bugzilla ##bz_ver## on perl ##perl_ver##\n"
-            . "* Running on ##os_name## ##os_ver##",
+    any  => 'herhangi bir',
+    blacklisted => '(kara listede)',
+    checking_for => 'Kontrol ediliyor',
+    checking_dbd      => 'Kurulu perl DBD modülleri aranıyor...',
+    checking_optional => 'Aşağıdaki Perl modülleri tercihe bağlı:',
+    checking_modules  => 'Perl modülleri aranıyor...',
+    done => 'tamamlandı.',
+    header => "* Perl ##perl_ver## üzerinde Bugzilla ##bz_ver##\n"
+            . "* ##os_name## ##os_ver## üzerinde çalışıyor",
     install_all => <<EOT,
 
-To attempt an automatic install of every required and optional module
-with one command, do:
+İster gerekli, ister tercihe bağlı olsun, tüm modüllerin otomatik kurulumu için:
 
   ##perl## install-module.pl --all
 
 EOT
     install_data_too_long => <<EOT,
-WARNING: Some of the data in the ##table##.##column## column is longer than
-its new length limit of ##max_length## characters. The data that needs to be
-fixed is printed below with the value of the ##id_column## column first and
-then the value of the ##column## column that needs to be fixed:
+UYARI: ##table##.##column## sütunundaki verinin bir kısmı yeni uzunluk sınırından (##max_length##) daha uzun. Düzeltilmesi gereken veri önce ##id_column## sütununun değeri, ardından da ##column## sütununun değeri olarak aşağıya listelenmiştir:
 
 EOT
-    install_module => 'Installing ##module## version ##version##...',
-    module_found => "found v##ver##",
-    module_not_found => "not found",
-    module_ok => 'ok',
-    module_unknown_version => "found unknown version",
-    template_precompile   => "Precompiling templates...",
-    template_removing_dir => "Removing existing compiled templates...",
+    install_module => '##module## sürüm ##version## yükleniyor...',
+    module_found => "s##ver## bulundu",
+    module_not_found => "bulunamadı",
+    module_ok => 'tamam',
+    module_unknown_version => "bilinmeyen bir sürüm bulundu",
+    template_precompile   => "Şablonlar ön-derleniyor...",
+    template_removing_dir => "Varolan derlenmiş şablonlar kaldırılıyor...",
 );
 
 1;
