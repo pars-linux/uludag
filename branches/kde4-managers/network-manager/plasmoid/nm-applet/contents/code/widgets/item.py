ebbd7caae2686f31c75779076f13f29a5709554b
gokmen.goksel <gokmen.goksel@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-02-19 10:52:57 +0000

    * Use QPushButton instead of QFrame..

--- branches/kde4-managers/network-manager/plasmoid/nm-applet/contents/code/widgets/item.py
+++ branches/kde4-managers/network-manager/plasmoid/nm-applet/contents/code/widgets/item.py
@@ -2,10 +2,15 @@
 # -*- coding: utf-8 -*-
 
 # Qt Libs
-from PyQt4.QtGui import QWidget, QFrame, QGraphicsLinearLayout, QPixmap
+from PyQt4.QtGui import QWidget, QFrame, QGraphicsLinearLayout, QPixmap, QIcon
+from PyQt4.QtCore import SIGNAL
 
 # Custom Widget
 from itemui import Ui_connectionItem
+
+_css = "padding-left:4px;text-align:left;padding-right:4px;"
+_cssEnter = "border:1px solid rgba(255,255,255,90);border-radius:5px;background-color: rgba(255, 255, 255, 40);"
+_cssLeave = "border:1px solid rgba(0,0,0,0);background-color: rgba(0,0,0,0);"
 
 class ConnectionItem(QWidget):
 
@@ -15,38 +20,38 @@ class ConnectionItem(QWidget):
         self.ui = Ui_connectionItem()
         self.ui.setupUi(self)
 
-        self.ui.connectionSignal.hide()
-
         self.dialog = dialog
         self.name = name
         self.package = package
-        self.setText(name)
+
+        self.connect(self.ui.button, SIGNAL("clicked()"), self.toggleConnection)
         self.setState()
 
     def enterEvent(self, event):
-        self.ui.frame.setFrameShadow(QFrame.Raised)
+        self.ui.button.setStyleSheet(_css + _cssEnter)
 
     def leaveEvent(self, event):
-        self.ui.frame.setFrameShadow(QFrame.Plain)
+        self.ui.button.setStyleSheet(_css + _cssLeave)
 
-    def mouseReleaseEvent(self, event):
+    def toggleConnection(self):
         self.dialog.parent.hide()
         self.dialog.iface.toggle(self.package, self.name)
 
     def setText(self, text):
-        self.ui.connectionName.setText(text)
+        self.ui.button.setText(text)
         self.updateGeometry()
 
     def setState(self, state="down", ip=''):
         if state.startswith("up"):
-            self.ui.connectionStatus.setPixmap(QPixmap(":/icons/check.png"))
-            self.setText("<b>%s</b> %s" % (self.name, ip))
+            icon = QIcon(QPixmap(":/icons/check.png"))
+            self.setText("%s - %s" % (self.name, ip))
         elif state == "connecting":
-            self.ui.connectionStatus.setPixmap(QPixmap(":/icons/working.png"))
-            self.setText("<b>%s</b>" % self.name)
-        else:
-            self.ui.connectionStatus.setPixmap(
-                    QPixmap(":/icons/network-%s.png" % self.package))
+            icon = QIcon(QPixmap(":/icons/working.png"))
             self.setText(self.name)
+        elif state == "down":
+            icon = QIcon(QPixmap(":/icons/network-%s.png" % self.package))
+            self.setText(self.name)
+        self.ui.button.setIcon(icon)
         self.lastState = state
+        del icon
 
--- branches/kde4-managers/network-manager/plasmoid/nm-applet/contents/code/widgets/itemui.py
+++ branches/kde4-managers/network-manager/plasmoid/nm-applet/contents/code/widgets/itemui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'nm-applet/contents/ui/item.ui'
 #
-# Created: Thu Feb 12 23:20:02 2009
+# Created: Thu Feb 19 12:46:18 2009
 #      by: PyQt4 UI code generator 4.4.4
 #
 # WARNING! All changes made in this file will be lost!
@@ -12,51 +12,33 @@ from PyQt4 import QtCore, QtGui
 class Ui_connectionItem(object):
     def setupUi(self, connectionItem):
         connectionItem.setObjectName("connectionItem")
-        connectionItem.resize(228, 32)
-        connectionItem.setMinimumSize(QtCore.QSize(0, 32))
-        connectionItem.setMaximumSize(QtCore.QSize(16777215, 32))
-        self.gridLayout_2 = QtGui.QGridLayout(connectionItem)
-        self.gridLayout_2.setMargin(0)
-        self.gridLayout_2.setSpacing(0)
-        self.gridLayout_2.setObjectName("gridLayout_2")
-        self.frame = QtGui.QFrame(connectionItem)
-        self.frame.setMinimumSize(QtCore.QSize(0, 30))
-        self.frame.setMaximumSize(QtCore.QSize(16777215, 32))
-        self.frame.setFrameShape(QtGui.QFrame.StyledPanel)
-        self.frame.setFrameShadow(QtGui.QFrame.Plain)
-        self.frame.setLineWidth(0)
-        self.frame.setObjectName("frame")
-        self.gridLayout = QtGui.QGridLayout(self.frame)
+        connectionItem.resize(210, 28)
+        connectionItem.setMinimumSize(QtCore.QSize(0, 28))
+        connectionItem.setMaximumSize(QtCore.QSize(16777215, 30))
+        self.gridLayout = QtGui.QGridLayout(connectionItem)
         self.gridLayout.setMargin(0)
+        self.gridLayout.setSpacing(0)
         self.gridLayout.setObjectName("gridLayout")
-        self.horizontalLayout = QtGui.QHBoxLayout()
-        self.horizontalLayout.setObjectName("horizontalLayout")
-        self.connectionStatus = QtGui.QLabel(self.frame)
-        self.connectionStatus.setMinimumSize(QtCore.QSize(22, 22))
-        self.connectionStatus.setMaximumSize(QtCore.QSize(22, 22))
-        self.connectionStatus.setPixmap(QtGui.QPixmap(":/icons/network-wireless_tools.png"))
-        self.connectionStatus.setScaledContents(True)
-        self.connectionStatus.setObjectName("connectionStatus")
-        self.horizontalLayout.addWidget(self.connectionStatus)
-        self.connectionName = QtGui.QLabel(self.frame)
-        self.connectionName.setObjectName("connectionName")
-        self.horizontalLayout.addWidget(self.connectionName)
-        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
-        self.horizontalLayout.addItem(spacerItem)
-        self.connectionSignal = QtGui.QProgressBar(self.frame)
-        self.connectionSignal.setMaximumSize(QtCore.QSize(80, 16777215))
-        self.connectionSignal.setProperty("value", QtCore.QVariant(24))
-        self.connectionSignal.setTextVisible(False)
-        self.connectionSignal.setObjectName("connectionSignal")
-        self.horizontalLayout.addWidget(self.connectionSignal)
-        self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 1, 1)
-        self.gridLayout_2.addWidget(self.frame, 0, 0, 1, 1)
+        self.button = QtGui.QPushButton(connectionItem)
+        self.button.setMinimumSize(QtCore.QSize(0, 28))
+        self.button.setMaximumSize(QtCore.QSize(16777215, 28))
+        self.button.setStyleSheet("""padding-left:4px;
+padding-right:4px;
+text-align:left;
+border:1px solid rgba(0,0,0,0);""")
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(":/icons/network-wireless_tools.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.button.setIcon(icon)
+        self.button.setFlat(False)
+        self.button.setObjectName("button")
+        self.gridLayout.addWidget(self.button, 0, 0, 1, 1)
 
         self.retranslateUi(connectionItem)
         QtCore.QMetaObject.connectSlotsByName(connectionItem)
 
     def retranslateUi(self, connectionItem):
         connectionItem.setWindowTitle(QtGui.QApplication.translate("connectionItem", "Form", None, QtGui.QApplication.UnicodeUTF8))
-        self.connectionName.setText(QtGui.QApplication.translate("connectionItem", "test", None, QtGui.QApplication.UnicodeUTF8))
+        self.button.setText(QtGui.QApplication.translate("connectionItem", "PushButton", None, QtGui.QApplication.UnicodeUTF8))
 
+import data_rc
 import data_rc
