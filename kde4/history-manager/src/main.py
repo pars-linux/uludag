8770c8ce468b6f6d6312ac50333f0a8da2c6728e
isbaran <isbaran@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-02-22 16:20:01 +0000

    default files

--- kde4/history-manager/src/main.py
+++ kde4/history-manager/src/main.py
@@ -25,28 +25,6 @@ from ui_mainwindow import Ui_MainManager
 
 from dbus.mainloop.qt import DBusQtMainLoop
 
-class MainManager(QtGui.QWidget):
-    def __init__(self, parent, standAlone=True):
-        QtGui.QWidget.__init__(self, parent)
-
-        self.ui = Ui_MainManager()
-
-        if standAlone:
-            self.ui.setupUi(self)
-        else:
-            self.ui.setupUi(parent)
-
-        self.link = comar.Link()
-
-    def getServices(self):
-        self.link.listenSignals("System.Service", self.handler)
-        # Get service list from comar link
-        self.link.System.Service.info(async=self.handleServices)
-
-    def handler(self, package, signal, args):
-        self.widgets[package].setState(args[1])
-        # print args, signal, package
-
 class Manager(KMainWindow):
     def __init__ (self, *args):
         KMainWindow.__init__(self)
