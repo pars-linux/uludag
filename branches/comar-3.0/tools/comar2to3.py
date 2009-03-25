0b8c6bb8657b54271242c93c70f5d670d0cb41d3
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-03-25 06:38:00 +0000

    - Do nothing if COMAR 3.0 DB is already initialized

--- branches/comar-3.0/tools/comar2to3.py
+++ branches/comar-3.0/tools/comar2to3.py
@@ -6,7 +6,8 @@ import glob
 import os
 import time
 
-COMAR_DB = "/var/db/comar/code"
+COMAR_DB_OLD = "/var/db/comar/code"
+COMAR_DB_NEW = "/var/db/comar3/scripts"
 COMAR_ADDRESS = "tr.org.pardus.comar.updated"
 COMAR_IFACE = "tr.org.pardus.comar"
 COMAR_TIMEOUT = 10
@@ -16,11 +17,19 @@ def main():
         print "Must be run as root."
         return -1
 
+    if len(os.listdir(COMAR_DB_NEW)):
+        # COMAR 3.0 database already initialized, do nothing.
+        return 0
+
     bus = dbus.SystemBus()
     obj = None
 
+    # Old PiSi releases start new COMAR service after all postInstall
+    # operations complete. We start our own COMAR service so we 
+    # guarantee that we register all scripts to new COMAR.
     os.system("/usr/sbin/comar3 -b %s &" % COMAR_ADDRESS)
 
+    # Wait until service gets ready
     timeout = COMAR_TIMEOUT
     while timeout > 0:
         try:
@@ -35,12 +44,13 @@ def main():
         print "Unable to start new COMAR service."
         return -2
 
-    for filename in os.listdir(COMAR_DB):
+    # Register all scripts
+    for filename in os.listdir(COMAR_DB_OLD):
         if filename.endswith(".py"):
             _group, _class, _app = filename.split("_", 2)
             _model = "%s.%s" % (_group, _class)
             _app = _app.rsplit(".py", 1)[0]
-            obj.register(_app, _model, os.path.join(COMAR_DB, filename), dbus_interface=COMAR_IFACE)
+            obj.register(_app, _model, os.path.join(COMAR_DB_OLD, filename), dbus_interface=COMAR_IFACE)
             print filename
 
     return 0
