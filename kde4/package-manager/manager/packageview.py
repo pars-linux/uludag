28b06bb6ad6e8476de880daaa9487a22693bfb1c
Faik Uygur <faikuygur@gmail.com>
2009-03-27 11:41:12 +0000

    we need to customize QTreeView

---
+++ kde4/package-manager/manager/packageview.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2009, TUBITAK/UEKAE
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file.
+#
+
+from PyQt4 import QtGui
+from PyQt4.QtCore import *
+
+class PackageView(QtGui.QTreeView):
+    def __init__(self, parent=None):
+        QtGui.QTreeView.__init__(self, parent)
