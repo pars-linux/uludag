5100ba932e90e447852a2090b1ed47dd709997f7
Gokmen Goksel <gokmen@goksel.me>
2009-03-30 20:37:52 +0000

    * update ui,po ...

--- kde4/service-manager/manager/po/update-po.sh
+++ kde4/service-manager/manager/po/update-po.sh
@@ -4,7 +4,7 @@ LANGUAGES=`ls po/*.po`
 TEMP=`mktemp`
 set -x
 
-xgettext -L "python" -k__tr -k_ -ki18n src/*.py -o po/base.pot
+xgettext -L "python" -k__tr -k_ -ki18n build/*.py -o po/base.pot
 for lang in $LANGUAGES
 do
     #msgcat --use-first -o $TEMP $lang po/yali4.pot
