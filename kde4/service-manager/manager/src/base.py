24e768683a47b1b67def2f41a4cdbb086875344b
Gokmen Goksel <gokmen@goksel.me>
2009-04-02 08:59:08 +0000

    * Split modules, add license..

---
+++ kde4/service-manager/manager/src/base.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2009, TUBITAK/UEKAE
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file.
+#
+
+# System
+import sys
+import comar
+
+# Qt Stuff
+from PyQt4 import QtGui
+from PyQt4.QtCore import *
+
+# PyKDE4 Stuff
+from PyKDE4.kdeui import *
+from PyKDE4.kdecore import *
+
+# Application Stuff
+from about import aboutData
+from ui import Ui_mainManager
+from uiitem import Ui_ServiceItemWidget
+from widgets import ServiceItemWidget, ServiceItem
+
+class MainManager(QtGui.QWidget):
+    def __init__(self, parent, standAlone=True):
+        QtGui.QWidget.__init__(self, parent)
+
+        # Create the ui
+        self.ui = Ui_mainManager()
+
+        if standAlone:
+            self.ui.setupUi(self)
+        else:
+            self.ui.setupUi(parent)
+
+        # Call Comar
+        self.link = comar.Link()
+        self.widgets = {}
+
+        # Fill service list
+        self.getServices()
+
+    def handleServices(self, package, exception, results):
+        # Handle request and fill the listServices in the ui
+        if not exception:
+            item = ServiceItem(self.ui.listServices, results, package)
+            self.widgets[package] = ServiceItemWidget(results, package, self)
+            self.ui.listServices.setItemWidget(item, self.widgets[package])
+            item.setSizeHint(QSize(38,48))
+
+    def getServices(self):
+        self.link.listenSignals("System.Service", self.handler)
+        # Get service list from comar link
+        self.link.System.Service.info(async=self.handleServices)
+
+    def handler(self, package, signal, args):
+        self.widgets[package].setState(args[1])
+        # print args, signal, package
+
+
