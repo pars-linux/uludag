f749b53d710f9deb50f757ff7043271dbfdffa76
gokmen.goksel <gokmen.goksel@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-04-06 11:25:54 +0000

    * New NM branch

---
+++ trunk/kde4/network-manager/new-nm/src/main.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2008-2009, TUBITAK/UEKAE
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file.
+#
+
+# System
+import sys
+
+# Qt Stuff
+from PyQt4.QtCore import SIGNAL
+
+# PyKDE4 Stuff
+from PyKDE4.kdeui import *
+from PyKDE4.kdecore import *
+
+def CreatePlugin(widget_parent, parent, component_data):
+    from kcmodule import ServiceManager
+    return ServiceManager(component_data, parent)
+
+if __name__ == '__main__':
+
+    # Service Manager
+    from standalone import ServiceManager
+
+    # Application Stuff
+    from about import aboutData
+
+    # DBUS-QT
+    from dbus.mainloop.qt import DBusQtMainLoop
+
+    # Set Command-line arguments
+    KCmdLineArgs.init(sys.argv, aboutData)
+
+    # Create a Kapplitcation instance
+    app = KApplication()
+
+    # DBUS MainLoop
+    DBusQtMainLoop(set_as_default = True)
+
+    # Create Main Widget
+    mainWindow = ServiceManager(None, 'service-manager')
+    mainWindow.show()
+
+    # Create connection for lastWindowClosed signal to quit app
+    app.connect(app, SIGNAL('lastWindowClosed()'), app.quit)
+
+    # Run the application
+    app.exec_()
+
