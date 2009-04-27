5b408a34fb416f488936501c90100923fc1fd8cf
Faik Uygur <faikuygur@gmail.com>
2009-04-27 13:19:48 +0000

    stajer app

---
+++ stajer/app/controllers/students_controller.rb
@@ -0,0 +1,46 @@
+class StudentsController < ApplicationController
+  before_filter :login_required
+
+  def index
+    @students = Student.all.sort {|a,b| b.score <=> a.score}
+  end
+
+  def show
+    @student = Student.find(params[:id])
+    @comment = Comment.new(:student => @student)
+  end
+
+  def new
+    @student = Student.new
+    5.times { @student.documents.build }
+  end
+
+  def create
+    @student = Student.new(params[:student])
+    if @student.save
+      flash[:notice] = 'Added successfully.'
+      redirect_to students_url
+    else
+      render :action => :new
+    end
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    if @student.update_attributes(params[:student])
+      flash[:notice] = 'Updated succesfully.'
+    else
+      render :action => :edit
+    end
+  end
+
+  def destroy
+    @student = Student.find(params[:id])
+    @student.destroy
+    redirect_to students_url
+  end
+end
