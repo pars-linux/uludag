c6adff57ae1deb2200d3887c283a930413fea18b
mete <mete@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-05-04 11:53:17 +0000

    Add StorageFormat class for base decorator

--- trunk/pare/src/formats/__init__.py
+++ trunk/pare/src/formats/__init__.py
@@ -7,4 +7,53 @@
 # Software Foundation; either version 2 of the License, or (at your option)
 # any later version.
 #
-# Please read the COPYING file.
+# Please read the COPYING file
+
+
+class StorageFormat(object):
+    """Generic Storage Format"""
+
+    type = None
+    partedFlag = None
+    _resizable = False
+    _bootable = False
+    _migratable = False
+    _maxSize = 0
+    _minSize = 0
+    _dump = False
+    _check = False
+
+    def __init__(self, *args, **kwargs):
+        """
+
+        """
+    @property
+    def type(self):
+        return self.type
+
+    @property
+    def resizable(self):
+        return self._resizable
+
+    @property
+    def bootable(self):
+        self._bootable
+
+    @property
+    def migratable(self):
+        return self._migratable
+
+    @property
+    def dump(self):
+        return self._dump
+
+    @property
+    def check(self):
+        return self._check
+
+    @property
+    def minSize(self):
+        return self._minSize
+
+    @property
+    def maxSize(self):
