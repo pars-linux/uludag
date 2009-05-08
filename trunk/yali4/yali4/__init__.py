d90da6198dcd3940a63e8ea6055eaae557c561fc
gokmen.goksel <gokmen.goksel@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-05-08 15:45:58 +0000

    * New release 2.1.3

--- trunk/yali4/yali4/__init__.py
+++ trunk/yali4/yali4/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2005-2008, TUBITAK/UEKAE
+# Copyright (C) 2005-2009, TUBITAK/UEKAE
 #
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free
@@ -10,7 +10,7 @@
 # Please read the COPYING file.
 #
 
-__version__ = "2.1.2"
+__version__ = "2.1.3"
 
 import sys
 import exceptions
