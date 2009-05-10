c4dfa5e92a21ace720f584f17371a011225dd386
isbaran <isbaran@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-05-10 17:25:11 +0000

    sortfilterproxy and resizetocontents is very slow, return to old style

--- kde4/history-manager/src/listitem.py
+++ kde4/history-manager/src/listitem.py
@@ -6,124 +6,10 @@ from PyQt4.QtCore import *
 
 from PyKDE4.kdecore import *
 
-import inspect
-from new import instancemethod
+class NewOperation(QTreeWidgetItem):
+    def __init__(self, operation, parent=None):
+        super(NewOperation, self).__init__(parent)
 
-ICON, DATE, TIME = range(3)
-
-class OperationDataModel(QAbstractItemModel):
-    def __init__(self):
-        super(OperationDataModel, self).__init__()
-
-        self.items = []
-
-    def rowCount(self, index=QModelIndex()):
-        return len(self.items)
-
-    def columnCount(self, index=QModelIndex()):
-        return 3
-
-    def index(self, row, column, parent=QModelIndex()):
-        return QAbstractItemModel.createIndex(self, row, column)
-
-    def parent(self, index):
-        return QModelIndex()
-
-    def getPackages(self, index):
-        if not index.isValid() or not (0 <= index.row() < len(self.items)):
-            return QVariant()
-
-        item = self.items[index.row()]
-        column = index.column()
-
-        return QVariant(item.op_pack)
-
-    def removeRows(self, position, rows=1, index=QModelIndex()):
-        self.beginRemoveRows(QModelIndex, position, position + row -1)
-
-        self.items = self.items[:position] + self.items[position+rows:]
-
-        self.endRemoveRows()
-        return True
-
-    def sortByNumber(self):
-        self.items = sorted(self.items)
-        self.reset()
-
-    def getProperty(self, index, property):
-        if not index.isValid() or not (0 <= index.row() < len(self.items)):
-            return QVariant()
-
-        item = self.items[index.row()]
-
-        members = inspect.getmembers(item)
-
-        i = 3
-        while i < len(members):
-            if not isinstance(members[i][1], instancemethod):
-                if property == members[i][0]:
-                    return QVariant(members[i][1])
-            i += 1
-
-        return QVariant()
-
-    def data(self, index, role=Qt.DisplayRole):
-        if not index.isValid() or not (0 <= index.row() < len(self.items)):
-            return QVariant()
-
-        item = self.items[index.row()]
-        column = index.column()
-
-        if role == Qt.DisplayRole:
-            if column == DATE:
-                return QVariant(item.op_date)
-            elif column == TIME:
-                return QVariant(item.op_time)
-        elif role == Qt.DecorationRole:
-            if column == ICON:
-                return QVariant(QPixmap(item.icon))
-
-        return QVariant()
-
-    def setData(self, index, value, role=Qt.EditRole):
-        if not index.isValid() and 0 <= index.row() < len(self.item):
-            return False
-
-        item = self.items(index.row())
-        column = index.column()
-
-        if role == Qt.DisplayRole:
-            if column == DATE:
-                item.op_date = value.toString()
-            if column == TIME:
-                item.op_time = value.toString()
-
-        self.emit(SIGNAL("dataChanged(QModelIndex, QModelIndex)"), index, index)
-        return True
-
-    def headerData(self, section, orientation, role=Qt.DisplayRole):
-        if role == Qt.TextAlignmentRole:
-            if orientation == Qt.Horizontal:
-                return QVariant(int(Qt.AlignLeft|Qt.AlignVCenter))
-            return QVariant(int(Qt.AlignRight|Qt.AlignVCenter))
-
-        if role !=  Qt.DisplayRole:
-            return QVariant()
-
-        if orientation == Qt.Horizontal:
-            if section == ICON:
-                return QVariant("")
-            elif section == DATE:
-                return QVariant(i18n("Date"))
-            elif section == TIME:
-                return QVariant(i18n("Time"))
-        elif orientation == Qt.Vertical:
-            return QVariant(self.items[section].op_no)
-
-        return QVariant(int(section+1))
-
-class NewOperation:
-    def __init__(self, operation):
         self.op_no = operation.no
         self.op_type = operation.type
         self.op_date = operation.date
@@ -137,57 +23,39 @@ class NewOperation:
         self.op_tag = operation.tag
         self.op_pack_len = len(self.op_pack)
 
-        self.op_type_int = 0
-        self.op_type_tr = ""
-        self.icon = None
+        self.sortby = 0
+        self.icon = ":/pics/%s.png" % self.op_type
+        self.op_type_tr = i18n("unknown")
+        self.op_type_int = -1
 
         if self.op_type == 'snapshot':
-            self.icon = ":/pics/snapshot.png"
             self.op_type_int = 1
             self.op_type_tr = i18n("snapshot")
         elif self.op_type == 'upgrade':
-            self.icon = ":/pics/upgrade.png"
             self.op_type_int = 2
             self.op_type_tr = i18n("upgrade")
         elif self.op_type == 'remove':
-            self.icon = ":/pics/remove.png"
             self.op_type_int = 3
             self.op_type_tr = i18n("remove")
         elif self.op_type == 'install':
-            self.icon = ":/pics/install.png"
             self.op_type_int = 4
             self.op_type_tr = i18n("install")
         elif self.op_type == 'takeback':
-            self.icon = ":/pics/takeback.png"
             self.op_type_int = 5
             self.op_type_tr = i18n("takeback")
-        else:
-            self.icon = "?"
+        elif self.op_type == "repoupdate":
             self.op_type_int = 6
-            self.op_type_tr = i18n("unknown")
+            self.op_type_tr = i18n("repo update")
 
-class SortFilterProxyModel(QSortFilterProxyModel):
-    def __init__(self, parent=None):
-        super(SortFilterProxyModel, self).__init__(parent)
+        self.setIcon(0, QIcon(self.icon))
+        self.setText(1, str(self.op_date))
+        self.setText(2, str(self.op_time))
 
-        self.sortby = None
+    def setSortColumn(self, val):
+        self.sortby = val
 
-    def lessThan(self, left, right):
-        lno = self.sourceModel().getProperty(left, "op_no").toInt()
-        rno = self.sourceModel().getProperty(right, "op_no").toInt()
-        ltype = self.sourceModel().getProperty(left, "op_type").toString()
-        rtype = self.sourceModel().getProperty(right, "op_type").toString()
+    def __lt__(self, other):
+        if self.sortby == 0:
+            return int(self.op_no) < int(other.op_no)
+        return QTreeWidgetItem.__lt__(self, other)
 
-        if self.sortby == None or self.sortby == "all":
-            return lno < rno
-
-        if ltype == rtype == self.sortby:
-            return lno < rno
-
-        if ltype == self.sortby:
-            return False
-
-        if rtype == self.sortby:
-            return True
-
-        return lno < rno
