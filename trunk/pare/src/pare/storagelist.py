12e3c3d261c49d739b29cd774a708598c116b97f
mete <mete@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-05-12 15:09:03 +0000

    Add process, register, cancel, find, getDependencies method to handle actions on StorageList

--- trunk/pare/src/pare/storagelist.py
+++ trunk/pare/src/pare/storagelist.py
@@ -14,6 +14,8 @@ import os
 from errors import *
 from storage import Disk, Partition
 from formats import StorageFormat
+from formats import FileSystem
+from storageactions import *
 from udev import *
 
 import gettext
@@ -46,23 +48,23 @@ class StorageList(oject):
         self._devices.append(new)
         log.debug("%s is added to storage list" % (new.name,))
     
-    def _remove(self, dev, force=None):
-        if dev not in self._devices:
-            raise ValueError("Device '%s' is not in storage list" % dev.name)
+    def _remove(self, device, force=None):
+        if device not in self._devices:
+            raise ValueError("Device '%s' is not in storage list" % device.name)
         
-        if not dev.isLeaf and not force:
-            log.debug("%s has %d kids" % (dev.name, dev.kids) )
-            raise ValueError("Device %s hasnt been removed" % dev.name)
+        if not device.isLeaf and not force:
+            log.debug("%s has %d kids" % (device.name, device.kids) )
+            raise ValueError("Device %s hasnt been removed" % device.name)
         
-        if isinstance(dev, Partition) and devi.disk is not None:
-            if dev.partedPartition.type == parted.PARTITION_EXTENDED and len(dev.disk.partedDisk.getLogicalPartitions() > 0 ):
-                raise ValueError("Can not remove %s extended Partitions. logical Partitons present" % dev.name)
+        if isinstance(device, Partition) and device.disk is not None:
+            if device.partedPartition.type == parted.PARTITION_EXTENDED and len(device.disk.partedDisk.getLogicalPartitions() > 0 ):
+                raise ValueError("Can not remove %s extended Partitions. logical Partitons present" % device.name)
             
-            dev.disk.partedDisk.removePartition(dev.partedPartition)
+            device.disk.partedDisk.removePartition(device.partedPartition)
             
         
-        self._devices.remove(dev)
-        log.debug("removed %s '%s' in storage list" % (dev.name, dev.type))
+        self._devices.remove(device)
+        log.debug("removed %s '%s' in storage list" % (device.name, device.type))
     
     def _checkInconsistenceDevices(self):
         
@@ -243,6 +245,26 @@ class StorageList(oject):
         
         return [d for d in self._devices if isinstance(d, device_class)]
     
+    def getDependencies(self, dependency):
+        dependencies = []
+        logicals = []
+        
+        if isinstance(dependency, Partition) and dependency.partType and dependency.isExtended:
+            for part in self.getDevicesByInstance(Partition):
+                if part.partType and part.isLogical and part.disk == dependency.disk:
+                    logicals.append(part)
+        
+        for device in self.devices().values():
+            if device.dependsOn(dependency):
+                dependencies.append(devicependency)
+            else:
+                for logical in logicals:
+                    device.dependsOn(logical)
+                    dependencies.append(device)
+                    break
+        
+        return dependencies
+            
     @property
     def devices(self):
         devices = {}
@@ -334,3 +356,218 @@ class StorageList(oject):
                 device.setup()
             except StorageError, StorageFormatError:
                 log.info("setup failed")
+    
+    
+    def process(self):
+        """
+            Not rely rules of registration! Be careful check again! 
+        """
+        
+        def _cmp(action1, action2):
+            ret = 0
+            
+            ###Destroy Action Compare StartPoint
+            if action1.isDestroy() and action2.isDestroy():
+                if action1.device.path == action2.device.path:
+                    # if the same device, destroy the format first
+                    if action1.isFormat() and action2.isFormat():
+                        ret = 0
+                    elif action1.isFormat() and not action2.isFormat():
+                        ret = -1
+                    elif not action1.isFormat() and action2.isFormat():
+                        ret = 1
+                elif action1.device.dependsOn(action2.device):
+                    ret = -1
+                elif action2.device.dependsOn(action1.device):
+                    ret = 1
+                
+                elif isinstance(action1.device, Partition) and isinstance(action2.device, Partition):
+                    ret = cmp(action2.device.name, action1.device.name)
+                # destroy partitions after lv, vgs
+                elif isinstance(action1, Partition) and not isinstance(action2, Disk):
+                    ret = 1
+                elif isinstance(action2, Partition) and not isinstance(action1, Disk):
+                    ret = -1
+            elif action1.isDestroy():
+                ret = -1
+            elif action2.isDestroy():
+                ret = 1
+            
+            ###Resize Action Compare StartPoint
+            elif action1.isResize() and action2.isResize():
+                if action1.device.path == action2.path:
+                    #same path
+                    if action1.obj and action2.obj:
+                        #same device
+                        ret = 0
+                    elif action1.isFormat() and not action2.isFormat():
+                        if action1.isGrow():
+                            ret = 1
+                        else:
+                            ret = -1
+                    elif not action1.isFormat() and action2.isFormat():
+                        if action1.isGrow():
+                            ret = -1
+                        else:
+                            ret = 1
+                    else:
+                        ret = cmp(action1.device.name, action2.device.name)
+                
+                elif action1.device.dependsOn(action2.device):
+                    if action1.isGrow():
+                        ret = 1
+                    else:
+                        ret = -1
+                elif action2.device.dependsOn(action2.device):
+                    if action2.isGrow():
+                        ret = -1
+                    else:
+                        ret = 1
+                elif isinstance(action1.device, Partition) and isinstance(action2.device, Partition):
+                    ret = cmp(action2.device.name, action1.device.name)
+                # resize partitions after lv, vgs
+                elif isinstance(action1, Partition) and not isinstance(action2, Disk):
+                    if action1.isGrow():
+                        ret = -1
+                    else:
+                        ret = 1
+                elif isinstance(action2, Partition) and not isinstance(action1, Disk):
+                    if action2.isGrow():
+                        ret = 1
+                    else:
+                        ret = -1
+                else:
+                    ret = 0
+            elif action1.isResize():
+                ret = -1
+            elif action2.isResize():
+                ret = 1
+            
+            ###Create Action Compare StartPoint
+            elif action1.isCreate() and action2.isCreate():
+                if action1.device.path == action2.device.path:
+                    if action1.device.obj == action2.device.obj:
+                        ret = 0
+                    elif action1.isFormat():
+                        ret = 1
+                    elif action2.isFormat():
+                        ret = -1
+                    else:
+                        ret = 0
+                elif action1.device.dependsOn(action2.device):
+                    ret = 1
+                elif action2.device.dependsOn(action1.device):
+                    ret = -1
+                # create partition before other types like lv,vg,pv
+                elif isinstance(action1.device, Partition) and isinstance(action2.device, Partition):
+                    ret = cmp(action1.device.name, action2.device.name)
+                elif isinstance(action1.device, Partition) and not isinstance(action2.device, Disk):
+                    ret = -1
+                elif isinstance(action2.device, Partition) and not isinstance(action1.device, Disk):
+                    ret = 1
+                else:
+                    ret = 0
+            
+            elif action1.isCreate():
+                ret = -1
+            elif action2.isCreate():
+                ret = 1
+            
+            ###Migrate Action Compare StartPoint
+            elif action1.isMigrate() and action2.isMigrate():
+                if action1.device.path == action2.device.path:
+                    ret = 0
+                elif action1.device.dependsOn(action2.device):
+                    ret = 1
+                elif action2.device.dependsOn(action1.device):
+                    ret = -1
+                elif isinstance(action1.device, Partition) and isinstance(action2.device, Partition):
+                    ret = cmp (action1.device.name, action2.device.name)
+                else:
+                    ret = cmp (action1.device.name, action2.device.name)
+            
+            else:
+                ret = 0
+            
+            log.debug("_cmp %d -- %s | %s" % (ret, action1, action2))
+            
+            return ret            
+                             
+        for action in self._actions:
+            log.debug("action:%s" % action)
+        
+        log.debug("resetting parted disks")
+        for device in self.devices().itervalues():
+            if isinstance(device, Disk):
+                device.resetPartedDisk()
+        
+        for action in self._actions:
+            log.info("executing action: %s" % action)
+            action.execute()
+            udev_settle()
+                
+                
+    def register(self, action):
+        """ Registered action can be performed later!
+            Modifications to the Device are handled before we get there :) 
+        """
+        if(action.isDestroy() or action.isResize() or (action.isCreate() and action.isFormat())) and action.device not in self._devices:
+            raise StorageListError("Device isnt in the list")
+        elif (action.isCreate() and action.isDevice()):
+            if action.device in self._devices:
+                self._remove(action.device)
+            for dev in self._devices:
+                if action.device.path == dev.path:
+                    self._remove(action.device)
+        
+        if action.isCreate() and action.isDevice():
+            self._add(action.device)
+        elif action.isDestroy() and action.isDevice():
+            self._remove(action.device)
+        elif action.isCreate() and action.isFormat():
+            if isinstance(action.device.format, FileSystem) and action.device.format.mountpoint in self.filesystems():
+                raise StorageListError("mount point already in use")
+        
+        log.debug("registered action %s" % action)
+        self._actions.append(action)
+
+    def cancel(self, action):
+        if action.isCreate() and action.isDevice():
+            self._remove(action.device)
+        elif action.isDestroy() and action.isDevice():
+            self._add(action.device)
+        elif action.isFormat() and (action.isCreate() or action.isMigrate() or action.isResize()):
+            action.cancel()
+            self._actions.remove(action)
+    
+    def find(self, device=None, type=None, object=None, path=None):
+        """
+            device -- 
+            type -- 
+            object --
+            path --
+        """
+        # return full action list
+        if device is None and type is None and object is None and path is None:
+            self._actions[:]
+        
+        _type = type_from_string(type)
+        _object = object_from_string(object)
+        
+        actions = []
+        for action in self._actions:
+            if device is not None and action.device != device:
+                continue
+            
+            if type is not None and action.type != type:
+                continue
+            
+            if object is None and action.obj != object:
+                continue
+            
+            if path is not None and action.device.path != device.path:
+                 continue
+            
+            actions.append(action)
+            
+        return actions
