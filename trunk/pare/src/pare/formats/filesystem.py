b1438d4e542f2a42e2862d24165a20f84e45a0eb
mete <mete@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-05-13 21:08:29 +0000

    I dont understand why unittest gives importing error like( from formats import StorageFormat)

--- trunk/pare/src/pare/formats/filesystem.py
+++ trunk/pare/src/pare/formats/filesystem.py
@@ -13,9 +13,148 @@ import os
 import logging
 log = logging.getLogger("pare")
 
-from . import StorageFormat
-from errors import *
-import sysutils
+from pare.errors import *
+
+
+from pare.sysutils import notify_kernel, get_sysfs_path_by_name
+from pare.utils.dm import dm_node_from_name
+
+
+import gettext
+_ = lambda x: gettext.ldgettext("pare", x)
+
+
+
+
+_formatType = {"ext2":"EXT2",
+               "ext3":"EXT3",
+               "ext4":"EXT4"}
+
+def get_format(type, *args, **kwargs):
+    """
+        type -- Format Class type
+        device -- path to the device which resides
+        uuid -- the UUID of (preexisting) formatted Device
+        exists -- whether or not format exists on device
+        
+    """
+    #FIXME:May be walk on os.path.dirname(_file_) to search .py files to get Decorated StoragFormat class
+    
+    formatClassIns =None
+    for key, formatClass in _formatType.items():
+        if type == key and issubclass(formatClass, StorageFormat):
+            formatClassIns = formatClass(*args, **kwargs)
+    
+    return formatClassIns
+        
+    
+
+class StorageFormat(object):
+    """Generic Storage Format"""
+
+    _type = None
+    partedFlag = None
+    _resizable = False
+    _bootable = False
+    _migratable = False
+    _maxSize = 0
+    _minSize = 0
+    _dump = False
+    _check = False
+
+    def __init__(self, *args, **kwargs):
+        """
+        device -- path to the underlying device (To be easy for resize or format arg list) 
+        exists -- whether this is existing format
+        uuid -- format's UUID 
+        """
+        self._device = kwargs.get("device")
+        self.uuid = kwargs.get("uuid")
+        self.exists = kwargs.get("exists")
+        self._migrate = False
+        
+    def _setDevice(self, dev):
+        if dev and not dev.starwith("/") :
+            raise ValueError("device must be fully qualified path")
+        
+        self._device = dev
+    
+    def _getDevice(self):
+        return self._device
+    
+    device = property(lambda p: p._getDevice(), lambda p:p._setDevice)
+    
+    @property
+    def type(self):
+        return self._type
+
+    @property
+    def resizable(self):
+        return self._resizable
+
+    @property
+    def bootable(self):
+        self._bootable
+
+    @property
+    def migratable(self):
+        return self._migratable
+
+    @property
+    def migrate(self):
+        return self._migrate
+    @property
+    def dump(self):
+        return self._dump
+
+    @property
+    def check(self):
+        return self._check
+
+    @property
+    def minSize(self):
+        return self._minSize
+
+    @property
+    def maxSize(self):
+        return self._maxSize
+
+    def create(self, *args, **kwargs):
+        dev = kwargs.get("device")
+        
+        if not os.path.exists(dev):
+            raise FormatCreateError("invalid device")
+        else:
+            self.device = dev
+    
+    def destroy(self):
+        #FIXME
+        #Anaconda uses zeroing begining and end of the device for 
+        #any meta data wiping in the future
+        pass
+    
+    def notifyKernel(self):
+        if not self.device:
+            return 
+        
+        if self.device.startwith("/dev/mapper/"):
+            try:
+                name = dm_node_from_name(os.path.basename(self.device))
+            except Exception as e:
+                log.warning("failed dm node from device" ,self.device)
+                return
+        elif self.device:
+            name = os.path.basename(self.device)
+        
+        path = get_sysfs_path_by_name(name)
+        
+        try:
+            notify_kernel(path, action="changed")
+        except Exception, e:
+            log.warning("failed to notify the kernel change:%s" % e )
+                
+
+
 
 class FileSystem(StorageFormat):
     _type = "generic"
