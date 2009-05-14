c89f9a566fe7fee7846dfcc62910b0a6f29b2aa1
faik <faik@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-05-14 08:03:08 +0000

    refactor a little

--- trunk/kde4/package-manager/manager/grouplist.py
+++ trunk/kde4/package-manager/manager/grouplist.py
@@ -14,6 +14,48 @@
 from PyQt4 import QtGui
 from PyQt4.QtCore import *
 
+from PyKDE4.kdeui import *
+from PyKDE4.kdecore import *
+
 class GroupList(QtGui.QListWidget):
     def __init__(self, parent=None):
         QtGui.QListWidget.__init__(self, parent)
+        self.lastSelected = None
+        self.connect(self, SIGNAL("itemClicked(QListWidgetItem*)"), self.groupChanged)
+
+    def setState(self, state):
+        self.state = state
+
+    def addGroups(self, groups):
+        for group in groups:
+            self.createGroupItem(group)
+        self.ensureGroupSelected()
+
+    def createGroupItem(self, group):
+        name, icon_path = group["name"], group["icon"]
+
+        package_count = len(self.state.groupPackages(name))
+        if package_count == 0:
+            return
+
+        icon = QtGui.QIcon(KIconLoader().loadMimeTypeIcon(icon_path, KIconLoader.Desktop, KIconLoader.SizeSmallMedium))
+        item = QtGui.QListWidgetItem(icon, "%s (%d)" % (name, package_count), self)
+        item.setData(Qt.UserRole, QVariant(unicode(name)))
+        item.setSizeHint(QSize(0, KIconLoader.SizeMedium))
+
+        if str(self.lastSelected) == name:
+            self.selectLastSelected(item)
+
+    def selectLastSelected(self, item):
+        self.setCurrentItem(item)
+
+    def ensureGroupSelected(self):
+        if self.currentRow() == -1 and self.count():
+            self.selectLastSelected(self.itemAt(0, 0))
+
+    def currentGroup(self):
+        return self.currentItem().data(Qt.UserRole).toString()
+
+    def groupChanged(self):
+        self.lastSelected = self.currentGroup()
+        self.emit(SIGNAL("groupChanged()"), self.lastSelected)
