36a3a631ce9884ce57ecb365c3b3deb363df98a9
isbaran <isbaran@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-05-15 11:13:15 +0000

    fix move / resize

--- trunk/kde4/history-manager/src/main.py
+++ trunk/kde4/history-manager/src/main.py
@@ -14,6 +14,7 @@
 import sys
 import comar
 import dbus
+from dbus.mainloop.qt import DBusQtMainLoop
 
 from PyQt4 import QtGui
 from PyQt4.QtCore import *
@@ -24,31 +25,40 @@ from PyKDE4.kdecore import *
 from about import aboutData
 from window import MainManager
 
-class Manager(KMainWindow):
-    def __init__ (self, *args):
-        KMainWindow.__init__(self)
-        self.setCentralWidget(MainManager(self))
-
-class ServiceManager(KCModule):
+class HistoryManager(KCModule):
     def __init__(self, component_data, parent):
         KCModule.__init__(self, component_data, parent)
 
-        DBusQtMainLoop(set_as_default = True)
-        MainManager(self, standAlone = False)
+        KGlobal.locale().insertCatalog("history-manager")
+
+        DBusQtMainLoop(set_as_default=True)
+        MainManager(self, standAlone=False)
+
+class Manager(KMainWindow):
+    def __init__(self, app):
+        KMainWindow.__init__(self)
+
+        KGlobal.locale().insertCatalog("history-manager")
+        settings = QSettings()
+
+        if settings.contains("pos") and settings.contains("size"):
+            self.move(self.mapToGlobal(settings.value("pos").toPoint()))
+            self.resize(settings.value("size").toSize())
+
+        # self.resize(640, 480)
+        self.setCentralWidget(MainManager(self, True, app))
 
 def CreatePlugin(widget_parent, parent, component_data):
-    return ServiceManager(component_data, parent)
+    return HistoryManager(component_data, parent)
 
 if __name__ == '__main__':
     KCmdLineArgs.init(sys.argv, aboutData)
-    app = KApplication()
+    app = KUniqueApplication()
 
     if not dbus.get_default_main_loop():
-        from dbus.mainloop.qt import DBusQtMainLoop
         DBusQtMainLoop(set_as_default = True)
 
-    mainWindow = Manager(None, 'history-manager')
+    mainWindow = Manager(app)
     mainWindow.show()
 
     app.exec_()
-    del mainWindow
