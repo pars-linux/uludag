bafd2bf76a3290c189eee2be22529ad6b6093657
gokmen.goksel <gokmen.goksel@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-05-21 08:07:43 +0000

    * po update

--- trunk/kde4/sysinfo/update-po.sh
+++ trunk/kde4/sysinfo/update-po.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LANGS='ca de es fr it nl pl pt_BR tr sv'
+LANGS='ca de es fr it nl pl pt_BR sv tr'
 
 xgettext src/*.cpp -o po/sysinfo.pot -ki18n -ktr2i18n -kI18N_NOOP -ktranslate -kaliasLocale
 
