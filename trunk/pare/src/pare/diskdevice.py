4fdbdee09459b826f77dc3d9f30aa225000f25a0
mete <mete@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-05-21 22:54:34 +0000

    Add Pare Abstraction for disk object

--- trunk/pare/src/pare/diskdevice.py
+++ trunk/pare/src/pare/diskdevice.py
@@ -29,46 +29,46 @@ import binascii
 from pardus.diskutils import *
 from pare.partition import Partition, FreeSpace
 from pare.errors import PareError
-import pare.sysutils as sysutils
-from pare.formats.filesystem import getFilesystem, FileSystem
+import pare.utils.sysutils as sysutils
+from pare.filesystem import getFilesystem, FileSystem
 from pare.parteddata import *
 
 import logging
 log = logging.getLogger("pare")
 
-class DeviceError(YaliError):
+class DeviceError(PareError):
     pass
 
 # all storage devices
-devices = []
+disks = []
 
 ##
 # initialize all devices and fill devices list
 def init_devices(force = False):
-    global devices
+    global disks
 
-    if devices and not force:
+    if disks and not force:
         return True
 
     clear_devices()
 
     devs = detect_all()
     for dev_path in devs:
-        d = Device(dev_path)
-        devices.append(d)
+        d = Disk(dev_path)
+        disks.append(d)
 
     def comp(x, y):
         """sort disks using getName()"""
-        x = x.getName()
-        y = y.getName()
+        x = x.name
+        y = y.name
 
         if x > y: return -1
         elif x == y: return 0
         else: return 1
 
-    devices.sort(comp,reverse=True)
+    disks.sort(comp,reverse=True)
 
-    if devices:
+    if disks:
         return True
 
     return False
@@ -87,7 +87,7 @@ class Disk:
     # @param arch: Architecture that we're partition for (defaults to 'x86')
     def __init__(self, path, arch="x86"):
 
-        spartedelf._arch = arch
+        self._arch = arch
         self._path = ""
         self._device = None
         self._model = ""
@@ -95,40 +95,28 @@ class Disk:
         self._partitions = []
         self._disklabel = ""
         self._length = 0       # total sectors
-        self._sector_size = 0
+        self._sectorSize = 0
         self._needs_commit = False
 
-        device = parted.Device.get(path)
+        device = parted.Device(path)
 
         self._model = device.model
         self._length = device.length
-        self._sectorSize = dev.sectorSize
+        self._sectorSize = device.sectorSize
 
         self._device = device
         try:
-            self._disk = parted.PedDisk.new(dev)
+            self._disk = parted.Disk.new(dev)
         except:
             label = archinfo[self._arch]["disklabel"]
-            diskType = parted.diskType(label)
-            self._disk = parted.freshDisk(self._device, ty=diskType)
+            self._disk = parted.freshDisk(self._device, ty=label)
 
         self._disklabel = self._disk.type
 
         self._path = path
 
-        self.update()
-
-
-    ##
-    # clear and re-fill partitions dict.
-    def update(self):
-        self._partitions = []
-
-        part = self._disk.getFirstPartition()
-        while part:
-            self.__addToPartitionsDict(part)
-            part = part.nextPartition()
-
+    
+    
     ##
     # do we have room for another primary partition?
     # @returns: boolean
@@ -165,23 +153,12 @@ class Disk:
     @property
     def model(self):
         return self._model
-    
-    @property
-    def partitions(self):
-        return self._partitions
-    
-    
-    def getPartition(self, num):
-        for part in self.partitions:
-            if part.minor == num:
-                return part
-        return None
 
     ##
     # check if the device has an extended partition
     # @returns: True/False
     def hasExtendedPartition(self):
-        if self.getExtendedPartition():
+        if self.extendedPartition:
             return True
         return False
 
@@ -220,6 +197,13 @@ class Disk:
     def numberOfPrimaryPartitions(self):
         return len(self.primaryPartitions)
 
+    def getAllPartitions(self):
+        allparts = []
+        part = self._disk.getFirstPartition()
+        while part:
+            allparts.append(part)
+            part = part.nextPartition()
+        return allparts
     ##
     # get the partition list in an order
     # @returns: [Partition]
@@ -259,11 +243,7 @@ class Disk:
             return 0
 
 
-    ##
-    # internal
-    # get the largest, continuous free space (in MB)
-    # @returns: parted.PedPartition
-    def __getLargestFreePartition(self):
+    def getLargestFreePartition(self):
         size = 0
         largest = None
         freespaces = self._disk.getFreeSpacePartitions()
@@ -274,168 +254,57 @@ class Disk:
         
         return largest
 
-
-    ###############################
-    # Partition mangling routines #
-    ###############################
-
-    ##
-    # Add (create) a new partition to the device
-    # @param part: parted partition; must be parted.PARTITION_FREESPACE
-    # @param type: parted partition type (eg. parted.PARTITION_PRIMARY)
-    # @param fs: filesystem.FileSystem or file system name (like "ext3")
-    # @param size_mb: size of the partition in MBs.
-    def addPartition(self, part, type, filesystem, size, flags = [], manualGeomStart = None):
-
-        # Don't set bootable flag if there is already a bootable
-        # partition in this disk. See bug #2217
-        if (parted.PARTITION_BOOT in flags) and self.hasBootablePartition():
-            flags = list(set(flags) - set([parted.PARTITION_BOOT]))
-
-        size = int((size * MEGABYTE) / self._sectorSize)
-
-        if not part:
-            part = self.__getLargestFreePartition()
-
-        if not manualGeomStart:
-            geom = part.geometry
-
-            # creating a partition
-            if geom.length >= size:
-                return self.addPartitionStartEnd(type,
-                                                 filesystem,
-                                                 geom.start,
-                                                 geom.start + size,
-                                                 flags)
-        else:
-            return self.addPartitionStartEnd(type,
-                                             filesystem,
-                                             manualGeomStart,
-                                             manualGeomStart + size,
-                                             flags)
-
-        # if you are here and then we have some problems..
-        raise DeviceError, ("Not enough free space on %s to create new partition" % self.getPath())
-
-    ##
-    # add a partition starting from a given geom...
-    def addPartitionFromStart(self, type, filesytem, start, size, flags = []):
-        size = int((size_mb * MEGABYTE) / self._sectorSize)
-        return self.addPartitionStartEnd(type, filesytem, start, start + size, flags)
-
-    ##
-    # Add (create) a new partition to the device from start to end.
-    #
-    # @param type: parted partition type (eg. parted.PARTITION_PRIMARY)
-    # @param fs: filesystem.FileSystem or file system name (string like "ext3")
-    # @param start: start geom..
-    # @param end: end geom
-    def addPartitionStartEnd(self, type, filesystem, start, end, flags = []):
+    def addPartition(self, type, filesystem, start, end, flags = []):
         self._needs_commit = True
-        if isinstance(filesystem, str):
-            filesystem = getFilesystem(filesystem)
-        
-        if isinstance(filesystem, FileSystem):
-            filesystemType = filesystem.fileSystemType
-        else:
-            filesystemType = None
 
-        constraint = self._disk.device.getConstraint()
-        geom = parted.Geometry(self._disk.device, start, end=end)
-        part = parted.Partition(disk, type, filesystem, geom)
+        constraint = self._device.getConstraint()
+        geom = parted.Geometry(self._device, start, end=end)
+        part = parted.Partition(self._disk, type, filesystem, geom)
         
         for flag in flags:
             part.setFlag(flag, 1)
 
         try:
-            self._disk.addPartition(part, constraint)
+            return self._disk.addPartition(part, constraint)
         except parted.error, e:
             raise DeviceError, e
-
-        return self.__addToPartitionsDict(part, ready=False)
-
-    ##
-    # (internal function)
-    # add partition to the partitions dictionary
-    # @param part: pyparted partition type
-    #
-    # @returns: Partition
-    def __addToPartitionsDict(self, part, ready=True):
-        geom = part.geometry
-        size = long(((geom.end - geom.start + 1) * self._sectorSize) / MEGABYTE)
-        if part.number >= 1:
-            type = ""
-            if part.fileSystem.name:
-                type = part.fileSystem.name
-            elif part.type & parted.PARTITION_EXTENDED:
-                type = "extended"
-
-            self._partitions.append(Partition(self, part,
-                                                    part.number,
-                                                    size,
-                                                    geom.start,
-                                                    geom.end,
-                                                    name,
-                                                    ready))
-
-        elif part.type & parted.PARTITION_FREESPACE and size >= 10:
-            self._partitions.append(FreeSpace(self, part,
-                                                    size,
-                                                    part.geom.start,
-                                                    part.geom.end))
-        return part
+        
+        return True
 
     ##
     # delete a partition
     # @param part: Partition
     def deletePartition(self, part):
-        self._needs_commit = True
-        self.disk.deletePartition(part.partition)
-        self.update()
+        return self._disk.deletePartition(part)
 
     def deleteAllPartitions(self):
-        self._needs_commit = True
-        self.disk.deleteAllPartitions()
-        self.update()
+        return self._disk.deleteAllPartitions()
 
-    def resizePartition(self, filesystem, size, part):
+    def resizePartition(self, filesystem, size, partition):
 
-        if isinstance(filesystem, str):
-            # a string... get the corresponding FileSystem object
-            fs = getFilesystem(filesystem)
-
-        if not isinstance(filesystem, FileSystem):
-            raise DeviceError, "filesystem is None, can't resize"
-
-        if not filesystem.resize(size, part):
-            raise DeviceError, "fs.resize ERROR"
-
-        start = part.partition.geom.start
-        fileSystemType = part.getFileSystemType()
+        start = partition.geom.start
         
-        if part.isLogical:
+        if partition.isLogical:
             type = parted.PARTITION_LOGICAL
         else:
             type = parted.PARTITION_NORMAL
             
         time.sleep(3)
-        self.deletePartition(part)
+        self.deletePartition(partition)
         self.commit()
-        np = self.addPartitionFromStart(type, fileSystemType, start, size)
+        np = self.addPartition(type, fileSystem, start, size)
         self.commit()
         return np
 
     def commit(self):
         try:
-            self.disk.commit()
+            self._disk.commit()
         except:
             sysutils.run("sync")
             time.sleep(3)
             sysutils.run("sync")
             log.error("Commit Failed!")
-            self.disk.commit()
-        
-        self.update()
+            self._disk.commit()
 
     def close(self):
         # pyparted will do it for us.
