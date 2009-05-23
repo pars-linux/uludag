7e6be2f4b135ae258781ed271af282bd68f2e874
mete <mete@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-05-23 22:23:09 +0000

    3 letters change everything

--- trunk/pare/src/pare/diskdevice.py
+++ trunk/pare/src/pare/diskdevice.py
@@ -106,7 +106,7 @@ class Disk:
 
         self._device = device
         try:
-            self._disk = parted.Disk.new(dev)
+            self._disk = parted.Disk.new(device)
         except:
             label = archinfo[self._arch]["disklabel"]
             self._disk = parted.freshDisk(self._device, ty=label)
