52e4fed043078d28e4dd0c0965342ae078cd6b66
faik <faik@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-05-28 13:30:16 +0000

    - get Settings class from old pm and change its name as Config - refactor settingsdialog code, use different class for every tab

--- trunk/kde4/package-manager/manager/src/settingsdialog.py
+++ trunk/kde4/package-manager/manager/src/settingsdialog.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+<#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2009 TUBITAK/UEKAE
@@ -20,26 +20,60 @@ from ui_settingsdialog import Ui_SettingsDialog
 
 import helpdialog
 
+class SettingsTab:
+    def __init__(self, settings):
+        self.settings = settings
+        self.changed = False
+        self.setupUi()
+        self.connectSignals()
+
+    def setupUi(self):
+        pass
+
+    def connectSignals(self):
+        pass
+
+    def save(self):
+        pass
+
+class GeneralSettings(SettingsTab):
+    def setupUi(self):
+        self.settings.moveUpButton.setIcon(KIcon("arrow-up"))
+        self.settings.moveDownButton.setIcon(KIcon("arrow-down"))
+        self.settings.addRepoButton.setIcon(KIcon("list-add"))
+        self.settings.removeRepoButton.setIcon(KIcon("list-remove"))
+
+class CacheSettings(SettingsTab):
+    def setupUi(self):
+        pass
+
+class RepositorySettings(SettingsTab):
+    def setupUi(self):
+        pass
+
+class ProxySettings(SettingsTab):
+    def setupUi(self):
+        pass
+
 class SettingsDialog(QtGui.QDialog, Ui_SettingsDialog):
     def __init__(self, state, parent=None):
         QtGui.QDialog.__init__(self, parent)
         self.state = state
         self.setupUi(self)
-        self.initialize()
         self.connectSignals()
 
-    def initialize(self):
-        self.moveUpButton.setIcon(KIcon("arrow-up"))
-        self.moveDownButton.setIcon(KIcon("arrow-down"))
-        self.addRepoButton.setIcon(KIcon("list-add"))
-        self.removeRepoButton.setIcon(KIcon("list-remove"))
+        self.generalSettings = GeneralSettings(self)
+        self.cacheSettings = CacheSettings(self)
+        self.repositorySettings = RepositorySettings(self)
+        self.proxySettings = ProxySettings(self)
 
     def connectSignals(self):
         self.connect(self.buttonOk, SIGNAL("clicked()"), self.saveSettings)
         self.connect(self.buttonHelp, SIGNAL("clicked()"), self.showHelp)
 
     def saveSettings(self):
-        pass
+        for settings in [self.generalSettings, self.cacheSettings, self.repositorySettings, self.proxySettings]:
+            settings.save()
 
     def showHelp(self):
         helpDialog = helpdialog.HelpDialog(self, helpdialog.PREFERENCES)
