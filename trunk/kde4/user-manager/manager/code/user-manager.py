7633a6add33312cd44bde86fa9ee1681027bdfcb
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-06-03 11:50:37 +0000

    Set Icon

--- trunk/kde4/user-manager/manager/code/user-manager.py
+++ trunk/kde4/user-manager/manager/code/user-manager.py
@@ -17,7 +17,7 @@ import dbus
 from PyQt4 import QtGui
 from PyQt4 import QtCore
 
-from PyKDE4.kdeui import KMainWindow, KApplication, KCModule
+from PyKDE4.kdeui import KMainWindow, KApplication, KCModule, KIcon
 from PyKDE4.kdecore import KCmdLineArgs, KGlobal
 
 from usermanager.about import aboutData, catalog
@@ -30,6 +30,7 @@ class MainWindow(KMainWindow):
         widget = MainWidget(self)
         self.resize(widget.size())
         self.setCentralWidget(widget)
+        self.setWindowIcon(KIcon("system-users"))
 
 
 if __name__ == "__main__":
