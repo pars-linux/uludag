0b91d79f499cdf93f3dc5513c32328baefb92638
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-06-16 11:49:21 +0000

    System.Settings backend, template

---
+++ trunk/kde4/system-manager/manager/scripts/mudur.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+def listLanguages():
+    return []
+
+def getLanguage():
+    return ""
+
+def setLanguage(lang):
+    pass
+
+
+def listKeymaps(language=""):
+    return []
+
+def getKeymap():
+    return ""
+
+def setKeymap(keymap):
+    pass
+
+
+def listLocales(language=""):
+    return []
+
+def getLocale():
+    return ""
+
+def setLocale(local):
+    pass
