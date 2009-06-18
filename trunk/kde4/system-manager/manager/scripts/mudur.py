ad99b6019bf44051910e8997f4cb4566ebacbb1f
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-06-18 13:47:11 +0000

    - GUI - Backend   - Get available languages, locales, keymaps, services, ...

--- trunk/kde4/system-manager/manager/scripts/mudur.py
+++ trunk/kde4/system-manager/manager/scripts/mudur.py
@@ -1,52 +1,73 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from pardus.localedata import languages as LANGUAGES
+
 def listLanguages():
-    return []
+    languages = []
+    for code, info in LANGUAGES.iteritems():
+        languages.append((code, str(info.name)))
+    return languages
 
 def getLanguage():
-    return ""
+    return "tr"
 
 def setLanguage(lang):
     pass
 
 
 def listKeymaps(language=""):
-    return []
+    keymaps = []
+    if language and language in LANGUAGES:
+        for keymap in LANGUAGES[language].keymaps:
+            keymaps.append((keymap.console_layout, str(keymap.name)))
+    else:
+        for language in LANGUAGES:
+            for keymap in LANGUAGES[language].keymaps:
+                keymaps.append((keymap.console_layout, str(keymap.name)))
+    return keymaps
 
 def getKeymap():
-    return ""
+    return "trq"
 
 def setKeymap(keymap):
     pass
 
 
 def listLocales(language=""):
-    return []
+    locales = []
+    if language and language in LANGUAGES:
+        locale = LANGUAGES[language].locale
+        locales.append((locale, str(locale)))
+    else:
+        for language in LANGUAGES:
+            locale = LANGUAGES[language].locale
+            locales.append((locale, str(locale)))
+    return locales
 
 def getLocale():
-    return ""
+    return "tr_TR.UTF-8"
 
 def setLocale(local):
     pass
 
 
 def getHeadStart():
-    return ""
+    return "xdm"
 
 def setHeadStart(package):
     pass
 
 
 def getClock():
-    return ""
+    return True, True
 
 def setClock(clock, adjust):
     pass
 
 
 def getTTYs():
-    return ""
+    return 6
 
 def setTTYs(count):
     pass
