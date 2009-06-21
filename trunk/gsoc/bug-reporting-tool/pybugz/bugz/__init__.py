b3a9d443f45b95a4fadabe025fb1ad1fd443dfdd
caio <caio@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-06-21 23:38:53 +0000

    bug-reporting-tool: adding `pybugz` and `apport` source trees.
    These trees already have the changes from upstream reflected in the `patches`
    directories.

---
+++ trunk/gsoc/bug-reporting-tool/pybugz/bugz/__init__.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+"""
+Python Bugzilla Interface
+
+Simple command-line interface to bugzilla to allow:
+ - searching
+ - getting bug info
+ - saving attachments
+
+Requirements
+------------
+ - Python 2.4 or later
+ - ElementTree (only for Python 2.4)
+
+Classes
+-------
+ - Bugz - Pythonic interface to Bugzilla
+ - PrettyBugz - Command line interface to Bugzilla
+
+"""
+
+__version__ = '0.7.4'
+__author__ = 'Alastair Tse <http://www.liquidx.net/>'
+__contributors__ = ['Santiago M. Mola <cooldwind@gmail.com']
+__revision__ = '$Id: $'
+__license__ = """Copyright (c) 2006, Alastair Tse, All rights reserved.
+This following source code is licensed under the GPL v2 License."""
+
+CONFIG_FILE = '.bugz'
+
