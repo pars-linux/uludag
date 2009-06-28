1c67da4794b4099cfc4430ea7f981c5ac24a70bc
emre.aladag <emre.aladag@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-06-28 22:34:05 +0000

    2009-06-29  Ahmet Emre ALADAĞ  <aladagemre@users.sourceforge.net>
            * Cleared up some old code (from multi-table system)
            * Theme adapted to color "lal".
            * Added a welcome, guide text.
            * Updated po/mo files.

--- staj-projeleri/paket-arama/search/pathsearch/models.py
+++ staj-projeleri/paket-arama/search/pathsearch/models.py
@@ -13,38 +13,3 @@ class Repo(models.Model):
     
     def __unicode__(self):
         return '%s %s - %s' % (self.package, self.path)
-
-class Pardus2007(models.Model):
-    package = models.CharField(max_length=60)
-    path    = models.CharField(max_length=200)
-    
-    class Meta:
-        db_table = 'pardus_2007'
-        ordering = ['package', 'path']
-
-    
-    def __unicode__(self):
-        return '%s - %s' % (self.package, self.path)
-    
-class Pardus2008(models.Model):
-    package = models.CharField(max_length=60)
-    path    = models.CharField(max_length=200)
-    
-    class Meta:
-        db_table = 'pardus_2008'
-        ordering = ['package', 'path']
-
-    
-    def __unicode__(self):
-        return '%s - %s' % (self.package, self.path)
-
-class Contrib2008(models.Model):
-    package = models.CharField(max_length=60)
-    path    = models.CharField(max_length=200)
-
-    class Meta:
-        db_table = 'contrib_2008'
-        ordering = ['package', 'path']
-
-    def __unicode__(self):
-        return '%s - %s' % (self.package, self.path)
