a90f145f174180390e34f1213989675df3b211e1
mete <mete@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-06-29 16:14:27 +0000

    -Use old bookmark.py

--- branches/migration4/src/migration/utils/bookmark.py
+++ branches/migration4/src/migration/utils/bookmark.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2006-2009 TUBITAK/UEKAE
+# Copyright (C) 2007, TUBITAK/UEKAE
 #
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free
 # Software Foundation; either version 2 of the License, or (at your option)
 # any later version.
-#
-# Please read the COPYING file.
 #
 
 import xml.dom.minidom
@@ -17,48 +15,14 @@ import ConfigParser
 import os
 from pysqlite2 import dbapi2 as sqlite
 
-
-class Bookmark(object):
-    def __init__(self, path):
-        self._path = path
-        self._dom = xml.dom.minidom.getDOMImplementation()
-        self._document = self.dom.createDocument(None, "bookmarks", None)
+class Bookmark:
+    def __init__(self):
+        self.dom = xml.dom.minidom.getDOMImplementation()
+        self.document = self.dom.createDocument(None, "bookmarks", None)
     
-    def set(self):
-        pass
-    
-    def get(self):
-        pass
-    
-    @property
-    def document(self):
-        return self._document
-    
-    def __getPath(self):
-        return self._path
-    
-    def _setPath(self, path):
-        self._path = path
-    
-    path = property(lambda p: p.__getPath(), lambda p,f: p.__setPath(f))
-     
-    def save(self, filename):
-        "Saves Bookmarks to an XML file"
-        xmlfile = open(filename, "w")
-        data = self.document.toprettyxml()
-        xmlfile.write(data)
-        xmlfile.close()
-    
-    def size(self):
-        return len(self.document.getElementsByTagName("bookmark")) * 200
-    
-    
-class FirefoxBookmark(Bookmark):
-    def __init__(self, path=None):
-        Bookmark.__init__(self, path)
-    
-    def get(self):
-        filename = os.path.join(self.path, "bookmarks.html")
+    def getFFBookmarks(self, path):
+        "Gets Firefox Bookmarks from a bookmarks.html file"
+        filename = os.path.join(path, "bookmarks.html")
         bookmarkfile = open(filename)
         data = bookmarkfile.read()
         bookmarkfile.close()
@@ -72,22 +36,147 @@ class FirefoxBookmark(Bookmark):
             mainnode.insertBefore(headernode, mainnode.firstChild)
         else:
             mainnode.appendChild(headernode)
-        
-    def set(self):
-        if os.path.lexists(os.path.join(self.path, "lock")):
+    
+    def getOperaBookmarks(self, path):
+        "Gets Opera Bookmarks from a opera6.adr file"
+        # open file:
+        filename = os.path.join(path, "opera6.adr")
+        bookmarkfile = open(filename)
+        # create main node:
+        groupnode = self.document.createElement("group")
+        headernode = self.document.createElement("header")
+        textnode = self.document.createTextNode("Opera Bookmarks")
+        groupnode.appendChild(headernode)
+        headernode.appendChild(textnode)
+        self.document.documentElement.appendChild(groupnode)
+        # parse file:
+        nodetype = None
+        nodename = None
+        nodeurl = None
+        for line in bookmarkfile:
+            line = line.strip()
+            if not line:
+                if nodetype == "FOLDER":
+                    newgroup = self.document.createElement("group")
+                    headernode = self.document.createElement("header")
+                    textnode = self.document.createTextNode(nodename)
+                    newgroup.appendChild(headernode)
+                    headernode.appendChild(textnode)
+                    groupnode.appendChild(newgroup)
+                    groupnode = newgroup
+                elif nodetype == "URL":
+                    newnode = self.document.createElement("bookmark")
+                    namenode = self.document.createElement("name")
+                    textnode = self.document.createTextNode(nodename)
+                    namenode.appendChild(textnode)
+                    newnode.appendChild(namenode)
+                    urlnode = self.document.createElement("url")
+                    textnode = self.document.createTextNode(nodeurl)
+                    urlnode.appendChild(textnode)
+                    newnode.appendChild(urlnode)
+                    groupnode.appendChild(newnode)
+                nodetype = None
+                nodename = None
+                nodeurl = None
+            elif line.startswith("#"):
+                nodetype = line.replace("#","",1)
+            elif line.startswith("NAME="):
+                nodename = line.replace("NAME=","",1)
+            elif line.startswith("URL="):
+                nodeurl = line.replace("URL=","",1)
+            elif line == "-":
+                groupnode = groupnode.parentNode
+    
+    def getIEBookmarks(self, directory):
+        "Gets IE Bookmarks from a Favourites directory"
+        def searchDirectory(directory, document, node):
+            files = os.listdir(directory)
+            for filename in files:
+                filepath = os.path.join(directory, filename)
+                base, ext = os.path.splitext(filename)
+                if os.path.isdir(filepath):
+                    groupnode = document.createElement("group")
+                    headernode = document.createElement("header")
+                    textnode = document.createTextNode(filename)
+                    headernode.appendChild(textnode)
+                    groupnode.appendChild(headernode)
+                    node.appendChild(groupnode)
+                    searchDirectory(filepath, document, groupnode)
+                elif ext == ".url":
+                    parser = ConfigParser.ConfigParser()
+                    parser.readfp(open(filepath))
+                    url = parser.get("InternetShortcut","URL")
+                    bookmarknode = document.createElement("bookmark")
+                    namenode = document.createElement("name")
+                    urlnode = document.createElement("url")
+                    textnode = document.createTextNode(base)
+                    namenode.appendChild(textnode)
+                    textnode = document.createTextNode(url)
+                    urlnode.appendChild(textnode)
+                    bookmarknode.appendChild(namenode)
+                    bookmarknode.appendChild(urlnode)
+                    node.appendChild(bookmarknode)
+            return xml
+        groupnode = self.document.createElement("group")
+        headernode = self.document.createElement("header")
+        textnode = self.document.createTextNode("Internet Explorer Bookmarks")
+        groupnode.appendChild(headernode)
+        headernode.appendChild(textnode)
+        self.document.documentElement.appendChild(groupnode)
+        searchDirectory(directory, self.document, groupnode)
+    
+    def setFFBookmarks(self, path):
+        "Adds bookmarks to firefox"
+        if os.path.lexists(os.path.join(path, "lock")):
             raise Exception, "Firefox is in use. Bookmarks cannot be saved."
-        
         filepath = os.path.join(path, "places.sqlite")
         if os.path.exists(filepath):
-            self.__handleVersion2(filepath)
+            self.setFF3Bookmarks(filepath)
             return
         filenpath = os.path.join(path, "bookmarks.html")
         if os.path.exists(filepath):
-            self.__handleVersion3(filepath)
+            self.setFF2Bookmarks(filepath)
             return
         raise Exception, "Bookmark file cannot be found."
-
-    def __handleVersion3(self, databasepath):
+        
+    def setFF2Bookmarks(self, filepath):
+        "Adds bookmarks to firefox using bookmarks.html file"
+        def getText(nodelist):
+            rc = ""
+            for node in nodelist:
+                if node.nodeType == node.TEXT_NODE:
+                    rc = rc + node.data
+            return rc
+        
+        def handleNode(node):
+            data = ""
+            if node.tagName == "bookmarks":
+                children = node.childNodes
+                for child in children:
+                    data += handleNode(child)
+            if node.tagName == "group":
+                header = node.getElementsByTagName("header")[0]
+                data += "<DT><H3>%s</H3>\n<DL><p>\n" % getText(header.childNodes)
+                children = node.childNodes
+                for child in children:
+                    data += handleNode(child)
+                data += "</DL><p>\n"
+            if node.tagName == "bookmark":
+                name = node.getElementsByTagName("name")[0]
+                url = node.getElementsByTagName("url")[0]
+                data += "<DT><A HREF=\"%s\">%s</A>\n" % (getText(url.childNodes), getText(name.childNodes))
+            return data
+        
+        bookmarkfile = open(filepath)
+        data = bookmarkfile.read()
+        bookmarkfile.close()
+        pairs = data.rsplit("</DL>",1)
+        data = handleNode(self.document.documentElement)
+        bookmarkfile = open(filename, "w")
+        bookmarkfile.write(pairs[0] + data + "</DL>" + pairs[1])
+        bookmarkfile.close()
+    
+    def setFF3Bookmarks(self, databasepath):
         "Adds bookmarks to firefox using places.sqlite database"
         def getText(nodelist):
             rc = ""
@@ -150,143 +239,17 @@ class FirefoxBookmark(Bookmark):
         conn.commit()
         c.close()
     
-    def __handleVersion2(self, filepath):
-        "Adds bookmarks to firefox using bookmarks.html file"
-        def getText(nodelist):
-            rc = ""
-            for node in nodelist:
-                if node.nodeType == node.TEXT_NODE:
-                    rc = rc + node.data
-            return rc
-        
-        def handleNode(node):
-            data = ""
-            if node.tagName == "bookmarks":
-                children = node.childNodes
-                for child in children:
-                    data += handleNode(child)
-            if node.tagName == "group":
-                header = node.getElementsByTagName("header")[0]
-                data += "<DT><H3>%s</H3>\n<DL><p>\n" % getText(header.childNodes)
-                children = node.childNodes
-                for child in children:
-                    data += handleNode(child)
-                data += "</DL><p>\n"
-            if node.tagName == "bookmark":
-                name = node.getElementsByTagName("name")[0]
-                url = node.getElementsByTagName("url")[0]
-                data += "<DT><A HREF=\"%s\">%s</A>\n" % (getText(url.childNodes), getText(name.childNodes))
-            return data
-        
-        bookmarkfile = open(filepath)
-        data = bookmarkfile.read()
-        bookmarkfile.close()
-        pairs = data.rsplit("</DL>",1)
-        data = handleNode(self.document.documentElement)
-        bookmarkfile = open(filename, "w")
-        bookmarkfile.write(pairs[0] + data + "</DL>" + pairs[1])
-        bookmarkfile.close()
+    def saveXML(self, filename):
+        "Saves Bookmarks to an XML file"
+        xmlfile = open(filename, "w")
+        data = self.document.toprettyxml()
+        xmlfile.write(data)
+        xmlfile.close()
     
-class OperaBookmark(Bookmark):
-    def __init__(self, path=None):
-        Bookmark.__init__(self, path)
+    def size(self):
+        return len(self.document.getElementsByTagName("bookmark")) * 200
         
-    def get(self):
-        "Gets Opera Bookmarks from a opera6.adr file"
-        # open file:
-        filename = os.path.join(self.path, "opera6.adr")
-        bookmarkfile = open(filename)
-        # create main node:
-        groupnode = self.document.createElement("group")
-        headernode = self.document.createElement("header")
-        textnode = self.document.createTextNode("Opera Bookmarks")
-        groupnode.appendChild(headernode)
-        headernode.appendChild(textnode)
-        self.document.documentElement.appendChild(groupnode)
-        # parse file:
-        nodetype = None
-        nodename = None
-        nodeurl = None
-        for line in bookmarkfile:
-            line = line.strip()
-            if not line:
-                if nodetype == "FOLDER":
-                    newgroup = self.document.createElement("group")
-                    headernode = self.document.createElement("header")
-                    textnode = self.document.createTextNode(nodename)
-                    newgroup.appendChild(headernode)
-                    headernode.appendChild(textnode)
-                    groupnode.appendChild(newgroup)
-                    groupnode = newgroup
-                elif nodetype == "URL":
-                    newnode = self.document.createElement("bookmark")
-                    namenode = self.document.createElement("name")
-                    textnode = self.document.createTextNode(nodename)
-                    namenode.appendChild(textnode)
-                    newnode.appendChild(namenode)
-                    urlnode = self.document.createElement("url")
-                    textnode = self.document.createTextNode(nodeurl)
-                    urlnode.appendChild(textnode)
-                    newnode.appendChild(urlnode)
-                    groupnode.appendChild(newnode)
-                nodetype = None
-                nodename = None
-                nodeurl = None
-            elif line.startswith("#"):
-                nodetype = line.replace("#","",1)
-            elif line.startswith("NAME="):
-                nodename = line.replace("NAME=","",1)
-            elif line.startswith("URL="):
-                nodeurl = line.replace("URL=","",1)
-            elif line == "-":
-                groupnode = groupnode.parentNode
-    
-
-class InternetExplorerBookmark(Bookmark):
-    def __init__(self, path=None):
-        Bookmark.__init__(self, path)
-
-    def get(self):
-        "Gets IE Bookmarks from a Favourites directory"
-        
-        def searchDirectory(directory, document, node):
-            files = os.listdir(directory)
-            for filename in files:
-                filepath = os.path.join(directory, filename)
-                base, ext = os.path.splitext(filename)
-                if os.path.isdir(filepath):
-                    groupnode = document.createElement("group")
-                    headernode = document.createElement("header")
-                    textnode = document.createTextNode(filename)
-                    headernode.appendChild(textnode)
-                    groupnode.appendChild(headernode)
-                    node.appendChild(groupnode)
-                    searchDirectory(filepath, document, groupnode)
-                elif ext == ".url":
-                    parser = ConfigParser.ConfigParser()
-                    parser.readfp(open(filepath))
-                    url = parser.get("InternetShortcut","URL")
-                    bookmarknode = document.createElement("bookmark")
-                    namenode = document.createElement("name")
-                    urlnode = document.createElement("url")
-                    textnode = document.createTextNode(base)
-                    namenode.appendChild(textnode)
-                    textnode = document.createTextNode(url)
-                    urlnode.appendChild(textnode)
-                    bookmarknode.appendChild(namenode)
-                    bookmarknode.appendChild(urlnode)
-                    node.appendChild(bookmarknode)
-            return xml
-        
-        groupnode = self.document.createElement("group")
-        headernode = self.document.createElement("header")
-        textnode = self.document.createTextNode("Internet Explorer Bookmarks")
-        groupnode.appendChild(headernode)
-        headernode.appendChild(textnode)
-        self.document.documentElement.appendChild(groupnode)
-        searchDirectory(directory, self.document, groupnode)
-
-class FirefoxBookmarkParser:
+class FFBookmarkParser(HTMLParser.HTMLParser):
     def __init__(self, document):
         self.document = document
         self.node = document.documentElement
