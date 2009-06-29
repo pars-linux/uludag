3bb3310d4a4b802f557b0a4fe16301b036ec5878
emre.aladag <emre.aladag@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-06-29 10:30:46 +0000

    2009-06-29  Ahmet Emre ALADAĞ  <aladagemre@users.sourceforge.net>
            * URL Changed: package/pkg_name/list/ -> packages/pkg_name/
            * Fixed minor bugs:
                    * extra entry parameter
                    * unused version parameter used for filtering
                    * package_name -> entry.package
                    * Some comment fixes

--- trunk/staj-projeleri/paket-arama/search/pathsearch/urls.py
+++ trunk/staj-projeleri/paket-arama/search/pathsearch/urls.py
@@ -1,21 +1,23 @@
+# -*- coding: utf-8 -*-
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('search.pathsearch.views',
     (r'^$', 'index'),
     (r'^(?P<version>[-0-9A-Za-z]+)/$', 'index'),
-    (r'^(?P<version>[-0-9A-Za-z]+)/package/(?P<package_name>[-_.0-9A-Za-z]+)/list/$', 'list_package_contents'),
+    (r'^(?P<version>[-0-9A-Za-z]+)/package/(?P<package_name>[-_.0-9A-Za-z]+)/$', 'list_package_contents'),
     (r'^(?P<version>[-0-9A-Za-z]+)/package/(?P<package_name>[-_.0-9A-Za-z]+)/(?P<term>[-/_.0-9A-Za-z]+)', 'search_in_package'),
-    (r'^(?P<version>[-0-9A-Za-z]+)/package/(?P<package_name>[-_.0-9A-Za-z]+)/$', 'search_for_package'),
+    (r'^(?P<version>[-0-9A-Za-z]+)/packages/(?P<package_name>[-_.0-9A-Za-z]+)/$', 'search_for_package'),    
     (r'^(?P<version>[-0-9A-Za-z]+)/(?P<term>[-_.0-9A-Za-z]+)/$', 'search_in_all_packages'),
     
 )
 
 """
-/search/2008/package/glibc/test.py -> search for test.py in glibc package.
+/search/pardus-2009/package/glibc/test.py -> search for test.py in glibc package.
 
-/search/2008/package/sahip -> search for package named sahip.
+/search/pardus-2009/packages/sahip -> search for package named sahip.
 
-1. /search/2008/package/{packagename}/{term} > search for term in a package
-2. /search/2008/package/{packagename} > search for package
-3. /search/2008/{term} > search for term in all packages. post:group->only package names, clicked -> 3.
+1. /search/pardus-2009/package/{packagename}/{term} > search for term in a package
+2. /search/pardus-2009/package/{packagename} -> contents of package
+3. /search/pardus-2009/packages/{packagename} > search for package
+4. /search/pardus-2009/{term} > search for term in all packages. post:group->only package names, clicked -> 3.
 """
