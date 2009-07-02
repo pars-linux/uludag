5385dae8a93aae982f1c618f70d5647904095e27
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-07-02 09:25:24 +0000

    New release

--- trunk/kde4/network-manager/manager/src/about.py
+++ trunk/kde4/network-manager/manager/src/about.py
@@ -18,7 +18,7 @@ from PyKDE4.kdecore import *
 appName     = "network-manager"
 catalog     = ""
 programName = ki18n("Network Manager")
-version     = "2.9.6"
+version     = "2.9.7"
 description = ki18n("Network Manager")
 license     = KAboutData.License_GPL
 copyright   = ki18n("(c) 2009 TUBITAK/UEKAE")
