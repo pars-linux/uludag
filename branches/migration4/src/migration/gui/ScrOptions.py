a2feefb49e97ae58b577d48c05f4aba2ffea6204
mete <mete@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-07-06 17:33:56 +0000

    Fixed ScrOptions

--- branches/migration4/src/migration/gui/ScrOptions.py
+++ branches/migration4/src/migration/gui/ScrOptions.py
@@ -16,6 +16,7 @@ from PyQt4.QtCore import *
 from PyKDE4.kdecore import i18n
 
 from migration.gui.ScreenWidget import ScreenWidget
+#from migration.gui.ui.optionsWidget import Ui_optionWidget
 import migration.gui.context as ctx
 
 class Widget(QtGui.QWidget, ScreenWidget):
@@ -24,151 +25,197 @@ class Widget(QtGui.QWidget, ScreenWidget):
 
     def __init__(self, *args):
         QtGui.QWidget.__init__(self,None)
-        self.sources = ctx.sources
-        print "ctx.sources:%s" % ctx.sources
-        #self.creator(self.sources)
+        #self.ui  =  Ui_optionWidget()
+        #self.ui.setupUi(self)
+        self.box = QtGui.QGridLayout(self)
+        #self.gridlayout.setContentsMargins(1,-1,11,-1)
+        #self.sources = ctx.sources
+        #self.vbox = QtGui.QVBoxLayout(self)
 
     def creator(self, sources):
-        self.vbox = QtGui.QVBoxLayout()
-        
+
         # Wallpaper:
         if sources.has_key("Wallpaper Path"):
-            self.wallpaperGroup = QtGui.QButtonGroup(self, "wpGroup")
-            self.wallpaperLayout = QtGui.QHBoxLayout()
-            self.vbox.addWidget(self.wallpaperGroup)
+            print "Wallpaper Path"
+            self.wallpaperGroup = QtGui.QButtonGroup(self)
+            self.wallpaperLayout = QtGui.QHBoxLayout(self.wallpaperGroup)
+            self.box.addWidget(self.wallpaperGroup)
+
             # New (current) Wallpaper:
             self.newLayout = QtGui.QVBoxLayout(None)
-            self.newLayout.setAlignment(Qt.AlignCenter)
+            #self.newLayout.setAlignment(Qt.AlignCenter)
             self.wallpaperLayout.addLayout(self.newLayout)
+
             # Thumbnail:
-            self.newThumb = QtGui.QLabel(self.wpGroup, "newThumb")
+            self.newThumb = QtGui.QLabel(self.wallpaperGroup)
             self.newLayout.addWidget(self.newThumb)
             if destinations.has_key("Wallpaper Path"):
                 newwp = QtGui.QImage(unicode(destinations["Wallpaper Path"]))
                 newwp = newwp.smoothScale(100, 100, QImage.ScaleMax)
                 pixmap = QtGui.QPixmap(newwp)
                 self.newThumb.setPixmap(pixmap)
+
             # Radio Button:
-            self.newRadio = QtGui.QRadioButton(self.wpGroup, "newRadio")
+            self.newRadio = QtGui.QRadioButton(self.wallpaperGroup)
             if destinations.has_key("Wallpaper Path"):
                 self.newRadio.setText(i18n("Keep current wallpaper"))
             else:
                 self.newRadio.setText(i18n("Don't use wallpaper"))
-            QtGui.QToolTip.add(self.newRadio, i18n("Does not change your wallpaper."))
+            self.newRadio.setToolTip(i18n("Does not change your wallpaper."))
             self.newRadio.setChecked(True)
             self.newLayout.addWidget(self.newRadio)
+
             # Old Wallpaper:
             self.oldLayout = QtGui.QVBoxLayout(None)
-            self.oldLayout.setAlignment(Qt.AlignCenter)
-            self.wpLayout.addLayout(self.oldLayout)
+            #self.oldLayout.setAlignment(Qt.AlignCenter)
+            self.wallpaperLayout.addLayout(self.oldLayout)
+
             # Thumbnail:
-            self.oldThumb = QtGui.QLabel(self.wpGroup, "oldThumb")
+            self.oldThumb = QtGui.QLabel(self.wallpaperGroup)
             oldwp = QtGui.QImage(unicode(sources["Wallpaper Path"]))
             oldwp = oldwp.smoothScale(100, 100, QImage.ScaleMax)
             pixmap = QtGui.QPixmap(oldwp)
             self.oldThumb.setPixmap(pixmap)
             self.oldLayout.addWidget(self.oldThumb)
+
             # Radio Button:
-            self.oldRadio = QtGui.QRadioButton(self.wpGroup, "oldRadio")
+            self.oldRadio = QtGui.QRadioButton(self.wallpaperGroup)
+            self.oldRadio.setObjectName("oldRadio")
             self.oldRadio.setText(i18n("Use my old wallpaper"))
-            QtGui.QToolTip.add(self.oldRadio, i18n("Copies your old wallpaper to Pardus and sets it as new background image."))
+            self.oldRadio.setToolTip(i18n("Copies your old wallpaper to Pardus and sets it as new background image."))
             self.oldLayout.addWidget(self.oldRadio)
-        
-         # Bookmarks:
+
+        # Bookmarks:
         if sources.has_key("Firefox Profile Path") or sources.has_key("Favorites Path"):
-            self.bookmarks = QtGui.QGroupBox(self, "Bookmarks")
+            print "Firefox Profile Path"
+
+            self.bookmarks = QtGui.QGroupBox("Bookmarks", self)
             self.bookmarks.setTitle(i18n("Bookmarks"))
             self.bookmarks.setAlignment(Qt.AlignLeft)
-            self.bookmarksLayout = QtGui.QVBoxLayout(self.bookmarks.layout())
-            self.vbox.addWidget(self.bookmarks)
-            
+            self.bookmarksLayout = QtGui.QVBoxLayout(self.bookmarks)
+            self.box.addWidget(self.bookmarks)
+            self.box.addWidget(self.bookmarks)
+
             # FF Bookmarks:
             if sources.has_key("Firefox Profile Path"):
-                self.fireFoxBookmarks = QtGui.QCheckBox(self.bookmarks, "fireFoxBookmarks")
+                print "Firefox Profile Path"
+
+                self.fireFoxBookmarks = QtGui.QCheckBox(self.bookmarks)
+                self.fireFoxBookmarks.setObjectName("FFBookmarks")
                 self.fireFoxBookmarks.setText(i18n("Firefox Bookmarks"))
                 self.fireFoxBookmarks.setChecked(True)
-                QtGui.QToolTip.add(self.fireFoxBookmarks, i18n("Copies your old Firefox bookmarks to Firefox under Pardus."))
+                self.fireFoxBookmarks.setToolTip(i18n("Copies your old Firefox bookmarks to Firefox under Pardus."))
                 self.bookmarksLayout.addWidget(self.fireFoxBookmarks)
-            
+
             # Opera Bookmarks:
             if sources.has_key("Opera Profile Path"):
-                self.operaBookmarks = QCheckBox(self.bookmarks, "operaBookmarks")
+                print "Opera Profile Path"
+
+                self.operaBookmarks = QCheckBox(self.bookmarks)
+                self.operaBookmarks.setObjectName("OperaBookmarks")
                 self.operaBookmarks.setText(i18n("Opera Bookmarks"))
                 self.operaBookmarks.setChecked(True)
-                QtGui.QToolTip.add(self.operaBookmarks, i18n("Copies your old Opera bookmarks to Firefox under Pardus."))
+                self.operaBookmarks.setToolTip(i18n("Copies your old Opera bookmarks to Firefox under Pardus."))
                 self.bookmarksLayout.addWidget(self.operaBookmarks)
-            
+
             # IE Bookmarks:
             if sources.has_key("Favorites Path"):
-                self.IEBookmarks = QtGui.QCheckBox(self.Bookmarks, "IEBookmarks")
+                print "Internet Explorer favorites"
+
+                self.IEBookmarks = QtGui.QCheckBox(self.bookmarks)
+                self.IEBookmarks.setObjectName("IEBookmarks")
                 self.IEBookmarks.setText(i18n("Internet Explorer favorites"))
                 self.IEBookmarks.setChecked(True)
-                QtGui.QToolTip.add(self.IEBookmarks, i18n("Copies your old Internet Explorer favorites to Firefox under Pardus."))
+                self.IEBookmarks.setToolTip(i18n("Copies your old Internet Explorer favorites to Firefox under Pardus."))
                 self.bookmarksLayout.addWidget(self.IEBookmarks)
-                
-         # Mail Accounts:
+
+
+        # Mail Accounts:
         if sources.has_key("Windows Mail Path") or sources.has_key("Thunderbird Profile Path"):
-            self.mailAccounts = QtGui.QGroupBox(self, "MailAccounts")
+            print "Mail Accounts"
+
+            self.mailAccounts = QtGui.QGroupBox("MailAccounts", self)
             self.mailAccounts.setTitle(i18n("E-Mail and News Accounts"))
             self.mailAccounts.setAlignment(Qt.AlignLeft)
-            self.mailAccountsLayout = QtGui.QVBoxLayout(self.mailAccounts.layout())
-            self.vbox.addWidget(self.mailAccounts)
-            
+            self.mailAccountsLayout = QtGui.QVBoxLayout(self.mailAccounts)
+            self.box.addWidget(self.mailAccounts)
+
             # Windows Mail Accounts:
             if sources.has_key("Windows Mail Path"):
-                self.winMail = QtGui.QCheckBox(self.mailAccounts, "WinMail")
+                print "Windows Mail Path"
+
+                self.winMail = QtGui.QCheckBox(self.mailAccounts)
+                self.winMail.setObjectName("WinMail")
                 self.winMail.setText(i18n("Windows Mail accounts"))
                 self.winMail.setChecked(True)
-                QtGui.QToolTip.add(self.winMail, i18n("Copies your old mail and newsgroup accounts to KMail and KNode applications."))
+                self.winMail.setToolTip(i18n("Copies your old mail and newsgroup accounts to KMail and KNode applications."))
                 self.mailAccountsLayout.addWidget(self.winMail)
-            
+
             # Thunderbird Accounts:
             if sources.has_key("Thunderbird Profile Path"):
-                self.TB = QtGui.QCheckBox(self.MailAccounts, "TB")
+                print "Thunderbird Profile Path"
+
+                self.TB = QtGui.QCheckBox(self.mailAccounts)
+                self.TB.setObjectName("TB")
                 self.TB.setText(i18n("Thunderbird accounts"))
                 self.TB.setChecked(True)
-                QToolTip.add(self.TB, i18n("Copies your old mail and newsgroup accounts to KMail and KNode applications."))
-                self.MailAccountsLayout.addWidget(self.TB)
-            
+                self.TB.setToolTip(i18n("Copies your old mail and newsgroup accounts to KMail and KNode applications."))
+                self.mailAccountsLayout.addWidget(self.TB)
+
             # E-Mails:
-            self.mail = QtGui.QCheckBox(self.mailAccounts, "mail")
+            self.mail = QtGui.QCheckBox(self.mailAccounts)
+            self.mail.setObjectName("mail")
             self.mail.setText(i18n("Copy e-mail messages from e-mail accounts"))
             self.mail.setChecked(True)
-            QtGui.QToolTip.add(self.mail, i18n("Copies your e-mail messages to KMail from selected applications above."))
+            self.mail.setToolTip(i18n("Copies your e-mail messages to KMail from selected applications above."))
             self.mailAccountsLayout.addWidget(self.mail)
-                
+
+
         # IM Accounts:
         if sources.has_key("Contacts Path") or sources.has_key("GTalk Key"):
-            self.IMAccounts = QtGui.QGroupBox(self, "IMAccounts")
+            print "IMAccounts"
+
+            self.IMAccounts = QtGui.QGroupBox("IMAccounts", self)
             self.IMAccounts.setTitle(i18n("Instant Messenger Accounts"))
             self.IMAccounts.self.IMAccounts.layout()
-            self.IMAccountsLayout = QVBoxLayout(self)
+            self.IMAccountsLayout = QVBoxLayout(self.IMAccounts)
             self.vbox.addWidget(self.IMAccounts)
+
             # MSN Accounts:
             if sources.has_key("Contacts Path"):
-                self.MSN = QtGui.QCheckBox(self.IMAccounts, "MSN")
+                print "MSN Accounts"
+
+                self.MSN = QtGui.QCheckBox(self.IMAccounts)
+                self.MSN.setObjectName("MSN")
                 self.MSN.setText(i18n("MSN accounts"))
                 self.MSN.setChecked(True)
-                QtGui.QToolTip.add(self.MSN, i18n("Copies your MSN Messenger accounts to Kopete."))
+                self.MSN.setToolTip(i18n("Copies your MSN Messenger accounts to Kopete."))
                 self.IMAccountsLayout.addWidget(self.MSN)
+
             # GTalk Accounts:
             if sources.has_key("GTalk Key"):
-                self.GTalk = QtGui.QCheckBox(self.IMAccounts, "GTalk")
+                print "GTalk"
+
+                self.GTalk = QtGui.QCheckBox(self.IMAccounts)
+                self.GTalk.setObjectName("GTalk")
                 self.GTalk.setText(i18n("GTalk accounts"))
                 self.GTalk.setChecked(True)
-                QtGui.QToolTip.add(self.GTalk, i18n("Copies your GTalk accounts to Kopete."))
+                self.GTalk.setToolTip(i18n("Copies your GTalk accounts to Kopete."))
                 self.IMAccountsLayout.addWidget(self.GTalk)
+
+            self.IMAccounts.addLayout(self.IMAccountsLayout)
+
         # Spacer:
-        spacer = QtGui.QSpacerItem(1,1,QSizePolicy.Minimum,QSizePolicy.Expanding)
-        self.vbox.addItem(spacer)
-        
+        spacer = QtGui.QSpacerItem(1,1, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        self.box.addItem(spacer)
+
     def getOptions(self):
         "Returns a dictionary consists of selected options"
         options = {}
-        self.sources["Copy E-Mails"] = True
+        ctx.sources["Copy E-Mails"] = True
         # Add fundamental items:
         for item in ["Partition", "OS Type", "User Name", "Home Path"]:
-            options[item] = self.sources[item]
+            options[item] = ctx.sources[item]
         # Add selected optional items:
         items = [("IEBookmarks", "Favorites Path"),
                  ("FFBookmarks", "Firefox Profile Path"),
@@ -180,13 +227,17 @@ class Widget(QtGui.QWidget, ScreenWidget):
                  ("GTalk", "GTalk Key"),
                  ("MSN", "Contacts Path")]
         for widgetname, dictname in items:
-            item = self.child(widgetname)
+            item = self.findChild(QtGui.QWidget, widgetname)
             if item and item.isChecked():
-                options[dictname] = self.sources[dictname]
+                options[dictname] = ctx.sources[dictname]
         return options
 
     def shown(self):
-        pass
+        print "ScrOptions.shown() çağrldı..."
+        print "ctx.sources:%s" % ctx.sources
+        self.creator(ctx.sources)
+        #self.gridlayout.addWidget(self.vbox)
 
     def execute(self):
+        print "len(self.options):%d" % len(self.getOptions())
         ctx.options = self.getOptions()
