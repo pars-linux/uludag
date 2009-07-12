8d6483dcaa772631123ea5301c87e00d6916d09b
fatih <fatih@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-07-12 16:04:49 +0000

    Set icon and caption

--- trunk/kde4/display-settings/code/display-settings.sh
+++ trunk/kde4/display-settings/code/display-settings.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-kcmshell4 kcm_displaysettings kcm_displaydevices energy kgamma
+lang=`kreadconfig --group Locale --key Language`
+if [ -n "$lang" ]; then
+    desktopfile=`kde4-config --path xdgdata-apps --locate kde4/displaysettings.desktop`
+    title=$(grep "^Name\[$lang\]=" $desktopfile | cut -d= -f 2)
+else
+    title=$(TEXTDOMAINDIR=`kde4-config --install locale` gettext display-settings "Display Settings")
+fi
+
+exec kcmshell4 --icon preferences-desktop-display --caption "$title" kcm_displaysettings kcm_displaydevices energy kgamma
