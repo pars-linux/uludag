a6754e780160ce4a96ae092ff93e46e0a3606af2
ozan.caglayan <ozan.caglayan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-07-13 13:47:23 +0000

    Use packages subdomain for the repo

--- pardus-upgrade/pardus-2008-to-2009.py
+++ pardus-upgrade/pardus-2008-to-2009.py
@@ -9,14 +9,14 @@ def upgrade_pisi():
 def remove_all_repos_and_add_2009_repository():
     for repo in pisi.api.list_repos():
         pisi.api.remove_repo(repo)
-    pisi.api.add_repo("pardus-2009", "http://paketler.pardus.org.tr/pardus-2009/pisi-index.xml.bz2")
+    pisi.api.add_repo("pardus-2009", "http://packages.pardus.org.tr/pardus-2009/pisi-index.xml.bz2")
 
 def upgrade_and_configure_packages():
     os.system("pisi up --ignore-comar -y")
     os.system("pisi cp -y")
 
 def install_missing_x11_drivers():
-    os.system("pisi it -y -c x11.driver")    
+    os.system("pisi it -y -c x11.driver")
 
 def install_display_manager():
     os.unlink('/etc/X11/kdm/kdmrc')
