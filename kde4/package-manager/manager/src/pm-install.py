10b66752ee47d552fa999058aa97b27b8735666a
Faik Uygur <faikuygur@gmail.com>
2009-07-14 07:27:46 +0000

    exception handler for pm

--- kde4/package-manager/manager/src/pm-install.py
+++ kde4/package-manager/manager/src/pm-install.py
@@ -68,9 +68,19 @@ class Operation(QObject):
     def install(self, packages):
         self.packages = packages
         self.iface.installPackages(self.packages)
-    
+
     def exceptionHandler(self, exception):
-        self.messageBox = QtGui.QMessageBox(i18n("Pisi Error"), unicode(exception), QtGui.QMessageBox.Critical, QtGui.QMessageBox.Ok, 0, 0)
+        if "urlopen error" in message or "Socket Error" in message:
+            errorTitle = i18n("Network Error")
+            errorMessage = i18n("Please check your network connections and try again.")
+        elif "Access denied" in message or "tr.org.pardus.comar.Comar.PolicyKit" in message:
+            errorTitle = i18n("Authorization Error")
+            errorMessage = i18n("You are not authorized for this operation.")
+        else:
+            errorTitle = i18n("Pisi Error")
+            errorMessage = message
+
+        self.messageBox = QtGui.QMessageBox(errorTitle, errorMessage, QtGui.QMessageBox.Critical, QtGui.QMessageBox.Ok, 0, 0)
         self.messageBox.show()
 
 class MainWindow(KMainWindow):
