92c750da76b047e08037090f613b64496c9f690f
gokmen.goksel <gokmen.goksel@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-07-15 13:23:10 +0000

    * Slideshows and texts are updated for final release .

--- trunk/yali4/yali4/gui/descSlide.py
+++ trunk/yali4/yali4/gui/descSlide.py
@@ -15,33 +15,23 @@ __trans = gettext.translation('yali4', fallback=True)
 _ = __trans.ugettext
 
 slideDesc = [
-{"yali":_("""Welcome! Our improved setup tool YALI will help you with the installation of Pardus 2009. YALI will help install Pardus 2009 quickly and easily following the partitioning of disk(s) using the intelligent partitioning tool.
-""")},
-{"kaptan":_("""Our Kaptan will show you the way after the installation. You can easily add additional repositories and manage basic settings like mouse, wallpaper and panel using in Kaptan.
-""")},
-{"tasma":_("""Tasma is the management and settings center of Pardus. Configuration operations including but not limited to adding a new user or hardware (e.g. printer, monitor) to the system, management of visual settings ranging from desktop icons to fonts are all brought together under Tasma's interface.
-""")},
-{"paket":_("""There are thousands of packages in Pardus software repositories. You can install, remove or update those packages and programs with just two clicks using the Package Manager!
-""")},
-{"ag":_("""Network Applet helps connecting to local networks and Internet easily and securely. Thanks to the profile support it has, it will make it easier to reconnect to cable or wireless connections used before, remembering the settings used.
-""")},
-{"firefox":_("""Mozilla Firefox, with its speed, constant development and many advanced features, will help securely surf the Internet. Pardus 2009 includes Mozilla Firefox 3.5, and you can enrich Firefox with thousands of extensions.
-""")},
-{"kontact":_("""Kontact, KDE Personal Information Manager, helps you manage all your emails, RSS feeds, diaries, to-do listes and blog entries in one combined interface. Kontact, with its Groupware feature, will be your best friend at work.
-""")},
-{"open":_("""OpenOffice.org, actively being used by hundreds of thousands of institutes and millions of users around the world, is an office software package including a word processor, a spreadsheet application, a presentation viewer and editor, a graphics editor and a database application.
-""")},
-{"gimp":_("""Gimp is an advanced image editing application that comes preinstalled with Pardus 2009. Gimp allows editing all common image file formats and has extensive abilities that will meet professional graphic artists' and designers' needs.
-""")},
-{"amarok":_("""Amarok is a convenient, functional, and advanced media player. It can fetch album covers and lyrics for you, and thanks to its Last.FM support, can suggest similar media, bringing even more possibilities.
-""")},
-{"kaffeine":_("""Kaffeine is the default video player of Pardus 2009. It has a high performance in playing DVDs, VCDs, and countless video file formats, including, but not limited to, MPEG, AVI and DivX. If you have a digital TV card, you can record your favourite TV programs using Kaffeine!
-""")},
-{"kopete":_("""You can use multiple accounts in various instant messaging services like MSN, Yahoo!, ICQ, AIM, GTalk and Jabber, and can talk to your friends in different networks using Kopete.
-""")},
-{"k3b":_("""Copying CDs (or DVDs), creating data or audio CDs, creating ISO images and burning CD/DVD images to discs are just a few of the things you can do with K3b.
-""")},
-{"destek":_("""You can find HOW-TOs, tutorials and package introductions in our community portal www.ozgurlukicin.com. If you happen to have any problems with Pardus 2009, you can get support using the forum pages and the #pardus room in IRC Freenode server (and you can use Konversation for that).
-""")},
+{"01":_("""Hello, there. The easiest-evet install tool YALI has begun Pardus installation. It is now time to relax and learn about some of the features of Pardus 2009 while it is being installed.""")},
+{"02":_("""Pardus 2009 is a Linux distribution that everybody can easily install and use. With Pardus 2009, you can do anything that you could otherwise do with other operating systems. With its fast, secure and stable infrastructure, Pardus opens the path to software libre.""")},
+{"03":_("""Pardus 2009 comes preinstalled with the open source desktop environment KDE 4.2. You will have a much better desktop experience compared to what you have seen so far. Following the installation, you can choose to use the classical desktop view that includes icons to your files and folders, or you can have any number of the preinstalled plasma widgets on your desktop and add interesting functionality to your view. You can have an analog clock on your desktop; even more, you can have a real-time weather report, your notes and RSS feeds right on your desktop.""")},
+{"04":_("""With the arrival of KDE 4.2, Konqueror is replaced with a new and very talented file manager, Dolphin. Dolphin is easy to use, and when it comes to viewing folders, it allows you to sort them in various ways, adding functionality like grouping and detailed information right on the spot. Anything you may need on viewing folders, you have it with Dolphin.""")},
+{"05":_("""With Pardus 2009, Kpdf is replaced with Okular. Okular supports many file formats and you can view all your files without any additional software, take notes on bookmarks on them.""")},
+{"06":_("""Pardus 2009 also includes other components updated with KDE 4.2. If you have used Pardus before, you may want to check out the welcome tour to see what those changes are.""")},
+{"07":_("""Pardus includes many genuine technologies aiming for an easier use and management. TASMA has been replaced with System Manager in Pardus 2009, but you will still find all these management tools built into the System Manager interface.""")},
+{"08":_("""Kaptan (Turkish for 'Captain') is what welcomes you to your Pardus installation. Using Kaptan, you can configure your desktop, changing the taskbar's location, wallpaper and desktop theme, and internet connection settings. Kaptan also allows you to add the Contribution Repository to your package management settings. Even more, Kaptan includes options to set intelligent search and an online Pardus Beginner's Tour.""")},
+{"09":_("""It is possible to install hundreds of software found in Pardus' package repositories with a single click. Using the Package Manager, you can install PiSi packages with the click of a button, not having to deal with screen after screen of detailed configuration options. After package installations, all you need to do is to start the applications. It is all that easy!""")},
+{"10":_("""Adding new users, changing boot order, starting or stopping system services or rolling your system configuration back to a previous state has never been easier. You can use the system managers for that and you can even add to your desktop as widgets managers that you use frequently.""")},
+{"11":_("""To connect to a wireless or 'wired' network is as easy as choosing a connection profile with Pardus 2009. With the network manager's advanced options, you can have different settings (e.g. static IP, a different DNS server) for different connections with the profiles you create. Choosing a different connection profile requires just two clicks.""")},
+{"12":_("""With Pardus 2009, Amarok has not only shed skin, but also changed internally. It provides much more than listening to music. You now have the option to read lyrics and learn about the singer or composer through Wikipedia while listening to a song, or use the intelligent playlist feature to group songs in various manners. As Amarok works seamlessly with most player devices, you can drag and drop songs to your players without the need for any external device drivers.""")},
+{"13":_("""Mozilla Firefox is the preferred browser with its speed, security and flexibility. With its addon support and a *very* rich addon repository, you can add new functionality to Mozilla Firefox. You will find many of the most visited websites along with Pardus-related websites in your bookmarks in its Pardus default installation. Enjoy a fast and secure internet with the new Mozilla Firefox 3.5.""")},
+{"14":_("""Connecting to your friends and contacts is really easy and seamlessly integrated into the desktop with Pardus 2009. Using Kontact, you can send and receive emails, manage your meetings, create address books, takes notes and follow RSS feeds. Using Kopete, you can connect to popular networks like MSN and GTalk, and chat online with your friends.""")},
+{"15":_("""World's leading preferred libre office suite OpenOffice.org brings a word processor, a spreadsheet, a presentation application, database, formula and drawing tools to your desktop for your office needs at home, at your workplace or school. With OpenOffice.org, you can view and edit documents created in other office suites and share your documents with friends and contacts that are using other office applications.""")},
+{"16":_("""Gimp is an advanced image editing software. Allowing creation and editing of common graphics and image file formats, it has abilities that professional graphics artists and designers need and an ability to integrate new filters easily with its addon system.""")},
+{"17":_("""Pardus, along with the preinstalled games, has many games in its repositories, allowing hours of fun with various kinds of games available through the package manager.""")},
+{"18":_("""Pardus 2009 is set to support most hardware right away. At the end of the installation, when you start with Pardus, the graphics card and sound hardware of your computer will be auto-detected , and the network devices will be prepared for connections, whether it be wireless or 'wired'. Adding a printer to your sistem is also really easy. All you need to do is plug the printer in, and it is set. Pardus will install the correct driver and set everything accordingly right away.""")},
+{"19":_("""We have a support portal to help improve your Pardus experience and help with problems you encounter. In the Özgürlükİçin portal, where content is developed by volunteer contributors, you will find guides, package and game introductions. If you happen to have any questions, the forum is waiting for you. If you need support, or if you want to support, your first stop should be www.ozgurlukicin.com""")},
 ]
-
