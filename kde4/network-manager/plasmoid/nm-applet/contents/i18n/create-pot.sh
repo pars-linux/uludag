abdd4670c2d10896ec982fa62eebae88545dd653
Gokmen Goksel <gokmen@goksel.me>
2009-07-17 14:18:28 +0000

    * Fix i18n bork..

--- kde4/network-manager/plasmoid/nm-applet/contents/i18n/create-pot.sh
+++ kde4/network-manager/plasmoid/nm-applet/contents/i18n/create-pot.sh
@@ -2,7 +2,7 @@
 
 # Create template file
 echo "Creating pot file..."
-xgettext --no-wrap --copyright-holder="Pardus" --package-name="$1" --keyword=i18n --keyword=i18np:1,2 -o ./$1-new.pot code/*/*.py
+xgettext --no-wrap --copyright-holder="Pardus" --package-name="$1" --keyword=i18n --keyword=i18np:1,2 -o ./$1-new.pot code/*/*.py code/main.py
 
 echo "Fixing header information..."
 sed 's/CHARSET/utf-8/' ./$1-new.pot > $1-new1.pot ; rm $1-new.pot
