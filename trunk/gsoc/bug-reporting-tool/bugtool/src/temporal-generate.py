ad646931f80765104968a8b781c4e942227cb89f
caio <caio@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-08-13 20:32:30 +0000

    bug-reporting-tool: adding new (placeholding) widgets and restructuring the work dir.

--- trunk/gsoc/bug-reporting-tool/bugtool/src/temporal-generate.py
+++ trunk/gsoc/bug-reporting-tool/bugtool/src/temporal-generate.py
@@ -3,8 +3,8 @@
 
 import os, glob
 
-for filename in glob.glob1("gui", "*.ui"):
-    os.system("/usr/kde/4/bin/pykde4uic -o gui/%s.py gui/%s" % (filename.split(".")[0], filename))
+for filename in glob.glob1("gui/widgets", "*.ui"):
+    os.system("pykdeuic4 -o gui/%s.py gui/widgets/%s" % (filename.split(".")[0], filename))
 
 for filename in glob.glob1("gui", "*.qrc"):
-    os.system("/usr/bin/pyrcc4 gui/%s -o gui/%s_rc.py" % (filename, filename.split(".")[0]))
+    os.system("pyrcc4 gui/%s -o gui/%s_rc.py" % (filename, filename.split(".")[0]))
