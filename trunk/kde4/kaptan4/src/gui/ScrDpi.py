37c906f8c98e5aee730693973a0db20e6839a5cf
pinar <pinar@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-08-14 13:40:11 +0000

    almost ready... thanks to Fatih =]

--- trunk/kde4/kaptan4/src/gui/ScrDpi.py
+++ trunk/kde4/kaptan4/src/gui/ScrDpi.py
@@ -18,6 +18,29 @@ from PyKDE4.kdecore import ki18n
 from gui.ScreenWidget import ScreenWidget
 from gui.dpiWidget import Ui_dpiWidget
 
+class DpiPreview(QtGui.QLabel):
+    def __init__(self, text, initialDpi):
+        QtGui.QLabel.__init__(self, text)
+
+        self.scr = QtGui.QX11Info.appScreen()
+        self.dpi = initialDpi
+
+    def paintEvent(self, event):
+        oldDpiX = QtGui.QX11Info.appDpiX(self.scr)
+        oldDpiY = QtGui.QX11Info.appDpiY(self.scr)
+
+        QtGui.QX11Info.setAppDpiX(self.scr, self.dpi)
+        QtGui.QX11Info.setAppDpiY(self.scr, self.dpi)
+
+        QtGui.QLabel.paintEvent(self, event)
+
+        QtGui.QX11Info.setAppDpiX(self.scr, oldDpiX)
+        QtGui.QX11Info.setAppDpiY(self.scr, oldDpiY)
+
+    def setDpi(self, value):
+        self.dpi = value
+        self.repaint()
+
 class Widget(QtGui.QWidget, ScreenWidget):
     title = ki18n("Welcome")
     desc = ki18n("Welcome to Kaptan Wizard :)")
@@ -27,6 +50,20 @@ class Widget(QtGui.QWidget, ScreenWidget):
         self.ui = Ui_dpiWidget()
         self.ui.setupUi(self)
 
+        currentDpi = QtGui.QX11Info.appDpiX()
+        self.pv = DpiPreview(ki18n("This is a sample text using 10 point font.").toString(), currentDpi)
+        self.pv.setWordWrap(True)
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.pv.setFont(font)
+        self.pv.setMinimumSize(200,100)
+        self.pv.setAlignment(Qt.AlignTop)
+
+        self.ui.verticalLayout.addWidget(self.pv)
+        self.ui.horizontalSlider.valueChanged.connect(self.pv.setDpi)
+
+        self.ui.horizontalSlider.setValue(currentDpi)
+        self.ui.spinBox.setValue(currentDpi)
     def shown(self):
         pass
 
