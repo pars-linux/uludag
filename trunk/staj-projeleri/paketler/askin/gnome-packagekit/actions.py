aff8676c00ccf2573305dd893d9a3f91da0219f8
askin.yollu <askin.yollu@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-08-21 13:46:29 +0000

    FIXED:sandbox violation

--- trunk/staj-projeleri/paketler/askin/gnome-packagekit/actions.py
+++ trunk/staj-projeleri/paketler/askin/gnome-packagekit/actions.py
@@ -7,9 +7,13 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
+    shelltools.unlink("py-compile" )
+    shelltools.sym("/bin/true", "%s/py-compile" % get.curDIR())
+    #autotools.autoreconf("-ivf")
     autotools.configure("--disable-schemas-install")
 
 def build():
