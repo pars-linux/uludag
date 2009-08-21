7fae15c174832b9a3dbf44bcc04a41d5a3d3d9e7
caio <caio@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-08-21 20:05:59 +0000

    bug-reporting-tool: updating setup.py files

--- gsoc/bug-reporting-tool/bugtool/src/temporal-generate.py
+++ gsoc/bug-reporting-tool/bugtool/src/temporal-generate.py
@@ -4,7 +4,7 @@
 import os, glob
 
 for filename in glob.glob1("gui/widgets", "*.ui"):
-    os.system("pykdeuic4 -o gui/%s.py gui/widgets/%s" % (filename.split(".")[0], filename))
+    os.system("pykde4uic -o gui/%s.py gui/widgets/%s" % (filename.split(".")[0], filename))
 
 for filename in glob.glob1("gui", "*.qrc"):
     os.system("pyrcc4 gui/%s -o gui/%s_rc.py" % (filename, filename.split(".")[0]))
