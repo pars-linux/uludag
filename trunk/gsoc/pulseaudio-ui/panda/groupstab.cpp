0b40cd5fcbea4843db5691a0069d8d9719899266
tharkang <tharkang@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-08-24 15:58:20 +0000

    I18N + changes in groups widget

--- trunk/gsoc/pulseaudio-ui/panda/groupstab.cpp
+++ trunk/gsoc/pulseaudio-ui/panda/groupstab.cpp
@@ -6,16 +6,16 @@
 GroupsTab::GroupsTab(GroupManager* manager, QWidget* parent): QWidget(parent)
 {
     this->manager = manager;
-    scrollArea = new QScrollArea(this);
+    //scrollArea = new QScrollArea(this);
     //scrollArea->setLayout(scrolledLayout);
-    scrolledWidget = new QWidget(scrollArea);
+    //scrolledWidget = new QWidget(scrollArea);
     //scrolledWidget = scrollArea;
-    scrolledLayout = new QVBoxLayout(scrolledWidget);
+    //scrolledLayout = new QVBoxLayout(scrolledWidget);
+    layout = new QVBoxLayout(this);
     foreach(QString name, manager->groupNames())
 	createGroup(name);
-    scrollArea->setWidget(scrolledWidget);
-    layout = new QVBoxLayout(this);
-    layout->addWidget(scrollArea);
+    //scrollArea->setWidget(scrolledWidget);
+    //layout->addWidget(scrollArea);
     QObject::connect(manager, SIGNAL(groupCreated(QString)), this, SLOT(createGroup(QString)));
     QObject::connect(manager, SIGNAL(groupRemoved(QString)), this, SLOT(removeGroup(QString)));
 }
@@ -24,8 +24,8 @@ GroupsTab::GroupsTab(GroupManager* manager, QWidget* parent): QWidget(parent)
 void GroupsTab::createGroup(QString name)
 {
     Group *g = manager->group(name);
-    GroupWidget *gw = new GroupWidget(g, scrolledWidget);
-    scrolledLayout->addWidget(gw);
+    GroupWidget *gw = new GroupWidget(g, this);
+    /*scrolledL*/layout->addWidget(gw);
 }
 
 void GroupsTab::removeGroup(QString name)
