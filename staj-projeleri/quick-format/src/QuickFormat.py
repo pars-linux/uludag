7d149c01d39fc8ba3d723080a47b58d414eb4106
renan.cakirerk <renan.cakirerk@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-09-02 11:00:48 +0000

    Added quick-format project

---
+++ staj-projeleri/quick-format/src/QuickFormat.py
@@ -0,0 +1,179 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'usb_format.ui'
+#
+# Created: Tue Sep  1 13:05:58 2009
+#      by: PyQt4 UI code generator 4.5.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+import sys,os
+from subprocess import Popen, PIPE, STDOUT, call
+
+
+fileSystems = {"Extended 4":"ext4", 
+               "Extended 3":"ext3",
+               "Extended 2":"ext2",
+               "FAT 16/32":"vfat",
+               "NTFS":"ntfs",
+               "Reiser FS":"reiserfs",
+               "XFS":"xfs"}
+
+    
+class Ui_MainWindow(object):
+    
+    def addFileSystems(self):
+        for fs in fileSystems:
+            ui.cmb_fileSystem.addItem(fs)
+            
+    def formatDisk(self): 
+        print "deem"
+       
+        fs = fileSystems[str(ui.cmb_fileSystem.itemText(ui.cmb_fileSystem.currentIndex()))]
+        if fs == "ntfs":
+            option = "-Q"
+        else:
+            option = ""
+        
+        ui.progressBar.setMaximum(0)
+        ui.lbl_progress = "Please wait while formatting..."
+        
+        proc = Popen("mkfs -t " + fs + " " + option + " /dev/sdb1", shell=True, stdout=PIPE,)
+        print proc.communicate()[0]
+        
+        ui.progressBar.setMaximum(1)
+        ui.lbl_progress = "Formatted Successfully!"
+#        print commands.getoutput("mkfs -t " + fs + " -Q /dev/sdb1")
+        
+#        proc = Popen("mkfs -t " + fs + " -Q /dev/sdb1", shell=True, stdout=PIPE,)
+#        output = proc.communicate()[0]
+#        print output
+#        
+##        while(True):
+##            print output
+
+    
+
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(270, 278)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        MainWindow.setSizePolicy(sizePolicy)
+        MainWindow.setMinimumSize(QtCore.QSize(270, 278))
+        MainWindow.setMaximumSize(QtCore.QSize(270, 278))
+        MainWindow.setToolButtonStyle(QtCore.Qt.ToolButtonIconOnly)
+        MainWindow.setDockOptions(QtGui.QMainWindow.AllowTabbedDocks|QtGui.QMainWindow.AnimatedDocks)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setEnabled(True)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtGui.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.grpBoxDeviceName = QtGui.QGroupBox(self.centralwidget)
+        self.grpBoxDeviceName.setMinimumSize(QtCore.QSize(0, 150))
+        self.grpBoxDeviceName.setMaximumSize(QtCore.QSize(16777215, 150))
+        font = QtGui.QFont()
+        font.setWeight(75)
+        font.setBold(True)
+        self.grpBoxDeviceName.setFont(font)
+        self.grpBoxDeviceName.setObjectName("grpBoxDeviceName")
+        self.verticalLayoutWidget = QtGui.QWidget(self.grpBoxDeviceName)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(10, 25, 241, 106))
+        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
+        self.verticalLayout_2 = QtGui.QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.lbl_fileSystem = QtGui.QLabel(self.verticalLayoutWidget)
+        font = QtGui.QFont()
+        font.setWeight(50)
+        font.setBold(False)
+        self.lbl_fileSystem.setFont(font)
+        self.lbl_fileSystem.setObjectName("lbl_fileSystem")
+        self.verticalLayout_2.addWidget(self.lbl_fileSystem)
+        self.cmb_fileSystem = QtGui.QComboBox(self.verticalLayoutWidget)
+        font = QtGui.QFont()
+        font.setWeight(50)
+        font.setBold(False)
+        self.cmb_fileSystem.setFont(font)
+        self.cmb_fileSystem.setObjectName("cmb_fileSystem")
+        self.verticalLayout_2.addWidget(self.cmb_fileSystem)
+        self.lbl_volumeLabel = QtGui.QLabel(self.verticalLayoutWidget)
+        font = QtGui.QFont()
+        font.setWeight(50)
+        font.setBold(False)
+        self.lbl_volumeLabel.setFont(font)
+        self.lbl_volumeLabel.setObjectName("lbl_volumeLabel")
+        self.verticalLayout_2.addWidget(self.lbl_volumeLabel)
+        self.lineEdit = QtGui.QLineEdit(self.verticalLayoutWidget)
+        self.lineEdit.setMinimumSize(QtCore.QSize(0, 25))
+        font = QtGui.QFont()
+        font.setWeight(50)
+        font.setBold(False)
+        self.lineEdit.setFont(font)
+        self.lineEdit.setObjectName("lineEdit")
+        self.verticalLayout_2.addWidget(self.lineEdit)
+        self.verticalLayout.addWidget(self.grpBoxDeviceName)
+        self.lbl_progress = QtGui.QLabel(self.centralwidget)
+        self.lbl_progress.setMaximumSize(QtCore.QSize(16777215, 20))
+        self.lbl_progress.setAlignment(QtCore.Qt.AlignCenter)
+        self.lbl_progress.setObjectName("lbl_progress")
+        self.verticalLayout.addWidget(self.lbl_progress)
+        self.progressBar = QtGui.QProgressBar(self.centralwidget)
+        self.progressBar.setMinimumSize(QtCore.QSize(0, 0))
+        self.progressBar.setMaximumSize(QtCore.QSize(16777215, 20))
+        self.progressBar.setAutoFillBackground(False)
+        self.progressBar.setMaximum(1)
+        self.progressBar.setProperty("value", QtCore.QVariant(55973))
+        self.progressBar.setTextVisible(False)
+        self.progressBar.setOrientation(QtCore.Qt.Horizontal)
+        self.progressBar.setInvertedAppearance(False)
+        self.progressBar.setTextDirection(QtGui.QProgressBar.TopToBottom)
+        self.progressBar.setObjectName("progressBar")
+        self.verticalLayout.addWidget(self.progressBar)
+        spacerItem = QtGui.QSpacerItem(20, 20, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Fixed)
+        self.verticalLayout.addItem(spacerItem)
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        spacerItem1 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem1)
+        self.btn_format = QtGui.QPushButton(self.centralwidget)
+        self.btn_format.setObjectName("btn_format")
+        self.horizontalLayout.addWidget(self.btn_format)
+        self.btn_cancel = QtGui.QPushButton(self.centralwidget)
+        self.btn_cancel.setObjectName("btn_cancel")
+        self.horizontalLayout.addWidget(self.btn_cancel)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 270, 23))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QObject.connect(self.btn_format, QtCore.SIGNAL("clicked()"), self.formatDisk)
+        QtCore.QObject.connect(self.btn_cancel, QtCore.SIGNAL("clicked()"), MainWindow.close)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "Disk Formatting Utility", None, QtGui.QApplication.UnicodeUTF8))
+        self.grpBoxDeviceName.setTitle(QtGui.QApplication.translate("MainWindow", "Kingston USB 2.0 4GB", None, QtGui.QApplication.UnicodeUTF8))
+        self.lbl_fileSystem.setText(QtGui.QApplication.translate("MainWindow", "File System", None, QtGui.QApplication.UnicodeUTF8))
+        self.lbl_volumeLabel.setText(QtGui.QApplication.translate("MainWindow", "Volume Label", None, QtGui.QApplication.UnicodeUTF8))
+        self.lbl_progress.setText(QtGui.QApplication.translate("MainWindow", "Progress", None, QtGui.QApplication.UnicodeUTF8))
+        self.btn_format.setText(QtGui.QApplication.translate("MainWindow", "Format", None, QtGui.QApplication.UnicodeUTF8))
+        self.btn_cancel.setText(QtGui.QApplication.translate("MainWindow", "Cancel", None, QtGui.QApplication.UnicodeUTF8))
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    MainWindow = QtGui.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    ui.addFileSystems()
+
+    
+    MainWindow.show()
+        
+    
+    sys.exit(app.exec_())
