7ca08a2a0223e73c186f7c5af013051add5514ec
gkmngrgn <gkmngrgn@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-09-03 07:51:45 +0000

    move func to true line.

--- trunk/staj-projeleri/puding/src/puding.py
+++ trunk/staj-projeleri/puding/src/puding.py
@@ -3,17 +3,17 @@
 #
 # puding.py
 # Copyright (C) Gökmen Görgen 2009 <gkmngrgn@gmail.com>
-# 
+#
 # puding.py is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
 # Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # puding.py is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -47,7 +47,7 @@ class Options:
         if opts.create:
             if not os.getuid() == 0:
                 print(_("You need superuser permissions to run this application."))
- 
+
                 sys.exit(0)
 
             try:
