bbcc8f5f25db147f244f95f7ce67c211b5f4c78f
enes.albay <enes.albay@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-09-07 13:05:10 +0000

    tv-manager improved with configurations

--- trunk/staj-projeleri/tv-manager/src/ui_tvconfigui.h
+++ trunk/staj-projeleri/tv-manager/src/ui_tvconfigui.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading ui file 'tvconfigui.ui'
 **
-** Created: Tue Sep 1 14:53:54 2009
+** Created: Wed Sep 2 16:24:44 2009
 **      by: Qt User Interface Compiler version 4.5.2
 **
 ** WARNING! All changes made in this file will be lost when recompiling ui file!
