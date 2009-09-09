dc93db15c45bbdf8eb71b128cf445b0ce52294a5
cihan <cihan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-09-09 18:57:32 +0000

    fix about.py

--- staj-projeleri/puma/code/about.py
+++ staj-projeleri/puma/code/about.py
@@ -4,8 +4,8 @@
 
 from PyKDE4.kdecore import KAboutData, ki18n
 
-appName = "Puma"
-catalog = ""
+appName = "puma"
+modName = "puma"
 programName = ki18n("Puma")
 version = "0.0.1"
 description = ki18n("PUMA")
@@ -15,4 +15,10 @@ text = ki18n(" ")
 homePage = "http://www.pardus.org.tr/eng/projects"
 bugEmail = "okyaycihan@gmail.com"
 catalog = appName
+aboutData = KAboutData(appName, catalog, programName, version, description, license, copyright, text, homePage, bugEmail)
+
+# Author(s)
+aboutData.addAuthor(ki18n("Cihan Okyay"), ki18n("Current Maintainer"))
+
+
 
