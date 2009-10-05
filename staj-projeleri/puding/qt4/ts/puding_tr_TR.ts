99b23aab69035b4dc51e1807cf8bd06fb71cffec
gkmngrgn <gkmngrgn@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-10-05 23:15:26 +0000

    update translation files. updated source files for translations.

--- staj-projeleri/puding/qt4/ts/puding_tr_TR.ts
+++ staj-projeleri/puding/qt4/ts/puding_tr_TR.ts
@@ -5,8 +5,8 @@
     <name>Create</name>
     <message>
         <location filename="uiQt.py" line="55"/>
-        <source>Select ISO Image</source>
-        <translation type="unfinished">CD kalıbını seçin</translation>
+        <source>Select CD Image</source>
+        <translation type="unfinished">CD Kalıbı Seçin</translation>
     </message>
     <message>
         <location filename="uiQt.py" line="56"/>
@@ -21,108 +21,145 @@
     <message>
         <location filename="uiQt.py" line="67"/>
         <source>You should select a valid directory.</source>
-        <translation type="unfinished">Geçerli bir dizin seçmeliniz.</translation>
+        <translation type="unfinished">Geçerli bir dizin seçmelisiniz.</translation>
     </message>
     <message>
-        <location filename="uiQt.py" line="74"/>
+        <location filename="uiQt.py" line="75"/>
+        <source>Puding is an USB image creator for Pardus Linux.</source>
+        <translation type="unfinished">Puding, Pardus için yazılmış bir USB kalıbıdır.</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="75"/>
+        <source>Copyright (c) %s TUBITAK / UEKAE</source>
+        <translation type="unfinished">Telif Hakkı (c) %s TÜBİTAK / UEKAE</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="77"/>
         <source>About Puding</source>
         <translation type="unfinished">Puding Hakkında</translation>
     </message>
     <message>
-        <location filename="uiQt.py" line="89"/>
+        <location filename="uiQt.py" line="100"/>
         <source>Directory is Invalid</source>
-        <translation type="unfinished">Dizin geçersiz</translation>
+        <translation type="unfinished">Geçersiz Dizin</translation>
     </message>
     <message>
-        <location filename="uiQt.py" line="89"/>
+        <location filename="uiQt.py" line="100"/>
         <source>Please check the USB disk path.</source>
-        <translation type="unfinished">Lütfen USB dizinini kontrol edin.</translation>
+        <translation type="unfinished">Lütfen USB bellek yolunu kontrol edin.</translation>
     </message>
     <message>
-        <location filename="uiQt.py" line="116"/>
-        <source>
-Please double check your path information. If you don&apos;t type the path to the USB stick correctly, you may damage your computer. Would you like to continue?
-
-CD Image Path: %s
-USB Device: %s (%s)
-
-Release Name: %s
-Md5sum: %s
-Download URL: %s</source>
-        <translation type="unfinished"></translation>
+        <location filename="uiQt.py" line="136"/>
+        <source>CD Image is Invalid</source>
+        <translation type="unfinished">Geçersiz CD Kalıbı</translation>
     </message>
     <message>
-        <location filename="uiQt.py" line="118"/>
-        <source>Confirm Informations</source>
-        <translation type="unfinished">Bilgileri Doğrulayın</translation>
+        <location filename="uiQt.py" line="131"/>
+        <source>Please set an CD image path.</source>
+        <translation type="unfinished">Lütfen bir CD kalıbı yolu belirtin.</translation>
     </message>
     <message>
-        <location filename="uiQt.py" line="137"/>
-        <source>ISO Image is Invalid</source>
-        <translation type="unfinished">CD Kalıbı Geçersiz</translation>
-    </message>
-    <message>
-        <location filename="uiQt.py" line="132"/>
-        <source>Please set an ISO image path.</source>
-        <translation type="unfinished">Lütfen CD kalıbı yolunu belirtin.</translation>
-    </message>
-    <message>
-        <location filename="uiQt.py" line="137"/>
-        <source>Please check the ISO image path.</source>
+        <location filename="uiQt.py" line="136"/>
+        <source>Please check the CD image path.</source>
         <translation type="unfinished">Lütfen CD kalıbı yolunu kontrol edin.</translation>
     </message>
     <message>
-        <location filename="uiQt.py" line="144"/>
+        <location filename="uiQt.py" line="143"/>
         <source>Verify Checksum</source>
-        <translation type="unfinished">Veriyi Doğrula</translation>
+        <translation type="unfinished">Kaynak Doğrulaması</translation>
     </message>
     <message>
-        <location filename="uiQt.py" line="145"/>
+        <location filename="uiQt.py" line="144"/>
         <source>The checksum of the source is checking now...</source>
-        <translation type="unfinished">Kaynak veri şimdi doğrulanıyor...</translation>
+        <translation type="unfinished">Şimdi kaynak doğrulaması yapılıyor...</translation>
     </message>
     <message>
-        <location filename="uiQt.py" line="165"/>
+        <location filename="uiQt.py" line="164"/>
         <source>Checksum invalid</source>
-        <translation type="unfinished">Doğrulama geçersiz</translation>
+        <translation type="unfinished">Geçersiz Doğrulama</translation>
     </message>
     <message>
-        <location filename="uiQt.py" line="184"/>
+        <location filename="uiQt.py" line="185"/>
         <source>Creating Image</source>
         <translation type="unfinished">Kalıp Oluşturuluyor</translation>
     </message>
     <message>
-        <location filename="uiQt.py" line="185"/>
+        <location filename="uiQt.py" line="186"/>
         <source>Creating image...</source>
         <translation type="unfinished">Kalıp oluşturuluyor...</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="207"/>
+        <source>USB Image is Ready</source>
+        <translation type="unfinished">USB Kalıbı Hazır</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="207"/>
+        <source>Your USB image is ready. Now you can install or run Pardus from USB storage.</source>
+        <translation type="unfinished">USB kalıbınız hazır. Şimdi Pardus&apos;unuzu USB bellekten kurabilir veya çalıştırabilirsiniz.</translation>
     </message>
 </context>
 <context>
     <name>Dialog</name>
     <message>
-        <location filename="qtSelectDisk.py" line="63"/>
+        <location filename="qtConfirmDialog.py" line="76"/>
+        <source>Confirm Informations</source>
+        <translation type="unfinished">Bilgileri Doğrula</translation>
+    </message>
+    <message>
+        <location filename="qtConfirmDialog.py" line="79"/>
+        <source>&lt;b&gt;USB Disk Path:&lt;/b&gt;</source>
+        <translation type="unfinished">&lt;b&gt;USB Bellek Yolu:&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="qtConfirmDialog.py" line="80"/>
+        <source>&lt;b&gt;Release Name:&lt;/b&gt;</source>
+        <translation type="unfinished">&lt;b&gt;Sürüm İsmi:&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="qtConfirmDialog.py" line="82"/>
+        <source>&lt;b&gt;Download URL:&lt;/b&gt;</source>
+        <translation type="unfinished">&lt;b&gt;İndirme Adresi:&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="qtProgressBar.py" line="35"/>
         <source>Dialog</source>
         <translation type="unfinished">Pencere</translation>
     </message>
     <message>
-        <location filename="qtProgressBar.py" line="36"/>
-        <source>TextLabel</source>
-        <translation type="unfinished"></translation>
+        <location filename="qtSelectDisk.py" line="63"/>
+        <source>Select USB Disk Path</source>
+        <translation type="unfinished">USB Bellek Yolunu Seçin</translation>
     </message>
     <message>
         <location filename="qtSelectDisk.py" line="64"/>
         <source>Available Disks:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Uygun Bellekler:</translation>
     </message>
     <message>
         <location filename="qtSelectDisk.py" line="65"/>
         <source>Or you can set mount disk directly:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Veya doğrudan bağlanmış dizini seçebilirsiniz:</translation>
     </message>
     <message>
         <location filename="qtSelectDisk.py" line="66"/>
         <source>Browse</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ara</translation>
+    </message>
+    <message>
+        <location filename="qtConfirmDialog.py" line="77"/>
+        <source>&lt;font color=&quot;red&quot;&gt;&lt;b&gt;Warning:&lt;/b&gt;&lt;/font&gt; Please double check your path information. If you don&apos;t type the path to the USB stick correctly, you may damage your computer. Would you like to continue?&lt;br /&gt;</source>
+        <translation type="unfinished">&lt;font color=&quot;red&quot;&gt;&lt;b&gt;Uyarı:&lt;/b&gt;&lt;/font&gt; Lütfen bilgilerin doğruluğundan emin olun. Eğer USB bellek yolunu yanlış yazmışsanız, sisteminize zarar verebilirsiniz. Devam etmek istiyor musunuz?&lt;br /&gt;</translation>
+    </message>
+    <message>
+        <location filename="qtConfirmDialog.py" line="78"/>
+        <source>&lt;b&gt;Image Path:&lt;/b&gt;</source>
+        <translation type="unfinished">&lt;b&gt;Kalıp Yolu:&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="qtConfirmDialog.py" line="81"/>
+        <source>&lt;b&gt;Checksum Value:&lt;/b&gt;</source>
+        <translation type="unfinished">&lt;b&gt;Doğrulama Değeri:&lt;/b&gt;</translation>
     </message>
 </context>
 <context>
@@ -130,12 +167,12 @@ Download URL: %s</source>
     <message>
         <location filename="qtMain.py" line="105"/>
         <source>Puding</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Puding</translation>
     </message>
     <message>
         <location filename="qtMain.py" line="106"/>
         <source>Image Path:</source>
-        <translation type="unfinished">CD Kalıbı Yolu:</translation>
+        <translation type="unfinished">Kalıp Yolu:</translation>
     </message>
     <message>
         <location filename="qtMain.py" line="109"/>
@@ -145,7 +182,7 @@ Download URL: %s</source>
     <message>
         <location filename="qtMain.py" line="108"/>
         <source>Disk Path:</source>
-        <translation type="unfinished">Disk Yolu:</translation>
+        <translation type="unfinished">Bellek Yolu:</translation>
     </message>
     <message>
         <location filename="qtMain.py" line="110"/>
@@ -176,27 +213,17 @@ Download URL: %s</source>
 <context>
     <name>ProgressIncrementCopy</name>
     <message>
-        <location filename="uiQt.py" line="308"/>
-        <source>Creating config files for boot loader...</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="uiQt.py" line="325"/>
-        <source>Copying pardus.img file...</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="uiQt.py" line="344"/>
-        <source>Copying %s...</source>
-        <translation type="unfinished"></translation>
+        <location filename="uiQt.py" line="336"/>
+        <source>Copying %s (%0.2fMB)</source>
+        <translation type="unfinished">Kopyalanıyor: %s (%0.2fMB)</translation>
     </message>
 </context>
 <context>
     <name>SelectDisk</name>
     <message>
-        <location filename="uiQt.py" line="220"/>
+        <location filename="uiQt.py" line="229"/>
         <source>Choose Mount Disk Path</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Bağlanmış Bellek Dizinini Seçin</translation>
     </message>
 </context>
 </TS>
