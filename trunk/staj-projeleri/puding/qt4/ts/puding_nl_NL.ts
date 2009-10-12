3a16d1f8a100b04d0aeef2e0556c37571660c1be
atolboo <atolboo@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-10-12 11:08:05 +0000

    Adding Dutch language file /trunk/staj-projeleri/puding/qt4/ts/puding_nl_NL.ts

---
+++ trunk/staj-projeleri/puding/qt4/ts/puding_nl_NL.ts
@@ -0,0 +1,188 @@
+<!DOCTYPE TS>
+<TS>
+  <context>
+    <name>Create</name>
+    <message>
+      <source>Select CD Image</source>
+      <translation>Selecteer CD-image</translation>
+    </message>
+    <message>
+      <source>Images</source>
+      <translation>Images</translation>
+    </message>
+    <message>
+      <source>Warning</source>
+      <translation>Waarschuwing</translation>
+    </message>
+    <message>
+      <source>You should select a valid directory.</source>
+      <translation>Selecteer een geldige map.</translation>
+    </message>
+    <message>
+      <source>Puding is an USB image creator for Pardus Linux.</source>
+      <translation>Puding maakt een USB-image aan voor Pardus Linux.</translation>
+    </message>
+    <message>
+      <source>Copyright (c) %s TUBITAK / UEKAE</source>
+      <translation>Copyright (c) %s TUBITAK / UEKAE</translation>
+    </message>
+    <message>
+      <source>About Puding</source>
+      <translation>Info over Puding</translation>
+    </message>
+    <message>
+      <source>Directory is Invalid</source>
+      <translation>Map is ongeldig</translation>
+    </message>
+    <message>
+      <source>Please check the USB disk path.</source>
+      <translation>Controleer het pad naar de USB-schijf.</translation>
+    </message>
+    <message>
+      <source>CD Image is Invalid</source>
+      <translation>CD-image is ongeldig</translation>
+    </message>
+    <message>
+      <source>Please set an CD image path.</source>
+      <translation>Stel het pad naar het CD-image in.</translation>
+    </message>
+    <message>
+      <source>Please check the CD image path.</source>
+      <translation>Controleer het pad naar het CD-image.</translation>
+    </message>
+    <message>
+      <source>Verify Checksum</source>
+      <translation>Controlesom verifiëren</translation>
+    </message>
+    <message>
+      <source>The checksum of the source is checking now...</source>
+      <translation>De controlesom van de bron wordt nu geverifieerd...</translation>
+    </message>
+    <message>
+      <source>Checksum invalid</source>
+      <translation>Controlesom is ongeldig</translation>
+    </message>
+    <message>
+      <source>Creating Image</source>
+      <translation>Image wordt aangemaakt</translation>
+    </message>
+    <message>
+      <source>Creating image...</source>
+      <translation>Image wordt aangemaakt...</translation>
+    </message>
+    <message>
+      <source>USB Image is Ready</source>
+      <translation>USB-image is gereed</translation>
+    </message>
+    <message>
+      <source>Your USB image is ready. Now you can install or run Pardus from USB storage.</source>
+      <translation>De USB-schijf is gereed. U kunt nu Pardus vanaf de USB-schijf installeren of opstarten.</translation>
+    </message>
+  </context>
+  <context>
+    <name>Dialog</name>
+    <message>
+      <source>Confirm Informations</source>
+      <translation>Gegevens bevestigen</translation>
+    </message>
+    <message>
+      <source>&lt;b>USB Disk Path:&lt;/b></source>
+      <translation>&lt;b>Pad naar USB-schijf:&lt;/b></translation>
+    </message>
+    <message>
+      <source>&lt;b>Release Name:&lt;/b></source>
+      <translation>&lt;b>Uitgavenaam:&lt;/b></translation>
+    </message>
+    <message>
+      <source>&lt;b>Download URL:&lt;/b></source>
+      <translation>&lt;b>Download-URL:&lt;/b></translation>
+    </message>
+    <message>
+      <source>Dialog</source>
+      <translation>Dialoog</translation>
+    </message>
+    <message>
+      <source>Select USB Disk Path</source>
+      <translation>Selecteer pad naar USB-schijf</translation>
+    </message>
+    <message>
+      <source>Available Disks:</source>
+      <translation>Beschikbare schijven:</translation>
+    </message>
+    <message>
+      <source>Or you can set mount disk directly:</source>
+      <translation>Of stel aankoppelpunt rechtstreeks in:</translation>
+    </message>
+    <message>
+      <source>Browse</source>
+      <translation>Zoeken</translation>
+    </message>
+    <message>
+      <source>&lt;font color=&quot;red&quot;>&lt;b>Warning:&lt;/b>&lt;/font> Please double check your path information. If you don't type the path to the USB stick correctly, you may damage your computer. Would you like to continue?&lt;br /></source>
+      <translation>&lt;font color=&quot;red&quot;>&lt;b>Waarschuwing:&lt;/b>&lt;/font> Wees er zeker van dat de 
+padinformatie juist is. Wanneer het pad naar de USB-stick niet juist is, kan het 
+de computer beschadigen. Doorgaan?&lt;br /> </translation>
+    </message>
+    <message>
+      <source>&lt;b>Image Path:&lt;/b></source>
+      <translation>&lt;b>Pad naar image:&lt;/b></translation>
+    </message>
+    <message>
+      <source>&lt;b>Checksum Value:&lt;/b></source>
+      <translation>&lt;b>Controlesomwaarde:&lt;/b></translation>
+    </message>
+  </context>
+  <context>
+    <name>MainWindow</name>
+    <message>
+      <source>Puding</source>
+      <translation>Puding</translation>
+    </message>
+    <message>
+      <source>Image Path:</source>
+      <translation>Pad naar image:</translation>
+    </message>
+    <message>
+      <source>Browse</source>
+      <translation>Zoeken</translation>
+    </message>
+    <message>
+      <source>Disk Path:</source>
+      <translation>Pad naar schijf:</translation>
+    </message>
+    <message>
+      <source>&amp;Create</source>
+      <translation>&amp;Aanmaken</translation>
+    </message>
+    <message>
+      <source>&amp;Quit</source>
+      <translation>Af&amp;sluiten</translation>
+    </message>
+    <message>
+      <source>&amp;File</source>
+      <translation>&amp;Bestand</translation>
+    </message>
+    <message>
+      <source>&amp;Help</source>
+      <translation>&amp;Help</translation>
+    </message>
+    <message>
+      <source>About</source>
+      <translation>Info over</translation>
+    </message>
+  </context>
+  <context>
+    <name>ProgressIncrementCopy</name>
+    <message>
+      <source>Copying %s (%0.2fMB)</source>
+      <translation>Kopiëren %s (%0.2fMB)</translation>
+    </message>
+  </context>
+  <context>
+    <name>SelectDisk</name>
+    <message>
+      <source>Choose Mount Disk Path</source>
+      <translation>Aankoppelpunt schijf selecteren</translation>
+    </message>
+  </context>
+</TS>
