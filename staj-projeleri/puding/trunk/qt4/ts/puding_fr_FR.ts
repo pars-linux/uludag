33c95f33d84c30887101649efb21e0f0d4f9728f
philippe.svetoslavsky <philippe.svetoslavsky@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-10-19 22:40:51 +0000

    French translation

---
+++ staj-projeleri/puding/trunk/qt4/ts/puding_fr_FR.ts
@@ -0,0 +1,248 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1"> 
+<context>
+    <name>Create</name>
+    <message>
+        <location filename="uiQt.py" line="55"/>
+        <source>Select CD Image</source>
+        <translation>Choisissez une image de CD</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="56"/>
+        <source>Images</source>
+        <translation>Images</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="67"/>
+        <source>Warning</source>
+        <translation>Attention</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="67"/>
+        <source>You should select a valid directory.</source>
+        <translation>Vous devez choisir un dossier valide.</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="75"/>
+        <source>Puding is an USB image creator for Pardus Linux.</source>
+        <translation>Puding est un créateur d'image de Pardus Linux pour clef USB.</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="75"/>
+        <source>Copyright (c) %s TUBITAK / UEKAE</source>
+        <translation>Copyright (c) %s TUBITAK / UEKAE</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="77"/>
+        <source>About Puding</source>
+        <translation>À propos de Puding</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="102"/>
+        <source>Directory is Invalid</source>
+        <translation>Le dossier indiqué est invalide</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="102"/>
+        <source>Please check the USB disk path.</source>
+        <translation>Vérifiez le chemin de la clef USB s'il vous plaît.</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="158"/>
+        <source>CD Image is Invalid</source>
+        <translation>L'image du CD est invalide</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="153"/>
+        <source>Please set an CD image path.</source>
+        <translation>Indiquez le chemin de l'image du CD s'il vous plaît.</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="158"/>
+        <source>Please check the CD image path.</source>
+        <translation>Vérifiez le chemin de l'image du CD.</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="165"/>
+        <source>Verify Checksum</source>
+        <translation>Vérifiez la somme de vérification (checksum)</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="166"/>
+        <source>The checksum of the source is checking now...</source>
+        <translation>Somme de vérification de la source en cours d'analyse...</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="186"/>
+        <source>Checksum invalid</source>
+        <translation>Somme de vérification invalide</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="201"/>
+        <source>Creating Image</source>
+        <translation>Création de l'image</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="201"/>
+        <source>Creating image...</source>
+        <translation>Création de l'image...</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="215"/>
+        <source>USB Image is Ready</source>
+        <translation>L'image USB est prête</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="215"/>
+        <source>Your USB image is ready. Now you can install or run Pardus from USB storage.</source>
+        <translation>votre image pour clef USB est prête. Vous pouvez maintenant installer ou lancer Pardus à artir de celle-ci.</translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="qtConfirmDialog.py" line="103"/>
+        <source>Confirm Informations</source>
+        <translation>Confirmer les informations</translation>
+    </message>
+    <message>
+        <location filename="qtConfirmDialog.py" line="110"/>
+        <source>&lt;b&gt;USB Disk Path:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Chemin de la clef USB :&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="qtConfirmDialog.py" line="108"/>
+        <source>&lt;b&gt;Release Name:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Nom de la version :&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="qtProgressBar.py" line="35"/>
+        <source>Dialog</source>
+        <translation>Dialogue</translation>
+    </message>
+    <message>
+        <location filename="qtSelectDisk.py" line="63"/>
+        <source>Select USB Disk Path</source>
+        <translation>Sélectionnez le chemin de la clef USB</translation>
+    </message>
+    <message>
+        <location filename="qtSelectDisk.py" line="64"/>
+        <source>Available Disks:</source>
+        <translation>Clefs disponibles :</translation>
+    </message>
+    <message>
+        <location filename="qtSelectDisk.py" line="65"/>
+        <source>Or you can set mount disk directly:</source>
+        <translation>Ou vous pouvez indiquer directement le point de montage de la clef :</translation>
+    </message>
+    <message>
+        <location filename="qtSelectDisk.py" line="66"/>
+        <source>Browse</source>
+        <translation>Naviguer</translation>
+    </message>
+    <message>
+        <location filename="qtConfirmDialog.py" line="104"/>
+        <source>&lt;font color=&quot;red&quot;&gt;&lt;b&gt;Warning:&lt;/b&gt;&lt;/font&gt; Please double check your path information. If you don&apos;t type the path to the USB stick correctly, you may damage your computer. Would you like to continue?&lt;br /&gt;</source>
+        <translation>&lt;font color=&quot;red&quot;&gt;&lt;b&gt;Attention :&lt;/b&gt;&lt;/font&gt; Vérifiez votre chemin. En cas d&apos;erreur, vous risquez d'endommager votre ordinateur. Voulez-vous continuer ?&lt;br /&gt;</translation>
+    </message>
+    <message>
+        <location filename="qtConfirmDialog.py" line="105"/>
+        <source>&lt;b&gt;Image Path:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Chemin de l'image :&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="qtConfirmDialog.py" line="106"/>
+        <source>&lt;b&gt;Capacity:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Capacité :&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="qtConfirmDialog.py" line="107"/>
+        <source>&lt;b&gt;Available:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Disponible :&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="qtConfirmDialog.py" line="109"/>
+        <source>&lt;i&gt;&lt;b&gt;USB Disk Informations&lt;/b&gt;&lt;/i&gt;</source>
+        <translation>&lt;i&gt;&lt;b&gt;Informations sur la clef USB&lt;/b&gt;&lt;/i&gt;</translation>
+    </message>
+    <message>
+        <location filename="qtConfirmDialog.py" line="111"/>
+        <source>&lt;i&gt;&lt;b&gt;Source Informations&lt;/b&gt;&lt;/i&gt;</source>
+        <translation>&lt;i&gt;&lt;b&gt;Informations sur la source&lt;/b&gt;&lt;/i&gt;</translation>
+    </message>
+    <message>
+        <location filename="qtConfirmDialog.py" line="112"/>
+        <source>&lt;b&gt;Used:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Utilisé :&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="qtConfirmDialog.py" line="113"/>
+        <source>&lt;b&gt;Image Size:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Taille de l'image :&lt;/b&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="qtMain.py" line="105"/>
+        <source>Puding</source>
+        <translation>Puding</translation>
+    </message>
+    <message>
+        <location filename="qtMain.py" line="106"/>
+        <source>Image Path:</source>
+        <translation>Chemin de l'image :</translation>
+    </message>
+    <message>
+        <location filename="qtMain.py" line="109"/>
+        <source>Browse</source>
+        <translation>Naviguer</translation>
+    </message>
+    <message>
+        <location filename="qtMain.py" line="108"/>
+        <source>Disk Path:</source>
+        <translation>Chemin de la clef USB :</translation>
+    </message>
+    <message>
+        <location filename="qtMain.py" line="110"/>
+        <source>&amp;Create</source>
+        <translation>&amp;Créer</translation>
+    </message>
+    <message>
+        <location filename="qtMain.py" line="114"/>
+        <source>&amp;Quit</source>
+        <translation">&amp;Quitter</translation>
+    </message>
+    <message>
+        <location filename="qtMain.py" line="112"/>
+        <source>&amp;File</source>
+        <translation>&amp;Fichier</translation>
+    </message>
+    <message>
+        <location filename="qtMain.py" line="113"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Aide</translation>
+    </message>
+    <message>
+        <location filename="qtMain.py" line="115"/>
+        <source>About</source>
+        <translation>À propos</translation>
+    </message>
+</context>
+<context>
+    <name>ProgressIncrementCopy</name>
+    <message>
+        <location filename="uiQt.py" line="347"/>
+        <source>Copying %s (%0.2fMB)</source>
+        <translation>Copie de %s (%0.2fMo)</translation>
+    </message>
+</context>
+<context>
+    <name>SelectDisk</name>
+    <message>
+        <location filename="uiQt.py" line="237"/>
+        <source>Choose Mount Disk Path</source>
+        <translation>Choisissez le chemin du point de montage</translation>
+    </message>
+</context>
+</TS>
