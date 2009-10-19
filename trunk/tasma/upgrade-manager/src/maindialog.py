aa0bd01d9a39b516919f98fa378da2e7e0fdcf69
faik <faik@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-10-19 13:26:16 +0000

    Start upgrade-manager kde3 port

---
+++ trunk/tasma/upgrade-manager/src/maindialog.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2009, TUBITAK/UEKAE
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file.
+#
+
+from qt import *
+
+from kdeui import *
+from kdecore import *
+
+from ui_maindialog import Ui_MainDialog
+
+import state
+
+class MainDialog(QDialog, Ui_MainDialog):
+    def __init__(self, parent=None):
+        QDialog.__init__(self, parent)
+        self.setupUi(self)
+        self.setWindowIcon(KIcon(":/data/package-manager.png"))
+        self.resetAllSteps()
+        self.setFonts()
+        self.state = state.State(self)
+
+    def setFonts(self):
+        self.normalFont = QFont()
+        self.normalFont.setWeight(50)
+        self.normalFont.setBold(False)
+
+        self.boldFont = QFont()
+        self.boldFont.setWeight(50)
+        self.boldFont.setBold(True)
+
+    def resetAllSteps(self):
+        for step in range(1, 5):
+            step_icon = getattr(self, "step%d_icon" % step)
+            step_icon.setPixmap(QPixmap(""))
+
+    def step_selected(self, step):
+        step_icon = getattr(self, "step%d_icon" % step)
+        step_icon.setPixmap(":/data/arrow.png")
+        step_label = getattr(self, "step%d_label" % step)
+        step_label.setFont(self.boldFont)
+
+    def step_finished(self, step):
+        step_icon = getattr(self, "step%d_icon" % step)
+        step_icon.setPixmap(":/data/check.png")
+        step_label = getattr(self, "step%d_label" % step)
+        step_label.setFont(self.normalFont)
