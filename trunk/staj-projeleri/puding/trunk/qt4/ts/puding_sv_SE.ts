e96b2f4dd8f24dab651f2475a115635b74d1b956
patrik <patrik@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-10-23 16:59:24 +0000

    Swedish translation.

---
+++ trunk/staj-projeleri/puding/trunk/qt4/ts/puding_sv_SE.ts
@@ -0,0 +1,249 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="sv_SE">
+<context>
+    <name>Create</name>
+    <message>
+        <location filename="uiQt.py" line="55"/>
+        <source>Select CD Image</source>
+        <translation>Välj avbild</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="56"/>
+        <source>Images</source>
+        <translation>Avbild</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="67"/>
+        <source>Warning</source>
+        <translation>Varning</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="67"/>
+        <source>You should select a valid directory.</source>
+        <translation>Välj en giltig katalog.</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="75"/>
+        <source>Puding is an USB image creator for Pardus Linux.</source>
+        <translation>Puding är en USB-avbildsskapare för Pardus Linux.</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="75"/>
+        <source>Copyright (c) %s TUBITAK / UEKAE</source>
+        <translation>Copyright (c) %s TUBITAK / UEKAE</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="77"/>
+        <source>About Puding</source>
+        <translation>Om Puding</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="102"/>
+        <source>Directory is Invalid</source>
+        <translation>Ogiltig katalog</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="102"/>
+        <source>Please check the USB disk path.</source>
+        <translation>Vänligen kontrollera USB-enhetens sökväg.</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="158"/>
+        <source>CD Image is Invalid</source>
+        <translation>CD-avbildningen är ogiltig</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="153"/>
+        <source>Please set an CD image path.</source>
+        <translation>Välj en avbild.</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="158"/>
+        <source>Please check the CD image path.</source>
+        <translation>Vänligen kontrollera avbildens sökväg.</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="165"/>
+        <source>Verify Checksum</source>
+        <translation>Verifiera kontrollsumman</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="166"/>
+        <source>The checksum of the source is checking now...</source>
+        <translation>Avbildens kontrollsumma verifieras...</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="186"/>
+        <source>Checksum invalid</source>
+        <translation>Felaktig kontrollsumma</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="201"/>
+        <source>Creating Image</source>
+        <translation>Skapar avbild</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="201"/>
+        <source>Creating image...</source>
+        <translation>Skapar avbild...</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="215"/>
+        <source>USB Image is Ready</source>
+        <translation>USB-enheten är färdig</translation>
+    </message>
+    <message>
+        <location filename="uiQt.py" line="215"/>
+        <source>Your USB image is ready. Now you can install or run Pardus from USB storage.</source>
+        <translation>Din USB-avbild är färdig. Du kan nu installera eller köra Pardus från den.</translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="qtConfirmDialog.py" line="103"/>
+        <source>Confirm Informations</source>
+        <translation>Bekräfta val</translation>
+    </message>
+    <message>
+        <location filename="qtConfirmDialog.py" line="110"/>
+        <source>&lt;b&gt;USB Disk Path:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Sökväg USB-enhet:&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="qtConfirmDialog.py" line="108"/>
+        <source>&lt;b&gt;Release Name:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Utgåvans namn:&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="qtProgressBar.py" line="35"/>
+        <source>Dialog</source>
+        <translation>Dialog</translation>
+    </message>
+    <message>
+        <location filename="qtSelectDisk.py" line="63"/>
+        <source>Select USB Disk Path</source>
+        <translation>Välj sökväg för USB-enhet</translation>
+    </message>
+    <message>
+        <location filename="qtSelectDisk.py" line="64"/>
+        <source>Available Disks:</source>
+        <translation>Tillgängliga enheter:</translation>
+    </message>
+    <message>
+        <location filename="qtSelectDisk.py" line="65"/>
+        <source>Or you can set mount disk directly:</source>
+        <translation>eller sätt monteringspunkt direkt:</translation>
+    </message>
+    <message>
+        <location filename="qtSelectDisk.py" line="66"/>
+        <source>Browse</source>
+        <translation>Välj</translation>
+    </message>
+    <message>
+        <location filename="qtConfirmDialog.py" line="104"/>
+        <source>&lt;font color=&quot;red&quot;&gt;&lt;b&gt;Warning:&lt;/b&gt;&lt;/font&gt; Please double check your path information. If you don&apos;t type the path to the USB stick correctly, you may damage your computer. Would you like to continue?&lt;br /&gt;</source>
+        <translation>&lt;font color=&quot;red&quot;&gt;&lt;b&gt;Varning:&lt;/b&gt;&lt;/font&gt; Vänligen kontrollera sökvägen. Om du anger fel sökväg kan du skada din dator. Vill du fortsätta?&lt;br /&gt;</translation>
+    </message>
+    <message>
+        <location filename="qtConfirmDialog.py" line="105"/>
+        <source>&lt;b&gt;Image Path:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Sökväg avbild:&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="qtConfirmDialog.py" line="106"/>
+        <source>&lt;b&gt;Capacity:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Kapacitet:&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="qtConfirmDialog.py" line="107"/>
+        <source>&lt;b&gt;Available:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Tillgängligt:&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="qtConfirmDialog.py" line="109"/>
+        <source>&lt;i&gt;&lt;b&gt;USB Disk Informations&lt;/b&gt;&lt;/i&gt;</source>
+        <translation>&lt;i&gt;&lt;b&gt;Information om USB-enhet&lt;/b&gt;&lt;/i&gt;</translation>
+    </message>
+    <message>
+        <location filename="qtConfirmDialog.py" line="111"/>
+        <source>&lt;i&gt;&lt;b&gt;Source Informations&lt;/b&gt;&lt;/i&gt;</source>
+        <translation>&lt;i&gt;&lt;b&gt;Information om avbild&lt;/b&gt;&lt;/i&gt;</translation>
+    </message>
+    <message>
+        <location filename="qtConfirmDialog.py" line="112"/>
+        <source>&lt;b&gt;Used:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Använt:&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="qtConfirmDialog.py" line="113"/>
+        <source>&lt;b&gt;Image Size:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Avbildens storlek:&lt;/b&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="qtMain.py" line="105"/>
+        <source>Puding</source>
+        <translation>Puding</translation>
+    </message>
+    <message>
+        <location filename="qtMain.py" line="106"/>
+        <source>Image Path:</source>
+        <translation>Sökväg avbild:</translation>
+    </message>
+    <message>
+        <location filename="qtMain.py" line="109"/>
+        <source>Browse</source>
+        <translation>Välj</translation>
+    </message>
+    <message>
+        <location filename="qtMain.py" line="108"/>
+        <source>Disk Path:</source>
+        <translation>Sökväg enhet:</translation>
+    </message>
+    <message>
+        <location filename="qtMain.py" line="110"/>
+        <source>&amp;Create</source>
+        <translation>&amp;Skapa</translation>
+    </message>
+    <message>
+        <location filename="qtMain.py" line="114"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Avsluta</translation>
+    </message>
+    <message>
+        <location filename="qtMain.py" line="112"/>
+        <source>&amp;File</source>
+        <translation>&amp;Arkiv</translation>
+    </message>
+    <message>
+        <location filename="qtMain.py" line="113"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Hjälp</translation>
+    </message>
+    <message>
+        <location filename="qtMain.py" line="115"/>
+        <source>About</source>
+        <translation>&amp;Om Puding</translation>
+    </message>
+</context>
+<context>
+    <name>ProgressIncrementCopy</name>
+    <message>
+        <location filename="uiQt.py" line="347"/>
+        <source>Copying %s (%0.2fMB)</source>
+        <translation>Kopierar %s (%0.2fMB)</translation>
+    </message>
+</context>
+<context>
+    <name>SelectDisk</name>
+    <message>
+        <location filename="uiQt.py" line="237"/>
+        <source>Choose Mount Disk Path</source>
+        <translation>Välj monteringspunkt</translation>
+    </message>
+</context>
+</TS>
