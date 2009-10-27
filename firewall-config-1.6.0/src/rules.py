b8046453de442b9a9e6e47075064a6f2337628ec
Fatih Aşıcı <fatih-asici@users.noreply.github.com>
2009-10-27 12:51:11 +0000

    Move firewall-manager tags

--- firewall-config-1.6.0/src/rules.py
+++
@@ -1,8 +0,0 @@
-named_rules = {
-    'inMail': '-p tcp -m multiport --dports 25,110',
-    'inWeb': '-p tcp -m multiport --dports 80,443',
-    'inRemote': '-p tcp -m multiport --dports 22',
-    'inWFS': '-p tcp -m multiport --dports 445',
-    'inIRC': '-p tcp -m multiport --dports 6667-6669',
-    'inFTP': '-p tcp -m multiport --dports 21',
-}
