fcf96fabef452b784ad21fb1c0c2ad076927c5ce
Fatih Aşıcı <fatih-asici@users.noreply.github.com>
2009-10-27 12:26:06 +0000

    Move yali tags

--- stajyer/app/models/document.rb
+++
@@ -1,5 +0,0 @@
-class Document < ActiveRecord::Base
-  belongs_to :student
-  has_attached_file :attached, :url => "/student/documents/:id/:style/:basename.:extension",
-                               :path => ':rails_root/public/student/documents/:id/:style/:basename.:extension'
-end
