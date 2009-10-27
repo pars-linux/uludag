b9534c234ad1503178b14d0d14a7a6f5088290d2
Fatih Aşıcı <fatih-asici@users.noreply.github.com>
2009-10-27 09:00:08 +0000

    Move kde3 branch of upgrade-manager

---
+++ upgrade-manager/kde3/src/state.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2009 TUBITAK/UEKAE
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING .
+#
+
+from qt import QObject, PYSIGNAL, QTimer
+from kdecore import i18n
+from kdeui import KMessageBox, KGuiItem
+
+import os
+import commander
+import pisiiface
+
+STEPS = ["prepare", "setRepositories", "download", "upgrade"]
+
+class State(QObject):
+
+    def __init__(self, parent):
+        self.parent = parent
+        self.comar = commander.Commander()
+        self.step = 0
+        self.connect(self.comar, PYSIGNAL("stepStarted(QString)"), self.stepStarted)
+        self.connect(self.comar, PYSIGNAL("stepFinished(QString)"), self.stepFinished)
+        self.connect(self.comar, PYSIGNAL("stepFinished(QString)"), lambda:QTimer.singleShot(1000, self.runNextStep))
+        self.connect(self.comar, PYSIGNAL("statusDownloading(int, int)"), self.statusDownloading)
+        self.connect(self.comar, PYSIGNAL("statusInstalling(QString, int, int)"), self.statusInstalling)
+        self.connect(self.comar, PYSIGNAL("statusConfiguring(QString, int, int)"), self.statusConfiguring)
+
+    def reset(self):
+        self.step = 0
+
+    def setProgress(self, total, current):
+        try:
+            percent = (total * 100) / current
+        except ZeroDivisionError:
+            percent = 100
+
+        self.progressBar.setProgress(percent)
+
+    def statusDownloading(self, total, current):
+        message = i18n("<qt>Downloading %1 of %2 packages</qt>").arg(current).arg(total)
+        self.parent.operationStatus.setText(message)
+        self.setProgress(total, current)
+
+    def statusInstalling(self, package, total, current):
+        message = i18n("<qt>Installing (%1) %2 of %3 packages</qt>").arg(package).arg(current).arg(total)
+        if current <= total:
+            self.parent.operationStatus.setText(message)
+            self.setProgress(total, current)
+
+    def statusConfiguring(self, package, total, current):
+        message = i18n("<qt>Configuring (%1) %2 of %3 packages</qt>").arg(package).arg(current).arg(total)
+        if current <= total:
+            self.parent.operationStatus.setText(message)
+            self.setProgress(total, current)
+
+    def stepStarted(self, operation):
+        # System.Upgrader.{prepare, setRepositories...}
+        step = operation.split(".")[-1]
+        self.parent.step_selected(STEPS.index(step) + 1)
+
+    def stepFinished(self, operation):
+        step = operation.split(".")[-1]
+        self.parent.step_finished(STEPS.index(step) + 1)
+        self.step += 1
+
+    def checkObsoletes(self):
+        obsoletes = pisiiface.getObsoletedList()
+        message = i18n("<qt>Following packages are obsoleted and are not maintained anymore in Pardus 2009 repositories. These packages are going to be removed from your system:<br><br> %1").arg(", ".join(obsoletes))
+        message += i18n("<br><br>Do you want to continue?</qt>")
+
+        if KMessageBox.Yes == KMessageBox.warningYesNo(self.parent,
+                                                       message,
+                                                       i18n("Warning"),
+                                                       KGuiItem(i18n("Continue"), "ok"),
+                                                       KGuiItem(i18n("Cancel"), "no"),
+                                                       ):
+            return True
+
+    def runNextStep(self):
+
+        if self.step >= len(STEPS):
+            return
+
+        if STEPS[self.step] == "download":
+            if not self.checkObsoletes():
+                self.parent.cancel()
+                return
+
+        if STEPS[self.step] in ["upgrade", "download"]:
+            self.parent.operationStatus.setText(i18n("Resolving dependencies..."))
+
+        method = getattr(self.comar, STEPS[self.step])
+        method()
