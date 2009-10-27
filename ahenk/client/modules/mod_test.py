9e17dca8bf1960682eab2cfa49da5da8392ce270
Fatih Aşıcı <fatih-asici@users.noreply.github.com>
2009-10-27 09:04:07 +0000

    Move mudur-1.0 branch

--- ahenk/client/modules/mod_test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-def test():
-    print "This is a test."
-
-timers = {
-        test: 10,
-}
--- playground/intern/paket-arama/src/index.sql
+++
@@ -1,3 +0,0 @@
-CREATE INDEX package_index USING BTREE on packages(package);
-CREATE INDEX repo_index USING BTREE on packages(repo);
-COMMIT;
