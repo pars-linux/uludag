3b69f9f52a506f8b3d3c361beec72a6efd8fcdbe
mete <mete@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-10-28 22:02:42 +0000

    Remove KActionSelector dependency and add replicas needed functionality

--- trunk/kde/pardusman/gui/languages.py
+++ trunk/kde/pardusman/gui/languages.py
@@ -52,21 +52,57 @@ class LanguagesDialog(QDialog, Ui_LanguagesDialog):
         # Ok/cancel buttons
         self.connect(self.buttonBox, SIGNAL("accepted()"), self.accept)
         self.connect(self.buttonBox, SIGNAL("rejected()"), self.reject)
+        self.connect(self.upButton, SIGNAL("clicked()"), self.buttonUpClicked)
+        self.connect(self.downButton, SIGNAL("clicked()"), self.buttonDownClicked)
+        self.connect(self.addButton, SIGNAL("clicked()"), self.buttonAddClicked)
+        self.connect(self.removeButton, SIGNAL("clicked()"), self.buttonRemoveClicked)
 
         # Go go go!
         self.initialize()
 
+    def buttonAddClicked(self):
+        for item in self.availableListWidget.selectedItems():
+            self.availableListWidget.takeItem(self.availableListWidget.row(item))
+            self.selectedListWidget.insertItem(self.selectedListWidget.currentRow() + 1, item)
+            self.selectedListWidget.setCurrentItem(item)
+
+    def buttonRemoveClicked(self):
+        for item in self.selectedListWidget.selectedItems():
+            self.selectedListWidget.takeItem(self.selectedListWidget.row(item))
+            self.availableListWidget.insertItem(self.selectedListWidget.currentRow() + 1, item)
+            self.availableListWidget.setCurrentItem(item)
+
+    def buttonUpClicked(self):
+        index = self.selectedListWidget.currentRow()
+        if index < 1:
+            return
+        else:
+            item = self.selectedListWidget.item(index)
+            self.selectedListWidget.takeItem(index)
+            self.selectedListWidget.insertItem(index-1, item)
+            self.selectedListWidget.setCurrentItem(item)
+
+    def buttonDownClicked(self):
+        index = self.selectedListWidget.currentRow()
+        if index < 0 or index > self.selectedListWidget.count()-1:
+            return
+        else:
+            item = self.selectedListWidget.item(index)
+            self.selectedListWidget.takeItem(index)
+            self.selectedListWidget.insertItem(index+1, item)
+            self.selectedListWidget.setCurrentItem(item)
+
     def accept(self):
         self.languages = []
-        selected = self.selectorLanguages.selectedListWidget()
+        selected = self.selectedListWidget
         for index in xrange(selected.count()):
             item = selected.item(index)
             self.languages.append(item.code)
         QDialog.accept(self)
 
     def initialize(self):
-        selected = self.selectorLanguages.selectedListWidget()
-        available = self.selectorLanguages.availableListWidget()
+        selected = self.selectedListWidget
+        available = self.availableListWidget
         for code in self.languages:
             item = LanguageWidgetItem(code, LANGUAGES[code])
             selected.addItem(item)
