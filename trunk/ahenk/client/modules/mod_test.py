cc6e474529ce5950ccf92a38c3d585ba03bf8692
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-11-03 13:44:41 +0000

    Policy modules are now lasses inherited from ahenj.ajan.utils.Policy

--- trunk/ahenk/client/modules/mod_test.py
+++ trunk/ahenk/client/modules/mod_test.py
@@ -1,12 +1,33 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import logging
+from ahenk.ajan import utils
 
-def updatePackages():
-    logging.debug("Updating!")
 
-def timers():
-    return {
-        "updatePackages": (updatePackages, 30),
-    }
+class PisiPolicy(utils.Policy):
+    label = "PiSi"
+
+    def init(self):
+        self.log.debug("*** Reloaded PiSi policy")
+
+    def settingsUpdated(self):
+        self.log.debug("*** Updated settings: %s" % self.settings)
+
+    def getTimers(self):
+        self.log.debug("*** Returning PiSi timers: %s" % self.settings)
+        interval = 20
+        if "pisiAutoUpdateInterval" in self.settings:
+            interval = int(self.settings["pisiAutoUpdateInterval"][0])
+
+        return {
+            "test123": (self.test, interval),
+        }
+
+    def apply(self):
+        self.log.debug("*** Applying PiSi settings: %s" % self.settings)
+
+    def test(self):
+        self.log.debug("*** TEST")
+
+
+policy = PisiPolicy()
