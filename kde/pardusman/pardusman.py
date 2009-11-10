3e23f2e9bc95e2a33a779bd3f3f36322867aa485
mete <mete@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-11-10 00:34:28 +0000

    Add collection selection for dvd

--- kde/pardusman/pardusman.py
+++ kde/pardusman/pardusman.py
@@ -14,6 +14,7 @@
 # System
 import sys
 import time
+from repotools import packages
 
 
 def maker(op, project_file):
@@ -28,45 +29,68 @@ def maker(op, project_file):
     start = time.time()
 
     if op == "make" or op == "make-repo":
+        print "make ilke girdi...."
         update_repo = True
         while True:
             try:
+                print "get_repo çağrıldı..."
                 project.get_repo(update_repo=update_repo)
-            except ExIndexBogus, e:
+            except packages.ExIndexBogus, e:
                 print "ERROR: Unable to load package index. URL is wrong, or file is corrupt."
                 return
-            except ExPackageCycle, e:
+            except packages.ExPackageCycle, e:
                 cycle = " > ".join(e.args[0])
                 print "ERROR: package index has errors. Cyclic dependency found:\n  %s." % cycle
                 return
-            except ExPackageMissing, e:
+            except packages.ExPackageMissing, e:
                 print "ERROR: Package index has errors. '%s' depends on non-existing '%s'." % e.args
                 return
+            print "get_missing çağrıldı"
             missing_components, missing_packages = project.get_missing()
+            print "missing_component"
+
             if len(missing_components):
                 print "WARNING: There are missing components. Removing."
-                for component in missing_components:
-                    if component in project.selected_components:
-                        project.selected_components.remove(component)
+                if project.package_collections:
+                    for component in missing_components:
+                        for collection in project.package_collections:
+                            if component in collection.packageSelectionselectedComponents:
+                                collection.packageSelection.selectedComponents.remove(component)
+                else:
+                    for component in missing_components:
+                        if component in project.selected_components:
+                            project.selected_components.remove(component)
                 update_repo=False
+            print "missing_packages"
             if len(missing_packages):
                 print "WARNING: There are missing packages. Removing."
-                for package in missing_packages:
-                    if package in project.selected_packages:
-                        project.selected_packages.remove(package)
+                if project.package_collections:
+                    for package in missing_packages:
+                        for collection in project.package_collections:
+                            if package in collection.packageSelectionselectedPackages:
+                                collection.packageSelection.selectedPackages.remove(package)
+                else:
+                    for package in missing_packages:
+                        if package in project.selected_packages:
+                            project.selected_packages.remove(package)
                 update_repo=False
             break
-
+        print "make_repos yaptı"
         maker.make_repos(project)
+
     if op == "check-repo":
+        print "print check_repo"
         maker.check_repo_files(project)
     if op == "make" or op == "make-live":
+        print "print make_image"
         maker.make_image(project)
     # install-live
     # configure-live
     if op == "make" or op == "make-live" or op == "pack-live":
+        print "squash_image"
         maker.squash_image(project)
     if op == "make" or op == "make-iso":
+        print "make_iso"
         maker.make_iso(project)
 
     end = time.time()
