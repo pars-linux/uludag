4729a7358597155fe4820804ea350e04b721e111
erenturkay <erenturkay@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-11-13 10:25:35 +0000

    Move urls for pending to the EOF

--- noan/noan/repository/urls.py
+++ noan/noan/repository/urls.py
@@ -1,10 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    # Pending
-    (r'^pending/$', 'noan.repository.views.page_pending_index'),
-    (r'^pending/(?P<distName>[^/]+)-(?P<distRelease>[^/]+)/$', 'noan.repository.views.list_pending_packages'),
-
     # Index
     (r'^$', 'noan.repository.views.repository_index'),
     # List all source pakages
@@ -15,4 +11,8 @@ urlpatterns = patterns('',
     (r'^(?P<distName>[^/]+)/(?P<distRelease>[^/]+)/(?P<sourceName>[^/]+)/(?P<packageName>[^/]+)/$', 'noan.repository.views.view_package_detail'),
     # Binary package (*.pisi) detail
     (r'^(?P<distName>[^/]+)/(?P<distRelease>[^/]+)/(?P<sourceName>[^/]+)/(?P<packageName>[^/]+)-(?P<binaryNo>\d+)/$', 'noan.repository.views.view_binary_detail'),
+
+    # Pending
+    (r'^pending/$', 'noan.repository.views.page_pending_index'),
+    (r'^pending/(?P<distName>[^/]+)-(?P<distRelease>[^/]+)/$', 'noan.repository.views.list_pending_packages'),
 )
