7d2096144e1055fa731a954ff249bca49591c87e
erenturkay <erenturkay@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-11-13 08:43:12 +0000

    make view names more readabe, it does not explain what it does :\ what a complicated name..

--- noan/noan/repository/urls.py
+++ noan/noan/repository/urls.py
@@ -3,18 +3,18 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     # Pending
     (r'^pending/$', 'noan.repository.views.page_pending_index'),
-    (r'^pending/(?P<distName>[^/]+)-(?P<distRelease>[^/]+)/$', 'noan.repository.views.page_pending'),
+    (r'^pending/(?P<distName>[^/]+)-(?P<distRelease>[^/]+)/$', 'noan.repository.views.list_pending_packages'),
 
     # Users
     (r'^users/$', 'noan.repository.views.page_users'),
     (r'^users/(?P<userName>[^/]+)/$', 'noan.repository.views.page_user'),
 
     # Index
-    (r'^$', 'noan.repository.views.page_index'),
+    (r'^$', 'noan.repository.views.repository_index'),
     # Sources
-    (r'^(?P<distName>[^/]+)-(?P<distRelease>[^/]+)/$', 'noan.repository.views.page_sources'),
+    (r'^(?P<distName>[^/]+)-(?P<distRelease>[^/]+)/$', 'noan.repository.views.list_source_packages'),
     # Source
-    (r'^(?P<distName>[^/]+)-(?P<distRelease>[^/]+)/(?P<sourceName>[^/]+)/$', 'noan.repository.views.page_source'),
+    (r'^(?P<distName>[^/]+)-(?P<distRelease>[^/]+)/(?P<sourceName>[^/]+)/$', 'noan.repository.views.view_source_package_detail'),
     # Package
     (r'^(?P<distName>[^/]+)-(?P<distRelease>[^/]+)/(?P<sourceName>[^/]+)/(?P<packageName>[^/]+)/$', 'noan.repository.views.page_package'),
     # Binary
