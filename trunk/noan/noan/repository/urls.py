65ce847dba80fb3996e79f47a690034bfd9bacd8
erenturkay <erenturkay@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-11-13 08:52:57 +0000

    * rename sources.html -> source-packages.list * change url behavior. /repository/Pardus/2009 is better than /repository/Pardus-2009

--- trunk/noan/noan/repository/urls.py
+++ trunk/noan/noan/repository/urls.py
@@ -12,11 +12,11 @@ urlpatterns = patterns('',
     # Index
     (r'^$', 'noan.repository.views.repository_index'),
     # Sources
-    (r'^(?P<distName>[^/]+)-(?P<distRelease>[^/]+)/$', 'noan.repository.views.list_source_packages'),
+    (r'^(?P<distName>[^/]+)/(?P<distRelease>[^/]+)/$', 'noan.repository.views.list_source_packages'),
     # Source
-    (r'^(?P<distName>[^/]+)-(?P<distRelease>[^/]+)/(?P<sourceName>[^/]+)/$', 'noan.repository.views.view_source_package_detail'),
+    (r'^(?P<distName>[^/]+)/(?P<distRelease>[^/]+)/(?P<sourceName>[^/]+)/$', 'noan.repository.views.view_source_package_detail'),
     # Package
-    (r'^(?P<distName>[^/]+)-(?P<distRelease>[^/]+)/(?P<sourceName>[^/]+)/(?P<packageName>[^/]+)/$', 'noan.repository.views.page_package'),
+    (r'^(?P<distName>[^/]+)/(?P<distRelease>[^/]+)/(?P<sourceName>[^/]+)/(?P<packageName>[^/]+)/$', 'noan.repository.views.page_package'),
     # Binary
-    (r'^(?P<distName>[^/]+)-(?P<distRelease>[^/]+)/(?P<sourceName>[^/]+)/(?P<packageName>[^/]+)/(?P<binaryNo>\d+)/$', 'noan.repository.views.page_binary'),
+    (r'^(?P<distName>[^/]+)/(?P<distRelease>[^/]+)/(?P<sourceName>[^/]+)/(?P<packageName>[^/]+)/(?P<binaryNo>\d+)/$', 'noan.repository.views.page_binary'),
 )
