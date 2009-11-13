c6b3d09dde0dd5c21b60bbb8eb0b7e9028162f58
erenturkay <erenturkay@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-11-13 22:05:38 +0000

    * Move CSS from base.html to style.html * Set /media/ not handled by Django, show directory index

--- noan/noan/urls.py
+++ noan/noan/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import *
 
+from noan.settings import DOCUMENT_ROOT
+
 # Enable the admin interface:
 from django.contrib import admin
 admin.autodiscover()
@@ -16,6 +18,8 @@ urlpatterns = patterns('',
     # Enable the admin interface:
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/(.*)', admin.site.root),
+    # Don't handle /media/
+    (r'^media/(.*)$', 'django.views.static.serve', {'document_root': '%s/media' % DOCUMENT_ROOT, 'show_indexes': True}),
 
     # login
     (r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'registration/login.html'}),
