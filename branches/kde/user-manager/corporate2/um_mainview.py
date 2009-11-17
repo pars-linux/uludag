439889170ccd1fabd6c2dd323b29e18afb076714
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-11-17 13:06:06 +0000

    UM for Corporate 2

---
+++ branches/kde/user-manager/corporate2/um_mainview.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2006, TUBITAK/UEKAE
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+
+import sys
+import time
+from qt import *
+from kdecore import *
+from kdeui import *
+
+import browser
+import useredit
+import groupedit
+from utility import *
+
+import comar
+import dbus
+
+class UserManager(QWidgetStack):
+    def __init__(self, parent):
+        QWidgetStack.__init__(self, parent)
+
+        self.link = comar.Link()
+        self.link.setLocale()
+
+        self.browse = browser.BrowseStack(self)
+        self.user = useredit.UserStack(self)
+        self.useredit = useredit.UserStack(self, edit=True)
+        self.group = groupedit.GroupStack(self)
+
+    def slotCancel(self):
+        self.raiseWidget(self.browse)
+
+    def slotAdd(self):
+        if self.browse.tab.currentPageIndex() == 0:
+            names = []
+            item = self.browse.users.firstChild()
+            while item:
+                names.append(item.nick)
+                item = item.nextSibling()
+            self.raiseWidget(self.user)
+            self.user.startAdd(self.browse.groups, names)
+        else:
+            self.raiseWidget(self.group)
+            self.group.startAdd()
+
+    def slotEdit(self):
+        self.raiseWidget(self.useredit)
+        self.useredit.startEdit(self.browse.groups, self.browse.users.selectedItem().uid)
