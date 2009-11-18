68301777b47219f98acc5266205194e8d9f1be17
Faik Uygur <faikuygur@gmail.com>
2009-11-18 14:26:42 +0000

    No summary for Groups anywhere

--- package-manager/corporate2/src/Group.py
+++ package-manager/corporate2/src/Group.py
@@ -11,18 +11,3 @@ class Group:
 
     def remove(self, package):
         self.packages.remove(package)
-
-class GroupTipper(QToolTip):
-    def __init__(self, parent):
-        super(GroupTipper, self).__init__(parent.groupsList.viewport())
-        self.groups = parent.groupDict
-        self.list = parent.groupsList
-        self.setWakeUpDelay(500)
-
-    def maybeTip(self, point):
-        item = self.list.itemAt(point)
-        if item:
-            group = self.groups[item]
-            self.tip(self.list.itemRect(item),
-                     u"<b>%s</b> - %s" %
-                     (group.name, group.summary))
