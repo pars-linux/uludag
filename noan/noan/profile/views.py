b5bf16e6c239a436f5cf3b28dad4f04e758f82f3
erenturkay <erenturkay@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-12-15 18:02:56 +0000

    * implement change password stuff

--- noan/noan/profile/views.py
+++ noan/noan/profile/views.py
@@ -13,7 +13,7 @@ from django.views.generic.list_detail import object_list
 # APP RELATED IMPORTS
 from noan.repository.models import Binary
 from noan.wrappers import render_response
-from noan.profile.forms import ProfileEditForm
+from noan.profile.forms import ProfileEditForm, PasswordChangeForm
 
 from noan.settings import USERS_PER_PAGE
 
@@ -27,7 +27,6 @@ def main(request):
 def user_profile(request):
     # form is posted
     if request.method == 'POST':
-        print "USER IS: %s" % request.user
         form = ProfileEditForm(request.POST)
         if form.is_valid():
             u = request.user
@@ -35,13 +34,28 @@ def user_profile(request):
             u.last_name = form.cleaned_data["lastname"]
             u.save()
 
-            return render_response(request, "profile/user-profile-edit.html", {"form": form, "profile_updated": True})
+            return render_response(request, "profile/user-profile-edit.html", {"profile_updated": True})
         else:
             return render_response(request, "profile/user-profile-edit.html", {"form": form})
 
     else:
         unbound_form = ProfileEditForm({"firstname": request.user.first_name, "lastname": request.user.last_name})
         return render_response(request, "profile/user-profile-edit.html", {"form": unbound_form})
+
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.POST)
+        form.setUser(request.user)
+        if form.is_valid():
+            request.user.set_password(form.cleaned_data["new_pass"])
+            request.user.save()
+            return render_response(request, "profile/password-change.html", {"form": form, "password_updated": True})
+        else:
+            return render_response(request, "profile/password-change.html", {"form": form})
+    else:
+        form = PasswordChangeForm()
+        return render_response(request, "profile/password-change.html", {"form": form})
 
 # List of the registered developers, (/user/list)
 def get_user_list(request):
