33906af30f7d3735ebbd898a2f4bb335945bb3eb
onur <onur@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2009-12-23 13:44:50 +0000

    lzm -> lzma

--- trunk/kde/pardusman/gui/main.py
+++ trunk/kde/pardusman/gui/main.py
@@ -398,7 +398,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.linePluginPackage.setText(unicode(self.project.plugin_package))
         self.lineParameters.setText(unicode(self.project.extra_params))
         self.comboType.setCurrentIndex(["install", "live"].index(self.project.type))
-        self.comboCompression.setCurrentIndex(["gzip", "lzm"].index(self.project.squashfs_comp_type))
+        self.comboCompression.setCurrentIndex(["gzip", "lzma"].index(self.project.squashfs_comp_type))
         self.comboSize.setCurrentIndex(["cd", "dvd", "usb", "custom"].index(self.project.media))
 
         self.listPackageCollection.clear()
