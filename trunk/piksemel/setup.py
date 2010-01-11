fb39705e5e09df3b99a2056c7211d913a54fa78c
fatih <fatih@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-01-11 09:36:14 +0000

    Compile with -fvisibility=hidden

--- trunk/piksemel/setup.py
+++ trunk/piksemel/setup.py
@@ -76,7 +76,9 @@ class Install(install):
 setup(
     name='piksemel',
     version=version,
-    ext_modules=[Extension('piksemel', ['src/iksemel.c', 'src/pyiks.c'])],
+    ext_modules=[Extension('piksemel',
+                            ['src/iksemel.c', 'src/pyiks.c'],
+                            extra_compile_args=["-fvisibility=hidden"])],
     cmdclass = {
         'install' : Install
     }
