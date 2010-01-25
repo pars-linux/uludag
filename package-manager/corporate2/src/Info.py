650d9e7428b714c3c3cef847237e398570524c89
Faik Uygur <faikuygur@gmail.com>
2010-01-25 13:44:43 +0000

    Initial counter

--- package-manager/corporate2/src/Info.py
+++ package-manager/corporate2/src/Info.py
@@ -13,6 +13,8 @@
 from qt import *
 from kdecore import *
 from InfoDialog import *
+from Notifier import *
+from Icons import *
 import PisiIface
 import Globals
 
@@ -21,12 +23,16 @@ class Info(InfoDialog):
         InfoDialog.__init__(self)
         self.parent = parent
         self.autoRestart = False
+        self.notifier = Notifier()
         self.iface = PisiIface.Iface()
+        self.timer = QTimer()
+        self.timer.connect(self.timer, SIGNAL("timeout()"), self.updateNotification)
         self.connect(self.okButton, SIGNAL('clicked()'), self.accept)
         self.connect(self.cancelButton, SIGNAL('clicked()'), self.reject)
         self.connect(self.autoRestartCheckBox, SIGNAL("toggled(bool)"), self.setAutoRestart)
 
     def reset(self):
+        self.counter = 10
         self.autoRestart = False
         self.autoRestartCheckBox.setChecked(False)
 
@@ -38,4 +44,19 @@ class Info(InfoDialog):
 
     def showRequirements(self):
         self.reset()
+        self.updateNotification()
         return self.exec_loop()
+
+    def updateNotification(self):
+        self.timer.stop()
+        self.showNotification()
+        if self.counter:
+            self.timer.start(1000)
+        self.counter -= 1
+
+    def showNotification(self):
+        icon = getIconPath("package-manager")
+        message = i18n("Will Restart in %1 Seconds!").arg(self.counter)
+        header = i18n("Will Restart!")
+        actions = ["cancel", unicode(i18n("Cancel"))]
+        self.notifier.show(icon, header, message, actions)
