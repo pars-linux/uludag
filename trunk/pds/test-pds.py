eb770da990efec75a9fcc3d5bc05d198e1f4d8ba
gokmen.goksel <gokmen.goksel@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-03-16 10:27:03 +0000

    * A new project named Pardus Desktop Services, v 0.1 beta \o/

--- trunk/pds/test-pds.py
+++ trunk/pds/test-pds.py
@@ -1,8 +1,20 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
-import time
+
+# Pardus Desktop Services Test-Suit
+# Copyright (C) 2010, TUBITAK/UEKAE
+# 2010 - Gökmen Göksel <gokmen:pardus.org.tr>
+
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+
+
+from time import time
 
 from PyQt4 import QtCore, QtGui
-from qticonloader import *
+import pds
 
 class Ui_Test(object):
     def setupUi(self, Test):
@@ -30,22 +42,28 @@ class Ui_Test(object):
         self.gridLayout.addWidget(self.label, 1, 0, 1, 3)
         self.getButton.clicked.connect(self.showIcon)
         QtCore.QMetaObject.connectSlotsByName(Test)
-        self.loader = QIconLoader(debug = False)
+        self.loader = pds.QIconLoader(debug = False)
 
-        print "Desktop Session : ", self.loader.desktopSession
-        print "Desktop Version : ", self.loader.desktopVersion
+        print "Desktop Session : ", self.loader.pds.session.Name
+        print "Desktop Version : ", self.loader.pds.session.Version
         print "Theme : ", self.loader.themeName
 
     def showIcon(self):
+        a = time()
+        print "Clicked !"
         icons = unicode(self.name.text())
         self.label.setPixmap(self.loader.load(icons.split(','), self.size.currentText()))
+        print 'It took : ', time() - a
 
 if __name__ == "__main__":
     import sys
     app = QtGui.QApplication(sys.argv)
     Test = QtGui.QWidget()
+    a = time()
+    print "Started !"
     ui = Ui_Test()
     ui.setupUi(Test)
     Test.show()
+    print 'It took : ', time() - a
     sys.exit(app.exec_())
 
