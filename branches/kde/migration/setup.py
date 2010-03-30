7c38c3826ca985e06ea631458a5253c7f95ed71d
renan.cakirerk <renan.cakirerk@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-03-30 08:04:51 +0000

    change setup.py

--- branches/kde/migration/setup.py
+++ branches/kde/migration/setup.py
@@ -21,7 +21,7 @@ from distutils.cmd import Command
 from distutils.command.build import build
 from distutils.command.install import install
 
-from src.migration import about as about
+#from src.migration import about as about
 
 def update_messages():
     # Create empty directory
@@ -29,16 +29,16 @@ def update_messages():
     os.makedirs(".tmp")
     # Collect UI files
     for filename in glob.glob1("src/migration/gui/ui", "*.ui"):
-        os.system("/usr/kde/4/bin/pykde4uic -o .tmp/ui_%s.py src/migration/gui/ui/%s" % (filename.split(".")[0], filename))
+        os.system("/usr/bin/pyuic4 -o .tmp/ui_%s.py src/migration/gui/ui/%s" % (filename.split(".")[0], filename))
     # Collect Python files
     for filename in glob.glob1("src/migration/gui", "*.py"):
         shutil.copy("src/migration/gui/%s" % filename, ".tmp")
     # Generate POT file
-    os.system("xgettext --default-domain=%s --keyword=_ --keyword=i18n --keyword=ki18n -o po/%s.pot .tmp/*" % (about.catalog, about.catalog))
+    os.system("xgettext --default-domain=%s --keyword=_ --keyword=i18n --keyword=ki18n -o po/%s.pot .tmp/*" % ("migration", "migration"))
     # Update PO files
     for item in os.listdir("po"):
         if item.endswith(".po"):
-            os.system("msgmerge -q -o .tmp/temp.po po/%s po/%s.pot" % (item, about.catalog))
+            os.system("msgmerge -q -o .tmp/temp.po po/%s po/%s.pot" % (item, "migration"))
             os.system("cp .tmp/temp.po po/%s" % item)
     # Remove temporary directory
     os.system("rm -rf .tmp")
@@ -57,7 +57,7 @@ class Build(build):
         os.system("cp -R src build/")
         # Copy compiled UIs and RCs
         for filename in glob.glob1("src/migration/gui/ui", "*.ui"):
-            os.system("/usr/kde/4/bin/pykde4uic -o build/migration/gui/ui/%s.py src/migration/gui/ui/%s" % (filename.split(".")[0], filename))
+            os.system("/usr/bin/pyuic4 -o build/migration/gui/ui/%s.py src/migration/gui/ui/%s" % (filename.split(".")[0], filename))
         for filename in glob.glob1("src/migration/gui/ui", "*.qrc"):
             os.system("/usr/bin/pyrcc4 src/migration/gui/ui/%s -o build/%s_rc.py" % (filename, filename.split(".")[0]))
 
@@ -65,12 +65,12 @@ class Install(install):
     def run(self):
         os.system("./setup.py build")
         if self.root:
-            kde_dir = "%s/usr/kde/4" % self.root
+            kde_dir = "%s/usr/kde/3.5" % self.root #edit
         else:
-            kde_dir = "/usr/kde/4"
+            kde_dir = "/usr/kde/3.5" #edit
         bin_dir = os.path.join(kde_dir, "bin")
         locale_dir = os.path.join(kde_dir, "share/locale")
-        autostart_dir = os.path.join(kde_dir, "share/applications/kde4")
+        autostart_dir = os.path.join(kde_dir, "share/applications/kde") #edit
         project_dir = os.path.join(kde_dir, "share/apps", about.appName)
         # Make directories
         makeDirs(bin_dir)
@@ -90,17 +90,17 @@ class Install(install):
                 os.makedirs(os.path.join(locale_dir, "%s/LC_MESSAGES" % lang))
             except OSError:
                 pass
-            shutil.copy("po/%s.mo" % lang, os.path.join(locale_dir, "%s/LC_MESSAGES" % lang, "%s.mo" % about.catalog))
+            shutil.copy("po/%s.mo" % lang, os.path.join(locale_dir, "%s/LC_MESSAGES" % lang, "%s.mo" % "migration"))
         # Rename
         #shutil.move(os.path.join(project_dir, "application.py"), os.path.join(project_dir, "%s.py" % about.appName))
         # Modes
-        os.chmod(os.path.join(project_dir, "%s.py" % about.appName), 0755)
+        os.chmod(os.path.join(project_dir, "%s.py" % "migration"), 0755)
         # Symlink
         try:
             if self.root:
-                os.symlink(os.path.join(project_dir.replace(self.root, ""), "%s.py" % about.appName), os.path.join(bin_dir, about.appName))
+                os.symlink(os.path.join(project_dir.replace(self.root, ""), "%s.py" % "migration"), os.path.join(bin_dir, "migration"))
             else:
-                os.symlink(os.path.join(project_dir, "%s.py" % about.appName), os.path.join(bin_dir, about.appName))
+                os.symlink(os.path.join(project_dir, "%s.py" % "migration"), os.path.join(bin_dir, "migration"))
         except OSError:
             pass
 
@@ -110,13 +110,13 @@ if "update_messages" in sys.argv:
     sys.exit(0)
 
 setup(
-      name              = about.appName,
-      version           = about.version,
-      description       = unicode(about.description),
-      license           = unicode(about.license),
-      author            = "",
-      author_email      = about.bugEmail,
-      url               = about.homePage,
+      name              = "Migration",
+      version           = "2",
+      description       = "abc",
+      license           = "GPLv2",
+      author            = "Renan Cakirerk",
+      author_email      = "",
+      url               = "",
       packages          = [''],
       package_dir       = {'': ''},
       data_files        = [],
