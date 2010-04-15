7260f7f3e347060aaa6544117f80c20ac048d142
ozan.caglayan <ozan.caglayan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-04-15 12:50:22 +0000

    krunner_pisi: Searches for a pisi package containing the given command and then installs it

---
+++ kde/krunner_pisi/pisirunner.h
@@ -0,0 +1,65 @@
+/*
+ *   Copyright (C) 2010 Ozan Çağlayan <ozan@pardus.org.tr>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License version 2 as
+ *   published by the Free Software Foundation
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef PISIRUNNER_H
+#define PISIRUNNER_H
+
+#include <Plasma/AbstractRunner>
+#include <KProcess>
+
+#include <QProcessEnvironment>
+#include <QHash>
+
+class QWidget;
+
+
+/**
+ * This class runs programs using the literal name of the binary, much as one
+ * would use at a shell prompt.
+ */
+class PisiRunner : public Plasma::AbstractRunner
+{
+    Q_OBJECT
+
+    public:
+        PisiRunner(QObject *parent, const QVariantList &args);
+        ~PisiRunner();
+
+        void match(Plasma::RunnerContext &context);
+        void run(const Plasma::RunnerContext &context, const Plasma::QueryMatch &action);
+
+    public Q_SLOTS:
+        void slotEndCommandNotFound();
+        void slotErrorCommandNotFound();
+
+    protected Q_SLOTS:
+        void init();
+        void prepareForMatchSession();
+
+    private:
+        bool m_enabled;
+        QHash<QString, QString> m_packageCache;
+        QString m_installer;
+        QStringList m_paths;
+        QProcessEnvironment m_processEnv;
+        KProcess* m_commandNotFound;
+};
+
+K_EXPORT_PLASMA_RUNNER(pisi, PisiRunner)
+
+#endif
