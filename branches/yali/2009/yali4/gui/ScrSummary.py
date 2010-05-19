91266a514253ef5c0e8c352f2c9699a6cf38b8ae
mete <mete@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-05-19 19:15:57 +0000

    -Only two button use to keep going installation     -Removing "Install" and "Cancel" buttons on Summary screen.     -Reboot action on popup menu takes "Cancel Button" 's place to reboot. Installation start with "Next" button clicking.

--- branches/yali/2009/yali4/gui/ScrSummary.py
+++ branches/yali/2009/yali4/gui/ScrSummary.py
@@ -61,8 +61,8 @@ Here you can see your install options before installation starts.
 
         # Handle translators tool problems ..
         try:
-            self.connect(self.ui.install, SIGNAL("clicked()"), ctx.mainScreen.slotNext)
-            self.connect(self.ui.cancel, SIGNAL("clicked()"), self.slotReboot)
+            #self.connect(self.ui.install, SIGNAL("clicked()"), ctx.mainScreen.slotNext)
+            #self.connect(self.ui.cancel, SIGNAL("clicked()"), self.slotReboot)
             self.connect(self.timer, SIGNAL("timeout()"), self.updateCounter)
         except:
             pass
@@ -80,6 +80,7 @@ Here you can see your install options before installation starts.
     def backCheck(self):
         self.timer.stop()
         ctx.yali.info.hide()
+        ctx.mainScreen.ui.buttonNext.setText(_("Next"))
         return True
 
     def updateCounter(self):
@@ -90,7 +91,8 @@ Here you can see your install options before installation starts.
             ctx.mainScreen.slotNext()
 
     def shown(self):
-        ctx.mainScreen.disableNext()
+        #ctx.mainScreen.disableNext()
+        ctx.mainScreen.ui.buttonNext.setText(_("Start Installation"))
         if ctx.installData.isKahyaUsed:
             self.startBombCounter()
         self.fillContent()
@@ -247,8 +249,8 @@ Here you can see your install options before installation starts.
                 ctx.mainScreen.moveInc = 0
                 return
 
-        self.ui.install.setEnabled(False)
-        self.ui.cancel.setEnabled(False)
+        #self.ui.install.setEnabled(False)
+        #self.ui.cancel.setEnabled(False)
         ctx.installData.installAllLangPacks = self.ui.installAllLangPacks.isChecked()
         ctx.mainScreen.processEvents()
 
@@ -318,5 +320,6 @@ Here you can see your install options before installation starts.
         ctx.debugger.log("GRUB will be installed to %s" % ctx.installData.bootLoaderDev)
 
         ctx.mainScreen.moveInc = 1
+        ctx.mainScreen.ui.buttonNext.setText(_("Next"))
         return True
 
