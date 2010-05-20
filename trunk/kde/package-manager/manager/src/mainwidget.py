310b6e5992e401b3d13f4616182f11c066accf08
gokmen.goksel <gokmen.goksel@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-05-20 08:56:14 +0000

    * Fix http error message , thnx to fatih

--- trunk/kde/package-manager/manager/src/mainwidget.py
+++ trunk/kde/package-manager/manager/src/mainwidget.py
@@ -211,7 +211,7 @@ class MainWidget(QtGui.QWidget, Ui_MainWidget):
         elif "Access denied" in message or "tr.org.pardus.comar.Comar.PolicyKit" in message:
             errorTitle = i18n("Authorization Error")
             errorMessage = i18n("You are not authorized for this operation.")
-        elif "HTTP Error 404":
+        elif "HTTP Error 404" in message:
             errorTitle = i18n("Pisi Error")
             errorMessage = i18n("Package not found. It may be upgraded in or removed from the repository. Please try upgrading repository informations.")
         else:
