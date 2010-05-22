18df883fabbb0c383e6f077bb13e67f12eb1834b
sarath.lakshman <sarath.lakshman@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-05-22 07:19:47 +0000

    Yali to work in-windowmode patch, Addition of YALI_LIVEINSTALL install_type and its spec

--- playground/gsoc/yali-live/bindYali.sh
+++ playground/gsoc/yali-live/bindYali.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-cp -rf /usr/lib/python2.6/site-packages/yali4/ /yali
+mkdir /yali
 mount --bind /yali/ /usr/lib/python2.6/site-packages/yali4/
 
 echo "Yali is ready in /yali ..."
+
+echo build and install
+
+python setup.py install
