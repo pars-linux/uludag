6d4773a9e2d5dc66f4d38c6a624b842b75ab82f1
sukhbirsingh <sukhbirsingh@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-06-01 19:14:42 +0000

    Added the testcreate file. In the last commit, it did not work.

---
+++ playground/gsoc/testing-framework/create-test-file.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from lxml import etree
+except ImportError:
+    import sys
+    sys.exit('Error importing the \'lxml\' library.\nYou need the lxml library package installed to run this software.\nExit')
+    
+def print_tag(question):
+    """Print the formatted prompt after accepting the input."""
+    formattedQuestion = question + ':' + '\n'
+    answer = raw_input(formattedQuestion)
+    return answer
+
+def main():
+    """Ask the user for the type of test case and output it accordingly."""
+    while True:
+        print 'What type of a test case do you want?'
+        print '1. Install\t2. GUI\n3. Shell\t4. Automated'
+        
+        try:
+            answer = int(raw_input('Enter a value [1 - 4] >. ')) 
+        except ValueError:
+            print '\nInvalid input. Enter a value between [1 - 4].\n'
+            continue
+        if not answer in range(1, 5):
+            print '\nInvalid input. Enter a value between [1 - 4].\n'
+            continue 
+   
+        testcases = {1 : "install", 2 : "gui", 3 : "shell", 4 : "automated"}
+        return testcases[answer]
+
+if __name__ == '__main__':
+    print '- Pardus GNU/ Linux Testing Framework -'
+    main()
