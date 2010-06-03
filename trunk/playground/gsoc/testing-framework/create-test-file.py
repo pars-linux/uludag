5616aa5a4aad772828c733f5b5c27a0772ed1f78
sukhbirsingh <sukhbirsingh@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-06-03 13:42:47 +0000

    Fixed various coding style issues and tried incorporating the PEP8 guidelines. Added some more code to parser. Will be adding some parsing code in the next commit.

--- trunk/playground/gsoc/testing-framework/create-test-file.py
+++ trunk/playground/gsoc/testing-framework/create-test-file.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env python
+#! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
 try:
     from lxml import etree
 except ImportError:
-    import sys
     sys.exit('Error importing the \'lxml\' library.\nYou need the lxml library package installed to run this software.\nExit')
+    
     
 def print_tag(question):
     """Print the formatted prompt after accepting the input."""
@@ -13,24 +14,25 @@ def print_tag(question):
     answer = raw_input(formattedQuestion)
     return answer
 
+
 def main():
     """Ask the user for the type of test case and output it accordingly."""
+    print '- Pardus GNU/ Linux Testing Framework -'
     while True:
         print 'What type of a test case do you want?'
         print '1. Install\t2. GUI\n3. Shell\t4. Automated'
-        
         try:
-            answer = int(raw_input('Enter a value [1 - 4] >. ')) 
+            answer = int(raw_input('Enter a value [1 - 4] > ')) 
         except ValueError:
-            print '\nInvalid input. Enter a value between [1 - 4].\n'
+            print '\nInvalid input. Enter a value between [1 - 4]\n'
             continue
         if not answer in range(1, 5):
-            print '\nInvalid input. Enter a value between [1 - 4].\n'
+            print '\nInvalid input. Enter a value between [1 - 4]\n'
             continue 
-   
-        testcases = {1 : "install", 2 : "gui", 3 : "shell", 4 : "automated"}
+        # Extend the dictionary below to add more test cases
+        testcases = {1: "install", 2: "gui", 3: "shell", 4: "automated"}
         return testcases[answer]
-
+    
+    
 if __name__ == '__main__':
-    print '- Pardus GNU/ Linux Testing Framework -'
     main()
--- trunk/playground/gsoc/testing-framework/parser.py
+++ trunk/playground/gsoc/testing-framework/parser.py
@@ -3,46 +3,52 @@
 
 import sys
 import os
-
 from optparse import OptionParser
 
 try:
     from lxml import etree
 except ImportError:
     sys.exit("""Error importing the 'lxml' library.\nYou need the 'lxml' library package installed to run this software.""")
+    
+    
+def parser(xmlfile):
+    """Parse the XML file"""
+    print 'Parsing file:\t[{0}]'.format(xmlfile)
+
 
 def check_xml_file(file):
-    """Load a XML file and check for validity of the syntax."""
+    """Load and check for a XML file."""
     fileExtension = os.path.splitext(file)
     fileAbsolutePath = os.path.abspath(file)
-    
+    # First check whether the file is valid and if yes, then check for the XML extension
     if not os.path.isfile(file): 
         sys.exit('The file \'{0}\' is not a valid file or the file does not exist.'.format(file))
     if not '.xml' in fileExtension:
-        sys.exit('Error: Only XML files are supported.\nThe file \'{0}\' has the extension \'{1}\' and \
-is an invalid test case file.'.format(file, fileExtension[1]))
-    
-    # We have a proper XML file now, let us parse it!
+        sys.exit('Error: Only XML files are supported.\nThe file \'{0}\' is an invalid test case file.'.format(file))
+    # We have a proper XML file to parse
     print '- Pardus GNU/ Linux Testing Framework -'
-    print 'Parsing file:\t[{0}]'.format(fileAbsolutePath)
-
+    parser(fileAbsolutePath)
+    
+    
 def main():
     """Handle the command line arguments."""
-    parser = OptionParser(usage = 'usage: %prog [options] arguments')
+    parser = OptionParser(usage='usage: %prog [options] arguments')
     parser.add_option('-f', '--file',
                       dest='filename',
                       metavar='FILE',
-                      help='Specifies the testcase XML file for input.')
+                      help='specifies the testcase XML file for input.')
     (options, args) = parser.parse_args()
-    
+    # If no arguments are passed just print the help message
     if not options.filename:
         parser.print_help()
         sys.exit(1)
+    # This should be changed according to the number of options and arguments
     if len(args) != 0:
         parser.error('Invalid number of arguments.')
         sys.exit(1)
-        
+
     check_xml_file(options.filename)
+    
     
 if __name__ == '__main__':
     main()
