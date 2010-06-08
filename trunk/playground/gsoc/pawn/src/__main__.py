de8c3cffad4811a6142dfa448c304416bf1fc116
ahmetalpbalkan <ahmetalpbalkan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-06-08 20:42:06 +0000

    ftp downloader concurrency fixes

--- trunk/playground/gsoc/pawn/src/__main__.py
+++ trunk/playground/gsoc/pawn/src/__main__.py
@@ -6,27 +6,20 @@ from ftpdownloader import FTPDownloader
 app = QtGui.QApplication(sys.argv)
 from versionmanager import Mirror
 m = Mirror()
-m.hostname='ftp.pardus.org.tr'
+m.hostname='ftp.linux.org.tr'
 m.login='true'
 m.username='anonymous'
 m.password='anonymous'
 m.port='21'
-m.path='/pub'
-m.filename='md5summer.exe' #'Pardus_2009.1_Anthropoides_virgo.iso'
+m.path='/'
+m.filename='ls-lR.gz' #'Pardus_2009.1_Anthropoides_virgo.iso'
 
 widget = QtGui.QWidget()
 widget.resize(500,500)
-d = FTPDownloader(m, 'a.txt')
+d = FTPDownloader(m, m.filename)
 
-pb = QtGui.QPushButton('click', widget)
+pb = QtGui.QPushButton('Download it!', widget)
 widget.connect(pb, QtCore.SIGNAL('clicked()'), d.startTransfer)
-#QtCore.QObject.connect(d, QtCore.SIGNAL('commandStarted(int)'), d.starts)
-QtCore.QObject.connect(d, QtCore.SIGNAL('commandFinished(int,bool)'), d.logCommandFinished)
-QtCore.QObject.connect(d, QtCore.SIGNAL('stateChanged(int)'), d.logChangeState)
-QtCore.QObject.connect(d, QtCore.SIGNAL('done(bool)'), d.processDone)
-#QtCore.QObject.connect(d, QtCore.SIGNAL('rawCommandReply(int, const QString)'), d.cmd) #TODO: Does not work!
-QtCore.QObject.connect(d, QtCore.SIGNAL('dataTransferProgress(qint64,qint64)'), d.traceTransferProgress)
-QtCore.QObject.connect(d, QtCore.SIGNAL('readyRead()'), d.writeBack)
-
+pb.resize(300,300)
 widget.show()
 sys.exit(app.exec_())
--- trunk/playground/gsoc/pawn/src/ftpdownloader.py
+++ trunk/playground/gsoc/pawn/src/ftpdownloader.py
@@ -1,55 +1,78 @@
-import sys, time
+import sys, os, time
 from PyQt4.QtNetwork import QFtp
 from PyQt4 import QtCore, QtGui
 from logger import getLogger
 log = getLogger("FTPDownloader")
 
 class FTPDownloader(QFtp):
-    '''
-    ISO downloader via ftp protocol specialized for
-    Mirror object -- a QFtp instance.
-    '''
+    '''ISO downloader via ftp protocol specialized for
+    Mirror object -- a QFtp instance. Transfers files
+    in Binary and PASV mode.'''
     
     def __init__(self, mirror, destinationFile):
 	QFtp.__init__(self) # TODO: or parent? solve this.
+	print dir(self.TransferType)
+	
 	self.mirror = mirror
 	self.filePath = destinationFile
-	self.speedCalculateInterval = 1.0 #seconds
+	self.speedCalculateInterval = 5.0 #seconds
+	self.downloading = False
+
+	QtCore.QObject.connect(self, QtCore.SIGNAL('commandFinished(int,bool)'), self.logCommandFinished)
+	QtCore.QObject.connect(self, QtCore.SIGNAL('stateChanged(int)'), self.logChangeState)
+	QtCore.QObject.connect(self, QtCore.SIGNAL('done(bool)'), self.processDone)
+	QtCore.QObject.connect(self, QtCore.SIGNAL('dataTransferProgress(qint64,qint64)'), self.traceTransferProgress)
+	QtCore.QObject.connect(self, QtCore.SIGNAL('readyRead()'), self.writeBack)
 
     def startTransfer(self):
-	print 'Start'
-        self.connectToHost(self.mirror.hostname, self.mirror.port)
+	if self.downloading:
+	    return
+
+        self.connectToHost(self.mirror.hostname, long(self.mirror.port))
         if (self.mirror.login=='true'):
             self.login(self.mirror.username, self.mirror.password)
+
         self.cd(self.mirror.path)
 
-	# prepare tests
+	self.initializeDownload()
+	self.initializeFile()
+
+        self.get(self.mirror.filename) #start the transfer
+        self.close()
+
+    def initializeDownload(self):
 	self.downloadSpeed = 0
 	self.averageSpeed = 0
 	self.initialBytes = 0
+	self.totalBytes = 0
 	self.timeCounter = time.time()
 	self.timeStart = time.time()
 	self.timeCompleted = 0
 	self.ETA = 0
 	self.percentageCompleted = 0
 	self.packetCounter = 0
+	self.downloading = False
 
-        self.get(self.mirror.filename) #start the transfer
-        self.close()
+    def initializeFile(self):
+	self.fsock = open(self.filePath, 'wb') # TODO: decide to use buffering
+	# TODO: decide binary mode
+	# TODO: decide to use os.tmpfile, os.tmpname etc. consider vulnerabilitiesfsock
 
     def logCommandFinished(self, id, error):
 	if error:
 	    log.error("FTP Error in command "+str(id)+". "+self.errorString())
 
     def traceTransferProgress(self, transferredSize, totalSize):
+	self.downloading = True
+
 	if self.packetCounter == 0:
-	    self.totalBytes = totalSize
+	    self.totalBytes = totalSize # TODO: Discuss is it faster w/o 'if'?
 
 	if(time.time() - self.timeCounter > self.speedCalculateInterval):
 	    self.downloadSpeed = (transferredSize-self.initialBytes) / (time.time()-self.timeCounter)
 	    self.timeCounter = time.time()
 	    self.initialBytes = transferredSize
-	    print self.downloadSpeed/1024, 'kilobytes/sec', self.percentageCompleted, '%', 'ETA:%d sec (%d min).'% (self.ETA, self.ETA/60)
+	    print self.downloadSpeed/1024, 'kilobytes/sec --', self.percentageCompleted, '%--', 'ETA:%d sec (%d min).'% (self.ETA, self.ETA/60)
 
 	self.percentageCompleted = 100.0*transferredSize/totalSize
 	try:
@@ -57,17 +80,33 @@ class FTPDownloader(QFtp):
 	except ZeroDivisionError:
 	    self.ETA = 0
 
-	self.packetCounter += 1
+	self.packetCounter += 1 # TODO: redundant?
 	
     def processDone(self, error):
-	print 'done'
+	self.downloading = False
 	self.timeCompleted = time.time()-self.timeStart
-	self.averageSpeed = self.totalBytes/self.timeCompleted
+	
+	if self.timeCompleted:
+	    self.averageSpeed = self.totalBytes/self.timeCompleted
+	else:
+	    self.averageSpeed = 0 #ZeroDivisionError
 	
 	if error:
 	    log.error("FTP transfer completed with errors: "+self.errorString())
+	    self.cleanCorruptFile()
 	else:
-	    log.info("FTP transfer completed in %s seconds. (%f kb/s)"  % (self.timeCompleted, self.averageSpeed/1024.0))
+	    log.info("FTP transfer completed in %s seconds. (%f kb/s) %d packets."  % (self.timeCompleted, self.averageSpeed/1024.0, self.packetCounter))
+
+	if not self.fsock.closed:
+	    self.fsock.close()
 
     def logChangeState(self, changed):
 	log.info("FTP state changed to "+str(changed))
+
+    def writeBack(self):
+	socketData = self.readAll()
+	self.fsock.write(socketData)
+
+    def cleanCorruptFile(self):
+	"Removes partially completed local file after a bad transfer."
+	os.remove(self.filePath)
