7fa1385cca4b0a5adcc7ad5c767cfbde24e0e27d
ahmetalpbalkan <ahmetalpbalkan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-06-09 17:15:39 +0000

    unstable gui preparations

--- trunk/playground/gsoc/pawn/src/__main__.py
+++ trunk/playground/gsoc/pawn/src/__main__.py
@@ -1,25 +1,14 @@
-import sys,time
+import sys
 from PyQt4 import QtGui
 from PyQt4 import QtCore
-from ftpdownloader import FTPDownloader
 
-app = QtGui.QApplication(sys.argv)
-from versionmanager import Mirror
-m = Mirror()
-m.hostname='ftp.linux.org.tr'
-m.login='true'
-m.username='anonymous'
-m.password='anonymous'
-m.port='21'
-m.path='/'
-m.filename='ls-lR.gz' #'Pardus_2009.1_Anthropoides_virgo.iso'
+from guiController import PaWnGui
 
-widget = QtGui.QWidget()
-widget.resize(500,500)
-d = FTPDownloader(m, m.filename)
+class PaWn():
+    def __init__(self):
+	self.gui = PaWnGui(self)
 
-pb = QtGui.QPushButton('Download it!', widget)
-widget.connect(pb, QtCore.SIGNAL('clicked()'), d.startTransfer)
-pb.resize(300,300)
-widget.show()
-sys.exit(app.exec_())
+if __name__=='__main__':
+    app = QtGui.QApplication(sys.argv)
+    pawn = PaWn()
+    sys.exit(app.exec_())
