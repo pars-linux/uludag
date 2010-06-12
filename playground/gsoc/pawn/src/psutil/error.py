65eece40d8857532abb24e0b1ea3a1508d6bc84f
ahmetalpbalkan <ahmetalpbalkan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-06-12 18:41:57 +0000

    excluded psutil. instead, using WMI for Windows and /proc/meminfo for Linux.

--- playground/gsoc/pawn/src/psutil/error.py
+++
@@ -1,7 +0,0 @@
-# this exception get overriden by the platform specific modules if necessary
-class NoSuchProcess(Exception):
-    """No process was found for the given parameters."""
-
-class AccessDenied(Exception):
-    """Exception raised when permission to perform an action is denied."""
-
