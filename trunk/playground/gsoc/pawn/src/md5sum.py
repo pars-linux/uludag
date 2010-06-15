06dc4d7fd335bae53767db8c97fbf4c22f07e1a8
ahmetalpbalkan <ahmetalpbalkan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-06-15 18:58:09 +0000

    md5sum utility credits.

--- trunk/playground/gsoc/pawn/src/md5sum.py
+++ trunk/playground/gsoc/pawn/src/md5sum.py
@@ -1,3 +1,7 @@
+#Adapted from ActiveState Code Python Recipe #266486
+#see http://code.activestate.com/recipes/266486-simple-md5-sum-utility/
+#for more. Removed deprecated md5 package and using hashlib instead.
+
 import hashlib
 
 from logger import getLogger
