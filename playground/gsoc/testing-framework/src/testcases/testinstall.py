a9c87f1695c2325d54cffb5a588b1d434569221f
sukhbirsingh <sukhbirsingh@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-06-22 16:37:02 +0000

    Fixed a major bug in the parser which crashed the program.

--- playground/gsoc/testing-framework/src/testcases/testinstall.py
+++ playground/gsoc/testing-framework/src/testcases/testinstall.py
@@ -12,18 +12,18 @@ def test_install(packagelist, installedpackages):
     if not packagestNotInstalled:
         print 'All the required packages are installed.'
         return
-    counter = 0
     print 'Number of packages to be installed: [ {0} ]'.format(totalPackages)
     print 'Installing required packages. Please wait ...'
+    counter = 0 
     while counter < totalPackages:
         package = packagestNotInstalled[counter]
         singlePackage = string.split(packagestNotInstalled[counter])
         try:
             install(singlePackage)
-            print 'Finished installing the required packages.'
-        except PrivilegeError:
+            print '\nFinished installing the required packages.'
+        except PrivilegeError:      # in case the user doesn't have permission
             print '[Error]: The framework is not running with root privileges.'
             print '[Solution]: To install the packages, please run the framework with root privileges.'
-        except:
+        except:     # if the package does not exist
             print "[Failed]: The package '{0}' was not found in the repository.".format(package)
         counter += 1
