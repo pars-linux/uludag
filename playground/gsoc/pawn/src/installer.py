8f234f5e58c60ca63183319b6f45702562719359
ahmetalpbalkan <ahmetalpbalkan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-06-22 20:57:06 +0000

    exception handling on windows registry management.

--- playground/gsoc/pawn/src/installer.py
+++ playground/gsoc/pawn/src/installer.py
@@ -15,53 +15,45 @@ class Installer():
         self.reg = wmi.WMI(namespace="DEFAULT").StdRegProv
         self.hlmPath = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\" + self.mainEngine.appid
 
+
+    def _setRegistryKey(self, hlmPath, keyName, keyValue, isDWORD = False):
+        try:
+            if isDWORD:
+                self.reg.SetDWORDValue(hDefKey = _winreg.HKEY_LOCAL_MACHINE,
+                  sSubKeyName = hlmPath, sValueName = keyName, uValue = keyValue)
+            else:
+                self.reg.SetStringValue(hDefKey = _winreg.HKEY_LOCAL_MACHINE,
+                  sSubKeyName = hlmPath, sValueName = keyName, sValue = keyValue)
+        except:
+            log.exception('Could not set registry key %s.' % keyName)
+
+
     def installationRegistry(self):
         # TODO: InstallLocation, DisplayIcon, Comments
         uninstallString = 'defrag.exe'
 
-        self.reg.CreateKey(
-          hDefKey=_winreg.HKEY_LOCAL_MACHINE,
-          sSubKeyName=self.hlmPath)
+        try:
+            self.reg.CreateKey(hDefKey=_winreg.HKEY_LOCAL_MACHINE,
+              sSubKeyName=self.hlmPath)
+            log.debug(self.hlmPath + ' registry key is created.')
+        except:
+            log.exception(self.hlmPath + ' registry key could not be created.')
 
-        log.debug(self.hlmPath + ' registry key created.')
-
-        self.reg.SetStringValue(
-          hDefKey = _winreg.HKEY_LOCAL_MACHINE,sSubKeyName = self.hlmPath,
-          sValueName = 'DisplayName', sValue = self.mainEngine.application)
-
-        self.reg.SetStringValue(
-          hDefKey = _winreg.HKEY_LOCAL_MACHINE,sSubKeyName = self.hlmPath,
-          sValueName = 'DisplayVersion', sValue = self.mainEngine.version)
-
-        self.reg.SetStringValue(
-          hDefKey = _winreg.HKEY_LOCAL_MACHINE, sSubKeyName = self.hlmPath,
-          sValueName = 'UninstallString', sValue = uninstallString)
-
-        self.reg.SetStringValue(
-          hDefKey = _winreg.HKEY_LOCAL_MACHINE, sSubKeyName = self.hlmPath,
-          sValueName = 'HelpLink', sValue = self.mainEngine.home)
-
-        self.reg.SetStringValue(
-          hDefKey = _winreg.HKEY_LOCAL_MACHINE, sSubKeyName = self.hlmPath,
-          sValueName = 'URLInfoAbout', sValue = self.mainEngine.home)
-
-        self.reg.SetStringValue(
-          hDefKey = _winreg.HKEY_LOCAL_MACHINE, sSubKeyName = self.hlmPath,
-          sValueName = 'Publisher', sValue = self.mainEngine.home)
-
-        self.reg.SetDWORDValue(
-          hDefKey = _winreg.HKEY_LOCAL_MACHINE, sSubKeyName = self.hlmPath,
-          sValueName = 'NoModify', uValue = 1)
-
-        self.reg.SetDWORDValue(
-          hDefKey = _winreg.HKEY_LOCAL_MACHINE, sSubKeyName = self.hlmPath,
-          sValueName = 'NoRepair', uValue = 1)
-
-        log.debug('Registry subkeys have been created.')
+        self._setRegistryKey(self.hlmPath, 'DisplayName', self.mainEngine.application)
+        self._setRegistryKey(self.hlmPath, 'DisplayVersion', self.mainEngine.version)
+        self._setRegistryKey(self.hlmPath, 'UninstallString', uninstallString)
+        self._setRegistryKey(self.hlmPath, 'HelpLink', self.mainEngine.home)
+        self._setRegistryKey(self.hlmPath, 'URLInfoAbout', self.mainEngine.home)
+        self._setRegistryKey(self.hlmPath, 'Publisher', self.mainEngine.home)
+        self._setRegistryKey(self.hlmPath, 'NoModify', 1, True)
+        self._setRegistryKey(self.hlmPath, 'NoRepair', 1, True)
 
     def uninstallationRegistry(self):
-        self.reg.DeleteKey(hDefKey=_winreg.HKEY_LOCAL_MACHINE, sSubKeyName=self.hlmPath)
-        log.debug('Registry subkey has been removed successfully.')
+        try:
+            self.reg.DeleteKey(hDefKey=_winreg.HKEY_LOCAL_MACHINE, sSubKeyName=self.hlmPath)
+            log.debug('Registry subkey has been removed successfully.')
+        except:
+            log.exception('Could not remove registry keys upon uninstallation.')
 
     def ejectCD(self):
         # If CD/DVD is used, eject it after installation.
