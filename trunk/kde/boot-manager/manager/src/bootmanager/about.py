0de33a67f381f7726a7f70703c6b7874c7e661b0
ozan.caglayan <ozan.caglayan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-06-24 13:40:01 +0000

    Move code directory under src for standardization, add ki18nc call for translators

--- trunk/kde/boot-manager/manager/src/bootmanager/about.py
+++ trunk/kde/boot-manager/manager/src/bootmanager/about.py
@@ -12,7 +12,7 @@
 #
 
 # PyKDE
-from PyKDE4.kdecore import KAboutData, ki18n
+from PyKDE4.kdecore import KAboutData, ki18n, ki18nc
 
 # Application Data
 appName     = "boot-manager"
@@ -21,8 +21,8 @@ programName = ki18n("Boot Manager")
 version     = "2.9.11"
 description = ki18n("Boot Manager")
 license     = KAboutData.License_GPL
-copyright   = ki18n("(c) 2006-2009 TUBITAK/UEKAE")
-text        = ki18n(" ")
+copyright   = ki18n("(c) 2006-2010 TUBITAK/UEKAE")
+text        = ki18n(None)
 homePage    = "http://www.pardus.org.tr/eng/projects"
 bugEmail    = "bugs@pardus.org.tr"
 catalog     = appName
@@ -30,6 +30,7 @@ aboutData   = KAboutData(appName, catalog, programName, version, description, li
 
 # Author(s)
 aboutData.addAuthor(ki18n("Bahadır Kandemir"), ki18n("Current Maintainer"))
+aboutData.setTranslator(ki18nc("NAME OF TRANSLATORS", "Your names"), ki18nc("EMAIL OF TRANSLATORS", "Your emails"))
 
 # Use this if icon name is different than appName
 aboutData.setProgramIconName("computer")
