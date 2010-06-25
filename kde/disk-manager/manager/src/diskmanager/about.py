b36bcad41c48d7a6ff824a9845553182f42f8b9a
ozan.caglayan <ozan.caglayan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-06-25 07:03:14 +0000

    Rename code/ to src/ for standard paths Sync setup.py add translator credit strings

--- kde/disk-manager/manager/src/diskmanager/about.py
+++ kde/disk-manager/manager/src/diskmanager/about.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2006-2009 TUBITAK/UEKAE
+# Copyright (C) 2006-2010 TUBITAK/UEKAE
 #
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free
@@ -12,7 +12,7 @@
 #
 
 # PyKDE
-from PyKDE4.kdecore import KAboutData, ki18n
+from PyKDE4.kdecore import KAboutData, ki18n, ki18nc
 
 # Application Data
 appName     = "disk-manager"
@@ -21,8 +21,8 @@ programName = ki18n("Disk Manager")
 version     = "2.9.9"
 description = ki18n("Disk Manager")
 license     = KAboutData.License_GPL
-copyright   = ki18n("(c) 2006-2009 TUBITAK/UEKAE")
-text        = ki18n(" ")
+copyright   = ki18n("(c) 2006-2010 TUBITAK/UEKAE")
+text        = ki18n(None)
 homePage    = "http://www.pardus.org.tr/eng/projects"
 bugEmail    = "bugs@pardus.org.tr"
 catalog     = appName
@@ -30,6 +30,7 @@ aboutData   = KAboutData(appName, catalog, programName, version, description, li
 
 # Author(s)
 aboutData.addAuthor(ki18n("Bahadır Kandemir"), ki18n("Current Maintainer"))
+aboutData.setTranslator(ki18nc("NAME OF TRANSLATORS", "Your names"), ki18nc("EMAIL OF TRANSLATORS", "Your emails"))
 
 # Use this if icon name is different than appName
 aboutData.setProgramIconName("drive-harddisk")
