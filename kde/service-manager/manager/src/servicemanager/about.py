4e0691265c570fea04f3590ba7810b3a1035d6ab
ozan.caglayan <ozan.caglayan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-06-25 07:19:40 +0000

    Rename code/ to src/ Sync setup.py Add translator credits to strings

--- kde/service-manager/manager/src/servicemanager/about.py
+++ kde/service-manager/manager/src/servicemanager/about.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2009, TUBITAK/UEKAE
+# Copyright (C) 2009-2010 TUBITAK/UEKAE
 #
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free
@@ -12,7 +12,7 @@
 #
 
 # PyKDE4 Stuff
-from PyKDE4.kdecore import *
+from PyKDE4.kdecore import KAboutData, ki18n, ki18nc
 
 # Application Data
 appName     = "service-manager"
@@ -21,7 +21,7 @@ programName = ki18n("Service Manager")
 version     = "2.9.5"
 description = ki18n("Service Manager")
 license     = KAboutData.License_GPL
-copyright   = ki18n("(c) 2009 TUBITAK/UEKAE")
+copyright   = ki18n("(c) 2009-2010 TUBITAK/UEKAE")
 text        = ki18n(None)
 homePage    = "http://www.pardus.org.tr/eng/projects"
 bugEmail    = "bugs@pardus.org.tr"
@@ -29,8 +29,9 @@ catalog     = appName
 aboutData   = KAboutData(appName, catalog, programName, version, description, license, copyright, text, homePage, bugEmail)
 
 # Authors
-aboutData.addAuthor (ki18n("Gökmen GÖKSEL"),    ki18n("Current Maintainer"))
-aboutData.addAuthor (ki18n("Bahadır Kandemir"), ki18n("COMAR Author"))
+aboutData.addAuthor(ki18n("Gökmen Göksel"), ki18n("Current Maintainer"))
+aboutData.addAuthor(ki18n("Bahadır Kandemir"), ki18n("COMAR Author"))
+aboutData.setTranslator(ki18nc("NAME OF TRANSLATORS", "Your names"), ki18nc("EMAIL OF TRANSLATORS", "Your emails"))
 
 # Use this if icon name is different than appName
 aboutData.setProgramIconName("flag-yellow")
