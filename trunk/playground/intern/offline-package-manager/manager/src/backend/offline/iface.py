4b5d517e4444f53bb8dd3dc4331c6eb7a5a90ed2
volkan.esgel <volkan.esgel@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-06-30 08:44:25 +0000

    Modify offline iface to call related pisi functions which are modified to work with offlinedb. Remove temporary pisi functions from offline iface, call these functions from pisi.

--- trunk/playground/intern/offline-package-manager/manager/src/backend/offline/iface.py
+++ trunk/playground/intern/offline-package-manager/manager/src/backend/offline/iface.py
@@ -17,19 +17,7 @@ import comar
 import pisi
 
 from pmlogging import logger
-
 from offlineparser import OfflineParser
-
-# for rewrited pisi.api functions
-#--------------------------------
-import pisi.context as ctx
-import pisi.pgraph as pgraph
-import pisi.util as util
-
-import gettext
-__trans = gettext.translation('pisi', fallback=True)
-_ = __trans.ugettext
-#--------------------------------
 
 class Singleton(object):
     def __new__(type):
@@ -161,7 +149,7 @@ class Iface(Singleton):
             return self.odb.list_installed()
 
     def getUpdates(self):
-        lu = set(self.list_upgradable())
+        lu = set(pisi.api.list_upgradable(pisi.db.offlinedb.OfflineDB))
         for replaced in self.replaces.keys():
             lu.remove(replaced)
             lu.add(self.replaces[replaced])
@@ -202,16 +190,16 @@ class Iface(Singleton):
             return self.odb.get_package(name)
 
     def getDepends(self, packages):
-        base = self.get_base_upgrade_order(packages)
+        base = pisi.api.get_base_upgrade_order(packages, pisi.db.offlinedb.OfflineDB)
         if not self.odb.has_package(packages[0]):
             deps = pisi.api.get_install_order(packages)
         else:
-            deps = self.get_upgrade_order(packages)
+            deps = pisi.api.get_upgrade_order(packages, pisi.db.offlinedb.OfflineDB)
         self.depends_list = list(set(deps) - set(packages))
         return list(set(deps) - set(packages))
 
     def getRequires(self, packages):
-        revDeps = set(self.get_remove_order(packages))
+        revDeps = set(pisi.api.get_remove_order(packages, pisi.db.offlinedb.OfflineDB))
         requires_list = list(set(revDeps) - set(packages))
         self.requires_list = list(set(revDeps) - set(packages))
         return list(set(revDeps) - set(packages))
@@ -281,192 +269,3 @@ class Iface(Singleton):
         index.packages = packages
 
         index.write(filename, sha1sum=False, compress=pisi.file.File.bz2, sign=None)
-        
-
-# --------------------------------------------------------------
-# Rewrited pisi.api functions are below with their requirements.
-# --------------------------------------------------------------
-
-    def list_upgradable(self):
-        """
-        Return a list of packages that are upgraded in the repository -> list_of_strings
-        """
-
-        #self.odb = pisi.db.offline_idb.Offline_InstallDB()  # already defined above InitDB()
-        is_upgradable = lambda pkg: self.is_upgradable(pkg, ctx.get_option('ignore_build_no'))
-
-        upgradable = filter(self.is_upgradable, self.odb.list_installed())
-        # replaced packages can not pass is_upgradable test, so we add them manually.list_installed())
-        upgradable.extend(pisi.api.list_replaces())
-
-        # consider also blacklist filtering
-        upgradable = pisi.blacklist.exclude_from(upgradable, ctx.const.blacklist)
-
-        return upgradable
-
-
-    def is_upgradable(self, name, ignore_build = False):
-
-        if not self.odb.has_package(name):
-            return False
-
-        (i_version, i_release, i_build, i_distro, i_distro_release) = self.odb.get_version_and_distro_release(name)
-
-        try:
-            version, release, build, distro, distro_release = self.pdb.get_version_and_distro_release(name, self.pdb.which_repo(name))
-        except KeyboardInterrupt:
-            raise
-        except Exception: #FIXME: what exception could we catch here, replace with that.
-            return False
-
-        if distro == i_distro and pisi.version.Version(distro_release) > pisi.version.Version(i_distro_release):
-            return True
-        elif ignore_build or (not i_build) or (not build):
-            return pisi.version.Version(i_release) < pisi.version.Version(release)
-        else:
-            return i_build < build
-
-
-    def get_base_upgrade_order(self, packages):
-        """
-        Return a list of packages of the system.base component that needs to be upgraded
-        or installed in install order -> list_of_strings
-        All the packages of the system.base component must be installed on the system
-        @param packages: list of package names -> list_of_strings
-        """
-        upgrade_order = self.upgrade_base
-        order = upgrade_order(packages)
-        return list(order)
-
-    def upgrade_base(self, A = set()):
-        ignore_build = ctx.get_option('ignore_build_no')
-        if not ctx.config.values.general.ignore_safety and not ctx.get_option('ignore_safety'):
-            if self.cdb.has_component('system.base'):
-                systembase = set(self.cdb.get_union_component('system.base').packages)
-                extra_installs = filter(lambda x: not self.odb.has_package(x), systembase - set(A))
-                if extra_installs:
-                    ctx.ui.warning(_('Safety switch: Following packages in system.base will be installed: ') +
-                            util.strlist(extra_installs))
-                G_f, install_order = pisi.operations.install.plan_install_pkg_names(extra_installs)
-                extra_upgrades = filter(lambda x: self.is_upgradable(x, ignore_build), systembase - set(install_order))
-                upgrade_order = []
-                if extra_upgrades:
-                    ctx.ui.warning(_('Safety switch: Following packages in system.base will be upgraded: ') +
-                            util.strlist(extra_upgrades))
-                    G_f, upgrade_order = self.plan_upgrade(extra_upgrades)
-                # return packages that must be added to any installation
-                return set(install_order + upgrade_order)
-            else:
-                ctx.ui.warning(_('Safety switch: the component system.base cannot be found'))
-        return set()
-
-
-    def get_upgrade_order(self, packages):
-        """
-        Return a list of packages in the upgrade order with extra needed
-        dependencies -> list_of_strings
-        @param packages: list of package names -> list_of_strings
-        """
-        upgrade_order = self.plan_upgrade
-        i_graph, order = upgrade_order(packages)
-        return order
-
-    def plan_upgrade(self, A):
-        # try to construct a pisi graph of packages to
-        # install / reinstall
-
-        G_f = pgraph.PGraph(self.pdb)               # construct G_f
-
-        replaces = self.pdb.get_replaces()
-        # Force upgrading of installed but replaced packages or else they will be removed (they are obsoleted also).
-        # This is not wanted for a replaced driver package (eg. nvidia-X).
-        A = set(A) | set(replaces.values())
-
-        # find the "install closure" graph of G_f by package
-        # set A using packagedb
-        for x in A:
-            G_f.add_package(x)
-        B = A
-
-        while len(B) > 0:
-            Bp = set()
-            for x in B:
-                pkg = self.pdb.get_package(x)
-                for dep in pkg.runtimeDependencies():
-                    # add packages that can be upgraded,
-                    if self.odb.has_package(dep.package) and dep.satisfied_by_installed():
-                        continue
-
-                    if dep.satisfied_by_repo():
-                        if not dep.package in G_f.vertices():
-                            Bp.add(str(dep.package))
-                        G_f.add_dep(x, dep)
-                    else:
-                        ctx.ui.error(_('Dependency %s of %s cannot be satisfied') % (dep, x))
-                        raise Exception(_("Upgrade is not possible."))
-
-            B = Bp
-            # now, search reverse dependencies to see if anything
-            # should be upgraded
-            B = A
-            while len(B) > 0:
-                Bp = set()
-                for x in B:
-                    pkg = self.pdb.get_package(x)
-                    rev_deps = self.pdb.get_rev_deps(x)
-                    for (rev_dep, depinfo) in rev_deps:
-                        # add only installed but unsatisfied reverse dependencies
-                        if (self.odb.has_package(rev_dep) and
-                                not depinfo.satisfied_by_installed() and self.is_upgradable(rev_dep)):
-                            if not depinfo.satisfied_by_repo():
-                                raise Exception(_('Reverse dependency %s of %s cannot be satisfied') % (rev_dep, x))
-                            if not rev_dep in G_f.vertices():
-                                Bp.add(rev_dep)
-                                G_f.add_plain_dep(rev_dep, x)
-                B = Bp
-
-        if ctx.config.get_option('debug'):
-            G_f.write_graphviz(sys.stdout)
-        order = G_f.topological_sort()
-        order.reverse()
-        return G_f, order
-
-
-    def get_remove_order(self, packages):
-        """
-        Return a list of packages in the remove order -> list_of_strings
-        @param packages: list of package names -> list_of_strings
-        """
-        remove_order = self.plan_remove
-        i_graph, order = remove_order(packages)
-        return order
-
-    def plan_remove(self, A):
-        # try to construct a pisi graph of packages to
-        # install / reinstall
-
-        G_f = pgraph.PGraph(self.odb)               # construct G_f
-
-        # find the (install closure) graph of G_f by package
-        # set A using packagedb
-        for x in A:
-            G_f.add_package(x)
-        B = A
-        while len(B) > 0:
-            Bp = set()
-            for x in B:
-                rev_deps = self.odb.get_rev_deps(x)
-                for (rev_dep, depinfo) in rev_deps:
-                    # we don't deal with uninstalled rev deps
-                    # and unsatisfied dependencies (this is important, too)
-                    if self.odb.has_package(rev_dep) and depinfo.satisfied_by_installed():
-                        if not rev_dep in G_f.vertices():
-                            Bp.add(rev_dep)
-                            G_f.add_plain_dep(rev_dep, x)
-            B = Bp
-        if ctx.config.get_option('debug'):
-            G_f.write_graphviz(sys.stdout)
-        order = G_f.topological_sort()
-        return G_f, order
-
-# ----------------------------------------------------------------------------------
