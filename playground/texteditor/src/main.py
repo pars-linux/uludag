30b92b3d2803c94869b60f8a84cf1cf9fe1fa6dd
taha.dogan.gunes <taha.dogan.gunes@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-06-30 19:30:30 +0000

    Saves unsaved changes, new file is working etc.

--- playground/texteditor/src/main.py
+++ playground/texteditor/src/main.py
@@ -20,21 +20,69 @@
 from PyQt4.QtCore import (QFile, QTextStream, SIGNAL, 
                            QTranslator,  QLocale, QIODevice,
                            QFileInfo)
+                           
 from PyQt4.QtGui import ( QApplication, QFileDialog,
                          QMessageBox, QMainWindow)
 import sys
+
 from main_ui import Ui_MainWindow
 
 class MainWindow(QMainWindow, Ui_MainWindow):
-    """Class for (MainWindow + Ui_MainWindow)"""
+    """
+    Class for (MainWindow + Ui_MainWindow)
+    """
     def __init__(self):
-        """SIGNALS and setupUi"""
+        """
+        SIGNALS and setupUi
+        """
         QMainWindow.__init__(self)
         self.setupUi(self)
-        self.connect(self.actionOpen, SIGNAL("triggered()"), self.load_file)
+        self.connect(self.actionExit,
+                     SIGNAL("triggered()"), self.close)
+        self.connect(self.actionOpen, 
+                     SIGNAL("triggered()"), self.load_file)
+        self.connect(self.actionSave, 
+                     SIGNAL("triggered()"), self.save_file)
+        self.connect(self.actionNew, 
+                     SIGNAL("triggered()"), self.new_file)
+        self.connect(self.textEdit.document(), 
+                     SIGNAL("modificationChanged(bool)"), self.modified)
+
+       
         self.path = ""
+        
+        
+    def closeEvent(self, event):
+        """
+        When user tries to quit the application.
+        """
+        if self.ready_to_go() is False:
+            event.ignore()
+            
+            
+    def ready_to_go(self):
+        """
+        To be check unsaved changes and asking to the user.
+        """
+        if self.textEdit.document().isModified():
+            question = QMessageBox.question(self,
+                                            self.tr("TextEditor - Unsaved Changes"),
+                                            self.tr("Do you want to save changes ?"),
+                                            QMessageBox.Yes|QMessageBox.No|
+                                            QMessageBox.Cancel)
+            if question == QMessageBox.Cancel:
+                return False
+            elif question == QMessageBox.Yes:
+                self.save_file()
+                return True
+            else:
+                return True
+        
+        
     def load_file(self):
-        """Loading a text file to the self.textEdit"""
+        """
+        Loading a text file to the self.textEdit
+        """
         fileobject = None
         self.path = QFileDialog.getOpenFileName(self, "TextEditor","")
         try:
@@ -48,16 +96,21 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                                 QFileInfo(self.path).fileName())
         except(IOError, OSError), error:
             QMessageBox.warning(self, 
-                               self.tr("TextEditor - Load Error",
-                                       "Unable to load %s:%s" % 
-                                       (self.filename, error)))
+                               self.tr("TextEditor - Load Error"),
+                               self.tr("Unable to load {0}:{1}".format(self.path,
+                               error)))                                                                                
         finally:
             if fileobject is not None:
                 fileobject.close()
+                
+                
     def save_file(self):
-        """Saving file to the path where getting from QFileDialog"""
+        """
+        Saving file to the path where getting from QFileDialog
+        """
         fileobject = None
-        self.path = QFileDialog.getSaveFileName(self,
+        if self.path is "":
+            self.path = QFileDialog.getSaveFileName(self,
                                                 "TextEditor", 
                                                 self.tr("unnamed"))
         try:
@@ -67,14 +120,37 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             textstream = QTextStream(fileobject)
             textstream.setCodec("UTF-8")
             textstream << self.textEdit.toPlainText()
+            self.setWindowTitle("%s - TextEditor" % 
+                                QFileInfo(self.path).fileName())
         except (IOError, OSError), error:
             QMessageBox.warning(self, 
                                 self.tr("TextEditor - Save Error"), 
-                                self.tr("Unable to save %s: %s" % 
-                                self.path, error))
+                                self.tr("Unable to save {0}:{1}".format( 
+                                self.path, error)))
         finally:
             if fileobject is not None:
                 fileobject.close()
+                
+                
+    def new_file(self):
+        """
+        To reset ui
+        """
+        document = self.textEdit.document()
+        document.clear()
+        document.setModified(False)
+        self.path = ""
+        self.setWindowTitle(self.tr("unnamed - TextEditor"))
+    def modified(self):
+        document = self.textEdit.document()
+        document.setModified(True)
+        if self.path is "":
+            self.setWindowTitle(self.tr("unnamed* - TextEditor"))
+        else:
+            self.setWindowTitle("%s* - TextEditor" % 
+                                QFileInfo(self.path).fileName())
+                               
+                               
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     locale = QLocale.system().name()
