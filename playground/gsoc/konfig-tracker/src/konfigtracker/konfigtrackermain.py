f29a8158e31f836eea57fda68cccabe1c1937d16
jain.basil.aliyas <jain.basil.aliyas@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-07-03 20:15:16 +0000

    Backend Completed

--- playground/gsoc/konfig-tracker/src/konfigtracker/konfigtrackermain.py
+++ playground/gsoc/konfig-tracker/src/konfigtracker/konfigtrackermain.py
@@ -26,6 +26,7 @@ class KonfigTracker(KMainWindow):
         	self.InitApplication()
 		self.monitor = Monitor(app)
 		self.monitor.start()
+		#this function is to be removed, once the gui is developed.
 
 	def InitApplication(self):
         	""" If there exist no database in this path, this function will create
@@ -37,3 +38,21 @@ class KonfigTracker(KMainWindow):
 			os.mkdir(path)
 			CreateDB(path)
 			Backup()
+			
+	def showMenu(self):
+		while True:
+			print "This is a retro style menu, just to confirm that backend works properly"
+			print 80*"*"
+			
+			print "1. Backups <to be shown in gui>"
+			print "2. Restore to selected backup"
+			print "3. Restore"
+			print "4. Export current backup"
+			print "Choice : "
+			ch = raw_input()
+			if ch == 1:
+				showCommit()
+			if ch == 2:
+				pass
+			if ch == 3:
+				pass	
