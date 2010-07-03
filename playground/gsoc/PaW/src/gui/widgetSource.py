25ad47b7f45403fef5e1cfd5ea140fe3f6142678
ahmetalpbalkan <ahmetalpbalkan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-07-03 13:02:06 +0000

    live cd gui modifications. note:pyuic4 on win.

--- playground/gsoc/PaW/src/gui/widgetSource.py
+++ playground/gsoc/PaW/src/gui/widgetSource.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file './widgetSource.ui'
+# Form implementation generated from reading ui file 'widgetSource.ui'
 #
-# Created: Mon Jun 14 20:48:52 2010
+# Created: Sat Jul 03 16:01:53 2010
 #      by: PyQt4 UI code generator 4.7.3
 #
 # WARNING! All changes made in this file will be lost!
@@ -51,6 +51,6 @@ class Ui_widgetSource(object):
     def retranslateUi(self, widgetSource):
         self.label.setText(QtGui.QApplication.translate("widgetSource", "Label for installation source about PaWn", None, QtGui.QApplication.UnicodeUTF8))
         self.optInternet.setText(QtGui.QApplication.translate("widgetSource", "Download Pardus from Internet (recommended)", None, QtGui.QApplication.UnicodeUTF8))
-        self.optCD.setText(QtGui.QApplication.translate("widgetSource", "I have Pardus installation CD", None, QtGui.QApplication.UnicodeUTF8))
+        self.optCD.setText(QtGui.QApplication.translate("widgetSource", "I have Pardus Live CD", None, QtGui.QApplication.UnicodeUTF8))
         self.optISO.setText(QtGui.QApplication.translate("widgetSource", "I have Pardus installation file (.ISO)", None, QtGui.QApplication.UnicodeUTF8))
 
