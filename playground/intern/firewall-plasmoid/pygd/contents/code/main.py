f4e49de2d3caa76141fedf8a12f7c097c057e18e
baris.akkurt <baris.akkurt@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-07-06 07:22:24 +0000

    some repairnig

--- playground/intern/firewall-plasmoid/pygd/contents/code/main.py
+++ playground/intern/firewall-plasmoid/pygd/contents/code/main.py
@@ -11,6 +11,12 @@ from PyKDE4 import plasmascript
 import comar
 import dbus
 from PyQt4.QtCore import *
+
+
+if not dbus.get_default_main_loop():
+    from dbus.mainloop.qt import DBusQtMainLoop
+    DBusQtMainLoop(set_as_default=True)
+
  
 class HelloWorldApplet(plasmascript.Applet):
     def __init__(self,parent,args=None):
@@ -18,36 +24,41 @@ class HelloWorldApplet(plasmascript.Applet):
         
     def baslat(self):
 	link=comar.Link()#comar baglantisi
-	link.System.Service[dbus.String("iptables")].start()#onemli satirlar bunlar
+	#link.System.Service[dbus.String("iptables")].start()#onemli satirlar bunlar
+	link.Network.Firewall[dbus.String("iptables")].setState("on")
+	#link.System.Service[dbus.String("iptables")].start(async=self.handler)
 	self.bilgi_label.setText("<p style='color:green'>Firewall is working now.</p>")
 
     def durdur(self):
 	link=comar.Link()
-	link.System.Service[dbus.String("iptables")].stop()
+	#link.System.Service[dbus.String("iptables")].stop()
+	link.Network.Firewall[dbus.String("iptables")].setState("off")
+	#link.System.Service[dbus.String("iptables")].start(async=self.handler)
 	self.bilgi_label.setText("<p style='color:red'>Firewall is stopped now.</p>")
+	
  
     def init(self):
 	link=comar.Link()
 	#gs=link.Network.Firewall[dbus.String("iptables")].getState()
 	#print gs
 	self.typeOf, self.description, self.stateOfFirewall=link.System.Service[dbus.String("iptables")].info()
-	#print deneme1
-	#print deneme2
-	print self.stateOfFirewall
-	print type(self.stateOfFirewall)
+	#print self.stateOfFirewall
+	#print type(self.stateOfFirewall)
         self.setHasConfigurationInterface(True)#burası true olacak sanırım ayar penceresi için
         self.setAspectRatioMode(Plasma.Square)
  
         self.theme = Plasma.Svg(self)
         self.theme.setImagePath("widgets/background")
         self.setBackgroundHints(Plasma.Applet.DefaultBackground)
- 
+
         self.layout = QGraphicsLinearLayout(Qt.Horizontal, self.applet)
         label = Plasma.Label(self.applet)
         label.setText("<h1>Firewall Plasmoid</h1>")
         
         self.bilgi_label=Plasma.Label(self.applet)
-        if self.stateOfFirewall==dbus.String("on"):
+        #self.bilgi_label.setText("")
+        #link.Network.Firewall[dbus.String("iptables")].getState()
+        if link.Network.Firewall[dbus.String("iptables")].getState()==dbus.String(u"on"):      #self.stateOfFirewall==dbus.String("on"):
 	    self.bilgi_label.setText("<p style='color:green'>Firewall is working now.</p>")
 	else:
 	    self.bilgi_label.setText("<p style='color:red'>Firewall is stopped now.</p>")
@@ -64,6 +75,16 @@ class HelloWorldApplet(plasmascript.Applet):
         
         QObject.connect(baslat_pb, SIGNAL("clicked()"), self.baslat)
         QObject.connect(durdur_pb, SIGNAL("clicked()"), self.durdur)
+        
+        link.listenSignals("Network.Firewall", self.handler)
+
+    def handler(self, *args):
+	link=comar.Link()
+	if link.Network.Firewall[dbus.String("iptables")].getState()==dbus.String(u"on"):      #self.stateOfFirewall==dbus.String("on"):
+	    self.bilgi_label.setText("<p style='color:green'>Firewall is working now.</p>")
+	else:
+	    self.bilgi_label.setText("<p style='color:red'>Firewall is stopped now.</p>")
+	#pass
       
  
 def CreateApplet(parent):
