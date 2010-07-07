5b998e6423104395ac097b13eea91f40566682f6
mehmet.burak.akturk <mehmet.burak.akturk@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-07-07 13:28:07 +0000

    pisi history functions were added. now rescue mode program can get pisi history from selected pardus system and can take back system to wanted date which is selected from pisi history list screen

--- trunk/playground/intern/rescue_mode/dbusTools.py
+++ trunk/playground/intern/rescue_mode/dbusTools.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import os, dbus, comar, subprocess
+import os, dbus, comar, subprocess, pisi
 from shellTools import chrootRun
 
 
@@ -9,7 +9,7 @@ _sys_dirs = ["dev","proc","sys"]
 
 class pardusDbus:
   def __init__(self,path):
-      
+    self.path = path 
     for _dir in _sys_dirs:
         tgt = os.path.join(path, _dir)
         os.system("mount --bind /%s %s" % (_dir, tgt))
@@ -18,11 +18,26 @@ class pardusDbus:
     chrootRun(path,"/sbin/update-environment")
     chrootRun(path,"/bin/service dbus start")
     
-    self.sokcet_file = os.path.join(path,"var/run/dbus/system_bus_socket")
+    self.socket_file = os.path.join(path,"var/run/dbus/system_bus_socket")
      
-    dbus.bus.BusConnection(address_or_type="unix:path=%s" %self.sokcet_file)
-    link = comar.Link(socket=self.sokcet_file)
+    dbus.bus.BusConnection(address_or_type="unix:path=%s" %self.socket_file)
+    link = comar.Link(socket=self.socket_file)
     self.baselayout = link.User.Manager["baselayout"]
+    
+    options = pisi.config.Options()
+    options.yes_all = True
+    options.ignore_dependency = True
+    options.ignore_safety = True
+    options.destdir=path
+    
+ #   dbus.SystemBus()
+    
+    pisi.api.set_dbus_sockname(self.socket_file)
+    pisi.api.set_dbus_timeout(1200)
+    pisi.api.set_options(options)
+    pisi.api.set_comar(True)
+    pisi.api.set_signal_handling(False)
+    
   def getUserlist(self):
  
     users = self.baselayout.userList()
@@ -32,9 +47,39 @@ class pardusDbus:
     info = self.baselayout.userInfo(uid)
     return self.baselayout.setUser(uid, info[1], info[3], info[4], password, info[5])
 
+  def getHistory(limit=50):
+    pdb = pisi.db.historydb.HistoryDB()
+    result = []
+    i=0
+    for op in pdb.get_last():
+        # Dont add repo updates to history list
+        if not op.type == 'repoupdate':
+            result.append(op)
+            i+=1
+            if i==limit:
+                break
+    return result
+  
+  def takeBack(self,operation):
+    # dirty hack for COMAR to find scripts.
+    os.symlink("/",self.path + self.path)
+    pisi.api.takeback(operation)
+    os.unlink(self.path +self.path)
+
+    
+  
+
+
 def main():
   a = pardusDbus("/mnt/rescue_disk/PARDUS_ROOT")
-  print a.getUserlist()
+  for b in a.getHistory():
+    print b.no
+  
+  print ""
+ # a = pardusDbus("/mnt/rescue_disk/PARDUS_ROOT1")
+  #for b in a.getHistory():
+   # print b.no
+  
 
 if __name__ == "__main__":
   main()
