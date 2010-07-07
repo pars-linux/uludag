ddc4a1595b1d4db03e44369192416706c892ebc2
jain.basil.aliyas <jain.basil.aliyas@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-07-07 23:12:42 +0000

    Basic GUI is shown now, yet to connect the signals and slots

--- trunk/playground/gsoc/konfig-tracker/src/konfigtracker.py
+++ trunk/playground/gsoc/konfig-tracker/src/konfigtracker.py
@@ -7,7 +7,7 @@ import sys
 from PyQt4.QtCore import SIGNAL
 
 #PyKDE4 stuffs
-from PyKDE4.kdeui import KApplication
+from PyKDE4.kdeui import KUniqueApplication
 from PyKDE4.kdecore import KCmdLineArgs
 
 if __name__ == "__main__":
@@ -19,12 +19,10 @@ if __name__ == "__main__":
     KCmdLineArgs.init(sys.argv,aboutData)
     
     #creating a kapplication instance
-    app = KApplication()
+    app = KUniqueApplication()
     MainWindow = KonfigTracker(app)
     MainWindow.show()
     app.connect(app, SIGNAL('lastWindowClosed()'), app.quit)
     
     # Run the application
-    app.exec_()
-    
-    
+    app.exec_()  
