b2724f19ee81006a894ca8428d15fd8e151f740a
ozan.caglayan <ozan.caglayan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-07-09 12:56:19 +0000

    Fix build

--- playground/intern/engelsiz/speech-dispatcher/actions.py
+++ playground/intern/engelsiz/speech-dispatcher/actions.py
@@ -10,14 +10,20 @@ from pisi.actionsapi import pisitools
 
 
 def setup():
+    #autotools.autoreconf("-fi")
     autotools.configure("--disable-static \
                          --without-ibmtts \
                          --without-ivona \
                          --without-nas \
                          --with-alsa \
+                         --with-flite \
                          --with-espeak \
                          --with-libao \
                          --with-pulse")
+
+    #pisitools.dosed('libtool', '^hardcode_libdir_flag_spec=.*', 'hardcode_libdir_flag_spec=""')
+    #pisitools.dosed('libtool', '^runpath_var=LD_RUN_PATH', 'runpath_var=DIE_RPATH_DIE')
+
 
 def build():
     autotools.make()
@@ -26,6 +32,10 @@ def install():
     autotools.install()
 
     pisitools.remove("/usr/share/info/ssip.info")
+
+    # Rename generically named binaries
+    pisitools.rename("/usr/bin/long_message", "spd_long_message")
+    pisitools.rename("/usr/bin/run_test", "spd_run_test")
 
     pisitools.dodoc("AUTHORS", "COPYING", "README")
 
