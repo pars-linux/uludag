4d19c72887ead4813ca959e50c430a9e97038efd
ahmetalpbalkan <ahmetalpbalkan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-07-10 19:59:17 +0000

    clean duplicate functions on usb-cd step controllers.

--- playground/gsoc/PaW/src/ui/controller/stepOptUSB.py
+++ playground/gsoc/PaW/src/ui/controller/stepOptUSB.py
@@ -1,11 +1,10 @@
-import os
 import sys
-import ConfigParser
-from utils import levenshtein
 
 from PyQt4 import QtGui, QtCore
 from ui.controller.stepTemplate import StepWidget
 from ui.gui.usb import Ui_usb
+from utils import locate_file_in_path
+from utils import version_name_from_gfxboot
 
 from logger import getLogger
 log = getLogger('USB Option Step')
@@ -13,6 +12,7 @@ log = getLogger('USB Option Step')
 class Widget(QtGui.QWidget, StepWidget):
     heading = "Install from USB Memory Stick"
     usb = None
+    gfxboot_cfg_file = "gfxboot.cfg"
 
     def __init__(self, mainEngine):
 	QtGui.QWidget.__init__(self,None)
@@ -27,12 +27,12 @@ class Widget(QtGui.QWidget, StepWidget):
 
     def onDriveUpdated(self):
         pass
-#        version = self.determineUSBVersion()
-#        if version:
-#            usbDrive = self.getSelectedUSBDrive()
-#            self.gui.lblPath.setText("%s CD detected in %s successfully." % (version.name, usbDrive.DeviceID))
-#        else:
-#            self.gui.lblPath.setText("Unrecognized Pardus CD in drive %s." % self.usbPath)
+        version = self.determineUSBVersion()
+        if version:
+            usbDrive = self.getSelectedUSBDrive()
+            self.gui.lblPath.setText("%s files detected in %s successfully." % (version.name, usbDrive.DeviceID))
+        else:
+            self.gui.lblPath.setText("Unrecognized Pardus CD in USB drive %s." % self.usbPath)
 
     def populateUSBs(self):
 	self.gui.comboDrive.clear()
@@ -54,54 +54,28 @@ class Widget(QtGui.QWidget, StepWidget):
 		return usb
 	return None
 
-    def locate_gfxboot_cfg(self, path):
-            """
-            Locates first occurrence of gfxboot.cfg in the path.
-                'path' should be an absolute path.
-            """
-            filename = 'gfxboot.cfg'
-            contents = os.listdir(path)
-            try: index = contents.index(filename)
-            except ValueError: index = -1 # indicates does not exist
-
-            if not index == -1 and os.path.isfile(os.path.join(path,filename)):
-                return os.path.join(path, filename)
-            else:
-                for item in contents:
-                    if os.path.isdir(os.path.join(path, item)): # nested dirs
-                        result = self.locate_gfxboot_cfg(os.path.join(path,item))
-                        if result: return result
-            return None
-
     def determineUSBVersion(self, tolerance = 10):
         """
         Determines Pardus release version by parsing gfxboot.cfg and
         obtaining distro name then comparing it with names defined in
         versions.xml file using Levenshtein distance of 'tolerance' value.
-        Newer version with appropriate distane will be matched.
         """
         # TODO: tolerance TBD.
         currentDrive = self.getSelectedUSBDrive()
 
-        gfxboot_cfg = self.locate_gfxboot_cfg('%s\\' % currentDrive.DeviceID)
+        gfxboot_cfg = locate_file_in_path('%s\\' % currentDrive.DeviceID, self.gfxboot_cfg_file)
         if not gfxboot_cfg:
             log.debug('Could not locate gfxboot.cfg')
             return None
 
-        config_parser = ConfigParser.ConfigParser()
-        config_parser.read(gfxboot_cfg)
-        distro_name = config_parser.get('base','distro')
+        distro_name = version_name_from_gfxboot(gfxboot_cfg)
 
         if not distro_name:
             log.debug('No distro specified in gfxboot.cfg')
             return None
 
-        result = None
-        for version in self.mainEngine.versionManager.versions:
-            l_distance = levenshtein(version.name, distro_name)
-            if l_distance < tolerance:
-                result = version
-
+        result = self.mainEngine.versionManager.getByDistance(distro_name, tolerance)
+        
         if result:
             log.debug('Detected version: %s' % result.name)
             return result
@@ -122,7 +96,7 @@ class Widget(QtGui.QWidget, StepWidget):
                 self.mainEngine.version = version
             else:
                 reply = QtGui.QMessageBox.warning(self, 'Unknown Pardus Live CD contents in USB.', 'Unable to identify Pardus release of CD/DVD in %s. It is NOT recommended to continue installation. Do you want to exit?' % currentDrive.DeviceID, QtGui.QMessageBox.Yes, QtGui.QMessageBox.No)
-                if reply == QtGui.QMessageBox.Yes: sys.exit()
+                if reply == QtGui.QMessageBox.Yes: sys.exit() # TODO: redirect to a exit event function
 
 	    return True
 
