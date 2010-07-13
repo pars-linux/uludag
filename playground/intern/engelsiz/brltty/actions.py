9a20f911f0e22cfaf3b41e4344d3babce057c086
eda.okur <eda.okur@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-07-13 09:05:15 +0000

    some changes/trials but still got errors...

--- playground/intern/engelsiz/brltty/actions.py
+++ playground/intern/engelsiz/brltty/actions.py
@@ -7,23 +7,28 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
 
 def setup():
-    autotools.configure("--disable-static \
-                         --disable-stripping \
+    autotools.configure("--disable-stripping \
+                         --disable-java-bindings \
+                         --disable-lisp-bindings \
+                         --disable-tcl-bindings \
                          --without-curses \
                          --with-espeak \
                          --with-speechd")
 
 def build():
-    autotools.make()
+    autotools.make("-j1")
 
 def install():
-    autotools.install()
+    #autotools.install()
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR()) 
+    #autotools.rawInstall("DESTDIR=\"%s\" BINDIR=%s" % (get.installDIR(), get.sbinDIR())) 
 
-    pisitools.dodir("/etc")
-    pisitools.dodir("/etc/brltty")
+    #pisitools.dodir("/etc")
+    #pisitools.dodir("/etc/brltty")
     
     pisitools.dodoc("README")
 
