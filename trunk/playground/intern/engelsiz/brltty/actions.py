a7fadd47891ea0bb46d0346d4a66d5ffec70914f
eda.okur <eda.okur@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-07-13 07:33:41 +0000

    brltty package primitive  trials

---
+++ trunk/playground/intern/engelsiz/brltty/actions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+
+def setup():
+    autotools.configure("--disable-static \
+                         --disable-stripping \
+                         --without-curses \
+                         --with-espeak \
+                         --with-speechd")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodir("/etc")
+    pisitools.dodir("/etc/brltty")
+    
+    pisitools.dodoc("README")
+
