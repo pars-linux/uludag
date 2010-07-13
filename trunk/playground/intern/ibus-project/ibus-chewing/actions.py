2d0522162495fc3c20583e9f92036915f50e004a
caner.basaran <caner.basaran@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-07-13 11:07:56 +0000

    cmake configure error

--- trunk/playground/intern/ibus-project/ibus-chewing/actions.py
+++ trunk/playground/intern/ibus-project/ibus-chewing/actions.py
@@ -5,6 +5,7 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import cmaketools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
@@ -12,10 +13,10 @@ from pisi.actionsapi import get
 WorkDir = "%s-%s-Source" % (get.srcNAME(), get.srcVERSION())
 
 def setup():
-    autotools.configure()
+    cmaketools.configure("-DCMAKE_BUILD_TYPE=RelWithDebInfo -DKDEDIR=%s" % get.kdeDIR())
 
 def build():
-    autotools.make()
+    cmaketools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s libexecdir=/%s" %(get.installDIR(), get.libexecDIR()))
