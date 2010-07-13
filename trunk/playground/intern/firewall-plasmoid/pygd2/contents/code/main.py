69fb52f7f33466128b9f34a049b2032fba229150
baris.akkurt <baris.akkurt@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-07-13 07:28:16 +0000

    accurate gui, some bug fixes

--- trunk/playground/intern/firewall-plasmoid/pygd2/contents/code/main.py
+++ trunk/playground/intern/firewall-plasmoid/pygd2/contents/code/main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from PyQt4.QtCore import *
-from PyQt4.QtGui import QCheckBox, QPixmap, QGraphicsLinearLayout, QSizePolicy
+from PyQt4.QtGui import QCheckBox, QPixmap, QSizePolicy, QGraphicsGridLayout
 from PyKDE4.plasma import Plasma
 from PyKDE4 import plasmascript
 import comar, dbus
@@ -23,42 +23,41 @@ class FirewallApplet(plasmascript.Applet):
 		self.link.Network.Firewall[dbus.String("iptables")].setState("on")
 		self.onOff_pb.setText("Stop")
 		self.lock_icon.setIcon("object-locked")
-		self.onOffInfo_label.setText("<p style='color:green'><h1>Firewall status: On</h1></p>")
+		self.onOffInfo_label.setText("<p style='color:green'><h3>Firewall status: ON</h3></p>")
 		self.infoBar_label.setText("Firewall was started.")
 	    except dbus.DBusException, e:
-		print unicode(e)
 		self.onOff_pb.setText("Start")
 		self.lock_icon.setIcon("object-unlocked")
-		self.onOffInfo_label.setText("<p style='color:red'><h1>Firewall status: Off</h1></p>")
-		self.infoBar_label.setText("Firewall didn't start.")
+		self.onOffInfo_label.setText("<p style='color:red'><h3>Firewall status: OFF</h3></p>")
+		self.infoBar_label.setText("ERROR: Firewall didn't start.")
 	else:
 	    try:
 		self.link.Network.Firewall[dbus.String("iptables")].setState("off")
 		self.onOff_pb.setText("Start")
 		self.lock_icon.setIcon("object-unlocked")
-		self.onOffInfo_label.setText("<p style='color:red'><h1>Firewall status: Off</h1></p>")
+		self.onOffInfo_label.setText("<p style='color:red'><h3>Firewall status: OFF</h3></p>")
 		self.infoBar_label.setText("Firewall was stopped.")
 	    except dbus.DBusException:
 		self.onOff_pb.setText("Stop")
 		self.lock_icon.setIcon("object-locked")
-		self.onOffInfo_label.setText("<p style='color:green'><h1>Firewall status: On</h1></p>")
-		self.infoBar_label.setText("Firewall didn't stop..")
+		self.onOffInfo_label.setText("<p style='color:green'><h3>Firewall status: ON</h3></p>")
+		self.infoBar_label.setText("ERROR: Firewall didn't stop.")
 	      
     def blockIncoming(self):
 	"""Blocks the incoming"""
 	if self.checkboxes[0].isChecked():
 	    try:
 		self.link.Network.Firewall[dbus.String("iptables")].setModuleState("block_incoming","on")
-		self.infoBar_label.setText("Incomings were blocked.")
+		self.infoBar_label.setText("Incoming connections were blocked.")
 	    except dbus.DBusException:
-		self.infoBar_label.setText("Incomings couldn't be blocked.")
+		self.infoBar_label.setText("ERROR: Incoming connections couldn't be blocked.")
 		self.checkboxes[0].setChecked(False)
 	else:
 	    try:
 		self.link.Network.Firewall[dbus.String("iptables")].setModuleState("block_incoming","off")
-		self.infoBar_label.setText("Block of incomings were cancelled.")
+		self.infoBar_label.setText("Incoming connection blockade was cancelled.")
 	    except dbus.DBusException:
-		self.infoBar_label.setText("Incomings block couldn't be cancelled.")
+		self.infoBar_label.setText("ERROR: Incoming connection blockade couldn't be cancelled.")
 		self.checkboxes[1].setChecked(True)
       
     def sharingInternet(self):
@@ -66,16 +65,16 @@ class FirewallApplet(plasmascript.Applet):
 	if self.checkboxes[1].isChecked():
 	    try:
 		self.link.Network.Firewall[dbus.String("iptables")].setModuleState("internet_sharing","on")
-		self.infoBar_label.setText("Internet sharing is successful.")
+		self.infoBar_label.setText("Sharing internet is successful.")
 	    except dbus.DBusException:
-		self.infoBar_label.setText("Internet couldn't be shared.")
+		self.infoBar_label.setText("ERROR: Sharing internet isn't successful.")
 		self.checkboxes[1].setChecked(False)
 	else:
 	    try:
 		self.link.Network.Firewall[dbus.String("iptables")].setModuleState("internet_sharing","off")
-		self.infoBar_label.setText("Internet sharing was cancelled.")
+		self.infoBar_label.setText("Sharing internet was cancelled.")
 	    except dbus.DBusException:
-		self.infoBar_label.setText("Internet share cancellation isn't successful.")
+		self.infoBar_label.setText("ERROR: Sharing internet cancellation isn't successful.")
 		self.checkboxes[1].setChecked(True)
       
     def blockOutgoing(self):
@@ -83,57 +82,73 @@ class FirewallApplet(plasmascript.Applet):
 	if self.checkboxes[2].isChecked():
 	    try:
 		self.link.Network.Firewall[dbus.String("iptables")].setModuleState("block_outgoing","on")
-		self.infoBar_label.setText("Outgoings were blocked.")
+		self.infoBar_label.setText("Outgoing connections were blocked.")
 	    except dbus.DBusException:
-		self.infoBar_label.setText("Outgoings couldn't be blocked.")
+		self.infoBar_label.setText("ERROR: Outgoing connections wasn't blocked.")
 		self.checkboxes[2].setChecked(False)
 	else:
 	    try:
 		self.link.Network.Firewall[dbus.String("iptables")].setModuleState("block_outgoing","off")
-		self.infoBar_label.setText("Outgoing blockade was cancelled.")
+		self.infoBar_label.setText("Outgoing connection blockade was cancelled.")
 	    except dbus.DBusException:
-		self.infoBar_label.setText("Outgoing blockade wasn't cancelled.")
+		self.infoBar_label.setText("ERROR: Outgoing connection blockade wasn't cancelled.")
 		self.checkboxes[2].setChecked(True)
 	    
     def init(self):
 	"""init method for the plasmoid. GUI stuff is located here."""
 	self.setHasConfigurationInterface(False)
-	self.setAspectRatioMode(Plasma.IgnoreAspectRatio)
-        self.resize(700,300)
+	self.setAspectRatioMode(Plasma.Square)#Plasma.IgnoreAspectRatio
+        #self.resize(400,250)
         self.theme = Plasma.Svg(self)
         self.theme.setImagePath("widgets/background")
         self.setBackgroundHints(Plasma.Applet.DefaultBackground)
         
         self.modules=self.link.Network.Firewall[dbus.String("iptables")].listModules()
         
-        incoming_icon=Plasma.IconWidget(self.applet)
-        incoming_icon.setIcon("application-x-smb-workgroup")
-        sharing_icon=Plasma.IconWidget(self.applet)
-        sharing_icon.setIcon("application-x-smb-server")
-        outgoing_icon=Plasma.IconWidget(self.applet)
-        outgoing_icon.setIcon("security-medium")
+        self.incoming_icon=Plasma.IconWidget(self.applet)
+        self.incoming_icon.setIcon("application-x-smb-workgroup")
+        self.incoming_icon.setMinimumWidth(100)
+        self.incoming_icon.setMinimumHeight(40)
+        self.incoming_icon.setMaximumHeight(40)
+        self.sharing_icon=Plasma.IconWidget(self.applet)
+        self.sharing_icon.setIcon("application-x-smb-server")
+        self.sharing_icon.setMinimumWidth(100)
+        self.sharing_icon.setMinimumHeight(40)
+        self.sharing_icon.setMaximumHeight(40)
+        self.outgoing_icon=Plasma.IconWidget(self.applet)
+        self.outgoing_icon.setIcon("security-medium")
+        self.outgoing_icon.setMinimumWidth(100)
+        self.outgoing_icon.setMinimumHeight(40)
+        self.outgoing_icon.setMaximumHeight(40)
         
         self.lock_icon=Plasma.IconWidget(self.applet)
+        self.lock_icon.setMinimumWidth(40)
+        self.lock_icon.setMinimumHeight(40)
+        self.lock_icon.setMaximumHeight(40)
         self.onOff_pb=Plasma.PushButton(self.applet)
         self.onOff_pb.setMinimumWidth(100)
-        self.onOff_pb.setMaximumHeight(50)
+        self.onOff_pb.setMinimumHeight(40)
+        self.onOff_pb.setMaximumHeight(40)
         self.onOffInfo_label=Plasma.Label(self.applet)
         self.onOffInfo_label.setMinimumWidth(200)
+        self.onOffInfo_label.setMaximumHeight(50)
         
         if self.link.Network.Firewall[dbus.String("iptables")].getState()==dbus.String(u"on"):
 	    self.lock_icon.setIcon("object-locked")
 	    self.onOff_pb.setText("Stop")
-	    self.onOffInfo_label.setText("<p style='color:green'><h1>Firewall status: On</h1></p>")
+	    self.onOffInfo_label.setText("<p style='color:green'><h3>Firewall status: ON</h3></p>")
 	else:
 	    self.lock_icon.setIcon("object-unlocked")
 	    self.onOff_pb.setText("Start")
-	    self.onOffInfo_label.setText("<p style='color:red'><h1>Firewall status: Off</h1></p>")
+	    self.onOffInfo_label.setText("<p style='color:red'><h3>Firewall status: OFF</h3></p>")
         
         self.checkboxes=[]
         counter=0
         for i in self.modules:
 	    self.checkboxes.append(Plasma.CheckBox(self.applet))
 	    self.checkboxes[counter].setText(self.link.Network.Firewall[dbus.String("iptables")].moduleInfo(i)[0])
+	    self.checkboxes[counter].setMinimumWidth(120)
+	    self.checkboxes[counter].setMaximumHeight(70)
 	    counter+=1
 	    
 	counter=0
@@ -146,39 +161,28 @@ class FirewallApplet(plasmascript.Applet):
 	    
 	self.infoBarHeader_label=Plasma.Label(self.applet)
 	self.infoBarHeader_label.setText("Latest news:")
+	self.infoBarHeader_label.setMinimumWidth(60)
+	self.infoBarHeader_label.setMaximumHeight(40)
 	self.infoBar_label=Plasma.Label(self.applet)
 	self.infoBar_label.setText("Plasmoid was loaded.")
+	self.infoBar_label.setMinimumWidth(300)
+	self.infoBar_label.setMaximumHeight(70)
 
-        self.layout_general=QGraphicsLinearLayout(Qt.Vertical, self.applet)
+        self.layout=QGraphicsGridLayout(self.applet)
         
-        self.layout_top = QGraphicsLinearLayout(Qt.Horizontal)  
-        self.layout_general.addItem(self.layout_top)
-        self.layout_top.addItem(self.lock_icon)
-        self.layout_top.addItem(self.onOffInfo_label)
-        self.layout_top.addItem(self.onOff_pb)
-        
-        self.layout_line1=QGraphicsLinearLayout(Qt.Horizontal)  
-        self.layout_general.addItem(self.layout_line1)
-        self.layout_line1.addItem(incoming_icon)
-        self.layout_line1.addItem(self.checkboxes[0])
-        
-        self.layout_line2=QGraphicsLinearLayout(Qt.Horizontal)  
-        self.layout_general.addItem(self.layout_line2)
-        self.layout_line2.addItem(sharing_icon))
-        self.layout_line2.addItem(self.checkboxes[1])
-        
-        self.layout_line3=QGraphicsLinearLayout(Qt.Horizontal)  
-        self.layout_general.addItem(self.layout_line3)
-        self.layout_line3.addItem(outgoing_icon)
-        self.layout_line3.addItem(self.checkboxes[2])
-        
-        self.layout_bottom=QGraphicsLinearLayout(Qt.Horizontal)  
-        self.layout_general.addItem(self.layout_bottom)
-        self.layout_bottom.addItem(self.infoBarHeader_label)
-        self.layout_bottom.addItem(self.infoBar_label)
-	
-	#self.layout_general.setAlignment(self.layout_line2, Qt.AlignLeft)
-	self.applet.setLayout(self.layout_general)
+        self.layout.addItem(self.lock_icon, 0,0)
+        self.layout.addItem(self.onOffInfo_label, 0,1)
+        self.layout.addItem(self.onOff_pb,0,2)
+        self.layout.addItem(self.incoming_icon, 1,0)
+        self.layout.addItem(self.checkboxes[0], 1,1)
+        self.layout.addItem(self.sharing_icon, 2,0)
+        self.layout.addItem(self.checkboxes[1], 2,1)
+        self.layout.addItem(self.outgoing_icon, 3,0)
+        self.layout.addItem(self.checkboxes[2], 3,1)
+        self.layout.addItem(self.infoBarHeader_label, 4,0)
+        self.layout.addItem(self.infoBar_label, 4,1)
+
+	self.applet.setLayout(self.layout)
         
         QObject.connect(self.onOff_pb, SIGNAL("clicked()"), self.onOff)
         QObject.connect(self.checkboxes[0], SIGNAL("toggled(bool)"), self.blockIncoming)
@@ -192,24 +196,22 @@ class FirewallApplet(plasmascript.Applet):
 	if self.link.Network.Firewall[dbus.String("iptables")].getState()==dbus.String(u"on"):
 	    self.lock_icon.setIcon("object-locked")
 	    self.onOff_pb.setText("Stop")
-	    self.onOffInfo_label.setText("<p style='color:green'><h1>Firewall status: On</h1></p>")
+	    self.onOffInfo_label.setText("<p style='color:green'><h3>Firewall status: ON</h3></p>")
 	    #self.infoBar_label.setText("Firewall was started.")
 	    #print "ifin ici"
 	else:
 	    self.lock_icon.setIcon("object-unlocked")
 	    self.onOff_pb.setText("Start")
-	    self.onOffInfo_label.setText("<p style='color:red'><h1>Firewall status: Off</h1></p>")
+	    self.onOffInfo_label.setText("<p style='color:red'><h3>Firewall status: OFF</h3></p>")
 	    #self.infoBar_label.setText("Firewall was stopped.")
 	    #print "elsin ici"
-	
-	sayici=0
+	counter=0
 	for i in self.modules:
-	    print "for"
 	    if self.link.Network.Firewall[dbus.String("iptables")].getModuleState(i)==dbus.String(u"on"):
-		self.checkboxes[sayici].setChecked(True)
+		self.checkboxes[counter].setChecked(True)
 	    else:
-		self.checkboxes[sayici].setChecked(False)
-	    sayici+=1
+		self.checkboxes[counter].setChecked(False)
+	    counter+=1
 	    
 def CreateApplet(parent):
     return FirewallApplet(parent) 
