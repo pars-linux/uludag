cd883ffda69985b52e4ccee03568444252e33e0a
caner.basaran <caner.basaran@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-07-14 08:07:34 +0000

    add disable static

--- playground/intern/ibus-project/anthy/actions.py
+++ playground/intern/ibus-project/anthy/actions.py
@@ -10,11 +10,12 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure()
+    autotools.configure("--disable-static")
 
 def build():
     autotools.make()
 
 def install():
     autotools.install()
-    pisitools.remove("/usr/lib/libanthy*.a")
+
+    pisitools.dodoc("AUTHORS", "COPYING", "ChangeLog", "DIARY", "NEWS", "README")
