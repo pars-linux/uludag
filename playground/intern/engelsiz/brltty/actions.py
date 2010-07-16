cf391fcab90bf4446ec955698d084b6075409e3b
ozan.caglayan <ozan.caglayan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-07-16 08:34:13 +0000

    Fix localstatedir, fix overlinking

--- playground/intern/engelsiz/brltty/actions.py
+++ playground/intern/engelsiz/brltty/actions.py
@@ -4,26 +4,29 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
-
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-
 def setup():
-    autotools.configure("--disable-stripping \
+    autotools.configure("--localstatedir=/var \
+                         --disable-stripping \
+                         --disable-caml-bindings \
+                         --disable-java-bindings \
                          --without-curses \
                          --with-flite \
                          --with-espeak \
-                         --with-speechd \
-                         --with-speech-dispatcher")
+                         --with-speechd")
+
+    # Fix overlinking
+    pisitools.dosed("config.mk", "^HOSTMKMOD =.*", "HOSTMKMOD = $(CC) $(LDFLAGS) -shared -o")
 
 def build():
-    autotools.make("-j1")
+    autotools.make()
 
 def install():
-    autotools.rawInstall("INSTALL_ROOT=%s" % get.installDIR()) 
-    
-    pisitools.remove("/usr/lib/libbrlapi.a") 
+    autotools.rawInstall("INSTALL_ROOT=%s" % get.installDIR())
+
+    pisitools.remove("/usr/lib/libbrlapi.a")
     pisitools.dodoc("README")
 
