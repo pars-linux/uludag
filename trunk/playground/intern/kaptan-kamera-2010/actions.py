8064f83e85c4ebe0841f3ff647279eb4f624014c
ozan.caglayan <ozan.caglayan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-07-19 10:50:02 +0000

    Drop setup()

--- trunk/playground/intern/kaptan-kamera-2010/actions.py
+++ trunk/playground/intern/kaptan-kamera-2010/actions.py
@@ -1,20 +1,14 @@
-# -*- coding: utf-8 -*-
- 
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
-from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 from pisi.actionsapi import pythonmodules
 
 WorkDir = "v4l2capture-%s" % get.srcVERSION()
-
-def setup():
-    pythonmodules.run()
 
 def build():
     pythonmodules.compile()
@@ -22,5 +16,3 @@ def build():
 def install():
     pythonmodules.install()
     pisitools.dodoc("README")
-
-
