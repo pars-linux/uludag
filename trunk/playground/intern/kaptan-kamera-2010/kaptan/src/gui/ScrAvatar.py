f3f3e1fd7be6d4e21c224bdba02773a023b93492
ozan.caglayan <ozan.caglayan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-07-21 07:48:19 +0000

    Fix whitespaces

--- trunk/playground/intern/kaptan-kamera-2010/kaptan/src/gui/ScrAvatar.py
+++ trunk/playground/intern/kaptan-kamera-2010/kaptan/src/gui/ScrAvatar.py
@@ -9,6 +9,7 @@
 #
 #TODO:
 #- left right position duzelecek
+#- kamera yoksa gorunmeyebilir
 # Please read the COPYING file.
 #
 
@@ -35,120 +36,117 @@ class Widget(QtGui.QWidget, ScreenWidget):
     title = ki18n("Kullanıcı profili")
 
     def __init__(self, *args):
-        QtGui.QWidget.__init__(self,None)
+        QtGui.QWidget.__init__(self, None)
         self.ui = Ui_Form()
         self.ui.setupUi(self)
-	
-	self.drawCrop = DrawCropMask(self.ui.camGoruntu)
-        
+
+        self.drawCrop = DrawCropMask(self.ui.camGoruntu)
+
         Widget.desc = QVariant(unicode("Kullanıcı profilinizi oluşturun"))
 
-	self.pictureTaken = 0
+        self.pictureTaken = 0
 
-	for dev in os.listdir("/dev"):
-	  if dev.startswith("video"):
-	    cam = v4l2capture.Video_device(os.path.join("/dev", dev))
-	    cam_driver, cam_card, cam_bus, cam_capabilities = cam.get_info()
-	    cam_str = "%s - %s" % (cam_card, os.path.join("/dev", dev))
+        for dev in os.listdir("/dev"):
+            if dev.startswith("video"):
+                cam = v4l2capture.Video_device(os.path.join("/dev", dev))
+                cam_driver, cam_card, cam_bus, cam_capabilities = cam.get_info()
+                cam_str = "%s - %s" % (cam_card, os.path.join("/dev", dev))
 
-	    if "video_capture" in cam_capabilities:
-			if "radio" in cam_capabilities or "tuner" in cam_capabilities:
-				continue
-			self.ui.comboBox.addItem(cam_str)
-	    
-	  #else:
-	  #  self.connect(self.ui.takeButton, QtCore.SIGNAL('clicked()'), self.noDeviceMessage)
-	   
-	self.timer = QtCore.QTimer(self)
-	self.connect(self.timer, QtCore.SIGNAL("timeout()"), self.refreshCam)
-        self.connect(self.ui.comboBox, QtCore.SIGNAL('activated(QString)'), self.activateCam)  
+                if "video_capture" in cam_capabilities:
+                    if "radio" in cam_capabilities or "tuner" in cam_capabilities:
+                        continue
+                    self.ui.comboBox.addItem(cam_str)
+
+        self.timer = QtCore.QTimer(self)
+        self.connect(self.timer, QtCore.SIGNAL("timeout()"), self.refreshCam)
+        self.connect(self.ui.comboBox, QtCore.SIGNAL('activated(QString)'), self.activateCam)
         self.connect(self.ui.saveButton, QtCore.SIGNAL('clicked()'), self.savePicture)
         self.connect(self.ui.takeButton, QtCore.SIGNAL('clicked()'), self.showPicture)
-	self.connect(self.ui.takeAgainButton, QtCore.SIGNAL('clicked()'), self.activateCam)
+        self.connect(self.ui.takeAgainButton, QtCore.SIGNAL('clicked()'), self.activateCam)
         self.connect(self.ui.chooseFileButton, QtCore.SIGNAL('clicked()'), self.chooseFile)
-        
-	self.ui.saveButton.hide()
+
+        self.ui.saveButton.hide()
         self.ui.takeAgainButton.hide()
-           
+
     def activateCam(self):
-      # Skip first item
-      if self.ui.comboBox.currentIndex() == 0:
-          return
-      self.ui.takeButton.show()
-      self.ui.saveButton.hide()
-      self.ui.takeAgainButton.hide()
-      self.timer.stop()
-      cam = str(self.ui.comboBox.currentText())
-      cam = cam.split("-")[1].strip()
-      self.video = v4l2capture.Video_device(cam)
+        # Skip first item
+        if self.ui.comboBox.currentIndex() == 0:
+            return
 
-      self.size_x, self.size_y = self.video.set_format(320, 240)
-      self.video.create_buffers(10)
-      self.video.queue_all_buffers()
-      self.video.start()
-      
-      select.select((self.video,), (), ())
+        self.ui.takeButton.show()
+        self.ui.saveButton.hide()
+        self.ui.takeAgainButton.hide()
+        self.timer.stop()
+        cam = str(self.ui.comboBox.currentText())
+        cam = cam.split("-")[1].strip()
+        self.video = v4l2capture.Video_device(cam)
 
-      self.timer.setInterval(30)
-      self.timer.start()
-      
+        self.size_x, self.size_y = self.video.set_format(320, 240)
+        self.video.create_buffers(10)
+        self.video.queue_all_buffers()
+        self.video.start()
+
+        select.select((self.video,), (), ())
+
+        self.timer.setInterval(30)
+        self.timer.start()
 
     def refreshCam(self):
-      try:
-	self.image_data = self.video.read_and_queue()
-	self.image_raw = Image.fromstring("RGB", (self.size_x, self.size_y), self.image_data)
-	self.image = ImageQt.ImageQt(self.image_raw).mirrored(True, False)
-	self.ui.camGoruntu.setPixmap(QtGui.QPixmap.fromImage(self.image))
-	self.updateCropMask()
-      except:
-	pass
+        try:
+            self.image_data = self.video.read_and_queue()
+            self.image_raw = Image.fromstring("RGB", (self.size_x, self.size_y), self.image_data)
+            self.image = ImageQt.ImageQt(self.image_raw).mirrored(True, False)
+            self.ui.camGoruntu.setPixmap(QtGui.QPixmap.fromImage(self.image))
+            self.updateCropMask()
+        except:
+            pass
 
     def showPicture(self):
-      self.timer.stop()
-      self.video.stop()
-      self.ui.saveButton.show()
-      self.ui.takeAgainButton.show()
-      self.ui.takeButton.hide()
+        self.timer.stop()
+        self.video.stop()
+        self.ui.saveButton.show()
+        self.ui.takeAgainButton.show()
+        self.ui.takeButton.hide()
 
     def savePicture(self):
-      self.pictureTaken = 1
-      home = os.path.expanduser("~")
-      self.image.save("profilim.jpg")
-      self.foto = Image.open("profilim.jpg")
-      self.space = (85, 70, 235, 195)
-      self.paste = self.foto.crop(self.space)
-      self.paste.save(home + "/new.jpg")
-      os.rename(home + "/new.jpg", home + "/.face.icon")
+        self.pictureTaken = 1
+        home = os.path.expanduser("~")
+        self.image.save("profilim.jpg")
+        self.foto = Image.open("profilim.jpg")
+        self.space = (85, 70, 235, 195)
+        self.paste = self.foto.crop(self.space)
+        self.paste.save(home + "/new.jpg")
+        os.rename(home + "/new.jpg", home + "/.face.icon")
 
     def shown(self):
         pass
 
     def execute(self):
-	if not self.pictureTaken == 1:
-	  self.timer.stop()
-	  try:
-	    self.video.stop()
-	  except:
-	    pass
-	  
-        return True     
-        
+        if not self.pictureTaken == 1:
+            self.timer.stop()
+            try:
+                self.video.stop()
+            except:
+                pass
+
+        return True
+
     def chooseFile(self):
-      self.selectedFile = QFileDialog.getOpenFileName(None,"Open Image", os.environ["HOME"], 'Image Files (*.png *.jpg *bmp)')
-      print self.selectedFile
-      
-      if self.selectedFile.isNull():
+        self.selectedFile = QFileDialog.getOpenFileName(None,"Open Image", os.environ["HOME"], 'Image Files (*.png *.jpg *bmp)')
+        print self.selectedFile
+
+        if self.selectedFile.isNull():
             return
-      else:
-	home = os.path.expanduser("~")
+        else:
+            home = os.path.expanduser("~")
 
-	self.ui.camGoruntu.setPixmap(QtGui.QPixmap(str(self.selectedFile)))
-	self.ui.camGoruntu.update()
+        self.ui.camGoruntu.setPixmap(QtGui.QPixmap(str(self.selectedFile)))
+        self.ui.camGoruntu.update()
 
-	self.paste.save(home + "/new2.jpg")
-	os.rename(home + "/new2.jpg", home + "/.face.icon")
- 
-        
+        self.paste.save(home + "/new2.jpg")
+        os.rename(home + "/new2.jpg", home + "/.face.icon")
+
+
 class DrawCropMask(QtGui.QWidget):
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
@@ -160,7 +158,7 @@ class DrawCropMask(QtGui.QWidget):
         painter.setRenderHint(QPainter.Antialiasing)
         painter.setPen(QColor(20,20,20, 0))
         painter.setBrush(QBrush(QColor(0, 0, 0, 200)))
-        
+
         rect = QRect(0, 0, 85, 240) # x, y, w, h
         rect2 = QRect(85, 0, 150, 45) 
         rect3 = QRect(235, 0, 85, 240)
@@ -175,4 +173,4 @@ class DrawCropMask(QtGui.QWidget):
         painter.end()
 
     def updateCropMask(self):
-	self.update()
+        self.update()
