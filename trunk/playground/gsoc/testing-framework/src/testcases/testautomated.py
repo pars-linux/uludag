05fb7dbde9a42d8a5483f1504efba254ec186a9b
sukhbirsingh <sukhbirsingh@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-07-28 12:16:03 +0000

    Fixed automated testcase to handle more than one case tag.

--- trunk/playground/gsoc/testing-framework/src/testcases/testautomated.py
+++ trunk/playground/gsoc/testing-framework/src/testcases/testautomated.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import subprocess
-
+import sys
 from clcolorize import colorize
 
 
@@ -10,26 +10,46 @@ class TestAutomated:
     """This class will perform an automated test, the purpose of which is to run
     a command, get its output and compare it with the expected output, which is
     already encoded in the testcase file."""
-    def __init__(self, package, commandtext, expectedtext, report=None):
+    def __init__(self, package, element, report=None):
         self.package = package
-        self.commandtext = commandtext.split()      
-        self.expected = expectedtext
+        self.element = element
         self.report = list()
 
     def test_automated_main(self):
         """Entry point for the testcase type automated."""
-        try:
-            runCommand = subprocess.Popen(self.commandtext,
-                                          stdout=subprocess.PIPE,
-                                          stderr=subprocess.PIPE)
-        except OSError:
-            self.report.append('Invalid command or invalid option: {0}'.format(''.join(self.commandtext)))
-            return
-        output, error = runCommand.communicate()
-        if error:
-            self.report.append('Error: {0}'.format(error))
-            return 
-        self.report.append('Output: ')
-        self.report.append('{0}'.format(output.rstrip()))
-        self.report.append('Expected: ')
-        self.report.append('{0}'.format(self.expected))
+        case = self.element.xpath('case')
+        totalCases = len(case)
+        counter = 0
+        while counter < totalCases:
+            print colorize('Case {0} of {1}', 'bold').format(counter+1, totalCases)
+            self.report.append('')
+            self.report.append('Case {0} of {1}'.format(counter+1, totalCases))
+            for text in case[counter].getiterator('text'):
+                print text.text
+            commandList = []
+            for command in case[counter].getiterator('command'):
+                commandList.append(command.text)
+            totalCommands = len(commandList)
+            self.report.append('Total Commands: {0}'.format(totalCommands))
+            self.report.append('-')
+            commandCounter = 0
+            while commandCounter < totalCommands:
+                self.report.append('Command {0}: {1}'.format(commandCounter+1, commandList[commandCounter]))
+                try:
+                    runCommand = subprocess.Popen(commandList[commandCounter].split(),
+                                                  stdout=subprocess.PIPE,
+                                                  stderr=subprocess.PIPE)
+                except OSError:
+                    self.report.append('Output: Invalid command or invalid option')
+                    commandCounter +=1 
+                    continue
+                output, error = runCommand.communicate()
+                if error:
+                    self.report.append('Error: {0}'.format(error.rstrip()))
+                    commandCounter +=1 
+                    continue
+                self.report.append('Output: {0}'.format(output.rstrip()))
+                commandCounter += 1
+            for expected in case[counter].getiterator('expected'):
+                self.report.append('Expected: {0}'.format(expected.text))            
+            counter += 1
--- trunk/playground/gsoc/testing-framework/src/testcases/testshell.py
+++ trunk/playground/gsoc/testing-framework/src/testcases/testshell.py
@@ -7,9 +7,8 @@ from clcolorize import colorize
 class TestShell:
     """This class is used to handle the testcase of shell, in which the user is
     told to run a certain command on and note down the output."""
-    def __init__(self, element, text, report=None):
+    def __init__(self, element, report=None):
         self.element = element
-        self.textlist = text
         self.report = list()
         
     def test_shell_main(self):
@@ -19,8 +18,9 @@ class TestShell:
         counter = 0
         while counter < totalCases:
             print colorize('Case {0} of {1}', 'bold').format(counter+1, totalCases)
-            print ''.join(self.textlist)
-            for text in self.element.getiterator('command'):
+            for text in case[counter].getiterator('text'):
+                print text.text
+            for text in case[counter].getiterator('command'):
                 print colorize(text.text, 'yellow')
             # Get the observations
             print colorize('Enter your observation of the test:', 'bold')
@@ -30,5 +30,4 @@ class TestShell:
             else:
                 self.report.append('Case {0}: No observation entered.'.format(counter+1))
             print '\n'
-            counter += 1        
-        
+            counter += 1
