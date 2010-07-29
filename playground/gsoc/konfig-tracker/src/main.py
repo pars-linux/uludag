9a85b893b1fc1b517f85504939df5044d2d04d5c
jain.basil.aliyas <jain.basil.aliyas@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-07-29 13:40:39 +0000

    Making the source directory clean, adding Authors etc.

---
+++ playground/gsoc/konfig-tracker/src/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# System
+import sys
+
+# Application stuffs
+from PyQt4.QtCore import SIGNAL
+
+#PyKDE4 stuffs
+from PyKDE4.kdeui import KUniqueApplication
+from PyKDE4.kdecore import KCmdLineArgs
+
+if __name__ == "__main__":
+    
+    from konfigtracker.about import aboutData
+    from konfigtracker.konfigtrackermain import KonfigTracker
+    
+    # set commandline arguments
+    KCmdLineArgs.init(sys.argv,aboutData)
+    
+    #creating a kapplication instance
+    app = KUniqueApplication()
+    MainWindow = KonfigTracker(app)
+    MainWindow.show()
+    app.connect(app, SIGNAL('lastWindowClosed()'), app.quit)
+    
+    # Run the application
+    app.exec_()  
