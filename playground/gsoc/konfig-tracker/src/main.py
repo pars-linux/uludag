7736deb6d7b054f05e178dbb985ba709268a3db2
jain.basil.aliyas <jain.basil.aliyas@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-07-30 20:44:37 +0000

    More changes

--- playground/gsoc/konfig-tracker/src/main.py
+++ playground/gsoc/konfig-tracker/src/main.py
@@ -3,6 +3,7 @@
 
 # System
 import sys
+import dbus
 
 # Application stuffs
 from PyQt4.QtCore import SIGNAL
@@ -16,14 +17,19 @@ if __name__ == "__main__":
     from konfigtracker.about import aboutData
     from konfigtracker.konfigtrackermain import KonfigTracker
     
+    # Create a dbus mainloop if its not exists
+    if not dbus.get_default_main_loop():
+        from dbus.mainloop.qt import DBusQtMainLoop
+        DBusQtMainLoop(set_as_default = True)
+
     # set commandline arguments
     KCmdLineArgs.init(sys.argv,aboutData)
     
     #creating a kapplication instance
-    app = KUniqueApplication()
+    app = KUniqueApplication(True, True)
     MainWindow = KonfigTracker(app)
     MainWindow.show()
-    app.connect(app, SIGNAL('lastWindowClosed()'), app.quit)
+    #app.connect(app, SIGNAL('lastWindowClosed()'), app.quit)
     
     # Run the application
     app.exec_()  
