ccb6e67a998751f4918253e3d51474f3d1ec3a01
sukhbirsingh <sukhbirsingh@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-08-03 07:04:07 +0000

    Beta release of the testcasewriter, with all the testcases working.

--- playground/gsoc/testing-framework/src/testcasewriter.py
+++ playground/gsoc/testing-framework/src/testcasewriter.py
@@ -21,8 +21,8 @@ class XMLWriter:
         """Entry point for the script."""
         while True:
             print 'Choose the type of testcase (1 - 6): '
-            options_cases = '1. install\t2. gui\n3. shell\t4. automated\n' \
-                                '5. (Exit)\t6. (Save and Exit)'
+            options_cases = '1. install  2. gui  3. shell  4. automated\n' \
+                                '5. (Exit)   6. (Save and Exit)'
             choice = self.get_number(options_cases, 6)
             # exit without saving anything
             if choice == 5:
@@ -39,6 +39,7 @@ class XMLWriter:
                 shell=self.shell,
                 automated=self.automated
             )[test_choice]()
+            print 'Ok'
             print ''
             
     def install(self):
@@ -69,6 +70,11 @@ class XMLWriter:
         if total_packages == 0:
             print self.skip_message
             return
+        cases = self.get_number('How many cases do you want?')
+        if cases == 0:
+            print 'There has to be at least ONE case in a gui testcase.'
+            return
+        print ''
         guiPackageElt = etree.SubElement(self.root_element, 'testcase',
                                                              lang='en',
                                                              test='gui')
@@ -77,49 +83,129 @@ class XMLWriter:
             packageElt = etree.SubElement(guiPackageElt, 'package')
             packageElt.text = package_list[counter]
             counter += 1
-        cases = self.get_number('How many cases do you want?')
-        if cases == 0:
-            print 'There has to be at least ONE case in a gui testcase.'
-            return
-        print ''
         case_counter = 0
         case_list = list()
         tag_text = 'Select the tag you want to enter:\n' \
-                     '1. <text>\t2. <download>\n3. <link>\t4. (END)'
-        # create the elements
-        guiCaseElt = etree.SubElement(guiPackageElt, 'case')
-        textElt = etree.SubElement(guiCaseElt, 'text')
-        downloadElt = etree.SubElement(guiCaseElt, 'download')
-        linkElt = etree.SubElement(guiCaseElt, 'link')
+                     '1. <text>   2. <download>   3. <link>\t4. (END)'     
         # now go over the cases
         while case_counter < cases:
             print 'Case {0} / {1}'.format(case_counter+1, cases)
+            guiCaseElt = etree.SubElement(guiPackageElt, 'case')
             while True:
                 tag_choice = self.get_number(tag_text, 4)
                 if tag_choice == 1:
                     text = self.get_text('Enter the text:')
+                    textElt = etree.SubElement(guiCaseElt, 'text')
                     textElt.text = text
                     continue
                 if tag_choice == 2:
                     download = self.get_text('Enter the download link text:')
+                    downloadElt = etree.SubElement(guiCaseElt, 'download')
                     downloadElt.text = download
                     continue
                 if tag_choice == 3:
                     link = self.get_text('Enter the link text:')
+                    linkElt = etree.SubElement(guiCaseElt, 'link')
                     linkElt.text = link
                     continue
                 if tag_choice == 4:
+                    if len(guiCaseElt) == 0:
+                        print 'No data for <case> tag was entered.'
+                        self.root_element.remove(guiPackageElt)
                     break
-                print ''                
+            case_counter += 1
+            
+    def shell(self):
+        """Input the packages for the testcase shell."""
+        print '\nTest type: SHELL'
+        package_list, total_packages = self.get_packages()
+        if total_packages == 0:
+            print self.skip_message
+            return
+        cases = self.get_number('How many cases do you want?')
+        if cases == 0:
+            print 'There has to be at least ONE case in a shell testcase.'
+            return
+        print ''
+        shellPackageElt = etree.SubElement(self.root_element, 'testcase',
+                                                              lang='en',
+                                                              test='shell')
+        counter = 0
+        while counter < total_packages:
+            packageElt = etree.SubElement(shellPackageElt, 'package')
+            packageElt.text = package_list[counter]
+            counter += 1
+        case_counter = 0
+        case_list = list()
+        tag_text = 'Select the tag you want to enter:\n' \
+                     '1. <text>\t2. <command>\t3. (END)'
+        while case_counter < cases:
+            print 'Case {0} / {1}'.format(case_counter+1, cases)
+            shellCaseElt = etree.SubElement(shellPackageElt, 'case')
+            while True:
+                tag_choice = self.get_number(tag_text, 3)
+                if tag_choice == 1:
+                    text = self.get_text('Enter the text:')
+                    textElt = etree.SubElement(shellCaseElt, 'text')
+                    textElt.text = text
+                    continue
+                if tag_choice == 2:
+                    download = self.get_text('Enter the command text:')
+                    downloadElt = etree.SubElement(shellCaseElt, 'command')
+                    downloadElt.text = download
+                    continue
+                if tag_choice == 3:
+                    if len(shellCaseElt) == 0:
+                        print 'No data for <case> tag was entered.'
+                        self.root_element.remove(shellPackageElt)
+                    break
             case_counter += 1
 
     def automated(self):
         """Input the packages for the testcase automated."""
-        print 'Automated got called'
-        
-    def shell(self):
-        """Input the packages for the testcase shell."""
-        print 'Shell got called'
+        print '\nTest type: AUTOMATED'
+        package_list, total_packages = self.get_packages()
+        if total_packages == 0:
+            print self.skip_message
+            return
+        cases = self.get_number('How many cases do you want?')
+        if cases == 0:
+            print 'There has to be at least ONE case in a automated testcase.'
+            return
+        print ''
+        automatedPackageElt = etree.SubElement(self.root_element, 'testcase',
+                                                                 lang='en',
+                                                                 test='automated')
+        counter = 0
+        while counter < total_packages:
+            packageElt = etree.SubElement(automatedPackageElt, 'package')
+            packageElt.text = package_list[counter]
+            counter += 1
+        case_counter = 0
+        case_list = list()
+        tag_text = 'Select the tag you want to enter:\n' \
+                     '1. <command>  2. <expected>  3. (END)'
+        while case_counter < cases:
+            print 'Case {0} / {1}'.format(case_counter+1, cases)
+            automatedCaseElt = etree.SubElement(automatedPackageElt, 'case')
+            while True:
+                tag_choice = self.get_number(tag_text, 3)
+                if tag_choice == 1:
+                    download = self.get_text('Enter the command text:')
+                    downloadElt = etree.SubElement(automatedCaseElt, 'command')
+                    downloadElt.text = download
+                    continue
+                if tag_choice == 2:
+                    expected = self.get_text('Enter the expected text:')
+                    expectedElt = etree.SubElement(automatedCaseElt, 'expected')
+                    expectedElt.text = expected
+                    continue
+                if tag_choice == 3:
+                    if len(automatedCaseElt) == 0:
+                        print 'No data for <case> tag was entered.'
+                        self.root_element.remove(automatedPackageElt)
+                    break
+            case_counter += 1
         
     def get_packages(self):
         """Input the list of packages."""
@@ -185,7 +271,7 @@ class XMLWriter:
                 continue
             if os.path.isfile(file_path):
                 print "The file '{0}' already exists.".format(file_path)
-                answer = raw_input('Do you wish to overwrite? ( y / n): ')
+                answer = raw_input('Do you wish to overwrite? ( y / n ): ')
                 if answer in ('y', 'Y', 'yes', 'YES'):
                         break
                 else:
@@ -197,8 +283,7 @@ class XMLWriter:
             sys.exit("Testcase XML file saved to: '{0}'".format(file_path))
         except IOError:
             sys.exit('An error was encountered while trying to save the file.')
-        # print etree.tostring(self.document_tree, pretty_print=True, xml_declaration=True)
-            
+
             
 if __name__ == '__main__':
     print 'Pardus Testing Framework - Testcase Writer\n'
