ac2e25d9c2c827fc65c4a4c424080f9e122a3fa3
sukhbirsingh <sukhbirsingh@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-08-16 06:20:54 +0000

    Added the GNU GPL license to the two main files.

--- playground/gsoc/testing-framework/src/package-testing.py
+++ playground/gsoc/testing-framework/src/package-testing.py
@@ -2,21 +2,15 @@
 # -*- coding: utf-8 -*-
 
 # Pardus Package Testing Framework - An Approach to Automated Package Testing
-# Copyright (C) 2010, Sukhbir Singh
-#
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 2.1 of the License, or (at your option) any later version.
-#
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with this library; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+# Copyright (C) 2010, Sukhbir Singh and Semen Cirit
+# Copyright (C) 2005 - 2009, TUBITAK/ UEKAE
+# 
+# This program is free software; you can redistribute it and/ or modify it under
+# the terms of the GNU General Public License (GNU GPL) as published by the
+# Free Software Foundation; either version 2 of the License, or (at your option)
+# any later version
+# 
+# Please read the COPYING file.
 
 import os
 import sys
