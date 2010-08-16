b022daceebf771c29d4c8aa53b38797b99ae8988
sukhbirsingh <sukhbirsingh@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-08-16 07:55:13 +0000

    Removed a major bug in testinstall that allowed for pacakges that were not allowed to be tested.

--- playground/gsoc/testing-framework/src/testcasewriter.py
+++ playground/gsoc/testing-framework/src/testcasewriter.py
@@ -6,9 +6,9 @@
 # Copyright (C) 2005 - 2009, TUBITAK/ UEKAE
 # 
 # This program is free software; you can redistribute it and/ or modify it under
-# the terms of the GNU General Public License (GNU GPL) as published by the
-# Free Software Foundation; either version 2 of the License, or (at your option)
-# any later version
+# the terms of the GNU General Public License (GNU GPL) as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option) any
+# later version
 # 
 # Please read the COPYING file.
 
