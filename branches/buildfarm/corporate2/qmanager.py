8ab96e4843ddf9e63d8ffe4e9e190de5e4439f17
gokcen.eraslan <gokcen.eraslan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-09-02 11:39:58 +0000

    Add package name also.

--- branches/buildfarm/corporate2/qmanager.py
+++ branches/buildfarm/corporate2/qmanager.py
@@ -70,7 +70,7 @@ class QueueManager:
                         continue
 
                     if component:
-                        path = "%s/%s/pspec.xml" % (config.localPspecRepo, component.replace(".", "/"))
+                        path = "%s/%s/%s/pspec.xml" % (config.localPspecRepo, component.replace(".", "/"), line)
 
                         if os.path.exists(path):
                             queueName.append(path)
