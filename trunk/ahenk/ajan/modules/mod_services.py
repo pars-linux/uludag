7dfb2c182ee5f8755435becac79048f490f488fc
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-09-16 09:48:17 +0000

    Service info backend

---
+++ trunk/ahenk/ajan/modules/mod_services.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Standard modules
+import logging
+import simplejson
+
+# COMAR
+import comar
+
+
+def process(message, dryrun=False):
+    if message.type == "command":
+        if message.command == "service info":
+            link = comar.Link()
+            args = []
+            for package in link.System.Service:
+                type_, desc_, status_ = link.System.Service[package].info()
+                args.append((package, desc_, status_))
+            message.reply("service info:%s" % simplejson.dumps(args))
