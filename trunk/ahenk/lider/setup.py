26951e55cf537f163f48ca5634be473480687568
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-10-12 15:44:53 +0000

    New release

--- trunk/ahenk/lider/setup.py
+++ trunk/ahenk/lider/setup.py
@@ -4,7 +4,7 @@
 from distutils.core import setup
 
 setup(name='ahenk-lider',
-      version='1.9.9',
+      version='1.9.10',
       description='Agent for Ahenk Remote Management Framework',
       author='Bahadır Kandemir',
       author_email='bahadir@python.net',
