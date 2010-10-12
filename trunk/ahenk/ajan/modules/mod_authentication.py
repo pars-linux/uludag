d13933192d3327e00f7d53e275291920d37df7d9
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-10-12 14:45:19 +0000

    Bind policies with authentication source handler

--- trunk/ahenk/ajan/modules/mod_authentication.py
+++ trunk/ahenk/ajan/modules/mod_authentication.py
@@ -18,5 +18,25 @@ def process(message, dryrun=False):
     """
     if message.type == "policy":
         if "authenticationType" in message.policy:
-            authenticationType = message.policy.get("authenticationType", "unix")[0]
-            logging.info("Authentication: Source is %s" % authenticationType)
+            authenticationType = message.policy.get("authenticationType", ["unix"])[0]
+            # UNIX database
+            if authenticationType == "unix":
+                logging.info("Authentication: Using UNIX password database.")
+                if not dryrun:
+                    os.system("ahenk_authentication --type unix")
+            # LDAP database
+            elif authenticationType == "ldap":
+                authenticationDomainLDAP = message.policy.get("authenticationDomainLDAP", [""])[0]
+                authenticationHostLDAP = message.policy.get("authenticationHostLDAP", [""])[0]
+                if authenticationHostLDAP and authenticationDomainLDAP:
+                    logging.info("Authentication: Using LDAP domain %s on %s" % (authenticationDomainLDAP, authenticationHostLDAP))
+                    if not dryrun:
+                        os.system("ahenk_authentication --type=ldap --host=%s --domain=%s" % (authenticationHostLDAP, authenticationDomainLDAP))
+            # Active Directory database
+            elif authenticationType == "ad":
+                authenticationDomainAD = message.policy.get("authenticationDomainAD", [""])[0]
+                authenticationHostAD = message.policy.get("authenticationHostAD", [""])[0]
+                if authenticationHostAD and authenticationDomainAD:
+                    logging.info("Authentication: Using Active Directory domain %s on %s" % (authenticationDomainAD, authenticationHostAD))
+                    if not dryrun:
+                        pass
