1e5b54100719ed0a35b5811d1b0dbf3c941a8fa9
caglar.kilimci <caglar.kilimci@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-11-29 07:50:01 +0000

    Reading config file changed

--- playground/caglar.kilimci/ahenk/lider/lider/helpers/profilereader.py
+++ playground/caglar.kilimci/ahenk/lider/lider/helpers/profilereader.py
@@ -23,19 +23,19 @@ class ProfileReader:
             for line in config_file:
                 line = line.strip()
                 if line.startswith("domain="):
-                    tmp_profile.set_domain = line.split("=", 1)[1]
-                    line = line.strip()
+                    tmp_profile.set_domain(line.split("=", 1)[1])
+                    line = config_file.next().strip()
                     if line.startswith("address="):
-                        tmp_profile.set_address = line.split("=", 1)[1]
-                        line = line.strip()
+                        tmp_profile.set_address(line.split("=", 1)[1])
+                        line = config_file.next().strip()
                         if line.startswith("username="):
-                            tmp_profile.set_username = line.split("=", 1)[1]
+                            tmp_profile.set_username(line.split("=", 1)[1])
                             self.profiles.append(tmp_profile)
-                            line = line.strip()
+                            config_file.next()
 
         return self.profiles
 
     def get_last_profile(self):
-        if not len(self.profiles):
+        if len(self.profiles):
             return self.profiles[0]
 
