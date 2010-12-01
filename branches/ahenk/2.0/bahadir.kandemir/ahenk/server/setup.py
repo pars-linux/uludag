7ed4caf5dbd029ce786a5127ff086e8764dc08fd
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-12-01 19:56:22 +0000

    New release

--- branches/ahenk/2.0/bahadir.kandemir/ahenk/server/setup.py
+++ branches/ahenk/2.0/bahadir.kandemir/ahenk/server/setup.py
@@ -5,7 +5,7 @@ import os
 from distutils.core import setup
 
 setup(name='ahenk-setup',
-      version='1.9.9',
+      version='1.9.20',
       description='Setup tools for for Ahenk Remote Management Framework',
       author='Bahadır Kandemir',
       author_email='bahadir@python.net',
