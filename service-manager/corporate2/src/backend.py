dfe3753d59a3478ffeb44dea5428603cd76341fa
Gokmen Goksel <gokmen@goksel.me>
2010-12-06 13:17:20 +0000

    - Show proper message box when error ocured - Fix size issue of services list - Update PO - Fix copyright headers

--- service-manager/corporate2/src/backend.py
+++ service-manager/corporate2/src/backend.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+#
+# Copyright (C) 2010, TUBITAK/UEKAE
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
 
 import comar
+
+from kdecore import i18n
+from kdeui import KMessageBox
 
 class Backend:
 
@@ -9,7 +20,10 @@ class Backend:
         self.link = comar.Link()
         self.link.setLocale()
         self.link.listenSignals("System.Service", parent.handleSignals)
+        self.parent = parent
 
+    def show_error(self, msg):
+        return KMessageBox.error(self.parent, msg, i18n('Error'))
 
     def info(self, service, async = None):
         if async:
@@ -27,9 +41,11 @@ class Backend:
             else:
                 method()
             return True
-
         except Exception, msg:
-            print 'Error: ', msg
+            if 'PolicyKit' in unicode(msg):
+                self.show_error(i18n('You are not authorized for this operation.'))
+            else:
+                self.show_error(i18n('An error occured:\n%s' % unicode(msg)))
             return False
 
     def start(self, service, async = None):
