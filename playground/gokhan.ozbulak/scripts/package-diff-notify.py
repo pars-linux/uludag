2827bcfff623f26338d11ab6e0549f8837eaa1f6
gokhan.ozbulak <gokhan.ozbulak@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-12-13 08:56:59 +0000

    Refactoring and clean up the script

--- playground/gokhan.ozbulak/scripts/package-diff-notify.py
+++ playground/gokhan.ozbulak/scripts/package-diff-notify.py
@@ -24,6 +24,7 @@ repoList = (
 ''' Structure : {packager_name -> {package_name -> [[[release1, version1],..,[releaseX, versionX]], [#package1,..,#packageX], [#patch1,..,#patchX], [distro_version1,..,distro_versionX] [packager_mail1,..,packager_mailX]]},..} '''
 repos = {}
 
+''' Option parser object '''
 options = None
 
 ''' This stores packager list maintaining same package in different distributions  '''
@@ -35,96 +36,53 @@ distroList = []
 
 ''' Modify here if necessary '''
 reportFile = "report"
-mailSender = "gozbulak@pardus.org.tr"
+mailSender = "sender_name_here"
+mailSenderUsr = "sender@pardus.org.tr"
 mailSenderPwd = "pwd_here"
 mailSubject = "Package Summary"
 mailServer = "mail.pardus.org.tr"
-contentHeader = "Here is a summary about your packages reside on different repositories.\nPlease, take action based on summary column below.\n\n\t\t\t\t\t***PACKAGES***\n"
-columns = "|%-50s|%-30s|%-10s|%-20s|%-10s|%-10s|%-100s|" %("Package", "Distro", "Release no", "Version no", "#Package", "#Patch", "Is there conflict?")
-contentHeader = "%s%s" %("Here is a summary about your packages reside on different repositories.\nPlease, take action based on summary column below.\n\n\t\t\t\t\t***PACKAGES***\n", columns)
-contentFooter = "You are getting this e-mail because you have packages in our repositories. If you think you shouldn't receive this e-mail please contact with %s" %(mailSender)
 
-# Remove this later
-def suppressprintHelp(detail=False, retVal=-1):
-    print "This is a notifier script to give detailed info to packagers about their packages.\nUsage:\n"\
-          "\tpackage-diff-notify [options] [<repoURL1> <repoURL2> ... <repoURLx>]\n"
+mailTemplate = """\
+From: %s <%s>
+To: %s
+Subject: [Pardus] Package Summary
+Content-Type: text/plain; charset="utf-8"
 
-    if detail:
-        print "\t%-20s%-50s\n" %("<repoURL>", "Compressed pisi index file(pisi-index) path in URL format. Use 'xz' or 'bz2' files")
-        print "Options:\n",\
-              "\t%-20s%-50s\n" %("--help", "Display this information"),\
-              "\t%-20s%-50s\n" %("--usecache", "Use the cached pisi index files compressed as 'xz' or 'bz2' format. Use without <repoURL>"),\
-              "\t%-20s%-50s\n" %("--nomail", "Prevent the util from sending e-mail to packagers"),\
-              "\t%-20s%-50s\n" %("--report", "Dump the output into a file named 'report'"),\
-              "\t%-20s%-50s\n" %("-h", "Short option for 'help'"),\
-              "\t%-20s%-50s\n" %("-uc", "Short option for 'usecache'"),\
-              "\t%-20s%-50s\n" %("-nm", "Short option for 'nomail'"),\
-              "\t%-20s%-50s\n" %("-r", "Short option for 'report'"),\
+Dear Pardus contributor,
 
-    sys.exit(retVal)
+Here is a summary about your packages reside on our Pardus repositories. Please, take action
+based on comments next to package name. Comments and their explanations are as follows:
+    Incomplete: This is shown if the package doesn't exist in one of the repositories at least.
+    Conflict:   This is shown if the package is maintained by more than one packager in diferent repositories.
+    Same:       This is shown if no difference among package attributes such as version number or number of patches applied
+                in different repositories.
+    Different:  This is opposite of comment 'Same'.
+-----------------------------------------------------------
 
-# Remove this later
-def suppressprocessCmdLine():
-    global repoList, options
-    repoListTemp = []
+%s
 
-    if sys.argv[1] == "--help" or sys.argv[1] == "-h":
-        printHelp(True, 0)
-
-    for arg in sys.argv[1:]:
-        if arg.startswith("--"):
-            ''' This is long option  '''
-            option = arg[arg.rfind("-") + 1:]
-            if option == "usecache": options["uc"] = True
-            elif option == "nomail": options["nm"] = True
-            elif option == "report": options["r"] = True
-            else:
-                printHelp(True)
-        elif arg.startswith("-"):
-            ''' This is short option  '''
-            option = arg[arg.rfind("-") + 1:]
-            if options.has_key(option):
-                options[option] = True
-            else:
-                printHelp(True)
-        else:
-            ''' This is repo url  '''
-            if not arg.endswith(".bz2") and not arg.endswith(".xz"):
-                printHelp(True)
-            repoListTemp.append(arg)
-
-    if options["uc"] and repoListTemp:
-        printHelp(True)
-    else:
-        if options["uc"]:
-            ''' In cache, only .bz2 and .xz files are considered for now  '''
-            for root, dirs, files in os.walk(os.getcwd()):
-                for name in files:
-                    if name.endswith(".bz2") or name.endswith(".xz"):
-                        repoListTemp.append(name)
-                        print repoListTemp
-        elif repoListTemp:
-            repoList = []
-            repoList.extend(repoListTemp)
+-----------------------------------------------------------
+You're getting this e-mail because you have packages in our repositories. If you think you shouldn't receive such e-mail, please contact with %s
+"""
 
 def processCmdLine():
     global options
     args = []
 
-    ''' Initialization option parser object '''
+    ''' Initialization of option parser object '''
     usageStr = "Usage: package-diff-notify [options] [repoURL [repoURL ...]]"
     desStr = "This is a notifier script to give detailed info to packagers about their packages."
     epiStr = "repoURL:\t  compressed pisi-index file path in URL format as xz or bz2"
-    tmp = "Use 'xz' or 'bz2' files."
 
     parser = OptionParser(prog = "package-diff-notify", version = "%prog 1.0", usage = usageStr, description = desStr, epilog = epiStr)
     parser.add_option("-u", "--uselocal", dest = "uselocal", action = "store_true", default = False, help = "use local pisi-index files as xz or bz2. Use without <repoURL>")
     parser.add_option("-n", "--nomail", dest = "nomail", action = "store_true", default = False, help = "prevent the util from sending e-mail to packagers")
     parser.add_option("-r", "--report", dest = "report", action = "store_true", default = False, help = "dump the output into separate files")
 
-    ''' Parsing the command line '''
+    ''' Parse the command line '''
     (options, args) = parser.parse_args()
 
+    ''' Process the command line  '''
     if options.uselocal and args:
         parser.print_help()
         return -1
@@ -141,7 +99,6 @@ def processCmdLine():
                 return -1
         elif args:
             repoList = args
-
 
 ''' This function reads source pisi index file as remote or local and constructs "repos" structure based on this file '''
 def fetchRepos():
@@ -191,7 +148,6 @@ def fetchRepos():
 ''' This function creates a summary entry whose structure is specified below for given repo, say 2009 or 2011  '''
 ''' summaryEntry = [packager_name, packager_mail, release_no, version_no, #package, #patch] '''
 def createSummaryEntry(packager, package, distro):
-    summaryEntry = []
     order = repos[packager][package][3].index(distro)
     summaryEntry = [
                     packager,
@@ -259,46 +215,6 @@ def prepareContentBody(packager):
 
     return content
 
-# Remove this later
-''' This function returns a string including status info about all packages of a packager  '''
-def suppressprepareContentBody(packager):
-    content = "|%s|\n" %(230 * "-")
-    for package in repos[packager].keys():
-        for order, distro in enumerate(repos[packager][package][3]):
-            ''' To prevent repeatation for package name and conflict check comment '''
-            if order == 0:
-                content = "%s|%-50s|" %(content, package)
-
-                ''' Conflict check  '''
-                conflictComment = "No"
-                ''' Check if  there are  multiple packagers maintaining the same package'''
-                if len(conflictList[package]) > 1:
-                    ''' Packager may have more than one e-mail address  '''
-                    tmpConflictList = list(conflictList[package])
-                    for mail in repos[packager][package][4]:
-                        if mail in conflictList[package]:
-                            ''' Excluding packagers' own mail addresses without breaking conflict list '''
-                            tmpConflictList.remove(mail)
-                    if len(tmpConflictList) > 1:
-                        conflictComment = "Yes, please contact with %s" %(tmpConflictList)
-            else:
-                content = "%s|%-50s|" % (content, " ")
-                conflictComment = ""
-
-            ''' Adding a new entry for the package as below '''
-            '''  Distro_name | Release_no | Version_no | #Package | #Patch | Is there conflict? '''
-            content = "%s%-30s|%-10s|%-20s|%-10s|%-10s|%-100s|\n" %(content,
-                                                                distro,
-                                                                repos[packager][package][0][order][0],
-                                                                repos[packager][package][0][order][1],
-                                                                repos[packager][package][1][order],
-                                                                repos[packager][package][2][order],
-                                                                conflictComment)
-
-        content = "%s|%s|\n" %(content, (230 * "-"))
-
-    return content
-
 ''' This function gathers all e-mail addresses a packager specifies in his/her packages '''
 def prepareReceiverMailList(packager):
     mailList = []
@@ -312,44 +228,52 @@ def prepareReceiverMailList(packager):
 
 ''' This function sends mail to the recipient whose details are passed  '''
 def sendMail(receiverList, contentBody):
-    msg = MIMEText("%s\n%s\n%s" % (contentHeader, contentBody.encode("utf-8"), contentFooter))
-    ''' Envelope Information, just to show the e-mail correctly in recipient inbox and not to be marked as spam '''
-    msg["Subject"] = mailSubject
-    msg["From"] = mailSender
+    if not mailSenderUsr or not mailSenderPwd or not mailServer:
+        print "No enough information for connecting and authenticating to SMTP server."
+        return -1
 
     try:
-        for receiver in receiverList:
-            if receiver == "x@pardus.org.tr":
-                smtp = smtplib.SMTP(mailServer)
-                msg["To"] = receiver
-                smtp.ehlo()
-                smtp.starttls()
-                smtp.ehlo()
-                smtp.login(mailSender, mailSenderPwd)
-                smtp.sendmail(mailSender, "gozbulak@pardus.org.tr", msg.as_string())
-                smtp.quit()
-    except Exception:
+        session = smtplib.SMTP(mailServer)
+    except:
+        print "Opening socket to SMTP server failed"
         return -1
+
+    try:
+        session.login(mailSenderUsr, mailSenderPwd)
+    except:
+        print "Authentication to SMTP server failed. Please, check your credentials."
+        return -1
+
+    for receiver in receiverList:
+        if receiver == "x@pardus.org.tr":
+            msg = mailTemplate %(mailSender, mailSenderUsr, receiver, contentBody, mailSenderUsr)
+            print "Sending e-mail to %s..." %(receiver),
+            try:
+                session.sendmail(mailSenderUsr, receiver, msg)
+                print "[OK]"
+            except:
+                print "[FAILED]"
+
+    session.quit()
 
 ''' This function traverses "repos" structure to send e-mail to the packagers about their package(s) status and generate a report if necessary '''
 def traverseRepos():
-    ''' Open report file if report option is set  '''
     for packager in repos.keys():
         contentBody = prepareContentBody(packager)
 
         if not options.nomail:
             receiverMailList = prepareReceiverMailList(packager)
             if sendMail(receiverMailList, contentBody):
-                print "Send mail to %s failed." %(receiverMailList)
+                return -1
 
         if options.report:
-            fp = open(packager, "w")
+            fp = open("_".join(packager.split(" ")), "w")
             fp.write("%s" %(contentBody))
 
 def main():
     if processCmdLine(): return -1
     fetchRepos()
-    traverseRepos()
+    if traverseRepos(): return -1
 
 if __name__ == "__main__":
     sys.exit(main())
