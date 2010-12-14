a9e0677b71a8ea328aa6f07db275ac5f8693ac6f
fatih.arslan <fatih.arslan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-12-14 14:32:43 +0000

    Add edit_grub and intersection functions

--- trunk/playground/fatih.arslan/scripts/get_primary_module.py
+++ trunk/playground/fatih.arslan/scripts/get_primary_module.py
@@ -5,12 +5,60 @@ import os
 import sys
 import glob
 import gzip
+import pisi
+import platform
+
+kernel_version = platform.uname()[2]
 
 sysdir = "/sys/bus/pci/devices/"
 driversDB = "/usr/share/X11/DriversDB"
+grub_file = "/boot/grub/grub.conf"
 
-packages = {"nvidia-current": ("xorg-video-nvidia-current",
-                               "nvidia-settings")}
+packages_a = {"nvidia-current": ("xorg-video-nvidia-current", "nvidia-settings")}
+packages_b = {"fglrx": ("foo", "foo2")}
+packages_c = {"foo": ("boo", "boo1", "boo3")}
+
+def edit_grub(driver_name):
+    if driver_name == "nvidia-current":
+        os_driver = "nouveau"
+    elif driver_name == "fglrx":
+        os_driver = "radeon"
+    else:
+        os_driver = False
+
+    grub_back = open("deneme", "w")
+    with open(grub_file) as grub:
+        for line in grub:
+            if "kernel" in line and kernel_version in line:
+                kernel_parameters = line.split()
+                for param in kernel_parameters:
+                    if "blacklist" in param or not os_driver:
+                        print "Grub.conf is already configured"
+                        grub_back.write(line)
+                        break
+                    elif os_driver:
+                        kernel_parameters.append(" blacklist=%s " % os_driver)
+                        new_kernel_line = "".join(kernel_parameters)
+                        grub_back.write(new_kernel_line)
+                        print "The parameter \"blacklist=%s\" is added to Grub.conf" % os_driver
+                        break
+            else:
+                grub_back.write(line)
+    grub_back.close()
+
+def resolve_intersections(driver_name):
+    obsolote = []
+    needed = []
+
+    packages_dicts = [packages_a, packages_b, packages_c]
+    for package in packages_dicts:
+        for driver, module in package.items():
+            if driver_name != driver:
+                obsolote.append(package)
+            else:
+                needed.append(package)
+
+    return (needed, obsolote)
 
 def get_kernel_module_package(kernel_flavor, name):
     '''Get the appropirate module for the specified kernel'''
@@ -41,7 +89,15 @@ def get_primary_driver():
 if __name__ == '__main__':
     driver_name = get_primary_driver()
     module_name = get_kernel_module_package(None, driver_name)
-    
-    print driver_name, module_name
-    #sys.exit(returnDevName())
+    needed, obsolote = resolve_intersections(driver_name)
+
+    print
+    print "Your driver name is              : %s" % driver_name
+    print "The module for this driver is    : %s" % module_name
+    print
+    print "Packages that shoul be installed : %s" % needed
+    print "Packages that shoul be removed   : %s" % obsolote
+    print
+
+    edit_grub(driver_name)
 
