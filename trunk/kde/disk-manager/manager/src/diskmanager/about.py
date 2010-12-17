05d7d5358440e2c76ee4577e42da528e26928bce
gokmen.goksel <gokmen.goksel@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2010-12-17 12:21:09 +0000

    - Let disk-manager work on 2011

--- trunk/kde/disk-manager/manager/src/diskmanager/about.py
+++ trunk/kde/disk-manager/manager/src/diskmanager/about.py
@@ -11,6 +11,8 @@
 # Please read the COPYING file.
 #
 
+PACKAGE = "Disk Manager"
+
 # PyKDE
 from PyKDE4.kdecore import KAboutData, ki18n, ki18nc
 
@@ -29,7 +31,8 @@ catalog     = appName
 aboutData   = KAboutData(appName, catalog, programName, version, description, license, copyright, text, homePage, bugEmail)
 
 # Author(s)
-aboutData.addAuthor(ki18n("Bahadır Kandemir"), ki18n("Current Maintainer"))
+aboutData.addAuthor(ki18n("Gökmen Göksel"), ki18n("Current Maintainer"))
+aboutData.addAuthor(ki18n("Bahadır Kandemir"), ki18n("First Developer"))
 aboutData.setTranslator(ki18nc("NAME OF TRANSLATORS", "Your names"), ki18nc("EMAIL OF TRANSLATORS", "Your emails"))
 
 # Use this if icon name is different than appName
