39c4ef1ea35f9e0508745c072d109be9ad64affc
mete <mete@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2011-01-09 20:35:23 +0000

    Version bump

--- corporate2/setup.py
+++ corporate2/setup.py
@@ -35,7 +35,7 @@ class Install(install):
 
 
 setup(name="yali-theme-pardus",
-      version= "Corporate2.0.1",
+      version= "Corporate2.0.2",
       description="Pardus theme for YALI (Yet Another Linux Installer)",
       license="GNU GPL2",
       author="Pardus Developers",
