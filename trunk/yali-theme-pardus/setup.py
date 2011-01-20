f154a3f417e1f8b1db1a1ae33f4f784429fde625
mete <mete@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2011-01-20 09:53:14 +0000

    Version bump

--- trunk/yali-theme-pardus/setup.py
+++ trunk/yali-theme-pardus/setup.py
@@ -32,7 +32,7 @@ class Install(install):
 
 
 setup(name="yali-theme-pardus",
-      version= "2011.0.4",
+      version= "2011.0.5",
       description="Pardus theme for YALI (Yet Another Linux Installer)",
       license="GNU GPL2",
       author="Pardus Developers",
