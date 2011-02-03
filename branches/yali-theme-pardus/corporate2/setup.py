d53ea7058cfdacb91a96c70daf7a1fbcacb72738
mete <mete@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2011-02-03 14:41:56 +0000

    Version bump

--- branches/yali-theme-pardus/corporate2/setup.py
+++ branches/yali-theme-pardus/corporate2/setup.py
@@ -35,7 +35,7 @@ class Install(install):
 
 
 setup(name="yali-theme-pardus",
-      version= "Corporate2.0.2",
+      version= "Corporate2.0.3",
       description="Pardus theme for YALI (Yet Another Linux Installer)",
       license="GNU GPL2",
       author="Pardus Developers",
