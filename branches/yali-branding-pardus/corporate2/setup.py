4a3ce0cbafb950ee9db9a9e8bcb24e8a734eab50
mete <mete@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2011-02-16 10:55:59 +0000

    Version bump

--- branches/yali-branding-pardus/corporate2/setup.py
+++ branches/yali-branding-pardus/corporate2/setup.py
@@ -57,7 +57,7 @@ class UpdatePO(Command):
         os.chdir("..")
 
 setup(name="yali-branding-pardus",
-      version= "Corporate2.0.8",
+      version= "Corporate2.0.9",
       description="Pardus branding files for YALI (Yet Another Linux Installer)",
       license="GNU GPL2",
       author="Pardus Developers",
