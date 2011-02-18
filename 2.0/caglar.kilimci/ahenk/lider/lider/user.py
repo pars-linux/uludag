9f9db4899adef1d59efa376f1b5665abf9cbe31b
caglar.kilimci <caglar.kilimci@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2011-02-18 12:44:28 +0000

    Control of password, matches or not.

--- 2.0/caglar.kilimci/ahenk/lider/lider/user.py
+++ 2.0/caglar.kilimci/ahenk/lider/lider/user.py
@@ -36,11 +36,22 @@ class DialogUser(QtGui.QDialog, Ui_dialogUser):
         # Attach generated UI
         self.setupUi(self)
 
-        self.connect(self.editName, QtCore.SIGNAL("textChanged(QString)"), self.__slotTextChanged)
+        self.connect(self.editName, QtCore.SIGNAL("textChanged(QString)"), self.__slotNameTextChanged)
+        self.connect(self.editPassword, QtCore.SIGNAL("textChanged(QString)"), self.__slotPasswordTextChanged)
+        self.connect(self.editConfirmPassword, QtCore.SIGNAL("textChanged(QString)"), self.__slotPasswordTextChanged)
 
-    def __slotTextChanged(self):
+        self.labelWarning.hide()
+
+    def __slotNameTextChanged(self):
         if not (self.editHomeDir.isModified()):
             self.editHomeDir.setText(self.editName.text())
+
+    def __slotPasswordTextChanged(self):
+        if len(str(self.editConfirmPassword.text())):
+            if self.editPassword.text() == self.editConfirmPassword.text():
+                self.labelWarning.hide()
+            else:
+                self.labelWarning.show()
 
     def get_name(self):
         """
@@ -92,3 +103,11 @@ class DialogUser(QtGui.QDialog, Ui_dialogUser):
             Sets group id.
         """
         self.spinGID.setValue(uid)
+
+    def accept(self):
+        if not (self.editPassword.text() == self.editConfirmPassword.text()):
+            print "Passwords do not match."
+            QtGui.QMessageBox.warning(self, "User Adding..", "Passwords do not match.")
+            return
+        else:
+            QtGui.QDialog.accept(self)
