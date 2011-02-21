6bf19f69114b2577d5cb85c445109a1e82a4263a
Bahadır Kandemir <kandemir@gmail.com>
2011-02-21 12:13:38 +0000

    Merged Beyza's changes

--- 2.0/bahadir.kandemir/ahenk/lider/lider/formmain.py
+++ 2.0/bahadir.kandemir/ahenk/lider/lider/formmain.py
@@ -668,8 +668,9 @@ class FormMain(QtGui.QWidget, Ui_FormMain):
         if dialog.exec_():
             name = dialog.get_name()
             label = dialog.get_label()
+            description = dialog.get_description()
             try:
-                self.directory.add_folder(parent_path, name, label)
+                self.directory.add_folder(parent_path, name, label, description)
             except directory.DirectoryConnectionError:
                 self.__update_status("directory", "error")
                 # TODO: Disconnect
