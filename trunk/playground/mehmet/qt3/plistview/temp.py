240cb79db92d517a4663b1e498e74b20724937a6
mehmet.ozdemir <mehmet.ozdemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2011-02-23 22:21:58 +0000

    * check box and group button widgets were added * set hover, selected and base colors of list view items'

--- trunk/playground/mehmet/qt3/plistview/temp.py
+++ trunk/playground/mehmet/qt3/plistview/temp.py
@@ -19,17 +19,23 @@ def loadIcon(name, group=KIcon.Desktop, size=16):
 def getIconSet(name, group=KIcon.Toolbar):
     return KGlobal.iconLoader().loadIconSet(name, group)
 
-
+# !!!!!!!!!!! viewportun dışında yapışık bi header olsa iyi olur
+# !!!! butonlar hover olunca gözüksün seçeneği ekle
 class PListView(QScrollView):
-    itemHeight = 20
+    itemHeight = 24
     iconHeight = 16
     arrowSize = 8
     def __init__(self, parent):
         QScrollView.__init__(self, parent)
-        self.viewport().setPaletteBackgroundColor(QColor(255,255,255))
         self.parent = parent
         self.items = []
         self.selectedItem = None
+        self.hoverItem = None
+
+        self.baseColor = KGlobalSettings.baseColor()
+        self.selectedColor = KGlobalSettings.highlightColor()
+        self.hoverColor = KGlobalSettings.buttonBackground()
+        self.viewport().setPaletteBackgroundColor(self.baseColor)
 
     def clear(self):
         #çocukları da silinecek !!!!!!!!!! böyleyken hepsini siliyo işte
@@ -70,6 +76,8 @@ class PListViewItem(QWidget):
 
     PLVIconButtonType = 1
     PLVRadioButtonType = 2
+    PLVCheckBoxType = 3
+    PLVButtonGroupType = 4
 
     widgetSpacing = 2
 
@@ -87,31 +95,41 @@ class PListViewItem(QWidget):
         self.nextItem = None
         self.firstChild = None
 
-        self.baseColor = QColor(200,200,200)
-        #self.baseColor = KGlobalSettings.baseColor()
-
-        #self.widget = QPushButton("Radio", self)
         self.icon = QPixmap("/usr/share/icons/BCTango/16x16/categories/package_network_www.png")
 
-        self.fillColor = QColor(25,255,255)
-        #self.fillColor = KGlobalSettings.buttonBackground()
         self.installEventFilter(self)
         self.show()
 
+    def findDepth(self):
+        parent = self.parent
+
+    def resetOldSelected(self, item):
+        item.isSelected = False
+        item.repaint()
+
     def eventFilter(self, target, event):
-        if(event.type()==QEvent.MouseButtonPress):
+        if(event.type() == QEvent.MouseButtonPress):
+            if not self.parent.selectedItem == self:
+                if self.parent.selectedItem:
+                    self.resetOldSelected(self.parent.selectedItem)
+                self.parent.selectedItem = self
+                self.isSelected = True
+                self.repaint()
+        elif (event.type() == QEvent.MouseButtonDblClick):
+            print 'collapse or expand'
+        elif (event.type() == QEvent.MouseButtonRelease):
             pass
-        elif(event.type()==QEvent.MouseButtonRelease):
-            pass
-        elif(event.type()==QEvent.Enter):
-            pass
-        elif(event.type()==QEvent.Leave):
-            pass
+        elif (event.type() == QEvent.Enter):
+            self.parent.hoverItem = self
+            self.repaint()
+        elif (event.type() == QEvent.Leave):
+            self.parent.hoverItem = None
+            self.repaint()
         return False
 
-    def setWidgetsBg(self):
+    def setWidgetsBg(self, color):
         for w in self.widgets:
-            w.setPaletteBackgroundColor(self.fillColor)
+            w.setPaletteBackgroundColor(color)
 
     def setWidgetsGeometry(self, width, height):
         if len(self.widgets):
@@ -123,15 +141,20 @@ class PListViewItem(QWidget):
 
     def paintEvent(self, event):
         paint = QPainter(self)
-        col = QColor(200,200,200)
-        paint.fillRect(event.rect(), QBrush(self.fillColor))
-        if len(self.widgets):
-            self.setWidgetsBg()
+
+        color = self.parent.baseColor
+        if self.isSelected:
+            color = self.parent.selectedColor
+        elif self.parent.hoverItem == self:
+            color = self.parent.hoverColor
+        paint.fillRect(event.rect(), QBrush(color))
+        if len(self.widgets) > 0:
+            self.setWidgetsBg(color)
 
         dip = (self.height() - self.icon.height()) / 2
         paint.drawPixmap(2 + PListView.arrowSize + 6, dip, self.icon)
 
-        col = QColor(20,20,20)
+        col = QColor(0,0,0)
         self.setPaletteBackgroundColor(col)
         arr = QPointArray(3)
         top = (self.height() - PListView.arrowSize) / 2
@@ -166,21 +189,49 @@ class PListViewItem(QWidget):
 
         w = 1
         h = max(fm.height(), PListView.itemHeight)
-        return QSize(w, 80)
+        return QSize(w, h)
 
     def addWidgetItem(self, type, args=None):
         if type == self.PLVIconButtonType:
-            self.addPLVIconButton(args)
+            return self.addPLVIconButton(args, self)
         elif type == self.PLVRadioButtonType:
-            self.addPLVRadioButton(args)
+            return self.addPLVRadioButton(args, self)
+        elif type == self.PLVCheckBoxType:
+            return self.addPLVCheckBox(args, self)
+        elif type == self.PLVButtonGroupType:
+            return self.addPLVButtonGroup(args, self)
 
-    def addPLVIconButton(self, args):
-        plvib = PLVIconButton(self, args)
-        self.widgets.append(plvib)
+    def addPLVIconButton(self, args, parent):
+        plvib = PLVIconButton(parent, args)
+        if parent == self:
+            self.widgets.append(plvib)
+        return plvib
 
-    def addPLVRadioButton(self, args):
-        plvrb = PLVRadioButton(self, args)
-        self.widgets.append(plvrb)
+    def addPLVRadioButton(self, args, parent):
+        plvrb = PLVRadioButton(parent, args)
+        if parent == self:
+            self.widgets.append(plvrb)
+        return plvrb
+
+    def addPLVCheckBox(self, args, parent):
+        plvrb = PLVCheckBox(parent, args)
+        if parent == self:
+            self.widgets.append(plvrb)
+        return plvrb
+
+    def addPLVButtonGroup(self, args, parent):
+        plvbg = PLVButtonGroup(parent, args)
+        buttonList = []
+        blist = args[0]
+        for type in blist:
+            if type == self.PLVIconButtonType:
+                buttonList.append(plvbg.layout().addWidget(self.addPLVIconButton(args, args[1][args[0].index(type)], plvbg)))
+            elif type == self.PLVRadioButtonType:
+                buttonList.append(plvbg.layout().addWidget(self.addPLVRadioButton(args, plvbg)))
+            elif type == self.PLVCheckBoxType:
+                buttonList.append(plvbg.layout().addWidget(self.addPLVCheckBox(args, plvbg)))
+        self.widgets.append(plvbg)
+        return [plvbg, buttonList]
 
 class PLVIconButton(QPushButton):
     def __init__(self, parent, args):
@@ -196,10 +247,20 @@ class PLVIconButton(QPushButton):
 class PLVRadioButton(QRadioButton):
     def __init__(self, parent, args):
         QRadioButton.__init__(self, parent)
-        #self.myWidth = size.width()
-        #self.myHeight = size.height()
         self.resize(16, 16)
 
+class PLVCheckBox(QCheckBox):
+    def __init__(self, parent, args):
+        QCheckBox.__init__(self, parent)
+        self.resize(20, 20)
+
+class PLVButtonGroup(QButtonGroup):
+    buttonSpacing = 8
+    def __init__(self, parent, args):
+        QButtonGroup.__init__(self, parent)
+        layout = QHBoxLayout(self)
+        self.resize((16+self.buttonSpacing)*len(args[0]), 16)
+        self.setFrameShape(QButtonGroup.NoFrame)
 
 
 
