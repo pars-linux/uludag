c0a619ca9dcb8d2ea4aad691f52a755c2e8bd4b1
mehmet.ozdemir <mehmet.ozdemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2011-03-13 00:11:00 +0000

    * PLabelEdit was added for Qt4

---
+++ playground/mehmet/qt4/app.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import re
+import time
+
+from PyQt4 import QtGui
+from PyQt4 import QtCore
+
+import sys
+import dbus
+
+
+from PyKDE4.kdeui import KMainWindow, KApplication, KCModule, KIcon
+from PyKDE4.kdecore import KCmdLineArgs, KGlobal
+from PyKDE4.kdecore import KAboutData, ki18n, ki18nc
+
+from peditlabel import PEditLabel
+
+PACKAGE = "Sample"
+appName     = "sample"
+modName     = "sample"
+programName = ki18n(PACKAGE)
+version     = "1.0.0"
+description = ki18n(PACKAGE)
+license     = KAboutData.License_GPL
+copyright   = ki18n("(c) 2006-2011 TUBITAK/UEKAE")
+text        = ki18n(None)
+homePage    = "http://developer.pardus.org.tr/projects/sample"
+bugEmail    = "bugs@pardus.org.tr"
+catalog     = appName
+aboutData   = KAboutData(appName, catalog, programName, version, description, license, copyright, text, homePage, bugEmail)
+aboutData.setProgramIconName("computer")
+
+
+class MainWindow(KMainWindow):
+    def __init__(self, parent=None):
+        KMainWindow.__init__(self, parent)
+        widget = PEditLabel(self, "deneme")
+        self.resize(widget.size())
+        self.setCentralWidget(widget)
+
+
+if __name__ == "__main__":
+
+    KCmdLineArgs.init(sys.argv, aboutData)
+    app = KApplication()
+
+    if not dbus.get_default_main_loop():
+        from dbus.mainloop.qt import DBusQtMainLoop
+        DBusQtMainLoop(set_as_default=True)
+
+    window = MainWindow()
+    window.show()
+
+    app.exec_()
+
+
+class Module(KCModule):
+    def __init__(self, component_data, parent):
+        KCModule.__init__(self, component_data, parent)
+
+        KGlobal.locale().insertCatalog(catalog)
+
+        if not dbus.get_default_main_loop():
+            from dbus.mainloop.qt import DBusQtMainLoop
+            DBusQtMainLoop(set_as_default=True)
+
+        MainWidget(self, embed=True)
+
+
+def CreatePlugin(widget_parent, parent, component_data):
+    return Module(component_data, parent)
+
+
