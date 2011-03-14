760b0a9eda7cb01771b88d61523a5de252c329c6
mehmet.ozdemir <mehmet.ozdemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2011-03-14 14:53:30 +0000

    * parent layout placements were handled (resizing the parent widget causes text change and maybe ... would be added etc.)

--- trunk/playground/mehmet/qt4/peditlabel.py
+++ trunk/playground/mehmet/qt4/peditlabel.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from PyQt4 import QtGui
+from PyQt4.QtGui import *
 from PyQt4 import QtCore
 from PyQt4.QtCore import *
 
@@ -11,11 +12,10 @@ from PyKDE4 import kdecore
 class PEditLabelIcon(QtGui.QLabel):
     def __init__(self, parent):
         QtGui.QLabel.__init__(self, parent)
+        self.parent = parent
         self.readOnlyIcon = "/usr/share/icons/oxygen/16x16/actions/edit-rename.png"
         self.editIcon = "/usr/share/icons/oxygen/16x16/actions/dialog-ok-apply.png"
         self.setIcon(self.readOnlyIcon)
-
-        self.parent = parent
 
         self.setMaximumSize(QtCore.QSize(self.pixmap().width(), self.pixmap().height()))
         self.setMinimumSize(QtCore.QSize(self.pixmap().width(), self.pixmap().height()))
@@ -41,14 +41,17 @@ class PEditLabelLabel(QtGui.QLabel):
     def __init__(self, parent, text):
         QtGui.QLabel.__init__(self, text, parent)
         self.parent = parent
+        self.setSizePolicy(QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Minimum))
 
+        self.installEventFilter(self)
+        self.updateSize()
+
+    def updateSize(self):
         width = self.fontMetrics().width(self.text())+2
         height = self.fontMetrics().height()+4
 
         self.setMaximumSize(QtCore.QSize(width, height))
         self.setMinimumSize(QtCore.QSize(width, height))
-
-        self.installEventFilter(self)
 
     def setText(self, text):
         QtGui.QLabel.setText(self, text)
@@ -71,6 +74,8 @@ class PEditLabelLineEdit(QtGui.QLineEdit):
     def __init__(self, parent):
         QtGui.QLineEdit.__init__(self, parent)
         self.parent = parent
+        self.setSizePolicy(QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Minimum))
+
         self.updateSize()
 
         self.installEventFilter(self)
@@ -78,9 +83,15 @@ class PEditLabelLineEdit(QtGui.QLineEdit):
 
     def editTextChanged(self, text):
         self.updateSize()
+        self.parent.updateSize()
 
     def updateSize(self):
         width = self.fontMetrics().width(self.text())+PEditLabelLineEdit.extraWidth
+        #if self.parent.width() <= width+2+self.parent.icon.width(): # !!!!!!!! hata veriyor, icon yok diyor
+        if self.parent.width() <= width+32:
+            width = self.parent.width()-32
+            if width <= 0:
+                width = PEditLabelLineEdit.extraWidth
         self.setMaximumSize(QtCore.QSize(width,self.height()))
         self.setMinimumSize(QtCore.QSize(width,self.height()))
 
@@ -89,7 +100,7 @@ class PEditLabelLineEdit(QtGui.QLineEdit):
         self.updateSize()
 
     def eventFilter(self, target, event):
-        if(event.type() == QtCore.QEvent.KeyPress):
+        if event.type() == QtCore.QEvent.KeyPress:
             if event.key() == QtCore.Qt.Key_Return or event.key() == QtCore.Qt.Key_Enter:
                 self.parent.endEditing()
         return False
@@ -99,37 +110,86 @@ class PEditLabelLineEdit(QtGui.QLineEdit):
 
 class PEditLabel(QtGui.QWidget):
     def __init__(self, parent, text):
+        self.parent = parent
         QtGui.QWidget.__init__(self, parent)
         layout = QtGui.QHBoxLayout(self)
-        layout.setSpacing(2)
-        self.label = PEditLabelLabel(self, text)
+        layout.setSpacing(0)
+        #self.setSizePolicy(QtGui.QSizePolicy(QtGui.QSizePolicy., QtGui.QSizePolicy.Minimum))
+        self.label = PEditLabelLabel(self, "")
         layout.addWidget(self.label)
         self.edit = PEditLabelLineEdit(self)
         self.edit.hide()
         layout.addWidget(self.edit)
         self.icon = PEditLabelIcon(self)
         layout.addWidget(self.icon)
-        layout.insertStretch(3)
+        layout.addWidget(QLabel("", self))
+        #layout.insertStretch(-1)
 
-        #self.label.setStyleSheet( "background-color: rgb( 128,128,0 )" )
-        #self.icon.setStyleSheet( "background-color: rgb( 128,128,128 )" )
-        #self.setStyleSheet( "background-color: rgb( 128,18,18 )" )
+        self.storedText = text
+
+        self.label.setStyleSheet("background-color: rgb( 128,128,0 )")
+        self.icon.setStyleSheet("background-color: rgb( 28,128,128 )")
+        self.setStyleSheet("background-color: rgb( 255,255,255 )")
 
         #self.resize(QtCore.QSize(400,400))
+        self.updateSize()
+
+    def updateSize(self):
+        pass
+        #self.setMinimumSize(QtCore.QSize(self.edit.width()+self.icon.width(), self.edit.height()+4))
+        #self.setMaximumSize(QtCore.QSize(self.edit.width()+self.icon.width(), self.edit.height()+4))
 
     def startEditing(self):
         self.icon.setIcon(self.icon.editIcon)
         self.label.hide()
-        self.edit.setText(self.label.text())
+        self.edit.setText(self.storedText)
         self.edit.show()
 
     def endEditing(self):
         self.icon.setIcon(self.icon.readOnlyIcon)
         self.edit.hide()
-        self.label.setText(self.edit.text())
+        self.edit.clearFocus()
+        self.storedText = self.edit.text()
+        self.label.setText(self.cropEditText())
         self.label.show()
+
+    def cropEditText(self):
+        textWidth = self.label.fontMetrics().width(self.edit.text())
+        if textWidth + 32 > self.width():
+            r1 = float(self.width())/(textWidth+32)
+            r2 = int(float(len(self.edit.text()))*r1)-3
+            return self.edit.text()[:r2]+"..."
+        else:
+            return self.edit.text()
+
+    def setText(self, text):
+        self.label.setText(text)
+
+    def text(self):
+        return self.label.text()
 
     def sizeHint(self):
         return QtCore.QSize(self.label.width()+self.icon.width(), self.label.height())
 
+    def resizeEvent(self, event):
+        print event.oldSize()
+        print event.size()
+        print ''
+        #self.label.updateSize()
+        self.label.setText(self.cropLabelText())
+        return QWidget.resizeEvent(self, event)
+
+    def cropLabelText(self):
+        if not self.label.text():
+            if not self.storedText:
+                raise
+            else:
+                self.label.setText(self.storedText)
+        textWidth = self.label.fontMetrics().width(self.label.text())
+        if textWidth + 32 > self.width():
+            r1 = float(self.width())/(textWidth+32)
+            r2 = int(float(len(self.label.text()))*r1)-3
+            return self.label.text()[:r2]+"..."
+        else:
+            return self.label.text()
 
