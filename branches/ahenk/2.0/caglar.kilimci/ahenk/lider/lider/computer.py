de9c9287575a3db3fc5c33a185740ef7016495bd
caglar.kilimci <caglar.kilimci@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2011-03-16 07:52:50 +0000

    issue 355 applied.

--- branches/ahenk/2.0/caglar.kilimci/ahenk/lider/lider/computer.py
+++ branches/ahenk/2.0/caglar.kilimci/ahenk/lider/lider/computer.py
@@ -35,6 +35,23 @@ class DialogComputer(QtGui.QDialog, Ui_dialogComputer):
 
         # Attach generated UI
         self.setupUi(self)
+        # Signals for password matching control
+        self.connect(self.editPassword, QtCore.SIGNAL("textChanged(QString)"), self.__slotPasswordTextChanged)
+        self.connect(self.editConfirmPassword, QtCore.SIGNAL("textChanged(QString)"), self.__slotPasswordTextChanged)
+
+        self.labelWarning.hide()
+
+
+    def __slotPasswordTextChanged(self):
+        """
+            If password confirmation field is not empty, compare password and its confirmation field whether same or not.
+            If they are not same, show warning label.
+        """
+        if len(str(self.editConfirmPassword.text())):
+            if self.editPassword.text() == self.editConfirmPassword.text():
+                self.labelWarning.hide()
+            else:
+                self.labelWarning.show()
 
     def get_name(self):
         """
@@ -51,6 +68,13 @@ class DialogComputer(QtGui.QDialog, Ui_dialogComputer):
         salted = Directory.make_password(password)
         return salted
 
+    def get_description(self):
+        """
+            Returns LDAP attribute name.
+        """
+        return str(self.editDescription.text())
+
+
     def set_name(self, user):
         """
             Sets computer name.
@@ -62,3 +86,18 @@ class DialogComputer(QtGui.QDialog, Ui_dialogComputer):
             Sets password.
         """
         self.editPassword.setText(password)
+
+    def set_description(self, password):
+        """
+            Sets LDAP attribute name.
+        """
+        self.editDescription.setText(password)
+
+    def accept(self):
+        """
+            Checks written passwords are same or not. If not, warn user.
+        """
+        if not (self.editPassword.text() == self.editConfirmPassword.text()):
+            QtGui.QMessageBox.warning(self, "Computer Adding..", "Passwords do not match.")
+        else:
+            QtGui.QDialog.accept(self)
