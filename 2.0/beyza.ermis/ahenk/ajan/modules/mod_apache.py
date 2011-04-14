14cdd454398046ef7974f4b770e16b50ed38d881
beyza.ermis <beyza.ermis@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2011-04-14 13:51:49 +0000

    Changes applied to apache module

--- 2.0/beyza.ermis/ahenk/ajan/modules/mod_apache.py
+++ 2.0/beyza.ermis/ahenk/ajan/modules/mod_apache.py
@@ -15,3 +15,18 @@ def process(message, options):
     if message.type == "policy":
         apacheDomains = message.policy.get("apacheDomains", [])
         print "Domainler:", apacheDomains
+
+        documentRoot = message.policy.get("documentRoot", [])
+        print "Root:", documentRoot
+
+        ip = message.policy.get("ip", [])
+        print "IP:", ip
+
+        port = message.policy.get("port", [])
+        print "Port:", port
+
+        serverAdmin = message.policy.get("serverAdmin", [])
+        print "Server Admin:", serverAdmin
+
+        serverName = message.policy.get("serverName", [])
+        print "Server Name:", serverName
