ec1605838d9fbd5ba3c3cc94baf2baf42170a34e
mehmet.ozdemir <mehmet.ozdemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2011-04-25 12:55:16 +0000

    remove comments

--- trunk/playground/mehmet/build/hello.cpp
+++ trunk/playground/mehmet/build/hello.cpp
@@ -4,12 +4,9 @@
 using namespace std;
 
 void print_hello(){
-       cout << "Hello World!";
-       cout << "Hello Bot!";
-       cout << "this must fail";
-
-        #cc
-#fdf
+    cout << "Hello World!";
+    cout << "Hello Bot!";
+    cout << "this must fail";
 
 }
 
