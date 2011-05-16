3ca7407f37f0771425c48f5f14b95c416a19b41e
mehmet.ozdemir <mehmet.ozdemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2011-05-16 21:21:55 +0000

    add execution options

--- trunk/playground/mehmet/doc.sh
+++ trunk/playground/mehmet/doc.sh
@@ -1,26 +1,9 @@
 #!/bin/bash
 
-# This script does svn chekout or update for the given projects to its
+# This script doesi an svn chekout or update for the given projects to its
 # directory and creates html doc files. Then moves these html files to
 # the given directory.
 
-# download below files to the workspace
-# svn export http://svn.pardus.org.tr/uludag/trunk/playground/mehmet/templates
-# svn export http://svn.pardus.org.tr/uludag/trunk/playground/mehmet/static
-
-#### SETTINGS ######
-#
-# CHANGE THIS
-
-# html files, index.html vs., will be moved there
-moveDir="/home/mehmet/yy/projects"
-####################
-
-
-if [ ! -d $moveDir ]
-then
-    mkdir $moveDir
-fi
 
 # base url for projects
 baseUrl="http://svn.pardus.org.tr/uludag/trunk/kde/"
@@ -33,55 +16,117 @@ projectSubDir="/manager"
 # this script's directory. svn checkout/update will be made here.
 rootDir=`pwd`
 
-for project in "${projects[@]}"
-do
-    cd $rootDir
+
+function showUsage {
+
+    echo -e "\tUsage: `./doc.sh` options (-f -dDirectory)"
+    echo -e "\twhere directory is destination directory for moving files: Default is \".output\""
+    echo -e "\tand f option forces a rebuild even if there is no change in repository."
+
+}
+
+
+function checkTemplate {
 
     if [ ! -d $rootDir/templates/ ]
     then
-        echo "Error: No templates directory found!"
-        exit 1
+        svn export http://svn.pardus.org.tr/uludag/trunk/playground/mehmet/templates
     fi
 
     if [ ! -d $rootDir/static/ ]
     then
-        echo "Error: No static directory found!"
+        svn export http://svn.pardus.org.tr/uludag/trunk/playground/mehmet/static
+    fi
+
+}
+
+
+function createDoc {
+
+    checkTemplate
+
+    for project in "${projects[@]}"
+    do
+        cd $rootDir
+
+        if [ ! -d $project ]
+        then
+            svn co $baseUrl$project
+        else
+            cd $project
+            currentRevision=`svn info | grep "Revision:" | cut -d: -f2`
+            svn up
+            newRevision=`svn info | grep "Revision:" | cut -d: -f2`
+            if [ $newRevision -eq $currentRevision ]
+            then
+                if [ "$2" != "yes" ]
+                then
+                    echo -e "\tInformation: Revision is not changed, no build is needed!"
+                    exit 0
+                fi
+            fi
+            cd ..
+        fi
+
+        if [ -d $project ]
+        then
+            cd $project$projectSubDir
+            python setup.py build kde4
+            cd doc
+            cp $rootDir/templates/* *_templates
+            cp $rootDir/static/* *_static
+            make html
+            echo $?
+            # is it enough to move just html dir?
+            if [ ! -d $mvDir/$project ]
+            then
+                mkdir $mvDir/$project
+                if [ ! $? -eq 0 ]
+                then
+                    # user didn't write absolute path
+                    mvDir=$rootDir/$mvDir
+                    mkdir $mvDir/$project
+                fi
+            else
+                rm -rf $mvDir/$project/*
+            fi
+            mv *_build/html/* $mvDir/$project
+        fi
+
+    done
+
+}
+
+
+#NO_ARGS=0
+#E_OPTERROR=85
+
+mvDir="/home/mehmet/developerdeneem/output"
+wantForce="no"
+
+
+while getopts ":d:f" Option
+do
+  case $Option in
+    d ) mvDir=$OPTARG;;
+    f ) wantForce="yes";;
+    * ) showUsage;;
+  esac
+done
+
+
+if [ ! -d $mvDir ]
+then
+    mkdir $mvDir
+    if [ ! $? -eq 0 ]
+    then
+        echo -e "\tError: Write absoute path for movedir."
         exit 1
     fi
+fi
 
-    if [ ! -d $project ]
-    then
-        svn co $baseUrl$project
-    else
-        cd $project
-        currentRevision=`svn info | grep "Revision:" | cut -d: -f2`
-        svn up
-        newRevision=`svn info | grep "Revision:" | cut -d: -f2`
-        if [ $newRevision -eq $currentRevision ]
-        then
-            echo "aynii"
-            exit 0
-        fi
-        cd ..
-    fi
 
-    if [ -d $project ]
-    then
-        cd $project$projectSubDir
-        python setup.py build
-        cd doc
-        cp $rootDir/templates/* *_templates
-        cp $rootDir/static/* *_static
-        make html
-        echo $?
-        # is it enough to move just html dir?
-        if [ ! -d $moveDir/$project ]
-        then
-            mkdir $moveDir/$project
-        else
-            rm -rf $moveDir/$project/*
-        fi
-        mv *_build/html/* $moveDir/$project
-    fi
+createDoc $mvDir $wantForce
 
-done
+exit 0
+
