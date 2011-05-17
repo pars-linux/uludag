c9ef8c886033d3f233eda0fbc297a0edf5b10144
gokmen.goksel <gokmen.goksel@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2011-05-17 21:14:25 +0000

    - GUI for Upgrade Manager is almost complete, we just need to integrate Onur's upgrade scripts in it \o/

--- trunk/playground/gokmen/um/gui.py
+++ trunk/playground/gokmen/um/gui.py
@@ -14,12 +14,83 @@
 
 import sys
 
+from PyQt4.QtCore import SIGNAL
 from PyQt4.QtGui import QWidget
-from ui.ui_main import Ui_UpgradeManager
+from ui import ui_main
+from ui import ui_mainscreen
+from ui import ui_screen_1
+from ui import ui_screen_2
+from ui import ui_screen_3
 
+from pds.gui import PMessageBox
+from pds.gui import TOPCENTER, MIDCENTER, BOTCENTER
+from pds.qpagewidget import QPageWidget
 from backend import Iface
 
-class UmGui(QWidget, Ui_UpgradeManager):
+def getWidget(page = None, title = ""):
+    widget = QWidget()
+    widget.title = title
+    if page:
+        page = page()
+        page.setupUi(widget)
+    return widget
+
+class UmMainScreen(QWidget, ui_mainscreen.Ui_UpgradeManager):
+
+    def __init__(self, parent = None):
+        QWidget.__init__(self, parent)
+        self.setupUi(self)
+
+        self.iface = Iface(self)
+
+        self.msgbox = PMessageBox(self)
+        self.msgbox.busy.setColor("black")
+
+        pageWidget = QPageWidget(self.widget_screens)
+        self.layout.addWidget(pageWidget)
+
+        self.button_next.clicked.connect(pageWidget.next)
+        self.button_previous.clicked.connect(pageWidget.prev)
+
+        # Update Page Title
+        self.connect(pageWidget, SIGNAL("currentChanged()"), lambda:\
+                    self.label_header.setText(pageWidget.getCurrentWidget().title))
+
+        # Welcome
+        pageWidget.createPage(
+                getWidget(ui_screen_1.Ui_Screen, "Welcome to Upgrade Manager..."))
+
+        # Repo Selection
+        pageWidget.createPage(
+                getWidget(ui_screen_2.Ui_Screen, "Select Upgrade Repository..."))
+
+        # Dummy Page
+        pageWidget.createPage(
+                getWidget(), inMethod = self.checkSystem)
+
+    def checkSystem(self):
+        self.button_next.setEnabled(False)
+        self.button_previous.setEnabled(False)
+        self.showMessage("Checking your system...")
+
+    def showMessage(self, message):
+        self.msgbox.busy.busy()
+        self.msgbox.setMessage(message)
+        self.msgbox.animate(start = TOPCENTER, stop = MIDCENTER)
+
+    def hideMessage(self):
+        if self.msgbox.isVisible():
+            self.msgbox.animate(start = CURRENT, stop = BOTCENTER, direction = OUT)
+
+    def updateProgress(self, package, downloaded, total, symbol, percent):
+        print package, downloaded, total, symbol, percent
+        self.progress.setValue(percent)
+        if percent == 100:
+            self.label.setText("Download Complete, please press Exit.")
+            self.exitButton.show()
+            self.progress.hide()
+
+class UmGui(QWidget, ui_main.Ui_UpgradeManager):
 
     def __init__(self, parent = None):
         QWidget.__init__(self, parent)
