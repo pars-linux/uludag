5344d3a7506d849262e0327de37987980446586c
Gokmen Goksel <gokmen@goksel.me>
2011-05-18 10:31:56 +0000

    - Find missing packages script from Onur implemented to the GUI \o/

--- playground/gokmen/um/gui.py
+++ playground/gokmen/um/gui.py
@@ -14,7 +14,7 @@
 
 import sys
 
-from PyQt4.QtCore import SIGNAL
+from PyQt4.QtCore import SIGNAL, QTimer
 from PyQt4.QtGui import QWidget
 from ui import ui_main
 from ui import ui_mainscreen
@@ -22,17 +22,26 @@ from ui import ui_screen_1
 from ui import ui_screen_2
 from ui import ui_screen_3
 
+from pds.thread import PThread
 from pds.gui import PMessageBox
-from pds.gui import TOPCENTER, MIDCENTER, BOTCENTER
+from pds.gui import TOPCENTER, MIDCENTER, BOTCENTER, CURRENT, OUT
 from pds.qpagewidget import QPageWidget
+
 from backend import Iface
+from backend import threaded
+from repo_helper import findMissingPackagesForDistupdate
+
+ARA_FORM      = "http://cekirdek.pardus.org.tr/~onur/2009to2011/packages/pisi-index.xml.bz2"
+REPO_TEMPLATE = "http://packages.pardus.org.tr/pardus/2011/%s/i686/pisi-index.xml.xz"
+FORCE_INSTALL = "http://svn.pardus.org.tr/uludag/trunk/pardus-upgrade/2009_to_2011.list"
 
 def getWidget(page = None, title = ""):
     widget = QWidget()
     widget.title = title
     if page:
-        page = page()
+        page = page.Ui_Screen()
         page.setupUi(widget)
+        widget.ui = page
     return widget
 
 class UmMainScreen(QWidget, ui_mainscreen.Ui_UpgradeManager):
@@ -41,46 +50,77 @@ class UmMainScreen(QWidget, ui_mainscreen.Ui_UpgradeManager):
         QWidget.__init__(self, parent)
         self.setupUi(self)
 
+        self.target_repo = REPO_TEMPLATE % 'stable'
         self.iface = Iface(self)
 
         self.msgbox = PMessageBox(self)
-        self.msgbox.busy.setColor("black")
+        self.msgbox.setStyleSheet(PMessageBox.Style)
+        self.msgbox.enableOverlay()
 
-        pageWidget = QPageWidget(self.widget_screens)
-        self.layout.addWidget(pageWidget)
+        self.thread = PThread(self, self.findMissingPackages, self.showResults)
 
-        self.button_next.clicked.connect(pageWidget.next)
-        self.button_previous.clicked.connect(pageWidget.prev)
+        self.pageWidget = QPageWidget(self.widget_screens)
+        self.layout.addWidget(self.pageWidget)
+
+        self.button_next.clicked.connect(self.pageWidget.next)
+        self.button_previous.clicked.connect(self.pageWidget.prev)
 
         # Update Page Title
-        self.connect(pageWidget, SIGNAL("currentChanged()"), lambda:\
-                    self.label_header.setText(pageWidget.getCurrentWidget().title))
+        self.connect(self.pageWidget, SIGNAL("currentChanged()"), lambda:\
+                    self.label_header.setText(self.pageWidget.getCurrentWidget().title))
 
         # Welcome
-        pageWidget.createPage(
-                getWidget(ui_screen_1.Ui_Screen, "Welcome to Upgrade Manager..."))
+        self.pageWidget.createPage(
+                getWidget(ui_screen_1, "Welcome to Upgrade Manager..."))
 
         # Repo Selection
-        pageWidget.createPage(
-                getWidget(ui_screen_2.Ui_Screen, "Select Upgrade Repository..."))
+        self.pageWidget.createPage(
+                getWidget(ui_screen_2, "Select Upgrade Repository..."))
 
         # Dummy Page
-        pageWidget.createPage(
-                getWidget(), inMethod = self.checkSystem)
+        self.pageWidget.createPage(
+                getWidget(ui_screen_3, "Checking your system..."),
+                inMethod = self.checkSystem, outMethod = self.hideMessage)
+
+        resultWidget = self.pageWidget.getWidget(2).ui
+        resultWidget.c_package.hide()
+        resultWidget.c_disk.hide()
+        resultWidget.success.hide()
 
     def checkSystem(self):
-        self.button_next.setEnabled(False)
-        self.button_previous.setEnabled(False)
+        # self.button_next.setEnabled(False)
+        # self.button_previous.setEnabled(False)
         self.showMessage("Checking your system...")
+        repoWidget = self.pageWidget.getWidget(1).ui
+        for repo in ('stable', 'devel', 'testing'):
+            if getattr(repoWidget, repo).isChecked():
+                self.target_repo = REPO_TEMPLATE % repo
+
+        self.thread.start()
+
+    def findMissingPackages(self):
+        self.missing_packages = findMissingPackagesForDistupdate(self.target_repo)
+
+    def showResults(self):
+        resultWidget = self.pageWidget.getWidget(2).ui
+        if self.missing_packages:
+            resultWidget.package_list.clear()
+            resultWidget.c_package.show()
+            resultWidget.package_list.addItems(self.missing_packages)
+        else:
+            resultWidget.success.show()
+        self.label_header.setText("Check results...")
+        self.hideMessage()
 
     def showMessage(self, message):
         self.msgbox.busy.busy()
         self.msgbox.setMessage(message)
-        self.msgbox.animate(start = TOPCENTER, stop = MIDCENTER)
+        if not self.msgbox.isVisible():
+            self.msgbox.animate(start = MIDCENTER, stop = MIDCENTER)
 
     def hideMessage(self):
         if self.msgbox.isVisible():
-            self.msgbox.animate(start = CURRENT, stop = BOTCENTER, direction = OUT)
+            self.msgbox.animate(start = CURRENT, stop = CURRENT, direction = OUT)
 
     def updateProgress(self, package, downloaded, total, symbol, percent):
         print package, downloaded, total, symbol, percent
