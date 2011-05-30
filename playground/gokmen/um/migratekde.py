bae7b8d4a932f9a07d5f4d316cb3bd07ddd6a8e8
gokcen.eraslan <gokcen.eraslan@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2011-05-30 10:21:20 +0000

    Create old KDE dirs first, and switch dst and src.

--- playground/gokmen/um/migratekde.py
+++ playground/gokmen/um/migratekde.py
@@ -5,32 +5,37 @@ import os
 import pwd
 
 globalSymlinkList = [
-("/usr/kde/4/bin", "/usr/bin"),
-("/usr/kde/4/env", "/usr/share/kde4/env"),
-("/usr/kde/4/include", "/usr/include/kde4"),
-("/usr/kde/4/lib", "/usr/lib"),
-("/usr/kde/4/share/applications", "/usr/share/applications"),
-("/usr/kde/4/share/apps", "/usr/share/kde4/apps"),
-("/usr/kde/4/share/autostart", "/usr/share/autostart"),
-("/usr/kde/4/share/config", "/usr/share/kde4/config"),
-("/usr/kde/4/share/config.kcfg", "/usr/share/config.kcfg"),
-("/usr/kde/4/share/desktop-directories", "/usr/share/desktop-directories"),
-("/usr/kde/4/share/doc/HTML", "/usr/share/doc/kde4/html"),
-("/usr/kde/4/share/emoticons", "/usr/share/emoticons"),
-("/usr/kde/4/share/icons", "/usr/share/icons"),
-("/usr/kde/4/share/kde4", "/usr/share/kde4"),
-("/usr/kde/4/share/locale", "/usr/share/locale"),
-("/usr/kde/4/share/man", "/usr/share/man"),
-("/usr/kde/4/share/mime", "/usr/share/mime"),
-("/usr/kde/4/share/mimelnk", "/usr/share/mimelnk"),
-("/usr/kde/4/share/sounds", "/usr/share/sounds"),
-("/usr/kde/4/share/templates", "/usr/share/templates"),
-("/usr/kde/4/share/wallpapers", "/usr/share/wallpapers"),
-("/usr/kde/4/shutdown", "/usr/share/kde4/shutdown"),
+("/usr/kde/4/bin",                          "/usr/bin"),
+("/usr/kde/4/env",                          "/usr/share/kde4/env"),
+("/usr/kde/4/include",                      "/usr/include/kde4"),
+("/usr/kde/4/lib",                          "/usr/lib"),
+("/usr/kde/4/share/applications",           "/usr/share/applications"),
+("/usr/kde/4/share/apps",                   "/usr/share/kde4/apps"),
+("/usr/kde/4/share/autostart",              "/usr/share/autostart"),
+("/usr/kde/4/share/config",                 "/usr/share/kde4/config"),
+("/usr/kde/4/share/config.kcfg",            "/usr/share/config.kcfg"),
+("/usr/kde/4/share/desktop-directories",    "/usr/share/desktop-directories"),
+("/usr/kde/4/share/doc/HTML",               "/usr/share/doc/kde4/html"),
+("/usr/kde/4/share/emoticons",              "/usr/share/emoticons"),
+("/usr/kde/4/share/icons",                  "/usr/share/icons"),
+("/usr/kde/4/share/kde4",                   "/usr/share/kde4"),
+("/usr/kde/4/share/locale",                 "/usr/share/locale"),
+("/usr/kde/4/share/man",                    "/usr/share/man"),
+("/usr/kde/4/share/mime",                   "/usr/share/mime"),
+("/usr/kde/4/share/mimelnk",                "/usr/share/mimelnk"),
+("/usr/kde/4/share/sounds",                 "/usr/share/sounds"),
+("/usr/kde/4/share/templates",              "/usr/share/templates"),
+("/usr/kde/4/share/wallpapers",             "/usr/share/wallpapers"),
+("/usr/kde/4/shutdown",                     "/usr/share/kde4/shutdown"),
 ]
 
 def migrateKDE():
-    for src, dst in globalSymlinkList:
+    try:
+        os.makedirs("/usr/kde/4/share/doc") #create old KDE dirs, if they don't exist
+    except OSError:
+        pass
+
+    for dst, src in globalSymlinkList:
         try:
             os.symlink(src, dst)
         except OSError:
