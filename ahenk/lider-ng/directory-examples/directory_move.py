0ec1445e3a53dbe20826de4e226c9db5d1303186
Bahadır Kandemir <kandemir@gmail.com>
2011-06-15 10:26:53 +0000

    Examples for LDAP module

---
+++ ahenk/lider-ng/directory-examples/directory_move.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+    Example application for editing LDAP access control lists.
+"""
+
+# Standard modules
+import sys
+
+# LDAP configuration
+import ldap_conf
+
+# Directory module
+from directory import Connection
+
+
+def main():
+    """
+        Example that lists current policies, and adds a dummy policy.
+    """
+
+    host, domain, username, password = ldap_conf.parse()
+
+    domain = "dc=%s" % domain.replace(".", ",dc=")
+
+    api = Connection()
+    if not api.connect(host, domain):
+        print 'Unable to connect to server'
+        return -1
+
+    if not api.login_simple(username, password):
+        print 'Unable to login to server'
+        return -1
+
+    root = api.find_node_by_address('dc=users,dc=example,dc=org')
+    node = api.find_node_by_address('cn=user1,dc=example,dc=org')
+    api.move_node(node, root)
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
