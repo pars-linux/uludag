0887b1cc5cbdf6c7c7b6284f16573c4069a4504f
bahadir.kandemir <bahadir.kandemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2011-06-16 05:39:33 +0000

    Update view in every inheritance policy change

--- trunk/ahenk/lider-ng/lider/talk.py
+++ trunk/ahenk/lider-ng/lider/talk.py
@@ -73,7 +73,7 @@ class Talk(QtCore.QThread):
         self.username = username
         self.password = password
 
-        myJid = jid.JID('%s@%s' % (self.username, self.domain))
+        myJid = jid.JID('%s@%s/Ahenk' % (self.username, self.domain))
         factory = client.XMPPClientFactory(myJid, self.password)
         factory.clientConnectionLost = self.__event_connection_lost
         factory.clientConnectionFailed = self.__event_connection_failed
