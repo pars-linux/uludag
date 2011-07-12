f5feba9ad90a524bd3e6beede18f02f3bd250ea3
batuhan.bayrakci <batuhan.bayrakci@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2011-07-12 07:02:42 +0000

    QuickFormat's first release for KDE3

---
+++ playground/intern/2011/quickformat-for-KDE3/code/quickformat/I18N.py
@@ -0,0 +1,12 @@
+import gettext
+
+__trans = gettext.translation("quickformat", fallback=True)
+
+def i18n(*text):
+    if len(text) == 1:
+        return __trans.ugettext(text[0])
+    ttt = unicode(__trans.ugettext(text[0]))
+    for i in range(1,len(text)):
+        ttt = ttt.replace('%%%d' % i, unicode(text[i]))
+    return ttt
+
