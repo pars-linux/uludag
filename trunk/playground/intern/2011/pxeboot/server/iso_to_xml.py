893235f4535f227f1814c1cca5e125170162c1ce
tuncer.colak <tuncer.colak@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2011-07-12 13:50:42 +0000

    Added pisi-index extracting and installed size

--- trunk/playground/intern/2011/pxeboot/server/iso_to_xml.py
+++ trunk/playground/intern/2011/pxeboot/server/iso_to_xml.py
@@ -3,9 +3,9 @@
 
 import os, sys
 import iso9660
-
+import bz2, pycdio
 import xml.etree.cElementTree as iks
-
+import piksemel
 #get as xml tree from string 
 def getTree(item, level = 0):
     i = "\n" + level * "    "
@@ -22,6 +22,40 @@ def getTree(item, level = 0):
         if level and (not item.tail or not item.tail.strip()):
             item.tail = i
 
+def ceil(x):
+    return int(round(x+0.5))
+
+#Extracting pisi-index.xml from .iso
+def extractIndex():
+    local_filename = "repo/pisi-index.xml.bz2"
+    statbuf = iso.stat (local_filename,True)
+    if statbuf is None:
+        print "Couldn't get ISO-9660 file information for file"  %local_filename
+        iso.close()
+        sys.exit(1)
+    a = ""
+    blocks = ceil(statbuf['size'] / pycdio.ISO_BLOCKSIZE)
+    for i in range(blocks):
+        lsn = statbuf['LSN'] + i
+        size, buf= iso.seek_read (lsn)
+        a += buf
+
+        if size <= 0 :
+            print "Error reading ISO 9660 file %s at LSN %d" % (local_filename, lsn)
+            sys.exit(1)
+    data = bz2.decompress(a)
+    return data
+
+def parseXml():
+    xmlStr = extractIndex()
+    doc = piksemel.parseString(xmlStr)
+    a = 0
+    for tags in doc.tags("Package"):
+        size = int(tags.getTagData("InstalledSize"))
+        a += int(size)
+    return a
+
+
 #if none given
 isoFolder = os.getcwd()
 
@@ -34,24 +68,25 @@ treeString = ""
 
 #get .iso file from directory
 filelist=[file for file in os.listdir(isoFolder) if file.lower().endswith(".iso")]
+
 if len(filelist) == 0:
     print "There is no ISO image file in '%s'" %isoFolder
     print "usage : %s [Path]" %sys.argv[0]
     sys.exit(1)
 
 for files_name in filelist: 
-        iso = iso9660.ISO9660.IFS ( source = isoFolder+"/"+files_name)
+        iso = iso9660.ISO9660.IFS ( source = os.path.join(isoFolder,files_name))
         name = iso.get_volume_id()
         print "ISO Image Name : %s" % name
-        isopath = isoFolder +"/"+ files_name
-        print "Path : %s" % isopath 
-        
-        isosize = os.path.getsize(isopath)/(1024*1024)
+        isopath = os.path.join(isoFolder,files_name)
+        print "Path : %s" % isopath
+
+        isosize = parseXml()
         print "Size : %s MB" %isosize
         version = "default"
 
         root = iks.Element("ISO9660")
-        
+
         name_tag = iks.SubElement(root,"Name")
         name_tag.text = name
 
@@ -62,12 +97,13 @@ for files_name in filelist:
         path_tag.text = isopath
 
         size_tag = iks.SubElement(root,"Size")
-        isosize = "%s MB" %isosize
+        isosize = "%s " %isosize
         size_tag.text = isosize
 
         getTree(root)
         treeString += iks.tostring(root)
         treeString +="\n"
+
 xmlfile = open("ISO Files.xml","w")
 xmlfile.write(treeString)
 xmlfile.close()
