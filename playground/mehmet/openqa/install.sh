2608ec6955fda45c562180d595f7bfa9117f8901
mehmet.ozdemir <mehmet.ozdemir@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2011-07-13 14:55:50 +0000

    fix dot misunderstanding directory

--- playground/mehmet/openqa/install.sh
+++ playground/mehmet/openqa/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 wget cekirdek.pardus.org.tr/~ckilimci/Pardus-2011.1-Beta-x86_64.iso
-RAIDLEVEL=10 DISTRI=pardus . Pardus-2011.1-Beta-x86_64.iso
+RAIDLEVEL=10 DISTRI=pardus ./ Pardus-2011.1-Beta-x86_64.iso
 
