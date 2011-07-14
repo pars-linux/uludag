f3b75c0a90f02d70845bb8e800d319a3774c29d4
tuncer.colak <tuncer.colak@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2011-07-14 08:45:41 +0000

    created urwid class

--- trunk/playground/intern/2011/pxeboot/server/iso_to_xml.py
+++ trunk/playground/intern/2011/pxeboot/server/iso_to_xml.py
@@ -9,99 +9,125 @@ import piksemel
 import urwid
 import urwid.raw_display
 
-def selectionmenu(filelist):
-    palette = [
-            ('header', 'black,underline', 'light gray', 'standout,underline',
-                'black,underline', '#88a'),
-            ('panel', 'light gray', 'dark blue', '',
-                '#ffd', '#00a'),
-            ('focus', 'light gray', 'dark red', 'standout')
-            ]
 
-    screen = urwid.raw_display.Screen()
-    screen.register_palette(palette)
+class getMenu():
 
-    lb = urwid.SimpleListWalker([])
+    def selectionmenu(self,filelist):
+        palette = [
+                ('header', 'black,underline', 'light gray', 'standout,underline',
+                    'black,underline', '#88a'),
+                ('panel', 'light gray', 'dark blue', '',
+                    '#ffd', '#00a'),
+                ('focus', 'light gray', 'dark red', 'standout')
+                ]
 
-    editedList = []
+        screen = urwid.raw_display.Screen()
+        screen.register_palette(palette)
 
-    def focus(widget):
-        return urwid.AttrMap(widget, None, 'focs')
+        listbox = urwid.SimpleListWalker([])
+        editedList = []
 
-    def selected_item (rb, state, user_data):
-        print "aaa %s" %editedList
-        if state:
-            return editedList 
+        def focus(widget):
+            return urwid.AttrMap(widget, None, 'focus')
 
-    def rb_state(text,i):
-        rb=urwid.CheckBox(files,False,False)
-        urwid.connect_signal(rb, 'change',selected_item,files)
-        return focus(rb)
+        def selected_item (cb, state):
+            if state==True:
+                editedList.append(cb.get_label())
+            else:
+                editedList.remove(cb.get_label())
 
-    def click_exit(button):
-        raise urwid.ExitMainLoop()
+        def createCB(text,i):
+            cb=urwid.CheckBox(files, False, has_mixed=False)
+            urwid.connect_signal(cb, 'change', selected_item)
+            return focus(cb)
 
-    lb.extend([
-        urwid.AttrMap(urwid.Text("Select Version"),'header')])
-
-    i = 1
-    for files in filelist:
-        lb.extend([urwid.AttrMap(
-            urwid.Columns([
-                urwid.Pile([
-                    rb_state(files,i)
-                    ]),
-                ]),'panel')
-            ])
-
-        i = i + 1
-        print files
-    lb.extend([urwid.AttrMap(
-        urwid.Columns([
-            urwid.Pile([
-                focus(urwid.Button("Exit",click_exit)),
-                ])
-            ]),'panel')
-        ])
-
-    def unhandled_input(key):
-        if key in ('Q','q','esc'):
+        def click_exit(button):
             raise urwid.ExitMainLoop()
 
-    urwid.MainLoop(urwid.ListBox(lb), screen=screen,
-        unhandled_input = unhandled_input).run()
-    print "editlist",editedList
-    return editedList
+        blank = urwid.Divider()
+
+        listbox.extend([
+                    blank,
+                    blank,
+                    urwid.Padding(
+                        urwid.AttrMap(
+                            urwid.Text("Select Version",align='center'),'header'),
+                            ('fixed left',35),('fixed right',35)),
+                    blank,
+                    blank
+                 ])
+
+        i = 1
+
+        for files in filelist:
+            listbox.extend([
+                urwid.Padding(
+                urwid.AttrMap(
+                    urwid.Columns([
+                        urwid.Pile([
+                            createCB(files,i)
+                            ]),
+                        ]),'panel'),('fixed left',60),('fixed right',60)),
+                blank
+                    ])
+
+            i = i + 1
+
+        listbox.extend([
+            urwid.Padding(
+                urwid.GridFlow([
+                    urwid.AttrWrap(
+                        urwid.Button("OK", click_exit),
+                                  'panel','focus')
+                               ],10, 2, 2, 'center'),
+                         ('fixed left',5), ('fixed right',5)),
+                  ])
+
+        def unhandled_input(key):
+            if key in ('Q','q','esc'):
+                raise urwid.ExitMainLoop()
+
+        urwid.MainLoop(urwid.ListBox(listbox), screen=screen,
+                        unhandled_input = unhandled_input).run()
+
+        return editedList
+
 
 #get as xml tree from string 
 def getTree(item, level = 0):
     i = "\n" + level * "    "
+
     if len(item):
         if not item.text or not item.text.strip():
             item.text = i + "   "
+
         for e in item:
             getTree(e, level + 1)
             if not e.tail or not e.tail.strip():
                 e.tail = i + "  "
+
         if not e.tail or not e.tail.strip():
             e.tail = i
+
     else:
         if level and (not item.tail or not item.tail.strip()):
             item.tail = i
 
 def ceil(x):
-    return int(round(x+0.5))
+    return int( round ( x + 0.5 ) )
 
-#Extracting pisi-index.xml from .iso
-def extractIndex():
-    local_filename = "repo/pisi-index.xml.bz2"
+#Extracting 'pisi-index.xml' and 'gfxboot.cfg' from .iso
+def extractData(local_filename):
     statbuf = iso.stat (local_filename,True)
+
     if statbuf is None:
         print "Couldn't get ISO-9660 file information for file"  %local_filename
         iso.close()
         sys.exit(1)
+
     a = ""
     blocks = ceil(statbuf['size'] / pycdio.ISO_BLOCKSIZE)
+
     for i in range(blocks):
         lsn = statbuf['LSN'] + i
         size, buf= iso.seek_read (lsn)
@@ -110,11 +136,16 @@ def extractIndex():
         if size <= 0 :
             print "Error reading ISO 9660 file %s at LSN %d" % (local_filename, lsn)
             sys.exit(1)
-    data = bz2.decompress(a)
-    return data
+
+    if local_filename == "repo/pisi-index.xml.bz2":
+        data = bz2.decompress(a)
+        return data
+    else :
+        return a.split("distro=")[1].split(" [")[0]
+
 
 def parseXml():
-    xmlStr = extractIndex()
+    xmlStr = extractData("repo/pisi-index.xml.bz2")
     doc = piksemel.parseString(xmlStr)
     a = 0
     for tags in doc.tags("Package"):
@@ -135,42 +166,46 @@ treeString = ""
 
 #get .iso file from directory
 filelist=[files for files in os.listdir(isoFolder) if files.lower().endswith(".iso")]
-filelist=selectionmenu(filelist)
+
 if len(filelist) == 0:
     print "There is no ISO image file in '%s'" %isoFolder
     print "usage : %s [Path]" %sys.argv[0]
     sys.exit(1)
 
-for files_name in filelist: 
-        iso = iso9660.ISO9660.IFS ( source = os.path.join(isoFolder,files_name))
-        name = iso.get_volume_id()
-        print "ISO Image Name : %s" % name
-        isopath = os.path.join(isoFolder,files_name)
-        print "Path : %s" % isopath
+listObject = getMenu()
 
-        isosize = parseXml()
-        print "Size : %s MB" %isosize
-        version = "default"
+filelist = listObject.selectionmenu(filelist)
+
+for files_name in filelist: 
+        iso = iso9660.ISO9660.IFS ( source = os.path.join( isoFolder,files_name ) )
 
         root = iks.Element("ISO9660")
 
-        name_tag = iks.SubElement(root,"Name")
+        name = extractData("boot/isolinux/gfxboot.cfg")
+        name_tag = iks.SubElement(root, "Name")
         name_tag.text = name
 
-        version_tag = iks.SubElement(root,"Version")
-        version_tag.text = version
+        if (name.find("x86_64") > 0):
+            architecture = "x64"
+        else:
+            architecture = "x86"
 
+        architecture_tag = iks.SubElement(root, "Architecture")
+        architecture_tag.text = architecture
+
+        isopath = os.path.join(isoFolder, files_name)
         path_tag = iks.SubElement(root, "Path")
-        path_tag.text = isopath
+        path_tag.text = unicode(isopath)
 
-        size_tag = iks.SubElement(root,"Size")
-        isosize = "%s " %isosize
+        isosize = parseXml()
+        isosize = "%s" %isosize
+        size_tag = iks.SubElement(root, "Size")
         size_tag.text = isosize
 
         getTree(root)
         treeString += iks.tostring(root)
-        treeString +="\n"
+        treeString += "\n"
 
-xmlfile = open("ISO Files.xml","w")
+xmlfile = open("pxeboot_iso_files.xml","w")
 xmlfile.write(treeString)
 xmlfile.close()
