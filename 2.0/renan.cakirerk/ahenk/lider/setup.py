9786be974364523a606f80c3cda51bd437bfe148
mete <mete@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2011-08-07 11:32:34 +0000

    New release

--- 2.0/renan.cakirerk/ahenk/lider/setup.py
+++ 2.0/renan.cakirerk/ahenk/lider/setup.py
@@ -4,7 +4,7 @@
 from distutils.core import setup
 
 setup(name='ahenk-lider',
-      version='1.9.26',
+      version='1.9.30',
       description='Agent for Ahenk Remote Management Framework',
       author='Bahadır Kandemir',
       author_email='bahadir@pardus.org.tr',
