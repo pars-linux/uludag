d4202e426152a0e7cbf532f2dec1d25f4d80a9f7
Bahadır Kandemir <kandemir@gmail.com>
2011-08-07 16:16:18 +0000

    New release

--- 2.0/bahadir.kandemir/ahenk/ajan/setup.py
+++ 2.0/bahadir.kandemir/ahenk/ajan/setup.py
@@ -4,7 +4,7 @@
 from distutils.core import setup
 
 setup(name='ahenk-agent',
-      version='1.9.26',
+      version='1.9.27',
       description='Agent for Ahenk Remote Management Framework',
       author='Bahadır Kandemir',
       author_email='bahadir@pardus.org.tr',
