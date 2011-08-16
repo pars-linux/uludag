7e956b6c6fe602e8b843964cc88ecbcb9f29804c
sinem.oguz <sinem.oguz@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2011-08-16 13:12:38 +0000

    I have made my second commit

--- trunk/playground/intern/2011/user-study-tool/main.py
+++ trunk/playground/intern/2011/user-study-tool/main.py
@@ -5,11 +5,12 @@ import sys
 
 from PyQt4.QtCore import SIGNAL
 
+
 if __name__ == '__main__':
+ 
     from PyKDE4.kdeui import *
     from PyKDE4.kdecore import *
-
-    from myStandalone import SurveyManager
+    
     PACKAGE     = "Pardus Kullanım Araştırmaları"
     appName     = "user-study"
     modName     = "userstudy"
@@ -29,6 +30,9 @@ if __name__ == '__main__':
     aboutData.addAuthor(ki18n("Gökmen Göksel"), ki18n("Current Maintainer"))
     aboutData.setTranslator(ki18nc("NAME OF TRANSLATORS", "Your names"), ki18nc("EMAIL OF TRANSLATORS", "Your emails"))
 
+    #if Pds.session == pds.Kde4:
+   
+    from myStandalone import SurveyManager
     # Set Command-line arguments
     KCmdLineArgs.init(sys.argv, aboutData)
 
@@ -38,6 +42,21 @@ if __name__ == '__main__':
     # Create Main Widget
     mainWindow = SurveyManager(None, appName)
     mainWindow.show()
+#else:
+    #from mainWin import MainManager
+
+    ## Pds Stuff
+    #from pds.quniqueapp import QUniqueApplication
+    
+    #app = QUniqueApplication(sys.argv, catalog = appName)
+    
+    ## Create Main Widget and make some settings
+    #mainWindow = MainManager(None)
+    #mainWindow.show()
+    #mainWindow.resize(640, 480)
+    #mainWindow.setWindowTitle(i18n(about.PACKAGE))
+        
+  
 
     app.connect(app, SIGNAL('lastWindowClosed()'), app.quit)
 
--- trunk/playground/intern/2011/user-study-tool/mainWin.py
+++ trunk/playground/intern/2011/user-study-tool/mainWin.py
@@ -6,9 +6,13 @@ import json
 
 from PyQt4 import QtGui
 from PyQt4.QtCore import *
+from PyQt4 import QtWebKit
+
+from pds.gui import *
 
 from myWidgets import SurveyItem,SurveyItemWidget
 from ui_mainMenu import Ui_mainManager
+from ui_detailWidget import Ui_InfoWidget 
 
 class MainManager(QtGui.QWidget):
     def __init__(self, parent, standAlone=True):
@@ -28,17 +32,29 @@ class MainManager(QtGui.QWidget):
 	self.data = json.load(json_object)
 	json_object.close()
 	
+	self.info = UserStudyItemInfo(self.ui.surveyList)
 		
         for survey in self.data["pardusUserStudyList"]:
             item = SurveyItem(survey["title"], self.ui.surveyList)
             item.setFlags(Qt.NoItemFlags | Qt.ItemIsEnabled)
             item.setSizeHint(QSize(38,48))
-	    self.widgets[survey["id"]] = SurveyItemWidget(survey["title"], self, item)
+            item.url = QUrl(survey["url"])
+	    w = SurveyItemWidget(survey["title"], self, item, self.ui.surveyList)
+	   
+	    self.widgets[survey["id"]] = w
             self.ui.surveyList.setItemWidget(item, self.widgets[survey["id"]])
+            self.connect(w.ui.infoButton, SIGNAL("clicked()"), w.showDescription)
+            
+           
             
         self.connect(self.ui.alwaysHelp, SIGNAL("clicked()"), self.setGlobalParticipation)
         self.connect(self.ui.askToHelp, SIGNAL("clicked()"), self.setGlobalParticipation)
 	self.connect(self.ui.rejectHelp, SIGNAL("clicked()"), self.setGlobalParticipation)
+	
+	#self.connect(self.ui.surveyList, SIGNAL("itemSelectionChanged()"),self.showDescription)
+	#self.info.clicked.connect(self.hideDescription)
+	
+	
             
     def setGlobalParticipation(self):
 	json_data =  open('user_studies.json','r')
@@ -46,15 +62,57 @@ class MainManager(QtGui.QWidget):
 	json_data.close()
 	
 	if self.ui.alwaysHelp.isChecked() == True:
-	  self.data["userParticipation"] = "Always Join" 
+	    self.data["userParticipation"] = "Always Join" 
 	elif self.ui.askToHelp.isChecked() == True:
-	  self.data["userParticipation"] = "Ask Before Join" 
+	    self.data["userParticipation"] = "Ask Before Join" 
 	else :
-	  self.data["userParticipation"] = "Do not Join" 
+	    self.data["userParticipation"] = "Do not Join" 
 	
 	f = open('user_studies2.json','w')
 	string = json.dump(self.data,f, indent = 2)
 	f.close()
+	
+    def showDescription(self, url):
+	self.info.ui.webView.load(url)
+	self.info.resize(self.ui.surveyList.size())
+	self.info.animate(start = MIDLEFT, stop = MIDCENTER)
+	QtGui.qApp.processEvents()
+	
+    
+
+class UserStudyItemInfo(PAbstractBox):
+  
+    def __init__(self, parent):
+	PAbstractBox.__init__(self, parent)
+	
+	self.ui = Ui_InfoWidget()
+	self.ui.setupUi(self)
+	
+        self.ui.webView = DetailPage(self, self)
+        self.ui.gridLayout.addWidget(self.ui.webView, 0, 0, 1, 1)
+	
+	self._animation = 2
+	self._duration = 500
+	
+	self.enableOverlay()
+	self.hide()
+	
+   
+class DetailPage(QtWebKit.QWebView):
+    def __init__(self, parent,  pdsWidget):
+	QtWebKit.QWebView.__init__(self, parent)
+	self.pdsWidget = pdsWidget
+	
+    def mousePressEvent(self, event):
+	if self.isVisible():
+	    self.pdsWidget.animate(start = MIDCENTER,
+			stop  = MIDRIGHT,
+			direction = OUT)
+	
+	
+	
+    
+	
 	
 	
 	  
