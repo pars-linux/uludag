47f0bf087f38bac735856ef2dfb07ca5a3b36d1a
erdi.gurbuz <erdi.gurbuz@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2011-08-22 09:00:09 +0000

    fixed bug setup.sh

--- trunk/playground/intern/2011/pxeboot/setup.sh
+++ trunk/playground/intern/2011/pxeboot/setup.sh
@@ -5,16 +5,14 @@ echo "1- ISO xml olusturuldu..."
 
 
 echo "2 - pxebootselect derleniyor..."
-gcc -g -Wall `pkg-config libxml-2.0 --cflags --libs` client/pxebootselect/pxebootselect.c -o client/pxebootselect/pxebootselect -lncurses -lmenu
-
+gcc -g -Wall `pkg-config libxml-2.0 --cflags --libs` client/pxe_select_boot/pxebootselect.c -o client/pxe_select_boot/pxebootselect -lncurses -lmenu
 
 echo "3 -Dosyalar kopyalanıyor..."
 cp client/initramfs/init /lib/initramfs
 cp client/initramfs/hotplug /lib/initramfs
 cp client/initramfs/profile.rc /lib/initramfs
 cp client/initramfs/udhcpc.script /lib/initramfs
-cp client/pxebootselect/pxebootselect /lib/initramfs
-cp client/initramfs/checkfirstload /lib/initramfs
+cp client/pxe_select_boot/pxebootselect /lib/initramfs
 
 echo "4 - Ilk yuklenecek initramfs olusturuluyor..."
 python client/initramfs/mkinitramfs -o /tftpboot/pts/latest-ptsp/ --pxeboot --network
