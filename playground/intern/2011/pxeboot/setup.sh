abeab51cb71eed169d776f165b206ef9e80cd521
erdi.gurbuz <erdi.gurbuz@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2011-08-22 12:45:44 +0000

    deleted redundancy in the setup.sh

--- playground/intern/2011/pxeboot/setup.sh
+++ playground/intern/2011/pxeboot/setup.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 python server/iso_to_xml.py /opt/ptsp
-echo "1- ISO xml olusturuldu..."
+echo "1 - ISO xml olusturuldu..."
 
 
 echo "2 - pxebootselect derleniyor..."
 gcc -g -Wall `pkg-config libxml-2.0 --cflags --libs` client/pxe_select_boot/pxebootselect.c -o client/pxe_select_boot/pxebootselect -lncurses -lmenu
 
-echo "3 -Dosyalar kopyalanıyor..."
+echo "3 - Dosyalar kopyalanıyor..."
 cp client/initramfs/init /lib/initramfs
 cp client/initramfs/hotplug /lib/initramfs
 cp client/initramfs/profile.rc /lib/initramfs
-cp client/initramfs/udhcpc.script /lib/initramfs
 cp client/pxe_select_boot/pxebootselect /lib/initramfs
 
 echo "4 - Ilk yuklenecek initramfs olusturuluyor..."
