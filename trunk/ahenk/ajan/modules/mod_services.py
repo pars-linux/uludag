9aa29e848bf9665a311b72cd6642aa5f3e7df26c
beyza.ermis <beyza.ermis@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2011-10-18 12:55:08 +0000

    Activated warning messages

--- trunk/ahenk/ajan/modules/mod_services.py
+++ trunk/ahenk/ajan/modules/mod_services.py
@@ -33,6 +33,10 @@ def process(message, options):
             if "serviceStart" in message.policy:
                 start_set = []
                 for packages in message.policy["serviceStart"]:
+
+                    print "start packages : "
+                    print packages
+
                     try:
                         for package in packages.split(","):
                             start_set.append(package)
@@ -41,16 +45,20 @@ def process(message, options):
                         break
 
                     for package in start_set:
-                        #try:
-                        link = comar.Link()
-                        link.System.Service[package].start()
-                        #message.reply("service.start.status", "Successful")
-                        #except DBusException, e:
-                            #message.reply("service.start.status", str(e))
+                        try:
+                            link = comar.Link()
+                            link.System.Service[package].start()
+                            message.reply("service.start.status", "Successful")
+                        except DBusException, e:
+                            message.reply("service.start.status", str(e))
 
             if "serviceStop" in message.policy:
                 stop_set = []
                 for packages in message.policy["serviceStop"]:
+
+                    print "stop packages : "
+                    print packages
+
                     try:
                         for package in packages.split(","):
                             stop_set.append(package)
@@ -59,10 +67,10 @@ def process(message, options):
                         break
 
                     for package in stop_set:
-                        #try:
-                        link = comar.Link()
-                        link.System.Service[package].stop()
-                        #message.reply("service.stop.status", "Successful")
-                        #except DBusException, e:
-                            #message.reply("service.stop.status", str(e))
+                        try:
+                            link = comar.Link()
+                            link.System.Service[package].stop()
+                            message.reply("service.stop.status", "Successful")
+                        except DBusException, e:
+                            message.reply("service.stop.status", str(e))
 
