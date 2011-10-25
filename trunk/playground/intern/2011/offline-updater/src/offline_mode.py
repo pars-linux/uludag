a892ef783d6f2694932783bc39581b879f312229
metin.akdere <metin.akdere@eb7592d8-20c1-4dec-9ef4-1051545c7cc8>
2011-10-25 11:40:34 +0000

    Some packages may not be installed from the repository, ignore them for now on

--- trunk/playground/intern/2011/offline-updater/src/offline_mode.py
+++ trunk/playground/intern/2011/offline-updater/src/offline_mode.py
@@ -46,23 +46,24 @@ class Offline(QtGui.QWidget):
         packagedb = pisi.db.packagedb.PackageDB()
         listPackages = installdb.list_installed()
         cnt = 0
-        
         packages = {}
         for i in listPackages:
-            packages[installdb.get_package(i).name] = (installdb.get_package(i).release, 
-                                                       packagedb.get_package_repo(installdb.get_package(i).name)[1], 
-                                                       installdb.get_package(i).version)
+            try:
+                packages[installdb.get_package(i).name] = (installdb.get_package(i).release, 
+                                                           packagedb.get_package_repo(installdb.get_package(i).name)[1], 
+                                                           installdb.get_package(i).version)
+            except:
+                pass
+
             if cnt == len(listPackages)/100:
                 self.ui.progressBar.setValue(self.ui.progressBar.value()+math.ceil(100/float(len(listPackages))))
                 cnt = 0
                 print self.ui.progressBar.value()
             cnt +=1
             QtGui.QApplication.processEvents()
-           
         filePackages = open(self.ui.le_path_export.text()+"/packageList.ofu","w")
         cPickle.dump(packages, filePackages, protocol=0)
         filePackages.close()
-       
         repo_list = {}
         i = 0
         cnt = 0
@@ -74,15 +75,12 @@ class Offline(QtGui.QWidget):
                 cnt=0
             cnt += 1
             QtGui.QApplication.processEvents()
-        
         fileRepos = open(self.ui.le_path_export.text()+"/repoList.ofu","w")
         cPickle.dump(repo_list, fileRepos, protocol = 0)
         fileRepos.close()
-            
         #print repo_list
         self.ui.progressBar.setValue(100)
         self.ui.pb_action.setEnabled(True)
-        
     def setupPackages(self):
         packageList = []
         for dirname, dirnames, filenames in os.walk(str(self.ui.le_path_setup.text())+'/packages'):
